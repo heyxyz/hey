71da69ab7ac6b34ea8443ad90a8448a111cd5894
yoginth.eth <yoginth@hey.com>
2022-03-18 21:22:48 +0530

    feat: add lens graphql

---
+++ src/graphql/MirrorFragment.ts
@@ -0,0 +1,43 @@
+import { gql } from '@apollo/client'
+
+import { PublicationProfileFragment } from './PublicationProfileFragment'
+
+export const MirrorFragment = gql`
+  fragment MirrorFragment on Mirror {
+    pubId: id
+    profile {
+      ...PublicationProfileFragment
+    }
+    stats {
+      totalAmountOfComments
+      totalAmountOfMirrors
+      totalAmountOfCollects
+    }
+    metadata {
+      content
+      description
+      media {
+        original {
+          url
+          mimeType
+        }
+      }
+    }
+    mirrorOf {
+      ... on Post {
+        id
+        profile {
+          handle
+        }
+      }
+      ... on Comment {
+        id
+        profile {
+          handle
+        }
+      }
+    }
+    createdAt
+  }
+  ${PublicationProfileFragment}
+`
