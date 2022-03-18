d021dbd833a9a014aeaff3eda77044ae5e4d3eef
bigint <bigint@lenster.xyz>
2022-03-18 21:11:03 +0530

    feat: add constants and style.css

---
+++ src/apollo.ts
@@ -0,0 +1,89 @@
+import {
+  ApolloClient,
+  ApolloLink,
+  HttpLink,
+  InMemoryCache
+} from '@apollo/client'
+import { lensStylePagination } from '@lib/lensStylePagination'
+import jwtDecode from 'jwt-decode'
+
+const REFRESH_AUTHENTICATION_MUTATION = `
+  mutation Refresh($request: RefreshRequest!) {
+    refresh(request: $request) {
+      accessToken
+      refreshToken
+    }
+  }
+`
+
+const httpLink = new HttpLink({
+  uri: 'https://api-mumbai.lens.dev',
+  fetch
+})
+
+const authLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem('accessToken')
+
+  operation.setContext({
+    headers: {
+      'x-access-token': token ? `Bearer ${token}` : ''
+    }
+  })
+
+  // @ts-ignore
+  const { exp }: { exp: any } = token ? jwtDecode(token) : ''
+
+  if (Date.now() >= exp * 1000) {
+    fetch('https://api-mumbai.lens.dev', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        operationName: 'Refresh',
+        query: REFRESH_AUTHENTICATION_MUTATION,
+        variables: {
+          request: { refreshToken: localStorage.getItem('refreshToken') }
+        }
+      })
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        operation.setContext({
+          headers: {
+            'x-access-token': token
+              ? `Bearer ${res?.data?.refresh?.accessToken}`
+              : ''
+          }
+        })
+        localStorage.setItem('accessToken', res?.data?.refresh?.accessToken)
+        localStorage.setItem('refreshToken', res?.data?.refresh?.refreshToken)
+      })
+  }
+
+  return forward(operation)
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          timeline: lensStylePagination(['request', ['profileId']]),
+          publications: lensStylePagination([
+            'request',
+            ['profileId', 'commentsOf', 'publicationTypes']
+          ]),
+          explorePublications: lensStylePagination([
+            'request',
+            ['sortCriteria']
+          ]),
+          followers: lensStylePagination(['request', ['profileId']]),
+          following: lensStylePagination(['request', ['address']]),
+          nfts: lensStylePagination(['request', ['ownerAddress', 'chainIds']])
+        }
+      }
+    }
+  })
+})
+
+export default client
---
+++ src/constants.ts
@@ -0,0 +1,22 @@
+// Environments
+export const IS_PRODUCTION = process.env.NODE_ENV === 'production'
+export const IS_DEVELOPMENT = process.env.NODE_ENV === 'development'
+export const IS_MAINNET = process.env.IS_MAINNET === 'true'
+
+// Messages
+export const ERROR_MESSAGE = 'Something went wrong!'
+export const CONNECT_WALLET = 'Please connect your wallet.'
+export const WRONG_NETWORK = 'Please change network to Polygon Mumbai Testnet.'
+
+// URLs
+export const STATIC_ASSETS = 'https://assets.lenster.xyz/images'
+
+// Web3
+export const INFURA_ID = '3d19324a72854976a7160e0e2ebc9c2b'
+
+// Addresses
+export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+export const LENSHUB_PROXY = '0xd7B3481De00995046C7850bCe9a5196B7605c367'
+export const FOLLOWNFT = '0x416a7d147e3e128f5ecc61a92676c1df2d4ac96a'
+export const REVERT_COLLECT_MODULE =
+  '0x98dfAB2360352D9Da122b5F43a4a4fa5D3Ce25a3'
