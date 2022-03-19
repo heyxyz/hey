55a45880f820c047e8435712dc3c8281dc94243f
yoginth.eth <yoginth@hey.com>
2022-03-19 11:37:09 +0530

    feat: add allowance module

--- src/components/Settings/Allowance/Allowance.tsx
+++ src/components/Settings/Allowance/Allowance.tsx
@@ -1,12 +1,10 @@
 import { gql, useLazyQuery } from '@apollo/client'
-import { Button } from '@components/UI/Button'
-import { Card, CardBody } from '@components/UI/Card'
-import GetModuleIcon from '@components/utils/GetModuleIcon'
+import { CardBody } from '@components/UI/Card'
 import { ApprovedAllowanceAmount } from '@generated/types'
-import { MinusIcon, PlusIcon } from '@heroicons/react/outline'
-import { getModule } from '@lib/getModule'
 import React from 'react'
 import { useTransaction } from 'wagmi'
+
+import Module from './Module'
 
 const GENERATE_ALLOWANCE_QUERY = gql`
   query GenerateModuleCurrencyApprovalData(
@@ -53,43 +51,8 @@ const Allowance: React.FC<Props> = ({ allowance }) => {
   return (
     <CardBody className="space-y-4">
       {allowance?.approvedModuleAllowanceAmount?.map(
-        (item: ApprovedAllowanceAmount) => (
-          <Card key={item.module}>
-            <CardBody className="flex items-center justify-between">
-              <div>
-                <div className="flex items-center space-x-3">
-                  <div className="text-brand-500">
-                    <GetModuleIcon module={item.module} size={4} />
-                  </div>
-                  <div className="font-bold">{getModule(item.module).name}</div>
-                </div>
-                <div className="text-sm text-gray-500">
-                  {item.contractAddress}
-                </div>
-              </div>
-              {item.allowance === '0x00' ? (
-                <Button
-                  variant="success"
-                  icon={<PlusIcon className="w-4 h-4" />}
-                  onClick={() =>
-                    handleAllowance(item.currency, '10000000000', item.module)
-                  }
-                >
-                  Allow
-                </Button>
-              ) : (
-                <Button
-                  variant="warning"
-                  icon={<MinusIcon className="w-4 h-4" />}
-                  onClick={() =>
-                    handleAllowance(item.currency, '0', item.module)
-                  }
-                >
-                  Revoke
-                </Button>
-              )}
-            </CardBody>
-          </Card>
+        (item: ApprovedAllowanceAmount, index: number) => (
+          <Module key={index} module={item} />
         )
       )}
     </CardBody>
