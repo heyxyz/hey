3925e8ef41213fbba0e4eb217602f1e9cd18d198
bigint <bigint@lenster.xyz>
2022-03-19 07:41:02 +0530

    feat: use outside click hook

---
+++ src/components/utils/hooks/useOnClickOutside.tsx
@@ -0,0 +1,34 @@
+import { RefObject, useEffect } from 'react'
+
+type AnyEvent = MouseEvent | TouchEvent
+
+function useOnClickOutside<T extends HTMLElement = HTMLElement>(
+  ref: RefObject<T>,
+  // eslint-disable-next-line no-unused-vars
+  handler: (event: AnyEvent) => void
+): void {
+  useEffect(() => {
+    const listener = (event: AnyEvent) => {
+      const el = ref?.current
+
+      // Do nothing if clicking ref's element or descendent elements
+      if (!el || el.contains(event.target as Node)) {
+        return
+      }
+
+      handler(event)
+    }
+
+    document.addEventListener(`mousedown`, listener)
+    document.addEventListener(`touchstart`, listener)
+
+    return () => {
+      document.removeEventListener(`mousedown`, listener)
+      document.removeEventListener(`touchstart`, listener)
+    }
+
+    // Reload only if ref or handler changes
+  }, [ref, handler])
+}
+
+export default useOnClickOutside
