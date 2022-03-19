a360371387b2bf2675745d2e499f09e4bbfd009a
yoginth.eth <yoginth@hey.com>
2022-03-19 10:05:00 +0530

    feat: refactor CollectionFragment

--- src/graphql/CollectionFragment.ts
+++ src/graphql/CollectionFragment.ts
@@ -1,111 +1,72 @@
 import { gql } from '@apollo/client'
 
-export const PostCollectionFragment = gql`
-  fragment PostCollectionFragment on Post {
-    collectModule {
-      ... on FeeCollectModuleSettings {
-        type
-        recipient
-        referralFee
-        amount {
-          asset {
-            symbol
-          }
-          value
+const CollectionFragment = gql`
+  fragment CollectionFragment on CollectModule {
+    ... on FeeCollectModuleSettings {
+      type
+      recipient
+      referralFee
+      amount {
+        asset {
+          symbol
         }
+        value
       }
-      ... on LimitedFeeCollectModuleSettings {
-        type
-        collectLimit
-        recipient
-        referralFee
-        amount {
-          asset {
-            symbol
-          }
-          value
+    }
+    ... on LimitedFeeCollectModuleSettings {
+      type
+      collectLimit
+      recipient
+      referralFee
+      amount {
+        asset {
+          symbol
         }
+        value
       }
-      ... on LimitedTimedFeeCollectModuleSettings {
-        type
-        collectLimit
-        recipient
-        endTimestamp
-        referralFee
-        amount {
-          asset {
-            symbol
-          }
-          value
+    }
+    ... on LimitedTimedFeeCollectModuleSettings {
+      type
+      collectLimit
+      recipient
+      endTimestamp
+      referralFee
+      amount {
+        asset {
+          symbol
         }
+        value
       }
-      ... on TimedFeeCollectModuleSettings {
-        type
-        recipient
-        endTimestamp
-        referralFee
-        amount {
-          asset {
-            symbol
-          }
-          value
+    }
+    ... on TimedFeeCollectModuleSettings {
+      type
+      recipient
+      endTimestamp
+      referralFee
+      amount {
+        asset {
+          symbol
         }
+        value
       }
     }
   }
 `
 
+export const PostCollectionFragment = gql`
+  fragment PostCollectionFragment on Post {
+    collectModule {
+      ...CollectionFragment
+    }
+  }
+  ${CollectionFragment}
+`
+
 export const CommentCollectionFragment = gql`
   fragment CommentCollectionFragment on Comment {
     collectModule {
-      ... on FeeCollectModuleSettings {
-        type
-        recipient
-        referralFee
-        amount {
-          asset {
-            symbol
-          }
-          value
-        }
-      }
-      ... on LimitedFeeCollectModuleSettings {
-        type
-        collectLimit
-        recipient
-        referralFee
-        amount {
-          asset {
-            symbol
-          }
-          value
-        }
-      }
-      ... on LimitedTimedFeeCollectModuleSettings {
-        type
-        collectLimit
-        recipient
-        endTimestamp
-        referralFee
-        amount {
-          asset {
-            symbol
-          }
-          value
-        }
-      }
-      ... on TimedFeeCollectModuleSettings {
-        type
-        recipient
-        endTimestamp
-        referralFee
-        amount {
-          asset {
-            symbol
-          }
-          value
-        }
-      }
+      ...CollectionFragment
     }
   }
+  ${CollectionFragment}
 `
