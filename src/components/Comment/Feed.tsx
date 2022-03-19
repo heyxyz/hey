65cea170cbff766a1855ee989c6843483d201ca8
yoginth.eth <yoginth@hey.com>
2022-03-19 07:37:41 +0530

    feat: add comments component

---
+++ src/components/Comment/Feed.tsx
@@ -0,0 +1,87 @@
+import { gql, useQuery } from '@apollo/client'
+import SinglePost from '@components/Post/SinglePost'
+import PostsShimmer from '@components/Shared/Shimmer/PostsShimmer'
+import { EmptyState } from '@components/UI/EmptyState'
+import { ErrorMessage } from '@components/UI/ErrorMessage'
+import { Spinner } from '@components/UI/Spinner'
+import { LensterPost } from '@generated/lenstertypes'
+import { CommentFragment } from '@gql/CommentFragment'
+import { CollectionIcon } from '@heroicons/react/outline'
+import React from 'react'
+import useInView from 'react-cool-inview'
+
+const PROFILE_FEED_QUERY = gql`
+  query ProfileFeed($request: PublicationsQueryRequest!) {
+    publications(request: $request) {
+      items {
+        __typename
+        ... on Comment {
+          ...CommentFragment
+        }
+      }
+      pageInfo {
+        next
+      }
+    }
+  }
+  ${CommentFragment}
+`
+
+interface Props {
+  post: LensterPost
+}
+
+const Feed: React.FC<Props> = ({ post }) => {
+  const { data, loading, error, fetchMore } = useQuery(PROFILE_FEED_QUERY, {
+    variables: {
+      request: { commentsOf: post.pubId, limit: 10 }
+    },
+    skip: !post.pubId
+  })
+
+  const pageInfo = data?.publications?.pageInfo
+  const { observe } = useInView({
+    threshold: 1,
+    onEnter: () => {
+      fetchMore({
+        variables: {
+          request: {
+            commentsOf: post.pubId,
+            cursor: pageInfo?.next,
+            limit: 10
+          }
+        }
+      })
+    }
+  })
+
+  if (loading) return <PostsShimmer />
+
+  if (data?.publications?.items?.length === 0)
+    return (
+      <EmptyState
+        message={<span>Be the first one to comment!</span>}
+        icon={<CollectionIcon className="w-8 h-8 text-brand-500" />}
+      />
+    )
+
+  return (
+    <>
+      {error && (
+        <ErrorMessage title="Failed to load comment feed" error={error} />
+      )}
+      <div className="space-y-3">
+        {data?.publications?.items?.map((post: LensterPost, index: number) => (
+          <SinglePost key={`${post.pubId}_${index}`} post={post} />
+        ))}
+      </div>
+      {pageInfo?.next && (
+        <span ref={observe} className="flex justify-center p-5">
+          <Spinner size="sm" />
+        </span>
+      )}
+    </>
+  )
+}
+
+export default Feed
---
+++ src/components/Pages/Thanks.tsx
@@ -0,0 +1,104 @@
+import Footer from '@components/Shared/Footer'
+import { HeartIcon } from '@heroicons/react/outline'
+import { useTheme } from 'next-themes'
+import React, { Fragment } from 'react'
+import { STATIC_ASSETS } from 'src/constants'
+
+interface Props {
+  name: string
+  logo: string
+  url: string
+  size: number
+  children: React.ReactNode
+}
+
+const Brand: React.FC<Props> = ({ name, logo, url, size, children }) => {
+  const { resolvedTheme } = useTheme()
+
+  return (
+    <div className="pt-10 space-y-5">
+      <img
+        className="mx-auto"
+        style={{ height: size }}
+        src={`${STATIC_ASSETS}/thanks/${logo}-${
+          resolvedTheme === 'dark' ? 'dark' : 'light'
+        }.svg`}
+        alt={`${name}'s Logo`}
+      />
+      <div className="pt-2">{children}</div>
+      <div>
+        <a className="font-bold" href={url} target="_blank" rel="noreferrer">
+          ➜ Go to {name}
+        </a>
+      </div>
+    </div>
+  )
+}
+
+const Thanks: React.FC = () => {
+  return (
+    <>
+      <div className="flex items-center justify-center w-full bg-brand-400 h-44">
+        <div className="relative text-center">
+          <div className="flex items-center space-x-2 text-3xl font-semibold text-white md:text-4xl">
+            <div>Thank you!</div>
+            <HeartIcon className="text-pink-600 h-7 w-7" />
+          </div>
+          <div className="text-white">for supporting our community</div>
+        </div>
+      </div>
+      <div className="relative">
+        <div className="flex justify-center">
+          <div className="relative mx-auto rounded-lg max-w-3/4 lg:w-2/4">
+            <div className="px-5 pb-10 space-y-10 text-center text-gray-900 divide-y max-w-none dark:text-gray-200 dark:divide-gray-800">
+              <Brand
+                name="Vercel"
+                logo="vercel"
+                url="https://vercel.com/?utm_source=Lenster&utm_campaign=oss"
+                size={40}
+              >
+                Vercel combines the best developer experience with an obsessive
+                focus on end-user performance. Vercel platform enables frontend
+                teams to do their best work.
+              </Brand>
+              <Brand
+                name="Upstash"
+                logo="upstash"
+                url="https://upstash.com"
+                size={55}
+              >
+                Upstash has REST API that enables access to Redis from
+                serverless and Edge functions like Cloudflare Workers and Fastly
+                Compute@Edge.
+              </Brand>
+              <Brand
+                name="Gitpod"
+                logo="gitpod"
+                url="https://gitpod.io"
+                size={50}
+              >
+                Gitpod streamlines developer workflows by providing prebuilt,
+                collaborative developer environments in your browser - powered
+                by VS Code.
+              </Brand>
+              <Brand
+                name="Netlify"
+                logo="netlify"
+                url="https://netlify.com"
+                size={50}
+              >
+                An intuitive Git-based workflow and powerful serverless platform
+                to build, deploy, and collaborate on web apps
+              </Brand>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center px-5 pt-2 pb-6">
+          <Footer />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Thanks
