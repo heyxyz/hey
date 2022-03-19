26e5166da8a737dddaaa0120a27265fae41e0681
yoginth.eth <yoginth@hey.com>
2022-03-19 16:34:57 +0530

    chore: use svg items

--- src/lib/getTokenImage.ts
+++ src/lib/getTokenImage.ts
@@ -1,15 +1,4 @@
-export const getTokenImage = (symbol: string) => {
-  switch (symbol) {
-    // Collect Modules
-    case 'WMATIC':
-      return 'https://assets.lenster.xyz/images/tokens/wmatic.webp'
-    case 'WETH':
-      return 'https://assets.lenster.xyz/images/tokens/weth.webp'
-    case 'USDC':
-      return 'https://assets.lenster.xyz/images/tokens/usdc.webp'
-    case 'DAI':
-      return 'https://assets.lenster.xyz/images/tokens/dai.webp'
-    default:
-      return 'https://assets.lenster.xyz/images/tokens/wmatic.webp'
-  }
-}
+import { STATIC_ASSETS } from 'src/constants'
+
+export const getTokenImage = (symbol: string) =>
+  `${STATIC_ASSETS}/tokens/${symbol.toLowerCase()}.svg`
