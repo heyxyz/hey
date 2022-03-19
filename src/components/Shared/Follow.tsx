16c63be05bb47590fb245b1c06def04f6ada7d5b
bigint <bigint@lenster.xyz>
2022-03-19 09:05:07 +0530

    feat: add followWithSig

--- src/components/Shared/Follow.tsx
+++ src/components/Shared/Follow.tsx
@@ -4,6 +4,8 @@ import { Button } from '@components/UI/Button'
 import { Spinner } from '@components/UI/Spinner'
 import { Profile } from '@generated/types'
 import { UserAddIcon } from '@heroicons/react/outline'
+import { omit } from '@lib/omit'
+import { splitSignature } from '@lib/splitSignature'
 import { Dispatch } from 'react'
 import toast from 'react-hot-toast'
 import {
@@ -12,14 +14,34 @@ import {
   LENSHUB_PROXY,
   WRONG_NETWORK
 } from 'src/constants'
-import { useAccount, useContractWrite, useNetwork } from 'wagmi'
+import {
+  useAccount,
+  useContractWrite,
+  useNetwork,
+  useSignTypedData
+} from 'wagmi'
 
 const CREATE_FOLLOW_TYPED_DATA_MUTATION = gql`
   mutation CreateFollowTypedData($request: FollowRequest!) {
     createFollowTypedData(request: $request) {
       id
+      expiresAt
       typedData {
+        domain {
+          name
+          chainId
+          version
+          verifyingContract
+        }
+        types {
+          FollowWithSig {
+            name
+            type
+          }
+        }
         value {
+          nonce
+          deadline
           profileIds
           datas
         }
@@ -41,30 +63,51 @@ const Follow: React.FC<Props> = ({
 }) => {
   const [{ data: network }] = useNetwork()
   const [{ data: account }] = useAccount()
+  const [{ loading: signLoading }, signTypedData] = useSignTypedData()
 
   const [{ loading }, write] = useContractWrite(
     {
       addressOrName: LENSHUB_PROXY,
       contractInterface: LensHubProxy
     },
-    'follow'
+    'followWithSig'
   )
 
   const [createFollowTypedData, { loading: typedDataLoading }] = useMutation(
     CREATE_FOLLOW_TYPED_DATA_MUTATION,
     {
-      onCompleted(data: any) {
-        const { profileIds, datas: followData } =
-          data?.createFollowTypedData?.typedData?.value
-        const inputArray = [profileIds, followData]
+      onCompleted({ createFollowTypedData }: any) {
+        const { typedData } = createFollowTypedData
+        signTypedData({
+          domain: omit(typedData?.domain, '__typename'),
+          types: omit(typedData?.types, '__typename'),
+          value: omit(typedData?.value, '__typename')
+        }).then((res) => {
+          if (!res.error) {
+            const { profileIds, datas: followData } = typedData?.value
+            const { v, r, s } = splitSignature(res.data)
+            const inputStruct = {
+              follower: account?.address,
+              profileIds,
+              datas: followData,
+              sig: {
+                v,
+                r,
+                s,
+                deadline: typedData.value.deadline
+              }
+            }
 
-        write({ args: inputArray }).then(({ error }) => {
-          if (!error) {
-            setFollowing(true)
-            toast.success('Followed successfully!')
+            write({ args: inputStruct }).then(({ error }) => {
+              if (!error) {
+                setFollowing(true)
+                toast.success('Followed successfully!')
+              } else {
+                toast.error(error?.message)
+              }
+            })
           } else {
-            // @ts-ignore
-            toast.error(error?.data?.message)
+            toast.error(res.error?.message)
           }
         })
       },
@@ -93,10 +136,10 @@ const Follow: React.FC<Props> = ({
       className="text-sm !px-3 !py-1.5"
       outline
       onClick={createFollow}
-      disabled={typedDataLoading || loading}
+      disabled={typedDataLoading || signLoading || loading}
       variant="success"
       icon={
-        typedDataLoading || loading ? (
+        typedDataLoading || signLoading || loading ? (
           <Spinner variant="success" size="xs" />
         ) : (
           <UserAddIcon className="w-4 h-4" />
