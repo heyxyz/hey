a546e7e991ddb67355a08f9534864b7f1957a510
bigint <bigint@lenster.xyz>
2022-03-19 08:37:04 +0530

    feat: add signTypedData

--- src/components/Post/Actions/Mirror.tsx
+++ src/components/Post/Actions/Mirror.tsx
@@ -5,6 +5,8 @@ import AppContext from '@components/utils/AppContext'
 import { LensterPost } from '@generated/lenstertypes'
 import { DuplicateIcon } from '@heroicons/react/outline'
 import { humanize } from '@lib/humanize'
+import { omit } from '@lib/omit'
+import { splitSignature } from '@lib/splitSignature'
 import { motion } from 'framer-motion'
 import { useContext } from 'react'
 import toast from 'react-hot-toast'
@@ -14,14 +16,34 @@ import {
   LENSHUB_PROXY,
   WRONG_NETWORK
 } from 'src/constants'
-import { useAccount, useContractWrite, useNetwork } from 'wagmi'
+import {
+  useAccount,
+  useContractWrite,
+  useNetwork,
+  useSignTypedData
+} from 'wagmi'
 
 const CREATE_MIRROR_TYPED_DATA_QUERY = gql`
   mutation CreateMirrorTypedData($request: CreateMirrorRequest!) {
     createMirrorTypedData(request: $request) {
       id
+      expiresAt
       typedData {
+        types {
+          MirrorWithSig {
+            name
+            type
+          }
+        }
+        domain {
+          name
+          chainId
+          version
+          verifyingContract
+        }
         value {
+          nonce
+          deadline
           profileId
           profileIdPointed
           pubIdPointed
@@ -41,42 +63,57 @@ const Mirror: React.FC<Props> = ({ post }) => {
   const { currentUser } = useContext(AppContext)
   const [{ data: network }] = useNetwork()
   const [{ data: account }] = useAccount()
+  const [{}, signTypedData] = useSignTypedData()
 
   const [{ loading }, write] = useContractWrite(
     {
       addressOrName: LENSHUB_PROXY,
       contractInterface: LensHubProxy
     },
-    'mirror'
+    'mirrorWithSig'
   )
 
   const [createMirrorTypedData, { loading: typedDataLoading }] = useMutation(
     CREATE_MIRROR_TYPED_DATA_QUERY,
     {
-      onCompleted(data: any) {
+      onCompleted({ createMirrorTypedData }: any) {
+        const { typedData } = createMirrorTypedData
         const {
           profileId,
           profileIdPointed,
           pubIdPointed,
           referenceModule,
           referenceModuleData
-        } = data?.createMirrorTypedData?.typedData?.value
+        } = typedData?.value
 
-        const inputStruct = {
-          profileId,
-          profileIdPointed,
-          pubIdPointed,
-          referenceModule,
-          referenceModuleData
-        }
-
-        write({ args: inputStruct }).then(({ error }) => {
-          if (!error) {
-            toast.success('Post has been mirrored!')
-          } else {
-            // @ts-ignore
-            toast.error(error?.data?.message)
+        signTypedData({
+          domain: omit(typedData?.domain, '__typename'),
+          types: omit(typedData?.types, '__typename'),
+          value: omit(typedData?.value, '__typename')
+        }).then((res) => {
+          const { v, r, s } = splitSignature(res.data)
+          const inputStruct = {
+            profileId,
+            profileIdPointed,
+            pubIdPointed,
+            referenceModule,
+            referenceModuleData,
+            sig: {
+              v,
+              r,
+              s,
+              deadline: typedData.value.deadline
+            }
           }
+
+          write({ args: inputStruct }).then(({ error }) => {
+            if (!error) {
+              toast.success('Post has been mirrored!')
+            } else {
+              // @ts-ignore
+              toast.error(error?.data?.message)
+            }
+          })
         })
       },
       onError() {
