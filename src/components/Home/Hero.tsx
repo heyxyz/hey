92eb6cc8c71975f3460ab18a47c0199f3d0ebd5b
yoginth.eth <yoginth@hey.com>
2022-03-20 10:06:49 +0530

    chore: update hero

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,21 +1,20 @@
 import React from 'react'
-import { STATIC_ASSETS } from 'src/constants'
 
 const Hero: React.FC = () => {
   return (
-    <div
-      className="text-white bg-brand-500 dark:bg-brand-700 md:block"
-      style={{
-        backgroundImage: `url('${STATIC_ASSETS}/patterns/2.svg')`
-      }}
-    >
-      <div className="container flex-grow max-w-screen-xl px-5 py-32 mx-auto space-y-5">
-        <div className="text-4xl font-extrabold tracking-wide">
-          What's poppin'?
-        </div>
-        <div className="text-xl">
-          Lenshub is a composable, decentralized, and permissionless social
-          media web app built with Lens Protocol 🌿
+    <div className="py-12 mb-4 bg-white border-b bg-hero">
+      <div className="container max-w-screen-xl px-5 mx-auto">
+        <div className="flex items-stretch w-full py-8 text-center sm:py-12 sm:text-left">
+          <div className="flex-1 flex-shrink-0 space-y-3">
+            <div className="text-2xl font-extrabold text-black sm:text-4xl">
+              Welcome to LensHub 👋
+            </div>
+            <div className="leading-7 text-gray-700">
+              LensHub is a composable, decentralized, and permissionless social
+              media web app built with Lens Protocol 🌿
+            </div>
+          </div>
+          <div className="flex-1 flex-shrink-0 hidden w-full sm:block"></div>
         </div>
       </div>
     </div>
