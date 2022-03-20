fc9eb5220dd4c9f5c5776e405ff31cf989808501
yoginth.eth <yoginth@hey.com>
2022-03-20 08:26:59 +0530

    chore: remove landing page and update links

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -14,8 +14,8 @@ export default function Custom404() {
       <div className="py-10 text-center">
         <h1 className="mb-4 text-3xl font-bold">Oops, Lost‽</h1>
         <div className="mb-4">This page could not be found.</div>
-        <Link href="/home" passHref>
-          <a href="/home">
+        <Link href="/" passHref>
+          <a href="/">
             <Button
               className="flex mx-auto item-center"
               size="lg"
