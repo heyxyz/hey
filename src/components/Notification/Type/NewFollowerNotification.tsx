f25bbf042a86cf392983bb35e0d9de0abcbe8e08
bigint <bigint@lenster.xyz>
2022-03-21 20:54:58 +0530

    feat: add notifications

--- src/components/Notification/Type/NewFollowerNotification.tsx
+++ src/components/Notification/Type/NewFollowerNotification.tsx
@@ -1,6 +1,5 @@
 import UserProfile from '@components/Shared/UserProfile'
 import WalletProfile from '@components/Shared/WalletProfile'
-import { Card, CardBody } from '@components/UI/Card'
 import { NewFollowerNotification } from '@generated/types'
 import React from 'react'
 
@@ -10,16 +9,12 @@ interface Props {
 
 const NewFollowerNotification: React.FC<Props> = ({ notification }) => {
   return (
-    <div>
-      <Card>
-        <CardBody>
-          {notification?.wallet?.defaultProfile ? (
-            <UserProfile profile={notification?.wallet?.defaultProfile} />
-          ) : (
-            <WalletProfile wallet={notification?.wallet} />
-          )}
-        </CardBody>
-      </Card>
+    <div className="p-5">
+      {notification?.wallet?.defaultProfile ? (
+        <UserProfile profile={notification?.wallet?.defaultProfile} />
+      ) : (
+        <WalletProfile wallet={notification?.wallet} />
+      )}
     </div>
   )
 }
