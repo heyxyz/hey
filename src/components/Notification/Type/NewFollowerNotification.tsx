a48f3f4fc156d8f803870e59434ca75be0155e05
yoginth.eth <yoginth@hey.com>
2022-03-22 08:47:38 +0530

    feat: update NewFollowerNotification

--- src/components/Notification/Type/NewFollowerNotification.tsx
+++ src/components/Notification/Type/NewFollowerNotification.tsx
@@ -1,20 +1,86 @@
-import UserProfile from '@components/Shared/UserProfile'
-import WalletProfile from '@components/Shared/WalletProfile'
+import Follow from '@components/Shared/Follow'
+import Unfollow from '@components/Shared/Unfollow'
 import { NewFollowerNotification } from '@generated/types'
-import React from 'react'
+import { BadgeCheckIcon } from '@heroicons/react/outline'
+import { getAvatar } from '@lib/getAvatar'
+import { isVerified } from '@lib/isVerified'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import Link from 'next/link'
+import React, { useState } from 'react'
+
+dayjs.extend(relativeTime)
 
 interface Props {
   notification: NewFollowerNotification
 }
 
 const NewFollowerNotification: React.FC<Props> = ({ notification }) => {
+  const [following, setFollowing] = useState<boolean>(
+    notification.isFollowedByMe
+  )
+  const { wallet } = notification
+
   return (
     <div className="p-5">
-      {notification?.wallet?.defaultProfile ? (
-        <UserProfile profile={notification?.wallet?.defaultProfile} />
-      ) : (
-        <WalletProfile wallet={notification?.wallet} />
-      )}
+      <div className="flex items-center justify-between">
+        <Link
+          href={
+            wallet?.defaultProfile
+              ? `/u/${wallet?.defaultProfile?.handle}`
+              : `https://mumbai.polygonscan.com/address/${wallet?.address}`
+          }
+        >
+          <a>
+            <div className="flex items-center space-x-3">
+              <img
+                src={
+                  wallet?.defaultProfile
+                    ? getAvatar(wallet?.defaultProfile)
+                    : `https://avatar.tobi.sh/${wallet?.address}.png`
+                }
+                className="w-10 h-10 bg-gray-200 border rounded-full dark:border-gray-700"
+                alt={
+                  wallet?.defaultProfile
+                    ? wallet?.defaultProfile?.handle
+                    : wallet?.address
+                }
+              />
+              <div>
+                <div className="flex items-center gap-1">
+                  <div className="font-bold">
+                    {wallet?.defaultProfile?.name ??
+                      wallet?.defaultProfile?.handle ??
+                      wallet.address}
+                  </div>
+                  {wallet?.defaultProfile &&
+                    isVerified(wallet?.defaultProfile?.handle) && (
+                      <BadgeCheckIcon className="w-4 h-4 text-brand-500" />
+                    )}
+                  <div className="pl-0.5 text-gray-600">followed you</div>
+                </div>
+                <div className="text-sm text-gray-500">
+                  {dayjs(new Date(notification.createdAt)).fromNow()}
+                </div>
+              </div>
+            </div>
+          </a>
+        </Link>
+        {wallet?.defaultProfile &&
+          (following ? (
+            <Unfollow
+              profile={wallet?.defaultProfile}
+              setFollowing={setFollowing}
+              showText
+            />
+          ) : (
+            <Follow
+              profile={wallet?.defaultProfile}
+              setFollowing={setFollowing}
+              showText
+            />
+          ))}
+      </div>
     </div>
   )
 }
