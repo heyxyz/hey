be7e39034319cb1018dac2ca0bd9937d66b62ef4
bigint <bigint@lenster.xyz>
2022-03-22 14:22:40 +0530

    fix: typo

--- src/components/Post/ViaLenster.tsx
+++ src/components/Post/ViaLenster.tsx
@@ -4,7 +4,7 @@ const ViaLenster: React.FC = () => {
   return (
     <div className="flex items-center px-5 py-3 space-x-2 text-gray-500 border-t dark:border-t-gray-800">
       <img src="/logo.svg" className="w-5 h-5" alt="Logo" />
-      <div>Posted via Lenaster</div>
+      <div>Posted via Lenster</div>
     </div>
   )
 }
