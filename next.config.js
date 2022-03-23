a48a80bfe5b27871fd4589d82f4e382c060b34f3
bigint <bigint@lenster.xyz>
2022-03-23 17:49:24 +0530

    feat: add pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
-module.exports = {
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    runtimeCaching
+  },
   reactStrictMode: true,
   env: {
     GIT_COMMIT_REF: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF,
     GIT_COMMIT_SHA: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA
   }
-}
+})
