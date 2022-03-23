0c6849428e926678f25f010ee74501e0e9dc9c37
bigint <bigint@lenster.xyz>
2022-03-23 08:37:39 +0530

    feat: add more metadata

---
+++ src/components/Community/Join.tsx
@@ -0,0 +1,158 @@
+import LensHubProxy from '@abis/LensHubProxy.json'
+import { gql, useMutation } from '@apollo/client'
+import { Button } from '@components/UI/Button'
+import { LensterPost } from '@generated/lenstertypes'
+import { CreateCollectBroadcastItemResult } from '@generated/types'
+import { PlusIcon } from '@heroicons/react/outline'
+import { omit } from '@lib/omit'
+import { splitSignature } from '@lib/splitSignature'
+import React from 'react'
+import toast from 'react-hot-toast'
+import {
+  CONNECT_WALLET,
+  ERROR_MESSAGE,
+  LENSHUB_PROXY,
+  WRONG_NETWORK
+} from 'src/constants'
+import {
+  chain,
+  useAccount,
+  useContractWrite,
+  useNetwork,
+  useSignTypedData
+} from 'wagmi'
+
+const CREATE_COLLECT_TYPED_DATA_MUTATION = gql`
+  mutation CreateCollectTypedData($request: CreateCollectRequest!) {
+    createCollectTypedData(request: $request) {
+      id
+      expiresAt
+      typedData {
+        types {
+          CollectWithSig {
+            name
+            type
+          }
+        }
+        domain {
+          name
+          chainId
+          version
+          verifyingContract
+        }
+        value {
+          nonce
+          deadline
+          profileId
+          pubId
+          data
+        }
+      }
+    }
+  }
+`
+
+interface Props {
+  community: LensterPost
+}
+
+const Join: React.FC<Props> = ({ community }) => {
+  const [{ data: network }] = useNetwork()
+  const [{ data: account }] = useAccount()
+  const [{ loading: signLoading }, signTypedData] = useSignTypedData()
+  const [{ loading: writeLoading }, write] = useContractWrite(
+    {
+      addressOrName: LENSHUB_PROXY,
+      contractInterface: LensHubProxy
+    },
+    'collectWithSig'
+  )
+
+  const [createCollectTypedData, { loading: typedDataLoading }] = useMutation(
+    CREATE_COLLECT_TYPED_DATA_MUTATION,
+    {
+      onCompleted({
+        createCollectTypedData
+      }: {
+        createCollectTypedData: CreateCollectBroadcastItemResult
+      }) {
+        const { typedData } = createCollectTypedData
+
+        signTypedData({
+          domain: omit(typedData?.domain, '__typename'),
+          types: omit(typedData?.types, '__typename'),
+          value: omit(typedData?.value, '__typename')
+        }).then((res) => {
+          if (!res.error) {
+            const { profileId, pubId, data: collectData } = typedData?.value
+            const { v, r, s } = splitSignature(res.data)
+            const inputStruct = {
+              collector: account?.address,
+              profileId,
+              pubId,
+              data: collectData,
+              sig: {
+                v,
+                r,
+                s,
+                deadline: typedData.value.deadline
+              }
+            }
+
+            write({ args: inputStruct }).then(({ error }: { error: any }) => {
+              if (!error) {
+                toast.success('Joined successfully!')
+              } else {
+                if (
+                  error?.data?.message ===
+                  'execution reverted: SafeERC20: low-level call failed'
+                ) {
+                  toast.error(
+                    `Please allow Empty Collect module in allowance settings`
+                  )
+                } else {
+                  toast.error(error?.data?.message)
+                }
+              }
+            })
+          } else {
+            toast.error(res.error?.message)
+          }
+        })
+      },
+      onError(error) {
+        toast.error(error.message ?? ERROR_MESSAGE)
+      }
+    }
+  )
+
+  const createCollect = async () => {
+    if (!account?.address) {
+      toast.error(CONNECT_WALLET)
+    } else if (network.chain?.id !== chain.polygonTestnetMumbai.id) {
+      toast.error(WRONG_NETWORK)
+    } else {
+      createCollectTypedData({
+        variables: {
+          request: {
+            publicationId: community.pubId
+          }
+        }
+      })
+    }
+  }
+
+  return (
+    <Button
+      onClick={createCollect}
+      disabled={typedDataLoading || signLoading || writeLoading}
+      icon={<PlusIcon className="w-4 h-4" />}
+      variant="success"
+      outline
+    >
+      Join
+    </Button>
+  )
+}
+
+export default Join
