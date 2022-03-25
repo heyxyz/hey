e2071c0cb4059a88c042e8644c584f2f1776e6a2
yoginth.eth <yoginth@hey.com>
2022-03-25 09:42:01 +0530

    feat: add more dropdown

--- src/components/Post/SinglePost.tsx
+++ src/components/Post/SinglePost.tsx
@@ -31,8 +31,8 @@ const SinglePost: React.FC<Props> = ({ post, type, showCard = true }) => {
         <PostType post={post} type={type} />
         <div className="flex justify-between pb-4">
           <UserProfile profile={post.profile} />
-          <Link href={`/post/${post.pubId}`}>
-            <a className="text-sm text-gray-500" href={`/post/${post.pubId}`}>
+          <Link href={`/posts/${post.pubId}`}>
+            <a className="text-sm text-gray-500" href={`/posts/${post.pubId}`}>
               {dayjs(new Date(post.createdAt)).fromNow()}
             </a>
           </Link>
