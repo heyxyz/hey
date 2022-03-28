747b7444249b77dc7309cef7c9a556dd1f5e39ba
yoginth.eth <yoginth@hey.com>
2022-03-28 09:08:13 +0530

    chore: add search limit

--- src/components/Shared/Navbar/Search.tsx
+++ src/components/Shared/Navbar/Search.tsx
@@ -42,26 +42,28 @@ const Search = () => {
   const handleSearch = async (evt: any) => {
     let keyword = evt.target.value
     setSearchText(keyword)
-    searchUsers({ variables: { request: { type: 'PROFILE', query: keyword } } })
+    searchUsers({
+      variables: { request: { type: 'PROFILE', query: keyword, limit: 8 } }
+    })
   }
 
   return (
     <>
       <Input
         type="text"
-        className="py-2 px-3 text-sm"
+        className="px-3 py-2 text-sm"
         placeholder="Search..."
         value={searchText}
         onChange={handleSearch}
       />
       {searchText.length > 0 && (
         <div
-          className="flex absolute flex-col mt-2 w-full sm:max-w-md"
+          className="absolute flex flex-col w-full mt-2 sm:max-w-md"
           ref={dropdownRef}
         >
           <Card className="overflow-y-auto py-2 max-h-[80vh]">
             {searchUsersLoading ? (
-              <div className="py-2 px-4 space-y-2 text-sm font-bold text-center">
+              <div className="px-4 py-2 space-y-2 text-sm font-bold text-center">
                 <Spinner size="sm" className="mx-auto" />
                 <div>Searching users</div>
               </div>
@@ -70,14 +72,14 @@ const Search = () => {
                 {searchUsersData?.search?.items?.map((profile: Profile) => (
                   <div
                     key={profile.handle}
-                    className="py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-800"
+                    className="px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-800"
                     onClick={() => setSearchText('')}
                   >
                     <UserProfile profile={profile} />
                   </div>
                 ))}
                 {searchUsersData?.search?.items?.length === 0 && (
-                  <div className="py-2 px-4">No matching users</div>
+                  <div className="px-4 py-2">No matching users</div>
                 )}
               </div>
             )}
