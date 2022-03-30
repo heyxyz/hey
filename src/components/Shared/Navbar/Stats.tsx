1a39e8b2128dccd2d2ea6a34ff78829a193df3e1
yoginth.eth <yoginth@hey.com>
2022-03-31 00:03:57 +0530

    feat: add lenster data

--- src/components/Shared/Navbar/Stats.tsx
+++ src/components/Shared/Navbar/Stats.tsx
@@ -36,15 +36,21 @@ const LENSTER_STATS_QUERY = gql`
 interface Props {
   icon: React.ReactChild
   title: React.ReactChild
+  isLenster?: boolean
 }
 
-const MenuItem: React.FC<Props> = ({ icon, title }) => (
+const MenuItem: React.FC<Props> = ({ icon, title, isLenster = false }) => (
   <Menu.Item
     as="div"
-    className="py-1 px-4 m-2 text-sm text-gray-700 dark:text-gray-200"
+    className="px-4 py-1 m-2 text-sm text-gray-700 dark:text-gray-200"
   >
     <div className="flex items-center space-x-2">
-      {icon}
+      <div className="flex items-center space-x-1">
+        {icon}
+        {isLenster && (
+          <img src="/logo.svg" className="w-3 h-3" alt="Lenster's Data" />
+        )}
+      </div>
       <div>{title}</div>
     </div>
   </Menu.Item>
@@ -60,7 +66,7 @@ const Stats: React.FC = () => {
     pollInterval: 1000
   })
 
-  if (loading) return <div className="m-3 h-4 rounded-lg shimmer" />
+  if (loading) return <div className="h-4 m-3 rounded-lg shimmer" />
   if (error)
     return <div className="m-3 font-bold text-red-500">{ERROR_MESSAGE}</div>
 
@@ -91,6 +97,7 @@ const Stats: React.FC = () => {
             <b>{humanize(stats?.totalPosts)}</b> total posts
           </span>
         }
+        isLenster
       />
       <MenuItem
         icon={<DuplicateIcon className="w-4 h-4" />}
@@ -99,6 +106,7 @@ const Stats: React.FC = () => {
             <b>{humanize(stats?.totalMirrors)}</b> total mirrors
           </span>
         }
+        isLenster
       />
       <MenuItem
         icon={<ChatAlt2Icon className="w-4 h-4" />}
@@ -107,6 +115,7 @@ const Stats: React.FC = () => {
             <b>{humanize(stats?.totalComments)}</b> total comments
           </span>
         }
+        isLenster
       />
       <MenuItem
         icon={<CollectionIcon className="w-4 h-4" />}
