21525845fe5118bbc82dff372a6b5f5cced4f671
yoginth.eth <yoginth@hey.com>
2022-03-31 11:40:29 +0530

    feat: add profile page shimmer

---
+++ src/components/Profile/ProfilePageShimmer.tsx
@@ -0,0 +1,48 @@
+import { GridItemEight, GridItemFour, GridLayout } from '@components/GridLayout'
+import PostsShimmer from '@components/Shared/Shimmer/PostsShimmer'
+import React from 'react'
+
+const ProfilePageShimmer: React.FC = () => {
+  return (
+    <>
+      <div className="shimmer h-52 sm:h-80" />
+      <GridLayout className="pt-6">
+        <GridItemFour>
+          <div className="px-5 mb-4 sm:px-0">
+            <div className="space-y-5">
+              <div className="relative w-32 h-32 -mt-24 sm:-mt-32 sm:w-52 sm:h-52">
+                <div className="w-32 h-32 bg-gray-200 rounded-xl ring-8 ring-gray-50 sm:w-52 sm:h-52 dark:bg-gray-700 dark:ring-black shimmer" />
+              </div>
+              <div className="py-3 space-y-3">
+                <div className="w-1/3 h-5 rounded-lg shimmer" />
+                <div className="w-1/4 h-3 rounded-lg shimmer" />
+              </div>
+              <div className="space-y-5">
+                <div className="flex gap-5">
+                  <div className="space-y-2">
+                    <div className="w-8 h-8 rounded-lg shimmer" />
+                    <div className="w-20 h-3 rounded-lg shimmer" />
+                  </div>
+                  <div className="space-y-2">
+                    <div className="w-8 h-8 rounded-lg shimmer" />
+                    <div className="w-20 h-3 rounded-lg shimmer" />
+                  </div>
+                </div>
+                <div className="w-28 rounded-lg h-[34px] shimmer" />
+                <div className="space-y-2">
+                  <div className="w-7/12 h-3 rounded-lg shimmer" />
+                  <div className="w-1/3 h-3 rounded-lg shimmer" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </GridItemFour>
+        <GridItemEight className="space-y-5">
+          <PostsShimmer />
+        </GridItemEight>
+      </GridLayout>
+    </>
+  )
+}
+
+export default ProfilePageShimmer
