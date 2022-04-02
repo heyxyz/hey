f726b152de709bf248d4a5504874f67459419f96
yoginth.eth <yoginth@hey.com>
2022-04-02 14:14:44 +0530

    feat: add getIPFSHash

--- src/lib/getIPFSHash.ts
+++ src/lib/getIPFSHash.ts
@@ -4,4 +4,5 @@ export const getIPFSHash = (url: string): any => {
       /Qm[1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,}/
     )
     ?.at(0)
+    ?.replace('.json', '')
 }
