68050f06474ca68e18da1fb8854e4e26d84d2a88
bigint <bigint@lenster.xyz>
2022-04-03 21:45:54 +0530

    chore: update color

--- src/components/Shared/Announcement/index.tsx
+++ src/components/Shared/Announcement/index.tsx
@@ -3,13 +3,13 @@ import { BeakerIcon } from '@heroicons/react/outline'
 
 const Announcement: React.FC = () => {
   return (
-    <Card className="mb-4 bg-yellow-50 dark:bg-yellow-900 !border-yellow-500">
+    <Card className="mb-4 bg-yellow-50 dark:bg-yellow-900 !border-yellow-600">
       <CardBody className="space-y-2.5">
-        <div className="flex font-bold text-yellow-500 items-center space-x-2">
-          <BeakerIcon className="h-5 w-5 text-yellow-500" />
+        <div className="flex items-center space-x-2 font-bold text-yellow-600">
+          <BeakerIcon className="w-5 h-5 text-yellow-600" />
           <p>Beta warning!</p>
         </div>
-        <p className="text-yellow-500 text-sm leading-[22px]">
+        <p className="text-yellow-600 text-sm leading-[22px]">
           Lenster is still in the beta phase and all contents are stored in
           Mumbai testnet.
         </p>
