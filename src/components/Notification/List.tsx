bb9b08d3daa528f1b2f4d99dac99310a6c213aff
bigint <bigint@lenster.xyz>
2022-04-03 11:46:06 +0530

    feat: add pagination

--- src/components/Notification/List.tsx
+++ src/components/Notification/List.tsx
@@ -1,11 +1,13 @@
 import { gql, useQuery } from '@apollo/client'
 import { ErrorMessage } from '@components/UI/ErrorMessage'
+import { Spinner } from '@components/UI/Spinner'
 import AppContext from '@components/utils/AppContext'
 import { Notification } from '@generated/types'
 import { CommentFragment } from '@gql/CommentFragment'
 import { PostFragment } from '@gql/PostFragment'
 import { Menu } from '@headlessui/react'
 import { useContext } from 'react'
+import useInView from 'react-cool-inview'
 
 import CommentNotification from './Type/CommentNotification'
 import FollowerNotification from './Type/FollowerNotification'
@@ -91,7 +93,23 @@ const List: React.FC = () => {
   const { currentUser } = useContext(AppContext)
   const { data, loading, error, fetchMore } = useQuery(NOTIFICATIONS_QUERY, {
     variables: {
-      request: { profileId: currentUser?.id, limit: 50 }
+      request: { profileId: currentUser?.id, limit: 10 }
+    }
+  })
+
+  const pageInfo = data?.notifications?.pageInfo
+  const { observe } = useInView({
+    threshold: 1,
+    onEnter: () => {
+      fetchMore({
+        variables: {
+          request: {
+            profileId: currentUser?.id,
+            cursor: pageInfo?.next,
+            limit: 10
+          }
+        }
+      })
     }
   })
 
@@ -123,6 +141,11 @@ const List: React.FC = () => {
           )}
         </div>
       ))}
+      {pageInfo?.next && (
+        <span ref={observe} className="flex justify-center p-5">
+          <Spinner size="sm" />
+        </span>
+      )}
     </div>
   )
 }
