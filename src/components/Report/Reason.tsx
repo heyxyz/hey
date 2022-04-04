6d9592382e0e439cd189fc6d65bdceb0ce4992af
bigint <bigint@lenster.xyz>
2022-04-04 20:09:49 +0530

    feat: add reason support

--- src/components/Report/Reason.tsx
+++ src/components/Report/Reason.tsx
@@ -1,16 +1,69 @@
 import React, { Dispatch } from 'react'
 
 interface Props {
-  setReason: Dispatch<string>
+  setType: Dispatch<string>
   setSubReason: Dispatch<string>
+  type: string
 }
 
-const Reason: React.FC<Props> = ({ setReason, setSubReason }) => {
+const Reason: React.FC<Props> = ({ setType, setSubReason, type }) => {
+  const Label = ({ children }: { children: React.ReactChild }) => (
+    <div className="mb-1 font-medium text-gray-800 dark:text-gray-200">
+      {children}
+    </div>
+  )
+
   return (
-    <div>
-      <div className="mb-1 font-medium text-gray-800 dark:text-gray-200">
-        WIP
+    <div className="space-y-3">
+      <div>
+        <Label>Type</Label>
+        <div>
+          <select
+            className="w-full bg-white border border-gray-300 outline-none rounded-xl dark:bg-gray-800 dark:border-gray-700 disabled:bg-gray-500 disabled:bg-opacity-20 disabled:opacity-60 focus:border-brand-500 focus:ring-brand-400"
+            onChange={(e) => setType(e.target.value)}
+          >
+            <option disabled selected>
+              Select type
+            </option>
+            <option value="illegalReason">Illegeal</option>
+            <option value="fraudReason">Fraud</option>
+            <option value="sensitiveReason">Sensitive</option>
+          </select>
+        </div>
       </div>
+      {type && (
+        <div>
+          <Label>Reason</Label>
+          <div>
+            <select
+              className="w-full bg-white border border-gray-300 outline-none rounded-xl dark:bg-gray-800 dark:border-gray-700 disabled:bg-gray-500 disabled:bg-opacity-20 disabled:opacity-60 focus:border-brand-500 focus:ring-brand-400"
+              onChange={(e) => setSubReason(e.target.value)}
+            >
+              <option disabled selected>
+                Select sub reason
+              </option>
+              {type === 'illegalReason' && (
+                <>
+                  <option value="ANIMAL_ABUSE">Animal Abuse</option>
+                  <option value="HUMAN_ABUSE">Human Abuse</option>
+                </>
+              )}
+              {type === 'fraudReason' && (
+                <>
+                  <option value="SCAM">Scam</option>
+                  <option value="IMPERSONATION">Impersonation</option>
+                </>
+              )}
+              {type === 'sensitiveReason' && (
+                <>
+                  <option value="NSFW">NSWF</option>
+                  <option value="OFFENSIVE">Offensive</option>
+                </>
+              )}
+            </select>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
