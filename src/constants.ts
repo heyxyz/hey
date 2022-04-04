2e779d1fafd1adb751afe40344bff6e5c426832a
yoginth.eth <yoginth@hey.com>
2022-04-04 10:42:19 +0530

    fix: rename to NEXT_PUBLIC_IS_MAINNET

--- src/constants.ts
+++ src/constants.ts
@@ -3,11 +3,12 @@ import { chain } from 'wagmi'
 // Environments
 export const IS_PRODUCTION = process.env.NODE_ENV === 'production'
 export const IS_DEVELOPMENT = process.env.NODE_ENV === 'development'
-export const IS_MAINNET = process.env.IS_MAINNET
+export const IS_MAINNET = process.env.NEXT_PUBLIC_IS_MAINNET === 'true'
 
 // Git
-export const GIT_COMMIT_SHA = process.env.GIT_COMMIT_SHA?.slice(0, 7)
-export const GIT_COMMIT_REF = process.env.GIT_COMMIT_REF
+export const GIT_COMMIT_SHA =
+  process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA?.slice(0, 7)
+export const GIT_COMMIT_REF = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF
 
 // Messages
 export const ERROR_MESSAGE = 'Something went wrong!'
