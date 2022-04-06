4c0899212a5a37478bcb5f15fdcbdfa1f3b536c3
bigint <bigint@lenster.xyz>
2022-04-06 08:47:34 +0530

    fix: commented comp

--- src/components/Post/Type/Commented.tsx
+++ src/components/Post/Type/Commented.tsx
@@ -1,7 +1,5 @@
 import Slug from '@components/Shared/Slug'
 import { LensterPost } from '@generated/lenstertypes'
-import { Profile } from '@generated/types'
-import { getAvatar } from '@lib/getAvatar'
 import Link from 'next/link'
 import { useTheme } from 'next-themes'
 import React from 'react'
@@ -15,28 +13,25 @@ const Commented: React.FC<Props> = ({ post }) => {
   const { resolvedTheme } = useTheme()
 
   return (
-    <div className="flex items-end">
+    <div className="flex items-end w-3/5">
       <img
         src={`${STATIC_ASSETS}/icons/${
           resolvedTheme === 'dark' ? 'comment-dark' : 'comment-light'
         }.svg`}
-        className="mr-2 mb-0.5 ml-5 w-4"
+        className="mr-1.5 ml-5 w-4"
         alt="Comment"
       />
-      <div className="flex items-center pb-3 space-x-1 text-sm text-gray-500">
+      <div className="flex items-center pb-3.5 space-x-1 text-sm text-gray-500">
         <Link href={`/u/${post?.commentOn?.profile?.handle}`}>
-          <a className="flex items-center space-x-2 font-bold">
-            <img
-              className="w-6 h-6 rounded-full"
-              src={getAvatar(post?.commentOn?.profile as Profile)}
-              alt={post?.commentOn?.profile?.handle}
-            />
+          <a>
             <Slug slug={post?.commentOn?.profile?.handle} prefix="@" />:
           </a>
         </Link>
         <Link href={`/posts/${post?.commentOn?.id}`}>
-          <a className="md:w-2/4 line-clamp-1">
-            {post?.commentOn?.metadata?.content}
+          <a className="line-clamp-1">
+            {post?.commentOn?.metadata?.content.trim()
+              ? post?.commentOn?.metadata?.content
+              : post?.commentOn?.metadata?.name}
           </a>
         </Link>
       </div>
