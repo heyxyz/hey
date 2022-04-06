aa0d8443829f8102a29ff8a311cef31ab081b9b7
yoginth.eth <yoginth@hey.com>
2022-04-06 09:53:34 +0530

    feat: add popover MVP

--- src/components/Shared/UserProfile.tsx
+++ src/components/Shared/UserProfile.tsx
@@ -3,12 +3,15 @@ import { BadgeCheckIcon } from '@heroicons/react/solid'
 import { formatUsername } from '@lib/formatUsername'
 import { getAvatar } from '@lib/getAvatar'
 import { isVerified } from '@lib/isVerified'
+import dynamic from 'next/dynamic'
 import Link from 'next/link'
 import React, { useState } from 'react'
 
 import Follow from './Follow'
 import Slug from './Slug'
 import Unfollow from './Unfollow'
+
+const UserPopover = dynamic(() => import('./Popover/UserPopover'))
 
 interface Props {
   profile: Profile
@@ -26,43 +29,45 @@ const UserProfile: React.FC<Props> = ({
   const [following, setFollowing] = useState<boolean>(isFollowing)
 
   return (
-    <div className="flex justify-between items-center">
-      <Link href={`/u/${profile?.handle}`}>
-        <a>
-          <div className="flex items-center space-x-3">
-            <img
-              src={getAvatar(profile)}
-              className="w-10 h-10 bg-gray-200 rounded-full border dark:border-gray-700"
-              alt={profile?.handle}
-            />
-            <div>
-              <div className="flex gap-1 items-center">
-                <div>{profile?.name ?? profile?.handle}</div>
-                {isVerified(profile?.id) && (
-                  <BadgeCheckIcon className="w-4 h-4 text-brand-500" />
+    <UserPopover>
+      <div className="flex justify-between items-center">
+        <Link href={`/u/${profile?.handle}`}>
+          <a>
+            <div className="flex items-center space-x-3">
+              <img
+                src={getAvatar(profile)}
+                className="w-10 h-10 bg-gray-200 rounded-full border dark:border-gray-700"
+                alt={profile?.handle}
+              />
+              <div>
+                <div className="flex gap-1 items-center">
+                  <div>{profile?.name ?? profile?.handle}</div>
+                  {isVerified(profile?.id) && (
+                    <BadgeCheckIcon className="w-4 h-4 text-brand-500" />
+                  )}
+                </div>
+                {profile?.name ? (
+                  <Slug className="text-sm" slug={profile?.handle} prefix="@" />
+                ) : (
+                  <Slug
+                    className="text-sm"
+                    slug={formatUsername(profile?.ownedBy)}
+                  />
                 )}
               </div>
-              {profile?.name ? (
-                <Slug className="text-sm" slug={profile?.handle} prefix="@" />
-              ) : (
-                <Slug
-                  className="text-sm"
-                  slug={formatUsername(profile?.ownedBy)}
-                />
-              )}
             </div>
-          </div>
-        </a>
-      </Link>
-      {showFollow &&
-        (followStatusLoading ? (
-          <div className="w-10 h-8 rounded-lg shimmer" />
-        ) : following ? (
-          <Unfollow profile={profile} setFollowing={setFollowing} />
-        ) : (
-          <Follow profile={profile} setFollowing={setFollowing} />
-        ))}
-    </div>
+          </a>
+        </Link>
+        {showFollow &&
+          (followStatusLoading ? (
+            <div className="w-10 h-8 rounded-lg shimmer" />
+          ) : following ? (
+            <Unfollow profile={profile} setFollowing={setFollowing} />
+          ) : (
+            <Follow profile={profile} setFollowing={setFollowing} />
+          ))}
+      </div>
+    </UserPopover>
   )
 }
 
