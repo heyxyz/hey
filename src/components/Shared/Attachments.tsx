d47b5811af192332b680a88b14994a8317f7ee4e
bigint <bigint@lenster.xyz>
2022-04-06 13:27:45 +0530

    feat: update close icon

--- src/components/Shared/Attachments.tsx
+++ src/components/Shared/Attachments.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@components/UI/Button'
 import { XIcon } from '@heroicons/react/outline'
 import { getIPFSLink } from '@lib/getIPFSLink'
 import { imagekitURL } from '@lib/imagekitURL'
@@ -85,11 +84,12 @@ const Attachments: React.FC<Props> = ({
               )}
               {isNew && (
                 <div className="m-3">
-                  <Button
-                    variant="danger"
-                    icon={<XIcon className="w-4 h-4" />}
+                  <button
+                    className="rounded-full bg-gray-900 p-1.5 opacity-75"
                     onClick={() => removeAttachment(attachment)}
-                  />
+                  >
+                    <XIcon className="w-4 h-4 text-white" />
+                  </button>
                 </div>
               )}
             </div>
