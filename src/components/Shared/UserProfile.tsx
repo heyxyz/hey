ab168511f1e975c8aa4a390321620bd9fcaa082d
bigint <bigint@lenster.xyz>
2022-04-06 16:01:50 +0530

    feat: add npm support

--- src/components/Shared/UserProfile.tsx
+++ src/components/Shared/UserProfile.tsx
@@ -3,15 +3,12 @@ import { BadgeCheckIcon } from '@heroicons/react/solid'
 import { formatUsername } from '@lib/formatUsername'
 import { getAvatar } from '@lib/getAvatar'
 import { isVerified } from '@lib/isVerified'
-import dynamic from 'next/dynamic'
 import Link from 'next/link'
 import React, { useState } from 'react'
 
 import Follow from './Follow'
 import Slug from './Slug'
 import Unfollow from './Unfollow'
-
-const UserPopover = dynamic(() => import('./Popover/UserPopover'))
 
 interface Props {
   profile: Profile
@@ -33,31 +30,27 @@ const UserProfile: React.FC<Props> = ({
       <Link href={`/u/${profile?.handle}`}>
         <a>
           <div className="flex items-center space-x-3">
-            <UserPopover handle={profile?.handle}>
-              <img
-                src={getAvatar(profile)}
-                className="w-10 h-10 bg-gray-200 rounded-full border dark:border-gray-700"
-                alt={profile?.handle}
-              />
-            </UserPopover>
-            <UserPopover handle={profile?.handle}>
-              <div>
-                <div className="flex gap-1 items-center">
-                  <div>{profile?.name ?? profile?.handle}</div>
-                  {isVerified(profile?.id) && (
-                    <BadgeCheckIcon className="w-4 h-4 text-brand-500" />
-                  )}
-                </div>
-                {profile?.name ? (
-                  <Slug className="text-sm" slug={profile?.handle} prefix="@" />
-                ) : (
-                  <Slug
-                    className="text-sm"
-                    slug={formatUsername(profile?.ownedBy)}
-                  />
+            <img
+              src={getAvatar(profile)}
+              className="w-10 h-10 bg-gray-200 rounded-full border dark:border-gray-700"
+              alt={profile?.handle}
+            />
+            <div>
+              <div className="flex gap-1 items-center">
+                <div>{profile?.name ?? profile?.handle}</div>
+                {isVerified(profile?.id) && (
+                  <BadgeCheckIcon className="w-4 h-4 text-brand-500" />
                 )}
               </div>
-            </UserPopover>
+              {profile?.name ? (
+                <Slug className="text-sm" slug={profile?.handle} prefix="@" />
+              ) : (
+                <Slug
+                  className="text-sm"
+                  slug={formatUsername(profile?.ownedBy)}
+                />
+              )}
+            </div>
           </div>
         </a>
       </Link>
