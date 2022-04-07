e6fa3941ebf25a5a3a1b0fbbc44b25037b922f72
yoginth.eth <yoginth@hey.com>
2022-04-07 08:42:33 +0530

    feat: add page-center

---
+++ src/pages/500.tsx
@@ -0,0 +1,30 @@
+import { Button } from '@components/UI/Button'
+import { HomeIcon } from '@heroicons/react/outline'
+import Link from 'next/link'
+
+export default function Custom500() {
+  return (
+    <div className="flex-col page-center">
+      <div className="py-10 text-center">
+        <h1 className="mb-4 text-3xl font-bold">
+          Looks like something went wrong!
+        </h1>
+        <div className="mb-4 text-gray-500">
+          We track these errors automatically, but if the problem persists feel
+          free to contact us. In the meantime, try refreshing.
+        </div>
+        <Link href="/" passHref>
+          <a href="/">
+            <Button
+              className="flex mx-auto item-center"
+              size="lg"
+              icon={<HomeIcon className="w-4 h-4" />}
+            >
+              <div>Go to home</div>
+            </Button>
+          </a>
+        </Link>
+      </div>
+    </div>
+  )
+}
