9be5919b9030959be6cb264ed0e37559ec26bb4b
yoginth.eth <yoginth@hey.com>
2022-04-07 11:46:01 +0530

    feat: trim urls

--- src/lib/linkifyOptions.ts
+++ src/lib/linkifyOptions.ts
@@ -1,4 +1,11 @@
 export const linkifyOptions = {
+  format: function (value: string, type: 'url') {
+    if (type === 'url' && value.length > 36) {
+      value = value.slice(0, 36) + '…'
+    }
+    return value
+  },
+
   formatHref: function (href: string, type: 'mention'): string {
     if (type === 'mention') {
       href = '/u/' + href.slice(1)
