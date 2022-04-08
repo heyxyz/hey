847ef1d7d0e128e819951bdeb70bb9da046bbb0e
yoginth.eth <yoginth@hey.com>
2022-04-08 12:31:27 +0530

    chore: cleanup MirrorNotification

--- src/components/Notification/Type/MirrorNotification.tsx
+++ src/components/Notification/Type/MirrorNotification.tsx
@@ -15,37 +15,27 @@ interface Props {
 
 const MirrorNotification: React.FC<Props> = ({ notification }) => {
   return (
-    <>
-      <div className="flex justify-between items-center">
-        <Link href={`/posts/${notification?.publication?.id}`}>
-          <a>
-            <div className="flex items-center space-x-3">
-              <NotificationProfileAvatar notification={notification} />
-              <div>
-                <div className="flex items-center space-x-2">
-                  <div>
-                    <NotificationProfileName notification={notification} />{' '}
-                    <span className="pl-0.5 text-gray-600">mirrored your </span>
-                    <Link href={`/posts/${notification?.publication.id}`}>
-                      <a className="font-bold">
-                        {notification?.publication?.__typename?.toLowerCase()}
-                      </a>
-                    </Link>
-                  </div>
-                </div>
-                <div className="text-sm text-gray-500 line-clamp-1">
-                  {notification?.publication?.metadata?.content}
-                </div>
-                <div className="flex items-center pt-1 space-x-1 text-gray-400 text-[12px]">
-                  <DuplicateIcon className="h-[15px] text-brand-500" />
-                  <div>{dayjs(new Date(notification.createdAt)).fromNow()}</div>
-                </div>
-              </div>
-            </div>
+    <div className="flex items-center space-x-3">
+      <NotificationProfileAvatar notification={notification} />
+      <div className="w-4/5">
+        <NotificationProfileName notification={notification} />{' '}
+        <span className="pl-0.5 text-gray-600">mirrored your </span>
+        <Link href={`/posts/${notification?.publication.id}`}>
+          <a className="font-bold">
+            {notification?.publication?.__typename?.toLowerCase()}
           </a>
         </Link>
+        <Link href={`/posts/${notification?.publication?.id}`}>
+          <a className="text-sm text-gray-500 line-clamp-1">
+            {notification?.publication?.metadata?.content}
+          </a>
+        </Link>
+        <div className="flex items-center pt-1 space-x-1 text-gray-400 text-[12px]">
+          <DuplicateIcon className="h-[15px] text-brand-500" />
+          <div>{dayjs(new Date(notification.createdAt)).fromNow()}</div>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
