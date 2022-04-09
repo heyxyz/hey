406880fe8d10a70895a61402d14a3968d6c4b6df
yoginth.eth <yoginth@hey.com>
2022-04-09 11:39:00 +0530

    feat: use home built uploader

--- src/lib/uploadAssetsToIPFS.ts
+++ src/lib/uploadAssetsToIPFS.ts
@@ -1,14 +1,18 @@
 export async function uploadAssetsToIPFS(data: File) {
   const formData = new FormData()
   formData.append('file', data, 'img')
-  const upload = await fetch('https://ipfs.infura.io:5001/api/v0/add', {
-    method: 'POST',
-    body: formData
-  })
-  const { Hash }: { Hash: string } = await upload.json()
+  try {
+    const upload = await fetch('https://ipfs.infura.io:5001/api/v0/add', {
+      method: 'POST',
+      body: formData
+    })
+    const { Hash }: { Hash: string } = await upload.json()
 
-  return {
-    item: `https://ipfs.infura.io/ipfs/${Hash}`,
-    type: data.type
+    return {
+      item: `https://ipfs.infura.io/ipfs/${Hash}`,
+      type: data.type
+    }
+  } catch (e) {
+    console.log('IPFS Upload error: ', e)
   }
 }
