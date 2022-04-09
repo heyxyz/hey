839b804e27f09c53884ef44aa11a2a505e12e13f
yoginth.eth <yoginth@hey.com>
2022-04-09 18:23:38 +0530

    chore: use export default

--- src/components/Notification/Profile.tsx
+++ src/components/Notification/Profile.tsx
@@ -1,6 +1,6 @@
 import { Notification } from '@generated/types'
 import formatAddress from '@lib/formatAddress'
-import { getAvatar } from '@lib/getAvatar'
+import getAvatar from '@lib/getAvatar'
 import { imagekitURL } from '@lib/imagekitURL'
 import Link from 'next/link'
 import React from 'react'
--- src/components/Profile/Details.tsx
+++ src/components/Profile/Details.tsx
@@ -15,7 +15,7 @@ import {
 } from '@heroicons/react/outline'
 import { BadgeCheckIcon } from '@heroicons/react/solid'
 import formatAddress from '@lib/formatAddress'
-import { getAvatar } from '@lib/getAvatar'
+import getAvatar from '@lib/getAvatar'
 import { isStaff } from '@lib/isStaff'
 import { isVerified } from '@lib/isVerified'
 import { linkifyOptions } from '@lib/linkifyOptions'
