7d31acee995c362189a8174c9e10c1213e2c3145
yoginth.eth <yoginth@hey.com>
2022-04-09 13:43:23 +0530

    feat: add timeout

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,9 @@
+import SEO from './utils/SEO'
+
 const Loading: React.FC = () => {
   return (
     <div className="flex flex-grow justify-center items-center h-screen animate-pulse">
+      <SEO />
       <img className="w-28 h-28" src="/logo.svg" alt="Logo" />
     </div>
   )
