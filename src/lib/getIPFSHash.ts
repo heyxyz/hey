0168226afff1eb557cbc629064753c481c43395c
yoginth.eth <yoginth@hey.com>
2022-04-09 18:23:58 +0530

    chore: use export default

--- src/lib/getIPFSHash.ts
+++ src/lib/getIPFSHash.ts
@@ -1,4 +1,4 @@
-export const getIPFSHash = (url: string): any => {
+const getIPFSHash = (url: string): any => {
   return url
     ?.match(
       /Qm[1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,}/
@@ -6,3 +6,5 @@ export const getIPFSHash = (url: string): any => {
     ?.at(0)
     ?.replace('.json', '')
 }
+
+export default getIPFSHash
