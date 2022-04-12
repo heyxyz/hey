1218964d349bc817f84d87521dff6c18e7b533bc
yoginth.eth <yoginth@hey.com>
2022-04-12 16:00:53 +0530

    feat: mvp nft page

---
+++ src/components/NFT/Create/index.tsx
@@ -0,0 +1,375 @@
+import LensHubProxy from '@abis/LensHubProxy.json'
+import { gql, useMutation, useQuery } from '@apollo/client'
+import { GridItemEight, GridItemFour, GridLayout } from '@components/GridLayout'
+import { CREATE_POST_TYPED_DATA_MUTATION } from '@components/Post/NewPost'
+import ChooseFile from '@components/Shared/ChooseFile'
+import SettingsHelper from '@components/Shared/SettingsHelper'
+import SwitchNetwork from '@components/Shared/SwitchNetwork'
+import { Button } from '@components/UI/Button'
+import { Card } from '@components/UI/Card'
+import { Form, useZodForm } from '@components/UI/Form'
+import { Input } from '@components/UI/Input'
+import { PageLoading } from '@components/UI/PageLoading'
+import { Spinner } from '@components/UI/Spinner'
+import { TextArea } from '@components/UI/TextArea'
+import AppContext from '@components/utils/AppContext'
+import SEO from '@components/utils/SEO'
+import { CreatePostBroadcastItemResult, Erc20 } from '@generated/types'
+import { PlusIcon } from '@heroicons/react/outline'
+import consoleLog from '@lib/consoleLog'
+import getTokenImage from '@lib/getTokenImage'
+import omit from '@lib/omit'
+import splitSignature from '@lib/splitSignature'
+import trackEvent from '@lib/trackEvent'
+import uploadAssetsToIPFS from '@lib/uploadAssetsToIPFS'
+import uploadToIPFS from '@lib/uploadToIPFS'
+import React, { ChangeEvent, FC, useContext, useState } from 'react'
+import toast from 'react-hot-toast'
+import {
+  CHAIN_ID,
+  CONNECT_WALLET,
+  DEFAULT_COLLECT_TOKEN,
+  ERROR_MESSAGE,
+  LENSHUB_PROXY,
+  WRONG_NETWORK
+} from 'src/constants'
+import Custom404 from 'src/pages/404'
+import { v4 as uuidv4 } from 'uuid'
+import {
+  useAccount,
+  useContractWrite,
+  useNetwork,
+  useSignTypedData
+} from 'wagmi'
+import { object, string } from 'zod'
+
+import Pending from './Pending'
+
+const MODULES_CURRENCY_QUERY = gql`
+  query EnabledCurrencyModules {
+    enabledModuleCurrencies {
+      name
+      symbol
+      decimals
+      address
+    }
+  }
+`
+
+const newNFTSchema = object({
+  name: string()
+    .min(2, { message: 'Name should be atleast 2 characters' })
+    .max(255, { message: 'Name should not exceed 255 characters' }),
+  amount: string().min(1, { message: 'Invalid amount' }),
+  referralFee: string()
+    .min(1, { message: 'Invalid Referral fee' })
+    .max(20, { message: 'Invalid Referral fee' }),
+  description: string()
+    .max(1000, { message: 'Description should not exceed 1000 characters' })
+    .nullable()
+})
+
+const Create: FC = () => {
+  const [nft, setNFT] = useState<string>()
+  const [nftType, setNFTType] = useState<string>()
+  const [isUploading, setIsUploading] = useState<boolean>(false)
+  const [uploading, setUploading] = useState<boolean>(false)
+  const [selectedCurrency, setSelectedCurrency] = useState<string>(
+    DEFAULT_COLLECT_TOKEN
+  )
+  const [selectedCurrencySymobol, setSelectedCurrencySymobol] =
+    useState<string>('WMATIC')
+  const { currentUser } = useContext(AppContext)
+  const { activeChain } = useNetwork()
+  const { data: account } = useAccount()
+  const { isLoading: signLoading, signTypedDataAsync } = useSignTypedData({
+    onError(error) {
+      toast.error(error?.message)
+    }
+  })
+  const { data: currencyData, loading } = useQuery(MODULES_CURRENCY_QUERY, {
+    onCompleted() {
+      consoleLog('Query', '#8b5cf6', `Fetched enabled module currencies`)
+    }
+  })
+  const {
+    data,
+    isLoading: writeLoading,
+    write
+  } = useContractWrite(
+    {
+      addressOrName: LENSHUB_PROXY,
+      contractInterface: LensHubProxy
+    },
+    'postWithSig',
+    {
+      onSuccess() {
+        form.reset()
+        trackEvent('new nft', 'create')
+      },
+      onError(error) {
+        toast.error(error?.message)
+      }
+    }
+  )
+
+  const form = useZodForm({
+    schema: newNFTSchema
+  })
+
+  const handleUpload = async (evt: ChangeEvent<HTMLInputElement>) => {
+    evt.preventDefault()
+    setUploading(true)
+    try {
+      // @ts-ignore
+      const attachment = await uploadAssetsToIPFS(evt.target.files[0])
+      setNFT(attachment.item)
+      setNFTType(attachment.type)
+    } finally {
+      setUploading(false)
+    }
+  }
+
+  const [createPostTypedData, { loading: typedDataLoading }] = useMutation(
+    CREATE_POST_TYPED_DATA_MUTATION,
+    {
+      onCompleted({
+        createPostTypedData
+      }: {
+        createPostTypedData: CreatePostBroadcastItemResult
+      }) {
+        consoleLog('Mutation', '#4ade80', 'Generated createPostTypedData')
+        const { typedData } = createPostTypedData
+        const {
+          profileId,
+          contentURI,
+          collectModule,
+          collectModuleData,
+          referenceModule,
+          referenceModuleData
+        } = typedData?.value
+
+        signTypedDataAsync({
+          domain: omit(typedData?.domain, '__typename'),
+          types: omit(typedData?.types, '__typename'),
+          value: omit(typedData?.value, '__typename')
+        }).then((signature) => {
+          const { v, r, s } = splitSignature(signature)
+          const inputStruct = {
+            profileId,
+            contentURI,
+            collectModule,
+            collectModuleData,
+            referenceModule,
+            referenceModuleData,
+            sig: {
+              v,
+              r,
+              s,
+              deadline: typedData.value.deadline
+            }
+          }
+          write({ args: inputStruct })
+        })
+      },
+      onError(error) {
+        toast.error(error.message ?? ERROR_MESSAGE)
+      }
+    }
+  )
+
+  const createNFT = async (
+    name: string,
+    amount: string,
+    referralFee: string,
+    description: string | null
+  ) => {
+    if (!account?.address) {
+      toast.error(CONNECT_WALLET)
+    } else if (activeChain?.id !== CHAIN_ID) {
+      toast.error(WRONG_NETWORK)
+    } else {
+      setIsUploading(true)
+      const { path } = await uploadToIPFS({
+        version: '1.0.0',
+        metadata_id: uuidv4(),
+        description: description,
+        content: description,
+        external_url: null,
+        image: nft,
+        imageMimeType: nftType,
+        name,
+        attributes: [
+          {
+            traitType: 'type',
+            value: 'nft'
+          }
+        ],
+        media: [],
+        appId: 'Lenster NFT'
+      }).finally(() => setIsUploading(false))
+
+      createPostTypedData({
+        variables: {
+          request: {
+            profileId: currentUser?.id,
+            contentURI: `https://ipfs.infura.io/ipfs/${path}`,
+            collectModule: {
+              feeCollectModule: {
+                amount: {
+                  currency: selectedCurrency,
+                  value: amount
+                },
+                recipient: currentUser?.ownedBy,
+                referralFee: parseInt(referralFee),
+                followerOnly: false
+              }
+            },
+            referenceModule: {
+              followerOnlyReferenceModule: false
+            }
+          }
+        }
+      })
+    }
+  }
+
+  if (loading) return <PageLoading message="Loading create NFT" />
+  if (!currentUser) return <Custom404 />
+
+  return (
+    <GridLayout>
+      <SEO title="Create NFT • Lenster" />
+      <GridItemFour>
+        <SettingsHelper
+          heading="Create NFT"
+          description="Create a new Non-fungible token"
+        />
+      </GridItemFour>
+      <GridItemEight>
+        <Card>
+          {data?.hash ? (
+            <Pending txHash={data?.hash} />
+          ) : (
+            <Form
+              form={form}
+              className="p-5 space-y-4"
+              onSubmit={({ name, amount, referralFee, description }) => {
+                createNFT(name, amount, referralFee, description)
+              }}
+            >
+              <div className="space-y-1.5">
+                <label>Image</label>
+                <div className="space-y-3">
+                  {nft && (
+                    <img
+                      className="object-cover w-full rounded-lg h-60"
+                      src={nft}
+                      alt={nft}
+                    />
+                  )}
+                  <div className="flex items-center space-x-3">
+                    <ChooseFile
+                      onChange={(evt: ChangeEvent<HTMLInputElement>) =>
+                        handleUpload(evt)
+                      }
+                    />
+                    {uploading && <Spinner size="sm" />}
+                  </div>
+                </div>
+              </div>
+              <Input
+                label="Name"
+                type="text"
+                placeholder="Item name"
+                {...form.register('name')}
+              />
+              <TextArea
+                label="Description"
+                placeholder="Tell us something about your NFT!"
+                {...form.register('description')}
+              />
+              <div>
+                <div className="mb-1 font-medium text-gray-800 dark:text-gray-200">
+                  Select Currency
+                </div>
+                <select
+                  className="w-full bg-white border border-gray-300 outline-none rounded-xl dark:bg-gray-800 dark:border-gray-700/80 disabled:bg-gray-500 disabled:bg-opacity-20 disabled:opacity-60 focus:border-brand-500 focus:ring-brand-400"
+                  onChange={(e) => {
+                    const currency = e.target.value.split('-')
+                    setSelectedCurrency(currency[0])
+                    setSelectedCurrencySymobol(currency[1])
+                  }}
+                >
+                  {currencyData?.enabledModuleCurrencies?.map(
+                    (currency: Erc20) => (
+                      <option
+                        key={currency.symbol}
+                        value={`${currency.address}-${currency.symbol}`}
+                      >
+                        {currency.name}
+                      </option>
+                    )
+                  )}
+                </select>
+              </div>
+              <Input
+                label="Price per unit"
+                type="number"
+                step="0.0001"
+                min="0"
+                max="100000"
+                prefix={
+                  <img
+                    className="w-6 h-6"
+                    src={getTokenImage(selectedCurrencySymobol)}
+                    alt={selectedCurrencySymobol}
+                  />
+                }
+                placeholder="5"
+                {...form.register('amount')}
+              />
+              <Input
+                label="Referral Fee"
+                type="number"
+                placeholder="5%"
+                min="0"
+                max="100"
+                {...form.register('referralFee')}
+              />
+              <div className="ml-auto">
+                {activeChain?.unsupported ? (
+                  <SwitchNetwork />
+                ) : (
+                  <Button
+                    type="submit"
+                    disabled={
+                      typedDataLoading ||
+                      isUploading ||
+                      signLoading ||
+                      writeLoading
+                    }
+                    icon={
+                      typedDataLoading ||
+                      isUploading ||
+                      signLoading ||
+                      writeLoading ? (
+                        <Spinner size="xs" />
+                      ) : (
+                        <PlusIcon className="w-4 h-4" />
+                      )
+                    }
+                  >
+                    Create
+                  </Button>
+                )}
+              </div>
+            </Form>
+          )}
+        </Card>
+      </GridItemEight>
+    </GridLayout>
+  )
+}
+
+export default Create
