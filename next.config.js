7e3cc973c9985d3e9dbf205ba662b4eca2496b56
yoginth.eth <yoginth@hey.com>
2022-04-12 10:24:14 +0530

    feat: add pwa support

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
-module.exports = {
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    runtimeCaching
+  },
   reactStrictMode: true
-}
+})
