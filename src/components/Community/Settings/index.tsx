52da3cfb122023b7754a9fb681cc0788db048b03
yoginth.eth <yoginth@hey.com>
2022-04-14 12:18:32 +0530

    feat: add delete info and warnings

--- src/components/Community/Settings/index.tsx
+++ src/components/Community/Settings/index.tsx
@@ -1,6 +1,7 @@
 import { useMutation } from '@apollo/client'
 import { HIDE_POST_MUTATION } from '@components/Post/Actions/Menu/Delete'
 import { Button } from '@components/UI/Button'
+import { WarningMessage } from '@components/UI/WarningMessage'
 import { Community } from '@generated/lenstertypes'
 import { TrashIcon } from '@heroicons/react/outline'
 import trackEvent from '@lib/trackEvent'
@@ -18,9 +19,18 @@ const Settings: FC<Props> = ({ community }) => {
   })
 
   return (
-    <div className="p-5">
+    <div className="p-5 space-y-5">
       <div>
+        <WarningMessage message="Only delete settings is available now, you can't edit any part of the community right now!" />
+      </div>
+      <div className="space-y-2">
+        <div className="font-bold text-red-500">Danger Zone</div>
+        <p>
+          Deleting your community will delete only from indexers and not from
+          the blockchain.
+        </p>
         <Button
+          className="!mt-5"
           icon={<TrashIcon className="h-5 w-5" />}
           variant="danger"
           onClick={() => {
