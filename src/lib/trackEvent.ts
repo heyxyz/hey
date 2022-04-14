5d50d50ad18d97d3e89bca0e1feeb9ac48e20471
yoginth.eth <yoginth@hey.com>
2022-04-14 22:04:59 +0530

    chore: add back umami

--- src/lib/trackEvent.ts
+++ src/lib/trackEvent.ts
@@ -4,13 +4,11 @@ import consoleLog from './consoleLog'
 
 const trackEvent = (event: string, type: string | null = 'click') => {
   // @ts-ignore
-  if (!event || !IS_PRODUCTION || !window?.gtag) return
+  if (!event || !IS_PRODUCTION || !window?.umami) return
 
   consoleLog('TrackEvent', '#22c55e', event)
   // @ts-ignore
-  return window?.gtag('event', type, {
-    event_label: event
-  })
+  return window?.umami?.trackEvent(event, type)
 }
 
 export default trackEvent
