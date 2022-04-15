99808a459444c279a4abf441a069284e91c18e7d
yoginth.eth <yoginth@hey.com>
2022-04-15 20:18:11 +0530

    docs: add allowance docs

--- src/components/Settings/Allowance/Module.tsx
+++ src/components/Settings/Allowance/Module.tsx
@@ -4,6 +4,7 @@ import GetModuleIcon from '@components/utils/GetModuleIcon'
 import { ApprovedAllowanceAmount } from '@generated/types'
 import { getModule } from '@lib/getModule'
 import React, { FC, useState } from 'react'
+import { POLYGONSCAN_URL } from 'src/constants'
 
 import AllowanceButton from './Button'
 
@@ -12,24 +13,29 @@ interface Props {
 }
 
 const Module: FC<Props> = ({ module }) => {
-  const [allowed, setAllowed] = useState<boolean>(module.allowance !== '0x00')
+  const [allowed, setAllowed] = useState<boolean>(module?.allowance !== '0x00')
 
   return (
-    <Card key={module.module}>
+    <Card key={module?.module}>
       <CardBody className="items-center justify-between block sm:flex">
         <div className="overflow-hidden mr-1.5 mb-3 sm:mb-0">
           <div className="flex items-center space-x-2">
             <div className="text-brand-500">
-              <GetModuleIcon module={module.module} size={4} />
+              <GetModuleIcon module={module?.module} size={4} />
             </div>
             <div className="font-bold whitespace-nowrap">
-              {getModule(module.module).name}
+              {getModule(module?.module).name}
             </div>
-            <HelpTooltip content={getModule(module.module).helper} />
+            <HelpTooltip content={getModule(module?.module).helper} />
           </div>
-          <div className="text-sm text-gray-500 truncate">
-            {module.contractAddress}
-          </div>
+          <a
+            href={`${POLYGONSCAN_URL}/address/${module?.contractAddress}`}
+            className="text-sm text-gray-500 truncate"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {module?.contractAddress}
+          </a>
         </div>
         <AllowanceButton
           module={module}
