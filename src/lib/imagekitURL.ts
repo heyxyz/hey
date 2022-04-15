bd98d58bba3e5235f537ded85399b0084075bfb9
yoginth.eth <yoginth@hey.com>
2022-04-15 10:14:40 +0530

    fix: imagekitURL ds issues

--- src/lib/imagekitURL.ts
+++ src/lib/imagekitURL.ts
@@ -1,10 +1,6 @@
 import { IMAGEKIT_URL } from 'src/constants'
 
-const imagekitURL = (
-  url: string | undefined | null,
-  height: string | number = '',
-  width: string | number = ''
-): string => {
+const imagekitURL = (url: string, height?: number, width?: number): string => {
   return height || width
     ? `${IMAGEKIT_URL}/tr:w-${height},h-${width}/${url}`
     : `${IMAGEKIT_URL}/${url}`
