73b13b238469a5f4b81c2ed2d942eb6decb69808
yoginth.eth <yoginth@hey.com>
2022-04-18 09:13:30 +0530

    fix: collect link

--- src/components/Post/Type/Collected.tsx
+++ src/components/Post/Type/Collected.tsx
@@ -1,8 +1,10 @@
 import Slug from '@components/Shared/Slug'
 import { LensterPost } from '@generated/lenstertypes'
 import { CollectionIcon } from '@heroicons/react/outline'
+import formatAddress from '@lib/formatAddress'
 import Link from 'next/link'
 import React, { FC } from 'react'
+import { POLYGONSCAN_URL } from 'src/constants'
 
 interface Props {
   post: LensterPost
@@ -15,11 +17,24 @@ const Collected: FC<Props> = ({ post, type }) => {
       <CollectionIcon className="w-4 h-4" />
       <div className="flex items-center space-x-1">
         <div>{type} by</div>
-        <Link href={`/u/${post?.collectedBy?.defaultProfile?.handle}`}>
-          <a href={`/u/${post?.collectedBy?.defaultProfile?.handle}`}>
-            <Slug slug={post?.collectedBy?.defaultProfile?.handle} prefix="@" />
+        {post?.collectedBy?.defaultProfile ? (
+          <Link href={`/u/${post?.collectedBy?.defaultProfile?.handle}`}>
+            <a href={`/u/${post?.collectedBy?.defaultProfile?.handle}`}>
+              <Slug
+                slug={post?.collectedBy?.defaultProfile?.handle}
+                prefix="@"
+              />
+            </a>
+          </Link>
+        ) : (
+          <a
+            href={`${POLYGONSCAN_URL}/address/${post?.collectedBy?.address}`}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <Slug slug={formatAddress(post?.collectedBy?.address)} />
           </a>
-        </Link>
+        )}
       </div>
     </div>
   )
