aabd97f8cf3a21aafb9b3776b95d1a4321178560
bigint <bigint@lenster.xyz>
2022-04-18 19:26:26 +0530

    feat: add beta toggle

---
+++ src/lib/isBeta.ts
@@ -0,0 +1,8 @@
+import { Profile } from '@generated/types'
+
+import getAttribute from './getAttribute'
+
+const isBeta = (profile: Profile): boolean =>
+  getAttribute(profile?.attributes, 'isBeta') === 'true'
+
+export default isBeta
