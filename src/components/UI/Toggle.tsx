acbdc78f006f8e9c0bb01173a7b24439f2ab7541
bigint <bigint@lenster.xyz>
2022-04-18 19:30:50 +0530

    chore: update toggle button

--- src/components/UI/Toggle.tsx
+++ src/components/UI/Toggle.tsx
@@ -14,14 +14,14 @@ export const Toggle: FC<Props> = ({ on, setOn }) => {
       onChange={setOn}
       className={clsx(
         on ? 'bg-brand-500' : 'bg-gray-200',
-        'inline-flex h-[24px] w-[60px] border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none'
+        'inline-flex h-[22px] w-[42.5px] border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none'
       )}
     >
       <span
         aria-hidden="true"
         className={clsx(
-          on ? 'translate-x-9' : 'translate-x-0',
-          'pointer-events-none inline-block h-[20px] w-[20px] rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200'
+          on ? 'translate-x-5' : 'translate-x-0',
+          'pointer-events-none inline-block h-[18px] w-[18px] rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200'
         )}
       />
     </Switch>
