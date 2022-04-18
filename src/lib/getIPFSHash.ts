1901510e4b5088ccfcdbdfeda57d6f85a01a06db
yoginth.eth <yoginth@hey.com>
2022-04-18 11:49:26 +0530

    fix: reduce any

--- src/lib/getIPFSHash.ts
+++ src/lib/getIPFSHash.ts
@@ -1,4 +1,4 @@
-const getIPFSHash = (url: string): any => {
+const getIPFSHash = (url: string): string | undefined => {
   return url
     ?.match(
       /Qm[1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,}/
