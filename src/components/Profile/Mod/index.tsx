4422b4f952dcc6f2efc04797b3457ec398300b0e
bigint <bigint@lenster.xyz>
2022-04-18 20:34:46 +0530

    chore: update mod

--- src/components/Profile/Mod/index.tsx
+++ src/components/Profile/Mod/index.tsx
@@ -1,7 +1,14 @@
 import { Card, CardBody } from '@components/UI/Card'
 import { Profile } from '@generated/types'
-import { AtSymbolIcon, CashIcon, HashtagIcon } from '@heroicons/react/outline'
+import {
+  AtSymbolIcon,
+  BeakerIcon,
+  CashIcon,
+  HashtagIcon
+} from '@heroicons/react/outline'
 import formatAddress from '@lib/formatAddress'
+import getAttribute from '@lib/getAttribute'
+import isBeta from '@lib/isBeta'
 import React, { FC, ReactChild } from 'react'
 import CopyToClipboard from 'react-copy-to-clipboard'
 import toast from 'react-hot-toast'
@@ -37,7 +44,15 @@ const ProfileMod: FC<Props> = ({ profile }) => {
     <Card className="mt-5 border-yellow-400 !bg-yellow-300 !bg-opacity-20">
       <CardBody>
         <div className="text-lg font-bold">Details</div>
-        <div className="mt-3 space-y-1">
+        <div className="mt-3 space-y-1.5">
+          {getAttribute(profile?.attributes, 'app') === 'Lenster' && (
+            <MetaDetails
+              icon={<img className="h-4 w-4" src="/logo.svg" alt="Logo" />}
+              value={profile?.handle}
+            >
+              Lenster account
+            </MetaDetails>
+          )}
           <MetaDetails
             icon={<HashtagIcon className="w-4 h-4 text-gray-500" />}
             value={profile?.id}
@@ -55,6 +70,12 @@ const ProfileMod: FC<Props> = ({ profile }) => {
             value={profile?.handle}
           >
             {profile?.handle}
+          </MetaDetails>
+          <MetaDetails
+            icon={<BeakerIcon className="w-4 h-4 text-gray-500" />}
+            value={profile?.handle}
+          >
+            {isBeta(profile) ? 'Beta user' : 'Non-beta user'}
           </MetaDetails>
         </div>
       </CardBody>
