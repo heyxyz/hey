6388c544fabe9eb223195b269afa02829dfbf8b6
yoginth.eth <yoginth@hey.com>
2022-04-21 09:46:42 +0530

    chore: mvp search

--- src/components/UI/MentionTextArea.tsx
+++ src/components/UI/MentionTextArea.tsx
@@ -1,3 +1,7 @@
+import { useLazyQuery } from '@apollo/client'
+import { SEARCH_USERS_QUERY } from '@components/Shared/Navbar/Search'
+import { Profile } from '@generated/types'
+import consoleLog from '@lib/consoleLog'
 import { Dispatch, FC } from 'react'
 import { Mention, MentionsInput } from 'react-mentions'
 
@@ -12,19 +16,47 @@ export const MentionTextArea: FC<Props> = ({
   setValue,
   placeholder = ''
 }) => {
+  const [searchUsers, { data: searchUsersData }] = useLazyQuery(
+    SEARCH_USERS_QUERY,
+    {
+      onCompleted(data) {
+        consoleLog(
+          'Lazy Query',
+          '#8b5cf6',
+          `Fetched ${data?.search?.items?.length} user mention result for ${value}`
+        )
+      }
+    }
+  )
+
+  const fetchUsers = (query: string, callback: any) => {
+    if (!query) return
+
+    searchUsers({
+      variables: { request: { type: 'PROFILE', query, limit: 8 } }
+    })
+      .then(({ data }) =>
+        data?.search?.items?.map((user: Profile) => ({
+          id: user.handle,
+          display: user.handle
+        }))
+      )
+      .then(callback)
+  }
+
   return (
     <MentionsInput
       className="mention-input h-16 mb-2"
       value={value}
       placeholder={placeholder}
-      onChange={(e) => setValue(e.target.value)}
+      onChange={(e) => {
+        setValue(e.target.value)
+      }}
     >
       <Mention
         trigger="@"
-        data={[
-          { id: 'yoginth', display: 'yoginth' },
-          { id: 'filip', display: 'filip' }
-        ]}
+        displayTransform={(login) => `@${login}`}
+        data={fetchUsers}
       />
     </MentionsInput>
   )
