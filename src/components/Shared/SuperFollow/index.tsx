835a349baf688b564e27b1c4042d42ea9fe01767
yoginth.eth <yoginth@hey.com>
2022-04-22 15:08:12 +0530

    chore: add again button

--- src/components/Shared/SuperFollow/index.tsx
+++ src/components/Shared/SuperFollow/index.tsx
@@ -40,7 +40,8 @@ const SuperFollow: FC<Props> = ({
       <Modal
         title={
           <span>
-            Super follow <Slug slug={profile?.handle} prefix="@" />
+            Super follow <Slug slug={profile?.handle} prefix="@" />{' '}
+            {again ? 'again' : ''}
           </span>
         }
         icon={<StarIcon className="w-5 h-5 text-pink-500" />}
