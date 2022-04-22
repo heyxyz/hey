4280ff3234662cdafe8d978b26c1ded79833b19a
bigint <bigint@lenster.xyz>
2022-04-22 14:58:58 +0530

    feat: add super follow badge

--- src/components/Profile/SuperFollowed.tsx
+++ src/components/Profile/SuperFollowed.tsx
@@ -3,14 +3,16 @@ import React, { FC } from 'react'
 
 interface Props {
   times: number
+  following?: boolean
 }
 
-const SuperFollowed: FC<Props> = ({ times }) => {
+const SuperFollowed: FC<Props> = ({ times, following = false }) => {
   return (
     <div className="flex items-center gap-1.5 text-pink-500 text-[13px]">
       <HeartIcon className="h-4 w-4" />
       <div>
-        Super followed {times ? `${times} ${times > 1 ? 'times' : 'time'}` : ''}
+        Super {following ? 'following' : 'followed'}{' '}
+        {times ? `${times} ${times > 1 ? 'times' : 'time'}` : ''}
       </div>
     </div>
   )
