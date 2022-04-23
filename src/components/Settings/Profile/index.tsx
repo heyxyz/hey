eb3cef9578514ad3d6d430683e62eed18e92eba0
yoginth.eth <yoginth@hey.com>
2022-04-23 11:08:15 +0530

    chore: use state

--- src/components/Settings/Profile/index.tsx
+++ src/components/Settings/Profile/index.tsx
@@ -4,9 +4,11 @@ import { Card, CardBody } from '@components/UI/Card'
 import { PageLoading } from '@components/UI/PageLoading'
 import AppContext from '@components/utils/AppContext'
 import SEO from '@components/utils/SEO'
+import { PhotographIcon } from '@heroicons/react/outline'
 import consoleLog from '@lib/consoleLog'
+import clsx from 'clsx'
 import { NextPage } from 'next'
-import React, { useContext, useState } from 'react'
+import React, { FC, ReactChild, useContext, useState } from 'react'
 import Custom404 from 'src/pages/404'
 import Custom500 from 'src/pages/500'
 
@@ -54,13 +56,13 @@ const PROFILE_SETTINGS_QUERY = gql`
 
 const ProfileSettings: NextPage = () => {
   const { currentUser } = useContext(AppContext)
-  const [nftSettings, setNftSettings] = useState<boolean>(false)
+  const [settingsType, setSettingsType] = useState<'NFT' | 'AVATAR'>('AVATAR')
   const { data, loading, error } = useQuery(PROFILE_SETTINGS_QUERY, {
     variables: { request: { profileIds: currentUser?.id } },
     skip: !currentUser?.id,
     onCompleted(data) {
       consoleLog('Query', '#8b5cf6', `Fetched profile settings`)
-      setNftSettings(!!data?.profiles?.items[0]?.picture?.uri)
+      setSettingsType(data?.profiles?.items[0]?.picture?.uri ? 'NFT' : 'AVATAR')
     }
   })
 
@@ -69,6 +71,31 @@ const ProfileSettings: NextPage = () => {
   if (!currentUser) return <Custom404 />
 
   const profile = data?.profiles?.items[0]
+
+  interface TypeButtonProps {
+    name: string
+    icon: ReactChild
+    type: 'NFT' | 'AVATAR'
+  }
+
+  const TypeButton: FC<TypeButtonProps> = ({ name, icon, type }) => (
+    <button
+      type="button"
+      onClick={() => {
+        setSettingsType(type)
+      }}
+      className={clsx(
+        {
+          'text-brand bg-brand-100 dark:bg-opacity-20 bg-opacity-100 font-bold':
+            settingsType === type
+        },
+        'flex items-center space-x-2 rounded-lg px-4 sm:px-3 py-2 sm:py-1 text-brand hover:bg-brand-100 dark:hover:bg-opacity-20 hover:bg-opacity-100'
+      )}
+    >
+      {icon}
+      <div className="hidden sm:block">{name}</div>
+    </button>
+  )
 
   return (
     <GridLayout>
@@ -79,8 +106,20 @@ const ProfileSettings: NextPage = () => {
       <GridItemEight className="space-y-5">
         <Profile profile={profile} />
         <Card>
-          <CardBody>
-            {nftSettings ? (
+          <CardBody className="space-y-5">
+            <div className="flex items-center space-x-2">
+              <TypeButton
+                icon={<PhotographIcon className="h-5 w-5" />}
+                type="AVATAR"
+                name="Upload avatar"
+              />
+              <TypeButton
+                icon={<PhotographIcon className="h-5 w-5" />}
+                type="NFT"
+                name="NFT Avatar"
+              />
+            </div>
+            {settingsType === 'NFT' ? (
               <NFTPicture profile={profile} />
             ) : (
               <Picture profile={profile} />
