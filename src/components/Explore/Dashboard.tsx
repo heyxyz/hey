48ee21dd383cacf2dd99fc8eda8aa6047664a3dd
franz.media <fkrekeler@gmail.com>
2022-04-24 07:57:08 +0200

    POAP MVP intgration

--- src/components/Explore/Dashboard.tsx
+++ src/components/Explore/Dashboard.tsx
@@ -1,10 +1,12 @@
 import { ApolloClient, gql, InMemoryCache } from '@apollo/client'
 import PostsShimmer from '@components/Shared/Shimmer/PostsShimmer'
+import { Card, CardBody } from '@components/UI/Card'
 import React, { FC, useEffect, useState } from 'react'
 /*
   query MyQuery {
   posts(first: 10, orderBy: timestamp, orderDirection: desc) {
     id
+    imageURI
     profileId {
       id
       creator
@@ -12,23 +14,15 @@ import React, { FC, useEffect, useState } from 'react'
   }
 }
 */
-const APIURL = 'https://api.thegraph.com/subgraphs/name/anudit/lens-protocol'
+const APIURL = 'https://api.thegraph.com/subgraphs/name/franz101/lens-protocol'
 const EXPLORE_POSTS_QUERY = gql`
   query {
-    posts(first: 10, orderBy: timestamp, orderDirection: desc) {
-      id
-      profileId {
-        id
-        creator
-      }
-    }
-  }
-`
-const EXPLORE_PROFILES_QUERY = gql`
-  query {
-    profiles(first: 10, orderBy: pubCount) {
+    profiles(first: 20, orderBy: pubCount, orderDirection: desc) {
       id
       pubCount
+      owner
+      imageURI
+      handle
     }
   }
 `
@@ -50,31 +44,81 @@ const Dashboard: FC<Props> = ({ feedType = 'TOP_USERS' }) => {
   useEffect(() => {
     const fetchData = async () => {
       const data = await client.query({
-        query:
-          'EXPLORE_PROFILES_QUERY' === query
-            ? EXPLORE_PROFILES_QUERY
-            : EXPLORE_POSTS_QUERY
+        query: EXPLORE_POSTS_QUERY as any
       })
-      switch (query) {
-        case 'EXPLORE_PROFILES_QUERY':
-          setData(data.data.profiles)
-          break
-        default:
-          setData(data.data.posts)
-          break
-      }
+      setData(data.data.profiles)
     }
-
     fetchData()
-  }, [query])
+  }, [])
 
   return (
     <>
-      <h3 onClick={() => setQuery('EXPLORE_PROFILES_QUERY')}>Profiles</h3>
-      <h3 onClick={() => setQuery('EXPLORE_POSTS_QUERY')}>Posts</h3>
-
       {!data && <PostsShimmer />}
-      {JSON.stringify(data)}
+      <h1>Power Users</h1>
+      <h2>Our most active community members</h2>
+      {(data || []).map((profile: any) => {
+        return (
+          <Card key="profile">
+            <CardBody>
+              <h3 key="itemHandler">{profile.handle}</h3>
+
+              <div className="flex justify-between items-center">
+                <a href={`/u/${profile?.handle}`}>
+                  <div className="flex items-center space-x-3">
+                    <img
+                      height={100}
+                      width={100}
+                      src={profile?.imageURI}
+                      // className={clsx(
+                      //   isBig ? 'w-14 h-14' : 'w-10 h-10',
+                      //   'bg-gray-200 rounded-full border dark:border-gray-700/80'
+                      // )}
+                      alt={profile?.handle}
+                    />
+                  </div>
+                </a>
+              </div>
+              {/* <Attachments
+                attachments={
+                  'https://ik.imagekit.io/lensterdev/tr:n-avatar/https://ipfs.infura.io/ipfs/QmaNPt96Y3NYwHDsrPD2Lv2Y9cySqpTRHpc4rsHwzei9bJ'
+                }
+              /> */}
+              {/* <div className="flex justify-between pb-4">
+                <UserProfile
+                  profile={
+                    post?.__typename === 'Mirror'
+                      ? post?.mirrorOf?.profile
+                      : post?.profile
+                  }
+                />
+                <Link href={`/posts/${post?.id}`}>
+                  <a
+                    href={`/posts/${post?.id}`}
+                    className="text-sm text-gray-500"
+                  >
+                    {dayjs(new Date(post?.createdAt)).fromNow()}
+                  </a>
+                </Link>
+              </div>
+              <PostBody post={post} />
+              {post?.metadata?.media?.length > 0 ? (
+                <Attachments attachments={post?.metadata?.media} />
+              ) : (
+                post?.metadata?.content &&
+                postType !== 'crowdfund' &&
+                postType !== 'community' &&
+                !!getURLFromPublication(post?.metadata?.content) && (
+                  <IFramely
+                    url={getURLFromPublication(post?.metadata?.content)}
+                  />
+                )
+              )} */}
+            </CardBody>
+            {/* <PostActions post={post} /> */}
+          </Card>
+        )
+      })}
+      {/* {JSON.stringify(data)} */}
       {/* {data?.explorePublications?.items?.length === 0 && (
         <EmptyState
           message={<div>No posts yet!</div>}
