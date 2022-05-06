95d57219c05485301cc9ddb2b7b70ec6e9890e98
bigint <bigint@lenster.xyz>
2022-05-06 12:12:47 +0530

    chore: fix mention input area autogrow

--- src/components/UI/MentionTextArea.tsx
+++ src/components/UI/MentionTextArea.tsx
@@ -91,7 +91,7 @@ export const MentionTextArea: FC<Props> = ({
   return (
     <div className="mb-2">
       <MentionsInput
-        className="mention-input h-16"
+        className="mention-input min-h-[4rem] pt-3"
         value={value}
         placeholder={placeholder}
         onChange={(e) => {
