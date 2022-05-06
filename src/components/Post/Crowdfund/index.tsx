ab83f4b6f6ae5e2e6288575bc787c69ce73e0d50
yoginth.eth <yoginth@hey.com>
2022-05-07 00:30:55 +0530

    feat: add markup comp

--- src/components/Post/Crowdfund/index.tsx
+++ src/components/Post/Crowdfund/index.tsx
@@ -1,16 +1,12 @@
 import { gql, useQuery } from '@apollo/client'
 import { GridItemSix, GridLayout } from '@components/GridLayout'
 import Collectors from '@components/Shared/Collectors'
+import Markup from '@components/Shared/Markup'
 import ReferralAlert from '@components/Shared/ReferralAlert'
 import CrowdfundShimmer from '@components/Shared/Shimmer/CrowdfundShimmer'
 import { Card } from '@components/UI/Card'
 import { Modal } from '@components/UI/Modal'
 import { Tooltip } from '@components/UI/Tooltip'
-import { HashtagMatcher } from '@components/utils/matchers/HashtagMatcher'
-import { MDBoldMatcher } from '@components/utils/matchers/markdown/MDBoldMatcher'
-import { MDCodeMatcher } from '@components/utils/matchers/markdown/MDCodeMatcher'
-import { MDItalicMatcher } from '@components/utils/matchers/markdown/MDItalicMatcher'
-import { MentionMatcher } from '@components/utils/matchers/MentionMatcher'
 import { LensterPost } from '@generated/lenstertypes'
 import {
   CashIcon,
@@ -21,8 +17,6 @@ import consoleLog from '@lib/consoleLog'
 import getTokenImage from '@lib/getTokenImage'
 import imagekitURL from '@lib/imagekitURL'
 import clsx from 'clsx'
-import { Interweave } from 'interweave'
-import { UrlMatcher } from 'interweave-autolink'
 import React, { FC, ReactNode, useEffect, useState } from 'react'
 import { STATIC_ASSETS } from 'src/constants'
 
@@ -129,19 +123,11 @@ const Crowdfund: FC<Props> = ({ fund }) => {
           <div className="mr-0 space-y-1 sm:mr-16">
             <div className="text-xl font-bold">{fund?.metadata?.name}</div>
             <div className="text-sm leading-7 whitespace-pre-wrap break-words">
-              <Interweave
-                content={fund?.metadata?.description
+              <Markup>
+                {fund?.metadata?.description
                   ?.replace(/\n\s*\n/g, '\n\n')
                   .trim()}
-                matchers={[
-                  new UrlMatcher('url'),
-                  new HashtagMatcher('hashtag'),
-                  new MentionMatcher('mention'),
-                  new MDBoldMatcher('mdBold'),
-                  new MDItalicMatcher('mdItalic'),
-                  new MDCodeMatcher('mdCode')
-                ]}
-              />
+              </Markup>
             </div>
             <div className="block sm:flex items-center !my-3 space-y-2 sm:space-y-0 sm:space-x-3">
               {fund?.stats?.totalAmountOfCollects > 0 && (
