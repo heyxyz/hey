caa94be6c8c39a9037212ea295d1eff27bda0099
yoginth.eth <yoginth@hey.com>
2022-05-07 14:30:20 +0530

    feat: spoiler mvp

--- src/components/utils/matchers/SpoilerMatcher.tsx
+++ src/components/utils/matchers/SpoilerMatcher.tsx
@@ -2,11 +2,11 @@ import { ChildrenNode, Matcher } from 'interweave'
 import React from 'react'
 
 export class SpoilerMatcher extends Matcher {
-  replaceWith(children: ChildrenNode, props: object) {
+  replaceWith(children: ChildrenNode) {
     let count = 1
 
     return (
-      <span className="mx-0.5 text-black text-opacity-0 bg-gray-900 rounded-lg cursor-pointer active:text-opacity-100 active:bg-gray-200 px-[5px] py-[2px]">
+      <span className="text-black text-opacity-0 bg-gray-700 rounded-md cursor-pointer hover:bg-gray-800 active:text-opacity-100 active:bg-gray-200 px-[2px] py-[1px] dark:active:bg-black active:dark:text-white">
         {children}
       </span>
     )
