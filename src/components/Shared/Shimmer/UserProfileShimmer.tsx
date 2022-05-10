2ff14ee21f35453cdd12b535054b28d0a9a313c1
yoginth.eth <yoginth@hey.com>
2022-05-10 11:07:40 +0530

    fix: padding in shimmer

--- src/components/Shared/Shimmer/UserProfileShimmer.tsx
+++ src/components/Shared/Shimmer/UserProfileShimmer.tsx
@@ -11,18 +11,18 @@ const UserProfileShimmer: FC<Props> = ({
   isBig = false
 }) => {
   return (
-    <div className="flex justify-between items-center">
+    <div className="flex justify-between items-center py-1">
       <div className="flex items-center space-x-3">
         <div
           className={clsx(
-            isBig ? 'w-14 h-14' : 'w-11 h-11',
+            isBig ? 'w-14 h-14' : 'w-10 h-10',
             'rounded-full shimmer'
           )}
         />
         <div className="space-y-3">
           <div className="w-28 h-3 rounded-lg shimmer" />
           <div className="w-20 h-3 rounded-lg shimmer" />
-          <div className="w-48 h-3 rounded-lg shimmer" />
+          {isBig && <div className="w-48 h-3 rounded-lg shimmer" />}
         </div>
       </div>
       {showFollow && <div className="w-10 h-8 rounded-lg shimmer" />}
