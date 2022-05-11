b1b48fce95f51281636fda7c2048dfce770e6aaa
bigint <bigint@lenster.xyz>
2022-05-11 21:46:56 +0530

    feat: add BROADCAST_MUTATION

---
+++ src/graphql/BroadcastMutation.ts
@@ -0,0 +1,14 @@
+import { gql } from '@apollo/client'
+
+export const BROADCAST_MUTATION = gql`
+  mutation Broadcast($request: BroadcastRequest!) {
+    broadcast(request: $request) {
+      ... on RelayerResult {
+        txHash
+      }
+      ... on RelayError {
+        reason
+      }
+    }
+  }
+`
