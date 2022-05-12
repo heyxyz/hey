b5fc1ae28c132db4561ed7f4d7d2a0c3af863b66
yoginth.eth <yoginth@hey.com>
2022-05-12 21:45:55 +0530

    feat: add broadcast data support for community join

--- src/components/Community/Join.tsx
+++ src/components/Community/Join.tsx
@@ -89,12 +89,14 @@ const Join: FC<Props> = ({ community, setJoined, showJoin = true }) => {
     }
   )
 
-  const [broadcast, { data: broadcastData, loading: broadcastLoading }] =
-    useMutation(BROADCAST_MUTATION, {
+  const [broadcast, { loading: broadcastLoading }] = useMutation(
+    BROADCAST_MUTATION,
+    {
       onError(error) {
         toast.error(error.message ?? ERROR_MESSAGE)
       }
-    })
+    }
+  )
   const [createCollectTypedData, { loading: typedDataLoading }] = useMutation(
     CREATE_COLLECT_TYPED_DATA_MUTATION,
     {
