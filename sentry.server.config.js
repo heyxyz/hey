b2258fcfdb3342eb2084215294e1d3e87ea2ff0a
yoginth.eth <yoginth@hey.com>
2022-05-12 23:09:12 +0530

    fix: remove duplicate release for sentry

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -1,10 +1,9 @@
 import * as Sentry from '@sentry/nextjs'
 
-import { GIT_COMMIT_SHA, IS_PRODUCTION } from './src/constants'
+import { IS_PRODUCTION } from './src/constants'
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   enabled: IS_PRODUCTION,
-  release: GIT_COMMIT_SHA,
   tracesSampleRate: 1.0
 })
