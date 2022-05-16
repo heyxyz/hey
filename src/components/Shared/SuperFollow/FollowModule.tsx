f77b640b10ddeb854410c1235676a6991a179f32
yoginth.eth <yoginth@hey.com>
2022-05-16 18:44:59 +0530

    chore: remove unused balanceLoading

--- src/components/Shared/SuperFollow/FollowModule.tsx
+++ src/components/Shared/SuperFollow/FollowModule.tsx
@@ -182,7 +182,7 @@ const FollowModule: FC<Props> = ({
     }
   )
 
-  const { data: balanceData } = useBalance({
+  const { data: balanceData, isLoading: balanceLoading } = useBalance({
     addressOrName: currentUser?.ownedBy,
     token: followModule?.amount?.asset?.address
   })
