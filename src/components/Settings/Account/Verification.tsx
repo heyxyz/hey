61e1a4f46b4583fcea346e97d25160d5b44ce7e4
yoginth.eth <yoginth@hey.com>
2022-05-16 19:59:30 +0530

    feat: add verification component

---
+++ src/components/Settings/Account/Verification.tsx
@@ -0,0 +1,36 @@
+import { Card, CardBody } from '@components/UI/Card'
+import AppContext from '@components/utils/AppContext'
+import { BadgeCheckIcon } from '@heroicons/react/solid'
+import isVerified from '@lib/isVerified'
+import React, { FC, useContext } from 'react'
+
+const Verification: FC = () => {
+  const { currentUser } = useContext(AppContext)
+
+  return (
+    <Card>
+      <CardBody className="space-y-2 linkify">
+        <div className="text-lg font-bold">Verified</div>
+        {isVerified(currentUser?.id) ? (
+          <div className="flex items-center space-x-1.5">
+            <span>Yes</span>
+            <BadgeCheckIcon className="h-5 w-5 text-brand" />
+          </div>
+        ) : (
+          <div>
+            No.{' '}
+            <a
+              href="https://tally.so/r/wgDajK"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              Request Verification
+            </a>
+          </div>
+        )}
+      </CardBody>
+    </Card>
+  )
+}
+
+export default Verification
