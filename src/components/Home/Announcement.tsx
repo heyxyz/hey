a1d4d7a9eb3d4c63d9fcd898f357e2fdfcad1b28
bigint <bigint@lenster.xyz>
2022-05-17 19:07:50 +0530

    chore: update beta warning

--- src/components/Home/Announcement.tsx
+++ src/components/Home/Announcement.tsx
@@ -12,8 +12,8 @@ const Announcement: FC = () => {
           <p>Beta warning!</p>
         </div>
         <p className="text-sm leading-[22px]">
-          Lenster is still in the beta phase and the Interface you see may tend
-          to change.
+          Lenster is still in the beta phase, things may break, please handle us
+          with care.
         </p>
         {!IS_MAINNET && (
           <div className="flex items-center space-x-1.5 text-sm font-bold">
