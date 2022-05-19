3be9573701fceb3479c02c8ad5b223ec3b322d3e
bigint <bigint@lenster.xyz>
2022-05-19 08:35:00 +0530

    chore: update order

--- data/verified.ts
+++ data/verified.ts
@@ -3,8 +3,6 @@ export const mainnetVerified = [
   '0x0210' /** @paris.lens */,
   '0x0160' /** @ethglobal.lens */,
   '0x8e' /** @christina.lens */,
-  '0x0d' /** @yoginth.lens */,
-  '0x0c' /** @lenster.lens */,
   '0x0b' /** @zer0dot.lens */,
   '0x10' /** @damarnez.lens */,
   '0x09' /** @nicolo.lens */,
@@ -14,6 +12,8 @@ export const mainnetVerified = [
   '0x04' /** @letsraave.lens */,
   '0x03' /** @aavegrants.lens */,
   '0x02' /** @aaveaave.lens */,
+  '0x0c' /** @lenster.lens */,
+  '0x0d' /** @yoginth.lens */,
   '0x01' /** @lensprotocol */
 ]
 
