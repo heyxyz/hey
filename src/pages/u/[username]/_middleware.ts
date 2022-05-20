d1e2d3818526320a077ba5be4031e3640b3adfaa
bigint <bigint@lenster.xyz>
2022-05-20 22:21:57 +0530

    feat: add profile seo

--- src/pages/u/[username]/_middleware.ts
+++ src/pages/u/[username]/_middleware.ts
@@ -1,5 +1,6 @@
 import { Profile } from '@generated/types'
 import generateMeta from '@lib/generateMeta'
+import getIPFSLink from '@lib/getIPFSLink'
 import { NextRequest } from 'next/server'
 import parser from 'ua-parser-js'
 
@@ -14,12 +15,24 @@ export async function middleware(req: NextRequest) {
     const data = await result.json()
     const profile: Profile = data?.profile
 
-    const title = `${profile?.name} (@${profile?.handle}) • Lenster`
+    if (data?.success) {
+      const title = profile?.name
+        ? `${profile?.name} (@${profile?.handle}) • Lenster`
+        : `@${profile?.handle} • Lenster`
+      const description = profile?.bio
+      const image =
+        profile &&
+        `https://ik.imagekit.io/lensterimg/tr:n-avatar/${getIPFSLink(
+          // @ts-ignore
+          profile?.picture?.original?.url ??
+            // @ts-ignore
+            profile?.picture?.uri ??
+            `https://avatar.tobi.sh/${profile?.ownedBy}_${profile?.handle}.png`
+        )}`
 
-    console.log(data)
-
-    return new Response(generateMeta(title), {
-      headers: { 'Content-Type': 'text/html' }
-    })
+      return new Response(generateMeta(title, description, image), {
+        headers: { 'Content-Type': 'text/html' }
+      })
+    }
   }
 }
