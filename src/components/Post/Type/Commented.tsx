96cfc896dbdeba78078647eb06da55573c36fb59
bigint <bigint@lenster.xyz>
2022-05-29 00:31:49 +0530

    feat: add timestamp

--- src/components/Post/Type/Commented.tsx
+++ src/components/Post/Type/Commented.tsx
@@ -1,34 +1,37 @@
 import Markup from '@components/Shared/Markup'
 import UserProfile from '@components/Shared/UserProfile'
 import { LensterPost } from '@generated/lenstertypes'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
 import Link from 'next/link'
 import React, { FC } from 'react'
+
+dayjs.extend(relativeTime)
 
 interface Props {
   post: LensterPost
 }
 
 const PostBody: FC<Props> = ({ post }) => (
-  <div>
-    <Link href={`/u/${post?.profile?.handle}`}>
-      <div className="h-10 w-10">
-        <UserProfile profile={post?.profile} />
+  <div className="flex justify-between">
+    <div>
+      <UserProfile profile={post?.profile} />
+      <div className="flex">
+        <div className="ml-5 mr-8 border-[0.8px] bg-gray-300 border-gray-300 -my-[4px]" />
+        {post?.metadata?.content?.trim() ? (
+          <div className="linkify pt-5 pb-6">
+            <Markup>{post?.metadata?.content}</Markup>
+          </div>
+        ) : (
+          post?.metadata?.name
+        )}
       </div>
-    </Link>
-    <div className="flex">
-      <div className="ml-5 mr-8 border-[0.8px] bg-gray-300 border-gray-300 -mb-[4px] mt-[4px]" />
-      <Link href={`/posts/${post?.id ?? post?.pubId}`}>
-        <a href={`/posts/${post?.id ?? post?.pubId}`}>
-          {post?.metadata?.content?.trim() ? (
-            <div className="linkify pt-5 pb-6">
-              <Markup>{post?.metadata?.content}</Markup>
-            </div>
-          ) : (
-            post?.metadata?.name
-          )}
-        </a>
-      </Link>
     </div>
+    <Link href={`/posts/${post?.id}`}>
+      <a href={`/posts/${post?.id}`} className="text-sm text-gray-500">
+        {dayjs(new Date(post?.createdAt)).fromNow()}
+      </a>
+    </Link>
   </div>
 )
 
