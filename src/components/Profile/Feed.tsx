4956041a1fd5f55ae37f6062433675f11f837efb
bigint <bigint@lenster.xyz>
2022-05-29 01:21:39 +0530

    chore: fix spacing

--- src/components/Profile/Feed.tsx
+++ src/components/Profile/Feed.tsx
@@ -107,7 +107,7 @@ const Feed: FC<Props> = ({ profile, type }) => {
         <>
           <Card className="divide-y-[1px]">
             {publications?.map((post: LensterPost, index: number) => (
-              <div key={`${post?.id}_${index}`} className="px-5 pt-5">
+              <div key={`${post?.id}_${index}`} className="p-5">
                 <SinglePost key={`${post?.id}_${index}`} post={post} />
               </div>
             ))}
