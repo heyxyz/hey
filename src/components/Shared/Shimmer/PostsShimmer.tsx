4d5a91d0c236f3a3ca7540a7679089b57b55a3e2
yoginth.eth <yoginth@hey.com>
2022-05-30 22:55:23 +0530

    fix: divider in dark mode

--- src/components/Shared/Shimmer/PostsShimmer.tsx
+++ src/components/Shared/Shimmer/PostsShimmer.tsx
@@ -5,7 +5,7 @@ import PostShimmer from './PostShimmer'
 
 const PostsShimmer: FC = () => {
   return (
-    <Card className="divide-y-[1px]">
+    <Card className="divide-y-[1px] dark:divide-gray-700/80">
       <PostShimmer />
       <PostShimmer />
       <PostShimmer />
