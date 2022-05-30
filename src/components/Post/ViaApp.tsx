f5484331b9aff7418afe3b1f970e74d43870eed6
bigint <bigint@lenster.xyz>
2022-05-30 14:04:53 +0530

    chore: update phaver

--- src/components/Post/ViaApp.tsx
+++ src/components/Post/ViaApp.tsx
@@ -16,7 +16,7 @@ const ViaApp: FC<Props> = ({ appConfig }) => {
     <div className="flex items-center py-3 px-5 space-x-2 text-gray-500 border-t dark:border-t-gray-700/80">
       <img
         src={`${STATIC_ASSETS}/apps/${appConfig.logo}`}
-        className="w-5 h-5"
+        className="w-5 h-5 rounded-full"
         height={20}
         width={20}
         alt={appConfig.name}
