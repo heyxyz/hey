cccdd13aeef1183e0e73137f155853765d05c391
bigint <bigint@lenster.xyz>
2022-06-03 11:23:00 +0530

    feat: add cross post component

---
+++ src/components/Shared/Beta.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+const Beta: FC = () => {
+  return (
+    <div className="bg-brand-500 text-white px-1.5 rounded-md shadow-sm border border-brand-600 text-xs">
+      Beta
+    </div>
+  )
+}
+
+export default Beta
