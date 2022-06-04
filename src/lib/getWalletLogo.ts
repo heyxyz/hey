405b58f9960265bb540750daa8ec84916fe82817
yoginth.eth <yoginth@hey.com>
2022-06-05 01:48:48 +0530

    chore: remove coinbase connector

--- src/lib/getWalletLogo.ts
+++ src/lib/getWalletLogo.ts
@@ -4,8 +4,6 @@ const getWalletLogo = (name: string): string => {
   switch (name) {
     case 'WalletConnect':
       return `${STATIC_ASSETS}/wallets/walletconnect.svg`
-    case 'Coinbase Wallet':
-      return `${STATIC_ASSETS}/wallets/coinbase.svg`
     default:
       return `${STATIC_ASSETS}/wallets/browser-wallet.svg`
   }
