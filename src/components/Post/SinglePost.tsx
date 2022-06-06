b04a47ea9f838eede5d1d2842cf2e90467bb9f41
bigint <bigint@lenster.xyz>
2022-06-06 22:47:16 +0530

    fix: pubid undefined

--- src/components/Post/SinglePost.tsx
+++ src/components/Post/SinglePost.tsx
@@ -38,8 +38,11 @@ const SinglePost: FC<Props> = ({
                 : post?.profile
             }
           />
-          <Link href={`/posts/${post?.id}`} prefetch={false}>
-            <a href={`/posts/${post?.id}`} className="text-sm text-gray-500">
+          <Link href={`/posts/${post?.id ?? post?.pubId}`} prefetch={false}>
+            <a
+              href={`/posts/${post?.id ?? post?.pubId}`}
+              className="text-sm text-gray-500"
+            >
               {dayjs(new Date(post?.createdAt)).fromNow()}
             </a>
           </Link>
