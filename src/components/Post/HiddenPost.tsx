5da35bb174dfa74dcc7760012fa8e58cb843c725
bigint <bigint@lenster.xyz>
2022-06-08 19:46:27 +0530

    fix: hidden post in darkmode and closes #400

--- src/components/Post/HiddenPost.tsx
+++ src/components/Post/HiddenPost.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const HiddenPost: FC<Props> = ({ type = 'Publication' }) => {
   return (
-    <Card className="!bg-gray-100">
+    <Card className="!bg-gray-100 dark:!bg-gray-800">
       <div className="px-4 py-3 italic text-sm">
         {type} was hidden by the author
       </div>
