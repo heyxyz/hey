3d9649693b24ddefc8812a061b4838a0a49254f0
yoginth.eth <yoginth@hey.com>
2022-06-09 09:35:23 +0530

    feat: add embed comp

--- src/components/Shared/IFramely/index.tsx
+++ src/components/Shared/IFramely/index.tsx
@@ -1,7 +1,7 @@
-import { Card } from '@components/UI/Card'
 import imagekitURL from '@lib/imagekitURL'
-import trackEvent from '@lib/trackEvent'
 import React, { FC, useEffect, useState } from 'react'
+
+import Embed from './Embed'
 
 interface Props {
   url: string
@@ -59,63 +59,7 @@ const IFramely: FC<Props> = ({ url }) => {
 
     if (!og.title) return null
 
-    return (
-      <div className="mt-4 text-sm sm:w-4/6">
-        <a
-          href={url}
-          target="_blank"
-          rel="noreferrer noopener"
-          onClick={() => trackEvent('oembed')}
-        >
-          <Card forceRounded>
-            {!og.isSquare && og.thumbnail && (
-              <img
-                className="w-full rounded-t-xl"
-                src={og.thumbnail}
-                alt="Thumbnail"
-              />
-            )}
-            <div className="flex items-center">
-              {og.isSquare && og.thumbnail && (
-                <img
-                  className="w-36 h-36 rounded-l-xl"
-                  height={144}
-                  width={144}
-                  src={og.thumbnail}
-                  alt="Thumbnail"
-                />
-              )}
-              <div className="p-5 truncate">
-                <div className="space-y-1.5">
-                  {og.title && (
-                    <div className="font-bold line-clamp-1">{og.title}</div>
-                  )}
-                  {og.description && (
-                    <div className="text-gray-500 line-clamp-2">
-                      {og.description}
-                    </div>
-                  )}
-                  {og.site && (
-                    <div className="flex items-center pt-1.5 space-x-1">
-                      {og.favicon && (
-                        <img
-                          className="w-4 h-4 rounded-full"
-                          height={16}
-                          width={16}
-                          src={og.favicon}
-                          alt="Favicon"
-                        />
-                      )}
-                      <div className="text-xs text-gray-500">{og.site}</div>
-                    </div>
-                  )}
-                </div>
-              </div>
-            </div>
-          </Card>
-        </a>
-      </div>
-    )
+    return <Embed og={og} />
   }
 }
 
