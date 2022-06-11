eb753b7a718b090b95b2a0df907806c90f335157
bigint <bigint@lenster.xyz>
2022-06-11 18:56:43 +0530

    fix: build warning

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,8 @@ export const IS_DEVELOPMENT = process.env.NODE_ENV === 'development'
 export const IS_MAINNET = process.env.NEXT_PUBLIC_IS_MAINNET === 'true'
 
 export const APP_NAME = 'Lenster'
-export const DESCRIPTION = `${APP_NAME} is a composable, decentralized, and permissionless social media web app built with Lens Protocol 🌿`
+export const DESCRIPTION =
+  'Lenster is a composable, decentralized, and permissionless social media web app built with Lens Protocol 🌿'
 export const DEFAULT_OG = 'https://assets.lenster.xyz/images/og/logo.jpeg'
 
 // Git
