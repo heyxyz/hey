a7fa4dd4758c89ab9fc51a16e39f8f7488d8b7ea
bigint <bigint@lenster.xyz>
2022-06-11 19:09:37 +0530

    fix: build warning

--- src/lib/generateMeta.ts
+++ src/lib/generateMeta.ts
@@ -1,5 +1,3 @@
-import { APP_NAME } from 'src/constants'
-
 const generateMeta = (
   title: string,
   description: string,
@@ -12,7 +10,7 @@ const generateMeta = (
       <meta name="description" content="${description}" />
     
       <meta property="og:url" content="https://lenster.xyz" />
-      <meta property="og:site_name" content="${APP_NAME}" />
+      <meta property="og:site_name" content="Lenster" />
       <meta property="og:title" content="${title}" />
       <meta property="og:description" content="${description}" />
       <meta property="og:image" content="${image}" />
@@ -20,7 +18,7 @@ const generateMeta = (
       <meta property="og:image:height" content="400" />
     
       <meta property="twitter:card" content="summary" />
-      <meta property="twitter:site" content="${APP_NAME}" />
+      <meta property="twitter:site" content="Lenster" />
       <meta property="twitter:title" content="${title}" />
       <meta property="twitter:description" content="${description}" />
       <meta property="twitter:image:src" content="${image}" />
