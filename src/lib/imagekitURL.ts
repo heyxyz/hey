9f483d74a524f9ccb659b5097852a39ff9f8ba2a
yoginth.eth <yoginth@hey.com>
2022-06-12 08:43:40 +0530

    chore: add fallback image

--- src/lib/imagekitURL.ts
+++ src/lib/imagekitURL.ts
@@ -1,7 +1,9 @@
 import { IMAGEKIT_URL } from 'src/constants'
 
 const imagekitURL = (url: string, name?: string): string => {
-  return name ? `${IMAGEKIT_URL}/tr:n-${name}/${url}` : `${IMAGEKIT_URL}/${url}`
+  return name
+    ? `${IMAGEKIT_URL}/tr:n-${name},tr:di-placeholder.webp/${url}`
+    : `${IMAGEKIT_URL}/tr:di-placeholder.webp/${url}`
 }
 
 export default imagekitURL
