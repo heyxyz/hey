0934826fa8b1221206a5224c0121e5bd753b311a
yoginth.eth <yoginth@hey.com>
2022-06-15 10:19:38 +0530

    fix: margin in communities list

--- src/components/Community/Communities/index.tsx
+++ src/components/Community/Communities/index.tsx
@@ -81,7 +81,7 @@ const Communities: NextPage = () => {
     <GridLayout>
       <SEO title={`Communities • ${APP_NAME}`} />
       <GridItemFour>
-        <div className="flex items-center mb- space-x-1.5 font-bold text-gray-500">
+        <div className="flex items-center mb-2 space-x-1.5 font-bold text-gray-500">
           <FireIcon className="w-5 h-5 text-yellow-500" />
           <div>Most Active</div>
         </div>
