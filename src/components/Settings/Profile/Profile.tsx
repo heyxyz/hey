8bb8e77fbfed93bd170d7854198bcc0bb82b3e56
yoginth.eth <yoginth@hey.com>
2022-06-16 09:11:11 +0530

    feat: add previousMetadata

--- src/components/Settings/Profile/Profile.tsx
+++ src/components/Settings/Profile/Profile.tsx
@@ -281,6 +281,7 @@ const Profile: FC<Props> = ({ profile }) => {
         ],
         version: '1.0.0',
         metadata_id: generateSnowflake(),
+        previousMetadata: profile?.metadata,
         createdOn: new Date(),
         appId: APP_NAME
       }).finally(() => setIsUploading(false))
