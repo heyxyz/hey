b6981d2855b4bdfde9ba20b95ffdd2891100367b
bigint <bigint@lenster.xyz>
2022-06-16 16:27:38 +0530

    feat: add slug to mentions

--- src/components/utils/matchers/MentionMatcher.tsx
+++ src/components/utils/matchers/MentionMatcher.tsx
@@ -1,3 +1,4 @@
+import Slug from '@components/Shared/Slug'
 import { Matcher } from 'interweave'
 import Link from 'next/link'
 import React from 'react'
@@ -5,7 +6,9 @@ import React from 'react'
 export function Mention({ ...props }: any) {
   return (
     <Link href={`/u/${props.display.slice(1)}`} prefetch={false}>
-      {props.display}
+      <a href={`/u/${props.display.slice(1)}`}>
+        <Slug className="text-md" slug={props.display} />
+      </a>
     </Link>
   )
 }
