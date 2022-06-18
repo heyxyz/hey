dbca8d5133193009b5a3ea09d88962f48b73e9bb
bigint <bigint@lenster.xyz>
2022-06-18 17:13:30 +0530

    chore: fix emojis in meta

--- src/lib/generateMeta.ts
+++ src/lib/generateMeta.ts
@@ -9,7 +9,6 @@ const generateMeta = (
       <title>${title}</title>
       <meta name="description" content="${description}" />
       <meta charset="UTF-8">
-    
       <meta property="og:url" content="https://lenster.xyz" />
       <meta property="og:site_name" content="Lenster" />
       <meta property="og:title" content="${title}" />
@@ -17,7 +16,6 @@ const generateMeta = (
       <meta property="og:image" content="${image}" />
       <meta property="og:image:width" content="400" />
       <meta property="og:image:height" content="400" />
-    
       <meta property="twitter:card" content="summary" />
       <meta property="twitter:site" content="Lenster" />
       <meta property="twitter:title" content="${title}" />
