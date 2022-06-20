cc9d2bacf167251b1abb229d99c8003b6ba83473
bigint <bigint@lenster.xyz>
2022-06-20 11:57:01 +0530

    chore: update machineID

--- src/lib/generateSnowflake.ts
+++ src/lib/generateSnowflake.ts
@@ -1,8 +1,7 @@
 import { generateSunflake } from 'sunflake'
 
 const generateSnowflake = generateSunflake({
-  machineId: Math.floor(Math.random() * 1e10),
-  epoch: Math.floor(Date.now() / 1000)
+  machineId: Math.floor(Date.now() / 1000)
 })
 
 export default generateSnowflake
