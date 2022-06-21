4bc5cb0d1aa9184c0bdb696cef4a3c3279638b84
bigint <bigint@lenster.xyz>
2022-06-21 09:33:31 +0530

    chore: remove doesfollow in profile query

--- src/components/Profile/Details.tsx
+++ src/components/Profile/Details.tsx
@@ -1,4 +1,3 @@
-import { gql, useQuery } from '@apollo/client'
 import Follow from '@components/Shared/Follow'
 import Markup from '@components/Shared/Markup'
 import Slug from '@components/Shared/Slug'
@@ -14,7 +13,6 @@ import {
   LocationMarkerIcon
 } from '@heroicons/react/outline'
 import { BadgeCheckIcon, ShieldCheckIcon } from '@heroicons/react/solid'
-import consoleLog from '@lib/consoleLog'
 import formatAddress from '@lib/formatAddress'
 import getAttribute from '@lib/getAttribute'
 import getAvatar from '@lib/getAvatar'
@@ -26,17 +24,8 @@ import React, { FC, ReactElement, useEffect, useState } from 'react'
 import { STATIC_ASSETS } from 'src/constants'
 import useAppStore from 'src/store'
 
-import DoesFollow from './DoesFollow'
 import Followerings from './Followerings'
 import ProfileMod from './Mod'
-
-export const DOES_FOLLOW_QUERY = gql`
-  query DoesFollow($request: DoesFollowRequest!) {
-    doesFollow(request: $request) {
-      follows
-    }
-  }
-`
 
 interface Props {
   profile: Profile
@@ -44,7 +33,7 @@ interface Props {
 
 const Details: FC<Props> = ({ profile }) => {
   const [followersCount, setFollowersCount] = useState<number>(0)
-  const [following, setFollowing] = useState<boolean>(false)
+  const [following, setFollowing] = useState<boolean>(profile?.isFollowedByMe)
   const { currentUser, staffMode } = useAppStore()
   const { resolvedTheme } = useTheme()
   const { data: ensName } = useENS(profile?.ownedBy ?? '')
@@ -54,37 +43,6 @@ const Details: FC<Props> = ({ profile }) => {
       setFollowersCount(profile?.stats?.totalFollowers)
     }
   }, [profile?.stats?.totalFollowers])
-
-  const { data: followData, loading: followLoading } = useQuery(
-    DOES_FOLLOW_QUERY,
-    {
-      variables: {
-        request: {
-          followInfos: [
-            {
-              // Am I following them
-              followerAddress: profile?.ownedBy,
-              profileId: currentUser?.id
-            },
-            {
-              // Do they follow me
-              followerAddress: currentUser?.ownedBy,
-              profileId: profile?.id
-            }
-          ]
-        }
-      },
-      skip: !profile || !currentUser,
-      onCompleted(data) {
-        setFollowing(data?.doesFollow[1]?.follows)
-        consoleLog(
-          'Query',
-          '#8b5cf6',
-          `Fetched has followed check Profile:${profile?.id} Following:${following}`
-        )
-      }
-    }
-  )
 
   const MetaDetails = ({
     children,
@@ -139,17 +97,19 @@ const Details: FC<Props> = ({ profile }) => {
               slug={formatAddress(profile?.ownedBy)}
             />
           )}
-          {currentUser && currentUser?.id !== profile?.id && (
-            <DoesFollow followData={followData?.doesFollow[0]} />
-          )}
+          {currentUser &&
+            currentUser?.id !== profile?.id &&
+            profile?.isFollowing && (
+              <div className="py-0.5 px-2 text-xs bg-gray-200 rounded-full dark:bg-gray-700">
+                Follows you
+              </div>
+            )}
         </div>
       </div>
       <div className="space-y-5">
         <Followerings followersCount={followersCount} profile={profile} />
         <div className="flex items-center space-x-2">
-          {followLoading ? (
-            <div className="w-28 rounded-lg h-[34px] shimmer" />
-          ) : followType !== 'RevertFollowModuleSettings' ? (
+          {followType !== 'RevertFollowModuleSettings' ? (
             following ? (
               <div className="flex space-x-2">
                 <Unfollow
