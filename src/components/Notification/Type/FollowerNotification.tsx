890e3fd581744c21e5ec53516b29605e7fe7f052
bigint <bigint@lenster.xyz>
2022-06-25 20:34:20 +0530

    chore: set width

--- src/components/Notification/Type/FollowerNotification.tsx
+++ src/components/Notification/Type/FollowerNotification.tsx
@@ -25,7 +25,7 @@ const FollowerNotification: FC<Props> = ({ notification }) => {
 
   return (
     <div className="flex justify-between items-start">
-      <div className="space-y-2">
+      <div className="space-y-2 w-4/5">
         <div className="flex items-center space-x-2">
           {isSuperFollow ? (
             <UserAddIcon className="h-6 w-6 text-pink-500" />
