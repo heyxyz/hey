5091474ab3817b567b612260359b0db17dca8a35
yoginth.eth <yoginth@hey.com>
2022-06-25 19:51:37 +0530

    chore: add card

--- src/components/Notification/index.tsx
+++ src/components/Notification/index.tsx
@@ -1,9 +1,20 @@
+import { LightningBoltIcon } from '@heroicons/react/outline'
 import { FC } from 'react'
 
 import List from './List'
 
 const Notification: FC = () => {
-  return <List />
+  return (
+    <div className="flex flex-grow justify-center px-0 sm:px-6 lg:px-8 py-8">
+      <div className="max-w-5xl w-full space-y-3">
+        <div className="flex items-center space-x-2">
+          <LightningBoltIcon className="h-5 w-5" />
+          <div className="font-bold">Notification</div>
+        </div>
+        <List />
+      </div>
+    </div>
+  )
 }
 
 export default Notification
