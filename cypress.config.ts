51c3ccf75f15814ce8efb97efba7c81b3b23ebf9
Yogi <yoginth@hey.com>
2022-06-26 11:45:11 +0000

    chore: update page size

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   video: false,
   screenshotOnRunFailure: false,
+  viewportWidth: 1920,
+  viewportHeight: 1080,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
