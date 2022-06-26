8425691d23af753f03e17d066e66a65a40819dd0
bigint <bigint@lenster.xyz>
2022-06-26 11:17:00 +0530

    chore: add basic tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  video: false,
+  screenshotOnRunFailure: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
