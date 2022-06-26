d619d342690b4304cb00c6ef6bfa3c1868ea6e91
bigint <bigint@lenster.xyz>
2022-06-26 17:45:50 +0530

    chore: fix configs

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  projectId: 'v8tv5e',
   video: false,
   screenshotOnRunFailure: false,
   viewportWidth: 1920,
   viewportHeight: 1080,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      config.env = process.env
     }
   }
 })
