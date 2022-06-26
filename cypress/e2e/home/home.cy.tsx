b2db051d9e63d5f5daae3f3a4920a3d4f64000a8
yoginth.eth <yoginth@hey.com>
2022-06-26 11:45:19 +0530

    chore: add basic tests

--- cypress/e2e/home/home.cy.tsx
+++ cypress/e2e/home/home.cy.tsx
@@ -1,28 +1,19 @@
-import { APP_NAME } from 'src/constants'
-
 context('Home Page', () => {
   beforeEach(() => {
     cy.visit('http://localhost:4783')
   })
 
-  it('should render the home page and display welcome message', () => {
-    cy.get('[data-test=app-name]').contains(`Welcome to ${APP_NAME} 👋`)
-    cy.get('[data-test=app-description]').contains(
-      `${APP_NAME} is a decentralized, and permissionless social media app built with Lens Protocol 🌿`
-    )
+  it('should render the home page', () => {
+    cy.get('[data-test=app-name]')
+    cy.get('[data-test=app-description]')
   })
 
-  it('should render explore list, recommended users', () => {
+  it('should render required components', () => {
     cy.get('[data-test=explore-feed]')
-    cy.get('div').contains('Recommended users')
-    cy.get('[data-test=recommended-users]>div')
-      .children()
-      .should('have.length', 5)
-
-    cy.get('[data-test=recommended-users]>div')
-      .children()
-      .closest('div')
-      .get('button')
+    cy.get('[data-test=recommended-users]')
+    cy.get('[data-test=beta-warning]')
+    cy.get('[data-test=footer]')
   })
 })
+
 export {}
