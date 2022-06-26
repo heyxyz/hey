c9ef4427e626e0c21e33c53d9a3712959c6b7f3d
bigint <bigint@lenster.xyz>
2022-06-26 11:56:48 +0530

    chore: refactor test

--- cypress/e2e/home/profile.cy.tsx
+++ cypress/e2e/home/profile.cy.tsx
@@ -1,20 +1,35 @@
 context('Home Page', () => {
-  beforeEach(() => {
+  before(() => {
     cy.visit('http://localhost:4783/u/lensprotocol')
   })
 
-  it('should render required components', () => {
+  it('should render profile cover', () => {
     cy.get('[data-test=profile-cover]')
+  })
+
+  it('should render profile avatar', () => {
     cy.get('[data-test=profile-avatar]')
+  })
+
+  it('should render profile name', () => {
     cy.get('[data-test=profile-name]')
+  })
+
+  it('should render profile slug', () => {
     cy.get('[data-test=profile-slug]')
+  })
+
+  it('should render profile follow', () => {
     cy.get('[data-test=profile-follow]')
+  })
+
+  it('should render profile followering', () => {
     cy.get('[data-test=profile-following]')
     cy.get('[data-test=profile-followers]')
+  })
+
+  it('should render profile meta', () => {
     cy.get('[data-test=profile-meta]')
-    cy.get('[data-test=profile-follow]')
-    cy.get('[data-test=profile-follow]')
-    cy.get('[data-test=profile-follow]')
   })
 })
 
