f624dc56814ec3ac1a35ee0c428e6f61e347005e
yoginth.eth <yoginth@hey.com>
2022-06-26 11:13:22 +0530

    chore: add basic tests

--- cypress/e2e/home/home.cy.tsx
+++ cypress/e2e/home/home.cy.tsx
@@ -1,23 +1,25 @@
+import { APP_NAME } from 'src/constants'
+
 context('Home Page', () => {
   beforeEach(() => {
     cy.visit('http://localhost:4783')
   })
 
   it('should render the home page and display welcome message', () => {
-    cy.get('h1').contains('Welcome to Lenster 👋')
-    cy.get('[data-cy=product-description]').contains(
-      'Lenster is a composable, decentralized, and permissionless social media web app built with Lens Protocol 🌿'
+    cy.get('[data-test=app-name]').contains(`Welcome to ${APP_NAME} 👋`)
+    cy.get('[data-test=app-description]').contains(
+      `${APP_NAME} is a decentralized, and permissionless social media app built with Lens Protocol 🌿`
     )
   })
 
   it('should render explore list, recommended users', () => {
-    cy.get('[data-cy=explore-feed]')
+    cy.get('[data-test=explore-feed]')
     cy.get('div').contains('Recommended users')
-    cy.get('[data-cy=user-recommendations]>div')
+    cy.get('[data-test=recommended-users]>div')
       .children()
       .should('have.length', 5)
 
-    cy.get('[data-cy=user-recommendations]>div')
+    cy.get('[data-test=recommended-users]>div')
       .children()
       .closest('div')
       .get('button')
