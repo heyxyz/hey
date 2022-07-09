fc2ce4513fc627b9425bf8b1507eec3161740300
yoginth.eth <yoginth@hey.com>
2022-07-09 18:23:13 +0530

    chore: remove pwa

--- next.config.js
+++ next.config.js
@@ -1,21 +1,15 @@
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
 const { withSentryConfig } = require('@sentry/nextjs')
 const withTM = require('next-transpile-modules')(['plyr-react'])
-
-const moduleExports = withPWA({
-  pwa: {
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-    runtimeCaching
-  },
-  reactStrictMode: process.env.NODE_ENV === 'production'
-})
 
 const sentryWebpackPluginOptions = {
   silent: true
 }
 
 module.exports = withTM(
-  withSentryConfig(moduleExports, sentryWebpackPluginOptions)
+  withSentryConfig(
+    {
+      reactStrictMode: process.env.NODE_ENV === 'production'
+    },
+    sentryWebpackPluginOptions
+  )
 )
