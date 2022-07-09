71914b562308c2cc8940b4c8df8ee256d5b77886
yoginth.eth <yoginth@hey.com>
2022-07-09 22:01:22 +0530

    chore: add cache header

--- next.config.js
+++ next.config.js
@@ -5,10 +5,19 @@ const sentryWebpackPluginOptions = {
   silent: true
 }
 
+const headers = [{ key: 'Cache-Control', value: 'public, max-age=3600' }]
+
 module.exports = withTM(
   withSentryConfig(
     {
-      reactStrictMode: process.env.NODE_ENV === 'production'
+      reactStrictMode: process.env.NODE_ENV === 'production',
+      async redirects() {
+        return [
+          { source: '/about', headers },
+          { source: '/privacy', headers },
+          { source: '/thanks', headers }
+        ]
+      }
     },
     sentryWebpackPluginOptions
   )
