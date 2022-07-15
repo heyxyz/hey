c2d6107d916653de4f988e9977ce3af16af90066
bigint <bigint@lenster.xyz>
2022-07-15 12:10:51 +0530

    chore: add reply from modal

--- src/components/Post/NewPost/Modal/index.tsx
+++ src/components/Post/NewPost/Modal/index.tsx
@@ -1,11 +1,15 @@
+import SinglePost from '@components/Post/SinglePost'
+import { Card } from '@components/UI/Card'
 import { Modal } from '@components/UI/Modal'
 import { PencilAltIcon } from '@heroicons/react/outline'
-import { FC, useState } from 'react'
+import { FC } from 'react'
+import { usePublicationStore } from 'src/store/publication'
 
 import NewPost from '..'
 
 const NewPostModal: FC = () => {
-  const [showModal, setShowModal] = useState<boolean>(false)
+  const { showNewPostModal, setShowNewPostModal, parentPub, setParentPub } =
+    usePublicationStore()
 
   return (
     <>
@@ -13,7 +17,8 @@ const NewPostModal: FC = () => {
         type="button"
         className="flex items-start"
         onClick={() => {
-          setShowModal(!showModal)
+          setParentPub(null)
+          setShowNewPostModal(!showNewPostModal)
         }}
       >
         <PencilAltIcon className="w-5 h-5 sm:w-6 sm:h-6" />
@@ -22,10 +27,15 @@ const NewPostModal: FC = () => {
         title="New Post"
         icon={<PencilAltIcon className="w-5 h-5 text-brand" />}
         size="md"
-        show={showModal}
-        onClose={() => setShowModal(false)}
+        show={showNewPostModal}
+        onClose={() => setShowNewPostModal(false)}
       >
-        <NewPost setShowModal={setShowModal} hideCard />
+        {parentPub ? (
+          <Card className="mx-5 mt-5">
+            <SinglePost post={parentPub} showType={false} showActions={false} />
+          </Card>
+        ) : null}
+        <NewPost setShowModal={setShowNewPostModal} hideCard />
       </Modal>
     </>
   )
