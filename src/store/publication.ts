998afe79fe459b755932c110fc7e693433185905
yoginth.eth <yoginth@hey.com>
2022-07-15 12:01:49 +0530

    chore: add store

--- src/store/publication.ts
+++ src/store/publication.ts
@@ -1,6 +1,21 @@
 /* eslint-disable no-unused-vars */
+import { LensterPost } from '@generated/lenstertypes'
 import create from 'zustand'
 import { persist } from 'zustand/middleware'
+
+interface PublicationState {
+  showNewPostModal: boolean
+  setShowNewPostModal: (showNewPostModal: boolean) => void
+  parentPub: LensterPost | null
+  setParentPub: (parentPub: LensterPost | null) => void
+}
+
+export const usePublicationStore = create<PublicationState>((set) => ({
+  showNewPostModal: false,
+  setShowNewPostModal: (showNewPostModal) => set(() => ({ showNewPostModal })),
+  parentPub: null,
+  setParentPub: (parentPub) => set(() => ({ parentPub }))
+}))
 
 interface PublicationPersistState {
   persistedPublication: string
