5b6a660a8023c02adaa09b00ff9efa2fb29b9cc9
bigint <bigint@lenster.xyz>
2022-07-15 12:28:45 +0530

    chore: remove persisted state

--- src/store/publication.ts
+++ src/store/publication.ts
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { LensterPost } from '@generated/lenstertypes'
 import create from 'zustand'
-import { persist } from 'zustand/middleware'
 
 interface PublicationState {
   showNewPostModal: boolean
@@ -16,19 +15,3 @@ export const usePublicationStore = create<PublicationState>((set) => ({
   parentPub: null,
   setParentPub: (parentPub) => set(() => ({ parentPub }))
 }))
-
-interface PublicationPersistState {
-  persistedPublication: string
-  setPersistedPublication: (persistedPublication: string) => void
-}
-
-export const usePublicationPersistStore = create(
-  persist<PublicationPersistState>(
-    (set) => ({
-      persistedPublication: '',
-      setPersistedPublication: (persistedPublication) =>
-        set(() => ({ persistedPublication }))
-    }),
-    { name: 'lenster.store' }
-  )
-)
