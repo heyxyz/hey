450335c80799ce3dab39d443c5dac3fc11f81dfd
yoginth.eth <yoginth@hey.com>
2022-07-20 13:10:10 +0530

    refactor: hashflags

--- data/hashflags.ts
+++ data/hashflags.ts
@@ -1,1 +1,10 @@
-export const hashflags = ['lenster', 'bitcoin', 'ethereum', 'lens']
+export const hashflags: any = {
+  lenster: 'lenster',
+  bitcoin: 'bitcoin',
+  ethereum: 'ethereum',
+  lens: 'lens',
+  bts: 'bts',
+  btsarmy: 'btsarmy',
+  blm: 'blm',
+  bhm: 'blm'
+}
