44d567fd4ca3f441209bdbd71824672d54031733
--global <69431456+bigint@users.noreply.github.com>
2022-07-22 10:41:58 +0530

    feat: add tooltip

--- src/components/Profile/Badges/index.tsx
+++ src/components/Profile/Badges/index.tsx
@@ -1,7 +1,9 @@
-import { Popover } from '@components/UI/Popover'
 import { Profile } from '@generated/types'
 import React, { FC } from 'react'
-import { STATIC_ASSETS } from 'src/constants'
+
+import ENS from './ENS'
+import ProofOfHumanity from './ProofOfHumanity'
+import Sybil from './Sybil'
 
 interface Props {
   profile: Profile
@@ -19,35 +21,9 @@ const Badges: FC<Props> = ({ profile }) => {
     <>
       <div className="w-full divider" />
       <div className="flex flex-row gap-3">
-        {profile?.onChainIdentity?.proofOfHumanity && (
-          <img
-            className="drop-shadow-xl"
-            height={75}
-            width={75}
-            src={`${STATIC_ASSETS}/badges/poh.png`}
-            alt="POH Badge"
-          />
-        )}
-        {profile?.onChainIdentity?.ens?.name && (
-          <img
-            className="drop-shadow-xl"
-            height={75}
-            width={75}
-            src={`${STATIC_ASSETS}/badges/ens.png`}
-            alt="ENS Badge"
-          />
-        )}
-        {profile?.onChainIdentity?.sybilDotOrg?.verified && (
-          <Popover content="gm" placement="top">
-            <img
-              className="drop-shadow-xl"
-              height={75}
-              width={75}
-              src={`${STATIC_ASSETS}/badges/sybil.png`}
-              alt="Sybil Badge"
-            />
-          </Popover>
-        )}
+        <ProofOfHumanity profile={profile} />
+        <ENS profile={profile} />
+        <Sybil profile={profile} />
       </div>
     </>
   )
