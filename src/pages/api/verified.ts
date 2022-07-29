055170e4ce1cfbb98a15bc23625b47ae0a50f064
--global <69431456+bigint@users.noreply.github.com>
2022-07-29 11:03:38 +0530

    feat: remove query check

--- src/pages/api/verified.ts
+++ src/pages/api/verified.ts
@@ -1,17 +1,15 @@
 import { withSentry } from '@sentry/nextjs'
 import { mainnetVerified, testnetVerified } from 'data/verified'
 import { NextApiRequest, NextApiResponse } from 'next'
-import { ERROR_MESSAGE } from 'src/constants'
+import { ERROR_MESSAGE, IS_MAINNET } from 'src/constants'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    if (req.query?.network === 'testnet') {
-      return res
-        .status(200)
-        .json({ success: true, profile_ids: testnetVerified })
-    }
-
-    return res.status(200).json({ success: true, profile_ids: mainnetVerified })
+    return res.status(200).json({
+      success: true,
+      network: IS_MAINNET ? 'mainnet' : 'testnet',
+      profile_ids: IS_MAINNET ? mainnetVerified : testnetVerified
+    })
   } catch (e) {
     return res.status(500).json({ success: false, message: ERROR_MESSAGE })
   }
