a6e9f0f44f5ecec6d8a9d2b8935eb82015301396
--global <69431456+bigint@users.noreply.github.com>
2022-08-02 12:24:40 +0530

    chore: add USE_ASSETS_CDN

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withTM = require('next-transpile-modules')(['plyr-react'])
 const { withAxiom } = require('next-axiom')
 
 const headers = [{ key: 'Cache-Control', value: 'public, max-age=3600' }]
+const useCDN = process.env.USE_ASSETS_CDN === 'true'
 
 module.exports = withAxiom(
   withTM(
@@ -11,6 +12,7 @@ module.exports = withAxiom(
       {
         reactStrictMode: false,
         trailingSlash: false,
+        assetPrefix: useCDN ? 'https://cdn.yogi.codes' : '',
         async rewrites() {
           return [
             {
