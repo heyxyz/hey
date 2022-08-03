7d450fe9763628a337c437177a4cc5e924865430
--global <69431456+bigint@users.noreply.github.com>
2022-08-03 13:37:40 +0530

    chore: rename scripts folder to script

--- script/gp-welcome.sh
+++ script/gp-welcome.sh
@@ -13,6 +13,6 @@ curl -s "$URL/" > /dev/null
 echo -e "✅ ${GREEN}${BOLD}Lenster client warmed up${NORMAL}${NC} 🔥"
 echo -e "\n👋 ${GREEN}${BOLD}Welcome to Lenster${NORMAL}${NC}\n"
 echo -e "💻 ${BOLD}Commands:${NORMAL}\n"
-awk -F':' '{printf "%s%s", $2, (/^Description/)?"\n":" -"}' ./scripts/commands.txt
+awk -F':' '{printf "%s%s", $2, (/^Description/)?"\n":" -"}' ./script/commands.txt
 echo -e "\n\nVisit: ${BLUE}${URL}${NC}\n\n"
 echo -e "${PURPLE}${BOLD}Happy coding${NORMAL}${NC} 😍\n"
