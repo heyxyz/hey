546f482018af31a3eaa73fce684131363886c896
Bigint <69431456+bigint@users.noreply.github.com>
2022-08-04 17:51:40 +0530

    chore: remove console

--- src/lib/dogstats.ts
+++ src/lib/dogstats.ts
@@ -3,7 +3,6 @@ import { DOGSTATS_ENABLED } from 'src/constants'
 
 export const Dogstats = {
   track: (name: string, props?: { result?: string }) => {
-    console.log(name, props)
     if (DOGSTATS_ENABLED) datadogLogs.logger.info(name, props)
   }
 }
