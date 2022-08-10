9f383bb769fbe845fd93df5684b892fe2ada85c9
Bigint <69431456+bigint@users.noreply.github.com>
2022-08-10 10:08:38 +0530

    chore: use eu mixpanel host

--- src/constants.ts
+++ src/constants.ts
@@ -22,6 +22,7 @@ export const RELAY_ON =
     ? process.env.NEXT_PUBLIC_RELAY_ON === 'true'
     : false;
 export const MIXPANEL_TOKEN = process.env.NEXT_PUBLIC_MIXPANEL_TOKEN ?? '';
+export const MIXPANEL_API_HOST = 'https://api-eu.mixpanel.com';
 
 // Messages
 export const ERROR_MESSAGE = 'Something went wrong!';
