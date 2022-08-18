f7b2c57c075c2431edcfc989225032122dd98488
bigint <69431456+bigint@users.noreply.github.com>
2022-08-18 21:09:05 +0530

    feat: worldcoin badge 🧿 (#462)
    * feat: add worldcoin badge
    
    * fix: use flex-wrap

--- src/components/Profile/Badges/index.tsx
+++ src/components/Profile/Badges/index.tsx
@@ -4,6 +4,7 @@ import React, { FC } from 'react';
 import Ens from './Ens';
 import ProofOfHumanity from './ProofOfHumanity';
 import Sybil from './Sybil';
+import Worldcoin from './Worldcoin';
 
 interface Props {
   profile: Profile;
@@ -22,10 +23,11 @@ const Badges: FC<Props> = ({ profile }) => {
   return (
     <>
       <div className="w-full divider" />
-      <div className="flex flex-row gap-3">
+      <div className="flex flex-wrap gap-3">
         <ProofOfHumanity profile={profile} />
         <Ens profile={profile} />
         <Sybil profile={profile} />
+        <Worldcoin profile={profile} />
       </div>
     </>
   );
