add4e74eef1c5b13fc6b2f3f0a223703488fbd1c
bigint <69431456+bigint@users.noreply.github.com>
2022-08-18 10:02:44 +0530

    fix|refactor: arweave upload (#453)
    Co-authored-by: sasi.eth <me@sasi.codes>

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,4 +1,4 @@
-import WebBundlr from '@bundlr-network/client';
+import Bundlr from '@bundlr-network/client';
 import { withSentry } from '@sentry/nextjs';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { APP_NAME, BUNDLR_CURRENCY, BUNDLR_NODE_URL, ERROR_MESSAGE } from 'src/constants';
@@ -12,19 +12,19 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     return res.status(400).json({ success: false, message: 'Bad request!' });
   }
 
-  const data = JSON.stringify(req.body);
+  const payload = JSON.stringify(req.body);
 
   try {
-    const bundlr = new WebBundlr(BUNDLR_NODE_URL, BUNDLR_CURRENCY, process.env.BUNDLR_PRIVATE_KEY);
+    const bundlr = new Bundlr(BUNDLR_NODE_URL, BUNDLR_CURRENCY, process.env.BUNDLR_PRIVATE_KEY);
     const tags = [
       { name: 'Content-Type', value: 'application/json' },
       { name: 'App-Name', value: APP_NAME }
     ];
-    const tx = bundlr.createTransaction(data, { tags });
-    await tx.sign();
-    await tx.upload();
 
-    return res.status(200).json({ success: true, id: tx.id });
+    const uploader = bundlr.uploader.chunkedUploader;
+    const { data } = await uploader.uploadData(Buffer.from(payload), { tags });
+
+    return res.status(200).json({ success: true, id: data.id });
   } catch (e) {
     console.log(e);
     return res.status(500).json({ success: false, message: ERROR_MESSAGE });
