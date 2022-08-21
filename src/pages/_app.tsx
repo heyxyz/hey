cc7db68d69edd5936e29760fe91863add5c1eca5
Bigint <69431456+bigint@users.noreply.github.com>
2022-08-21 15:02:29 +0530

    chore: lazy load sitelayout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,22 @@
 import '../styles.css';
 
-import SiteLayout from '@components/SiteLayout';
+import Loading from '@components/Loading';
 import { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
+import { Suspense } from 'react';
 
 const Provider = dynamic(() => import('./Provider'));
+const SiteLayout = dynamic(() => import('@components/SiteLayout'));
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <Provider>
-      <SiteLayout>
-        <Component {...pageProps} />
-      </SiteLayout>
-    </Provider>
+    <Suspense fallback={<Loading />}>
+      <Provider>
+        <SiteLayout>
+          <Component {...pageProps} />
+        </SiteLayout>
+      </Provider>
+    </Suspense>
   );
 };
 
