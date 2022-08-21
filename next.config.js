a8b1c17f941d41b557c1fb05297e4c368dea9eef
Bigint <69431456+bigint@users.noreply.github.com>
2022-08-21 18:06:30 +0530

    chore: add batch_requests

--- next.config.js
+++ next.config.js
@@ -18,10 +18,6 @@ module.exports = withTM(
           {
             source: '/sitemaps/:match*',
             destination: 'https://sitemap.lenster.xyz/sitemaps/:match*'
-          },
-          {
-            source: '/collect/:slug',
-            destination: 'https://api-eu.mixpanel.com/:slug'
           }
         ];
       },
