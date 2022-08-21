64a3990a4ba4e0a9a43e2eb76ed685e740f589c0
Bigint <69431456+bigint@users.noreply.github.com>
2022-08-21 10:57:37 +0530

    chore: fix copy
    Co-Authored-By: sasi.eth <me@sasi.codes>

--- src/components/Home/EnableDispatcher.tsx
+++ src/components/Home/EnableDispatcher.tsx
@@ -17,10 +17,10 @@ const EnableDispatcher: FC = () => {
       <CardBody className="space-y-2.5 text-brand-600">
         <div className="flex items-center space-x-2 font-bold">
           <HandIcon className="w-5 h-5" />
-          <p>Set dispatcher</p>
+          <p>Action Required</p>
         </div>
         <p className="text-sm leading-[22px]">
-          We suggest you to enable dispatcher so you don't need to sign all your transactions in {APP_NAME}.
+          You can enable dispatcher to interact with {APP_NAME} without signing any of your transactions.
         </p>
         <ToggleDispatcher buttonSize="sm" />
       </CardBody>
