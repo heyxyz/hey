4f275e770d895949486b9ff79e9a251dc858883c
Bigint <69431456+bigint@users.noreply.github.com>
2022-08-23 21:03:00 +0530

    feat: add streak delete to clearAuthData() util

--- src/lib/clearAuthData.ts
+++ src/lib/clearAuthData.ts
@@ -4,6 +4,7 @@ const clearAuthData = () => {
   Cookies.remove('accessToken');
   Cookies.remove('refreshToken');
   localStorage.removeItem('lenster.store');
+  localStorage.removeItem('streak');
 };
 
 export default clearAuthData;
