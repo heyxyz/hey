11402381f845cf005a411513848a8f72432f7b32
Bigint <69431456+bigint@users.noreply.github.com>
2022-08-23 17:32:02 +0530

    chore: remove ping

--- src/apollo/lib/keyFields.ts
+++ src/apollo/lib/keyFields.ts
@@ -1,3 +1,8 @@
+/**
+ *
+ * @param publication The publication to get the key fields from.
+ * @returns key fields of the publication.
+ */
 export const publicationKeyFields = (publication: any) => {
   return `${publication.__typename}:${JSON.stringify({
     id: publication.id,
