f6c1d70486b1b12cb05d897d511175f4dfe5617f
Bigint <69431456+bigint@users.noreply.github.com>
2022-08-24 11:42:10 +0530

    chore: remove ts directives

--- src/constants.ts
+++ src/constants.ts
@@ -95,4 +95,4 @@ export const ALLOWED_MEDIA_TYPES = ['video/mp4', 'image/jpeg', 'image/png', 'ima
 
 // Bundlr
 export const BUNDLR_CURRENCY = 'matic';
-export const BUNDLR_NODE_URL = IS_MAINNET ? 'https://node2.bundlr.network' : 'https://devnet.bundlr.network';
+export const BUNDLR_NODE_URL = 'https://node2.bundlr.network';
