1d7cdfe9f8414aba65d3d4b92b3cd765f74f84a8
Bigint <69431456+bigint@users.noreply.github.com>
2022-08-25 17:30:14 +0530

    chore: verify @toucanprotocol.lens

--- data/verified.ts
+++ data/verified.ts
@@ -1,4 +1,5 @@
 export const mainnetVerified = [
+  '0x0c67' /** @toucanprotocol.lens */,
   '0x1c5f' /** @allmylovezoe.lens */,
   '0xe06b' /** @operacrypto.lens */,
   '0x250e' /** @alexatallah.lens */,
