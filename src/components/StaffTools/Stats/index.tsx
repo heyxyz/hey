d794f4c18104ae9313a2de1aa2ce068bbea50ad7
Bigint <69431456+bigint@users.noreply.github.com>
2022-08-27 10:56:51 +0530

    chore: add more staff tools check

--- src/components/StaffTools/Stats/index.tsx
+++ src/components/StaffTools/Stats/index.tsx
@@ -1,18 +1,20 @@
 import { GridItemEight, GridItemFour, GridLayout } from '@components/GridLayout';
 import { Card, CardBody } from '@components/UI/Card';
 import Seo from '@components/utils/Seo';
+import isStaff from '@lib/isStaff';
 import { NextPage } from 'next';
 import React from 'react';
 import { APP_NAME } from 'src/constants';
 import Custom404 from 'src/pages/404';
-import { useAppStore } from 'src/store/app';
+import { useAppPersistStore, useAppStore } from 'src/store/app';
 
 import Sidebar from '../Sidebar';
 
 const Stats: NextPage = () => {
   const currentProfile = useAppStore((state) => state.currentProfile);
+  const staffMode = useAppPersistStore((state) => state.staffMode);
 
-  if (!currentProfile) {
+  if (!currentProfile || !isStaff(currentProfile?.id) || !staffMode) {
     return <Custom404 />;
   }
 
