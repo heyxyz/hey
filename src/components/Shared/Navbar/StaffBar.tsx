63ac44010d0abefa4fc289d33d881b3b1968fef7
bigint <69431456+bigint@users.noreply.github.com>
2022-08-28 18:53:31 +0530

    feat: move stats to staff page (#567)

--- src/components/Shared/Navbar/StaffBar.tsx
+++ src/components/Shared/Navbar/StaffBar.tsx
@@ -1,10 +1,7 @@
-import { Menu, Transition } from '@headlessui/react';
-import { ChartPieIcon, GlobeAltIcon, HashtagIcon, TemplateIcon } from '@heroicons/react/outline';
+import { GlobeAltIcon, HashtagIcon, TemplateIcon } from '@heroicons/react/outline';
 import Link from 'next/link';
-import React, { FC, Fragment, ReactNode } from 'react';
+import React, { FC, ReactNode } from 'react';
 import { GIT_COMMIT_SHA, IS_MAINNET, IS_PRODUCTION } from 'src/constants';
-
-import Stats from './Stats';
 
 interface Props {
   children: ReactNode;
@@ -46,33 +43,7 @@ const StaffBar: FC = () => {
           </a>
         )}
       </div>
-      <div className="flex items-center space-x-3">
-        <Menu>
-          {({ open }) => (
-            <>
-              <Menu.Button as="button">
-                <ChartPieIcon className="w-4 h-4" />
-              </Menu.Button>
-              <Transition
-                show={open}
-                as={Fragment}
-                enter="transition ease-out duration-100"
-                enterFrom="transform opacity-0 scale-95"
-                enterTo="transform opacity-100 scale-100"
-                leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100"
-                leaveTo="transform opacity-0 scale-95"
-              >
-                <Menu.Items
-                  static
-                  className="absolute right-2 z-10 py-1 mt-6 bg-white rounded-xl border shadow-sm origin-top-right dark:bg-gray-900 focus:outline-none dark:border-gray-700/80"
-                >
-                  <Stats />
-                </Menu.Items>
-              </Transition>
-            </>
-          )}
-        </Menu>
+      <div className="flex items-center">
         <Link href="/stafftools">
           <TemplateIcon className="w-4 h-4" />
         </Link>
