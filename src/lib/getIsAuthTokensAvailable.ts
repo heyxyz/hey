a07c4fd06fec84efeab1c05700f25a63a00e4f4d
bigint <69431456+bigint@users.noreply.github.com>
2022-08-29 13:16:02 +0530

    fix: refactor auth (#571)
    Co-authored-by: sasi.eth <29498872+sasicodes@users.noreply.github.com>

---
+++ src/lib/getIsAuthTokensAvailable.ts
@@ -0,0 +1,8 @@
+const getIsAuthTokensAvailable = () => {
+  const accessToken = localStorage.getItem('accessToken');
+  const refreshToken = localStorage.getItem('refreshToken');
+  const isAvailable = accessToken !== 'undefined' && refreshToken !== 'undefined';
+  return isAvailable;
+};
+
+export default getIsAuthTokensAvailable;
