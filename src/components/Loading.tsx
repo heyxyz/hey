b3fa03c0975fdd3c08243ea3ae1c94f726dbe510
Bigint <69431456+bigint@users.noreply.github.com>
2022-08-29 12:57:20 +0530

    fix: loading screen

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -4,7 +4,7 @@ import Seo from './utils/Seo';
 
 const Loading: FC = () => {
   return (
-    <div className="flex flex-grow justify-center items-center h-screen">
+    <div className="grid h-screen place-items-center">
       <Seo />
       <img className="w-28 h-28" height={112} width={112} src="/logo.svg" alt="Logo" />
     </div>
