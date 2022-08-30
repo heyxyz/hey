dcd779f6bcc6ad503f8f0e0ce0f4bbc192b54f23
bigint <69431456+bigint@users.noreply.github.com>
2022-08-30 14:39:09 +0530

    fix: call refresh api in async way (#574)

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,4 +1,12 @@
-import { ApolloClient, ApolloLink, from, HttpLink, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloLink,
+  from,
+  fromPromise,
+  HttpLink,
+  InMemoryCache,
+  toPromise
+} from '@apollo/client';
 import { RetryLink } from '@apollo/client/link/retry';
 import result from '@generated/types';
 import { cursorBasedPagination } from '@lib/cursorBasedPagination';
@@ -46,44 +54,11 @@ const authLink = new ApolloLink((operation, forward) => {
   if (!accessToken || accessToken === 'undefined') {
     clearStorage();
     return forward(operation);
-  } else {
-    const isExpireSoon = Date.now() >= parseJwt(accessToken)?.exp * 1000;
-    if (isExpireSoon) {
-      axios(API_URL, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        data: JSON.stringify({
-          operationName: 'Refresh',
-          query: REFRESH_AUTHENTICATION_MUTATION,
-          variables: {
-            request: { refreshToken: localStorage.getItem('refreshToken') }
-          }
-        })
-      })
-        .then(({ data }) => {
-          const accessToken = data?.data?.refresh?.accessToken;
-          const refreshToken = data?.data?.refresh?.refreshToken;
-          operation.setContext({
-            headers: {
-              'x-access-token': `Bearer ${accessToken}`
-            }
-          });
+  }
 
-          if (!accessToken || !refreshToken) {
-            clearStorage();
-            window.location.reload();
-          }
+  const expiringSoon = Date.now() >= parseJwt(accessToken)?.exp * 1000;
 
-          localStorage.setItem('accessToken', accessToken);
-          localStorage.setItem('refreshToken', refreshToken);
-        })
-        .catch(() => {
-          clearStorage();
-          window.location.reload();
-          console.log(ERROR_MESSAGE);
-        });
-    }
-
+  if (!expiringSoon) {
     operation.setContext({
       headers: {
         'x-access-token': accessToken ? `Bearer ${accessToken}` : ''
@@ -92,6 +67,39 @@ const authLink = new ApolloLink((operation, forward) => {
 
     return forward(operation);
   }
+
+  return fromPromise(
+    axios(API_URL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      data: JSON.stringify({
+        operationName: 'Refresh',
+        query: REFRESH_AUTHENTICATION_MUTATION,
+        variables: {
+          request: { refreshToken: localStorage.getItem('refreshToken') }
+        }
+      })
+    })
+      .then(({ data }) => {
+        const accessToken = data?.data?.refresh?.accessToken;
+        const refreshToken = data?.data?.refresh?.refreshToken;
+        operation.setContext({
+          headers: {
+            'x-access-token': `Bearer ${accessToken}`
+          }
+        });
+
+        localStorage.setItem('accessToken', accessToken);
+        localStorage.setItem('refreshToken', refreshToken);
+
+        return toPromise(forward(operation));
+      })
+      .catch(() => {
+        console.log(ERROR_MESSAGE);
+
+        return toPromise(forward(operation));
+      })
+  );
 });
 
 const cache = new InMemoryCache({
