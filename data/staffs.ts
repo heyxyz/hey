1ab16de769f53152d0f58052665be1f49e545485
Bigint <69431456+bigint@users.noreply.github.com>
2022-09-01 20:38:54 +0530

    chore: add @stani.lens to staff

--- data/staffs.ts
+++ data/staffs.ts
@@ -1,3 +1,7 @@
-export const mainnetStaffs = ['0x06' /** @wagmi.lens */, '0x0d' /** @yoginth.lens */];
+export const mainnetStaffs = [
+  '0x06' /** @wagmi.lens */,
+  '0x05' /** @stani.lens */,
+  '0x0d' /** @yoginth.lens */
+];
 
 export const testnetStaffs = ['0x15' /** @yoginth.test */];
