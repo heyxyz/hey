052bed3c4ace6ba92dff788885a81c9521259dae
Bigint <69431456+bigint@users.noreply.github.com>
2022-09-02 15:27:23 +0530

    fix: duplicate tag and limit

--- src/lib/getTags.ts
+++ src/lib/getTags.ts
@@ -1,13 +1,14 @@
 const getTags = (inputText: string) => {
   var regex = /(?:^|\s)#([\dA-Za-z]+)/gm;
-  var matches = [];
+  var matches: any = [];
   var match;
 
   while ((match = regex.exec(inputText))) {
     matches.push(match[1]);
   }
 
-  return matches;
+  // @ts-ignore
+  return matches.filter((item, pos) => matches.indexOf(item) == pos).slice(0, 5);
 };
 
 export default getTags;
