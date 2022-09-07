cb477ead09435eb8b2c9a2418849db0344fd66f1
bigint <69431456+bigint@users.noreply.github.com>
2022-09-07 13:44:49 +0530

    feat: add writeQuery

--- src/components/Publication/QueuedPublication.tsx
+++ src/components/Publication/QueuedPublication.tsx
@@ -1,13 +1,14 @@
-import { useQuery } from '@apollo/client';
+import { useApolloClient, useQuery } from '@apollo/client';
 import Markup from '@components/Shared/Markup';
 import UserProfile from '@components/Shared/UserProfile';
 import { Profile } from '@generated/types';
-import { TX_STATUS_QUERY } from '@gql/HasTxHashBeenIndexed';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import React, { FC } from 'react';
 import { useAppStore } from 'src/store/app';
 import { usePublicationPersistStore } from 'src/store/publication';
+
+import { PUBLICATION_QUERY } from '.';
 
 dayjs.extend(relativeTime);
 
@@ -19,17 +20,29 @@ const QueuedPublication: FC<Props> = ({ txn }) => {
   const currentProfile = useAppStore((state) => state.currentProfile);
   const txnQueue = usePublicationPersistStore((state) => state.txnQueue);
   const setTxnQueue = usePublicationPersistStore((state) => state.setTxnQueue);
+  const { cache } = useApolloClient();
 
-  const { data, loading } = useQuery(TX_STATUS_QUERY, {
+  const { data } = useQuery(PUBLICATION_QUERY, {
     variables: {
-      request: { txHash: txn?.txnHash }
+      request: { txHash: txn?.txnHash },
+      reactionRequest: currentProfile ? { profileId: currentProfile?.id } : null,
+      profileId: currentProfile?.id ?? null
     },
     pollInterval: 1000,
     onCompleted: (data) => {
-      if (data?.hasTxHashBeenIndexed?.indexed) {
-        console.log('tx indexed');
-        // remove from queue
+      console.log(data);
+      if (data?.publication) {
         setTxnQueue(txnQueue.filter((o) => o.txnHash !== txn?.txnHash));
+        cache.modify({
+          fields: {
+            timeline() {
+              cache.writeQuery({
+                data: data?.publication,
+                query: PUBLICATION_QUERY
+              });
+            }
+          }
+        });
       }
     }
   });
