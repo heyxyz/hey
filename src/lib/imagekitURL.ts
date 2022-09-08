b1e53aa7ba89ed8c71e28418a70d1650114c316c
bigint <69431456+bigint@users.noreply.github.com>
2022-09-08 22:11:35 +0530

    revert: support progressive loading of images from cdn

--- src/lib/imagekitURL.ts
+++ src/lib/imagekitURL.ts
@@ -8,8 +8,8 @@ import { IMAGEKIT_URL } from 'src/constants';
  */
 const imagekitURL = (url: string, name?: string): string => {
   return name
-    ? `${IMAGEKIT_URL}/tr:n-${name},pr-true,tr:di-placeholder.webp/${url}`
-    : `${IMAGEKIT_URL}/pr-true,tr:di-placeholder.webp/${url}`;
+    ? `${IMAGEKIT_URL}/tr:n-${name},tr:di-placeholder.webp/${url}`
+    : `${IMAGEKIT_URL}/tr:di-placeholder.webp/${url}`;
 };
 
 export default imagekitURL;
