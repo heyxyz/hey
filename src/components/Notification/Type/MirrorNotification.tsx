bb9f1ad828b9cad2a1c388a57183a7c9310faecd
bigint <69431456+bigint@users.noreply.github.com>
2022-09-12 16:20:35 +0530

    chore: revert notification fix

--- src/components/Notification/Type/MirrorNotification.tsx
+++ src/components/Notification/Type/MirrorNotification.tsx
@@ -16,8 +16,6 @@ interface Props {
 
 const MirrorNotification: FC<Props> = ({ notification }) => {
   const publicationType = notification?.publication?.metadata?.attributes[0]?.value;
-  // @ts-ignore
-  const pubId = notification?.publication?.pubId;
 
   return (
     <div className="flex justify-between items-start">
@@ -29,14 +27,17 @@ const MirrorNotification: FC<Props> = ({ notification }) => {
         <div className="ml-9">
           <NotificationProfileName profile={notification?.profile} />{' '}
           <span className="pl-0.5 text-gray-600 dark:text-gray-400">mirrored your </span>
-          <Link href={`/posts/${pubId}`} className="font-bold">
+          <Link href={`/posts/${notification?.publication?.id}`} className="font-bold">
             {notification?.publication?.__typename === 'Post'
               ? publicationType === 'crowdfund'
                 ? 'crowdfund'
                 : notification?.publication?.__typename?.toLowerCase()
               : notification?.publication?.__typename?.toLowerCase()}
           </Link>
-          <Link href={`/posts/${pubId}`} className="text-gray-500 line-clamp-2 linkify mt-2">
+          <Link
+            href={`/posts/${notification?.publication?.id}`}
+            className="text-gray-500 line-clamp-2 linkify mt-2"
+          >
             {publicationType === 'crowdfund' ? (
               notification?.publication?.metadata?.name
             ) : (
