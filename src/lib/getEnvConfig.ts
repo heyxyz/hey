4c71cb994532a09e9adb9567da8113a7e8c25b71
Bigint <69431456+bigint@users.noreply.github.com>
2022-09-15 20:03:21 +0530

    chore: add sandbox env support

---
+++ src/lib/getEnvConfig.ts
@@ -0,0 +1,56 @@
+import {
+  LENS_NETWORK,
+  MAINNET_API_URL,
+  MAINNET_DEFAULT_TOKEN,
+  MAINNET_FREE_COLLECT_MODULE,
+  MAINNET_LENS_PERIPHERY,
+  MAINNET_LENSHUB_PROXY,
+  SANDBOX_API_URL,
+  SANDBOX_FREE_COLLECT_MODULE,
+  SANDBOX_LENS_PERIPHERY,
+  SANDBOX_LENSHUB_PROXY,
+  TESTNET_API_URL,
+  TESTNET_DEFAULT_TOKEN,
+  TESTNET_FREE_COLLECT_MODULE,
+  TESTNET_LENS_PERIPHERY,
+  TESTNET_LENSHUB_PROXY
+} from 'src/constants';
+
+const getEnvConfig = () => {
+  switch (LENS_NETWORK) {
+    case 'mainnet':
+      return {
+        apiEndpoint: MAINNET_API_URL,
+        lensHubProxyAddress: MAINNET_LENSHUB_PROXY,
+        lensPeripheryAddress: MAINNET_LENS_PERIPHERY,
+        freeCollectModuleAddress: MAINNET_FREE_COLLECT_MODULE,
+        defaultCollectToken: MAINNET_DEFAULT_TOKEN
+      };
+    case 'testnet':
+      return {
+        apiEndpoint: TESTNET_API_URL,
+        lensHubProxyAddress: TESTNET_LENSHUB_PROXY,
+        lensPeripheryAddress: TESTNET_LENS_PERIPHERY,
+        freeCollectModuleAddress: TESTNET_FREE_COLLECT_MODULE,
+        defaultCollectToken: TESTNET_DEFAULT_TOKEN
+      };
+    case 'sandbox':
+      return {
+        apiEndpoint: SANDBOX_API_URL,
+        lensHubProxyAddress: SANDBOX_LENSHUB_PROXY,
+        lensPeripheryAddress: SANDBOX_LENS_PERIPHERY,
+        freeCollectModuleAddress: SANDBOX_FREE_COLLECT_MODULE,
+        defaultCollectToken: TESTNET_DEFAULT_TOKEN
+      };
+    default:
+      return {
+        apiEndpoint: MAINNET_API_URL,
+        lensHubProxyAddress: MAINNET_LENSHUB_PROXY,
+        lensPeripheryAddress: MAINNET_LENS_PERIPHERY,
+        freeCollectModuleAddress: MAINNET_FREE_COLLECT_MODULE,
+        defaultCollectToken: MAINNET_DEFAULT_TOKEN
+      };
+  }
+};
+
+export default getEnvConfig;
