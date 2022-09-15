161352c5a68173b70d2aee97a074339c09bed410
bigint <69431456+bigint@users.noreply.github.com>
2022-09-15 20:05:25 +0530

    chore: add sandbox env support (#663)

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,34 @@
+import getEnvConfig from '@lib/getEnvConfig';
 import { chain } from 'wagmi';
 
 // Environments
 export const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 
-// Blockchain Network
-export const API_URL = process.env.NEXT_PUBLIC_API_URL ?? 'https://api.lens.dev';
-export const IS_MAINNET = API_URL === 'https://api.lens.dev';
+// Lens Network
+export const LENS_NETWORK = process.env.NEXT_PUBLIC_LENS_NETWORK ?? 'mainnet';
+export const MAINNET_API_URL = 'https://api.lens.dev';
+export const TESTNET_API_URL = 'https://api-mumbai.lens.dev';
+export const SANDBOX_API_URL = 'https://api-sandbox-mumbai.lens.dev';
+export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+export const MAINNET_LENSHUB_PROXY = '0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d';
+export const MAINNET_LENS_PERIPHERY = '0xeff187b4190E551FC25a7fA4dFC6cf7fDeF7194f';
+export const MAINNET_FREE_COLLECT_MODULE = '0x23b9467334bEb345aAa6fd1545538F3d54436e96';
+export const MAINNET_DEFAULT_TOKEN = '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270';
+export const TESTNET_LENSHUB_PROXY = '0x60Ae865ee4C725cd04353b5AAb364553f56ceF82';
+export const TESTNET_LENS_PERIPHERY = '0xD5037d72877808cdE7F669563e9389930AF404E8';
+export const TESTNET_FREE_COLLECT_MODULE = '0x0BE6bD7092ee83D44a6eC1D949626FeE48caB30c';
+export const TESTNET_DEFAULT_TOKEN = '0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889';
+export const SANDBOX_LENSHUB_PROXY = '0x7582177F9E536aB0b6c721e11f383C326F2Ad1D5';
+export const SANDBOX_LENS_PERIPHERY = '0x5dDD1d6c04E805D830574A31Bf7979D416c6d7c5';
+export const SANDBOX_FREE_COLLECT_MODULE = '0xD57C675b542bFC24242a1088514306fFaeaFc6Fb';
+
+export const API_URL = getEnvConfig().apiEndpoint;
+export const LENSHUB_PROXY = getEnvConfig().lensHubProxyAddress;
+export const LENS_PERIPHERY = getEnvConfig().lensPeripheryAddress;
+export const FREE_COLLECT_MODULE = getEnvConfig().freeCollectModuleAddress;
+export const DEFAULT_COLLECT_TOKEN = getEnvConfig().defaultCollectToken;
+
+export const IS_MAINNET = API_URL === MAINNET_API_URL;
 
 // Application
 export const APP_NAME = 'Lenster';
@@ -61,15 +84,6 @@ export const ERRORS = {
   notMined:
     'A previous transaction may not been mined yet or you have passed in a invalid nonce. You must wait for that to be mined before doing another action, please try again in a few moments. Nonce out of sync.'
 };
-
-// Addresses
-export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
-export const LENSHUB_PROXY = process.env.NEXT_PUBLIC_LENSHUB_PROXY ?? '';
-export const LENS_PERIPHERY = process.env.NEXT_PUBLIC_LENS_PERIPHERY ?? '';
-export const FREE_COLLECT_MODULE = process.env.NEXT_PUBLIC_FREE_COLLECT_MODULE ?? '';
-export const DEFAULT_COLLECT_TOKEN = IS_MAINNET
-  ? '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270'
-  : '0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889';
 
 // Regex
 export const URL_REGEX =
