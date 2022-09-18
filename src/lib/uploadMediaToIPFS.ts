a13b7255007899ea975ed3bc1a52bf0e3d831d09
sasi.eth <me@sasi.codes>
2022-09-18 10:35:55 +0530

    fix(temp): update estuary endpoint (#667)

--- src/lib/uploadMediaToIPFS.ts
+++ src/lib/uploadMediaToIPFS.ts
@@ -14,7 +14,7 @@ const uploadMediaToIPFS = async (data: any): Promise<LensterAttachment[]> => {
       const file = data.item(i);
       const formData = new FormData();
       formData.append('data', file, uuid());
-      const upload = await axios(`https://shuttle-5.estuary.tech/content/add`, {
+      const upload = await axios(`https://shuttle-4.estuary.tech/content/add`, {
         method: 'POST',
         data: formData,
         headers: {
