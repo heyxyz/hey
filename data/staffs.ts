036321b18a488c0a5596d23dc8275ff5bd709b09
bigint <69431456+bigint@users.noreply.github.com>
2022-09-24 14:14:34 +0530

    chore: verify cultivatordao.lens

--- data/staffs.ts
+++ data/staffs.ts
@@ -1,8 +1,10 @@
 export const mainnetStaffs = [
-  '0x16' /** @davidev.lens */,
-  '0x06' /** @wagmi.lens */,
-  '0x05' /** @stani.lens */,
-  '0x0d' /** @yoginth.lens */
+  "0x16", // davidev.lens,
+  "0x06", // wagmi.lens
+  "0x05", // stani.lens
+  "0x0d", // yoginth.lens
 ];
 
-export const testnetStaffs = ['0x15' /** @yoginth.test */];
+export const testnetStaffs = [
+  "0x15", // yoginth.test
+];
