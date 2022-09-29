95fa1413d51be5541aefa9f9dcdc896d2ade02a9
Bigint <69431456+bigint@users.noreply.github.com>
2022-09-29 21:45:17 +0530

    chore: create contracts file

--- src/constants.ts
+++ src/constants.ts
@@ -9,18 +9,6 @@ export const LENS_NETWORK = process.env.NEXT_PUBLIC_LENS_NETWORK ?? 'mainnet';
 export const MAINNET_API_URL = 'https://api.lens.dev';
 export const TESTNET_API_URL = 'https://api-mumbai.lens.dev';
 export const SANDBOX_API_URL = 'https://api-sandbox-mumbai.lens.dev';
-export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
-export const MAINNET_LENSHUB_PROXY = '0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d';
-export const MAINNET_LENS_PERIPHERY = '0xeff187b4190E551FC25a7fA4dFC6cf7fDeF7194f';
-export const MAINNET_FREE_COLLECT_MODULE = '0x23b9467334bEb345aAa6fd1545538F3d54436e96';
-export const MAINNET_DEFAULT_TOKEN = '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270';
-export const TESTNET_LENSHUB_PROXY = '0x60Ae865ee4C725cd04353b5AAb364553f56ceF82';
-export const TESTNET_LENS_PERIPHERY = '0xD5037d72877808cdE7F669563e9389930AF404E8';
-export const TESTNET_FREE_COLLECT_MODULE = '0x0BE6bD7092ee83D44a6eC1D949626FeE48caB30c';
-export const TESTNET_DEFAULT_TOKEN = '0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889';
-export const SANDBOX_LENSHUB_PROXY = '0x7582177F9E536aB0b6c721e11f383C326F2Ad1D5';
-export const SANDBOX_LENS_PERIPHERY = '0x5dDD1d6c04E805D830574A31Bf7979D416c6d7c5';
-export const SANDBOX_FREE_COLLECT_MODULE = '0xD57C675b542bFC24242a1088514306fFaeaFc6Fb';
 
 export const API_URL = getEnvConfig().apiEndpoint;
 export const LENSHUB_PROXY = getEnvConfig().lensHubProxyAddress;
@@ -44,6 +32,7 @@ export const CONTACT_EMAIL = 'support@lenster.xyz';
 export const RELAY_ON = process.env.NEXT_PUBLIC_RELAY_ON === 'true';
 export const MIXPANEL_TOKEN = process.env.NEXT_PUBLIC_MIXPANEL_TOKEN ?? '';
 export const MIXPANEL_API_HOST = 'https://utils.lenster.xyz/collect';
+export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
 // Messages
 export const ERROR_MESSAGE = 'Something went wrong!';
