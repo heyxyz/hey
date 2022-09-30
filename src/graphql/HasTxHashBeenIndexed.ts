0ad4f1f2dfddd24a24b18e7fda6577f053dd539b
bigint <69431456+bigint@users.noreply.github.com>
2022-09-30 23:55:03 +0530

    fix: use TX_STATUS_QUERY for post is indexed or not (#707)

--- src/graphql/HasTxHashBeenIndexed.ts
+++ src/graphql/HasTxHashBeenIndexed.ts
@@ -4,7 +4,13 @@ export const TX_STATUS_QUERY = gql`
   query HasTxHashBeenIndexed($request: HasTxHashBeenIndexedRequest!) {
     hasTxHashBeenIndexed(request: $request) {
       ... on TransactionIndexedResult {
+        metadataStatus {
+          status
+        }
         indexed
+      }
+      ... on TransactionError {
+        reason
       }
     }
   }
