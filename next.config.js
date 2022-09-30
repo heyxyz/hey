d7ebd307a73daa3eb8928083d388bd697b28373f
Bigint <69431456+bigint@users.noreply.github.com>
2022-09-30 22:57:11 +0530

    fix: remove swcMinify

--- next.config.js
+++ next.config.js
@@ -12,7 +12,6 @@ module.exports = withTM(
       },
       reactStrictMode: false,
       trailingSlash: false,
-      swcMinify: true,
       experimental: {
         newNextLinkBehavior: true
       },
