a5a4cad17ad91b718b046eaefd6734ea9c02d8a0
Bigint <69431456+bigint@users.noreply.github.com>
2022-10-01 13:19:53 +0530

    fix: publication overflow for threaded and closes #431

--- src/components/Publication/ThreadBody.tsx
+++ src/components/Publication/ThreadBody.tsx
@@ -37,8 +37,8 @@ const ThreadBody: FC<Props> = ({ publication }) => {
         <span className="text-xs text-gray-500">{dayjs(new Date(timestamp)).fromNow()}</span>
       </div>
       <div className="flex">
-        <div className="mr-8 ml-5 bg-gray-300 border-gray-300 dark:bg-gray-700 dark:border-gray-700 border-[0.8px] -my-[4px]" />
-        <div className="pt-4 pb-5 w-full">
+        <div className="mr-8 ml-5 bg-gray-300 border-gray-300 dark:bg-gray-700 dark:border-gray-700 border-[0.8px] -my-[3px]" />
+        <div className="pt-4 pb-5 w-[85%] sm:w-full">
           {publication?.hidden ? (
             <HiddenPublication type={publication.__typename} />
           ) : (
