448bf0fc750533c49cc8f5562456af9ad55dee71
Bigint <69431456+bigint@users.noreply.github.com>
2022-10-01 13:34:13 +0530

    fix: bg color in beta warning

--- src/components/Home/Announcement.tsx
+++ src/components/Home/Announcement.tsx
@@ -5,7 +5,7 @@ import { APP_NAME, IS_MAINNET } from 'src/constants';
 
 const Announcement: FC = () => {
   return (
-    <Card as="aside" className="mb-4 bg-yellow-50 dark:bg-yellow-900 !border-yellow-600">
+    <Card as="aside" className="mb-4 border-yellow-400 !bg-yellow-300 !bg-opacity-20">
       <CardBody className="space-y-2.5 text-yellow-600">
         <div className="flex items-center space-x-2 font-bold">
           <BeakerIcon className="w-5 h-5" />
