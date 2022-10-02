790ba2024e152507787365121e65612feaeacedc
Bigint <69431456+bigint@users.noreply.github.com>
2022-10-02 11:04:02 +0530

    chore: limit files

--- src/components/Shared/ChooseFile.tsx
+++ src/components/Shared/ChooseFile.tsx
@@ -9,7 +9,7 @@ const ChooseFile: FC<Props> = ({ onChange }) => {
     <input
       className="pr-1 text-sm text-gray-700 bg-white rounded-xl border border-gray-300 shadow-sm cursor-pointer dark:text-white dark:bg-gray-800 focus:outline-none dark:border-gray-700/80 focus:border-brand-400"
       type="file"
-      accept="image/*"
+      accept=".png, .jpg, .jpeg, .gif"
       onChange={onChange}
     />
   );
