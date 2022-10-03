21dffb23253c29ec550d2a213778d9f03b90cbca
Bigint <69431456+bigint@users.noreply.github.com>
2022-10-03 19:55:29 +0530

    feat: add composer-v2 flag

--- data/feature-flags.ts
+++ data/feature-flags.ts
@@ -6,5 +6,10 @@ export const featureFlags = [
     key: 'messages',
     name: 'Messages',
     enabledFor: [...lensterMembers, ...aaveMembers]
+  },
+  {
+    key: 'composer-v2',
+    name: 'Composer v2',
+    enabledFor: [...lensterMembers]
   }
 ];
