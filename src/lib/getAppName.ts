2fad6adddfe7247c87a2194da5f43396f6164d8e
Bigint <69431456+bigint@users.noreply.github.com>
2022-10-04 06:54:45 +0530

    fix: sonarjs errors

--- src/lib/getAppName.ts
+++ src/lib/getAppName.ts
@@ -1,8 +1,6 @@
 const getAppName = (str: string): string => {
   const initCase = str.charAt(0).toUpperCase() + str.slice(1);
-  const removeHyphen = initCase.replace(/-/g, ' ');
-
-  return removeHyphen;
+  return initCase.replace(/-/g, ' ');
 };
 
 export default getAppName;
