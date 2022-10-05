9fdb130a164269f74f8d3998625271f5278677ae
Bigint <69431456+bigint@users.noreply.github.com>
2022-10-06 01:00:41 +0530

    chore: add xmtp members directly

--- data/feature-flags.ts
+++ data/feature-flags.ts
@@ -1,11 +1,16 @@
 import { lensterMembers } from './lenster-members';
-import { xmtpMembers } from './xmtp-members';
 
 export const featureFlags = [
   {
     key: 'messages',
     name: 'Messages',
-    enabledFor: [...lensterMembers, ...xmtpMembers]
+    enabledFor: [
+      ...lensterMembers,
+      '0xe248', // nick-molnar.lens
+      '0x010e04', // elisealix22.lens
+      '0x5cce', // saulmc.lens
+      '0x014309' // iambhavya.lens
+    ]
   },
   {
     key: 'composer-v2',
