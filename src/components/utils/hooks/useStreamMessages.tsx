e0cffb36f8d990689e74b171251364781d4c097b
Elise Alix <elise@xmtp.com>
2022-10-06 04:26:02 -0400

    feat: add core message components and store (#743)
    Co-authored-by: bhavya2611 <bhavya.y.mehta@gmail.com>
    Co-authored-by: bigint <69431456+bigint@users.noreply.github.com>

---
+++ src/components/utils/hooks/useStreamMessages.tsx
@@ -0,0 +1,57 @@
+import { Conversation, Message, Stream } from '@xmtp/xmtp-js';
+import { useEffect, useState } from 'react';
+import { useMessageStore } from 'src/store/message';
+
+const useStreamMessages = (conversation: Conversation, onMessageCallback: () => void) => {
+  const messageState = useMessageStore((state) => state);
+  const { messages, setMessages } = messageState;
+  const [stream, setStream] = useState<Stream<Message>>();
+
+  useEffect(() => {
+    if (!conversation) {
+      return;
+    }
+    const streamMessages = async () => {
+      const newStream = await conversation.streamMessages();
+      setStream(newStream);
+      for await (const msg of newStream) {
+        if (setMessages) {
+          const newMessages = messages.get(conversation.peerAddress) ?? [];
+          newMessages.push(msg);
+          const uniqueMessages = [
+            ...Array.from(new Map(newMessages.map((item) => [item['id'], item])).values())
+          ];
+          messages.set(conversation.peerAddress, uniqueMessages);
+          setMessages(new Map(messages));
+        }
+        if (onMessageCallback) {
+          onMessageCallback();
+        }
+      }
+    };
+    streamMessages();
+    return () => {
+      const closeStream = async () => {
+        if (!stream) {
+          return;
+        }
+        await stream.return();
+      };
+      closeStream();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [conversation]);
+
+  const sendMessage = async (message: string) => {
+    if (!conversation) {
+      return;
+    }
+    await conversation.send(message);
+  };
+
+  return {
+    sendMessage
+  };
+};
+
+export default useStreamMessages;
---
+++ src/pages/messages/[address].tsx
@@ -0,0 +1,3 @@
+import Message from '@components/Messages/message';
+
+export default Message;
