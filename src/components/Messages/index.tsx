f5cc1e20b1952503945e8240b3493886137dd762
Elise Alix <elise@xmtp.com>
2022-10-06 04:26:02 -0400

    feat: add core message components and store (#743)
    Co-authored-by: bhavya2611 <bhavya.y.mehta@gmail.com>
    Co-authored-by: bigint <69431456+bigint@users.noreply.github.com>

--- src/components/Messages/index.tsx
+++ src/components/Messages/index.tsx
@@ -1,13 +1,91 @@
+import { Card } from '@components/UI/Card';
 import { GridItemEight, GridItemFour, GridLayout } from '@components/UI/GridLayout';
 import MetaTags from '@components/utils/MetaTags';
 import isFeatureEnabled from '@lib/isFeatureEnabled';
-import { NextPage } from 'next';
+import { Client, Conversation, Stream } from '@xmtp/xmtp-js';
+import { useRouter } from 'next/router';
+import { FC, useEffect, useState } from 'react';
 import { APP_NAME } from 'src/constants';
 import Custom404 from 'src/pages/404';
 import { useAppStore } from 'src/store/app';
+import { useMessageStore } from 'src/store/message';
+import { useSigner } from 'wagmi';
 
-const Messages: NextPage = () => {
+const Messages: FC = () => {
+  const { data: signer } = useSigner();
   const currentProfile = useAppStore((state) => state.currentProfile);
+  const [stream, setStream] = useState<Stream<Conversation>>();
+  const messageState = useMessageStore((state) => state);
+  const { client, setClient, conversations, setConversations, messages, setMessages, setLoading } =
+    messageState;
+  const router = useRouter();
+
+  useEffect(() => {
+    const initXmtpClient = async () => {
+      if (signer && !client) {
+        const xmtp = await Client.create(signer);
+        setClient(xmtp);
+      }
+    };
+    if (isFeatureEnabled('messages', currentProfile?.id)) {
+      initXmtpClient();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [signer]);
+
+  useEffect(() => {
+    if (!client || !isFeatureEnabled('messages', currentProfile?.id)) {
+      return;
+    }
+
+    async function listConversations() {
+      setLoading(true);
+      const convos = (await client?.conversations?.list()) || [];
+      Promise.all(
+        convos.map(async (convo) => {
+          if (convo.peerAddress !== currentProfile?.ownedBy) {
+            const newMessages = await convo.messages();
+            messages.set(convo.peerAddress, newMessages);
+            setMessages(new Map(messages));
+            conversations.set(convo.peerAddress, convo);
+            setConversations(new Map(conversations));
+          }
+        })
+      ).then(() => {
+        setLoading(false);
+      });
+    }
+    const streamConversations = async () => {
+      const newStream = (await client?.conversations?.stream()) || [];
+      setStream(newStream);
+      for await (const convo of newStream) {
+        if (convo.peerAddress !== currentProfile?.ownedBy) {
+          const newMessages = await convo.messages();
+          messages.set(convo.peerAddress, newMessages);
+          setMessages(new Map(messages));
+          conversations.set(convo.peerAddress, convo);
+          setConversations(new Map(conversations));
+        }
+      }
+    };
+    listConversations();
+    streamConversations();
+
+    return () => {
+      const closeStream = async () => {
+        if (!stream) {
+          return;
+        }
+        await stream.return();
+      };
+      closeStream();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [client]);
+
+  const onConversationSelected = (address: string) => {
+    router.push(address ? `/messages/${address}` : '/messages/');
+  };
 
   if (!isFeatureEnabled('messages', currentProfile?.id)) {
     return <Custom404 />;
@@ -17,10 +95,34 @@ const Messages: NextPage = () => {
     <GridLayout>
       <MetaTags title={`Messages • ${APP_NAME}`} />
       <GridItemFour>
-        <div>gm</div>
+        <Card className="h-[86vh] px-2 pt-3">
+          <div className="flex justify-between">
+            <div className="font-black text-lg">Messages</div>
+            <div>
+              <button className="text-xs border border-p-100 p-1 rounded">New Message</button>
+            </div>
+          </div>
+          <div className="flex justify-between p-4">
+            <div className="text-xs">Lens profiles</div>
+            <div className="text-xs">All messages</div>
+          </div>
+          <div>
+            {Array.from(conversations.keys()).map((convo: string) => {
+              return (
+                <div
+                  onClick={() => onConversationSelected(convo)}
+                  key={`convo_${convo}`}
+                  className="border p-5 text-xs"
+                >
+                  {convo}
+                </div>
+              );
+            })}
+          </div>
+        </Card>
       </GridItemFour>
       <GridItemEight>
-        <div>gm</div>
+        <Card className="h-[86vh]">Hello</Card>
       </GridItemEight>
     </GridLayout>
   );
---
+++ src/store/message.ts
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+import { Client, Conversation, Message } from '@xmtp/xmtp-js';
+import create from 'zustand';
+
+interface MessageState {
+  client: Client | undefined;
+  setClient: (client: Client) => void;
+  conversations: Map<string, Conversation>;
+  setConversations: (conversations: Map<string, Conversation>) => void;
+  messages: Map<string, Message[]>;
+  setMessages: (messages: Map<string, Message[]>) => void;
+  loading: boolean;
+  setLoading: (loading: boolean) => void;
+}
+
+export const useMessageStore = create<MessageState>((set) => ({
+  client: undefined,
+  setClient: (client) => set(() => ({ client })),
+  conversations: new Map(),
+  setConversations: (conversations) => set(() => ({ conversations })),
+  messages: new Map(),
+  setMessages: (messages) => set(() => ({ messages })),
+  loading: false,
+  setLoading: (loading) => set(() => ({ loading }))
+}));
