5bff90e412b16856a922cdba9f130c7467494fdc
Elise Alix <elise@xmtp.com>
2022-10-07 01:27:22 -0400

    grab data from graphql for profiles

--- src/components/Messages/index.tsx
+++ src/components/Messages/index.tsx
@@ -1,13 +1,16 @@
-import ConversationPreview from '@components/Conversation/ConversationPreview';
+import { useQuery } from '@apollo/client';
+import MessagePreview from '@components/Messages/MessagePreview';
 import { Card } from '@components/UI/Card';
 import { GridItemEight, GridItemFour, GridLayout } from '@components/UI/GridLayout';
+import { PageLoading } from '@components/UI/PageLoading';
 import MetaTags from '@components/utils/MetaTags';
-import { Profile } from '@generated/types';
+import { Profile, ProfilesDocument } from '@generated/types';
 import isFeatureEnabled from '@lib/isFeatureEnabled';
-import { Client, Conversation, Stream } from '@xmtp/xmtp-js';
+import { Client, Conversation, Message, Stream } from '@xmtp/xmtp-js';
 import { FC, useEffect, useState } from 'react';
 import { APP_NAME } from 'src/constants';
 import Custom404 from 'src/pages/404';
+import Custom500 from 'src/pages/500';
 import { useAppStore } from 'src/store/app';
 import { useMessageStore } from 'src/store/message';
 import { useSigner } from 'wagmi';
@@ -15,12 +18,38 @@ import { useSigner } from 'wagmi';
 const Messages: FC = () => {
   const { data: signer } = useSigner();
   const currentProfile = useAppStore((state) => state.currentProfile);
-  const [profiles, setProfiles] = useState<Profile[]>();
   const [stream, setStream] = useState<Stream<Conversation>>();
+  const [profileMap, setProfileMap] = useState<Map<string, Profile>>();
+  const [messageMap, setMessageMap] = useState<Map<string, Message>>();
   const messageState = useMessageStore((state) => state);
-  const { client, setClient, conversations, setConversations, messages, setMessages, setLoading } =
+  const { client, setClient, conversations, setConversations, loadingMessages, setLoadingMessages } =
     messageState;
   const isMessagesEnabled = isFeatureEnabled('messages', currentProfile?.id);
+
+  const peerAddresses = Array.from(conversations.values()).map((convo) => convo.peerAddress);
+  peerAddresses.push(currentProfile?.ownedBy);
+  const { loading: loadingProfiles, error: profilesError } = useQuery(ProfilesDocument, {
+    // TODO(elise): Right now this isn't guaranteed to cover all profiles.
+    // We'll likely have to loop through all pages since peerAddresses contains all conversations.
+    variables: {
+      request: { ownedBy: peerAddresses, limit: 50 }
+    },
+    skip: !currentProfile?.id,
+    onCompleted: (data) => {
+      if (!data?.profiles?.items?.length) {
+        return;
+      }
+      const profiles = data.profiles.items as Profile[];
+      const newProfileMap = new Map<string, Profile>();
+      for (const profile of profiles) {
+        if (newProfileMap.get(profile.ownedBy)?.isDefault) {
+          return;
+        }
+        newProfileMap.set(profile.ownedBy, profile);
+      }
+      setProfileMap(newProfileMap);
+    }
+  });
 
   useEffect(() => {
     const initXmtpClient = async () => {
@@ -36,44 +65,50 @@ const Messages: FC = () => {
   }, [signer]);
 
   useEffect(() => {
-    if (!client || !isMessagesEnabled) {
+    if (!isMessagesEnabled || !client) {
       return;
     }
 
-    // const { data, loading, error } = useQuery(Profiles, {
-    //   variables: { request: { ownedBy: }, who: currentProfile?.id ?? null },
-    //   skip: !username
-    // });
-    // const fetchProfiles = async (peerAddress: string) => {
-    //   useQuery(ProfilesDocument)
-    // }
-
-    const updateMessageStore = async (convo: Conversation) => {
+    const fetchMostRecentMessage = async (
+      convo: Conversation,
+      messageMap: Map<string, Message>,
+      conversationMap: Map<string, Conversation>
+    ) => {
       if (convo.peerAddress !== currentProfile?.ownedBy) {
         const newMessages = await convo.messages({ limit: 1 });
-        messages.set(convo.peerAddress, newMessages);
-        setMessages(new Map(messages));
-        conversations.set(convo.peerAddress, convo);
-        setConversations(new Map(conversations));
+        if (newMessages.length === 0) {
+          return;
+        }
+        console.log('NEW MESSAGE: ' + newMessages[0].senderAddress);
+        messageMap.set(convo.peerAddress, newMessages[0]);
+        conversationMap.set(convo.peerAddress, convo);
       }
     };
 
     const listConversations = async () => {
-      setLoading(true);
+      const tempConversations = new Map<string, Conversation>();
+      const tempMessage = new Map<string, Message>();
+      setLoadingMessages(true);
       const convos = (await client?.conversations?.list()) || [];
-      Promise.all(
-        convos.map(async (convo) => {
-          updateMessageStore(convo);
+      Promise.all([
+        convos.map((convo) => {
+          fetchMostRecentMessage(convo, tempMessage, tempConversations);
         })
-      ).then(() => {
-        setLoading(false);
+      ]).then(() => {
+        setMessageMap(tempMessage);
+        setConversations(tempConversations);
+        setLoadingMessages(false);
       });
     };
+
     const streamConversations = async () => {
       const newStream = (await client?.conversations?.stream()) || [];
       setStream(newStream);
+      const newMessageMap = messageMap || new Map<string, Message>();
       for await (const convo of newStream) {
-        updateMessageStore(convo);
+        fetchMostRecentMessage(convo, newMessageMap, conversations);
+        setMessageMap(new Map(newMessageMap));
+        setConversations(new Map(conversations));
       }
     };
     listConversations();
@@ -90,9 +125,27 @@ const Messages: FC = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [client]);
 
-  if (!isFeatureEnabled('messages', currentProfile?.id)) {
+  if (!isMessagesEnabled) {
     return <Custom404 />;
   }
+
+  if (loadingMessages || loadingProfiles) {
+    return <PageLoading message="Loading messages" />;
+  }
+
+  if (profilesError) {
+    return <Custom500 />;
+  }
+
+  if (!profileMap || !messageMap) {
+    return null;
+  }
+
+  // TODO(elise): These aren't lining up yet!
+  const profileAddy = Array.from(profileMap.keys()).map((addy) => addy);
+  const messageAddy = Array.from(messageMap.keys()).map((addy) => addy);
+  console.log('PROFILE MAP ADDYS: ' + profileAddy);
+  console.log('MESSAGE MAP ADDYS: ' + messageAddy);
 
   return (
     <GridLayout>
@@ -110,12 +163,16 @@ const Messages: FC = () => {
             <div className="text-xs">All messages</div>
           </div>
           <div>
-            {Object.entries(conversations).map(([address, conversation], index: number) => (
-              <ConversationPreview
-                key={`${address}_${index}`}
-                address={address}
-                conversation={conversation}
-              />
+            {Object.entries(profileMap).map(([address, profile], index: number) => (
+              <div className="p-5" key={profile?.id}>
+                {messageMap.get(address) !== null ? (
+                  <MessagePreview
+                    key={`${address}_${index}`}
+                    profile={profile}
+                    message={messageMap.get(address)!}
+                  />
+                ) : null}
+              </div>
             ))}
           </div>
         </Card>
