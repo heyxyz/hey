7c2dde11a0e332721bbd0ff31f8f48ba797bd332
Bigint <69431456+bigint@users.noreply.github.com>
2022-10-08 11:55:49 +0530

    chore: rename type to field in getModule

--- src/lib/getModule.ts
+++ src/lib/getModule.ts
@@ -10,7 +10,7 @@ export const getModule = (
   name: string
 ): {
   name: string;
-  type: string;
+  field: string;
   helper: string;
 } => {
   switch (name) {
@@ -18,42 +18,42 @@ export const getModule = (
     case CollectModules.FeeCollectModule:
       return {
         name: 'Fee Collect',
-        type: 'collectModule',
+        field: 'collectModule',
         helper:
           'The Fee Collect Module allows for any follower to collect the associated publication provided they pay a fee set by the poster.'
       };
     case CollectModules.LimitedFeeCollectModule:
       return {
         name: 'Limited Fee Collect',
-        type: 'collectModule',
+        field: 'collectModule',
         helper:
           'The Limited Fee Collect Module allows for any follower to collect the associated publication, provided they pay a fee, up to a specific limit of mints.'
       };
     case CollectModules.TimedFeeCollectModule:
       return {
         name: 'Timed Fee Collect',
-        type: 'collectModule',
+        field: 'collectModule',
         helper:
           'The Timed Fee Collect Module allows for any follower to collect the associated publication, provided they pay a fee, up to a specific time limit. The present whitelisted Timed Fee Collect module only has a 24-hour time limit to reduce gas usage and optimize efficiency.'
       };
     case CollectModules.LimitedTimedFeeCollectModule:
       return {
         name: 'Limited Time Fee Collect',
-        type: 'collectModule',
+        field: 'collectModule',
         helper:
           'The Limited Timed Fee Collect Module allows for any follower to collect the associate publication, provided they pay a fee, up to a specific time limit and mint cap. It is essentially a combination of the Timed Fee Collect Module and the Limited Fee Collect Module.'
       };
     case CollectModules.FreeCollectModule:
       return {
         name: 'Free Collect',
-        type: 'collectModule',
+        field: 'collectModule',
         helper:
           'The Free Collect Module allows any profile to collect the publication this module is attached to.'
       };
     case CollectModules.RevertCollectModule:
       return {
         name: 'Revert Collect',
-        type: 'collectModule',
+        field: 'collectModule',
         helper:
           'The Revert Collect Module causes all collect actions on a given publication to fail, thus making the publication uncollectible.'
       };
@@ -62,7 +62,7 @@ export const getModule = (
     case FollowModules.FeeFollowModule:
       return {
         name: 'Fee Follow',
-        type: 'followModule',
+        field: 'followModule',
         helper:
           'The Fee Follow Module only allows addresses to follow a given profile, so long as they pay a fee specified by the profile owner. Users can set the currency and amount required to be paid.'
       };
@@ -71,12 +71,12 @@ export const getModule = (
     case ReferenceModules.FollowerOnlyReferenceModule:
       return {
         name: 'Follower Only Reference',
-        type: 'referenceModule',
+        field: 'referenceModule',
         helper:
           'The Follower Only Reference Module ensures that only a profile is only allowed to mirror or comment on content.'
       };
     default:
-      return { name: name, type: 'collectModule', helper: 'Others' };
+      return { name: name, field: 'collectModule', helper: 'Others' };
   }
 };
 
