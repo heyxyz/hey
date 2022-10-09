a56ac007ef64cfda6da5fc69e93ecf5a9ef0dbec
Bigint <69431456+bigint@users.noreply.github.com>
2022-10-09 09:38:13 +0530

    chore: remove excessive DOM usage

--- src/components/Publication/Type/Mirrored.tsx
+++ src/components/Publication/Type/Mirrored.tsx
@@ -12,19 +12,17 @@ const Mirrored: FC<Props> = ({ publication }) => {
   return (
     <div className="flex items-center pb-4 space-x-1 text-gray-500 text-[13px]">
       <SwitchHorizontalIcon className="w-4 h-4" />
-      <div className="space-x-1">
-        <Link href={`/u/${publication?.profile?.handle}`} className="max-w-xs truncate">
-          {publication?.profile?.name ? (
-            <b>{publication?.profile?.name}</b>
-          ) : (
-            <Slug slug={publication?.profile?.handle} prefix="@" />
-          )}
-        </Link>
-        <Link href={`/posts/${publication?.mirrorOf?.id}`}>
-          <span>mirrored the </span>
-          <b>{publication?.mirrorOf.__typename?.toLowerCase()}</b>
-        </Link>
-      </div>
+      <Link href={`/u/${publication?.profile?.handle}`} className="max-w-xs truncate">
+        {publication?.profile?.name ? (
+          <b>{publication?.profile?.name}</b>
+        ) : (
+          <Slug slug={publication?.profile?.handle} prefix="@" />
+        )}
+      </Link>
+      <Link href={`/posts/${publication?.mirrorOf?.id}`}>
+        <span>mirrored the </span>
+        <b>{publication?.mirrorOf.__typename?.toLowerCase()}</b>
+      </Link>
     </div>
   );
 };
