eaf46e45e738ff19471ae70cc77fc9dc85b99e51
Bigint <69431456+bigint@users.noreply.github.com>
2022-10-17 22:44:50 +0530

    feat: add bundlr upload support

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,15 +1,22 @@
+import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import type { Express, Request, Response } from 'express';
 import express from 'express';
+
+import upload from './upload';
 
 dotenv.config();
 
 const app: Express = express();
 const port = 3000;
 
+app.use(bodyParser.json({ type: 'application/json' }));
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Lenster API 🌸');
 });
+
+app.post('/upload', upload);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
