5632098f75192a245e0984db88f11cfc10cfd964
Elise Alix <elise@xmtp.com>
2022-10-18 09:00:33 -0400

    use simple query for address lookup

--- src/components/Messages/Message.tsx
+++ src/components/Messages/Message.tsx
@@ -8,7 +8,7 @@ import useMessagePreviews from '@components/utils/hooks/useMessagePreviews';
 import useSendMessage from '@components/utils/hooks/useSendMessage';
 import useStreamMessages from '@components/utils/hooks/useStreamMessages';
 import MetaTags from '@components/utils/MetaTags';
-import { ProfileDocument } from '@generated/types';
+import { ProfileAddressDocument } from '@generated/types';
 import isFeatureEnabled from '@lib/isFeatureEnabled';
 import { useRouter } from 'next/router';
 import type { FC } from 'react';
@@ -30,8 +30,8 @@ const Message: FC = () => {
     query: { profileId }
   } = useRouter();
 
-  const { data, loading, error } = useQuery(ProfileDocument, {
-    variables: { request: { profileId: profileId }, who: currentProfile?.id ?? null },
+  const { data, loading, error } = useQuery(ProfileAddressDocument, {
+    variables: { request: { profileId: profileId } },
     skip: !profileId
   });
 
@@ -65,7 +65,7 @@ const Message: FC = () => {
   if (!isFeatureEnabled('messages', currentProfile?.id)) {
     return <Custom404 />;
   }
-  const showLoading = !profile || !currentProfile || !address || !selectedConversation;
+
   if (error) {
     return <Custom500 />;
   }
@@ -73,6 +73,8 @@ const Message: FC = () => {
   if (!loading && !address) {
     return <Custom404 />;
   }
+
+  const showLoading = !profile || !currentProfile || !address || !selectedConversation;
 
   return (
     <GridLayout>
