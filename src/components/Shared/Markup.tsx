e85fddec083df2932618a0222c3d5bef6194df14
0xkeivin <65915812+0xkeivin@users.noreply.github.com>
2022-10-18 15:33:22 +0800

    feat: markdown links support (#854)
    Co-authored-by: bigint <69431456+bigint@users.noreply.github.com>

--- src/components/Shared/Markup.tsx
+++ src/components/Shared/Markup.tsx
@@ -1,6 +1,7 @@
 import { HashtagMatcher } from '@components/utils/matchers/HashtagMatcher';
 import { MDBoldMatcher } from '@components/utils/matchers/markdown/MDBoldMatcher';
 import { MDCodeMatcher } from '@components/utils/matchers/markdown/MDCodeMatcher';
+import { MDLinkMatcher } from '@components/utils/matchers/markdown/MDLinkMatcher';
 import { MDQuoteMatcher } from '@components/utils/matchers/markdown/MDQuoteMatcher';
 import { MDStrikeMatcher } from '@components/utils/matchers/markdown/MDStrikeMatcher';
 import { MentionMatcher } from '@components/utils/matchers/MentionMatcher';
@@ -30,6 +31,7 @@ const Markup: FC<Props> = ({ children, className = '' }) => {
         new MDStrikeMatcher('mdStrike'),
         new MDQuoteMatcher('mdQuote'),
         new MDCodeMatcher('mdCode'),
+        new MDLinkMatcher('mdLink'),
         new SpoilerMatcher('spoiler'),
         new UrlMatcher('url', { validateTLD: false })
       ]}
---
+++ src/components/utils/matchers/markdown/MDLinkMatcher.tsx
@@ -0,0 +1,29 @@
+import type { ChildrenNode } from 'interweave';
+import { Matcher } from 'interweave';
+import { v4 as uuid } from 'uuid';
+
+const createHyperlink = (href: string | undefined, title: string | undefined) => {
+  const keyId = '_' + href + '-' + uuid().slice(-7);
+  return (
+    <a key={keyId} href={href} target="_blank" rel="noopener/ noreferrer">
+      {title}
+    </a>
+  );
+};
+
+export class MDLinkMatcher extends Matcher {
+  replaceWith(children: ChildrenNode, props: any) {
+    return createHyperlink(props.href, props.title);
+  }
+
+  asTag(): string {
+    return 'a';
+  }
+
+  match(value: string) {
+    return this.doMatch(value, /\[(.*?)\]\((.*?)\)/u, (matches) => ({
+      href: matches[2],
+      title: matches[1]
+    }));
+  }
+}
