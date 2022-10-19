8f31eb0473ef36ba4871096950413639cf54e434
Nicholas Molnar <nicholas@xmtp.com>
2022-10-19 08:50:33 -0700

    Delete addMessages

--- src/lib/addMessages.ts
+++
@@ -1,9 +0,0 @@
-import type { Message } from '@xmtp/xmtp-js';
-
-import getUniqueMessages from './getUniqueMessages';
-
-const addMessages = (existing: Message[], newMessages: Message[]): Message[] => {
-  return getUniqueMessages(existing.concat(newMessages));
-};
-
-export default addMessages;
