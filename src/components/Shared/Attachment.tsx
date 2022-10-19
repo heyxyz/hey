a4d549be8a7152c3cfab2de0b7b5c5e8f0748dfc
Sasi <me@sasi.codes>
2022-10-19 17:08:14 +0530

    chore: remove unwanted div wrapper

--- src/components/Shared/Attachment.tsx
+++ src/components/Shared/Attachment.tsx
@@ -78,87 +78,85 @@ const Attachment: FC<Props> = ({ attachments, setAttachments }) => {
   };
 
   return (
-    <div>
-      <Menu as="div">
-        {({ open }) => (
-          <>
-            <Menu.Button className="rounded-full hover:bg-gray-300 hover:bg-opacity-20" aria-label="More">
-              {loading ? (
-                <Spinner size="sm" />
-              ) : (
-                <Tooltip placement="top" content="Media">
-                  <PhotographIcon className="w-5 h-5 text-brand" />
-                </Tooltip>
-              )}
-            </Menu.Button>
-            <Transition
-              show={open}
-              as={Fragment}
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
+    <Menu as="div">
+      {({ open }) => (
+        <>
+          <Menu.Button className="rounded-full hover:bg-gray-300 hover:bg-opacity-20" aria-label="More">
+            {loading ? (
+              <Spinner size="sm" />
+            ) : (
+              <Tooltip placement="top" content="Media">
+                <PhotographIcon className="w-5 h-5 text-brand" />
+              </Tooltip>
+            )}
+          </Menu.Button>
+          <Transition
+            show={open}
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items
+              static
+              className="absolute p-1.5 w-max bg-white rounded-lg border shadow-sm dark:bg-gray-900 focus:outline-none z-[5] dark:border-gray-700/80"
             >
-              <Menu.Items
-                static
-                className="absolute p-1.5 w-max bg-white rounded-lg border shadow-sm dark:bg-gray-900 focus:outline-none z-[5] dark:border-gray-700/80"
+              <label
+                className="flex py-2 px-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 gap-1 space-x-1 items-center cursor-pointer"
+                htmlFor={`image_${id}`}
               >
-                <label
-                  className="flex py-2 px-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 gap-1 space-x-1 items-center cursor-pointer"
-                  htmlFor={`image_${id}`}
-                >
-                  <PhotographIcon className="w-5 h-5 text-brand" />
-                  <span className="text-sm">Upload image(s)</span>
-                  <input
-                    id={`image_${id}`}
-                    type="file"
-                    multiple
-                    accept="image/*"
-                    className="hidden"
-                    onChange={handleAttachment}
-                    disabled={attachments.length >= 4}
-                  />
-                </label>
-                <label
-                  className="flex py-2 px-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 gap-1 space-x-1 items-center cursor-pointer"
-                  htmlFor={`video_${id}`}
-                >
-                  <VideoCameraIcon className="w-5 h-5 text-brand" />
-                  <span className="text-sm">Upload video</span>
-                  <input
-                    id={`video_${id}`}
-                    type="file"
-                    multiple
-                    accept="video/*"
-                    className="hidden"
-                    onChange={handleAttachment}
-                    disabled={attachments.length >= 4}
-                  />
-                </label>
-                <label
-                  className="flex py-2 px-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 gap-1 space-x-1 items-center cursor-pointer"
-                  htmlFor={`audio_${id}`}
-                >
-                  <MusicNoteIcon className="w-5 h-5 text-brand" />
-                  <span className="text-sm">Upload audio</span>
-                  <input
-                    id={`audio_${id}`}
-                    type="file"
-                    multiple
-                    accept={ALLOWED_AUDIO_TYPES.join(',')}
-                    className="hidden"
-                    onChange={handleAttachment}
-                    disabled={attachments.length >= 4}
-                  />
-                </label>
-              </Menu.Items>
-            </Transition>
-          </>
-        )}
-      </Menu>
-    </div>
+                <PhotographIcon className="w-5 h-5 text-brand" />
+                <span className="text-sm">Upload image(s)</span>
+                <input
+                  id={`image_${id}`}
+                  type="file"
+                  multiple
+                  accept="image/*"
+                  className="hidden"
+                  onChange={handleAttachment}
+                  disabled={attachments.length >= 4}
+                />
+              </label>
+              <label
+                className="flex py-2 px-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 gap-1 space-x-1 items-center cursor-pointer"
+                htmlFor={`video_${id}`}
+              >
+                <VideoCameraIcon className="w-5 h-5 text-brand" />
+                <span className="text-sm">Upload video</span>
+                <input
+                  id={`video_${id}`}
+                  type="file"
+                  multiple
+                  accept="video/*"
+                  className="hidden"
+                  onChange={handleAttachment}
+                  disabled={attachments.length >= 4}
+                />
+              </label>
+              <label
+                className="flex py-2 px-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 gap-1 space-x-1 items-center cursor-pointer"
+                htmlFor={`audio_${id}`}
+              >
+                <MusicNoteIcon className="w-5 h-5 text-brand" />
+                <span className="text-sm">Upload audio</span>
+                <input
+                  id={`audio_${id}`}
+                  type="file"
+                  multiple
+                  accept={ALLOWED_AUDIO_TYPES.join(',')}
+                  className="hidden"
+                  onChange={handleAttachment}
+                  disabled={attachments.length >= 4}
+                />
+              </label>
+            </Menu.Items>
+          </Transition>
+        </>
+      )}
+    </Menu>
   );
 };
 
