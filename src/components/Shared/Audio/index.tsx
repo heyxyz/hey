d2af868bbd59628fc865c2983d420e1b09aa7a6d
Sasi <me@sasi.codes>
2022-10-19 19:50:30 +0530

    chore: use brand icons

--- src/components/Shared/Audio/index.tsx
+++ src/components/Shared/Audio/index.tsx
@@ -1,5 +1,6 @@
 import type { LensterPublication } from '@generated/lenstertypes';
 import type { Attribute } from '@generated/types';
+import { PauseIcon, PlayIcon } from '@heroicons/react/solid';
 import getTimeFromSeconds from '@lib/formatSeconds';
 import getAttributeFromTrait from '@lib/getAttributeFromTrait';
 import getThumbnailUrl from '@lib/getThumbnailUrl';
@@ -97,42 +98,14 @@ const Audio: FC<Props> = ({ src, isNew = false, publication, txn }) => {
         />
         <div className="flex py-5 px-3 flex-col justify-between w-full">
           <div className="flex justify-between">
-            <div className="flex items-center space-x-3 w-full">
-              <div>
-                <button
-                  type="button"
-                  onClick={handlePlayPause}
-                  className="p-2.5 bg-gray-500 hover:bg-gray-600 text-white rounded-full"
-                >
-                  {playing ? (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      className="w-5 h-5"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M6.75 5.25a.75.75 0 01.75-.75H9a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H7.5a.75.75 0 01-.75-.75V5.25zm7.5 0A.75.75 0 0115 4.5h1.5a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H15a.75.75 0 01-.75-.75V5.25z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  ) : (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      className="w-5 h-5 pl-0.5"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  )}
-                </button>
-              </div>
+            <div className="flex items-center space-x-2.5 w-full">
+              <button type="button" onClick={handlePlayPause}>
+                {playing ? (
+                  <PauseIcon className="w-[50px] h-[50px] text-gray-500 hover:text-gray-600" />
+                ) : (
+                  <PlayIcon className="w-[50px] h-[50px] text-gray-500 hover:text-gray-600" />
+                )}
+              </button>
               <div className="w-full pr-3">
                 {isNew && !txn ? (
                   <div className="flex flex-col w-full">
