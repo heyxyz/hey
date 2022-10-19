3b9fa39d5bf705ed786ab988326ecad2689f8933
Sasi <me@sasi.codes>
2022-10-19 20:21:48 +0530

    fix: attachment menu

--- src/components/Shared/Attachment.tsx
+++ src/components/Shared/Attachment.tsx
@@ -4,6 +4,7 @@ import type { LensterAttachment } from '@generated/lenstertypes';
 import { Menu, Transition } from '@headlessui/react';
 import { MusicNoteIcon, PhotographIcon, VideoCameraIcon } from '@heroicons/react/outline';
 import uploadMediaToIPFS from '@lib/uploadMediaToIPFS';
+import clsx from 'clsx';
 import type { ChangeEvent, Dispatch, FC } from 'react';
 import { Fragment } from 'react';
 import { useId, useState } from 'react';
@@ -17,6 +18,7 @@ interface Props {
 
 const Attachment: FC<Props> = ({ attachments, setAttachments }) => {
   const [loading, setLoading] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
   const id = useId();
 
   const hasVideos = (files: any) => {
@@ -54,6 +56,7 @@ const Attachment: FC<Props> = ({ attachments, setAttachments }) => {
 
   const handleAttachment = async (evt: ChangeEvent<HTMLInputElement>) => {
     evt.preventDefault();
+    setShowMenu(false);
     setLoading(true);
 
     try {
@@ -79,83 +82,98 @@ const Attachment: FC<Props> = ({ attachments, setAttachments }) => {
 
   return (
     <Menu as="div">
-      {({ open }) => (
-        <>
-          <Menu.Button className="rounded-full hover:bg-gray-300 hover:bg-opacity-20" aria-label="More">
-            {loading ? (
-              <Spinner size="sm" />
-            ) : (
-              <Tooltip placement="top" content="Media">
-                <PhotographIcon className="w-5 h-5 text-brand" />
-              </Tooltip>
-            )}
-          </Menu.Button>
-          <Transition
-            show={open}
-            as={Fragment}
-            enter="transition ease-out duration-100"
-            enterFrom="transform opacity-0 scale-95"
-            enterTo="transform opacity-100 scale-100"
-            leave="transition ease-in duration-75"
-            leaveFrom="transform opacity-100 scale-100"
-            leaveTo="transform opacity-0 scale-95"
+      <Menu.Button
+        onClick={() => setShowMenu(!showMenu)}
+        className="rounded-full hover:bg-gray-300 hover:bg-opacity-20"
+        aria-label="More"
+      >
+        {loading ? (
+          <Spinner size="sm" />
+        ) : (
+          <Tooltip placement="top" content="Media">
+            <PhotographIcon className="w-5 h-5 text-brand" />
+          </Tooltip>
+        )}
+      </Menu.Button>
+      <Transition
+        show={showMenu}
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items
+          static
+          className="absolute w-max bg-white rounded-lg border shadow-sm dark:bg-gray-900 focus:outline-none z-[5] dark:border-gray-700/80"
+        >
+          <Menu.Item
+            as="div"
+            className={({ active }: { active: boolean }) => clsx({ 'dropdown-active': active }, 'menu-item')}
           >
-            <Menu.Items
-              static
-              className="absolute p-1.5 w-max bg-white rounded-lg border shadow-sm dark:bg-gray-900 focus:outline-none z-[5] dark:border-gray-700/80"
+            <label
+              className="flex rounded-lg gap-1 space-x-1 items-center cursor-pointer"
+              htmlFor={`image_${id}`}
             >
-              <label
-                className="flex py-2 px-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 gap-1 space-x-1 items-center cursor-pointer"
-                htmlFor={`image_${id}`}
-              >
-                <PhotographIcon className="w-5 h-5 text-brand" />
-                <span className="text-sm">Upload image(s)</span>
-                <input
-                  id={`image_${id}`}
-                  type="file"
-                  multiple
-                  accept="image/*"
-                  className="hidden"
-                  onChange={handleAttachment}
-                  disabled={attachments.length >= 4}
-                />
-              </label>
-              <label
-                className="flex py-2 px-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 gap-1 space-x-1 items-center cursor-pointer"
-                htmlFor={`video_${id}`}
-              >
-                <VideoCameraIcon className="w-5 h-5 text-brand" />
-                <span className="text-sm">Upload video</span>
-                <input
-                  id={`video_${id}`}
-                  type="file"
-                  multiple
-                  accept="video/*"
-                  className="hidden"
-                  onChange={handleAttachment}
-                  disabled={attachments.length >= 4}
-                />
-              </label>
-              <label
-                className="flex py-2 px-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 gap-1 space-x-1 items-center cursor-pointer"
-                htmlFor={`audio_${id}`}
-              >
-                <MusicNoteIcon className="w-5 h-5 text-brand" />
-                <span className="text-sm">Upload audio</span>
-                <input
-                  id={`audio_${id}`}
-                  type="file"
-                  multiple
-                  accept={ALLOWED_AUDIO_TYPES.join(',')}
-                  className="hidden"
-                  onChange={handleAttachment}
-                  disabled={attachments.length >= 4}
-                />
-              </label>
-            </Menu.Items>
-          </Transition>
-        </>
-      )}
+              <PhotographIcon className="w-4 h-4 text-brand" />
+              <span className="text-sm">Upload image(s)</span>
+              <input
+                id={`image_${id}`}
+                type="file"
+                multiple
+                accept="image/*"
+                className="hidden"
+                onChange={handleAttachment}
+                disabled={attachments.length >= 4}
+              />
+            </label>
+          </Menu.Item>
+          <Menu.Item
+            as="div"
+            className={({ active }: { active: boolean }) => clsx({ 'dropdown-active': active }, 'menu-item')}
+          >
+            <label
+              className="flex rounded-lg gap-1 space-x-1 items-center cursor-pointer"
+              htmlFor={`video_${id}`}
+            >
+              <VideoCameraIcon className="w-4 h-4 text-brand" />
+              <span className="text-sm">Upload video</span>
+              <input
+                id={`video_${id}`}
+                type="file"
+                multiple
+                accept="video/*"
+                className="hidden"
+                onChange={handleAttachment}
+                disabled={attachments.length >= 4}
+              />
+            </label>
+          </Menu.Item>
+          <Menu.Item
+            as="div"
+            className={({ active }: { active: boolean }) => clsx({ 'dropdown-active': active }, 'menu-item')}
+          >
+            <label
+              className="flex rounded-lg gap-1 space-x-1 items-center cursor-pointer"
+              htmlFor={`audio_${id}`}
+            >
+              <MusicNoteIcon className="w-4 h-4 text-brand" />
+              <span className="text-sm">Upload audio</span>
+              <input
+                id={`audio_${id}`}
+                type="file"
+                multiple
+                accept={ALLOWED_AUDIO_TYPES.join(',')}
+                className="hidden"
+                onChange={handleAttachment}
+                disabled={attachments.length >= 4}
+              />
+            </label>
+          </Menu.Item>
+        </Menu.Items>
+      </Transition>
     </Menu>
   );
 };
