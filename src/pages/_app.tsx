ee5b9b152547d06f4566994392cc1b724b7e60e4
Bigint <69431456+bigint@users.noreply.github.com>
2022-10-20 10:52:04 +0530

    chore: remove lazy load

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,16 @@
 import '../styles.css';
 
 import Layout from '@components/Layout';
-import Loading from '@components/Loading';
 import Providers from '@components/Providers';
 import type { AppProps } from 'next/app';
-import { Suspense } from 'react';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <Suspense fallback={<Loading />}>
-      <Providers>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </Providers>
-    </Suspense>
+    <Providers>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Providers>
   );
 };
 
