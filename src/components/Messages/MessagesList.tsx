264692c660229a673c335ac6cff01e807552fa0a
Bhavya Mehta <bhavya.y.mehta@gmail.com>
2022-10-21 16:03:56 +0200

    spacing in message list

--- src/components/Messages/MessagesList.tsx
+++ src/components/Messages/MessagesList.tsx
@@ -74,7 +74,7 @@ const DateDividerBorder: FC<Props> = ({ children }) => (
 );
 
 const DateDivider: FC<{ date?: Date }> = ({ date }) => (
-  <div className="flex align-items-center items-center p-4 pl-2">
+  <div className="flex align-items-center items-center p-4 pt-0 pl-2">
     <DateDividerBorder>
       <span className="mx-11 flex-none text-gray-300 text-sm font-bold">{formatDate(date)}</span>
     </DateDividerBorder>
