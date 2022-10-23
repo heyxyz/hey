be7305d8ce9153275c296e7a3a1504de594ab73d
Sasi <me@sasi.codes>
2022-10-23 13:58:31 +0530

    chore: update new infinite scroll

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -11,8 +11,8 @@ import { TimelineDocument } from '@generated/types';
 import { CollectionIcon } from '@heroicons/react/outline';
 import type { FC } from 'react';
 import { useEffect } from 'react';
-import InfiniteScroll from 'react-infinite-scroller';
-import { PAGINATION_THRESHOLD } from 'src/constants';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { SCROLL_THRESHOLD } from 'src/constants';
 import { useAppStore } from 'src/store/app';
 import { useTimelinePersistStore } from 'src/store/timeline';
 import { useTransactionPersistStore } from 'src/store/transaction';
@@ -86,10 +86,10 @@ const Timeline: FC = () => {
 
   return (
     <InfiniteScroll
-      pageStart={0}
-      threshold={PAGINATION_THRESHOLD}
+      dataLength={publications?.length ?? 0}
+      scrollThreshold={SCROLL_THRESHOLD}
       hasMore={hasMore}
-      loadMore={loadMore}
+      next={loadMore}
       loader={<InfiniteLoader />}
     >
       <Card className="divide-y-[1px] dark:divide-gray-700/80">
