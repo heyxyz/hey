fc1357edb0c5eb91d1deb98deb413d0830e16f13
Sasi <me@sasi.codes>
2022-10-25 15:20:50 +0530

    chore: replace icons with label

--- src/components/Timeline/Publication/EventType/Combined.tsx
+++ src/components/Timeline/Publication/EventType/Combined.tsx
@@ -1,5 +1,4 @@
 import type { FeedItem } from '@generated/types';
-import { ChatAlt2Icon, CollectionIcon, HeartIcon, SwitchHorizontalIcon } from '@heroicons/react/outline';
 import type { FC } from 'react';
 
 import ProfileCircles from './ProfileCircles';
@@ -9,11 +8,23 @@ interface Props {
 }
 
 const Combined: FC<Props> = ({ feedItem }) => {
+  const mirrors = feedItem.mirrors;
+  const collects = feedItem.collects;
+  const reactions = feedItem.reactions;
+  const comments = feedItem.comments ?? [];
+
+  const mirrorsLength = mirrors.length;
+  const collectsLength = collects.length;
+  const reactionsLength = reactions.length;
+  const commentsLength = comments?.length ?? 0;
+
+  const getReactionsLength = () => {
+    return [mirrorsLength, collectsLength, reactionsLength, commentsLength ?? 0].filter((n) => n > 0).length;
+  };
+
+  const totalActions = getReactionsLength();
+
   const getAllProfiles = () => {
-    const mirrors = feedItem.mirrors;
-    const collects = feedItem.collects;
-    const reactions = feedItem.reactions;
-    const comments = feedItem.comments ?? [];
     let profiles = [...mirrors, ...collects, ...reactions, ...comments].map((event) => event.profile);
     profiles = profiles.filter(
       (profile, index, self) => index === self.findIndex((t) => t.id === profile.id)
@@ -24,10 +35,29 @@ const Combined: FC<Props> = ({ feedItem }) => {
   return (
     <div className="flex items-center pb-4 space-x-1 text-gray-500 text-[13px]">
       <div className="flex items-center space-x-1">
-        {feedItem.mirrors.length ? <SwitchHorizontalIcon className="w-4 h-4" /> : null}
-        {feedItem.comments?.length ? <ChatAlt2Icon className="w-4 h-4" /> : null}
-        {feedItem.collects?.length ? <CollectionIcon className="w-4 h-4" /> : null}
-        {feedItem.reactions?.length ? <HeartIcon className="w-4 h-4" /> : null}
+        {mirrorsLength ? (
+          <span>Mirrored{totalActions < 3 ? (totalActions !== 1 ? ' and ' : '') : ', '}</span>
+        ) : null}
+        {commentsLength ? (
+          <span>
+            Commented
+            {totalActions < 3
+              ? collectsLength && reactionsLength
+                ? ' and '
+                : !mirrorsLength && totalActions !== 1
+                ? ' and '
+                : ''
+              : ', '}
+            {totalActions >= 3 && (!collectsLength || !reactionsLength) ? ' and ' : ''}
+          </span>
+        ) : null}
+        {collectsLength ? (
+          <span>
+            Collected
+            {totalActions >= 3 && reactionsLength ? ' and ' : reactionsLength ? ' and ' : ''}
+          </span>
+        ) : null}
+        {reactionsLength ? <span>Reacted</span> : null}
       </div>
       <ProfileCircles profiles={getAllProfiles()} context="by" />
     </div>
