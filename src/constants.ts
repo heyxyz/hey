37b0935f5fe2ec7dc452337ce6de259b74b11edf
sasi.eth <me@sasi.codes>
2022-10-26 22:58:23 +0530

    chore: support all video/audio formats (#940)

--- src/constants.ts
+++ src/constants.ts
@@ -87,8 +87,17 @@ export const HANDLE_SANITIZE_REGEX = /[^\d .A-Za-z]/g;
 
 // Utils
 export const ALLOWED_IMAGE_TYPES = ['image/jpeg', 'image/png', 'image/webp', 'image/gif'];
-export const ALLOWED_AUDIO_TYPES = ['audio/wav', 'audio/mpeg', 'audio/ogg', 'audio/mp4'];
-export const ALLOWED_MEDIA_TYPES = ['video/mp4', ...ALLOWED_IMAGE_TYPES, ...ALLOWED_AUDIO_TYPES];
+export const ALLOWED_AUDIO_TYPES = [
+  'audio/mpeg',
+  'audio/wav',
+  'audio/mp4',
+  'audio/aac',
+  'audio/ogg',
+  'audio/webm',
+  'audio/flac'
+];
+export const ALLOWED_VIDEO_TYPES = ['video/mp4', 'video/mpeg', 'video/ogg', 'video/webm', 'video/quicktime'];
+export const ALLOWED_MEDIA_TYPES = [...ALLOWED_VIDEO_TYPES, ...ALLOWED_IMAGE_TYPES, ...ALLOWED_AUDIO_TYPES];
 
 // Bundlr
 export const BUNDLR_CURRENCY = 'matic';
