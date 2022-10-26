28cde34e388faedb7369c9511529c849772f2b46
Bigint <69431456+bigint@users.noreply.github.com>
2022-10-26 12:47:49 +0530

    fix: dev error

--- next.config.js
+++ next.config.js
@@ -11,7 +11,6 @@ module.exports = withSentryConfig(
     reactStrictMode: false,
     trailingSlash: false,
     experimental: {
-      appDir: true,
       scrollRestoration: true,
       newNextLinkBehavior: true
     },
