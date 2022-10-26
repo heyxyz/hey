29df923bfb55857f9b8d333357d6216237867209
Sasi <me@sasi.codes>
2022-10-26 11:50:02 +0530

    chore: supported video types

--- src/constants.ts
+++ src/constants.ts
@@ -87,7 +87,15 @@ export const HANDLE_SANITIZE_REGEX = /[^\d .A-Za-z]/g;
 
 // Utils
 export const ALLOWED_IMAGE_TYPES = ['image/jpeg', 'image/png', 'image/webp', 'image/gif'];
-export const ALLOWED_AUDIO_TYPES = ['audio/wav', 'audio/mpeg', 'audio/ogg', 'audio/mp4'];
+export const ALLOWED_AUDIO_TYPES = [
+  'audio/mpeg',
+  'audio/wav',
+  'audio/mp4',
+  'audio/aac',
+  'audio/ogg',
+  'audio/webm',
+  'audio/flac'
+];
 export const ALLOWED_VIDEO_TYPES = ['video/mp4', 'video/mpeg', 'video/ogg', 'video/webm', 'video/quicktime'];
 export const ALLOWED_MEDIA_TYPES = [...ALLOWED_VIDEO_TYPES, ...ALLOWED_IMAGE_TYPES, ...ALLOWED_AUDIO_TYPES];
 
