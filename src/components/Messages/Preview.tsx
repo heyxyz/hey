4156a17e73dcfbb2a8f8d5cf2a32c44f9a491f8d
Elise Alix <elise@xmtp.com>
2022-10-27 11:47:37 -0400

    ellipsize message content and profile name in list

--- src/components/Messages/Preview.tsx
+++ src/components/Messages/Preview.tsx
@@ -9,7 +9,6 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 import { useRouter } from 'next/router';
 import type { FC } from 'react';
 import React from 'react';
-import { MESSAGE_PREVIEW_LENGTH } from 'src/constants';
 
 dayjs.extend(relativeTime);
 
@@ -28,31 +27,29 @@ const Preview: FC<Props> = ({ profile, message, conversationKey }) => {
 
   return (
     <div className="hover:bg-gray-100 py-3 cursor-pointer" onClick={() => onConversationSelected(profile.id)}>
-      <div className="flex justify-between space-x-1.5 px-5 flex-wrap">
-        <div className="flex items-center space-x-3">
-          <img
-            src={getAvatar(profile)}
-            loading="lazy"
-            className="w-10 h-10 bg-gray-200 rounded-full border dark:border-gray-700/80"
-            height={40}
-            width={40}
-            alt={profile?.handle}
-          />
-          <div>
-            <div className="flex gap-1 items-center max-w-sm truncate">
+      <div className="flex justify-between space-x-3 px-5">
+        <img
+          src={getAvatar(profile)}
+          loading="lazy"
+          className="w-10 h-10 bg-gray-200 rounded-full border dark:border-gray-700/80"
+          height={40}
+          width={40}
+          alt={profile?.handle}
+        />
+        <div className="w-full">
+          <div className="flex w-full justify-between space-x-1">
+            <div className="flex gap-1 items-center max-w-sm line-clamp-1">
               <div className={clsx('text-md')}>{profile?.name ?? profile.handle}</div>
               {isVerified(profile?.id) && <BadgeCheckIcon className="w-4 h-4 text-brand" />}
             </div>
-            <span className="text-sm text-gray-500 line-clamp-1">
-              {message.content && message.content.length > MESSAGE_PREVIEW_LENGTH
-                ? message.content.substring(0, MESSAGE_PREVIEW_LENGTH)
-                : message.content}
-            </span>
+            {message.sent && (
+              <span className="min-w-fit pt-0.5 text-xs text-gray-500">
+                {dayjs(new Date(message.sent)).fromNow()}
+              </span>
+            )}
           </div>
+          <span className="text-sm text-gray-500 line-clamp-1">{message.content}</span>
         </div>
-        {message.sent && (
-          <span className="min-w-fit text-xs text-gray-500">{dayjs(new Date(message.sent)).fromNow()}</span>
-        )}
       </div>
     </div>
   );
