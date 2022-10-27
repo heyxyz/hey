363bb90e8181061c1e2646901e03fd58d7d474ee
Bigint <69431456+bigint@users.noreply.github.com>
2022-10-27 19:20:06 +0530

    chore: remove lazy loading from _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,18 @@
 import '../styles.css';
 
-import Loading from '@components/Loading';
+import Layout from '@components/Layout';
+import Providers from '@components/Providers';
 import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app';
-import { lazy, Suspense } from 'react';
-const Providers = lazy(() => import('@components/Providers'));
-const Layout = lazy(() => import('@components/Layout'));
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <Suspense fallback={<Loading />}>
-      <Providers>
-        <Layout>
-          <Component {...pageProps} />
-          <Analytics />
-        </Layout>
-      </Providers>
-    </Suspense>
+    <Providers>
+      <Layout>
+        <Component {...pageProps} />
+        <Analytics />
+      </Layout>
+    </Providers>
   );
 };
 
