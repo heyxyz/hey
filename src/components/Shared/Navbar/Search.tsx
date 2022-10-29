c9728a5c34f82ffb02b240148abd6f43f9db754e
Bigint <69431456+bigint@users.noreply.github.com>
2022-10-29 08:37:52 +0530

    fix: search component to show pointer

--- src/components/Shared/Navbar/Search.tsx
+++ src/components/Shared/Navbar/Search.tsx
@@ -90,17 +90,17 @@ const Search: FC<Props> = ({ hideDropdown = false, onProfileSelected, placeholde
             ) : (
               <>
                 {profiles.map((profile: Profile) => (
-                  <div key={profile?.handle} className="py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-800">
-                    <div
-                      onClick={() => {
-                        if (onProfileSelected) {
-                          onProfileSelected(profile);
-                        }
-                        setSearchText('');
-                      }}
-                    >
-                      <UserProfile linkToProfile={!onProfileSelected} profile={profile} />
-                    </div>
+                  <div
+                    key={profile?.handle}
+                    className="py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-800 cursor-pointer"
+                    onClick={() => {
+                      if (onProfileSelected) {
+                        onProfileSelected(profile);
+                      }
+                      setSearchText('');
+                    }}
+                  >
+                    <UserProfile linkToProfile={!onProfileSelected} profile={profile} />
                   </div>
                 ))}
                 {profiles.length === 0 && <div className="py-2 px-4">No matching users</div>}
