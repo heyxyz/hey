4c700364690cc2803daa06d4fcdeacdcb9ee2da7
Bigint <69431456+bigint@users.noreply.github.com>
2022-10-30 18:05:26 +0530

    chore: remove withsentry

--- src/pages/api/tunnel.ts
+++ src/pages/api/tunnel.ts
@@ -1,4 +1,3 @@
-import { withSentry } from '@sentry/nextjs';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import * as url from 'url';
 
@@ -10,12 +9,12 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const { host, path } = url.parse(header.dsn);
     const projectId = path?.endsWith('/') ? path.slice(0, -1) : path;
     const ingestUrl = `https://${host}/api${projectId}/envelope/`;
-    const response = await fetch(ingestUrl, { method: 'POST', body: envelope });
+    await fetch(ingestUrl, { method: 'POST', body: envelope });
 
-    return res.status(200).json({ status: response.json() });
+    return res.status(200).json({ status: 'ok' });
   } catch {
     return res.status(400).json({ status: 'invalid request' });
   }
 };
 
-export default withSentry(handler);
+export default handler;
