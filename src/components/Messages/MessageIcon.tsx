bca14ef14712abf5d6c8452e93dc48e3cfd23f2f
Elise Alix <elise@xmtp.com>
2022-11-02 02:59:24 -0400

    fix: resolve issue where badge would pop back up on messages (#1062)

--- src/components/Messages/MessageIcon.tsx
+++ src/components/Messages/MessageIcon.tsx
@@ -4,7 +4,6 @@ import conversationMatchesProfile from '@lib/conversationMatchesProfile';
 import type { DecodedMessage } from '@xmtp/xmtp-js';
 import { fromNanoString, SortDirection } from '@xmtp/xmtp-js';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import type { FC } from 'react';
 import { useEffect } from 'react';
 import { useAppStore } from 'src/store/app';
@@ -17,7 +16,6 @@ const MessageIcon: FC = () => {
   const viewedMessagesAtNs = useMessagePersistStore((state) => state.viewedMessagesAtNs);
   const showMessagesBadge = useMessagePersistStore((state) => state.showMessagesBadge);
   const setShowMessagesBadge = useMessagePersistStore((state) => state.setShowMessagesBadge);
-  const { pathname } = useRouter();
 
   const shouldShowBadge = (viewedAt: string | undefined, messageSentAt: Date | undefined): boolean => {
     if (!messageSentAt) {
@@ -66,49 +64,48 @@ const MessageIcon: FC = () => {
       }
     };
 
-    const streamAllMessages = async () => {
+    // For v1 badging, only badge when not already viewing messages. Once we have
+    // badging per-conversation, we can remove this.
+    const newMessageValidator = (profileId: string): boolean => {
+      return !window.location.pathname.startsWith('/messages') && currentProfile.id === profileId;
+    };
+
+    const streamAllMessages = async (messageValidator: (profileId: string) => boolean) => {
       messageStream = await cachedClient.conversations.streamAllMessages();
 
       for await (const message of messageStream) {
-        if (pathname.startsWith('/messages')) {
-          // For v1 badging, only badge when not already viewing messages. Once we have
-          // badging per-conversation, we can remove this.
-          return;
-        }
-        const conversationId = message.conversation.context?.conversationId;
-        const isFromPeer = currentProfile.ownedBy !== message.senderAddress;
-        if (isFromPeer && conversationId && matcherRegex.test(conversationId)) {
-          const showBadge = shouldShowBadge(viewedMessagesAtNs.get(currentProfile.id), message.sent);
-          showMessagesBadge.set(currentProfile.id, showBadge);
-          setShowMessagesBadge(new Map(showMessagesBadge));
+        if (messageValidator(currentProfile.id)) {
+          const conversationId = message.conversation.context?.conversationId;
+          const isFromPeer = currentProfile.ownedBy !== message.senderAddress;
+          if (isFromPeer && conversationId && matcherRegex.test(conversationId)) {
+            const showBadge = shouldShowBadge(viewedMessagesAtNs.get(currentProfile.id), message.sent);
+            showMessagesBadge.set(currentProfile.id, showBadge);
+            setShowMessagesBadge(new Map(showMessagesBadge));
+          }
         }
       }
     };
 
     fetchShowBadge();
-    streamAllMessages();
+    streamAllMessages(newMessageValidator);
 
     return () => {
       closeMessageStream();
     };
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cachedClient, currentProfile?.id, pathname]);
-
-  if (!currentProfile) {
-    return null;
-  }
+  }, [cachedClient, currentProfile?.id]);
 
   return (
     <Link
       href="/messages"
       className="flex items-start justify-center rounded-md hover:bg-gray-300 p-1 hover:bg-opacity-20 min-w-[40px]"
       onClick={() => {
-        clearMessagesBadge(currentProfile.id);
+        currentProfile && clearMessagesBadge(currentProfile.id);
       }}
     >
       <MailIcon className="w-5 h-5 sm:w-6 sm:h-6" />
-      {showMessagesBadge.get(currentProfile.id) && <span className="w-2 h-2 bg-red-500 rounded-full" />}
+      {showMessagesBadge.get(currentProfile?.id) && <span className="w-2 h-2 bg-red-500 rounded-full" />}
     </Link>
   );
 };
