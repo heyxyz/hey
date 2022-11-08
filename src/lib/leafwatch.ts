2eaf704d0da4e71ca240dab7769be00e8edaa81e
bigint <69431456+bigint@users.noreply.github.com>
2022-11-08 11:12:20 +0530

    chore: add useragent parser

--- src/lib/leafwatch.ts
+++ src/lib/leafwatch.ts
@@ -1,9 +1,6 @@
 import axios from 'axios';
 import { DATADOG_TOKEN, IS_PRODUCTION, LEAFWATCH_HOST } from 'src/constants';
-import parser from 'ua-parser-js';
 import { v4 as uuid } from 'uuid';
-
-import getUserLocale from './getUserLocale';
 
 const enabled = DATADOG_TOKEN && IS_PRODUCTION;
 const isBrowser = typeof window !== 'undefined';
@@ -13,7 +10,6 @@ const isBrowser = typeof window !== 'undefined';
  */
 export const Leafwatch = {
   track: (name: string, options?: Record<string, any>) => {
-    const ua = parser(navigator.userAgent);
     const { state } = JSON.parse(
       localStorage.getItem('lenster.store') || JSON.stringify({ state: { profileId: null } })
     );
@@ -33,12 +29,9 @@ export const Leafwatch = {
           url: location.href,
           referrer: document.referrer,
           sha: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA,
-          browser: {
-            name: ua.browser.name,
-            version: ua.browser.version,
-            language: getUserLocale()
-          },
-          device: { os: ua.os.name }
+          http: {
+            useragent: navigator.userAgent
+          }
         }
       }).catch(() => {
         console.error('Error while sending analytics event to Leafwatch');
