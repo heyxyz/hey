7538fe53c92959ec034fb0d6e5fee67fb370130c
bigint <69431456+bigint@users.noreply.github.com>
2022-11-11 09:54:35 +0530

    feat: add more states

--- src/store/access-settings.ts
+++ src/store/access-settings.ts
@@ -3,14 +3,24 @@ import create from 'zustand';
 interface AccessSettingsState {
   restricted: boolean;
   setRestricted: (restricted: boolean) => void;
+  collectToView: boolean;
+  setCollectToView: (collectToView: boolean) => void;
+  followToView: boolean;
+  setFollowToView: (followToView: boolean) => void;
   reset: () => void;
 }
 
 export const useAccessSettingsStore = create<AccessSettingsState>((set) => ({
   restricted: false,
   setRestricted: (restricted) => set(() => ({ restricted })),
+  collectToView: false,
+  setCollectToView: (collectToView) => set(() => ({ collectToView })),
+  followToView: false,
+  setFollowToView: (followToView) => set(() => ({ followToView })),
   reset: () =>
     set(() => ({
-      restricted: false
+      restricted: false,
+      collectToView: false,
+      followToView: false
     }))
 }));
