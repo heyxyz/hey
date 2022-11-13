b53ff10d551a5586fd555fe0808a6ac74fbebfac
harish-sethuraman <ssharishkumar@gmail.com>
2022-11-14 00:10:14 +0530

    chore: remove unwanted code

--- src/components/Shared/Markdown/index.tsx
+++ src/components/Shared/Markdown/index.tsx
@@ -1,6 +1,5 @@
 import LexicalAutoLinkPlugin from '@components/Shared/Lexical/Plugins/AutoLinkPlugin';
 import type { LensterPublication } from '@generated/lenstertypes';
-import type { ElementTransformer } from '@lexical/markdown';
 import { $convertFromMarkdownString, TEXT_FORMAT_TRANSFORMERS } from '@lexical/markdown';
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import { ContentEditable } from '@lexical/react/LexicalContentEditable';
@@ -14,16 +13,6 @@ import { ERROR_MESSAGE } from 'src/constants';
 import MentionsPlugin, { AtSignMentionsRegex } from '../../Shared/Lexical/Plugins/AtMentionsPlugin';
 import { $createMentionNode, MentionNode } from '../Lexical/Nodes/MentionsNode';
 
-const createBlockNode = (
-  createNode: (match: Array<string>) => ElementNode & MentionNode
-): ElementTransformer['replace'] => {
-  return (parentNode, children, match) => {
-    const node = createNode(match);
-    node.append(...children);
-    parentNode.replace(node);
-    node.select(0, 0);
-  };
-};
 const TRANSFORMERS = [
   ...TEXT_FORMAT_TRANSFORMERS,
   {
