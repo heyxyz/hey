f842b5c5529fa2f54fdde9546ed6e38beedbaaae
bigint <69431456+bigint@users.noreply.github.com>
2022-11-18 08:38:52 +0530

    chore: fetch data from api instead of arweave

--- src/components/Shared/Status.tsx
+++ src/components/Shared/Status.tsx
@@ -9,7 +9,7 @@ import type { CreatePublicSetProfileMetadataUriRequest } from '@generated/types'
 import {
   useCreateSetProfileMetadataTypedDataMutation,
   useCreateSetProfileMetadataViaDispatcherMutation,
-  useProfileQuery
+  useProfileSettingsQuery
 } from '@generated/types';
 import { PencilIcon } from '@heroicons/react/outline';
 import getAttribute from '@lib/getAttribute';
@@ -17,12 +17,12 @@ import getSignature from '@lib/getSignature';
 import onError from '@lib/onError';
 import splitSignature from '@lib/splitSignature';
 import uploadToArweave from '@lib/uploadToArweave';
-import axios from 'axios';
 import type { FC } from 'react';
 import { useState } from 'react';
 import toast from 'react-hot-toast';
-import { LENS_PERIPHERY, RELAY_ON, SIGN_WALLET } from 'src/constants';
+import { APP_NAME, LENS_PERIPHERY, RELAY_ON, SIGN_WALLET } from 'src/constants';
 import { useAppStore } from 'src/store/app';
+import { v4 as uuid } from 'uuid';
 import { useContractWrite, useSignTypedData } from 'wagmi';
 
 import EmojiPicker from './EmojiPicker';
@@ -32,35 +32,17 @@ const Status: FC = () => {
   const currentProfile = useAppStore((state) => state.currentProfile);
   const userSigNonce = useAppStore((state) => state.userSigNonce);
   const setUserSigNonce = useAppStore((state) => state.setUserSigNonce);
-  const [loading, setLoading] = useState(true);
-  const [metadata, setMetadata] = useState<any>(null);
-  const [error, setError] = useState(null);
   const [isUploading, setIsUploading] = useState(false);
   const [emoji, setEmoji] = useState<string | null>(null);
   const [status, setStatus] = useState<string | null>(null);
 
-  useProfileQuery({
-    variables: { request: { profileId: currentProfile?.id }, who: currentProfile?.id ?? null },
+  const { data, loading, error } = useProfileSettingsQuery({
+    variables: { request: { profileId: currentProfile?.id } },
     skip: !currentProfile?.id,
-    onCompleted: async ({ profile }) => {
-      console.log(profile?.metadata);
-      try {
-        const metadata = await axios({
-          method: 'GET',
-          url: profile?.metadata
-        });
-        setMetadata(metadata?.data);
-        setStatus(getAttribute(metadata?.data?.attributes, 'statusMessage'));
-        setEmoji(getAttribute(metadata?.data?.attributes, 'statusEmoji'));
-      } catch (error: any) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    },
-    onError: (error: any) => {
-      setError(error);
-      setLoading(false);
+    onCompleted: (data) => {
+      const profile = data?.profile;
+      setEmoji(getAttribute(profile?.attributes, 'statusEmoji'));
+      setStatus(getAttribute(profile?.attributes, 'statusMessage'));
     }
   });
 
@@ -134,15 +116,52 @@ const Status: FC = () => {
     }
   };
 
+  const profile = data?.profile;
+
   const editStatus = async () => {
     if (!currentProfile) {
       return toast.error(SIGN_WALLET);
     }
-
     setIsUploading(true);
-    metadata.attributes.push({ traitType: 'string', key: 'statusEmoji', value: emoji });
-    metadata.attributes.push({ traitType: 'string', key: 'statusMessage', value: status });
-    const id = await uploadToArweave(metadata).finally(() => setIsUploading(false));
+    const id = await uploadToArweave({
+      name: profile?.name ?? '',
+      bio: profile?.bio ?? '',
+      cover_picture:
+        profile?.coverPicture?.__typename === 'MediaSet' ? profile?.coverPicture?.original?.url ?? '' : '',
+      attributes: [
+        {
+          traitType: 'string',
+          key: 'location',
+          value: getAttribute(profile?.attributes, 'location')
+        },
+        {
+          traitType: 'string',
+          key: 'website',
+          value: getAttribute(profile?.attributes, 'website')
+        },
+        {
+          traitType: 'string',
+          key: 'twitter',
+          value: getAttribute(profile?.attributes, 'twitter')?.replace('https://twitter.com/', '')
+        },
+        {
+          traitType: 'boolean',
+          key: 'hasPrideLogo',
+          value: getAttribute(profile?.attributes, 'hasPrideLogo')
+        },
+        { traitType: 'string', key: 'statusEmoji', value: emoji },
+        { traitType: 'string', key: 'statusMessage', value: status },
+        {
+          traitType: 'string',
+          key: 'app',
+          value: APP_NAME
+        }
+      ],
+      version: '1.0.0',
+      metadata_id: uuid(),
+      createdOn: new Date(),
+      appId: APP_NAME
+    }).finally(() => setIsUploading(false));
 
     const request = {
       profileId: currentProfile?.id,
