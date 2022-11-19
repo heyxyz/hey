1a8015d89042178ae39a1855bd4e756c84206c17
bigint <69431456+bigint@users.noreply.github.com>
2022-11-20 01:02:55 +0530

    fix: api

--- apps/api/next.config.js
+++ apps/api/next.config.js
@@ -3,5 +3,14 @@ const withTM = require('next-transpile-modules')(['lens', 'data', 'abis']);
 
 module.exports = withTM({
   reactStrictMode: false,
-  trailingSlash: false
+  trailingSlash: false,
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [{ key: 'Access-Control-Allow-Origin', value: '*' }]
+      }
+    ];
+  }
 });
