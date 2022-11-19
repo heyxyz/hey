adc4a1b8e26a3eebe17d131c2eed4c1369d74b39
bigint <69431456+bigint@users.noreply.github.com>
2022-11-20 01:04:07 +0530

    fix: api

--- apps/api/next.config.js
+++ apps/api/next.config.js
@@ -9,7 +9,15 @@ module.exports = withTM({
       {
         // matching all API routes
         source: '/api/:path*',
-        headers: [{ key: 'Access-Control-Allow-Origin', value: '*' }]
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+          }
+        ]
       }
     ];
   }
