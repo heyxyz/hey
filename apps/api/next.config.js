b43823a29e665828d0634baa12bfae581d981c64
bigint <69431456+bigint@users.noreply.github.com>
2022-11-20 00:26:05 +0530

    chore: update next config

--- apps/api/next.config.js
+++ apps/api/next.config.js
@@ -3,19 +3,5 @@ const withTM = require('next-transpile-modules')(['lens', 'data', 'abis']);
 
 module.exports = withTM({
   reactStrictMode: false,
-  trailingSlash: false,
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          { key: 'X-Content-Type-Options', value: 'nosniff' },
-          { key: 'X-Frame-Options', value: 'DENY' },
-          { key: 'X-XSS-Protection', value: '1; mode=block' },
-          { key: 'Referrer-Policy', value: 'strict-origin' },
-          { key: 'Permissions-Policy', value: 'interest-cohort=()' }
-        ]
-      }
-    ];
-  }
+  trailingSlash: false
 });
