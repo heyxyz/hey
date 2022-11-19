5331e4fd8c9c1c8f63e2578582086bf0b1d640f8
bigint <69431456+bigint@users.noreply.github.com>
2022-11-19 20:53:34 +0530

    chore: verify chainlink

--- packages/data/verified.ts
+++ packages/data/verified.ts
@@ -2,6 +2,7 @@ import { aaveMembers } from './aave-members';
 import { lensterMembers } from './lenster-members';
 
 export const mainnetVerified = [
+  '0x018d18', // chainlink.lens
   '0x4dc9', // jasper.lens
   '0xf5b6', // henri.lens
   '0x0187b3', // vitalik.lens
