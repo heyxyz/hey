e8e5e733501d2bf42e111df7f6c4d3566ebdf82a
bigint <69431456+bigint@users.noreply.github.com>
2022-11-20 00:56:41 +0530

    fix: api

--- apps/api/next.config.js
+++ apps/api/next.config.js
@@ -3,23 +3,5 @@ const withTM = require('next-transpile-modules')(['lens', 'data', 'abis']);
 
 module.exports = withTM({
   reactStrictMode: false,
-  trailingSlash: false,
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-          }
-        ]
-      }
-    ];
-  }
+  trailingSlash: false
 });
