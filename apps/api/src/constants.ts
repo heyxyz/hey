19360c3334fd89c2dc55476fb2c546c4d8a935ef
bigint <69431456+bigint@users.noreply.github.com>
2022-11-20 00:08:39 +0530

    feat: add api app

---
+++ apps/api/src/constants.ts
@@ -0,0 +1,16 @@
+export const APP_NAME = 'Lenster';
+export const DESCRIPTION =
+  'Lenster is a composable, decentralized, and permissionless social media web app built with Lens Protocol 🌿';
+export const DEFAULT_OG = 'https://assets.lenster.xyz/images/og/logo.jpeg';
+export const ERROR_MESSAGE = 'Something went wrong!';
+export const EVER_API = 'https://endpoint.4everland.co';
+export const IPFS_GATEWAY = 'https://lens.infura-ipfs.io/ipfs/';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL ?? '';
+
+// Datadog
+export const DATADOG_TOKEN = process.env.NEXT_PUBLIC_DATADOG_API_KEY ?? '';
+export const DATADOG_APPLICATION_KEY = process.env.NEXT_PUBLIC_DATADOG_APPLICATION_KEY ?? '';
+
+// Bundlr
+export const BUNDLR_CURRENCY = 'matic';
+export const BUNDLR_NODE_URL = 'https://node2.bundlr.network';
---
+++ apps/api/src/pages/api/metadata/upload.ts
@@ -0,0 +1,32 @@
+import Bundlr from '@bundlr-network/client';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { APP_NAME, BUNDLR_CURRENCY, BUNDLR_NODE_URL, ERROR_MESSAGE } from 'src/constants';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ success: false, message: 'Invalid method!' });
+  }
+
+  if (!req.body) {
+    return res.status(400).json({ success: false, message: 'Bad request!' });
+  }
+
+  const payload = JSON.stringify(req.body);
+
+  try {
+    const bundlr = new Bundlr(BUNDLR_NODE_URL, BUNDLR_CURRENCY, process.env.BUNDLR_PRIVATE_KEY);
+    const tags = [
+      { name: 'Content-Type', value: 'application/json' },
+      { name: 'App-Name', value: APP_NAME }
+    ];
+
+    const uploader = bundlr.uploader.chunkedUploader;
+    const { data } = await uploader.uploadData(Buffer.from(payload), { tags });
+
+    return res.status(200).json({ success: true, id: data.id });
+  } catch {
+    return res.status(500).json({ success: false, message: ERROR_MESSAGE });
+  }
+};
+
+export default handler;
