8e8f129d24424ac9e75eefe69b972ded675a7632
bigint <69431456+bigint@users.noreply.github.com>
2022-11-20 00:49:57 +0530

    fix: api

--- apps/api/src/pages/api/metadata/upload.ts
+++ apps/api/src/pages/api/metadata/upload.ts
@@ -23,7 +23,13 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const uploader = bundlr.uploader.chunkedUploader;
     const { data } = await uploader.uploadData(Buffer.from(payload), { tags });
 
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
     return res.status(200).json({ success: true, id: data.id });
   } catch {
     return res.status(500).json({ success: false, message: ERROR_MESSAGE });
