cd06073524965a1775a9c4e90ee8064b914aff95
bigint <69431456+bigint@users.noreply.github.com>
2022-11-20 09:49:34 +0530

    chore: update api headers

--- apps/api/next.config.js
+++ apps/api/next.config.js
@@ -3,12 +3,13 @@ const withTM = require('next-transpile-modules')(['lens', 'data', 'abis']);
 
 module.exports = withTM({
   reactStrictMode: false,
+  trailingSlash: false,
   async headers() {
     return [
       {
         source: '/api/:path*',
         headers: [
-          { key: 'Access-Control-Allow-Origin', value: 'https://lenster.xyz' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
           { key: 'Access-Control-Allow-Headers', value: 'Content-Type' }
         ]
       }
