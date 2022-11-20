321c094463cfb5e08b099c89df189fbed3dd68f7
bigint <69431456+bigint@users.noreply.github.com>
2022-11-20 09:53:51 +0530

    chore: update api headers

--- apps/api/next.config.js
+++ apps/api/next.config.js
@@ -10,7 +10,8 @@ module.exports = withTM({
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' }
+          { key: 'Access-Control-Max-Age', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: '1728000' }
         ]
       }
     ];
