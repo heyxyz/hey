c4e23c4987ebafd2c42ce7919b6fbd8312ac7c26
Pavel Svitek <pavel.svitek@maklando.ch>
2022-11-21 12:53:34 +0100

    fix lib/isEnsVerified corner case

--- apps/web/src/lib/isEnsVerified.ts
+++ apps/web/src/lib/isEnsVerified.ts
@@ -8,13 +8,16 @@ import type { Profile } from 'lens';
  * Examples for handle "vitalik.lens":
  *  - ENS: "vitalik.eth" returns `true`
  *  - ENS: "lens.vitalik.eth" returns `true`
+ *  - ENS "anothervitalik.eth" returns `false` (trying to impersonate)
  *  - ENS "vitalik.eth.somebodyelse.eth" returns `false` (trying to impersonate)
  */
 function isEnsVerified(profile: Profile): boolean {
   const ensName = (profile?.onChainIdentity?.ens?.name || '') as string;
-  const handleWithEnsSuffix = profile?.handle.replace(/\.lens$/, '.eth');
 
-  return ensName.endsWith(handleWithEnsSuffix);
+  const ensPrimaryDomain = ensName.split('.').slice(-2)[0];
+  const lensHandle = profile.handle.replace(/\.lens$/, '');
+
+  return ensPrimaryDomain === lensHandle;
 }
 
 export default isEnsVerified;
