efcc1252de2afd6ecabf887df7ff4458db0d932c
bigint <69431456+bigint@users.noreply.github.com>
2022-11-23 14:07:42 +0530

    chore: use simpleanalyticscdn

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import '../styles.css';
 
 import Loading from '@components/Loading';
+import { IS_PRODUCTION } from 'data/constants';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import { lazy, Suspense } from 'react';
 const Providers = lazy(() => import('@components/Providers'));
 const Layout = lazy(() => import('@components/Layout'));
@@ -14,6 +16,18 @@ const App = ({ Component, pageProps }: AppProps) => {
           <Component {...pageProps} />
         </Layout>
       </Providers>
+      {IS_PRODUCTION && (
+        <>
+          <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
+          <noscript>
+            <img
+              src="https://queue.simpleanalyticscdn.com/noscript.gif"
+              alt=""
+              referrerPolicy="no-referrer-when-downgrade"
+            />
+          </noscript>
+        </>
+      )}
     </Suspense>
   );
 };
