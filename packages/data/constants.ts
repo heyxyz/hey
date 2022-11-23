f1c9931e93f155a508b1f49ea34930f8fb68c76d
bigint <69431456+bigint@users.noreply.github.com>
2022-11-23 19:27:26 +0530

    chore: default relay settings

--- packages/data/constants.ts
+++ packages/data/constants.ts
@@ -42,7 +42,7 @@ export const GIT_COMMIT_SHA = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA?.sli
 
 // Misc
 export const CONTACT_EMAIL = 'support@lenster.xyz';
-export const RELAY_ON = process.env.NEXT_PUBLIC_RELAY_ON === 'true';
+export const RELAY_ON = true;
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
 // Messages
