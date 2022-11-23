126e2335007e8045c50e5cc6bdba67ba3cecc81d
bigint <69431456+bigint@users.noreply.github.com>
2022-11-23 15:14:03 +0530

    fix: typo

--- apps/web/src/components/Settings/Allowance/Button.tsx
+++ apps/web/src/components/Settings/Allowance/Button.tsx
@@ -40,7 +40,7 @@ const AllowanceButton: FC<Props> = ({ title = 'Allow', module, allowed, setAllow
       toast.success(`Module ${allowed ? 'disabled' : 'enabled'} successfully!`);
       setShowWarningModal(false);
       setAllowed(!allowed);
-      Leafwatch.track(`Module ${allowed ? 'disabled' : 'enabled'}`);
+      Leafwatch.track(`module_${allowed ? 'disabled' : 'enabled'}`);
     },
     onError
   });
