407292de4c6c3980e9da92d3e791b78c49f90daf
bigint <69431456+bigint@users.noreply.github.com>
2022-11-23 19:25:53 +0530

    chore: move stats to simple analytics (#1287)

--- apps/api/src/constants.ts
+++ apps/api/src/constants.ts
@@ -1,7 +1,3 @@
-// Datadog
-export const DATADOG_TOKEN = process.env.NEXT_PUBLIC_DATADOG_API_KEY ?? '';
-export const DATADOG_APPLICATION_KEY = process.env.NEXT_PUBLIC_DATADOG_APPLICATION_KEY ?? '';
-
 // Bundlr
 export const BUNDLR_CURRENCY = 'matic';
 export const BUNDLR_NODE_URL = 'https://node2.bundlr.network';
