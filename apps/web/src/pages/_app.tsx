a81a8c254e5524d76fbea992f44c669448c58f84
bigint <69431456+bigint@users.noreply.github.com>
2022-11-24 20:31:41 +0530

    Revert "chore: move to umami" (#1302)

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles.css';
 
 import Loading from '@components/Loading';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import { lazy, Suspense } from 'react';
 const Providers = lazy(() => import('@components/Providers'));
 const Layout = lazy(() => import('@components/Layout'));
@@ -14,6 +15,16 @@ const App = ({ Component, pageProps }: AppProps) => {
           <Component {...pageProps} />
         </Layout>
       </Providers>
+
+      {/* Begin Simple Analytics */}
+      <Script strategy="lazyOnload" id="sa-events">
+        {`window.sa_event=window.sa_event||function(){var a=[].slice.call(arguments);window.sa_event.q?window.sa_event.q.push(a):window.sa_event.q=[a]};`}
+      </Script>
+      <Script strategy="lazyOnload" data-collect-dnt="true" src="https://sa.lenster.xyz/latest.js" />
+      <noscript>
+        <img src="https://sa.lenster.xyz/noscript.gif" alt="" referrerPolicy="no-referrer-when-downgrade" />
+      </noscript>
+      {/* End Simple Analytics */}
     </Suspense>
   );
 };
