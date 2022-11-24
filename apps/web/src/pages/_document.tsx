2dfffbed0d862f5323ecfd964c089dc227fe1269
bigint <69431456+bigint@users.noreply.github.com>
2022-11-24 23:41:25 +0530

    chore: move sa to _document

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 class LensterDocument extends Document {
   render() {
@@ -21,6 +22,19 @@ class LensterDocument extends Document {
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+
+          {/* Simple Analytics */}
+          <Script strategy="lazyOnload" id="sa-events">
+            {`window.sa_event=window.sa_event||function(){var a=[].slice.call(arguments);window.sa_event.q?window.sa_event.q.push(a):window.sa_event.q=[a]};`}
+          </Script>
+          <Script strategy="lazyOnload" data-collect-dnt="true" src="https://sa.lenster.xyz/latest.js" />
+          <noscript>
+            <img
+              src="https://sa.lenster.xyz/noscript.gif"
+              alt=""
+              referrerPolicy="no-referrer-when-downgrade"
+            />
+          </noscript>
         </Head>
         <body>
           <Main />
