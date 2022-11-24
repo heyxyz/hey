7d8ebe926a14412ef8f03d00bb6dc9f666b08e42
bigint <69431456+bigint@users.noreply.github.com>
2022-11-24 23:35:21 +0530

    chore: remove umami

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -1,6 +1,4 @@
-import { IS_PRODUCTION } from 'data/constants';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 class LensterDocument extends Document {
   render() {
@@ -23,13 +21,6 @@ class LensterDocument extends Document {
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          {IS_PRODUCTION ? (
-            <Script
-              strategy="lazyOnload"
-              data-website-id="d85fe26e-625b-4973-abd1-5dd8fdc4782f"
-              src="https://umami.lenster.xyz/umami.js"
-            />
-          ) : null}
         </Head>
         <body>
           <Main />
