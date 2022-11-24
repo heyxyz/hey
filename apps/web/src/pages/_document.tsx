3f575ac12a7619d39af80c749e493d564ac5a882
bigint <69431456+bigint@users.noreply.github.com>
2022-11-24 20:31:41 +0530

    Revert "chore: move to umami" (#1302)

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -1,6 +1,4 @@
-import { IS_PRODUCTION } from 'data/constants';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 class LensterDocument extends Document {
   render() {
@@ -10,13 +8,6 @@ class LensterDocument extends Document {
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <link rel="shortcut icon" href="/favicon.ico" />
-          {IS_PRODUCTION ? (
-            <Script
-              strategy="lazyOnload"
-              data-website-id="d85fe26e-625b-4973-abd1-5dd8fdc4782f"
-              src="https://umami.lenster.xyz/umami.js"
-            />
-          ) : null}
         </Head>
         <body>
           <Main />
