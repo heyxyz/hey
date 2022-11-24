2727bfc7398d42a374736527e91a9df62e19c8e9
Ape Dev <83542848+ape-dev-cs@users.noreply.github.com>
2022-11-24 08:22:36 +0000

    chore: verify memester & founders (#1288)
    Co-authored-by: bigint <69431456+bigint@users.noreply.github.com>

--- packages/data/verified.ts
+++ packages/data/verified.ts
@@ -2,6 +2,10 @@ import { aaveMembers } from './aave-members';
 import { lensterMembers } from './lenster-members';
 
 export const mainnetVerified = [
+  '0xb1c8', // memester-xyz.lens
+  '0x8ba4', // devanon.lens
+  '0xa518', // apedev.lens
+  '0xf2a3', // papadari.lens
   '0x01873a', // curtisroach.lens
   '0xc1bd', // dragaan.lens
   '0x014c1f', // mexicotte.lens
