7d6da9d5d1246758b893cf1cf49a80eac67c7367
bigint <69431456+bigint@users.noreply.github.com>
2022-11-28 15:41:35 +0530

    chore: add upsert

--- apps/pro/src/pages/api/user/[id].ts
+++ apps/pro/src/pages/api/user/[id].ts
@@ -4,16 +4,22 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 const prisma = new PrismaClient();
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'POST') {
+  if (req.method === 'GET') {
     try {
-      const { id } = req.body;
-      const data = await prisma.user.upsert({
-        where: { userId: id },
-        create: { userId: id, proExpiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365) },
-        update: { proExpiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365) }
-      });
+      const { id } = req.query;
+      if (id) {
+        const data = await prisma.user.findFirst({
+          where: { userId: id as string }
+        });
 
-      return res.status(200).json({ data });
+        if (data?.proExpiresAt) {
+          return res.status(200).json({ isPro: data?.proExpiresAt > new Date() });
+        }
+
+        return res.status(200).json({ isPro: false });
+      }
+
+      return res.status(200).json({ message: 'User ID is empty' });
     } catch (error) {
       console.log(error);
       return res.status(500).json({ message: 'Something went wrong' });
