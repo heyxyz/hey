14be5b7e3793220f5b885e0642b9bdcdc5ef13cf
bigint <69431456+bigint@users.noreply.github.com>
2022-11-28 23:06:52 +0530

    chore: get data

--- apps/pro/src/pages/api/webhook.ts
+++ apps/pro/src/pages/api/webhook.ts
@@ -1,16 +1,24 @@
 import { PrismaClient } from '@prisma/client';
+import { ethers } from 'ethers';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { FeeCollector } from 'src/abis/FeeCollector';
 
 const prisma = new PrismaClient();
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     try {
-      const { id } = req.body;
+      const { hash } = req.body.event.activity[0];
+      const iface = new ethers.utils.Interface(FeeCollector);
+      const provider = new ethers.providers.JsonRpcProvider('https://rpc.ankr.com/polygon');
+      const tx = await provider.getTransactionReceipt(hash);
+      const userId = JSON.parse(JSON.stringify(iface.parseLog(tx.logs[2]))).args[2];
+
+      const updateData = { proExpiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365), txHash: hash };
       const data = await prisma.user.upsert({
-        where: { userId: id },
-        create: { userId: id, proExpiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365) },
-        update: { proExpiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365) }
+        where: { userId },
+        create: { userId, ...updateData },
+        update: updateData
       });
 
       return res.status(200).json({ data });
