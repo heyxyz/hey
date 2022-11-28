d85a09d01981c8042b917d96e0ed48a2cd031da8
bigint <69431456+bigint@users.noreply.github.com>
2022-11-28 15:41:35 +0530

    chore: add upsert

--- apps/pro/src/pages/api/index.ts
+++ apps/pro/src/pages/api/index.ts
@@ -1,19 +1,7 @@
-import { PrismaClient } from '@prisma/client';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-const prisma = new PrismaClient();
-
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'GET') {
-    try {
-      const data = await prisma.user.findMany({});
-      return res.status(200).json({ data });
-    } catch (error) {
-      return res.status(500).json({ msg: 'Something went wrong' });
-    }
-  } else {
-    return res.status(405).json({ msg: 'Method not allowed' });
-  }
+  return res.setHeader('Content-Type', 'text/html').setHeader('Cache-Control', 's-maxage=86400').send('gm');
 };
 
 export default handler;
