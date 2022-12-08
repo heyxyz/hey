0673d5ee58040cd5da7b0c8acf0c1454faa420ba
bigint <69431456+bigint@users.noreply.github.com>
2022-12-08 18:36:53 +0530

    chore: remove followers gating

--- apps/web/src/components/Composer/Actions/AccessSettings/BasicSettings.tsx
+++ apps/web/src/components/Composer/Actions/AccessSettings/BasicSettings.tsx
@@ -1,6 +1,6 @@
 import ToggleWithHelper from '@components/Shared/ToggleWithHelper';
 import { Card } from '@components/UI/Card';
-import { CollectionIcon, UsersIcon } from '@heroicons/react/outline';
+import { CollectionIcon } from '@heroicons/react/outline';
 import type { FC } from 'react';
 import { useAccessSettingsStore } from 'src/store/access-settings';
 
@@ -20,34 +20,19 @@ const BasicSettings: FC = () => {
         label="Add restrictions on who can view this post"
       />
       {restricted && (
-        <>
-          <Card className="p-5 mt-5">
-            <div className="space-y-2">
-              <div className="flex items-center space-x-2">
-                <CollectionIcon className="h-4 w-4 text-brand-500" />
-                <span>Collectors can view</span>
-              </div>
-              <ToggleWithHelper
-                on={collectToView}
-                setOn={() => setCollectToView(!collectToView)}
-                label="People need to collect it first to be able to view it"
-              />
+        <Card className="p-5 mt-5">
+          <div className="space-y-2">
+            <div className="flex items-center space-x-2">
+              <CollectionIcon className="h-4 w-4 text-brand-500" />
+              <span>Collectors can view</span>
             </div>
-          </Card>
-          <Card className="p-5 mt-5">
-            <div className="space-y-2">
-              <div className="flex items-center space-x-2">
-                <UsersIcon className="h-4 w-4 text-brand-500" />
-                <span>Followers can view</span>
-              </div>
-              <ToggleWithHelper
-                on={followToView}
-                setOn={() => setFollowToView(!followToView)}
-                label="People need to follow you to be able to view it"
-              />
-            </div>
-          </Card>
-        </>
+            <ToggleWithHelper
+              on={collectToView}
+              setOn={() => setCollectToView(!collectToView)}
+              label="People need to collect it first to be able to view it"
+            />
+          </div>
+        </Card>
       )}
     </div>
   );
