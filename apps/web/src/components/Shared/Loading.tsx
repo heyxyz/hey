c246fecb6b8c4cf0fc42d7e037d6409a7985971c
chiragbadhe <badhechirag633@gmail.com>
2022-12-08 12:25:03 +0530

    fix: increase spinner size #52

--- apps/web/src/components/Shared/Loading.tsx
+++ apps/web/src/components/Shared/Loading.tsx
@@ -6,7 +6,7 @@ const Loading: FC = () => {
   return (
     <div className="grid h-screen place-items-center">
       <MetaTags />
-      <object className="h-36 w-36" height={122} width={122} type="image/svg+xml" data="/logo-loader.svg" />
+      <object className="h-[160px] w-[160px]" type="image/svg+xml" data="/logo-loader.svg" />
     </div>
   );
 };
