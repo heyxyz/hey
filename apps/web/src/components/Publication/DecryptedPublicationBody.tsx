cb34fd9dfafd872625d2e53d50a8d0afaa4ba851
bigint <69431456+bigint@users.noreply.github.com>
2022-12-10 22:28:27 +0530

    chore: add to view design

--- apps/web/src/components/Publication/DecryptedPublicationBody.tsx
+++ apps/web/src/components/Publication/DecryptedPublicationBody.tsx
@@ -1,9 +1,12 @@
 import Attachments from '@components/Shared/Attachments';
 import IFramely from '@components/Shared/IFramely';
 import Markup from '@components/Shared/Markup';
+import { Card } from '@components/UI/Card';
 import type { LensterPublication } from '@generated/types';
-import { EyeIcon } from '@heroicons/react/outline';
+import { CollectionIcon, EyeIcon, UserAddIcon } from '@heroicons/react/outline';
+import { LockClosedIcon } from '@heroicons/react/solid';
 import { LensEnvironment, LensGatedSDK } from '@lens-protocol/sdk-gated';
+import formatHandle from '@lib/formatHandle';
 import getURLs from '@lib/getURLs';
 import axios from 'axios';
 import clsx from 'clsx';
@@ -39,11 +42,34 @@ const DecryptedPublicationBody: FC<Props> = ({ publication }) => {
   };
 
   if (!canDecrypt) {
-    return <div>To view...</div>;
+    return (
+      <Card className="text-sm rounded-xl w-fit p-8 shadow-sm" onClick={(event) => event.stopPropagation()}>
+        <div className="font-bold flex items-center space-x-2">
+          <LockClosedIcon className="h-5 w-5 text-green-500" />
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-brand-500 to-blue-500 font-black">
+            Unlock this by...
+          </span>
+        </div>
+        <div className="pt-3.5 space-y-2">
+          <div className="flex items-center space-x-2">
+            <CollectionIcon className="h-4 w-4" />
+            <span>
+              Collect this <b className="lowercase">{publication?.__typename}</b>
+            </span>
+          </div>
+          <div className="flex items-center space-x-2">
+            <UserAddIcon className="h-4 w-4" />
+            <span>
+              Follow <b>@{formatHandle(publication?.profile?.handle)}</b>
+            </span>
+          </div>
+        </div>
+      </Card>
+    );
   }
 
-  return (
-    <div className="break-words">
+  if (!decryptedData) {
+    return (
       <button
         onClick={async (event) => {
           event.stopPropagation();
@@ -52,6 +78,11 @@ const DecryptedPublicationBody: FC<Props> = ({ publication }) => {
       >
         Unlock
       </button>
+    );
+  }
+
+  return (
+    <div className="break-words">
       {JSON.stringify(decryptedData)}
       <Markup
         className={clsx(
