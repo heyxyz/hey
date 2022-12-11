1b2ddac50739a3c0deaf35fb9517e5b0a3ac9405
bigint <69431456+bigint@users.noreply.github.com>
2022-12-11 16:27:15 +0530

    chore: add terms

--- apps/web/src/components/Pages/Privacy.tsx
+++ apps/web/src/components/Pages/Privacy.tsx
@@ -6,7 +6,7 @@ import type { FC } from 'react';
 const Privacy: FC = () => {
   return (
     <>
-      <MetaTags title={`Privacy • ${APP_NAME}`} />
+      <MetaTags title={`Privacy Policy • ${APP_NAME}`} />
       <div className="flex justify-center items-center w-full h-48 bg-brand-400">
         <div className="relative text-center">
           <h1 className="text-3xl font-bold text-white md:text-4xl">Privacy Policy</h1>
@@ -25,8 +25,8 @@ const Privacy: FC = () => {
               <div className="mb-5 text-xl font-bold text-black dark:text-white">1. Overview</div>
               <div className="space-y-5 leading-7">
                 <p>
-                  By accessing the site, you agree to not only this Privacy Policy, but also to our Terms of
-                  Use and any other written agreements for using the site. And you agree to our collection and
+                  By accessing the Site, you agree to not only this Privacy Policy, but also to our Terms of
+                  Use and any other written agreements for using the Site. And you agree to our collection and
                   use of any personal data (as described here) as well as the features of
                   public/permissionless blockchain technology.
                 </p>
@@ -37,14 +37,14 @@ const Privacy: FC = () => {
                   such information.
                 </p>
                 <p>
-                  By accessing or using the site, you accept and assume certain inherent features related to
+                  By accessing or using the Site, you accept and assume certain inherent features related to
                   engaging in recording the data on the blockchain. Interactions with the Lens Protocol rely
                   on smart contracts stored on a publicly available blockchain, cryptographic tokens generated
                   by the smart contracts, and other nascent software, applications and systems that interact
                   with blockchain-based networks. One of the defining features of blockchain technology is
                   that its entries are immutable, which means, as a technical matter, they generally cannot be
                   deleted or modified by anyone. If you are not comfortable assuming the inherently immutable
-                  and public nature of all entries on the blockchain, you should not engage with our site.
+                  and public nature of all entries on the blockchain, you should not engage with our Site.
                 </p>
               </div>
               {/* 1. Overview ends */}
@@ -52,8 +52,8 @@ const Privacy: FC = () => {
               <div className="mt-8 mb-5 text-xl font-bold text-black dark:text-white">
                 2. Information Collection
               </div>
-              <div className="space-y-5 mb-5 leading-7">
-                <p>We may collect the following information about you when you use the site:</p>
+              <div className="space-y-5 leading-7">
+                <p>We may collect the following information about you when you use the Site:</p>
                 <ul className="list-disc list-inside">
                   <li>Information you provide such as feedback, question and issues reports.</li>
                 </ul>
@@ -67,11 +67,11 @@ const Privacy: FC = () => {
                 </p>
                 <ul className="space-y-3 list-disc list-inside">
                   <li>
-                    <b>Log Files</b>, to record events or errors that occur when using our site.
+                    <b>Log Files</b>, to record events or errors that occur when using our Site.
                   </li>
                   <li>
                     <b>Cookies</b>, small data stored on your device that are necessary for you to browse the
-                    site.
+                    Site.
                   </li>
                   <li>
                     <b>Public Information</b>, data from activity that is publicly visible and/or accessible
@@ -86,9 +86,9 @@ const Privacy: FC = () => {
                 3. Use of Information
               </div>
               <p className="leading-7">
-                We may need to use it to operate and manage the Services on this site (or other places),
+                We may need to use it to operate and manage the Services on this Site (or other places),
                 provide you support, ensure we comply with laws and regulation, and enforce the security of
-                the site or make other improvements.
+                the Site or make other improvements.
               </p>
               {/* 3. Use of Information ends */}
               {/* 4. Third-Parties begins */}
@@ -104,7 +104,7 @@ const Privacy: FC = () => {
               <div className="space-y-5 leading-7">
                 <p>
                   We use <b>Simple Analytics</b> to collect various events from user actions to analyse and
-                  make decisions for site improvements.
+                  make decisions for Site improvements.
                 </p>
                 <p>
                   All the data are anonymous and we don’t track user details such as name, handle, email,
@@ -113,7 +113,7 @@ const Privacy: FC = () => {
                 <p>
                   We may change to other third-party analytics service provider. The Privacy Policy of
                   Analytics subjects to the every provider. You should review everything before using the
-                  site.
+                  Site.
                 </p>
               </div>
               {/* 5. Analytics ends */}
@@ -121,18 +121,18 @@ const Privacy: FC = () => {
               <div className="mt-8 mb-5 text-xl font-bold text-black dark:text-white">
                 6. Your Rights and Choices
               </div>
-              <div className="space-y-5 mb-5 leading-7">
-                <p>We may collect the following information about you when you use the site:</p>
+              <div className="space-y-5 leading-7">
+                <p>We may collect the following information about you when you use the Site:</p>
                 <ul className="space-y-2 list-disc list-inside">
                   <li>
                     <b>Cookies</b>. We will only use strictly necessary cookies. These cookies are essential
-                    for you to browse the site and use its features, including accessing secure areas of the
-                    site.
+                    for you to browse the Site and use its features, including accessing secure areas of the
+                    Site.
                   </li>
                   <li className="linkify">
                     <b>Do Not Track</b>. Your browser settings may allow you to automatically transmit a “Do
                     Not Track” signal to the online services you visit. Note, however, there is no industry
-                    consensus as to what site and app operators should do with regard to these signals.
+                    consensus as to what Site and app operators should do with regard to these signals.
                     Accordingly, unless and until the law is interpreted to require us to do so, we do not
                     monitor or take action with respect to “Do Not Track” signals. For more information on “Do
                     Not Track,” visit{' '}
@@ -156,7 +156,7 @@ const Privacy: FC = () => {
               {/* 8. Children begins */}
               <div className="mt-8 mb-5 text-xl font-bold text-black dark:text-white">8. Children</div>
               <p className="leading-7">
-                The site is intended for general audiences and are not directed at children. To use the site,
+                The Site is intended for general audiences and are not directed at children. To use the Site,
                 you must legally be able to enter into the Agreement. We do not knowingly collect personal
                 information from children.
               </p>
@@ -168,17 +168,17 @@ const Privacy: FC = () => {
               <p className="leading-7">
                 We reserve the right to revise and reissue this Privacy Policy at any time. Any changes will
                 be effective immediately upon our posting of the revised Privacy Policy. For the avoidance of
-                doubt, your continued use of the site indicates your consent to the revised Privacy Policy
+                doubt, your continued use of the Site indicates your consent to the revised Privacy Policy
                 then posted.
               </p>
               {/* 9. Changes to Policy ends */}
-              {/* 9. Contact begins */}
+              {/* 10. Contact begins */}
               <div className="mt-8 mb-5 text-xl font-bold text-black dark:text-white">10. Contact</div>
               <p className="leading-7">
                 If you have any questions or comments about this Privacy Policy, our data practices, or our
                 compliance with applicable law, please contact us at privacy@lenster.xyz
               </p>
-              {/* 9. Contact ends */}
+              {/* 10. Contact ends */}
             </div>
           </div>
         </div>
