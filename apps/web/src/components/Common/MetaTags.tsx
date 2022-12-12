222c6f262e8a9515839e115a252a2645db6160d6
bigint <69431456+bigint@users.noreply.github.com>
2022-12-12 16:21:10 +0530

    fix: imgproxy

--- apps/web/src/components/Common/MetaTags.tsx
+++ apps/web/src/components/Common/MetaTags.tsx
@@ -14,11 +14,6 @@ const MetaTags: FC<Props> = ({ title = APP_NAME, description = DESCRIPTION }) =>
       <meta name="description" content={description} />
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
 
-      <link rel="preconnect" href="https://media.lenster.xyz" />
-      <link rel="dns-prefetch" href="https://media.lenster.xyz" />
-      <link rel="preconnect" href="https://assets.lenster.xyz" />
-      <link rel="dns-prefetch" href="https://assets.lenster.xyz" />
-
       <meta property="og:url" content="https://lenster.xyz" />
       <meta property="og:site_name" content={APP_NAME} />
       <meta property="og:title" content={title} />
