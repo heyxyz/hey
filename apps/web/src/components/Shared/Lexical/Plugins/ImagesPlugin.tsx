9dec768a35e21a22e0d5a561c1556690f8f91a88
bigint <69431456+bigint@users.noreply.github.com>
2022-12-14 19:21:44 +0530

    fix: copy paste text in lexical (#1408)

--- apps/web/src/components/Shared/Lexical/Plugins/ImagesPlugin.tsx
+++ apps/web/src/components/Shared/Lexical/Plugins/ImagesPlugin.tsx
@@ -1,6 +1,7 @@
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import { mergeRegister } from '@lexical/utils';
 import { COMMAND_PRIORITY_NORMAL, PASTE_COMMAND } from 'lexical';
+import type { ClipboardEvent } from 'react';
 import { useEffect } from 'react';
 
 type ImagesPluginProps = {
@@ -13,21 +14,26 @@ const ImagesPlugin = (props: ImagesPluginProps): JSX.Element | null => {
 
   useEffect(() => {
     return mergeRegister(
-      editor.registerCommand<InputEvent>(
+      editor.registerCommand<InputEvent & ClipboardEvent>(
         PASTE_COMMAND,
-        (event: InputEvent) => {
-          /*
-           * This registers a paste event listener on the editor.
-           * The InputEvent/ClipboardEvent will be triggered both when the user pastes something into the editor.
-           */
+        (event) => {
           if (event) {
-            const { dataTransfer } = event;
+            const { dataTransfer, clipboardData } = event;
+
+            // If the clipboard data contains text, we don't want to handle the image paste event.
+            if (clipboardData?.getData('Text')) {
+              return false;
+            }
+
+            // If the clipboard data contains files, we want to handle the image paste event.
             if (dataTransfer && dataTransfer.files.length) {
               const { files } = dataTransfer;
               onPaste && onPaste(files);
             }
+
             return true;
           }
+
           return false;
         },
         COMMAND_PRIORITY_NORMAL
