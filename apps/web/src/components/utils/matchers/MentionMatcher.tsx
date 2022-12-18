b4a4b8fe5850d6c7e385f7dc2dcd69c277dc55af
0x harry <ranahari@gmail.com>
2022-12-18 14:43:51 +0800

    feat: add user popover for user mentions (#1274)
    Co-authored-by: harry rana <115021446+xjjrckl@users.noreply.github.com>
    Co-authored-by: bigint <69431456+bigint@users.noreply.github.com>

--- apps/web/src/components/utils/matchers/MentionMatcher.tsx
+++ apps/web/src/components/utils/matchers/MentionMatcher.tsx
@@ -1,12 +1,21 @@
 import Slug from '@components/Shared/Slug';
+import UserPreview from '@components/Shared/UserPreview';
 import { Analytics } from '@lib/analytics';
 import formatHandle from '@lib/formatHandle';
 import { Matcher } from 'interweave';
+import type { Profile } from 'lens';
 import Link from 'next/link';
 import { createElement } from 'react';
 import { PUBLICATION } from 'src/tracking';
 
 export const Mention = ({ ...props }: any) => {
+  const profile = {
+    __typename: 'Profile',
+    handle: props?.display.slice(1),
+    name: null,
+    id: null
+  };
+
   return (
     <Link
       href={`/u/${formatHandle(props.display.slice(1))}`}
@@ -15,7 +24,17 @@ export const Mention = ({ ...props }: any) => {
         Analytics.track(PUBLICATION.MENTION_CLICK);
       }}
     >
-      <Slug slug={formatHandle(props.display)} />
+      {profile?.handle ? (
+        <UserPreview
+          isBig={props?.isBig}
+          profile={profile as Profile}
+          followStatusLoading={props?.followStatusLoading}
+        >
+          <Slug slug={formatHandle(props.display)} />
+        </UserPreview>
+      ) : (
+        <Slug slug={formatHandle(props.display)} />
+      )}
     </Link>
   );
 };
