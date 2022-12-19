acf1935311687647d7a62b9720f3a4c8461be599
bigint <69431456+bigint@users.noreply.github.com>
2022-12-19 15:01:54 +0530

    chore: move login to global modal

---
+++ apps/web/src/store/auth.ts
@@ -0,0 +1,11 @@
+import create from 'zustand';
+
+interface AuthState {
+  showAuthModal: boolean;
+  setShowAuthModal: (showAuthModal: boolean) => void;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  showAuthModal: false,
+  setShowAuthModal: (showAuthModal) => set(() => ({ showAuthModal }))
+}));
