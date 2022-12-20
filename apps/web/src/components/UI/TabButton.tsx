8b94323b0edd6426534b3bdaef1669d3dfe35f31
bigint <69431456+bigint@users.noreply.github.com>
2022-12-20 21:37:39 +0530

    chore: remove count from feed type and tab button (#1436)

--- apps/web/src/components/UI/TabButton.tsx
+++ apps/web/src/components/UI/TabButton.tsx
@@ -1,4 +1,3 @@
-import nFormatter from '@lib/nFormatter';
 import clsx from 'clsx';
 import type { FC, ReactNode } from 'react';
 
@@ -6,34 +5,23 @@ interface Props {
   name: string;
   icon: ReactNode;
   active: boolean;
-  count?: number;
   showOnSm?: boolean;
   onClick: () => void;
 }
 
-const TabButton: FC<Props> = ({ name, icon, active, count, showOnSm = false, onClick }) => {
+const TabButton: FC<Props> = ({ name, icon, active, showOnSm = false, onClick }) => {
   return (
     <button
       type="button"
       onClick={onClick}
       className={clsx(
         { 'text-brand bg-brand-100 dark:bg-opacity-20 bg-opacity-100': active },
-        'flex items-center space-x-2 rounded-lg text-sm px-4 sm:px-3 font-medium py-2 sm:py-1.5 lt-text-gray-500 hover:bg-brand-100 dark:hover:bg-opacity-20 hover:bg-opacity-100'
+        'flex items-center space-x-2 rounded-lg text-sm px-4 sm:px-3 font-medium py-2 sm:py-1.5 text-gray-500 hover:bg-brand-100 dark:hover:bg-opacity-20 hover:bg-opacity-100'
       )}
       aria-label={name}
     >
       {icon}
       <span className={clsx({ 'hidden sm:block': !showOnSm })}>{name}</span>
-      {count ? (
-        <span
-          className={clsx(
-            { 'bg-brand-200 dark:bg-brand-800': active },
-            'px-2 text-xs rounded-full bg-gray-200 dark:bg-gray-800'
-          )}
-        >
-          {nFormatter(count)}
-        </span>
-      ) : null}
     </button>
   );
 };
