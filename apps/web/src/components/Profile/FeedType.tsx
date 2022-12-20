85a25ab3d1a2c1f45cce9564778f78fdfe5ed513
bigint <69431456+bigint@users.noreply.github.com>
2022-12-20 21:37:39 +0530

    chore: remove count from feed type and tab button (#1436)

--- apps/web/src/components/Profile/FeedType.tsx
+++ apps/web/src/components/Profile/FeedType.tsx
@@ -1,19 +1,17 @@
 import TabButton from '@components/UI/TabButton';
 import { ChatAlt2Icon, FilmIcon, PencilAltIcon, PhotographIcon } from '@heroicons/react/outline';
 import { Analytics } from '@lib/analytics';
-import type { ProfileStats } from 'lens';
 import type { Dispatch, FC } from 'react';
 import { PROFILE } from 'src/tracking';
 
 import MediaFilter from './Filters/MediaFilter';
 
 interface Props {
-  stats: ProfileStats;
   setFeedType: Dispatch<string>;
   feedType: string;
 }
 
-const FeedType: FC<Props> = ({ stats, setFeedType, feedType }) => {
+const FeedType: FC<Props> = ({ setFeedType, feedType }) => {
   return (
     <div className="flex justify-between items-center">
       <div className="flex overflow-x-auto gap-3 px-5 pb-2 mt-3 sm:px-0 sm:mt-0 md:pb-0">
@@ -21,7 +19,6 @@ const FeedType: FC<Props> = ({ stats, setFeedType, feedType }) => {
           name="Feed"
           icon={<PencilAltIcon className="w-4 h-4" />}
           active={feedType === 'FEED'}
-          count={stats?.totalPosts + stats?.totalMirrors}
           onClick={() => {
             setFeedType('FEED');
             Analytics.track(PROFILE.SWITCH_FEED);
@@ -31,7 +28,6 @@ const FeedType: FC<Props> = ({ stats, setFeedType, feedType }) => {
           name="Replies"
           icon={<ChatAlt2Icon className="w-4 h-4" />}
           active={feedType === 'REPLIES'}
-          count={stats?.totalComments}
           onClick={() => {
             setFeedType('REPLIES');
             Analytics.track(PROFILE.SWITCH_REPLIES);
