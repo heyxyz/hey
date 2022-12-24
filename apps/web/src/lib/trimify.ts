3d13dec478d55c9e71bee66842774bcb820490aa
bigint <69431456+bigint@users.noreply.github.com>
2022-12-24 12:24:29 +0530

    chore: update deps (#1449)

--- apps/web/src/lib/trimify.ts
+++ apps/web/src/lib/trimify.ts
@@ -3,6 +3,6 @@
  * @param value - Value to trim
  * @returns trimmed value
  */
-const trimify = (value: string): string => value?.replace(/\n\s*\n/g, '\n\n').trim();
+const trimify = (value: string): string => value?.replace(/\n\n\s*\n/g, '\n\n').trim();
 
 export default trimify;
