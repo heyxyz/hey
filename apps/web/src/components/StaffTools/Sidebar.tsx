455f34f012e2e9d454231f5e6bfafbec53ffd724
bigint <69431456+bigint@users.noreply.github.com>
2022-12-25 11:30:58 +0530

    chore: update SettingsSidebar

--- apps/web/src/components/StaffTools/Sidebar.tsx
+++ apps/web/src/components/StaffTools/Sidebar.tsx
@@ -4,22 +4,20 @@ import type { FC } from 'react';
 
 const StaffToolsSidebar: FC = () => {
   return (
-    <div className="px-3 mb-4 space-y-1.5 sm:px-0">
-      <Sidebar
-        items={[
-          {
-            title: 'Stats',
-            icon: <ChartPieIcon className="w-4 h-4" />,
-            url: '/stafftools'
-          },
-          {
-            title: 'Analytics',
-            icon: <ChartBarIcon className="w-4 h-4" />,
-            url: '/stafftools/analytics'
-          }
-        ]}
-      />
-    </div>
+    <Sidebar
+      items={[
+        {
+          title: 'Stats',
+          icon: <ChartPieIcon className="w-4 h-4" />,
+          url: '/stafftools'
+        },
+        {
+          title: 'Analytics',
+          icon: <ChartBarIcon className="w-4 h-4" />,
+          url: '/stafftools/analytics'
+        }
+      ]}
+    />
   );
 };
 
