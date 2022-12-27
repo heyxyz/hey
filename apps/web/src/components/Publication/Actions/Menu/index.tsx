d6daac96996dbc8577147e0c520be902e4cfe6ad
bigint <69431456+bigint@users.noreply.github.com>
2022-12-27 11:07:24 +0530

    chore: remove unused open prop

--- apps/web/src/components/Publication/Actions/Menu/index.tsx
+++ apps/web/src/components/Publication/Actions/Menu/index.tsx
@@ -5,7 +5,6 @@ import { DotsVerticalIcon } from '@heroicons/react/outline';
 import { Analytics } from '@lib/analytics';
 import clsx from 'clsx';
 import type { FC } from 'react';
-import { Fragment } from 'react';
 import { useAppStore } from 'src/store/app';
 import { PUBLICATION } from 'src/tracking';
 
@@ -24,33 +23,29 @@ const PublicationMenu: FC<Props> = ({ publication }) => {
 
   return (
     <Menu as="div" className="relative">
-      {({ open }) => (
-        <>
-          <Menu.Button
-            className="p-1.5 rounded-full hover:bg-gray-300 hover:bg-opacity-20"
-            onClick={() => {
-              Analytics.track(PUBLICATION.MORE);
-            }}
-            aria-label="More"
-          >
-            <DotsVerticalIcon className={clsx('lt-text-gray-500', iconClassName)} />
-          </Menu.Button>
-          <MenuTransition>
-            <Menu.Items
-              static
-              className="absolute right-0 mt-1 w-max bg-white rounded-xl border shadow-sm dark:bg-gray-900 focus:outline-none z-[5] dark:border-gray-700"
-            >
-              {currentProfile?.id === publication?.profile?.id ? (
-                <Delete publication={publication} />
-              ) : (
-                <Report publication={publication} />
-              )}
-              <Embed publication={publication} />
-              <Permalink publication={publication} />
-            </Menu.Items>
-          </MenuTransition>
-        </>
-      )}
+      <Menu.Button
+        className="p-1.5 rounded-full hover:bg-gray-300 hover:bg-opacity-20"
+        onClick={() => {
+          Analytics.track(PUBLICATION.MORE);
+        }}
+        aria-label="More"
+      >
+        <DotsVerticalIcon className={clsx('lt-text-gray-500', iconClassName)} />
+      </Menu.Button>
+      <MenuTransition>
+        <Menu.Items
+          static
+          className="absolute right-0 mt-1 w-max bg-white rounded-xl border shadow-sm dark:bg-gray-900 focus:outline-none z-[5] dark:border-gray-700"
+        >
+          {currentProfile?.id === publication?.profile?.id ? (
+            <Delete publication={publication} />
+          ) : (
+            <Report publication={publication} />
+          )}
+          <Embed publication={publication} />
+          <Permalink publication={publication} />
+        </Menu.Items>
+      </MenuTransition>
     </Menu>
   );
 };
