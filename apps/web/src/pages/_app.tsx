ed77871bb7ce97764bee93d0cbf42339d655b7a5
Olof Andersson <olof.andersson@gmail.com>
2022-12-29 20:15:55 +0100

    move I18nProvider to Providers

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,27 +1,18 @@
 import '../styles.css';
 
 import Loading from '@components/Shared/Loading';
-import { initLocale } from '@lib/i18n';
-import { i18n } from '@lingui/core';
-import { I18nProvider } from '@lingui/react';
 import type { AppProps } from 'next/app';
-import React, { lazy, Suspense, useEffect } from 'react';
+import { lazy, Suspense } from 'react';
 
 const Providers = lazy(() => import('@components/Common/Providers'));
 
 const App = ({ Component, pageProps }: AppProps) => {
-  useEffect(() => {
-    initLocale();
-  }, []);
-
   return (
-    <I18nProvider i18n={i18n}>
-      <Suspense fallback={<Loading />}>
-        <Providers>
-          <Component {...pageProps} />
-        </Providers>
-      </Suspense>
-    </I18nProvider>
+    <Suspense fallback={<Loading />}>
+      <Providers>
+        <Component {...pageProps} />
+      </Providers>
+    </Suspense>
   );
 };
 
