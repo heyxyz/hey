174006af568d93c07ba8ceffce99c7faaa7e6801
bigint <69431456+bigint@users.noreply.github.com>
2022-12-30 18:37:46 +0530

    chore: add more strings

--- apps/web/src/components/Profile/Badges/ProofOfHumanity.tsx
+++ apps/web/src/components/Profile/Badges/ProofOfHumanity.tsx
@@ -1,5 +1,6 @@
 import { Tooltip } from '@components/UI/Tooltip';
 import { CheckCircleIcon } from '@heroicons/react/solid';
+import { Trans } from '@lingui/macro';
 import { STATIC_IMAGES_URL } from 'data/constants';
 import type { Profile } from 'lens';
 import type { FC } from 'react';
@@ -17,7 +18,9 @@ const ProofOfHumanity: FC<Props> = ({ profile }) => {
     <Tooltip
       content={
         <span className="flex items-center space-x-1">
-          <span>Proof of Humanity verified</span>
+          <Trans>
+            <span>Proof of Humanity verified</span>
+          </Trans>
           <CheckCircleIcon className="h-4 w-4" />
         </span>
       }
