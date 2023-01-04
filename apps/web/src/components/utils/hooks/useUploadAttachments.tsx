85beb8bf34da424daa5fbbb0a937db45d327af8a
bigint <69431456+bigint@users.noreply.github.com>
2023-01-04 18:43:43 +0530

    fix: add image, video and audio upload limits (#1518)

--- apps/web/src/components/utils/hooks/useUploadAttachments.tsx
+++ apps/web/src/components/utils/hooks/useUploadAttachments.tsx
@@ -1,5 +1,6 @@
 import type { NewLensterAttachment } from '@generated/types';
 import uploadToIPFS from '@lib/uploadToIPFS';
+import { t } from '@lingui/macro';
 import { useCallback } from 'react';
 import { toast } from 'react-hot-toast';
 import { usePublicationStore } from 'src/store/publication';
@@ -16,6 +17,7 @@ const useUploadAttachments = () => {
       setIsUploading(true);
       const files = Array.from(attachments);
       const attachmentIds: string[] = [];
+
       const previewAttachments: NewLensterAttachment[] = files.map((file: any) => {
         const attachmentId = uuid();
         attachmentIds.push(attachmentId);
@@ -27,9 +29,39 @@ const useUploadAttachments = () => {
           previewItem: URL.createObjectURL(file)
         };
       });
+
+      const hasLargeAttachment = files.map((file: any) => {
+        const isImage = file.type.includes('image');
+        const isVideo = file.type.includes('video');
+        const isAudio = file.type.includes('audio');
+
+        if (isImage && file.size > 10000000) {
+          toast.error(t`Image size should be less than 10MB`);
+          return false;
+        }
+
+        if (isVideo && file.size > 50000000) {
+          toast.error(t`Video size should be less than 50MB`);
+          return false;
+        }
+
+        if (isAudio && file.size > 20000000) {
+          toast.error(t`Audio size should be less than 20MB`);
+          return false;
+        }
+
+        return true;
+      });
+
       addAttachments(previewAttachments);
       let attachmentsIPFS: NewLensterAttachment[] = [];
       try {
+        if (hasLargeAttachment.includes(false)) {
+          setIsUploading(false);
+          removeAttachments(attachmentIds);
+          return [];
+        }
+
         const attachmentsUploaded = await uploadToIPFS(attachments);
         if (attachmentsUploaded) {
           attachmentsIPFS = previewAttachments.map((attachment: NewLensterAttachment, index: number) => ({
