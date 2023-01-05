1ba5b5ce40083e51ee72b0ced8c821b8eb742ca1
bigint <69431456+bigint@users.noreply.github.com>
2023-01-05 19:20:05 +0530

    feat: persists notification tab clicks in URL query params even after… (#1537)

---
+++ apps/web/src/components/Notification/FeedType.tsx
@@ -0,0 +1,62 @@
+import TabButton from '@components/UI/TabButton';
+import { AtSymbolIcon, ChatAlt2Icon, CollectionIcon, LightningBoltIcon } from '@heroicons/react/outline';
+import { Analytics } from '@lib/analytics';
+import { t } from '@lingui/macro';
+import type { Dispatch, FC } from 'react';
+import { NOTIFICATION } from 'src/tracking';
+
+interface Props {
+  setFeedType: Dispatch<string>;
+  feedType: string;
+}
+
+const FeedType: FC<Props> = ({ setFeedType, feedType }) => {
+  return (
+    <div className="flex justify-between items-center">
+      <div className="flex overflow-x-auto gap-3 px-5 pb-2 mt-3 sm:px-0 sm:mt-0 md:pb-0">
+        <TabButton
+          name={t`All notifications`}
+          icon={<LightningBoltIcon className="w-4 h-4" />}
+          active={feedType === 'ALL'}
+          type="all"
+          onClick={() => {
+            setFeedType('ALL');
+            Analytics.track(NOTIFICATION.SWITCH_ALL);
+          }}
+        />
+        <TabButton
+          name={t`Mentions`}
+          icon={<AtSymbolIcon className="w-4 h-4" />}
+          active={feedType === 'MENTIONS'}
+          type="mentions"
+          onClick={() => {
+            setFeedType('MENTIONS');
+            Analytics.track(NOTIFICATION.SWITCH_MENTIONS);
+          }}
+        />
+        <TabButton
+          name={t`Comments`}
+          icon={<ChatAlt2Icon className="w-4 h-4" />}
+          active={feedType === 'COMMENTS'}
+          type="comments"
+          onClick={() => {
+            setFeedType('COMMENTS');
+            Analytics.track(NOTIFICATION.SWITCH_COMMENTS);
+          }}
+        />
+        <TabButton
+          name={t`Collects`}
+          icon={<CollectionIcon className="w-4 h-4" />}
+          active={feedType === 'COLLECTS'}
+          type="collects"
+          onClick={() => {
+            setFeedType('COLLECTS');
+            Analytics.track(NOTIFICATION.SWITCH_COLLECTS);
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default FeedType;
--- apps/web/src/components/Notification/index.tsx
+++ apps/web/src/components/Notification/index.tsx
@@ -1,20 +1,24 @@
 import MetaTags from '@components/Common/MetaTags';
-import TabButton from '@components/UI/TabButton';
-import { AtSymbolIcon, ChatAlt2Icon, CollectionIcon, LightningBoltIcon } from '@heroicons/react/outline';
-import { Analytics } from '@lib/analytics';
-import { t } from '@lingui/macro';
 import { APP_NAME } from 'data/constants';
+import { useRouter } from 'next/router';
 import type { FC } from 'react';
 import { useState } from 'react';
 import Custom404 from 'src/pages/404';
 import { useAppStore } from 'src/store/app';
-import { NOTIFICATION } from 'src/tracking';
 
+import FeedType from './FeedType';
 import List from './List';
 
 const Notification: FC = () => {
+  const {
+    query: { type }
+  } = useRouter();
   const currentProfile = useAppStore((state) => state.currentProfile);
-  const [feedType, setFeedType] = useState<'ALL' | 'MENTIONS' | 'COMMENTS' | 'COLLECTS'>('ALL');
+  const [feedType, setFeedType] = useState(
+    type && ['all', 'mentions', 'comments', 'collects'].includes(type as string)
+      ? type.toString().toUpperCase()
+      : 'ALL'
+  );
 
   if (!currentProfile) {
     return <Custom404 />;
@@ -25,42 +29,7 @@ const Notification: FC = () => {
       <MetaTags title={`Notifications • ${APP_NAME}`} />
       <div className="max-w-4xl w-full space-y-3">
         <div className="flex gap-3 pb-2">
-          <TabButton
-            name={t`All notifications`}
-            icon={<LightningBoltIcon className="w-4 h-4" />}
-            active={feedType === 'ALL'}
-            onClick={() => {
-              setFeedType('ALL');
-              Analytics.track(NOTIFICATION.SWITCH_ALL);
-            }}
-          />
-          <TabButton
-            name={t`Mentions`}
-            icon={<AtSymbolIcon className="w-4 h-4" />}
-            active={feedType === 'MENTIONS'}
-            onClick={() => {
-              setFeedType('MENTIONS');
-              Analytics.track(NOTIFICATION.SWITCH_MENTIONS);
-            }}
-          />
-          <TabButton
-            name={t`Comments`}
-            icon={<ChatAlt2Icon className="w-4 h-4" />}
-            active={feedType === 'COMMENTS'}
-            onClick={() => {
-              setFeedType('COMMENTS');
-              Analytics.track(NOTIFICATION.SWITCH_COMMENTS);
-            }}
-          />
-          <TabButton
-            name={t`Collects`}
-            icon={<CollectionIcon className="w-4 h-4" />}
-            active={feedType === 'COLLECTS'}
-            onClick={() => {
-              setFeedType('COLLECTS');
-              Analytics.track(NOTIFICATION.SWITCH_COLLECTS);
-            }}
-          />
+          <FeedType setFeedType={setFeedType} feedType={feedType} />
         </div>
         <List feedType={feedType} />
       </div>
