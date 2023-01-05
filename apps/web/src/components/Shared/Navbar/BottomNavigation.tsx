b48cc40b3b303c8481cd4094fc7f3be81ecab3e1
Sasi <me@sasi.codes>
2023-01-05 16:30:56 +0530

    chore: add bottom navbar and header changes (#1533)
    Co-authored-by: bigint <69431456+bigint@users.noreply.github.com>

---
+++ apps/web/src/components/Shared/Navbar/BottomNavigation.tsx
@@ -0,0 +1,52 @@
+import { HomeIcon, LightningBoltIcon, MailIcon, ViewGridIcon } from '@heroicons/react/outline';
+import {
+  HomeIcon as HomeIconSolid,
+  LightningBoltIcon as LightningBoltIconSolid,
+  MailIcon as MailIconSolid,
+  ViewGridIcon as ViewGridIconSolid
+} from '@heroicons/react/solid';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React from 'react';
+
+const BottomNavigation = () => {
+  const router = useRouter();
+  const isActivePath = (path: string) => router.pathname === path;
+
+  return (
+    <div className="fixed pb-safe inset-x-0 bg-white border-t border-gray-200 dark:border-gray-800 dark:bg-black bottom-0 z-10 md:hidden">
+      <div className="grid grid-cols-4">
+        <Link href="/" className="my-3 mx-auto">
+          {isActivePath('/') ? (
+            <HomeIconSolid className="w-6 h-6 text-indigo-500" />
+          ) : (
+            <HomeIcon className="w-6 h-6" />
+          )}
+        </Link>
+        <Link href="/explore" className="my-3 mx-auto">
+          {isActivePath('/explore') ? (
+            <ViewGridIconSolid className="w-6 h-6 text-indigo-500" />
+          ) : (
+            <ViewGridIcon className="w-6 h-6" />
+          )}
+        </Link>
+        <Link href="/notifications" className="my-3 mx-auto">
+          {isActivePath('/notifications') ? (
+            <LightningBoltIconSolid className="w-6 h-6 text-indigo-500" />
+          ) : (
+            <LightningBoltIcon className="w-6 h-6" />
+          )}
+        </Link>
+        <Link href="/messages" className="my-3 mx-auto">
+          {isActivePath('/messages') ? (
+            <MailIconSolid className="w-6 h-6 text-indigo-500" />
+          ) : (
+            <MailIcon className="w-6 h-6" />
+          )}
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default BottomNavigation;
