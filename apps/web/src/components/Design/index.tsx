a22b502294dccdff61b2a9d3777f632f4502d5ef
bigint <69431456+bigint@users.noreply.github.com>
2023-01-06 13:46:41 +0530

    chore: add metatag to design

--- apps/web/src/components/Design/index.tsx
+++ apps/web/src/components/Design/index.tsx
@@ -1,4 +1,6 @@
+import MetaTags from '@components/Common/MetaTags';
 import { GridLayout } from '@components/UI/GridLayout';
+import { APP_NAME } from 'data/constants';
 import type { NextPage } from 'next';
 
 import Colors from './Colors';
@@ -7,10 +9,12 @@ import Typography from './Typography';
 const Design: NextPage = () => {
   return (
     <GridLayout>
+      <MetaTags title={`Design • ${APP_NAME}`} />
       <div className="col-span-12">
         <Typography />
-        <div className="divider my-5" />
+        <div className="divider my-6" />
         <Colors />
+        <div className="divider my-6" />
       </div>
     </GridLayout>
   );
