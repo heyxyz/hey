e3eca2aec6dcfff10ad2a66c0897f9d5728232ce
Sasi <me@sasi.codes>
2023-01-11 15:33:37 +0530

    Update apps/web/src/components/Shared/Navbar/MobileDrawerMenu.tsx
    Co-authored-by: Strek <ssharishkumar@gmail.com>

--- apps/web/src/components/Shared/Navbar/MobileDrawerMenu.tsx
+++ apps/web/src/components/Shared/Navbar/MobileDrawerMenu.tsx
@@ -82,7 +82,7 @@ const MobileDrawerMenu: FC<Props> = ({ mobileDrawerRef }) => {
           <div className="mx-5 my-2">
             <YourProfile onClick={closeDrawer} className="py-4" />
             <Settings onClick={closeDrawer} className="py-4" />
-            {isGardener(currentProfile?.id) && <Mod onClick={() => closeDrawer()} className="py-4" />}
+            {isGardener(currentProfile?.id) && <Mod onClick={closeDrawer} className="py-4" />}
             <ThemeSwitch className="py-4" onClick={() => closeDrawer()} />
           </div>
           <div className="divider" />
