e34e106bda45147caf5578647eeb39f6ac2c6ae4
Sasi <me@sasi.codes>
2023-01-12 15:59:36 +0530

    chore: pr comments

--- apps/web/src/components/Shared/SwitchProfiles.tsx
+++ apps/web/src/components/Shared/SwitchProfiles.tsx
@@ -5,6 +5,7 @@ import getAvatar from '@lib/getAvatar';
 import type { Profile } from 'lens';
 import React from 'react';
 import { useAppPersistStore, useAppStore } from 'src/store/app';
+import { useGlobalModalStateStore } from 'src/store/modals';
 import { PROFILE } from 'src/tracking';
 
 const SwitchProfiles = () => {
@@ -12,14 +13,12 @@ const SwitchProfiles = () => {
   const currentProfile = useAppStore((state) => state.currentProfile);
   const setCurrentProfile = useAppStore((state) => state.setCurrentProfile);
   const setProfileId = useAppPersistStore((state) => state.setProfileId);
+  const setShowProfileSwitchModal = useGlobalModalStateStore((state) => state.setShowProfileSwitchModal);
 
   return (
     <div className="py-2">
       {profiles.map((profile: Profile, index) => (
-        <div
-          key={profile?.id}
-          className="block text-sm text-gray-700 rounded-lg cursor-pointer dark:text-gray-200"
-        >
+        <div key={profile?.id} className="text-gray-700 px-2 rounded-lg cursor-pointer dark:text-gray-200">
           <button
             type="button"
             className="flex items-center py-1.5 px-4 space-x-2 w-full rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800"
@@ -28,6 +27,7 @@ const SwitchProfiles = () => {
               setCurrentProfile(selectedProfile);
               setProfileId(selectedProfile.id);
               Analytics.track(PROFILE.SWITCH_PROFILE);
+              setShowProfileSwitchModal(false);
             }}
           >
             {currentProfile?.id === profile?.id && <CheckCircleIcon className="w-4 h-4 text-green-500" />}
