086836c678144937b5ee7ae12ccf8e65aac2ec57
bigint <69431456+bigint@users.noreply.github.com>
2023-01-12 15:33:04 +0530

    chore: refactor profile metadata (#1597)

--- apps/web/src/components/Settings/Profile/Profile.tsx
+++ apps/web/src/components/Settings/Profile/Profile.tsx
@@ -180,27 +180,17 @@ const ProfileSettingsForm: FC<Props> = ({ profile }) => {
                   'app'
                 ].includes(attr.key)
             )
-            .map(({ traitType, key, value }) => ({ traitType, key, value })) ?? []),
-          { traitType: 'string', key: 'location', value: location },
-          { traitType: 'string', key: 'website', value: website },
-          { traitType: 'string', key: 'twitter', value: twitter },
-          { traitType: 'boolean', key: 'hasPrideLogo', value: pride },
-          {
-            traitType: 'string',
-            key: 'statusEmoji',
-            value: getAttribute(profile?.attributes, 'statusEmoji')
-          },
-          {
-            traitType: 'string',
-            key: 'statusMessage',
-            value: getAttribute(profile?.attributes, 'statusMessage')
-          },
-          { traitType: 'string', key: 'app', value: APP_NAME }
+            .map(({ key, value }) => ({ key, value })) ?? []),
+          { key: 'location', value: location },
+          { key: 'website', value: website },
+          { key: 'twitter', value: twitter },
+          { key: 'hasPrideLogo', value: pride },
+          { key: 'statusEmoji', value: getAttribute(profile?.attributes, 'statusEmoji') },
+          { key: 'statusMessage', value: getAttribute(profile?.attributes, 'statusMessage') },
+          { key: 'app', value: APP_NAME }
         ],
         version: '1.0.0',
-        metadata_id: uuid(),
-        createdOn: new Date(),
-        appId: APP_NAME
+        metadata_id: uuid()
       }).finally(() => setIsUploading(false));
 
       const request = {
