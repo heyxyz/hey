0f65483004c967bf8fc50ebdc04669a449bc160e
bigint <69431456+bigint@users.noreply.github.com>
2023-01-13 11:20:57 +0530

    feat: add lock icon for gated publications (#1605)

--- apps/web/src/components/Publication/Actions/index.tsx
+++ apps/web/src/components/Publication/Actions/index.tsx
@@ -1,5 +1,7 @@
+import { Tooltip } from '@components/UI/Tooltip';
 import type { LensterPublication } from '@generated/types';
-import clsx from 'clsx';
+import { LockClosedIcon } from '@heroicons/react/solid';
+import { t } from '@lingui/macro';
 import type { ElectedMirror } from 'lens';
 import type { FC } from 'react';
 import { useAppStore } from 'src/store/app';
@@ -12,37 +14,36 @@ import Mirror from './Mirror';
 
 interface Props {
   publication: LensterPublication;
-  isFullPublication?: boolean;
   electedMirror?: ElectedMirror;
 }
 
-const PublicationActions: FC<Props> = ({ publication, electedMirror, isFullPublication = false }) => {
+const PublicationActions: FC<Props> = ({ publication, electedMirror }) => {
   const currentProfile = useAppStore((state) => state.currentProfile);
   const collectModuleType = publication?.collectModule.__typename;
   const canMirror = currentProfile ? publication?.canMirror?.result : true;
 
   return (
-    <span
-      className={clsx(
-        { 'justify-between': isFullPublication },
-        'flex gap-6 items-center pt-3 -ml-2 lt-text-gray-500 sm:gap-8'
+    <div className="flex items-center justify-between pt-3 -ml-2">
+      <span
+        className="flex items-center gap-6 sm:gap-8"
+        onClick={(event) => {
+          event.stopPropagation();
+        }}
+      >
+        <Comment publication={publication} />
+        {canMirror && <Mirror publication={publication} />}
+        <Like publication={publication} />
+        {collectModuleType !== 'RevertCollectModuleSettings' && (
+          <Collect electedMirror={electedMirror} publication={publication} />
+        )}
+        <Analytics publication={publication} />
+      </span>
+      {publication.isGated && (
+        <Tooltip placement="top" content={t`Gated Publication`}>
+          <LockClosedIcon className="h-4 w-4 text-green-500" />
+        </Tooltip>
       )}
-      onClick={(event) => {
-        event.stopPropagation();
-      }}
-    >
-      <Comment publication={publication} isFullPublication={isFullPublication} />
-      {canMirror && <Mirror publication={publication} isFullPublication={isFullPublication} />}
-      <Like publication={publication} isFullPublication={isFullPublication} />
-      {collectModuleType !== 'RevertCollectModuleSettings' && (
-        <Collect
-          electedMirror={electedMirror}
-          publication={publication}
-          isFullPublication={isFullPublication}
-        />
-      )}
-      <Analytics publication={publication} isFullPublication={isFullPublication} />
-    </span>
+    </div>
   );
 };
 
