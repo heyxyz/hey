82e0d6e1ca32ee4a2459e3198f3b4bbf60958c9b
bigint <69431456+bigint@users.noreply.github.com>
2023-01-17 19:04:37 +0530

    chore: add gitcoin path

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -38,6 +38,12 @@ module.exports = withPWA({
         source: '/donate',
         destination: 'https://gitcoin.co/grants/5007/lenster',
         permanent: true
+      },
+      {
+        source: '/gitcoin',
+        destination:
+          'https://grant-explorer.gitcoin.co/#/round/1/0xd95a1969c41112cee9a2c931e849bcef36a16f4c/0xbe519f8c8f7ff2acb359dd757c5d2bf25b05d9fd29b8684885aa27f1fa487fcf-0xd95a1969c41112cee9a2c931e849bcef36a16f4c',
+        permanent: true
       }
     ];
   },
