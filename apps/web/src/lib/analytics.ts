8486ac5760c1696a437730022a44eb7b5b9fcd5b
bigint <69431456+bigint@users.noreply.github.com>
2023-01-18 13:21:01 +0530

    chore: add metadata support

--- apps/web/src/lib/analytics.ts
+++ apps/web/src/lib/analytics.ts
@@ -4,10 +4,10 @@ const isBrowser = typeof window !== 'undefined';
  * Simple Analytics
  */
 export const Analytics = {
-  track: (name: string) => {
+  track: (name: string, metadata?: Object) => {
     if (isBrowser) {
       try {
-        (window as any)?.sa_event?.(name);
+        (window as any)?.sa_event?.(name, metadata);
       } catch {
         console.error('Error while sending analytics event to simple analytics');
       }
