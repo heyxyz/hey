5bdf4a7686d793e18fec48c85377b229a3775299
bigint <69431456+bigint@users.noreply.github.com>
2023-01-18 12:22:41 +0530

    revert: remove transpilePackages

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -8,6 +8,7 @@ const withPWA = require('next-pwa')({
 module.exports = withPWA({
   reactStrictMode: false,
   trailingSlash: false,
+  transpilePackages: ['data'],
   experimental: {
     scrollRestoration: true,
     newNextLinkBehavior: true
