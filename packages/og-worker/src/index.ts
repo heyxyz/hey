42356d21f2a373312fe01f68fe68bb1c56b217c0
bigint <69431456+bigint@users.noreply.github.com>
2023-01-25 20:44:46 +0530

    feat: add og-worker and media-worker service (#1678)

---
+++ packages/og-worker/src/index.ts
@@ -0,0 +1,20 @@
+export default {
+  async fetch(request: Request) {
+    return await handleRequest(request);
+  }
+};
+
+async function handleRequest(request: Request) {
+  const userAgent = request.headers.get('User-Agent') || '';
+  const re = /bot|telegram|baidu|bing|yandex|iframely|whatsapp|facebook/i;
+
+  if (re.test(userAgent)) {
+    const url = new URL(request.url);
+    const path = url.pathname;
+    const res = await fetch(`https://api.lenster.xyz/og?uri=${path}`);
+
+    return res;
+  }
+
+  return fetch(request);
+}
