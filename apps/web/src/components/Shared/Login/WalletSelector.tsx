dde32c3b2f5c9775601638964d4251cb19c9cde3
Olof Andersson <olof.andersson@gmail.com>
2023-01-28 13:31:52 +0100

    start signing automatically

--- apps/web/src/components/Shared/Login/WalletSelector.tsx
+++ apps/web/src/components/Shared/Login/WalletSelector.tsx
@@ -1,7 +1,6 @@
 import SwitchNetwork from '@components/Shared/SwitchNetwork';
 import { Button } from '@components/UI/Button';
 import { Spinner } from '@components/UI/Spinner';
-import { KeyIcon } from '@heroicons/react/outline';
 import { XCircleIcon } from '@heroicons/react/solid';
 import { Analytics } from '@lib/analytics';
 import onError from '@lib/onError';
@@ -9,13 +8,13 @@ import { Trans } from '@lingui/macro';
 import { ERROR_MESSAGE } from 'data/constants';
 import { useAuthenticateMutation, useChallengeLazyQuery, useUserProfilesLazyQuery } from 'lens';
 import type { Dispatch, FC } from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 import { CHAIN_ID } from 'src/constants';
 import { useAppPersistStore, useAppStore } from 'src/store/app';
 import { useAuthStore } from 'src/store/auth';
 import { USER } from 'src/tracking';
-import { useAccount, useDisconnect, useNetwork, useSignMessage } from 'wagmi';
+import { useAccount, useNetwork, useSignMessage } from 'wagmi';
 
 interface Props {
   setHasProfile: Dispatch<boolean>;
@@ -29,8 +28,7 @@ const WalletSelector: FC<Props> = ({ setHasProfile }) => {
   const [loading, setLoading] = useState(false);
 
   const { chain } = useNetwork();
-  const { disconnect } = useDisconnect();
-  const { address } = useAccount();
+  const { address, connector, isConnected } = useAccount();
   const { signMessageAsync } = useSignMessage({ onError });
   const [loadChallenge, { error: errorChallenge }] = useChallengeLazyQuery({
     fetchPolicy: 'no-cache'
@@ -92,6 +90,13 @@ const WalletSelector: FC<Props> = ({ setHasProfile }) => {
     }
   };
 
+  useEffect(() => {
+    if (connector?.id && isConnected) {
+      handleSign();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isConnected]);
+
   return (
     <div className="space-y-3">
       <div className="space-y-2.5">
@@ -112,18 +117,6 @@ const WalletSelector: FC<Props> = ({ setHasProfile }) => {
         ) : (
           <SwitchNetwork />
         )}
-        <button
-          onClick={() => {
-            disconnect?.();
-            Analytics.track(USER.CHANGE_WALLET);
-          }}
-          className="text-sm underline flex items-center space-x-1"
-        >
-          <KeyIcon className="h-4 w-4" />
-          <div>
-            <Trans>Change wallet</Trans>
-          </div>
-        </button>
       </div>
       {(errorChallenge || errorAuthenticate || errorProfiles) && (
         <div className="flex items-center space-x-1 font-bold text-red-500">
--- apps/web/src/components/Shared/Navbar/LoginButton.tsx
+++ apps/web/src/components/Shared/Navbar/LoginButton.tsx
@@ -3,26 +3,41 @@ import { Analytics } from '@lib/analytics';
 import { Trans } from '@lingui/macro';
 import { useConnectModal } from '@rainbow-me/rainbowkit';
 import type { FC } from 'react';
+import toast from 'react-hot-toast';
 import { useAuthStore } from 'src/store/auth';
 import { USER } from 'src/tracking';
+import { useAccount, useDisconnect } from 'wagmi';
 
 const LoginButton: FC = () => {
   const setShowLoginFlow = useAuthStore((state) => state.setShowLoginFlow);
   const { openConnectModal } = useConnectModal();
+  const { isConnected } = useAccount();
+  const { disconnect } = useDisconnect({
+    onError(error) {
+      toast.error(error?.message);
+    }
+  });
 
   return (
-    <Button
-      icon={<img className="mr-0.5 w-4 h-4" height={16} width={16} src="/lens.png" alt="Lens Logo" />}
-      onClick={() => {
-        if (openConnectModal) {
-          openConnectModal();
-        }
-        setShowLoginFlow(true);
-        Analytics.track(USER.LOGIN);
-      }}
-    >
-      <Trans>Login</Trans>
-    </Button>
+    <>
+      <Button
+        icon={<img className="mr-0.5 w-4 h-4" height={16} width={16} src="/lens.png" alt="Lens Logo" />}
+        onClick={() => {
+          if (openConnectModal) {
+            openConnectModal();
+          }
+          setShowLoginFlow(true);
+          Analytics.track(USER.LOGIN);
+        }}
+      >
+        <Trans>Login</Trans>
+      </Button>
+      {isConnected && (
+        <Button title="Disconnect wallet" onClick={() => disconnect?.()}>
+          Disconnect wallet
+        </Button>
+      )}
+    </>
   );
 };
 
