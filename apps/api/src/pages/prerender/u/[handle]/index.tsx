53ab7dde45f1d41dc525be548d142d766d89a4bd
bigint <69431456+bigint@users.noreply.github.com>
2023-01-29 20:01:34 +0530

    feat: add profile prerender

---
+++ apps/api/src/pages/prerender/u/[handle]/index.tsx
@@ -0,0 +1,32 @@
+import Profile from '@components/Profile';
+import { HANDLE_SUFFIX, LENSPROTOCOL_HANDLE } from 'data/constants';
+import { PrerenderProfileDocument } from 'lens';
+import type { GetServerSidePropsContext } from 'next';
+import client from 'src/apollo';
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const handle = context.params?.handle;
+
+  if (!handle) {
+    return {
+      props: { data: null }
+    };
+  }
+
+  let processedHandle;
+  if (handle.includes(HANDLE_SUFFIX)) {
+    processedHandle = handle;
+  } else {
+    processedHandle = handle === LENSPROTOCOL_HANDLE ? handle : handle.concat(HANDLE_SUFFIX);
+  }
+  const { data } = await client.query({
+    query: PrerenderProfileDocument,
+    variables: { request: { handle: processedHandle } }
+  });
+
+  return {
+    props: { profile: data?.profile }
+  };
+}
+
+export default Profile;
