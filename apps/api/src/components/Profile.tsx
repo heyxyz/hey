00877d72975b2de6d1248a4cf3e4e2bf25f52840
bigint <69431456+bigint@users.noreply.github.com>
2023-01-29 23:50:34 +0530

    feat: add schema.org for user

--- apps/api/src/components/Profile.tsx
+++ apps/api/src/components/Profile.tsx
@@ -4,6 +4,7 @@ import { OG_MEDIA_PROXY_URL } from 'data/constants';
 import type { MediaSet, NftImage } from 'lens';
 import { Profile } from 'lens';
 import type { FC } from 'react';
+import { JsonLd } from 'react-schemaorg';
 
 import DefaultTags from './Shared/DefaultTags';
 import Tags from './Shared/Tags';
@@ -30,6 +31,45 @@ const Profile: FC<Props> = ({ profile }) => {
   return (
     <>
       <Tags title={title} description={description} image={image} />
+      <JsonLd<any>
+        item={{
+          '@context': 'https://schema.org',
+          '@type': 'ProfilePage',
+          author: {
+            '@type': 'Person',
+            additionalName: profile.handle,
+            description: profile.bio,
+            givenName: profile.name ?? profile.handle,
+            identifier: profile.id,
+            image: {
+              '@type': 'ImageObject',
+              contentUrl: image,
+              thumbnailUrl: image
+            },
+            interactionStatistic: [
+              {
+                '@type': 'InteractionCounter',
+                interactionType: 'https://schema.org/FollowAction',
+                name: 'Follows',
+                userInteractionCount: profile.stats.totalFollowers
+              },
+              {
+                '@type': 'InteractionCounter',
+                interactionType: 'https://schema.org/SubscribeAction',
+                name: 'Following',
+                userInteractionCount: profile.stats.totalFollowing
+              },
+              {
+                '@type': 'InteractionCounter',
+                interactionType: 'https://schema.org/WriteAction',
+                name: 'Posts',
+                userInteractionCount: profile.stats.totalPosts
+              }
+            ],
+            url: `https://lenster.xyz/u/${profile.handle}}`
+          }
+        }}
+      />
       <div>{title}</div>
     </>
   );
