27af715b2761a1a92f03cf42a46faaf7d4c5083b
Sasi <me@sasi.codes>
2023-02-03 19:04:35 +0530

    chore: cleanup

--- apps/web/src/components/Profile/NftGallery/Picker.tsx
+++ apps/web/src/components/Profile/NftGallery/Picker.tsx
@@ -4,7 +4,6 @@ import { EmptyState } from '@components/UI/EmptyState';
 import { ErrorMessage } from '@components/UI/ErrorMessage';
 import InfiniteLoader from '@components/UI/InfiniteLoader';
 import { Input } from '@components/UI/Input';
-import { Tab } from '@headlessui/react';
 import { CheckIcon, CollectionIcon, SearchIcon, XIcon } from '@heroicons/react/outline';
 import formatHandle from '@lib/formatHandle';
 import { t, Trans } from '@lingui/macro';
@@ -19,8 +18,6 @@ import { CHAIN_ID } from 'src/constants';
 import { useAppStore } from 'src/store/app';
 import { useNftGalleryStore } from 'src/store/nft-gallery';
 import { mainnet } from 'wagmi/chains';
-
-const tabs = [{ name: 'Recent' }, { name: 'Floor price' }, { name: 'A-Z' }];
 
 const Picker: FC = () => {
   const [searchText, setSearchText] = useState('');
@@ -115,23 +112,6 @@ const Picker: FC = () => {
         }
         onChange={handleSearch}
       />
-      <Tab.Group>
-        <Tab.List className="space-x-2">
-          {tabs.map((tab, index) => (
-            <Tab
-              key={index}
-              className={clsx(
-                'Recent' === tab.name ? 'bg-brand-500 text-white' : 'bg-brand-100 text-brand',
-                'bg-brand-100 border-brand-300 dark:border-brand-500 rounded-full border px-3 py-1.5 text-xs dark:bg-opacity-20 sm:px-4'
-              )}
-            >
-              <span className="flex items-center space-x-2">
-                <span className="hidden sm:block">{tab.name}</span>
-              </span>
-            </Tab>
-          ))}
-        </Tab.List>
-      </Tab.Group>
       <InfiniteScroll
         dataLength={nfts?.length ?? 0}
         scrollThreshold={SCROLL_THRESHOLD}
