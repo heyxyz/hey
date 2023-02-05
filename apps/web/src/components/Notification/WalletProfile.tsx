4f977db036709b33ca868937f5dd8df70d3d3dc0
bigint <69431456+bigint@users.noreply.github.com>
2023-02-05 22:44:12 +0530

    fix: remove stamp urls from skiplist

--- apps/web/src/components/Notification/WalletProfile.tsx
+++ apps/web/src/components/Notification/WalletProfile.tsx
@@ -1,5 +1,6 @@
 import formatAddress from '@lib/formatAddress';
 import getStampFyiURL from '@lib/getStampFyiURL';
+import imageProxy from '@lib/imageProxy';
 import { POLYGONSCAN_URL } from 'data/constants';
 import type { Wallet } from 'lens';
 import type { FC } from 'react';
@@ -15,7 +16,7 @@ export const NotificationWalletProfileAvatar: FC<Props> = ({ wallet }) => {
         onError={({ currentTarget }) => {
           currentTarget.src = getStampFyiURL(wallet?.address);
         }}
-        src={getStampFyiURL(wallet?.address)}
+        src={imageProxy(getStampFyiURL(wallet?.address))}
         className="h-8 w-8 rounded-full border bg-gray-200 dark:border-gray-700"
         height={32}
         width={32}
