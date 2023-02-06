aa359446209af1f8f078270c9853f180c35e71b6
bigint <69431456+bigint@users.noreply.github.com>
2023-02-06 16:17:21 +0530

    chore: move getStampFyiURL to utils

--- apps/web/src/components/Notification/WalletProfile.tsx
+++ apps/web/src/components/Notification/WalletProfile.tsx
@@ -1,9 +1,9 @@
 import formatAddress from '@lib/formatAddress';
-import getStampFyiURL from '@lib/getStampFyiURL';
 import imageProxy from '@lib/imageProxy';
 import { POLYGONSCAN_URL } from 'data/constants';
 import type { Wallet } from 'lens';
 import type { FC } from 'react';
+import getStampFyiURL from 'utils/getStampFyiURL';
 
 interface Props {
   wallet: Wallet;
--- apps/web/src/components/Shared/Lexical/Plugins/AtMentionsPlugin.tsx
+++ apps/web/src/components/Shared/Lexical/Plugins/AtMentionsPlugin.tsx
@@ -6,7 +6,6 @@ import {
   useBasicTypeaheadTriggerMatch
 } from '@lexical/react/LexicalTypeaheadMenuPlugin';
 import formatHandle from '@lib/formatHandle';
-import getStampFyiURL from '@lib/getStampFyiURL';
 import imageProxy from '@lib/imageProxy';
 import { AVATAR } from 'data/constants';
 import type { MediaSet, NftImage, Profile, ProfileSearchResult } from 'lens';
@@ -16,6 +15,7 @@ import type { FC } from 'react';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import * as ReactDOM from 'react-dom';
 import getIPFSLink from 'utils/getIPFSLink';
+import getStampFyiURL from 'utils/getStampFyiURL';
 
 import { $createMentionNode } from '../Nodes/MentionsNode';
 
