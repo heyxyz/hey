578cadcb525fd94800addb51f3eb988c5b7aa504
bigint <69431456+bigint@users.noreply.github.com>
2023-02-06 17:59:26 +0530

    fix: CI

--- apps/web/src/lib/getAvatar.ts
+++ apps/web/src/lib/getAvatar.ts
@@ -1,7 +1,7 @@
 import { AVATAR, ZERO_ADDRESS } from 'data/constants';
 import getIPFSLink from 'utils/getIPFSLink';
+import getStampFyiURL from 'utils/getStampFyiURL';
 
-import getStampFyiURL from './getStampFyiURL';
 import imageProxy from './imageProxy';
 
 const skipList = [
