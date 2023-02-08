46470b4465d8993d73ae6ce829a6efa74e5510a9
bigint <69431456+bigint@users.noreply.github.com>
2023-02-08 22:21:29 +0530

    feat: add leafwatch worker (#1765)

---
+++ packages/leafwatch-worker/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: ['weblint'],
+  rules: {
+    'import/no-anonymous-default-export': 'off',
+    'import/no-anonymous-default-export': 'off',
+    'no-use-before-define': 'off'
+  }
+};
---
+++ packages/leafwatch-worker/src/index.ts
@@ -0,0 +1,64 @@
+import { uuid } from '@cfworker/uuid';
+
+type EnvType = {
+  DATADOG_TOKEN: string;
+};
+
+export default {
+  async fetch(request: Request, env: EnvType) {
+    return await handleRequest(request, env);
+  }
+};
+
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Content-Type': 'application/json'
+};
+
+const handleRequest = async (request: Request, env: EnvType) => {
+  if (request.method !== 'POST') {
+    return new Response(JSON.stringify({ success: false, message: 'Only POST requests are supported' }), {
+      headers
+    });
+  }
+
+  if (request.headers.get('origin') !== 'https://lenster.xyz') {
+    return new Response(JSON.stringify({ success: false, message: 'Origin not allowed' }), { headers });
+  }
+
+  const payload = await request.json();
+
+  if (!payload) {
+    return new Response(JSON.stringify({ success: false, message: 'No body provided' }), { headers });
+  }
+
+  try {
+    const appenedPayload = {
+      ...payload,
+      ddsource: 'leafwatch',
+      ip: request.headers.get('cf-connecting-ip') || 'unknown'
+    };
+    const datadogRes = await fetch(
+      `https://http-intake.logs.datadoghq.com/api/v2/logs?dd-api-key=${
+        env.DATADOG_TOKEN
+      }&dd-request-id=${uuid()}`,
+      {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: appenedPayload
+      }
+    );
+
+    return new Response(
+      JSON.stringify({
+        success: true,
+        payload: appenedPayload,
+        ddResponse: await datadogRes.json()
+      }),
+      { headers }
+    );
+  } catch {
+    return new Response(JSON.stringify({ success: false, message: 'Something went wrong!' }), { headers });
+  }
+};
