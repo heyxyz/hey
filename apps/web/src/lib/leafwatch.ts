62dfa1b77ee8d7aede7b579b0203fb2dc604ebd7
bigint <69431456+bigint@users.noreply.github.com>
2023-02-09 17:50:19 +0530

    feat: add proper datadog error logging (#1773)

--- apps/web/src/lib/leafwatch.ts
+++ apps/web/src/lib/leafwatch.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { LEAFWATCH_WORKER_URL } from 'data/constants';
+import { RAVEN_WORKER_URL } from 'data/constants';
 
 const isBrowser = typeof window !== 'undefined';
 
@@ -13,7 +13,7 @@ export const Leafwatch = {
     );
 
     if (isBrowser) {
-      axios(LEAFWATCH_WORKER_URL, {
+      axios(RAVEN_WORKER_URL, {
         method: 'POST',
         data: {
           event: name,
@@ -21,7 +21,8 @@ export const Leafwatch = {
           profile: state.profileId,
           url: location.href,
           referrer: document.referrer,
-          userAgent: navigator.userAgent
+          userAgent: navigator.userAgent,
+          ddsource: 'leafwatch'
         }
       }).catch(() => {
         console.error('Error while sending analytics event to Leafwatch');
