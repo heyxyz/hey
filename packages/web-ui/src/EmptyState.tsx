bfd1db252d80c703a319b027fceb8029a1fb491c
bigint <69431456+bigint@users.noreply.github.com>
2023-02-09 14:11:01 +0530

    feat: add web-ui package

---
+++ packages/web-ui/src/EmptyState.tsx
@@ -0,0 +1,19 @@
+import type { FC, ReactNode } from 'react';
+import { Card } from 'web-ui';
+
+interface Props {
+  message: ReactNode;
+  icon: ReactNode;
+  hideCard?: boolean;
+}
+
+export const EmptyState: FC<Props> = ({ message, icon, hideCard = false }) => {
+  return (
+    <Card className={hideCard ? 'border-0 !bg-transparent !shadow-none' : ''}>
+      <div className="grid justify-items-center space-y-2 p-5">
+        <div>{icon}</div>
+        <div>{message}</div>
+      </div>
+    </Card>
+  );
+};
