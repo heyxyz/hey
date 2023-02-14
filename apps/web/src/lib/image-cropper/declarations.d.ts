c3e48e808775c064a5ec6934f7424fbab0567d03
Olof Andersson <olof.andersson@gmail.com>
2023-02-14 18:07:42 +0100

    remove unused

--- apps/web/src/lib/image-cropper/declarations.d.ts
+++ apps/web/src/lib/image-cropper/declarations.d.ts
@@ -1,8 +1,3 @@
-declare module '*.css' {
-  const content: string;
-  export default content;
-}
-
 declare module 'normalize-wheel' {
   const normalizeWheel: (event: WheelEvent) => {
     spinX: number;
