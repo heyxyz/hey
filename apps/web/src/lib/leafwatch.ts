468ad0d5fa6dd52ea220f09721cb23b123af430c
bigint <69431456+bigint@users.noreply.github.com>
2023-02-15 18:31:48 +0530

    chore: add enabled var to validate hostname

--- apps/web/src/lib/leafwatch.ts
+++ apps/web/src/lib/leafwatch.ts
@@ -2,23 +2,24 @@ import { LEAFWATCH_TOKEN, LEAFWATCH_URL } from 'data/constants';
 import posthog from 'posthog-js';
 
 const isBrowser = typeof window !== 'undefined';
+const enabled = isBrowser && window.location.hostname === 'lenster.xyz';
 
 /**
  * Leafwatch analytics
  */
 export const Leafwatch = {
   track: (name: string, metadata?: Record<string, any>) => {
-    if (isBrowser) {
+    if (enabled) {
       posthog.capture(name, metadata);
     }
   },
   identify: (id: string, metadata?: Record<string, any>) => {
-    if (isBrowser) {
+    if (enabled) {
       posthog.identify(id, metadata);
     }
   },
   init: () => {
-    if (isBrowser) {
+    if (enabled) {
       posthog.init(LEAFWATCH_TOKEN, {
         api_host: LEAFWATCH_URL,
         capture_pageview: false,
