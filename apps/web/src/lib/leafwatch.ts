7c0d4adc86acb1ba77f4fca526fdb6513935444c
bigint <69431456+bigint@users.noreply.github.com>
2023-02-15 18:44:16 +0530

    chore: add more leafwatch init props

--- apps/web/src/lib/leafwatch.ts
+++ apps/web/src/lib/leafwatch.ts
@@ -23,9 +23,15 @@ export const Leafwatch = {
       posthog.init(LEAFWATCH_TOKEN, {
         api_host: LEAFWATCH_URL,
         capture_pageview: false,
+        capture_pageleave: false,
+        disable_session_recording: true,
+        opt_out_capturing_by_default: true,
+        advanced_disable_decide: true,
+        advanced_disable_toolbar_metrics: true,
         request_batching: false,
         autocapture: false,
         cookie_name: 'leafwatch',
+        secure_cookie: true,
         persistence: 'localStorage',
         persistence_name: 'leafwatch_features'
       });
