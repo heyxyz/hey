fa1dc1c369d7c925e5adfd42e126d9ff85555f99
Miguel Piedrafita <github@miguelpiedrafita.com>
2023-02-15 03:22:42 +0000

    wip

--- apps/web/src/components/Settings/Dispatcher/ToggleDispatcher.tsx
+++ apps/web/src/components/Settings/Dispatcher/ToggleDispatcher.tsx
@@ -8,12 +8,13 @@ import onError from '@lib/onError';
 import splitSignature from '@lib/splitSignature';
 import { t } from '@lingui/macro';
 import type { ISuccessResult } from '@worldcoin/idkit';
-import { useIDKit } from '@worldcoin/idkit';
+import { IDKitWidget } from '@worldcoin/idkit';
 import { LensHubProxy } from 'abis';
 import clsx from 'clsx';
 import { IDKIT_ACTION_ID, IDKIT_BRIDGE, IS_MAINNET, LENSHUB_PROXY } from 'data/constants';
 import { useBroadcastMutation, useCreateSetDispatcherTypedDataMutation, useIsIdKitVerifiedQuery } from 'lens';
-import type { FC } from 'react';
+import type { FC, MutableRefObject } from 'react';
+import { useCallback, useRef } from 'react';
 import toast from 'react-hot-toast';
 import { useAppStore } from 'src/store/app';
 import { SETTINGS } from 'src/tracking';
@@ -24,53 +25,19 @@ interface Props {
 }
 
 const ToggleDispatcher: FC<Props> = ({ buttonSize = 'md' }) => {
+  const openIDKit = useRef<() => void | null>(null);
   const { data: idKitData } = useIsIdKitVerifiedQuery();
   const userSigNonce = useAppStore((state) => state.userSigNonce);
   const setUserSigNonce = useAppStore((state) => state.setUserSigNonce);
   const currentProfile = useAppStore((state) => state.currentProfile);
   const canUseRelay = currentProfile?.dispatcher?.canUseRelay;
 
-  const { setOpen: setIDKitOpen } = useIDKit({
-    autoClose: true,
-    enableTelemetry: true,
-    actionId: IDKIT_ACTION_ID,
-    handleVerify: async (result: ISuccessResult) => {
-      const response = await fetch(IDKIT_BRIDGE, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          ...result,
-          is_production: IS_MAINNET,
-          action_id: IDKIT_ACTION_ID,
-          signal: currentProfile?.ownedBy
-        })
-      });
-
-      if (response.ok) {
-        return;
-      }
-
-      if (response.status === 400 && (await response.json()).code === 'already_verified') {
-        throw new Error(
-          'You have already verified this phone number with Lens. You can only verify one wallet with one phone number.'
-        );
-      }
-
-      throw new Error('Something went wrong. Please try again.');
-    },
-    signal: currentProfile?.ownedBy,
-    copy: {
-      title: 'Lens Dispatcher',
-      heading: t`Verify your phone number to increase gassless limits`
-    }
-  });
-
   const onCompleted = () => {
     toast.success(t`Profile updated successfully!`);
     Leafwatch.track(SETTINGS.DISPATCHER.TOGGLE);
 
     if (!idKitData?.isIDKitPhoneVerified) {
-      setIDKitOpen(true);
+      openIDKit.current?.();
     }
   };
 
@@ -127,32 +94,80 @@ const ToggleDispatcher: FC<Props> = ({ buttonSize = 'md' }) => {
     } catch {}
   };
 
+  const handleIDKitVerify = useCallback(
+    async (result: ISuccessResult) => {
+      const response = await fetch(IDKIT_BRIDGE, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          ...result,
+          is_production: IS_MAINNET,
+          action_id: IDKIT_ACTION_ID,
+          signal: currentProfile?.ownedBy
+        })
+      });
+
+      if (response.ok) {
+        return;
+      }
+
+      if (response.status === 400 && (await response.json()).code === 'already_verified') {
+        throw new Error(
+          'You have already verified this phone number with Lens. You can only verify one wallet with one phone number.'
+        );
+      }
+
+      throw new Error('Something went wrong. Please try again.');
+    },
+    [currentProfile?.ownedBy]
+  );
+
   const isLoading = signLoading || writeLoading || broadcastLoading || typedDataLoading;
   const broadcastTxHash =
     broadcastData?.broadcast.__typename === 'RelayerResult' && broadcastData.broadcast.txHash;
 
-  return writeData?.hash ?? broadcastTxHash ? (
-    <div className="mt-2">
-      <IndexStatus txHash={writeData?.hash ?? broadcastTxHash} reload />
-    </div>
-  ) : (
-    <Button
-      variant={canUseRelay ? 'danger' : 'primary'}
-      className={clsx({ 'text-sm': buttonSize === 'sm' }, `mr-auto`)}
-      disabled={isLoading}
-      icon={
-        isLoading ? (
-          <Spinner variant={canUseRelay ? 'danger' : 'primary'} size="xs" />
-        ) : canUseRelay ? (
-          <XIcon className="h-4 w-4" />
-        ) : (
-          <CheckCircleIcon className="h-4 w-4" />
-        )
-      }
-      onClick={toggleDispatcher}
-    >
-      {canUseRelay ? t`Disable dispatcher` : t`Enable dispatcher`}
-    </Button>
+  return (
+    <>
+      <IDKitWidget
+        enableTelemetry
+        methods={['phone', 'orb']}
+        actionId={IDKIT_ACTION_ID}
+        handleVerify={handleIDKitVerify}
+        signal={currentProfile?.ownedBy}
+        copy={{
+          title: 'Lens Dispatcher',
+          heading: t`Verify your phone number to increase gassless limits`
+        }}
+      >
+        {({ open }) => {
+          (openIDKit as MutableRefObject<() => void>).current = open;
+          return <span />;
+        }}
+      </IDKitWidget>
+      {writeData?.hash ?? broadcastTxHash ? (
+        <div className="mt-2">
+          <IndexStatus txHash={writeData?.hash ?? broadcastTxHash} reload />
+        </div>
+      ) : (
+        <Button
+          variant={canUseRelay ? 'danger' : 'primary'}
+          className={clsx({ 'text-sm': buttonSize === 'sm' }, `mr-auto`)}
+          disabled={isLoading}
+          icon={
+            isLoading ? (
+              <Spinner variant={canUseRelay ? 'danger' : 'primary'} size="xs" />
+            ) : canUseRelay ? (
+              <XIcon className="h-4 w-4" />
+            ) : (
+              <CheckCircleIcon className="h-4 w-4" />
+            )
+          }
+          onClick={toggleDispatcher}
+        >
+          {canUseRelay ? t`Disable dispatcher` : t`Enable dispatcher`}
+        </Button>
+      )}
+    </>
   );
 };
 
--- apps/web/src/components/Settings/Dispatcher/index.tsx
+++ apps/web/src/components/Settings/Dispatcher/index.tsx
@@ -1,11 +1,16 @@
 import MetaTags from '@components/Common/MetaTags';
+import { Button } from '@components/UI/Button';
 import { Card } from '@components/UI/Card';
 import { GridItemEight, GridItemFour, GridLayout } from '@components/UI/GridLayout';
+import { CheckCircleIcon } from '@heroicons/react/outline';
 import { Leafwatch } from '@lib/leafwatch';
 import { t, Trans } from '@lingui/macro';
-import { APP_NAME } from 'data/constants';
+import type { ISuccessResult } from '@worldcoin/idkit';
+import { IDKitWidget } from '@worldcoin/idkit';
+import { APP_NAME, IDKIT_ACTION_ID, IDKIT_BRIDGE, IS_MAINNET } from 'data/constants';
+import { useIsIdKitVerifiedQuery } from 'lens';
 import type { FC } from 'react';
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import Custom404 from 'src/pages/404';
 import { useAppStore } from 'src/store/app';
 import { PAGEVIEW } from 'src/tracking';
@@ -14,11 +19,40 @@ import SettingsSidebar from '../Sidebar';
 import ToggleDispatcher from './ToggleDispatcher';
 
 const DispatcherSettings: FC = () => {
+  const { data: idKitData } = useIsIdKitVerifiedQuery();
   const currentProfile = useAppStore((state) => state.currentProfile);
 
   useEffect(() => {
     Leafwatch.track(PAGEVIEW, { page: 'settings', subpage: 'dispatcher' });
   }, []);
+
+  const handleIDKitVerify = useCallback(
+    async (result: ISuccessResult) => {
+      const response = await fetch(IDKIT_BRIDGE, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          ...result,
+          is_production: IS_MAINNET,
+          action_id: IDKIT_ACTION_ID,
+          signal: currentProfile?.ownedBy
+        })
+      });
+
+      if (response.ok) {
+        return;
+      }
+
+      if (response.status === 400 && (await response.json()).code === 'already_verified') {
+        throw new Error(
+          'You have already verified this phone number with Lens. You can only verify one wallet with one phone number.'
+        );
+      }
+
+      throw new Error('Something went wrong. Please try again.');
+    },
+    [currentProfile?.ownedBy]
+  );
 
   if (!currentProfile) {
     return <Custom404 />;
@@ -30,7 +64,7 @@ const DispatcherSettings: FC = () => {
       <GridItemFour>
         <SettingsSidebar />
       </GridItemFour>
-      <GridItemEight>
+      <GridItemEight className="space-y-4">
         <Card className="linkify space-y-2 p-5">
           <div className="flex items-center space-x-2">
             <div className="text-lg font-bold">
@@ -45,6 +79,38 @@ const DispatcherSettings: FC = () => {
           </div>
           <ToggleDispatcher />
         </Card>
+        {currentProfile?.dispatcher?.canUseRelay && !idKitData?.isIDKitPhoneVerified && (
+          <Card className="linkify space-y-2 p-5">
+            <div className="flex items-center space-x-2">
+              <div className="text-lg font-bold">Unlock higher dispatcher limits</div>
+            </div>
+            <div className="pb-2">
+              <Trans>By verifying your phone number, we'll relax the gass-less limits.</Trans>
+            </div>
+            <IDKitWidget
+              enableTelemetry
+              methods={['phone', 'orb']}
+              actionId={IDKIT_ACTION_ID}
+              handleVerify={handleIDKitVerify}
+              signal={currentProfile?.ownedBy}
+              copy={{
+                title: 'Lens Dispatcher',
+                heading: t`Verify your phone number to increase gassless limits`
+              }}
+            >
+              {({ open }) => (
+                <Button
+                  onClick={open}
+                  variant="primary"
+                  className="mr-auto"
+                  icon={<CheckCircleIcon className="h-4 w-4" />}
+                >
+                  <Trans>Verify with IDKit</Trans>
+                </Button>
+              )}
+            </IDKitWidget>
+          </Card>
+        )}
       </GridItemEight>
     </GridLayout>
   );
