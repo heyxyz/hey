90fa1f560da828d61bfd69097b82eee210eb576f
bigint <69431456+bigint@users.noreply.github.com>
2023-02-22 08:42:46 +0530

    feat: add split config

--- apps/web/src/components/Composer/Actions/CollectSettings/CollectForm.tsx
+++ apps/web/src/components/Composer/Actions/CollectSettings/CollectForm.tsx
@@ -6,15 +6,11 @@ import { Spinner } from '@components/UI/Spinner';
 import {
   ClockIcon,
   CollectionIcon,
-  PlusIcon,
   StarIcon,
   SwitchHorizontalIcon,
-  UserGroupIcon,
-  UsersIcon,
-  XCircleIcon
+  UserGroupIcon
 } from '@heroicons/react/outline';
 import { Mixpanel } from '@lib/mixpanel';
-import splitNumber from '@lib/splitNumber';
 import { t, Trans } from '@lingui/macro';
 import type { Erc20 } from 'lens';
 import { CollectModules, useEnabledModulesQuery } from 'lens';
@@ -24,6 +20,8 @@ import { useAccessSettingsStore } from 'src/store/access-settings';
 import { useAppStore } from 'src/store/app';
 import { useCollectModuleStore } from 'src/store/collect-module';
 import { PUBLICATION } from 'src/tracking';
+
+import SplitConfig from './SplitConfig';
 
 interface Props {
   setShowModal: Dispatch<boolean>;
@@ -204,17 +202,6 @@ const CollectForm: FC<Props> = ({ setShowModal }) => {
     }
   };
 
-  const splitEvenly = () => {
-    const equalSplits = splitNumber(100, recipients.length);
-    const splits = recipients.map((recipient, i) => {
-      return {
-        recipient: recipient.recipient,
-        split: equalSplits[i]
-      };
-    });
-    setRecipients([...splits]);
-  };
-
   return (
     <div className="space-y-3 p-5">
       <ToggleWithHelper
@@ -351,105 +338,7 @@ const CollectForm: FC<Props> = ({ setShowModal }) => {
                   label={t`Limit collecting to the first 24h`}
                 />
               </div>
-              <div className="space-y-2 pt-5">
-                <div className="flex items-center space-x-2">
-                  <UsersIcon className="text-brand-500 h-4 w-4" />
-                  <span>
-                    <Trans>Split revenue</Trans>
-                  </span>
-                </div>
-                <ToggleWithHelper
-                  on={recipients.length > 0}
-                  setOn={() => {
-                    if (recipients.length > 0) {
-                      setRecipients([]);
-                    } else {
-                      setRecipients([{ recipient: currentProfile?.ownedBy, split: 100 }]);
-                    }
-                    Mixpanel.track(PUBLICATION.NEW.COLLECT_MODULE.TOGGLE_MULTIPLE_RECIPIENTS_COLLECT);
-                  }}
-                  label={t`Set multiple recipients for the collect fee`}
-                />
-                {hasRecipients ? (
-                  <div className="space-y-3">
-                    <div className="no-scrollbar max-h-[20vh] overflow-auto">
-                      {recipients.map((recipient, index) => (
-                        <div key={index} className="flex items-center space-x-2 py-2 pt-2 text-sm">
-                          <Input
-                            placeholder="0x1234..."
-                            value={recipient.recipient}
-                            disabled={index === 0}
-                            onChange={(event) => {
-                              setRecipients(
-                                recipients.map((r, i) => {
-                                  if (i === index) {
-                                    return { ...r, recipient: event.target.value };
-                                  }
-                                  return r;
-                                })
-                              );
-                            }}
-                          />
-                          <div className="w-1/3">
-                            <Input
-                              type="number"
-                              placeholder="5"
-                              min="1"
-                              max="100"
-                              value={recipient.split}
-                              iconRight="%"
-                              onChange={(event) => {
-                                setRecipients(
-                                  recipients.map((r, i) => {
-                                    if (i === index) {
-                                      return { ...r, split: parseInt(event.target.value) };
-                                    }
-                                    return r;
-                                  })
-                                );
-                              }}
-                            />
-                          </div>
-                          <button
-                            onClick={() => {
-                              setRecipients(recipients.filter((_, i) => i !== index));
-                            }}
-                          >
-                            <XCircleIcon className="h-5 w-5 text-red-500" />
-                          </button>
-                        </div>
-                      ))}
-                    </div>
-                    <div className="flex items-center justify-between">
-                      <Button
-                        size="sm"
-                        outline
-                        icon={<PlusIcon className="h-3 w-3" />}
-                        onClick={() => {
-                          setRecipients([...recipients, { recipient: '', split: 0 }]);
-                        }}
-                      >
-                        Add recipient
-                      </Button>
-                      <Button
-                        size="sm"
-                        outline
-                        icon={<SwitchHorizontalIcon className="h-3 w-3" />}
-                        onClick={splitEvenly}
-                      >
-                        Split evenly
-                      </Button>
-                    </div>
-                    {splitTotal > 100 ? (
-                      <div className="text-sm font-bold text-red-500">
-                        <Trans>
-                          Splits cannot exceed 100%. Total: <span>{splitTotal}</span>%
-                        </Trans>
-                      </div>
-                    ) : null}
-                  </div>
-                ) : null}
-              </div>
+              <SplitConfig />
             </>
           )}
           <div className="space-y-2 pt-5">
