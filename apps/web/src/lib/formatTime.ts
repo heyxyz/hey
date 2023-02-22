0a02ffadb47d23ea4ebca25eaf5e098445f22688
bigint <69431456+bigint@users.noreply.github.com>
2023-02-22 11:44:25 +0530

    chore: move twitter format to formattime util

--- apps/web/src/lib/formatTime.ts
+++ apps/web/src/lib/formatTime.ts
@@ -2,9 +2,11 @@ import { i18n } from '@lingui/core';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import utc from 'dayjs/plugin/utc';
+import dayjsTwitter from 'dayjs-twitter';
 
 dayjs.extend(relativeTime);
 dayjs.extend(utc);
+dayjs.extend(dayjsTwitter);
 
 export const formatTime = (date: Date | undefined): string => {
   return date ? i18n.date(date, { dateStyle: 'medium', timeStyle: 'medium' }) : '';
@@ -16,4 +18,9 @@ export const getTimeAddedOneDay = () => {
 
 export const getTimeFromNow = (date: Date) => {
   return dayjs(new Date(date)).fromNow();
+};
+
+export const getTwitterFormat = (date: Date | string) => {
+  // @ts-ignore
+  return dayjs(new Date(date)).twitter();
 };
