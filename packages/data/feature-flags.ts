326844fd751e2bcc35cca2cace4db0e34c96fa28
bigint <69431456+bigint@users.noreply.github.com>
2023-02-22 13:11:24 +0530

    feat: add feature flag

--- packages/data/feature-flags.ts
+++ packages/data/feature-flags.ts
@@ -20,6 +20,11 @@ export const featureFlags = [
     enabledFor: !IS_PRODUCTION ? [...mainnetStaffs, ...testnetStaffs] : []
   },
   {
+    key: 'multiple-recipient-collect',
+    name: 'Multiple recipient collect',
+    enabledFor: [...lensterMembers, ...aaveMembers]
+  },
+  {
     key: 'gated-locales',
     name: 'Gated locales',
     enabledFor: ['0x01adb7', '0x216f', '0x6b66', '0x6b15', '0x01adb3', ...lensterMembers, ...aaveMembers]
