2cf34086588f12a8b9190e52dfa12d0e30007ff7
bigint <69431456+bigint@users.noreply.github.com>
2023-02-24 11:02:45 +0530

    chore: use nullish coalescing for embed htmls (#1859)

--- apps/web/src/components/Shared/IFramely/index.tsx
+++ apps/web/src/components/Shared/IFramely/index.tsx
@@ -41,7 +41,7 @@ const IFramely: FC<Props> = ({ url }) => {
     isSquare:
       data?.links?.thumbnail &&
       data?.links?.thumbnail[0]?.media?.width === data?.links?.thumbnail[0]?.media?.height,
-    html: data?.links?.player ? data?.links?.player[0]?.html : null
+    html: data?.links?.player?.[0]?.html ?? data?.links?.reader?.[0]?.html ?? null
   };
 
   if (!og.title) {
