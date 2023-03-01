493ef5aecaaafa0476466f84197a2648ab52a123
bigint <69431456+bigint@users.noreply.github.com>
2023-03-01 15:44:27 +0530

    feat: move to react-markdown from interweave (#1880)

---
+++ apps/web/src/components/Shared/Markup/ExternalLink.tsx
@@ -0,0 +1,24 @@
+import { stopEventPropagation } from '@lib/stopEventPropagation';
+import Link from 'next/link';
+import type { FC } from 'react';
+
+import type { MarkupLinkProps } from '.';
+
+const ExternalLink: FC<MarkupLinkProps> = ({ href, title = href }) => {
+  if (!href) {
+    return null;
+  }
+
+  return (
+    <Link
+      href={href}
+      onClick={stopEventPropagation}
+      target={href.includes(location.host) ? '_self' : '_blank'}
+      rel="noopener"
+    >
+      {title}
+    </Link>
+  );
+};
+
+export default ExternalLink;
