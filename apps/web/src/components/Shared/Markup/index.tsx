c17e150bd14fc6e0c6190b46358758ff7d9a1bbb
bigint <69431456+bigint@users.noreply.github.com>
2023-03-02 12:42:51 +0530

    chore: update markup folder structure

--- apps/web/src/components/Shared/Markup/index.tsx
+++ apps/web/src/components/Shared/Markup/index.tsx
@@ -8,36 +8,8 @@ import remarkBreaks from 'remark-breaks';
 import linkifyRegex from 'remark-linkify-regex';
 import stripMarkdown from 'strip-markdown';
 
-import ExternalLink from './ExternalLink';
-import Hashtag from './Hashtag';
-import Mention from './Mention';
-
-export interface MarkupLinkProps {
-  href?: string;
-  title?: string;
-}
-
-const Code = (props: any) => {
-  return <code className="rounded-lg bg-gray-300 px-[5px] py-[2px] text-sm dark:bg-gray-700" {...props} />;
-};
-
-const MarkupLink = ({ href, title = href }: MarkupLinkProps) => {
-  if (!href) {
-    return null;
-  }
-
-  // Mentions
-  if (href.startsWith('@')) {
-    return <Mention href={href} title={title} />;
-  }
-
-  // Hashtags
-  if (href.startsWith('#')) {
-    return <Hashtag href={href} title={title} />;
-  }
-
-  return <ExternalLink href={href} title={title} />;
-};
+import Code from './Code';
+import MarkupLink from './MarkupLink';
 
 interface Props {
   children: string;
