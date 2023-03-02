11a970c6b4282d116a491a49f7de1b20e22a8701
bigint <69431456+bigint@users.noreply.github.com>
2023-03-02 17:41:49 +0530

    fix

--- apps/web/src/lib/getDaVerified.ts
+++ apps/web/src/lib/getDaVerified.ts
@@ -1,17 +1,20 @@
 import type { EthereumNode } from '@lens-protocol/data-availability-verifier';
-import { checkDAProof, Environment } from '@lens-protocol/data-availability-verifier';
+import { checkDAProof, Deployment, Environment } from '@lens-protocol/data-availability-verifier';
 
 const getDaVerified = async (proof: string) => {
   const ethereumNode: EthereumNode = {
     environment: Environment.MUMBAI,
+    deployment: Deployment.STAGING,
     nodeUrl: 'https://rpc.ankr.com/polygon_mumbai'
   };
+  console.log('proof', proof);
   const result = await checkDAProof(proof, ethereumNode);
   if (result.isSuccess()) {
     console.log('proof valid', result.successResult!);
     return true;
   }
 
+  console.log('proof valid', result);
   return false;
 };
 
