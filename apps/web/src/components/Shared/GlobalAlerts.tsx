d9b833832de2215c04b4c9f55446421f1cfb9455
bigint <69431456+bigint@users.noreply.github.com>
2023-03-07 11:29:18 +0530

    feat: migrate publication delete confirmation (#1911)

---
+++ apps/web/src/components/Shared/GlobalAlerts.tsx
@@ -0,0 +1,9 @@
+import type { FC } from 'react';
+
+import DeletePublication from './Alert/DeletePublication';
+
+const GlobalAlerts: FC = () => {
+  return <DeletePublication />;
+};
+
+export default GlobalAlerts;
