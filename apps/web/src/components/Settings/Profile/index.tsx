abdf9c6fdd6c2e0fd8763a29634d3c62d20998a0
bigint <69431456+bigint@users.noreply.github.com>
2023-03-11 00:07:24 +0530

    chore: rename NFT to Nft

--- apps/web/src/components/Settings/Profile/index.tsx
+++ apps/web/src/components/Settings/Profile/index.tsx
@@ -17,7 +17,7 @@ import { useAppStore } from 'src/store/app';
 import { PAGEVIEW } from 'src/tracking';
 
 import SettingsSidebar from '../Sidebar';
-import NftPicture from './NftPictures';
+import NftPicture from './NftPicture';
 import Picture from './Picture';
 import ProfileSettingsForm from './Profile';
 
