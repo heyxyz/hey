7158bb44b1418dbedc657f84dd855dc75f7610ee
bigint <69431456+bigint@users.noreply.github.com>
2023-03-11 00:12:37 +0530

    chore: rename NFT to Nft

--- apps/web/src/components/Shared/Shimmer/NFTSShimmer.tsx
+++ apps/web/src/components/Shared/Shimmer/NFTSShimmer.tsx
@@ -1,14 +1,14 @@
 import type { FC } from 'react';
 
-import NFTShimmer from './NFTShimmer';
+import NftShimmer from './NftShimmer';
 
 const NFTSShimmer: FC = () => {
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-      <NFTShimmer />
-      <NFTShimmer />
-      <NFTShimmer />
-      <NFTShimmer />
+      <NftShimmer />
+      <NftShimmer />
+      <NftShimmer />
+      <NftShimmer />
     </div>
   );
 };
