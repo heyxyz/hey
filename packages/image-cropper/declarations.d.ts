696b57a2e31007270506dbfefdfa0ea3ae51afe2
Olof Andersson <olof.andersson@gmail.com>
2023-03-11 18:21:36 +0100

    types/normalize-wheel dependency

--- packages/image-cropper/declarations.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'normalize-wheel' {
-  const normalizeWheel: (event: WheelEvent) => {
-    spinX: number;
-    spinY: number;
-    pixelX: number;
-    pixelY: number;
-  };
-  export default normalizeWheel;
-}
