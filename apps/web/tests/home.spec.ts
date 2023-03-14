d96f48d3698f755376702c2ba59207cd65f5621f
bigint <69431456+bigint@users.noreply.github.com>
2023-03-14 12:08:15 +0530

    feat: add tests

--- apps/web/tests/home.spec.ts
+++ apps/web/tests/home.spec.ts
@@ -1,7 +1,17 @@
 import { expect, test } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:4783');
+});
 
+test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Lenster/);
+});
+
+test('has home hero', async ({ page }) => {
+  await expect(page.getByTestId('home-hero')).toContainText('Welcome to Lenster 👋');
+});
+
+test('has explore feed', async ({ page }) => {
+  await expect(page.getByTestId('explore-feed')).toBeVisible();
 });
