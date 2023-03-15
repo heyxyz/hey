629fe3e4025936509fb49245d8c91d28cf3c5667
Olof Andersson <olof.andersson@gmail.com>
2023-03-15 14:45:29 +0100

    feat: image cropper (#1772)
    Co-authored-by: bigint <69431456+bigint@users.noreply.github.com>

---
+++ packages/image-cropper/types.ts
@@ -0,0 +1,23 @@
+export interface Size {
+  width: number;
+  height: number;
+}
+
+export interface MediaSize {
+  width: number;
+  height: number;
+  naturalWidth: number;
+  naturalHeight: number;
+}
+
+export interface Point {
+  x: number;
+  y: number;
+}
+
+export interface Area {
+  width: number;
+  height: number;
+  x: number;
+  y: number;
+}
