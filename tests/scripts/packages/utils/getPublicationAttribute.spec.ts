5b439beeef6d52c4aa48a1c7d09ab3264db2e2fe
bigint <69431456+bigint@users.noreply.github.com>
2023-03-16 15:33:27 +0530

    feat: add tests for getPublicationAttribute

---
+++ tests/scripts/packages/utils/getPublicationAttribute.spec.ts
@@ -0,0 +1,29 @@
+import { expect, test } from '@playwright/test';
+import type { MetadataAttributeOutput } from 'lens';
+import getPublicationAttribute from 'utils/getPublicationAttribute';
+
+test.describe('getPublicationAttribute', async () => {
+  const attributes: MetadataAttributeOutput[] = [
+    { traitType: 'type', value: 'book' },
+    { traitType: 'author', value: 'John Doe' },
+    { traitType: 'year', value: '2021' }
+  ];
+
+  test('should return empty string if attributes is undefined', () => {
+    expect(getPublicationAttribute(undefined, 'type')).toBe('');
+  });
+
+  test('should return empty string if traitType is not found in attributes', () => {
+    expect(getPublicationAttribute(attributes, 'title')).toBe('');
+  });
+
+  test('should return the value of the matching traitType', () => {
+    expect(getPublicationAttribute(attributes, 'author')).toBe('John Doe');
+    expect(getPublicationAttribute(attributes, 'year')).toBe('2021');
+  });
+
+  test('should return the first matching traitType if there are multiple matches', () => {
+    const updatedAttributes = [...attributes, { traitType: 'author', value: 'Jane Smith' }];
+    expect(getPublicationAttribute(updatedAttributes, 'author')).toBe('John Doe');
+  });
+});
