5d9a99d68e14fa58d93dd599270a1ab1e54dcece
bigint <69431456+bigint@users.noreply.github.com>
2023-03-16 19:40:44 +0530

    feat: add tests for getUniswapURL

---
+++ tests/scripts/packages/utils/getUniswapURL.spec.ts
@@ -0,0 +1,32 @@
+import { expect, test } from '@playwright/test';
+import getUniswapURL from 'utils/getUniswapURL';
+
+test.describe('getUniswapURL', () => {
+  test('should return a valid URL', () => {
+    const amount = 123.45;
+    const outputCurrency = '0x0123456789abcdef';
+    const expectedURL =
+      'https://app.uniswap.org/#/swap?exactField=output&exactAmount=123.45&outputCurrency=0x0123456789abcdef&chain=polygon';
+    const result = getUniswapURL(amount, outputCurrency);
+    expect(result).toBe(expectedURL);
+  });
+
+  test('should handle zero amount', () => {
+    const amount = 0;
+    const outputCurrency = '0x0123456789abcdef';
+    const expectedURL =
+      'https://app.uniswap.org/#/swap?exactField=output&exactAmount=0&outputCurrency=0x0123456789abcdef&chain=polygon';
+    const result = getUniswapURL(amount, outputCurrency);
+    expect(result).toBe(expectedURL);
+  });
+
+  test('should handle empty output currency', () => {
+    const amount = 123.45;
+    const outputCurrency = '';
+    // Note: The resulting URL will still contain "&outputCurrency=", but with an empty value.
+    const expectedURL =
+      'https://app.uniswap.org/#/swap?exactField=output&exactAmount=123.45&outputCurrency=&chain=polygon';
+    const result = getUniswapURL(amount, outputCurrency);
+    expect(result).toBe(expectedURL);
+  });
+});
