286bfcf3e60545ea458d3a5a1401a73de9ded2b5
bigint <69431456+bigint@users.noreply.github.com>
2023-03-16 19:58:29 +0530

    fix: test

--- tests/scripts/packages/utils/getTokenImage.spec.ts
+++ tests/scripts/packages/utils/getTokenImage.spec.ts
@@ -1,13 +1,14 @@
 import { expect, test } from '@playwright/test';
+import { STATIC_IMAGES_URL } from 'data/constants';
 import getTokenImage from 'utils/getTokenImage';
 
 test.describe('getTokenImage', () => {
   test('should return the correct image url for a given token symbol', () => {
-    expect(getTokenImage('ETH')).toEqual('https://example.com/static/images/tokens/eth.svg');
-    expect(getTokenImage('BTC')).toEqual('https://example.com/static/images/tokens/btc.svg');
+    expect(getTokenImage('ETH')).toEqual(`${STATIC_IMAGES_URL}/images/tokens/eth.svg`);
+    expect(getTokenImage('BTC')).toEqual(`{STATIC_IMAGES_URL}/images/tokens/btc.svg`);
   });
 
   test('should convert the token symbol to lowercase before generating the url', () => {
-    expect(getTokenImage('Eth')).toEqual('https://example.com/static/images/tokens/eth.svg');
+    expect(getTokenImage('Eth')).toEqual(`${STATIC_IMAGES_URL}/images/tokens/eth.svg`);
   });
 });
