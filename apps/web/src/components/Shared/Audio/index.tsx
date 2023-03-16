e6525e076f9c8f3a043babb88d42d540ab056707
bigint <69431456+bigint@users.noreply.github.com>
2023-03-16 15:34:36 +0530

    feat: add tests for getPublicationAttribute (#1966)

--- apps/web/src/components/Shared/Audio/index.tsx
+++ apps/web/src/components/Shared/Audio/index.tsx
@@ -1,6 +1,5 @@
 import type { OptimisticTransaction } from '@generated/types';
 import { PauseIcon, PlayIcon } from '@heroicons/react/solid';
-import getPublicationAttribute from '@lib/getPublicationAttribute';
 import getThumbnailUrl from '@lib/getThumbnailUrl';
 import { Mixpanel } from '@lib/mixpanel';
 import { t } from '@lingui/macro';
@@ -10,6 +9,7 @@ import type { ChangeEvent, FC } from 'react';
 import { useRef, useState } from 'react';
 import { usePublicationStore } from 'src/store/publication';
 import { PUBLICATION } from 'src/tracking';
+import getPublicationAttribute from 'utils/getPublicationAttribute';
 import { object, string } from 'zod';
 
 import CoverImage from './CoverImage';
