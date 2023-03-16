022cb09376880057935e821bb401f187c98f6127
bigint <69431456+bigint@users.noreply.github.com>
2023-03-16 18:46:16 +0530

    feat: add tests for getTags (#1969)

---
+++ tests/scripts/packages/utils/getTags.spec.ts
@@ -0,0 +1,28 @@
+import { expect, test } from '@playwright/test';
+import getTags from 'utils/getTags';
+
+test.describe('getTags', () => {
+  test('should return empty array if no tags are found', () => {
+    const inputText = 'This is a text without any tags';
+    const result = getTags(inputText);
+    expect(result).toEqual([]);
+  });
+
+  test('should return an array of matching tags (without duplicates)', () => {
+    const inputText = 'This #is a #text with #multiple #tags and #duplicate #tags';
+    const result = getTags(inputText);
+    expect(result).toEqual(['is', 'text', 'multiple', 'tags', 'duplicate']);
+  });
+
+  test('should only return up to 5 tags (in case of more than 5 matches)', () => {
+    const inputText = 'This #is a #text with #more than #five #tags #present';
+    const result = getTags(inputText);
+    expect(result).toHaveLength(5);
+  });
+
+  test('should ignore hashtags within words', () => {
+    const inputText = 'This#tag should be ignored because it is part of a word';
+    const result = getTags(inputText);
+    expect(result).toEqual([]);
+  });
+});
