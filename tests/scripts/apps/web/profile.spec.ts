c96a9a48c3507bd3cf4684d5f7aa8b0b0a4b190b
bigint <69431456+bigint@users.noreply.github.com>
2023-03-16 10:33:33 +0530

    feat: add profile prerender test case (#1962)

--- tests/scripts/apps/web/profile.spec.ts
+++ tests/scripts/apps/web/profile.spec.ts
@@ -1,12 +1,11 @@
 import { expect, test } from '@playwright/test';
 import { APP_NAME } from 'data/constants';
-
-import { BASE_URL } from '../../constants';
+import { WEB_BASE_URL } from 'test/constants';
 
 test.beforeEach(async ({ page }) => {
-  await page.goto(`${BASE_URL}/privacy`);
+  await page.goto(`${WEB_BASE_URL}/u/yoginth`);
 });
 
 test('has title', async ({ page }) => {
-  await expect(page).toHaveTitle(`Privacy Policy • ${APP_NAME}`);
+  await expect(page).toHaveTitle(`Yoginth (@yoginth) • ${APP_NAME}`);
 });
