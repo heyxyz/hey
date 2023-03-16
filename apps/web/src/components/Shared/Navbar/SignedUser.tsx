03a25afefc6ec0c72083216e127b7ca542c5f5d1
bigint <69431456+bigint@users.noreply.github.com>
2023-03-16 09:53:42 +0530

    feat: add tests for isGardener

--- apps/web/src/components/Shared/Navbar/SignedUser.tsx
+++ apps/web/src/components/Shared/Navbar/SignedUser.tsx
@@ -1,7 +1,6 @@
 import { Image } from '@components/UI/Image';
 import { Menu } from '@headlessui/react';
 import getAvatar from '@lib/getAvatar';
-import isGardener from '@lib/isGardener';
 import { Trans } from '@lingui/macro';
 import clsx from 'clsx';
 import type { Profile } from 'lens';
@@ -9,6 +8,7 @@ import type { FC } from 'react';
 import { useAppStore } from 'src/store/app';
 import { useGlobalModalStateStore } from 'src/store/modals';
 import formatHandle from 'utils/formatHandle';
+import isGardener from 'utils/isGardener';
 import isStaff from 'utils/isStaff';
 
 import MenuTransition from '../MenuTransition';
