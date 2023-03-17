8b297a46e6e34e60e358f613189f2d6cee34fc93
bigint <69431456+bigint@users.noreply.github.com>
2023-03-17 13:16:44 +0530

    feat: add tests for hasPrideLogo (#1980)

---
+++ tests/scripts/packages/utils/hasPrideLogo.spec.ts
@@ -0,0 +1,20 @@
+import { expect, test } from '@playwright/test';
+import type { Profile } from 'lens';
+import hasPrideLogo from 'utils/hasPrideLogo';
+
+test.describe('hasPrideLogo', () => {
+  test('should return true if profile has pride logo attribute set as true', () => {
+    const profileWithPrideLogo = { attributes: [{ key: 'hasPrideLogo', value: 'true' }] };
+    expect(hasPrideLogo(profileWithPrideLogo as Profile)).toBe(true);
+  });
+
+  test('should return false if profile does not have pride logo attribute', () => {
+    const profileWithoutPrideLogo = { attributes: [{}] };
+    expect(hasPrideLogo(profileWithoutPrideLogo as Profile)).toBe(false);
+  });
+
+  test('should return false if profile has pride logo attribute set as false', () => {
+    const profileWithFalsePrideLogo = { attributes: [{ key: 'hasPrideLogo', value: 'false' }] };
+    expect(hasPrideLogo(profileWithFalsePrideLogo as Profile)).toBe(false);
+  });
+});
