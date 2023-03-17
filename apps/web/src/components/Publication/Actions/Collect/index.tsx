0dc74305772bb63728a298a43a0d67f21142019c
bigint <69431456+bigint@users.noreply.github.com>
2023-03-17 19:30:14 +0530

    feat: add tests for nFormatter

--- apps/web/src/components/Publication/Actions/Collect/index.tsx
+++ apps/web/src/components/Publication/Actions/Collect/index.tsx
@@ -6,7 +6,6 @@ import { CollectionIcon } from '@heroicons/react/outline';
 import { CollectionIcon as CollectionIconSolid } from '@heroicons/react/solid';
 import { getModule } from '@lib/getModule';
 import { Mixpanel } from '@lib/mixpanel';
-import nFormatter from '@lib/nFormatter';
 import { t } from '@lingui/macro';
 import { motion } from 'framer-motion';
 import type { ElectedMirror, Publication } from 'lens';
@@ -16,6 +15,7 @@ import type { FC } from 'react';
 import { useEffect, useState } from 'react';
 import { PUBLICATION } from 'src/tracking';
 import humanize from 'utils/humanize';
+import nFormatter from 'utils/nFormatter';
 
 const CollectModule = dynamic(() => import('./CollectModule'), {
   loading: () => <Loader message={t`Loading collect`} />
---
+++ tests/scripts/packages/utils/nFormatter.spec.ts
@@ -0,0 +1,29 @@
+import { expect, test } from '@playwright/test';
+import nFormatter from 'utils/nFormatter';
+
+test.describe('nFormatter', () => {
+  test('should format a number without any suffix if it is less than 10000 and no digits provided', () => {
+    const result = nFormatter(1234);
+    expect(result).toEqual('1,234');
+  });
+
+  test('should format a number with suffix if it is greater than or equal to 1000000', () => {
+    const result = nFormatter(1234567890);
+    expect(result).toEqual('1.2G');
+  });
+
+  test('should format a number with suffix and with specified decimal places', () => {
+    const result = nFormatter(1234567, 2);
+    expect(result).toEqual('1.23M');
+  });
+
+  test('should format a number with suffix and with default decimal places', () => {
+    const result = nFormatter(1234567);
+    expect(result).toEqual('1.2M');
+  });
+
+  test('should return "0" if input is zero', () => {
+    const result = nFormatter(0);
+    expect(result).toEqual('0');
+  });
+});
