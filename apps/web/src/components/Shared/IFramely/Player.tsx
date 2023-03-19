09bfa380510b10adfca4f0ef7764d6e4e39fce91
bigint <69431456+bigint@users.noreply.github.com>
2023-03-19 18:09:40 +0530

    chore: add publication tests (#2099)

--- apps/web/src/components/Shared/IFramely/Player.tsx
+++ apps/web/src/components/Shared/IFramely/Player.tsx
@@ -7,7 +7,7 @@ interface PlayerProps {
 
 const Player: FC<PlayerProps> = ({ og }) => {
   return (
-    <div className="mt-4 w-5/6 text-sm">
+    <div className="mt-4 w-5/6 text-sm" data-testid={`rich-oembed-${og.url}`}>
       <div className="iframely-player" dangerouslySetInnerHTML={{ __html: og.html }} />
     </div>
   );
