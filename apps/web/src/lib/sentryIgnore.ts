7fa024384a93c45e3c9e5f6029bc049d6c86b173
bigint <69431456+bigint@users.noreply.github.com>
2023-03-19 22:45:22 +0530

    feat: add sentry (#2097)

---
+++ apps/web/src/lib/sentryIgnore.ts
@@ -0,0 +1,34 @@
+/**
+ * Sentry ignore
+ */
+export const denyUrls = [
+  // Browser extensions
+  /^chrome-extension:\/\//i,
+  /^moz-extension:\/\//i,
+  /^safari-web-extension:\/\//i
+];
+
+export const ignoreErrors = [
+  // Wallets
+  /.*TrustWeb3Provider*/gm,
+  /.*trustwallet*/gm,
+  /.*solana*/gm,
+  // Known errors
+  /.*Loading chunk*/gm,
+  /.*Load failed*/gm,
+  /.*Cancel rendering route*/gm,
+  /.*currentTarget, detail, isTrusted, target*/gm,
+  /.*Network Error*/gm,
+  /.*Failed to fetch*/gm,
+  /.*User rejected request*/gm,
+  /.*The source https*/gm,
+  /.*Missing or invalid topic field*/gm,
+  /.*AbortError: The user aborted a request*/gm,
+  /.*Cannot read properties of undefined (reading 'slice')*/gm,
+  /.*INVALID_ARGUMENT*/gm,
+  /.*Invalid JSON RPC response*/gm,
+  /.*Blocked a frame with origin*/gm,
+  /.*User closed modal*/gm,
+  /.*Non-Error promise rejection captured with keys: code, message, stack*/gm,
+  /.*Something went wrong!*/gm
+];
