e470e737a237f00d5159836f91ecb68df36d8ae6
bigint <69431456+bigint@users.noreply.github.com>
2023-03-22 13:28:50 +0530

    chore: refactor more utils

--- packages/utils/getStampFyiURL.ts
+++ packages/utils/getStampFyiURL.ts
@@ -1,10 +1,11 @@
 /**
- *
- * @param address The address to get the cdn.stamp.fyi url for
- * @returns cdn.stamp.fyi url
+ * Returns the cdn.stamp.fyi URL for the specified Ethereum address.
+ * @param address - The Ethereum address to get the URL for.
+ * @returns The cdn.stamp.fyi URL.
  */
-const getStampFyiURL = (address: string) => {
-  return `https://cdn.stamp.fyi/avatar/eth:${address.toLowerCase()}?s=300`;
+const getStampFyiURL = (address: string): string => {
+  const lowerCaseAddress = address.toLowerCase();
+  return `https://cdn.stamp.fyi/avatar/eth:${lowerCaseAddress}?s=300`;
 };
 
 export default getStampFyiURL;
