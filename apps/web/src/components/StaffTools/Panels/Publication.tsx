766e7038773e46e45af03db7e6afaeea4017fc5f
bigint <69431456+bigint@users.noreply.github.com>
2023-03-23 20:48:59 +0530

    fix: sanitizeDStorageUrl to parse arweave gateway + test cases (#2156)

--- apps/web/src/components/StaffTools/Panels/Publication.tsx
+++ apps/web/src/components/StaffTools/Panels/Publication.tsx
@@ -3,7 +3,7 @@ import { ShieldCheckIcon } from '@heroicons/react/solid';
 import type { Publication } from 'lens';
 import type { FC } from 'react';
 import { Card } from 'ui';
-import getIPFSLink from 'utils/getIPFSLink';
+import sanitizeDStorageUrl from 'utils/sanitizeDStorageUrl';
 
 import MetaDetails from './MetaDetails';
 
@@ -53,10 +53,10 @@ const PublicationStaffTool: FC<PublicationStaffToolProps> = ({ publication }) =>
         </MetaDetails>
         <MetaDetails
           icon={<LinkIcon className="lt-text-gray-500 h-4 w-4" />}
-          value={getIPFSLink(publication?.onChainContentURI)}
+          value={sanitizeDStorageUrl(publication?.onChainContentURI)}
           title="On-chain content URI"
         >
-          <a href={getIPFSLink(publication?.onChainContentURI)} target="_blank" rel="noreferrer">
+          <a href={sanitizeDStorageUrl(publication?.onChainContentURI)} target="_blank" rel="noreferrer">
             Open
           </a>
         </MetaDetails>
---
+++ packages/utils/sanitizeDStorageUrl.ts
@@ -0,0 +1,23 @@
+import { ARWEAVE_GATEWAY, IPFS_GATEWAY } from 'data/constants';
+
+/**
+ * Returns the IPFS link for a given hash.
+ *
+ * @param hash The IPFS hash.
+ * @returns The IPFS link.
+ */
+const sanitizeDStorageUrl = (hash?: string): string => {
+  if (!hash) {
+    return '';
+  }
+
+  let link = hash.replace(/^Qm[1-9A-Za-z]{44}/gm, `${IPFS_GATEWAY}${hash}`);
+  link = link.replace('https://ipfs.io/ipfs/', IPFS_GATEWAY);
+  link = link.replace('ipfs://ipfs/', IPFS_GATEWAY);
+  link = link.replace('ipfs://', IPFS_GATEWAY);
+  link = link.replace('ar://', ARWEAVE_GATEWAY);
+
+  return link;
+};
+
+export default sanitizeDStorageUrl;
