3f7861c4f30602ce79ccabffb6a0ca6547c05524
bigint <69431456+bigint@users.noreply.github.com>
2023-03-27 22:12:36 +0530

    feat: wallet connect v2 support (#2229)

--- packages/lib/getWalletDetails.ts
+++ packages/lib/getWalletDetails.ts
@@ -13,7 +13,7 @@ interface WalletDetails {
  */
 const getWalletDetails = (name: string): WalletDetails => {
   const walletDetails: Record<string, WalletDetails> = {
-    WalletConnectLegacy: {
+    WalletConnect: {
       name: 'Wallet Connect',
       logo: `${STATIC_IMAGES_URL}/wallets/walletconnect.svg`
     }
