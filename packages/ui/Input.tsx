f768fc58fd3651d1d3cf95f037a7ea6a49d828d3
bigint <69431456+bigint@users.noreply.github.com>
2023-03-27 20:26:53 +0530

    fix: use ternary operator in clsx instead of object

--- packages/ui/Input.tsx
+++ packages/ui/Input.tsx
@@ -43,13 +43,9 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(function Input(
         )}
         <div
           className={clsx(
-            { '!border-red-500': error },
-            { 'focus-within:ring-1': !error },
-            { 'rounded-r-xl': prefix },
-            { 'rounded-xl': !prefix },
-            {
-              'bg-gray-500 bg-opacity-20 opacity-60': props.disabled
-            },
+            { 'bg-gray-500 bg-opacity-20 opacity-60': props.disabled },
+            error ? '!border-red-500' : 'focus-within:ring-1',
+            prefix ? 'rounded-r-xl' : 'rounded-xl',
             'focus-within:border-brand-500 focus-within:ring-brand-400 flex w-full items-center border border-gray-300 bg-white dark:border-gray-700 dark:bg-gray-900'
           )}
         >
@@ -57,8 +53,7 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(function Input(
             id={id}
             className={clsx(
               { 'placeholder-red-500': error },
-              { 'rounded-r-xl': prefix },
-              { 'rounded-xl': !prefix },
+              prefix ? 'rounded-r-xl' : 'rounded-xl',
               'peer w-full border-none bg-transparent outline-none focus:ring-0',
               className
             )}
