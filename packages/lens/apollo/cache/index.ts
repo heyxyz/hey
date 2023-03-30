3e90e5827f103bbc26bca10ddace1f20c61268b4
bigint <69431456+bigint@users.noreply.github.com>
2023-03-30 15:29:19 +0530

    chore: add separate links and fieldPolicies for apollo (#2268)

---
+++ packages/lens/apollo/cache/index.ts
@@ -0,0 +1,45 @@
+import { InMemoryCache } from '@apollo/client';
+
+import result from '../../generated';
+import { publicationKeyFields } from '../lib';
+import createExplorePublicationsFieldPolicy from './createExplorePublicationsFieldPolicy';
+import createFeedFieldPolicy from './createFeedFieldPolicy';
+import createFeedHighlightsFieldPolicy from './createFeedHighlightsFieldPolicy';
+import createFollowersFieldPolicy from './createFollowersFieldPolicy';
+import createFollowingFieldPolicy from './createFollowingFieldPolicy';
+import createMutualFollowersProfilesFieldPolicy from './createMutualFollowersProfilesFieldPolicy';
+import createNftsFieldPolicy from './createNftsFieldPolicy';
+import createNotificationsFieldPolicy from './createNotificationsFieldPolicy';
+import createProfilesFieldPolicy from './createProfilesFieldPolicy';
+import createPublicationsFieldPolicy from './createPublicationsFieldPolicy';
+import createSearchFieldPolicy from './createSearchFieldPolicy';
+import createWhoCollectedPublicationFieldPolicy from './createWhoCollectedPublicationFieldPolicy';
+import createWhoReactedPublicationFieldPolicy from './createWhoReactedPublicationFieldPolicy';
+
+const cache = new InMemoryCache({
+  possibleTypes: result.possibleTypes,
+  typePolicies: {
+    Post: { keyFields: publicationKeyFields },
+    Comment: { keyFields: publicationKeyFields },
+    Mirror: { keyFields: publicationKeyFields },
+    Query: {
+      fields: {
+        feed: createFeedFieldPolicy(),
+        feedHighlights: createFeedHighlightsFieldPolicy(),
+        explorePublications: createExplorePublicationsFieldPolicy(),
+        publications: createPublicationsFieldPolicy(),
+        nfts: createNftsFieldPolicy(),
+        notifications: createNotificationsFieldPolicy(),
+        followers: createFollowersFieldPolicy(),
+        following: createFollowingFieldPolicy(),
+        search: createSearchFieldPolicy(),
+        profiles: createProfilesFieldPolicy(),
+        whoCollectedPublication: createWhoCollectedPublicationFieldPolicy(),
+        whoReactedPublication: createWhoReactedPublicationFieldPolicy(),
+        mutualFollowersProfiles: createMutualFollowersProfilesFieldPolicy()
+      }
+    }
+  }
+});
+
+export default cache;
