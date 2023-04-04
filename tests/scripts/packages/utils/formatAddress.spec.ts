6709ee56ec5039faed08df26795ed4236de19adf
bigint <69431456+bigint@users.noreply.github.com>
2023-04-04 18:50:34 +0530

    fix: test

--- tests/scripts/packages/utils/formatAddress.spec.ts
+++ tests/scripts/packages/utils/formatAddress.spec.ts
@@ -10,13 +10,13 @@ test.describe('formatAddress', () => {
   test('should match the expected format and shortens middle characters with ellipsis', () => {
     const result = formatAddress('0x7b9AB70D065f7bA8c57bC819B10E35DdB7A41008');
     // should shorten address so it only shows first 4 characters and last 4
-    expect(result).toBe('0x7b…1008');
+    expect(result).toBe('0x7b9A…1008');
   });
 
   test('should match the expected format with 5 on each side and shortens middle characters with ellipsis', () => {
     const result = formatAddress('0x7b9AB70D065f7bA8c57bC819B10E35DdB7A41008', 5);
     // should shorten address so it only shows first 4 characters and last 4
-    expect(result).toBe('0x7b9…41008');
+    expect(result).toBe('0x7b9A…41008');
   });
 
   test("doesn't make any modifications if the address doesn't match the regex", () => {
