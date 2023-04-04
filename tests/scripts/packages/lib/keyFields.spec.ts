a82c479285337a53aaa4e78c2ba93b916bc02fcd
bigint <69431456+bigint@users.noreply.github.com>
2023-04-04 18:51:52 +0530

    fix: test cases

---
+++ tests/scripts/packages/lib/keyFields.spec.ts
@@ -0,0 +1,23 @@
+import { expect, test } from '@playwright/test';
+import { publicationKeyFields } from 'lens/apollo/lib';
+
+test.describe('keyFields', () => {
+  test.describe('publicationKeyFields', () => {
+    const publication = {
+      __typename: 'Post',
+      id: '123',
+      createdAt: '2022-01-01'
+    };
+
+    test('should generate the expected publication key fields string', () => {
+      const expectedResult = 'Post:{"id":"123","createdAt":"2022-01-01"}';
+      expect(publicationKeyFields(publication)).toEqual(expectedResult);
+    });
+
+    test('should return a non-empty string', () => {
+      const result = publicationKeyFields(publication);
+      expect(result).toBeDefined();
+      expect(result.length).toBeGreaterThan(0);
+    });
+  });
+});
---
+++ tests/scripts/packages/lib/splitNumber.spec.ts
@@ -0,0 +1,32 @@
+import { expect, test } from '@playwright/test';
+import splitNumber from 'lib/splitNumber';
+
+test.describe('splitNumber', () => {
+  test('should split number equally', () => {
+    expect(splitNumber(10, 2)).toEqual([5, 5]);
+  });
+
+  test('should split numbers evenly and get remainder', () => {
+    expect(splitNumber(7, 3)).toEqual([3, 2, 2]);
+  });
+
+  test('should default to one part if parts is not specified', () => {
+    expect(splitNumber(8)).toEqual([8]);
+  });
+
+  test('should default to 1 if num and parts are not specified', () => {
+    expect(splitNumber()).toEqual([1]);
+  });
+
+  test('should have at least one item in all parts', () => {
+    expect(splitNumber(4, 5)).toEqual([1, 1, 1, 1, 0]);
+  });
+
+  test('should return array should always have the length of parts even when there is no exact split', () => {
+    expect(splitNumber(11, 3)).toEqual([4, 4, 3]);
+  });
+
+  test('should return array should be sorted in descending order according to input parameters ', () => {
+    expect(splitNumber(14, 4)).toEqual([4, 4, 3, 3]);
+  });
+});
