a5e1511b8db2b7b6431353822382e63db736ac9f
bigint <69431456+bigint@users.noreply.github.com>
2023-04-04 20:37:03 +0530

    feat: add vote check support for snapshot (#2375)

--- apps/web/src/components/Shared/Snapshot/Choices.tsx
+++ apps/web/src/components/Shared/Snapshot/Choices.tsx
@@ -1,38 +1,53 @@
 import { MenuAlt2Icon } from '@heroicons/react/outline';
+import { CheckCircleIcon } from '@heroicons/react/solid';
 import { t } from '@lingui/macro';
+import clsx from 'clsx';
 import type { FC } from 'react';
-import type { Proposal } from 'snapshot';
+import type { Proposal, Vote } from 'snapshot';
 import { Card } from 'ui';
+
+import New from '../Badges/New';
 
 interface HeaderProps {
   proposal: Proposal;
+  votes: Vote[];
 }
 
-const Choices: FC<HeaderProps> = ({ proposal }) => {
+const Choices: FC<HeaderProps> = ({ proposal, votes }) => {
   const { choices, scores, scores_total } = proposal;
-  const sortedChoices = choices
-    .map((choice, index) => ({
-      choice,
-      percentage: ((scores?.[index] ?? 0) / (scores_total ?? 1)) * 100
-    }))
-    .sort((a, b) => b.percentage - a.percentage);
+  const vote = votes[0];
+  const choicesWithVote = choices.map((choice, index) => ({
+    choice,
+    voted: vote?.choice - 1 === index,
+    percentage: ((scores?.[index] ?? 0) / (scores_total ?? 1)) * 100
+  }));
+  const sortedChoices = choicesWithVote.sort((a, b) => b.percentage - a.percentage);
 
   return (
     <Card className="mt-5">
-      <div className="flex items-center space-x-2 border-b px-5 py-3 font-bold">
-        <MenuAlt2Icon className="h-5 w-5" />
-        <b>{proposal.state === 'active' ? t`Current results` : t`Results`}</b>
+      <div className="flex items-center justify-between border-b px-5 py-3">
+        <div className="flex items-center space-x-2 font-bold">
+          <MenuAlt2Icon className="h-5 w-5" />
+          <b>{proposal.state === 'active' ? t`Current results` : t`Results`}</b>
+        </div>
+        <New />
       </div>
-      <div className="space-y-3 p-5">
-        {sortedChoices.map(({ choice, percentage }) => (
-          <div key={choice} className="space-y-1 text-sm">
-            <div className="flex items-center justify-between">
-              <b>{choice}</b>
-              <span className="lt-text-gray-500">{percentage.toFixed(2)}%</span>
-            </div>
-            <div className="relative w-full">
+      <div className="space-y-4 p-5">
+        {sortedChoices.map(({ choice, voted, percentage }) => (
+          <div key={choice} className="flex items-center space-x-2.5 text-sm">
+            <CheckCircleIcon className={clsx(voted ? 'text-green-500' : 'text-gray-500', 'h-6 w-6 ')} />
+            <div className="w-full space-y-1">
+              <div className="flex items-center justify-between">
+                <b>{choice}</b>
+                <span className="lt-text-gray-500">
+                  {Number.isNaN(percentage) ? 0 : percentage.toFixed(2)}%
+                </span>
+              </div>
               <div className="flex h-2.5 overflow-hidden rounded-full bg-gray-300">
-                <div style={{ width: `${percentage.toFixed(2)}%` }} className="bg-brand-500" />
+                <div
+                  style={{ width: `${percentage.toFixed(2)}%` }}
+                  className={clsx(voted ? 'bg-green-500' : 'bg-brand-500')}
+                />
               </div>
             </div>
           </div>
--- packages/snapshot/generated.ts
+++ packages/snapshot/generated.ts
@@ -503,11 +503,12 @@ export type Vp = {
   vp_state?: Maybe<Scalars['String']>;
 };
 
-export type ProposalQueryVariables = Exact<{
+export type SnapshotQueryVariables = Exact<{
   id?: InputMaybe<Scalars['String']>;
+  where?: InputMaybe<VoteWhere>;
 }>;
 
-export type ProposalQuery = {
+export type SnapshotQuery = {
   __typename?: 'Query';
   proposal?: {
     __typename?: 'Proposal';
@@ -520,6 +521,7 @@ export type ProposalQuery = {
     scores_total?: number | null;
     space?: { __typename?: 'Space'; id: string; name?: string | null } | null;
   } | null;
+  votes?: Array<{ __typename?: 'Vote'; choice: any } | null> | null;
 };
 
 export interface PossibleTypesResultData {
@@ -532,8 +534,8 @@ const result: PossibleTypesResultData = {
 };
 export default result;
 
-export const ProposalDocument = gql`
-  query Proposal($id: String) {
+export const SnapshotDocument = gql`
+  query Snapshot($id: String, $where: VoteWhere) {
     proposal(id: $id) {
       id
       author
@@ -547,37 +549,41 @@ export const ProposalDocument = gql`
       scores
       scores_total
     }
+    votes(where: $where) {
+      choice
+    }
   }
 `;
 
 /**
- * __useProposalQuery__
+ * __useSnapshotQuery__
  *
- * To run a query within a React component, call `useProposalQuery` and pass it any options that fit your needs.
- * When your component renders, `useProposalQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * To run a query within a React component, call `useSnapshotQuery` and pass it any options that fit your needs.
+ * When your component renders, `useSnapshotQuery` returns an object from Apollo Client that contains loading, error, and data properties
  * you can use to render your UI.
  *
  * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
  *
  * @example
- * const { data, loading, error } = useProposalQuery({
+ * const { data, loading, error } = useSnapshotQuery({
  *   variables: {
  *      id: // value for 'id'
+ *      where: // value for 'where'
  *   },
  * });
  */
-export function useProposalQuery(
-  baseOptions?: Apollo.QueryHookOptions<ProposalQuery, ProposalQueryVariables>
+export function useSnapshotQuery(
+  baseOptions?: Apollo.QueryHookOptions<SnapshotQuery, SnapshotQueryVariables>
 ) {
   const options = { ...defaultOptions, ...baseOptions };
-  return Apollo.useQuery<ProposalQuery, ProposalQueryVariables>(ProposalDocument, options);
+  return Apollo.useQuery<SnapshotQuery, SnapshotQueryVariables>(SnapshotDocument, options);
 }
-export function useProposalLazyQuery(
-  baseOptions?: Apollo.LazyQueryHookOptions<ProposalQuery, ProposalQueryVariables>
+export function useSnapshotLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<SnapshotQuery, SnapshotQueryVariables>
 ) {
   const options = { ...defaultOptions, ...baseOptions };
-  return Apollo.useLazyQuery<ProposalQuery, ProposalQueryVariables>(ProposalDocument, options);
+  return Apollo.useLazyQuery<SnapshotQuery, SnapshotQueryVariables>(SnapshotDocument, options);
 }
-export type ProposalQueryHookResult = ReturnType<typeof useProposalQuery>;
-export type ProposalLazyQueryHookResult = ReturnType<typeof useProposalLazyQuery>;
-export type ProposalQueryResult = Apollo.QueryResult<ProposalQuery, ProposalQueryVariables>;
+export type SnapshotQueryHookResult = ReturnType<typeof useSnapshotQuery>;
+export type SnapshotLazyQueryHookResult = ReturnType<typeof useSnapshotLazyQuery>;
+export type SnapshotQueryResult = Apollo.QueryResult<SnapshotQuery, SnapshotQueryVariables>;
