8d24a2df346e1f18dec734af7fcc0d501d5e0301
bigint <69431456+bigint@users.noreply.github.com>
2023-04-04 20:40:35 +0530

    fix: publication overflow on thread body (#2376)

--- apps/web/src/components/Publication/ThreadBody.tsx
+++ apps/web/src/components/Publication/ThreadBody.tsx
@@ -26,7 +26,7 @@ const ThreadBody: FC<ThreadBodyProps> = ({ publication }) => {
       <PublicationHeader publication={publication} />
       <div className="flex">
         <div className="-my-6 ml-5 mr-8 border-[0.8px] border-gray-300 bg-gray-300 dark:border-gray-700 dark:bg-gray-700" />
-        <div className="w-full pb-5">
+        <div className="w-full pb-5 pr-[53px]">
           {publication?.hidden ? (
             <HiddenPublication type={publication.__typename} />
           ) : (
