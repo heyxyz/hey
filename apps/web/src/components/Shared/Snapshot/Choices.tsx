2176d5a26ce7309c9ce157c8efdfd5ac850e4951
bigint <69431456+bigint@users.noreply.github.com>
2023-04-05 20:29:10 +0530

    feat: ad sign support

--- apps/web/src/components/Shared/Snapshot/Choices.tsx
+++ apps/web/src/components/Shared/Snapshot/Choices.tsx
@@ -4,7 +4,9 @@ import { t } from '@lingui/macro';
 import clsx from 'clsx';
 import type { FC } from 'react';
 import type { Proposal, Vote } from 'snapshot';
+import { useAppStore } from 'src/store/app';
 import { Card } from 'ui';
+import { useSignTypedData } from 'wagmi';
 
 import New from '../Badges/New';
 
@@ -14,14 +16,48 @@ interface HeaderProps {
 }
 
 const Choices: FC<HeaderProps> = ({ proposal, votes }) => {
+  const currentProfile = useAppStore((state) => state.currentProfile);
+  const { signTypedDataAsync, isLoading: typedDataLoading } = useSignTypedData({});
+
   const { choices, scores, scores_total } = proposal;
   const vote = votes[0];
   const choicesWithVote = choices.map((choice, index) => ({
+    position: index + 1,
     choice,
     voted: Array.isArray(vote?.choice) ? vote?.choice.includes(index + 1) : vote?.choice === index + 1,
     percentage: ((scores?.[index] ?? 0) / (scores_total ?? 1)) * 100
   }));
   const sortedChoices = choicesWithVote.sort((a, b) => b.percentage - a.percentage);
+
+  const sign = async (position: number) => {
+    const typedData = {
+      domain: { name: 'snapshot', version: '0.1.4' },
+      types: {
+        Vote: [
+          { name: 'from', type: 'address' },
+          { name: 'space', type: 'string' },
+          { name: 'timestamp', type: 'uint64' },
+          { name: 'proposal', type: 'bytes32' },
+          { name: 'choice', type: 'uint32' },
+          { name: 'reason', type: 'string' },
+          { name: 'app', type: 'string' },
+          { name: 'metadata', type: 'string' }
+        ]
+      },
+      value: {
+        space: proposal.space?.id as string,
+        proposal: proposal.id as `0x${string}`,
+        choice: position,
+        app: 'snapshot',
+        reason: '',
+        metadata: '{}',
+        from: currentProfile?.ownedBy,
+        timestamp: Number((Date.now() / 1e3).toFixed()) as any
+      }
+    };
+    const sig = await signTypedDataAsync(typedData);
+    console.log('gm', sig);
+  };
 
   return (
     <Card className="mt-5">
@@ -33,9 +69,12 @@ const Choices: FC<HeaderProps> = ({ proposal, votes }) => {
         <New />
       </div>
       <div className="space-y-4 p-5">
-        {sortedChoices.map(({ choice, voted, percentage }) => (
+        {sortedChoices.map(({ position, choice, voted, percentage }) => (
           <div key={choice} className="flex items-center space-x-2.5 text-sm">
-            <CheckCircleIcon className={clsx(voted ? 'text-green-500' : 'text-gray-500', 'h-6 w-6 ')} />
+            <CheckCircleIcon
+              onClick={() => sign(position)}
+              className={clsx(voted ? 'text-green-500' : 'text-gray-500', 'h-6 w-6 ')}
+            />
             <div className="w-full space-y-1">
               <div className="flex items-center justify-between">
                 <b>{choice}</b>
