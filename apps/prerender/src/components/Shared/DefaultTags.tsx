d7dd578c16e8d8d844b647fe1614e7dc86e55fdc
bigint <69431456+bigint@users.noreply.github.com>
2023-04-06 19:45:44 +0530

    chore: improve metatags for profile (#2401)

--- apps/prerender/src/components/Shared/DefaultTags.tsx
+++ apps/prerender/src/components/Shared/DefaultTags.tsx
@@ -1,12 +1,13 @@
 import { APP_NAME, DEFAULT_OG, DESCRIPTION } from 'data/constants';
 import type { FC } from 'react';
+import { BASE_URL } from 'src/constants';
 
 import Tags from './Tags';
 
 const DefaultTags: FC = () => {
   return (
     <>
-      <Tags title={APP_NAME} description={DESCRIPTION} image={DEFAULT_OG} />
+      <Tags title={APP_NAME} description={DESCRIPTION} image={DEFAULT_OG} canonical={BASE_URL} />
       <div>{APP_NAME}</div>
     </>
   );
--- apps/prerender/src/components/Shared/Tags.tsx
+++ apps/prerender/src/components/Shared/Tags.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import type { FC } from 'react';
+import { BASE_URL } from 'src/constants';
 
 interface TagsProps {
   title: string;
@@ -7,21 +8,23 @@ interface TagsProps {
   image: string;
   cardType?: 'summary' | 'summary_large_image';
   schema?: any;
+  canonical: string;
 }
 
-const Tags: FC<TagsProps> = ({ title, description, image, cardType = 'summary', schema }) => {
+const Tags: FC<TagsProps> = ({ title, description, image, cardType = 'summary', schema, canonical }) => {
   return (
     <Head>
       <title>{title}</title>
-      <meta name="description" content={description} />
       <meta charSet="UTF-8" />
-      <meta property="og:url" content="https://lenster.xyz" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content={description} />
+      <meta property="og:url" content={BASE_URL} />
       <meta property="og:site_name" content="Lenster" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
-      <meta property="og:image:width" content="400" />
-      <meta property="og:image:height" content="400" />
+      <meta property="og:image:width" content="512" />
+      <meta property="og:image:height" content="512" />
       <meta property="twitter:card" content={cardType} />
       <meta property="twitter:site" content="Lenster" />
       <meta property="twitter:title" content={title} />
@@ -30,6 +33,7 @@ const Tags: FC<TagsProps> = ({ title, description, image, cardType = 'summary', 
       <meta property="twitter:image:width" content="400" />
       <meta property="twitter:image:height" content="400" />
       <meta property="twitter:creator" content="lensterxyz" />
+      <link rel="canonical" href={canonical} />
       {schema ? schema : null}
     </Head>
   );
