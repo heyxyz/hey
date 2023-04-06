f541225ea20112a5841178a143377e54656880d4
bigint <69431456+bigint@users.noreply.github.com>
2023-04-06 20:01:10 +0530

    chore: add publishedTime and improve publication metatags (#2402)

--- apps/prerender/src/components/Shared/Tags.tsx
+++ apps/prerender/src/components/Shared/Tags.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import type { FC } from 'react';
-import { BASE_URL } from 'src/constants';
 
 interface TagsProps {
   title: string;
@@ -8,23 +7,33 @@ interface TagsProps {
   image: string;
   cardType?: 'summary' | 'summary_large_image';
   schema?: any;
-  canonical: string;
+  url: string;
+  publishedTime?: string;
 }
 
-const Tags: FC<TagsProps> = ({ title, description, image, cardType = 'summary', schema, canonical }) => {
+const Tags: FC<TagsProps> = ({
+  title,
+  description,
+  image,
+  cardType = 'summary',
+  schema,
+  url,
+  publishedTime
+}) => {
   return (
     <Head>
       <title>{title}</title>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="description" content={description} />
-      <meta property="og:url" content={BASE_URL} />
+      <meta property="og:url" content={url} />
       <meta property="og:site_name" content="Lenster" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
       <meta property="og:image:width" content="512" />
       <meta property="og:image:height" content="512" />
+      <meta property="og:type" content="article" />
       <meta property="twitter:card" content={cardType} />
       <meta property="twitter:site" content="Lenster" />
       <meta property="twitter:title" content={title} />
@@ -33,7 +42,8 @@ const Tags: FC<TagsProps> = ({ title, description, image, cardType = 'summary', 
       <meta property="twitter:image:width" content="400" />
       <meta property="twitter:image:height" content="400" />
       <meta property="twitter:creator" content="lensterxyz" />
-      <link rel="canonical" href={canonical} />
+      {publishedTime ? <meta property="article:published_time" content={publishedTime} /> : null}
+      <link rel="canonical" href={url} />
       {schema ? schema : null}
     </Head>
   );
