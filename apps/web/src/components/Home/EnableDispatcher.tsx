58905d5c5b76e52e0a186c412471a4640eca4c34
Sasi <me@sasi.codes>
2023-04-07 19:50:32 +0530

    chore: update copies

--- apps/web/src/components/Home/EnableDispatcher.tsx
+++ apps/web/src/components/Home/EnableDispatcher.tsx
@@ -1,7 +1,7 @@
 import ToggleDispatcher from '@components/Settings/Dispatcher/ToggleDispatcher';
 import { HandIcon } from '@heroicons/react/outline';
 import { Trans } from '@lingui/macro';
-import { APP_NAME } from 'data/constants';
+import { APP_NAME, OLD_LENS_RELAYER_ADDRESS } from 'data/constants';
 import getIsDispatcherEnabled from 'lib/getIsDispatcherEnabled';
 import type { FC } from 'react';
 import { useAppStore } from 'src/store/app';
@@ -10,6 +10,21 @@ import { Card } from 'ui';
 const EnableDispatcher: FC = () => {
   const currentProfile = useAppStore((state) => state.currentProfile);
   const isDispatcherEnabled = getIsDispatcherEnabled(currentProfile);
+  const isOldDispatcherEnabled =
+    currentProfile?.dispatcher?.address?.toLocaleLowerCase() === OLD_LENS_RELAYER_ADDRESS.toLocaleLowerCase();
+
+  const getDescription = () => {
+    if (isOldDispatcherEnabled) {
+      return (
+        <Trans>We highly recommend that you update to our new dispatcher for a faster experience.</Trans>
+      );
+    }
+    return (
+      <Trans>
+        You can enable dispatcher to interact with {APP_NAME} without signing any of your transactions.
+      </Trans>
+    );
+  };
 
   if (isDispatcherEnabled) {
     return null;
@@ -26,11 +41,7 @@ const EnableDispatcher: FC = () => {
           <Trans>Action Required</Trans>
         </p>
       </div>
-      <p className="text-sm leading-[22px]">
-        <Trans>
-          You can enable dispatcher to interact with {APP_NAME} without signing any of your transactions.
-        </Trans>
-      </p>
+      <p className="text-sm leading-[22px]">{getDescription()}</p>
       <ToggleDispatcher buttonSize="sm" />
     </Card>
   );
--- packages/lib/getIsDispatcherEnabled.tsx
+++ packages/lib/getIsDispatcherEnabled.tsx
@@ -1,11 +1,10 @@
+import { OLD_LENS_RELAYER_ADDRESS } from 'data';
 import type { Profile } from 'lens';
-
-const LENS_RELAYER_ADDRESS = '0xD1FecCF6881970105dfb2b654054174007f0e07E';
 
 const getIsDispatcherEnabled = (profile: Profile | null) => {
   return (
     profile?.dispatcher?.canUseRelay &&
-    profile.dispatcher?.address?.toLocaleLowerCase() !== LENS_RELAYER_ADDRESS.toLocaleLowerCase()
+    profile.dispatcher?.address?.toLocaleLowerCase() !== OLD_LENS_RELAYER_ADDRESS.toLocaleLowerCase()
   );
 };
 
