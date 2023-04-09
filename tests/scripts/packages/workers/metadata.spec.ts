ba0f78a8ead42c8343e5b3c12f453294922cc42a
bigint <69431456+bigint@users.noreply.github.com>
2023-04-09 17:58:44 +0530

    chore: add more arweave test

--- tests/scripts/packages/workers/metadata.spec.ts
+++ tests/scripts/packages/workers/metadata.spec.ts
@@ -1,22 +1,25 @@
 import { expect, test } from '@playwright/test';
 import { METADATA_BASE_URL } from 'test/constants';
 
-const expectedArweaveId = 'JulWV7TSj0BopTFhW0dvdo0ZtRdHFc6Rg-OjhcAWBsY';
-const metatadata = { ping: 'pong' };
+test('should return 405 if method is not POST', async ({ request }) => {
+  const getMetadata = request.get(METADATA_BASE_URL);
+  const status = await (await getMetadata).status();
+
+  expect(status).toBe(405);
+});
+
+test('should return 400 if payload is not provided', async ({ request }) => {
+  const postMetadata = request.post(METADATA_BASE_URL, {});
+  const status = await (await postMetadata).status();
+
+  expect(status).toBe(400);
+});
 
 test('should upload to arweave', async ({ request }) => {
   const postMetadata = request.post(METADATA_BASE_URL, {
-    data: metatadata
+    data: { ping: 'pong' }
   });
   const response = await (await postMetadata).json();
 
   expect(response.success).toBeTruthy();
-  expect(response.id).toBe(expectedArweaveId);
-});
-
-test('should have arweave metadata', async ({ request }) => {
-  const arweaveData = request.get(`https://arweave.net/${expectedArweaveId}`);
-  const response = await (await arweaveData).json();
-
-  expect(metatadata.ping).toBe(response.ping);
 });
