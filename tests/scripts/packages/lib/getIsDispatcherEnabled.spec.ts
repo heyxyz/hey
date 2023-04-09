8ea9c3dc9321d25018e9a2a712afa84d20fc54c0
0x0d <69431456+bigint@users.noreply.github.com>
2023-04-09 19:53:25 +0530

    feat: add test cases for getIsDispatcherEnabled (#2466)

---
+++ tests/scripts/packages/lib/getIsDispatcherEnabled.spec.ts
@@ -0,0 +1,33 @@
+import { expect, test } from '@playwright/test';
+import { OLD_LENS_RELAYER_ADDRESS } from 'data';
+import getIsDispatcherEnabled from 'lib/getIsDispatcherEnabled';
+
+test.describe('getIsDispatcherEnabled', () => {
+  test('returns false when profile is null', () => {
+    expect(getIsDispatcherEnabled(null)).toBeFalsy();
+  });
+
+  test('returns false when profile does not have relay enabled', () => {
+    const profileWithoutRelay: any = {
+      dispatcher: { canUseRelay: false, address: null }
+    };
+
+    expect(getIsDispatcherEnabled(profileWithoutRelay)).toBeFalsy();
+  });
+
+  test('returns true when profile has relay enabled and a different relayer address', () => {
+    const profileWithRelay: any = {
+      dispatcher: { canUseRelay: true, address: '0x1234567890123456789012345678901234567890' }
+    };
+
+    expect(getIsDispatcherEnabled(profileWithRelay)).toBeTruthy();
+  });
+
+  test('returns false when profile has relay enabled but an old relayer address', () => {
+    const profileWithOldRelayer: any = {
+      dispatcher: { canUseRelay: true, address: OLD_LENS_RELAYER_ADDRESS }
+    };
+
+    expect(getIsDispatcherEnabled(profileWithOldRelayer)).toBeFalsy();
+  });
+});
--- tests/scripts/packages/lib/hasPrideLogo.spec.ts
+++ tests/scripts/packages/lib/hasPrideLogo.spec.ts
@@ -5,16 +5,16 @@ import hasPrideLogo from 'lib/hasPrideLogo';
 test.describe('hasPrideLogo', () => {
   test('should return true if profile has pride logo attribute set as true', () => {
     const profileWithPrideLogo = { attributes: [{ key: 'hasPrideLogo', value: 'true' }] };
-    expect(hasPrideLogo(profileWithPrideLogo as Profile)).toBe(true);
+    expect(hasPrideLogo(profileWithPrideLogo as Profile)).toBeTruthy();
   });
 
   test('should return false if profile does not have pride logo attribute', () => {
     const profileWithoutPrideLogo = { attributes: [{}] };
-    expect(hasPrideLogo(profileWithoutPrideLogo as Profile)).toBe(false);
+    expect(hasPrideLogo(profileWithoutPrideLogo as Profile)).toBeFalsy();
   });
 
   test('should return false if profile has pride logo attribute set as false', () => {
     const profileWithFalsePrideLogo = { attributes: [{ key: 'hasPrideLogo', value: 'false' }] };
-    expect(hasPrideLogo(profileWithFalsePrideLogo as Profile)).toBe(false);
+    expect(hasPrideLogo(profileWithFalsePrideLogo as Profile)).toBeFalsy();
   });
 });
