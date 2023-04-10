fc882bfeb7ddb21e5bac6dc0fd39a6493a271b86
0x0d <69431456+bigint@users.noreply.github.com>
2023-04-10 10:20:47 +0530

    chore: add publication prerender test cases (#2480)

--- tests/scripts/apps/prerender/publication.spec.ts
+++ tests/scripts/apps/prerender/publication.spec.ts
@@ -9,3 +9,11 @@ test.beforeEach(async ({ page }) => {
 test('should have page title', async ({ page }) => {
   await expect(page).toHaveTitle(`Post by @yoginth.lens • ${APP_NAME}`);
 });
+
+test('should have publication', async ({ page }) => {
+  await expect(page.getByTestId('publication-0x0d-0x01')).toContainText('gm frens 👋');
+});
+
+test('should have comment feed', async ({ page }) => {
+  await expect(page.getByTestId('comment-feed')).toBeVisible();
+});
