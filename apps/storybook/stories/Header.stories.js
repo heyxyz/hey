de3cb930d6bbfc2a6d5f7669631849e6caf6e2a9
bigint <69431456+bigint@users.noreply.github.com>
2023-04-11 13:52:13 +0530

    chore: add tailwind preset and storybook

---
+++ apps/storybook/stories/Header.stories.js
@@ -0,0 +1,22 @@
+import { Header } from './Header';
+
+export default {
+  title: 'Example/Header',
+  component: Header,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/7.0/react/writing-docs/docs-page
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/7.0/react/configure/story-layout
+    layout: 'fullscreen',
+  },
+};
+
+export const LoggedIn = {
+  args: {
+    user: {
+      name: 'Jane Doe',
+    },
+  },
+};
+
+export const LoggedOut = {};
