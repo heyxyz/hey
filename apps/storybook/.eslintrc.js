bd5dd0afc5f1f94394178c371d00111558aa7ce7
0x0d <69431456+bigint@users.noreply.github.com>
2023-04-11 18:00:19 +0530

    chore: remove unwanted deps (#2498)

--- apps/storybook/.eslintrc.js
+++ apps/storybook/.eslintrc.js
@@ -1,14 +1,4 @@
 module.exports = {
   root: true,
-  extends: ['weblint'],
-  overrides: [
-    {
-      files: ['**/*.stories.{ts,tsx}'],
-      extends: ['plugin:storybook/recommended'],
-      rules: {}
-    }
-  ],
-  rules: {
-    'import/no-anonymous-default-export': 'off'
-  }
+  extends: ['weblint']
 };
