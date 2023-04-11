0983d9eec7a2de41cce6129bc7a9ce46e011f30a
0x0d <69431456+bigint@users.noreply.github.com>
2023-04-11 16:57:44 +0530

    chore: storybook vercel support (#2497)

--- apps/storybook/.storybook/preview.ts
+++ apps/storybook/.storybook/preview.ts
@@ -3,10 +3,6 @@ import './style.css';
 const preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
-    html: {
-      root: '#html-addon-root',
-      removeEmptyComments: true
-    },
     controls: {
       hideNoControlsWarning: true
     }
