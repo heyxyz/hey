af060bbd2334a2ff1ff8805fd49f12181e48d4d4
bigint <69431456+bigint@users.noreply.github.com>
2023-04-11 16:28:01 +0530

    chore: add build support

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,7 +1,7 @@
 import { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
