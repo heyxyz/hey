798ed350633e17872c50bcc806ff25d22dc896b0
0x0d <69431456+bigint@users.noreply.github.com>
2023-04-15 00:25:31 +0530

    fix: detected the use of variables before they are defined (#2532)

--- packages/workers/freshdesk/src/index.ts
+++ packages/workers/freshdesk/src/index.ts
@@ -2,12 +2,6 @@ interface EnvType {
   POSTMARK_TOKEN: string;
 }
 
-export default {
-  async fetch(request: Request, env: EnvType) {
-    return await handleRequest(request, env);
-  }
-};
-
 const headers = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'Content-Type',
@@ -52,5 +46,11 @@ const handleRequest = async (request: Request, env: EnvType) => {
     });
   } catch {
     return new Response(JSON.stringify({ success: false, message: 'Something went wrong!' }), { headers });
+  }
+};
+
+export default {
+  async fetch(request: Request, env: EnvType) {
+    return await handleRequest(request, env);
   }
 };
