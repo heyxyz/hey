33f94a33ca81ba61b03c0cbf14f72adc6ae8bdb2
0x0d <69431456+bigint@users.noreply.github.com>
2023-04-17 21:23:49 +0530

    chore: add more and refactor i18n strings (#2583)

--- apps/web/src/components/StaffTools/Panels/Publication.tsx
+++ apps/web/src/components/StaffTools/Panels/Publication.tsx
@@ -1,5 +1,6 @@
 import { CollectionIcon, GlobeAltIcon, HashtagIcon, LinkIcon } from '@heroicons/react/outline';
 import { ShieldCheckIcon } from '@heroicons/react/solid';
+import { t, Trans } from '@lingui/macro';
 import type { Publication } from 'lens';
 import sanitizeDStorageUrl from 'lib/sanitizeDStorageUrl';
 import type { FC } from 'react';
@@ -18,13 +19,15 @@ const PublicationStaffTool: FC<PublicationStaffToolProps> = ({ publication }) =>
     <Card as="aside" className="mt-5 border-yellow-400 !bg-yellow-300 !bg-opacity-20 p-5">
       <div className="flex items-center space-x-2 text-yellow-600">
         <ShieldCheckIcon className="h-5 w-5" />
-        <div className="text-lg font-bold">Staff tool</div>
+        <div className="text-lg font-bold">
+          <Trans>Staff tool</Trans>
+        </div>
       </div>
       <div className="mt-3 space-y-1.5">
         <MetaDetails
           icon={<HashtagIcon className="lt-text-gray-500 h-4 w-4" />}
           value={publication?.id}
-          title="Publication ID"
+          title={t`Publication ID`}
         >
           {publication?.id}
         </MetaDetails>
@@ -32,7 +35,7 @@ const PublicationStaffTool: FC<PublicationStaffToolProps> = ({ publication }) =>
           <MetaDetails
             icon={<HashtagIcon className="lt-text-gray-500 h-4 w-4" />}
             value={publication?.commentOn?.id}
-            title="Parent ID"
+            title={t`Comment on`}
           >
             {publication?.commentOn?.id}
           </MetaDetails>
@@ -40,24 +43,24 @@ const PublicationStaffTool: FC<PublicationStaffToolProps> = ({ publication }) =>
         <MetaDetails
           icon={<CollectionIcon className="lt-text-gray-500 h-4 w-4" />}
           value={publication?.collectModule?.type}
-          title="Collect module"
+          title={t`Collect module`}
         >
           {publication?.collectModule?.type}
         </MetaDetails>
         <MetaDetails
           icon={<GlobeAltIcon className="lt-text-gray-500 h-4 w-4" />}
           value={publication?.id}
-          title="Posted via"
+          title={t`App ID`}
         >
           {publication?.appId}
         </MetaDetails>
         <MetaDetails
           icon={<LinkIcon className="lt-text-gray-500 h-4 w-4" />}
           value={sanitizeDStorageUrl(publication?.onChainContentURI)}
-          title="On-chain content URI"
+          title={t`On-chain content URI`}
         >
           <a href={sanitizeDStorageUrl(publication?.onChainContentURI)} target="_blank" rel="noreferrer">
-            Open
+            <Trans>Open</Trans>
           </a>
         </MetaDetails>
       </div>
