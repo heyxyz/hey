39831c93aeb8e15a5b05462acb84884fd79c1535
0x0d <69431456+bigint@users.noreply.github.com>
2023-04-17 18:00:40 +0530

    feat: add notification and profile export (#2582)

---
+++ apps/web/src/lib/downloadJson.ts
@@ -0,0 +1,17 @@
+/**
+ * Download JSON file
+ * @param data JSON data
+ * @param fileName File name
+ * @param callback Callback function
+ */
+const downloadJson = (data: any, fileName: string, callback?: any) => {
+  const element = document.createElement('a');
+  const file = new Blob([JSON.stringify(data)], { type: 'application/json' });
+  element.href = URL.createObjectURL(file);
+  element.download = `${fileName}.json`;
+  document.body.appendChild(element);
+  element.click();
+  callback?.();
+};
+
+export default downloadJson;
