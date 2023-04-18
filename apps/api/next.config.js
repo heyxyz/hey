fd81b4d26015037cd69687ab0b23aee0e3563b2d
0x0d <69431456+bigint@users.noreply.github.com>
2023-04-18 10:30:38 +0530

    chore: update graphql schema (#2590)
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>
    Co-authored-by: Sagar <sagar@lenster.xyz>

--- apps/api/next.config.js
+++ apps/api/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['lens'],
   async rewrites() {
     return [{ source: '/:match*', destination: '/api/:match*' }];
   }
--- apps/api/src/graphql/resolvers/UserResolver.ts
+++ apps/api/src/graphql/resolvers/UserResolver.ts
@@ -1,3 +1,4 @@
+import isAuthenticated from '@gql/middlewares/isAuthenticated';
 import { db } from '@lib/prisma';
 
 import { builder } from '../builder';
@@ -5,20 +6,55 @@ import { builder } from '../builder';
 builder.prismaObject('User', {
   findUnique: (user) => ({ id: user.id }),
   fields: (t) => ({
-    id: t.exposeID('id', {}),
-    address: t.exposeString('address', {})
+    id: t.exposeID('id'),
+    address: t.exposeString('address')
+  })
+});
+
+const UserRequest = builder.inputType('UserRequest', {
+  fields: (t) => ({
+    id: t.string({ required: true })
   })
 });
 
 builder.queryField('user', (t) =>
   t.prismaField({
     type: 'User',
-    args: { id: t.arg.id({}) },
-    resolve: (query, _root, { id }) => {
+    args: {
+      request: t.arg({ type: UserRequest })
+    },
+    resolve: (query, _root, { request }) => {
       return db.user.findFirst({
         ...query,
-        where: { id },
+        where: { id: request.id },
         rejectOnNotFound: true
+      });
+    }
+  })
+);
+
+const CreateUserRequest = builder.inputType('CreateUserRequest', {
+  fields: (t) => ({
+    id: t.string({ required: true }),
+    address: t.string({ required: true })
+  })
+});
+
+builder.mutationField('createUser', (t) =>
+  t.prismaField({
+    type: 'User',
+    args: {
+      request: t.arg({ type: CreateUserRequest })
+    },
+    resolve: async (query, _root, { request }, context) => {
+      await isAuthenticated(context);
+
+      return db.user.create({
+        ...query,
+        data: {
+          id: request.id,
+          address: request.address
+        }
       });
     }
   })
