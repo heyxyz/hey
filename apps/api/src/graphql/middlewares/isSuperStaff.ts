e26eef0703b8bba53248d9b36a11e1165f40daaf
0x0d <69431456+bigint@users.noreply.github.com>
2023-04-18 13:48:52 +0530

    chore: rip api (#2595)

--- apps/api/src/graphql/middlewares/isSuperStaff.ts
+++
@@ -1,7 +0,0 @@
-import { SUPER_STAFF_ADDRESSES } from 'src/constants';
-
-const isSuperStaff = (address: string) => {
-  return SUPER_STAFF_ADDRESSES.includes(address);
-};
-
-export default isSuperStaff;
