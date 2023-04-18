5c531cf39b42b76108582061fc8d523ef5ce5592
bigint <69431456+bigint@users.noreply.github.com>
2023-04-18 11:19:16 +0530

    feat: add create user endpoint based on getAddressFromJwt

--- apps/api/src/graphql/helpers/getAddressFromJwt.ts
+++ apps/api/src/graphql/helpers/getAddressFromJwt.ts
@@ -1,13 +1,16 @@
 import type { Context } from '@gql/builder';
 import { parseJwt } from 'lens/apollo/lib';
 
+/**
+ * Get the address from the JWT
+ * @param context The pothos context object
+ * @returns The address
+ */
 const getAddressFromJwt = (context: Context): string => {
   if (!context.req.headers.authorization) {
     throw new Error('No authorization header');
   }
-
   const decoded = parseJwt(context.req.headers.authorization);
-  console.log(decoded);
 
   return decoded.id;
 };
