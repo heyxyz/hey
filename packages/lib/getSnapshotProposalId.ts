b9cf0e6d70cb31f553022e0fdf79a5833980bdb6
bigint <69431456+bigint@users.noreply.github.com>
2023-04-19 22:53:10 +0530

    fix: snapshot id fetcher

--- packages/lib/getSnapshotProposalId.ts
+++ packages/lib/getSnapshotProposalId.ts
@@ -1,8 +1,21 @@
+/**
+ * Get Snapshot proposal ID from URL
+ * @param url Snapshot proposal URL
+ * @returns Snapshot proposal ID
+ */
 const getSnapshotProposalId = (url: string): string | null => {
-  const regex = /^https:\/\/snapshot\.org\/#\/[\da-z]+\.eth\/proposal\/(0x[\dA-Fa-f]{64})$/;
-  const match = url.match(regex);
+  if (!url) {
+    return null;
+  }
 
-  return match ? match[1] : null;
+  const parsedUrl = new URL(url);
+  const proposalId = parsedUrl.hash.match(/\/proposal\/(0x[\dA-Fa-f]{64})/);
+
+  if (parsedUrl.host !== 'snapshot.org') {
+    return null;
+  }
+
+  return proposalId?.[1] || null;
 };
 
 export default getSnapshotProposalId;
