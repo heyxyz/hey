bdbbf55d998b85f7d03461b54b869011015960b9
Alain Nicolas <alainncls@users.noreply.github.com>
2023-04-25 07:58:51 +0200

    LENS-41 - Toggle dispatcher mode on account creation (#12)

--- apps/web/src/components/Shared/Login/New/index.tsx
+++ apps/web/src/components/Shared/Login/New/index.tsx
@@ -1,19 +1,25 @@
+import ChooseFile from '@components/Shared/ChooseFile';
 import useSimpleDebounce from '@components/utils/hooks/useSimpleDebounce';
 import { PlusIcon } from '@heroicons/react/outline';
+import uploadToIPFS from '@lib/uploadToIPFS';
 import { t, Trans } from '@lingui/macro';
 import {
   APP_NAME,
   HANDLE_REGEX,
+  IS_RELAYER_AVAILABLE,
   LENS_PROFILE_CREATOR,
   LENS_PROFILE_CREATOR_ABI,
   ZERO_ADDRESS
 } from 'data/constants';
+import { useCreateProfileMutation } from 'lens';
 import getStampFyiURL from 'lib/getStampFyiURL';
-import type { FC } from 'react';
-import React, { useState } from 'react';
+import type { ChangeEvent, FC } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, ErrorMessage, Form, Input, Spinner, useZodForm } from 'ui';
 import { useAccount, useContractWrite, usePrepareContractWrite, useWaitForTransaction } from 'wagmi';
 import { object, string } from 'zod';
+
+import Pending from './Pending';
 
 const newUserSchema = object({
   handle: string()
@@ -33,7 +39,9 @@ const NewProfile: FC<NewProfileProps> = ({ isModal = false }) => {
 
   const [handle, setHandle] = useState('');
   const [avatar, setAvatar] = useState(getStampFyiURL(address ?? ZERO_ADDRESS));
-  //const [uploading, setUploading] = useState(false);
+  const [uploading, setUploading] = useState(false);
+  const [createProfile, { data, loading }] = useCreateProfileMutation();
+  const [isCreationLoading, setIsCreationLoading] = useState(false);
 
   const debouncedHandle = useSimpleDebounce(handle);
 
@@ -50,45 +58,78 @@ const NewProfile: FC<NewProfileProps> = ({ isModal = false }) => {
         to: address,
         handle: debouncedHandle,
         imageURI: avatar,
-        followModule: '0x0000000000000000000000000000000000000000',
+        followModule: ZERO_ADDRESS,
         followModuleInitData: '0x',
         followNFTURI: ''
       }
     ],
     enabled: Boolean(debouncedHandle)
   });
-  const { data, write } = useContractWrite(config);
+  const { data: txData, write } = useContractWrite(config);
 
   const { isLoading, error } = useWaitForTransaction({
-    hash: data?.hash
+    hash: txData?.hash
   });
 
-  /*const handleUpload = async (evt: ChangeEvent<HTMLInputElement>) => {
-                                                                          evt.preventDefault();
-                                                                          setUploading(true);
-                                                                          try {
-                                                                            const attachment = await uploadToIPFS(evt.target.files);
-                                                                            if (attachment[0]?.item) {
-                                                                              setAvatar(attachment[0].item);
-                                                                            }
-                                                                          } finally {
-                                                                            setUploading(false);
-                                                                          }
-                                                                        };*/
+  const handleUpload = async (evt: ChangeEvent<HTMLInputElement>) => {
+    evt.preventDefault();
+    setUploading(true);
+    try {
+      const attachment = await uploadToIPFS(evt.target.files);
+      if (attachment[0]?.item) {
+        setAvatar(attachment[0].item);
+      }
+    } finally {
+      setUploading(false);
+    }
+  };
 
   const handleCreateProfile = async () => {
     write?.();
   };
 
-  return (
-    <Form form={form} className="space-y-4" onSubmit={handleCreateProfile}>
-      {error && (
+  useEffect(() => {
+    setIsCreationLoading(
+      IS_RELAYER_AVAILABLE ? data?.createProfile.__typename === 'RelayerResult' : !!txData?.hash
+    );
+  }, [data, txData]);
+
+  return isCreationLoading ? (
+    <Pending
+      handle={form.getValues('handle')}
+      txHash={
+        IS_RELAYER_AVAILABLE && data?.createProfile.__typename === 'RelayerResult'
+          ? data?.createProfile?.txHash
+          : txData?.hash
+      }
+    />
+  ) : (
+    <Form
+      form={form}
+      className="space-y-4"
+      onSubmit={({ handle }) => {
+        if (IS_RELAYER_AVAILABLE) {
+          const username = handle.toLowerCase();
+          createProfile({
+            variables: {
+              request: {
+                handle: username,
+                profilePictureUri: avatar || getStampFyiURL(address ?? ZERO_ADDRESS)
+              }
+            }
+          });
+        } else {
+          handleCreateProfile();
+        }
+      }}
+    >
+      {data?.createProfile.__typename === 'RelayError' && (data?.createProfile.reason || error) && (
         <ErrorMessage
           className="mb-3"
           title="Create profile failed!"
           error={{
             name: 'Create profile failed!',
-            message: error.message
+            message: IS_RELAYER_AVAILABLE ? data?.createProfile?.reason : error?.message || ''
           }}
         />
       )}
@@ -116,19 +157,18 @@ const NewProfile: FC<NewProfileProps> = ({ isModal = false }) => {
               <img className="h-60 w-60 rounded-lg" height={240} width={240} src={avatar} alt={avatar} />
             </div>
           )}
-          {/* TODO: reactivate avatar upload once we have a S3 bucket
           <div>
             <div className="flex items-center space-x-3">
               <ChooseFile onChange={(evt: ChangeEvent<HTMLInputElement>) => handleUpload(evt)} />
               {uploading && <Spinner size="sm" />}
             </div>
-          </div>*/}
+          </div>
         </div>
       </div>
       <Button
         className="ml-auto"
         type="submit"
-        disabled={!write || isLoading || !isConnected}
+        disabled={!write || loading || isLoading || !isConnected}
         icon={isLoading ? <Spinner size="xs" /> : <PlusIcon className="h-4 w-4" />}
       >
         <Trans>{isConnected ? 'Sign up' : 'Connect your wallet'}</Trans>
