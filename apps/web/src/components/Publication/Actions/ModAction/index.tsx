df43c613ce93853329ba85ea929194504c2749f8
bigint <69431456+bigint@users.noreply.github.com>
2023-04-25 20:41:44 +0530

    chore: update mod report alert

--- apps/web/src/components/Publication/Actions/ModAction/index.tsx
+++ apps/web/src/components/Publication/Actions/ModAction/index.tsx
@@ -1,30 +1,15 @@
-import Slug from '@components/Shared/Slug';
-import {
-  BanIcon,
-  ExclamationCircleIcon,
-  ExclamationIcon,
-  PhotographIcon,
-  RefreshIcon,
-  ShieldCheckIcon
-} from '@heroicons/react/outline';
+import { CashIcon, StatusOfflineIcon } from '@heroicons/react/outline';
 import { Mixpanel } from '@lib/mixpanel';
-import { t, Trans } from '@lingui/macro';
+import { t } from '@lingui/macro';
 import clsx from 'clsx';
 import type { Publication } from 'lens';
-import {
-  PublicationReportingFraudSubreason,
-  PublicationReportingSensitiveSubreason,
-  PublicationReportingSpamSubreason,
-  useReportPublicationMutation
-} from 'lens';
+import { PublicationReportingSpamSubreason, useReportPublicationMutation } from 'lens';
 import { stopEventPropagation } from 'lib/stopEventPropagation';
 import type { FC, ReactNode } from 'react';
-import { useState } from 'react';
 import { toast } from 'react-hot-toast';
 import { useGlobalAlertStateStore } from 'src/store/alerts';
-import { useGlobalModalStateStore } from 'src/store/modals';
 import { MOD } from 'src/tracking';
-import { Alert, Button } from 'ui';
+import { Button } from 'ui';
 
 interface ModActionProps {
   publication: Publication;
@@ -32,36 +17,20 @@ interface ModActionProps {
 }
 
 const ModAction: FC<ModActionProps> = ({ publication, className = '' }) => {
-  const setShowReportModal = useGlobalModalStateStore((state) => state.setShowReportModal);
   const setShowModActionAlert = useGlobalAlertStateStore((state) => state.setShowModActionAlert);
-  const [showReportAlert, setShowReportAlert] = useState(false);
-
   const [createReport, { loading }] = useReportPublicationMutation();
 
-  const reportPublication = async ({
-    type,
-    subreason,
-    showToast = true
-  }: {
-    type: string;
-    subreason: string;
-    showToast?: boolean;
-  }) => {
+  const reportPublication = async ({ type, subreason }: { type: string; subreason: string }) => {
     return await createReport({
       variables: {
         request: {
           publicationId: publication?.id,
-          reason: {
-            [type]: { reason: type.replace('Reason', '').toUpperCase(), subreason }
-          }
+          reason: { [type]: { reason: type.replace('Reason', '').toUpperCase(), subreason } }
         }
       },
       onCompleted: () => {
-        setShowModActionAlert(false, null);
-      }
-    }).finally(() => {
-      if (showToast) {
         toast.success(t`Publication reported successfully`);
+        setShowModActionAlert(false, null);
       }
     });
   };
@@ -104,77 +73,16 @@ const ModAction: FC<ModActionProps> = ({ publication, className = '' }) => {
     >
       <ReportButton
         type="spamReason"
-        subreason={PublicationReportingSpamSubreason.FakeEngagement}
-        icon={<ExclamationCircleIcon className="h-4 w-4" />}
-        label={t`Spam`}
-      />
-      <ReportButton
-        type="fraudReason"
-        subreason={PublicationReportingFraudSubreason.Scam}
-        icon={<PhotographIcon className="h-4 w-4" />}
-        label={t`Scam`}
-      />
-      <ReportButton
-        type="sensitiveReason"
-        subreason={PublicationReportingSensitiveSubreason.Nsfw}
-        icon={<ExclamationIcon className="h-4 w-4" />}
-        label={t`NSFW`}
+        subreason={PublicationReportingSpamSubreason.SomethingElse}
+        icon={<CashIcon className="h-4 w-4" />}
+        label={t`Relayer spam`}
       />
       <ReportButton
         type="spamReason"
-        subreason={PublicationReportingSpamSubreason.Repetitive}
-        icon={<RefreshIcon className="h-4 w-4" />}
-        label={t`Repetitive`}
+        subreason={PublicationReportingSpamSubreason.LowSignal}
+        icon={<StatusOfflineIcon className="h-4 w-4" />}
+        label={t`Low signal spam`}
       />
-      <Button
-        disabled={loading}
-        variant="danger"
-        size="sm"
-        outline
-        icon={<BanIcon className="h-4 w-4" />}
-        onClick={() => setShowReportAlert(true)}
-      >
-        <Trans>Report</Trans>
-      </Button>
-      <Button
-        onClick={() => {
-          setShowReportModal(true, publication);
-        }}
-        size="sm"
-        disabled={loading}
-        icon={<ShieldCheckIcon className="h-4 w-4" />}
-      >
-        <Trans>Others</Trans>
-      </Button>
-      <Alert
-        title={t`Report?`}
-        description={t`Are you sure? You want to report this user so that we can take action.`}
-        confirmText={t`Report`}
-        show={showReportAlert}
-        isPerformingAction={loading}
-        isDestructive
-        onConfirm={() => {
-          for (let i = 0; i < 3; i++) {
-            reportPublication({
-              type: 'spamReason',
-              subreason: PublicationReportingSpamSubreason.FakeEngagement,
-              showToast: false
-            }).finally(() => {
-              setShowReportAlert(false);
-            });
-          }
-          Mixpanel.track(MOD.REPORT, {
-            report_reason: 'heavySpam',
-            report_subreason: 'HEAVY_SPAM',
-            report_publication_id: publication?.id
-          });
-        }}
-        onClose={() => setShowReportAlert(false)}
-      >
-        <b>
-          User: <Slug slug={publication.profile.handle} prefix="@" />
-        </b>
-      </Alert>
     </span>
   );
 };
