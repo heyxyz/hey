ff7a0f817aa86aeb204bb2ee7f169f2eab2113e3
0x0d <69431456+bigint@users.noreply.github.com>
2023-04-29 23:10:55 +0530

    feat: add poll length settings modal (#2747)

--- apps/web/src/components/Composer/Actions/PollSettings/PollEditor.tsx
+++ apps/web/src/components/Composer/Actions/PollSettings/PollEditor.tsx
@@ -1,12 +1,17 @@
-import { MenuAlt2Icon } from '@heroicons/react/outline';
+import { ClockIcon, MenuAlt2Icon } from '@heroicons/react/outline';
 import { XCircleIcon } from '@heroicons/react/solid';
-import { t } from '@lingui/macro';
+import { t, Trans } from '@lingui/macro';
 import type { FC } from 'react';
+import { useState } from 'react';
 import { usePublicationStore } from 'src/store/publication';
-import { Card, Tooltip } from 'ui';
+import { Button, Card, Input, Modal, Tooltip } from 'ui';
 
 const PollEditor: FC = () => {
   const setShowPollEditor = usePublicationStore((state) => state.setShowPollEditor);
+  const pollConfig = usePublicationStore((state) => state.pollConfig);
+  const setPollConfig = usePublicationStore((state) => state.setPollConfig);
+
+  const [showPollLengthModal, setShowPollLengthModal] = useState(false);
 
   return (
     <Card className="m-5 px-5 py-3">
@@ -15,7 +20,57 @@ const PollEditor: FC = () => {
           <MenuAlt2Icon className="text-brand h-4 w-4" />
           <b>Poll</b>
         </div>
-        <div>
+        <div className="flex items-center space-x-3">
+          <Button
+            variant="primary"
+            size="sm"
+            icon={<ClockIcon className="h-4 w-4" />}
+            onClick={() => setShowPollLengthModal(true)}
+            outline
+          >
+            {pollConfig.pollLength} days
+          </Button>
+          <Modal
+            title={t`Poll length`}
+            icon={<ClockIcon className="text-brand h-5 w-5" />}
+            show={showPollLengthModal}
+            onClose={() => setShowPollLengthModal(false)}
+          >
+            <div className="p-5">
+              <Input
+                label={t`Poll length (days)`}
+                type="number"
+                value={pollConfig.pollLength}
+                min={1}
+                max={30}
+                onChange={(e) =>
+                  setPollConfig({
+                    ...pollConfig,
+                    pollLength: Number(e.target.value)
+                  })
+                }
+              />
+              <div className="flex space-x-2 pt-5">
+                <Button
+                  className="ml-auto"
+                  variant="danger"
+                  onClick={() => {
+                    setPollConfig({
+                      ...pollConfig,
+                      pollLength: 7
+                    });
+                    setShowPollLengthModal(false);
+                  }}
+                  outline
+                >
+                  <Trans>Cancel</Trans>
+                </Button>
+                <Button className="ml-auto" variant="primary" onClick={() => setShowPollLengthModal(false)}>
+                  <Trans>Save</Trans>
+                </Button>
+              </div>
+            </div>
+          </Modal>
           <Tooltip placement="top" content={t`Delete`}>
             <button className="flex" onClick={() => setShowPollEditor(false)}>
               <XCircleIcon className="h-5 w-5 text-red-400" />
