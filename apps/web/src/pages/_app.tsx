4f3da90c5ce5afe745c31689190d2c816f7c84a1
Alexander Gusev <alxx.gusev@gmail.com>
2023-04-30 19:25:45 +0200

    Added favicons for unread notifications state

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -3,13 +3,45 @@ import '../styles.css';
 import Loading from '@components/Shared/Loading';
 import circluarStd from '@lib/lensterFont';
 import type { AppProps } from 'next/app';
-import { lazy, Suspense } from 'react';
+import Head from 'next/head';
+import { lazy, Suspense, useMemo } from 'react';
+import { useAppPersistStore } from 'src/store/app';
 
 const Providers = lazy(() => import('@components/Common/Providers'));
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const notificationCount = useAppPersistStore(
+    (state) => state.notificationCount
+  );
+
+  const hasNotifications = useMemo(
+    () => notificationCount > 0,
+    [notificationCount]
+  );
+
   return (
     <Suspense fallback={<Loading />}>
+      <Head>
+        <meta name="testme" content="yes" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href={`/favicon-32x32${hasNotifications ? '-notification' : ''}.png`}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href={`/favicon-16x16${hasNotifications ? '-notification' : ''}.png`}
+        />
+      </Head>
+
       <Providers>
         <style jsx global>{`
           body {
--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -23,25 +23,6 @@ class LensterDocument extends Document {
             content="default"
           />
           <meta name="apple-mobile-web-app-title" content="Lenster" />
-
-          {/* Icons */}
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
         </Head>
         <body>
           <Main />
