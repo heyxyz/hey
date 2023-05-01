8521aa8c3a15fa27cf0229f20ec19dad1622041e
bigint <69431456+bigint@users.noreply.github.com>
2023-05-01 21:36:34 +0530

    refactor: notifications dot and count

--- apps/web/src/components/Notification/NotificationIcon.tsx
+++ apps/web/src/components/Notification/NotificationIcon.tsx
@@ -4,17 +4,20 @@ import Link from 'next/link';
 import type { FC } from 'react';
 import { useEffect, useState } from 'react';
 import { useAppPersistStore, useAppStore } from 'src/store/app';
+import { useNotificationPersistStore } from 'src/store/notification';
 
 const NotificationIcon: FC = () => {
   const currentProfile = useAppStore((state) => state.currentProfile);
-  const notificationCount = useAppPersistStore(
-    (state) => state.notificationCount
+  const getNotificationCount = useNotificationPersistStore(
+    (state) => state.getNotificationCount
   );
-  const setNotificationCount = useAppPersistStore(
+  const setNotificationCount = useNotificationPersistStore(
     (state) => state.setNotificationCount
   );
-  const [showBadge, setShowBadge] = useState(false);
-  const { data } = useNotificationCountQuery({
+  const [unreadNotificationCount, setUnreadNotificationCount] =
+    useState<number>(0);
+
+  const { data, loading } = useNotificationCountQuery({
     variables: {
       request: {
         profileId: currentProfile?.id,
@@ -22,28 +25,41 @@ const NotificationIcon: FC = () => {
       }
     },
     skip: !currentProfile?.id,
-    fetchPolicy: 'no-cache' // without no-cache the totalcount is NaN and returns the same.
+    fetchPolicy: 'no-cache'
   });
 
   useEffect(() => {
-    if (currentProfile && data) {
-      const currentCount = data?.notifications?.pageInfo?.totalCount || 0;
-      setShowBadge(notificationCount !== currentCount);
+    if (!currentProfile || loading) {
+      return;
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [data]);
+
+    const currentTotalCount = data?.notifications?.pageInfo?.totalCount || 0;
+    const readNotificationCount = getNotificationCount(currentProfile?.id);
+
+    if (readNotificationCount) {
+      setUnreadNotificationCount(currentTotalCount - readNotificationCount);
+    } else {
+      setNotificationCount(currentProfile?.id, currentTotalCount);
+      setUnreadNotificationCount(0);
+    }
+  }, [data, loading]);
 
   return (
     <Link
       href="/notifications"
       className="hidden min-w-[40px] items-start justify-center rounded-md p-1 hover:bg-gray-300/20 md:flex"
       onClick={() => {
-        setNotificationCount(data?.notifications?.pageInfo?.totalCount || 0);
-        setShowBadge(false);
+        setNotificationCount(
+          currentProfile?.id,
+          data?.notifications?.pageInfo?.totalCount || 0
+        );
+        setUnreadNotificationCount(0);
       }}
     >
       <BellIcon className="h-5 w-5 sm:h-6 sm:w-6" />
-      {showBadge && <span className="h-2 w-2 rounded-full bg-red-500" />}
+      {unreadNotificationCount > 0 && (
+        <span className="h-2 w-2 rounded-full bg-red-500" />
+      )}
     </Link>
   );
 };
