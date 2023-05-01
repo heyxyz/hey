11c4a3d5b5ed5a8724e509593434184d212aab82
Nicholas Molnar <65710+neekolas@users.noreply.github.com>
2023-05-01 12:14:29 -0700

    feat: dexie powered message db (#2711)

---
+++ apps/web/src/components/utils/hooks/useMessageDb.tsx
@@ -0,0 +1,115 @@
+import type { DecodedMessage } from '@xmtp/xmtp-js';
+import { useLiveQuery } from 'dexie-react-hooks';
+import type { Profile } from 'lens';
+import { useCallback } from 'react';
+import { useAppStore } from 'src/store/app';
+import type { LensProfile, PreviewMessage } from 'src/store/message-db';
+import { db } from 'src/store/message-db';
+
+const decodedMessageToPreview = (
+  conversationKey: string,
+  myProfileId: string,
+  decoded: DecodedMessage
+): PreviewMessage => ({
+  conversationKey,
+  myProfileId,
+  sent: decoded.sent,
+  messageBytes: decoded.toBytes()
+});
+
+const assertProfileId = (profileId: string | undefined) => {
+  if (!profileId) {
+    throw new Error('No profile id');
+  }
+};
+
+export const useMessageDb = () => {
+  const currentProfile = useAppStore((state) => state.currentProfile);
+
+  const batchPersistPreviewMessages = useCallback(
+    async (previewMap: Map<string, DecodedMessage>) => {
+      const myProfileId = currentProfile?.id;
+      assertProfileId(myProfileId);
+      await db.transaction('rw', db.previewMessages, async () => {
+        for (const [conversationKey, message] of previewMap.entries()) {
+          const record = decodedMessageToPreview(conversationKey, currentProfile?.id, message);
+          await db.persistPreviewMessage(record);
+        }
+      });
+    },
+    [currentProfile]
+  );
+
+  const persistPreviewMessage = useCallback(
+    async (conversationKey: string, message: DecodedMessage) => {
+      const myProfileId = currentProfile?.id;
+      assertProfileId(myProfileId);
+      const record = decodedMessageToPreview(conversationKey, myProfileId, message);
+      await db.persistPreviewMessage(record);
+    },
+    [currentProfile]
+  );
+
+  const batchPersistProfiles = useCallback(
+    async (profiles: Map<string, Partial<Profile>>) => {
+      const myProfileId = currentProfile?.id;
+      assertProfileId(myProfileId);
+      await db.transaction('rw', db.lensProfiles, async () => {
+        for (const [conversationKey, profile] of profiles.entries()) {
+          const record = { ...profile, myProfileId, conversationKey };
+          await db.persistProfile(record as LensProfile);
+        }
+      });
+    },
+    [currentProfile]
+  );
+
+  const persistProfile = useCallback(
+    async (conversationKey: string, profile: Profile) => {
+      const myProfileId = currentProfile?.id;
+      assertProfileId(myProfileId);
+      const record = { ...profile, myProfileId, conversationKey };
+      await db.persistProfile(record as LensProfile);
+    },
+    [currentProfile]
+  );
+
+  const previewMessages = useLiveQuery(async () => {
+    if (!currentProfile) {
+      return;
+    }
+    return db.previewMessages.where('myProfileId').equals(currentProfile.id).sortBy('sent');
+  }, [currentProfile]);
+
+  const messageProfiles = useLiveQuery(async () => {
+    if (!currentProfile) {
+      return;
+    }
+
+    const profiles = await db.lensProfiles.where('myProfileId').equals(currentProfile.id).sortBy('name');
+
+    return new Map(profiles.map((p) => [p.conversationKey, p]));
+  }, [currentProfile]);
+
+  return {
+    persistPreviewMessage,
+    batchPersistPreviewMessages,
+    batchPersistProfiles,
+    persistProfile,
+    previewMessages,
+    messageProfiles
+  };
+};
+
+export const useGetProfile = (myProfileId: string | undefined, conversationKey: string) => {
+  const profile = useLiveQuery(() => {
+    if (!myProfileId) {
+      return;
+    }
+    return db.lensProfiles.get([myProfileId, conversationKey]);
+  }, [myProfileId, conversationKey]);
+
+  return {
+    profile
+  };
+};
---
+++ apps/web/src/store/message-db.ts
@@ -0,0 +1,42 @@
+import type { Table } from 'dexie';
+import Dexie from 'dexie';
+import type { Profile } from 'lens';
+
+export interface PreviewMessage {
+  conversationKey: string;
+  myProfileId: string;
+  sent: Date;
+  messageBytes: Uint8Array;
+}
+
+export type LensProfile = Profile & {
+  myProfileId: string;
+  conversationKey: string;
+};
+
+export class MessageDB extends Dexie {
+  // 'friends' is added by dexie when declaring the stores()
+  // We just tell the typing system this is the case
+  previewMessages!: Table<PreviewMessage>;
+  lensProfiles!: Table<LensProfile>;
+
+  constructor() {
+    super('messageDb');
+    this.version(1).stores({
+      previewMessages: 'conversationKey, myProfileId, sent', // Primary key and indexed props
+      lensProfiles: '[myProfileId+conversationKey], myProfileId'
+    });
+  }
+
+  async persistPreviewMessage(message: PreviewMessage) {
+    const { conversationKey, myProfileId, sent } = message;
+    await this.previewMessages.put(message, [conversationKey, myProfileId, sent]);
+  }
+
+  async persistProfile(profile: LensProfile) {
+    const { myProfileId, conversationKey } = profile;
+    await this.lensProfiles.put(profile, [myProfileId, conversationKey]);
+  }
+}
+
+export const db = new MessageDB();
