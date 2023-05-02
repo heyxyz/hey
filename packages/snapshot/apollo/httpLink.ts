900bf2c310d37cb30c3304f0bbf1fb2319333b4b
0x0d <69431456+bigint@users.noreply.github.com>
2023-05-02 19:52:48 +0530

    feat: add testnet support for snapshot (#2785)

--- packages/snapshot/apollo/httpLink.ts
+++ packages/snapshot/apollo/httpLink.ts
@@ -1,7 +1,8 @@
 import { HttpLink } from '@apollo/client';
+import { SNAPSHOT_HUB_URL } from 'data';
 
 const httpLink = new HttpLink({
-  uri: 'https://hub.snapshot.org/graphql',
+  uri: `${SNAPSHOT_HUB_URL}/graphql`,
   fetchOptions: 'no-cors',
   fetch
 });
