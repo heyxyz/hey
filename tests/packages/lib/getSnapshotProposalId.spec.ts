770a983cf5f06c28fd6d7184dea376c658e6f742
bigint <69431456+bigint@users.noreply.github.com>
2023-05-02 19:23:24 +0530

    fix: test cases

--- tests/packages/lib/getSnapshotProposalId.spec.ts
+++ tests/packages/lib/getSnapshotProposalId.spec.ts
@@ -3,33 +3,33 @@ import getSnapshotProposalId from 'lib/getSnapshotProposalId';
 
 test.describe('getSnapshotProposalId', () => {
   test('should return null when host is not snapshot.org', () => {
-    expect(getSnapshotProposalId('https://google.com')).toBeNull();
+    expect(getSnapshotProposalId(['https://google.com'])).toBeNull();
   });
 
   test('should return null for invalid urls', () => {
-    expect(getSnapshotProposalId('https://snapshot.org')).toBeNull();
+    expect(getSnapshotProposalId(['https://snapshot.org'])).toBeNull();
     expect(
-      getSnapshotProposalId('https://snapshot.org/#/invalid-url')
+      getSnapshotProposalId(['https://snapshot.org/#/invalid-url'])
     ).toBeNull();
     expect(
-      getSnapshotProposalId(
+      getSnapshotProposalId([
         'https://snapshot.org/#/123.eth/proposal/invalid-id'
-      )
+      ])
     ).toBeNull();
   });
 
   test('should return the proposal id for valid urls', () => {
     expect(
-      getSnapshotProposalId(
+      getSnapshotProposalId([
         'https://snapshot.org/#/123.eth/proposal/0x1234567890123456789012345678901234567890123456789012345678901234'
-      )
+      ])
     ).toEqual(
       '0x1234567890123456789012345678901234567890123456789012345678901234'
     );
     expect(
-      getSnapshotProposalId(
+      getSnapshotProposalId([
         'https://snapshot.org/#/abc123.eth/proposal/0xabcdefABCDEFabcdefABCDEFabcdefABCDEFABCDEFabcdefABCDEFABCDEFabcd'
-      )
+      ])
     ).toEqual(
       '0xabcdefABCDEFabcdefABCDEFabcdefABCDEFABCDEFabcdefABCDEFABCDEFabcd'
     );
