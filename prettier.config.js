f0711ee0f1021605ab5c12fb5cbd12250b293c1e
0x0d <69431456+bigint@users.noreply.github.com>
2023-05-02 17:26:24 +0530

    feat: add proper prettier for monorepo (#2782)

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  semi: true,
+  useTabs: false,
+  trailingComma: 'none',
+  singleQuote: true,
+  tabWidth: 2,
+  endOfLine: 'lf'
+};
