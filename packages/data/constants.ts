506f4082742c7eeea61a05f3bdb89d501f0f04fa
bigint <69431456+bigint@users.noreply.github.com>
2023-05-03 17:49:17 +0530

    feat: add feature flag for testnet

--- packages/data/constants.ts
+++ packages/data/constants.ts
@@ -83,10 +83,13 @@ export const SNAPSHOR_RELAY_WORKER_URL = IS_PRODUCTION
   ? 'https://snapshot-relay.lenster.xyz'
   : 'http://localhost:8085';
 
-// Web3
+// Tokens / Keys
 export const ALCHEMY_KEY = '7jxlM7yIx-aJXDivcEZxsLFFRKQS6-ue';
 export const WALLETCONNECT_PROJECT_ID = 'cd542acc70c2b548030f9901a52e70c8';
 export const LIVEPEER_TOKEN = '4d4daf15-a037-4b8c-b7de-51f1994cc416';
+export const GROWTHBOOK_KEY = IS_MAINNET
+  ? 'sdk-fDLRMwvpyh4Kq3b'
+  : 'sdk-STENQl8vU1da648';
 
 // Regex
 export const URL_REGEX =
