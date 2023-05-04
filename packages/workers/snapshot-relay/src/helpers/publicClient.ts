38d312d2876408199a4553ca80b62b9be326d84d
bigint <69431456+bigint@users.noreply.github.com>
2023-05-04 20:05:00 +0530

    chore: add proper docs

--- packages/workers/snapshot-relay/src/helpers/publicClient.ts
+++ packages/workers/snapshot-relay/src/helpers/publicClient.ts
@@ -1,6 +1,11 @@
 import { createPublicClient, http } from 'viem';
 import { polygon, polygonMumbai } from 'viem/chains';
 
+/**
+ * Create a public client for Polygon and Polygon Mumbai
+ * @param isMainnet Is mainnet
+ * @returns Public viem client
+ */
 const publicClient = (isMainnet: boolean): any => {
   return createPublicClient({
     chain: isMainnet ? polygon : polygonMumbai,
