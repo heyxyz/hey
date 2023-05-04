44e25d422fcda7ce8c6cf13c01c4335992238265
bigint <69431456+bigint@users.noreply.github.com>
2023-05-04 19:26:53 +0530

    fix: loading space and proposal

--- apps/web/src/components/Shared/Snapshot/index.tsx
+++ apps/web/src/components/Shared/Snapshot/index.tsx
@@ -1,9 +1,8 @@
-import { LENSTER_POLLS_SPACE } from 'data';
+import { LENSTER_POLLS_SPACE, ZERO_ADDRESS } from 'data';
 import generateSnapshotAccount from 'lib/generateSnapshotAccount';
 import { stopEventPropagation } from 'lib/stopEventPropagation';
 import type { FC, ReactNode } from 'react';
 import { useState } from 'react';
-import { useInView } from 'react-cool-inview';
 import type { Proposal, Vote } from 'snapshot';
 import { useSnapshotQuery, useSpaceQuery } from 'snapshot';
 import { webClient } from 'snapshot/apollo';
@@ -34,16 +33,13 @@ interface SnapshotProps {
 
 const Snapshot: FC<SnapshotProps> = ({ proposalId }) => {
   const currentProfile = useAppStore((state) => state.currentProfile);
-  const [pollInterval, setPollInterval] = useState(0);
-  const [voterAddress, setVoterAddress] = useState<string | null>(null);
-  const { observe, inView } = useInView();
+  const [voterAddress, setVoterAddress] = useState<string>(ZERO_ADDRESS);
 
   const { loading: spaceLoading } = useSpaceQuery({
     client: webClient,
     variables: { id: proposalId },
     skip: !proposalId,
     onCompleted: async ({ proposal }) => {
-      console.log(proposal);
       if (proposal?.space?.id === LENSTER_POLLS_SPACE) {
         const { address } = await generateSnapshotAccount({
           ownedBy: currentProfile?.ownedBy,
@@ -64,13 +60,8 @@ const Snapshot: FC<SnapshotProps> = ({ proposalId }) => {
       id: proposalId,
       where: { voter: voterAddress, proposal: proposalId }
     },
-    skip: spaceLoading,
-    pollInterval: inView ? pollInterval : 0,
-    onCompleted: (data) => {
-      if (data.proposal?.state === 'active') {
-        setPollInterval(10000);
-      }
-    }
+    skip: spaceLoading || !voterAddress || !proposalId,
+    fetchPolicy: 'no-cache'
   });
 
   if (spaceLoading || loading) {
@@ -93,7 +84,7 @@ const Snapshot: FC<SnapshotProps> = ({ proposalId }) => {
 
   if (isLensterPoll) {
     return (
-      <span onClick={stopEventPropagation} ref={observe}>
+      <span onClick={stopEventPropagation}>
         <Choices
           proposal={proposal as Proposal}
           votes={votes as Vote[]}
@@ -106,7 +97,6 @@ const Snapshot: FC<SnapshotProps> = ({ proposalId }) => {
 
   return (
     <Wrapper dataTestId={`snapshot-${proposal.id}`}>
-      <span ref={observe} />
       <Header proposal={proposal as Proposal} />
       <Choices
         proposal={proposal as Proposal}
