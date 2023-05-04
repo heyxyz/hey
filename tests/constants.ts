90dfa54c25cd886d205938509b09d9b2c632704a
bigint <69431456+bigint@users.noreply.github.com>
2023-05-04 21:52:51 +0530

    chore: fix and add poll test cases

--- tests/constants.ts
+++ tests/constants.ts
@@ -1,4 +1,4 @@
-export const isNightly = true;
+export const isNightly = process.env.NIGHTLY === 'true';
 export const WEB_BASE_URL = isNightly
   ? 'https://lenster.xyz'
   : 'http://localhost:4783';
