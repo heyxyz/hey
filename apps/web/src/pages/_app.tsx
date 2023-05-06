c510c657e67d302c501920cfee4b865e92419d11
bigint <69431456+bigint@users.noreply.github.com>
2023-05-06 22:00:45 +0530

    chore: remove suspense

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -4,24 +4,22 @@ import Loading from '@components/Shared/Loading';
 import circluarStd from '@lib/lensterFont';
 import type { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
-import { Suspense } from 'react';
 
 const Providers = dynamic(() => import('@components/Common/Providers'), {
-  ssr: false
+  ssr: false,
+  loading: () => <Loading />
 });
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <Suspense fallback={<Loading />}>
-      <Providers>
-        <style jsx global>{`
-          body {
-            font-family: ${circluarStd.style.fontFamily};
-          }
-        `}</style>
-        <Component {...pageProps} />
-      </Providers>
-    </Suspense>
+    <Providers>
+      <style jsx global>{`
+        body {
+          font-family: ${circluarStd.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </Providers>
   );
 };
 
