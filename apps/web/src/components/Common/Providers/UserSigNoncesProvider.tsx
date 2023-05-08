24092328fd631d1ab2ec2749ac80d59cf6b4d2a6
bigint <69431456+bigint@users.noreply.github.com>
2023-05-08 14:22:04 +0530

    fix: json rpc errors because of nonce out of sync

---
+++ apps/web/src/components/Common/Providers/UserSigNoncesProvider.tsx
@@ -0,0 +1,27 @@
+import { useUserSigNoncesQuery } from 'lens';
+import type { FC } from 'react';
+import { isSupported, share } from 'shared-zustand';
+import { useAppPersistStore } from 'src/store/app';
+import { useNonceStore } from 'src/store/nonce';
+
+const UserSigNoncesProvider: FC = () => {
+  const profileId = useAppPersistStore((state) => state.profileId);
+  const setUserSigNonce = useNonceStore((state) => state.setUserSigNonce);
+
+  if (isSupported()) {
+    share('userSigNonce', useNonceStore);
+  }
+
+  // Sync nonce every 10 seconds
+  useUserSigNoncesQuery({
+    skip: !profileId,
+    onCompleted: ({ userSigNonces }) => {
+      setUserSigNonce(userSigNonces.lensHubOnChainSigNonce);
+    },
+    pollInterval: 10000
+  });
+
+  return null;
+};
+
+export default UserSigNoncesProvider;
