2efbfc7390639bb5bb42c00ca641f6e8be10420f
bigint <69431456+bigint@users.noreply.github.com>
2023-05-10 00:39:28 +0530

    feat: add getPoll snapshot endpoint (#2839)

--- packages/workers/snapshot-relay/src/index.ts
+++ packages/workers/snapshot-relay/src/index.ts
@@ -1,18 +1,22 @@
 import { createCors, error, Router } from 'itty-router';
 
 import createPoll from './handlers/createPoll';
+import getPoll from './handlers/getPoll';
 import votePoll from './handlers/votePoll';
 import type { Env } from './types';
 
 const { preflight, corsify } = createCors({
   origins: ['*'],
-  methods: ['POST']
+  methods: ['GET', 'POST']
 });
 
 const router = Router();
 
 router.all('*', preflight);
 router.get('/', () => new Response('Snapshot Relay'));
+router.get('/getPoll/:id/:voter', ({ params }) =>
+  getPoll(params.id, params.voter)
+);
 router.post('/createPoll', createPoll);
 router.post('/votePoll', votePoll);
 
