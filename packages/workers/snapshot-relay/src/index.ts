3679dba008afe0fb9d158aa5c2f69c27035dd6af
bigint <69431456+bigint@users.noreply.github.com>
2023-05-15 12:45:13 +0530

    feat: use worker to load and render polls (#2857)
    * feat: use worker to load and render polls
    
    * chore: remove console voter address
    
    * chore: rename to genric names instead of poll
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * chore: rename to proposal_source

--- packages/workers/snapshot-relay/src/index.ts
+++ packages/workers/snapshot-relay/src/index.ts
@@ -1,29 +1,41 @@
-import { createCors, error, Router } from 'itty-router';
+import { createCors, error, json, Router } from 'itty-router';
 
 import createPoll from './handlers/createPoll';
-import getPoll from './handlers/getPoll';
+import getProposal from './handlers/getProposal';
 import votePoll from './handlers/votePoll';
 import type { Env } from './types';
 
 const { preflight, corsify } = createCors({
   origins: ['*'],
-  methods: ['GET', 'POST']
+  methods: ['HEAD', 'GET', 'POST']
 });
 
 const router = Router();
 
 router.all('*', preflight);
 router.get('/', () => new Response('Snapshot Relay'));
-router.get('/getPoll/:id/:voter', ({ params }) =>
-  getPoll(params.id, params.voter)
+router.get('/getProposal/:id/:voter', ({ params }) =>
+  getProposal(params.id, params.voter)
 );
 router.post('/createPoll', createPoll);
 router.post('/votePoll', votePoll);
 
+const routerHandleStack = (request: Request, env: Env, ctx: ExecutionContext) =>
+  router.handle(request, env, ctx).then(json);
+
+const handleFetch = async (
+  request: Request,
+  env: Env,
+  ctx: ExecutionContext
+) => {
+  try {
+    return await routerHandleStack(request, env, ctx);
+  } catch {
+    return error(500);
+  }
+};
+
 export default {
   fetch: (request: Request, env: Env, context: ExecutionContext) =>
-    router
-      .handle(request, env, context)
-      .then(corsify)
-      .catch((error_) => error(500, error_))
+    handleFetch(request, env, context).then(corsify)
 };
