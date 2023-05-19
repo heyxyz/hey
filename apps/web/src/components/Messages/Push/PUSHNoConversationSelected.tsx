e483cf920de8cfefce2926a3f4177afc46cdd4a7
Nilesh Gupta <guptanilesh2312@gmail.com>
2023-05-19 16:19:04 +0530

    default page design updated

--- apps/web/src/components/Messages/Push/PUSHNoConversationSelected.tsx
+++ apps/web/src/components/Messages/Push/PUSHNoConversationSelected.tsx
@@ -3,15 +3,15 @@ import type { FC } from 'react';
 
 const PUSHNoConversationSelected: FC = () => {
   return (
-    <div className="m-auto">
-      <span className="text-center text-5xl">👋 from Push</span>
-      <h3 className="mb-2 mt-3 text-lg">
-        <Trans>Select a conversation</Trans>
-      </h3>
-      <p className="text-md lt-text-gray-500 max-w-xs">
-        <Trans>
-          Choose an existing conversation or create a new one to start messaging
-        </Trans>
+    <div className="m-auto text-center">
+      <div className="rounded-bl-[28px] rounded-br-[28px] rounded-tl-[2px] rounded-tr-[28px] border border-gray-300 p-5 text-center text-3xl">
+        Say 👋 to Lenster Messages
+      </div>
+      <p className="text-md lt-text-gray-500 m-auto mt-6 max-w-xs">
+        <Trans>You haven’t started a conversation yet.</Trans>
+      </p>
+      <p className="text-md lt-text-gray-500 m-auto max-w-sm">
+        <Trans>Start a new chat by searching or using the + button</Trans>
       </p>
     </div>
   );
