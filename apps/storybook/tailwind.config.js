79b032ad8fefaffa093fb6ed01beb2d316d7ce19
bigint <69431456+bigint@users.noreply.github.com>
2023-05-21 20:04:47 +0530

    chore: rip storybook (#2898)

--- apps/storybook/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-const base = require('ui/tailwind-preset');
-
-module.exports = {
-  ...base,
-  content: ['./stories/**/*.{ts,tsx}', '../../packages/ui/src/*.{ts,tsx}'],
-  plugins: [require('@tailwindcss/aspect-ratio'), require('@tailwindcss/forms')]
-};
