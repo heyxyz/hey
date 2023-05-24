976b32868b375fd768ad81b1929e17ade9e5c604
bigint <69431456+bigint@users.noreply.github.com>
2023-05-24 19:31:05 +0530

    feat: add r2 cache

--- packages/workers/iframely/src/handlers/getIframely.ts
+++ packages/workers/iframely/src/handlers/getIframely.ts
@@ -19,13 +19,27 @@ export default async (url: string, env: Env) => {
     const object = await env.LENSTER_IFRAMELY.get(key);
 
     if (object) {
+      const headers = new Headers();
+      object.writeHttpMetadata(headers);
+      headers.set('etag', object.httpEtag);
+
+      const responseBody = new Response(object.body, { headers });
+
       return new Response(
-        JSON.stringify({ success: true, fromR2: true, data: object.body })
+        JSON.stringify({
+          success: true,
+          cached: true,
+          key,
+          data: await responseBody.json()
+        })
       );
     }
+
     await env.LENSTER_IFRAMELY.put(key, JSON.stringify(data));
 
-    return new Response(JSON.stringify({ success: true, data }));
+    return new Response(
+      JSON.stringify({ success: true, cached: false, key, data })
+    );
   } catch (error) {
     console.error('Failed to get iframely data', error);
     return new Response(
