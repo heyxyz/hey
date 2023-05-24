77bdd658ae5081d7e8f599ad288037232bb27b2f
bigint <69431456+bigint@users.noreply.github.com>
2023-05-24 20:20:26 +0530

    chore: update iframely domain cache

--- packages/workers/iframely/src/handlers/getIframely.ts
+++ packages/workers/iframely/src/handlers/getIframely.ts
@@ -14,16 +14,12 @@ export default async (request: IRequest, env: Env) => {
     }
 
     const decodedUrl = decodeURIComponent(url);
-    const response = await fetch(
-      `https://iframely-node.lenster.xyz/iframely?url=${decodedUrl}`
-    );
 
     const encodedSeed = new TextEncoder().encode(url);
     const digest = await crypto.subtle.digest({ name: 'SHA-256' }, encodedSeed);
     const key = [...new Uint8Array(digest)]
       .map((b) => b.toString(16).padStart(2, '0'))
       .join('');
-    const data = await response.json();
     const object = await env.LENSTER_IFRAMELY.get(key);
 
     if (object) {
@@ -31,22 +27,21 @@ export default async (request: IRequest, env: Env) => {
       object.writeHttpMetadata(headers);
       headers.set('etag', object.httpEtag);
       const responseBody = new Response(object.body, { headers });
-      const cachedResponse = new Response(
+
+      return new Response(
         JSON.stringify({
           success: true,
           cached: true,
           key,
           iframely: await responseBody.json()
-        }),
-        {
-          headers: {
-            'Cache-Control': 'public, max-age=31536000'
-          }
-        }
+        })
       );
-
-      return cachedResponse;
     }
+
+    const response = await fetch(
+      `https://iframely-node.lenster.xyz/iframely?url=${decodedUrl}`
+    );
+    const data = await response.json();
 
     await env.LENSTER_IFRAMELY.put(key, JSON.stringify(data));
 
