2c5733e5a065b3926b21a8191ce6e324953c89ec
bigint <69431456+bigint@users.noreply.github.com>
2023-05-25 16:29:36 +0530

    chore: add cache to getIframely

--- packages/workers/iframely/src/handlers/getIframely.ts
+++ packages/workers/iframely/src/handlers/getIframely.ts
@@ -29,12 +29,7 @@ export default async (request: IRequest, env: Env) => {
           cached: true,
           key,
           iframely: await responseBody.json()
-        }),
-        {
-          headers: {
-            'cache-control': 'public, max-age=31536000, immutable'
-          }
-        }
+        })
       );
     }
 
