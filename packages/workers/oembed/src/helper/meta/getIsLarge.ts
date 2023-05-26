483184fd1693df99b68dcb71abe5a033d3286a04
bigint <69431456+bigint@users.noreply.github.com>
2023-05-26 17:10:07 +0530

    feat: add lens open graph standards (#2948)
    * feat: add lenster og standards
    
    * chore: migrate to lens standards
    
    * chore: migrate to lens standards

--- packages/workers/oembed/src/helper/meta/getIsLarge.ts
+++ packages/workers/oembed/src/helper/meta/getIsLarge.ts
@@ -1,13 +1,19 @@
 import type { Document } from 'linkedom';
 
 const getIsLarge = (document: Document): boolean | null => {
+  const lens =
+    document.querySelector('meta[name="lens:card"]') ||
+    document.querySelector('meta[property="lens:card"]');
   const twitter =
     document.querySelector('meta[name="twitter:card"]') ||
     document.querySelector('meta[property="twitter:card"]');
 
   const largeTypes = ['summary_large_image', 'player'];
 
-  if (twitter) {
+  if (lens) {
+    const card = lens.getAttribute('content') || '';
+    return largeTypes.includes(card);
+  } else if (twitter) {
     const card = twitter.getAttribute('content') || '';
     return largeTypes.includes(card);
   }
