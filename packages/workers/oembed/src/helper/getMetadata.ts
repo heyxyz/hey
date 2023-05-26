de570173cdd5d56d120ca19c5d6ef575818395f4
bigint <69431456+bigint@users.noreply.github.com>
2023-05-26 22:56:46 +0530

    feat: add oembed proxy

--- packages/workers/oembed/src/helper/getMetadata.ts
+++ packages/workers/oembed/src/helper/getMetadata.ts
@@ -1,5 +1,7 @@
+import { encode } from '@cfworker/base64url';
 import { parseHTML } from 'linkedom';
 
+import type { Env } from '../types';
 import generateIframe from './meta/generateIframe';
 import getDescription from './meta/getDescription';
 import getEmbedUrl from './meta/getEmbedUrl';
@@ -18,7 +20,7 @@ interface Metadata {
   html: string | null;
 }
 
-const getMetadata = async (url: string): Promise<any> => {
+const getMetadata = async (url: string, env: Env): Promise<any> => {
   const { html } = await fetch(url, {
     cf: {
       cacheTtl: 60 * 60 * 24 * 7,
@@ -30,13 +32,22 @@ const getMetadata = async (url: string): Promise<any> => {
   }));
 
   const { document } = parseHTML(html);
+  const isLarge = getIsLarge(document);
+  const image = getImage(document);
+  const isProduction = env.WORKER_ENV === 'production';
+  const workerUrl = isProduction
+    ? 'https://oembed.lenster.xyz'
+    : 'http://localhost:8087';
+  const proxiedUrl = `${workerUrl}/image?hash=${encode(
+    image || ''
+  )}&transform=${isLarge ? 'large' : 'square'}`;
   const metadata: Metadata = {
     url,
     title: getTitle(document),
     description: getDescription(document),
-    image: getImage(document),
+    image: image ? proxiedUrl : null,
     site: getSite(document),
-    isLarge: getIsLarge(document),
+    isLarge,
     html: generateIframe(getEmbedUrl(document), url)
   };
 
