e6c5597112814e28407d2394bf3a1e6190c3dde4
bigint <69431456+bigint@users.noreply.github.com>
2023-05-26 12:23:42 +0530

    fix: width in oembeds

--- packages/workers/oembed/src/helper/iframe.ts
+++ packages/workers/oembed/src/helper/iframe.ts
@@ -3,12 +3,10 @@ const generateIframe = (url: string, site: string) => {
     case 'youtube.com':
     case 'youtu.be':
     case 'lenstube.xyz':
-      return `<iframe width="560" height="315" src="${url}" frameborder="0" allow="accelerometer; autoplay; clipboard-write;" allowfullscreen></iframe>`;
-    case 'oohlala.xyz':
-      return `<iframe src="${url}" style="top: 0; left: 0; width: 100%; height: 100%; position: absolute; border: 0;" allowfullscreen scrolling="no"></iframe>`;
+      return `<iframe src="${url}" width="560" height="315" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media" allowfullscreen></iframe>`;
     case 'open.spotify.com':
       const spotifyUrl = url.replace('/track', '/embed/track');
-      return `<iframe src="${spotifyUrl}" style="border-radius: 12px" width="100%" height="152" frameborder="0" allowfullscreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`;
+      return `<iframe src="${spotifyUrl}" width="560" height="155" frameborder="0" allow="autoplay; clipboard-write; encrypted-media"></iframe>`;
     default:
       return `<iframe src="${url}" frameborder="0" allowfullscreen></iframe>`;
   }
