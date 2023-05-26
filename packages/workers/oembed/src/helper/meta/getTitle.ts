2ef91be99048000d66d86557fc2d2d3d6eb2116d
bigint <69431456+bigint@users.noreply.github.com>
2023-05-26 17:10:07 +0530

    feat: add lens open graph standards (#2948)
    * feat: add lenster og standards
    
    * chore: migrate to lens standards
    
    * chore: migrate to lens standards

--- packages/workers/oembed/src/helper/meta/getTitle.ts
+++ packages/workers/oembed/src/helper/meta/getTitle.ts
@@ -1,12 +1,19 @@
 import type { Document } from 'linkedom';
 
 const getTitle = (document: Document): string | null => {
-  const og = document.querySelector('meta[property="og:title"]');
+  const lens =
+    document.querySelector('meta[name="lens:title"]') ||
+    document.querySelector('meta[property="lens:title"]');
+  const og =
+    document.querySelector('meta[name="og:title"]') ||
+    document.querySelector('meta[property="og:title"]');
   const twitter =
     document.querySelector('meta[name="twitter:title"]') ||
     document.querySelector('meta[property="twitter:title"]');
 
-  if (og) {
+  if (lens) {
+    return lens.getAttribute('content');
+  } else if (og) {
     return og.getAttribute('content');
   } else if (twitter) {
     return twitter.getAttribute('content');
