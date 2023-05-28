e56820572d164d61be96979226c77b09b3d3f239
bigint <69431456+bigint@users.noreply.github.com>
2023-05-28 15:17:20 +0530

    feat: add worker

--- apps/web/src/lib/leafwatch.ts
+++ apps/web/src/lib/leafwatch.ts
@@ -1,14 +1,42 @@
-import { MIXPANEL_ENABLED } from 'data/constants';
+import { LEAFWATCH_WORKER_URL, Localstorage } from 'data';
 import type { Dict } from 'mixpanel-browser';
-import mixpanel from 'mixpanel-browser';
+
+let worker: Worker;
+
+if (typeof Worker !== 'undefined') {
+  worker = new Worker(new URL('./worker', import.meta.url));
+}
 
 /**
  * Leafwatch analytics
  */
 export const Mixpanel = {
-  track: (name: string, props?: Dict) => {
-    if (MIXPANEL_ENABLED) {
-      mixpanel.track(name, props);
-    }
+  track: (name: string, properties?: Dict) => {
+    const fingerprint = JSON.parse(
+      localStorage.getItem(Localstorage.FingerprintStore) ||
+        JSON.stringify({ state: { fingerprint: null } })
+    );
+    const user = JSON.parse(
+      localStorage.getItem(Localstorage.LensterStore) ||
+        JSON.stringify({ state: { profileId: null } })
+    );
+    const { referrer } = document;
+    const referrerDomain = referrer ? new URL(referrer).hostname : null;
+
+    worker.postMessage({
+      name,
+      properties,
+      user_id: user.state.profileId,
+      fingerprint: fingerprint.state.fingerprint,
+      referrer: referrerDomain
+    });
+
+    worker.onmessage = function (event: MessageEvent) {
+      const response = event.data;
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', `${LEAFWATCH_WORKER_URL}/ingest`);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.send(JSON.stringify(response));
+    };
   }
 };
