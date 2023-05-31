fa20294c4241805087c9e40743c069bf5dd730d4
Alain Nicolas <alainncls@users.noreply.github.com>
2023-05-31 11:27:52 +0200

    LENS-91 - Create a service to provide the frontend with an STS endpoint (#46)

--- packages/workers/sts-generator/src/index.ts
+++ packages/workers/sts-generator/src/index.ts
@@ -1,26 +1,75 @@
-import { AssumeRoleCommand, STSClient } from '@aws-sdk/client-sts';
+import { AssumeRoleCommand, AssumeRoleRequest, STSClient } from '@aws-sdk/client-sts';
+import dotenv from 'dotenv';
+import express from 'express';
+import { promises as fs } from 'fs';
 
-interface EnvType {
-  EVER_ACCESS_KEY: string;
-  EVER_ACCESS_SECRET: string;
+dotenv.config();
+
+interface Env {
+  bucketName: string;
+  bucketRegion: string;
+  stsEndpoint: string;
+  filePath: string;
+  secretContent?: string;
 }
 
-export default {
-  async fetch(request: Request, env: EnvType) {
-    return await handleRequest(request, env);
+let envVars: Env;
+let params: AssumeRoleRequest;
+const app = express();
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', process.env.CORS ?? 'http://localhost:4783');
+  res.header('Access-Control-Allow-Methods', 'GET');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.get('/', async (req, res) => {
+  try {
+    if (!envVars.secretContent || envVars.secretContent === '') {
+      envVars.secretContent = await fs.readFile(envVars.filePath, { encoding: 'utf8' });
+    }
+
+    const { accessKeyId, secretAccessKey } = JSON.parse(envVars.secretContent);
+
+    const stsClient = new STSClient({
+      endpoint: envVars.stsEndpoint,
+      region: envVars.bucketRegion,
+      credentials: { accessKeyId, secretAccessKey }
+    });
+
+    const data = await stsClient.send(new AssumeRoleCommand(params));
+
+    res.send(
+      JSON.stringify({
+        success: true,
+        accessKeyId: data.Credentials?.AccessKeyId,
+        secretAccessKey: data.Credentials?.SecretAccessKey,
+        sessionToken: data.Credentials?.SessionToken
+      })
+    );
+  } catch (error) {
+    console.error(error);
+    res.send(JSON.stringify({ success: false, message: 'Something went wrong!' }));
   }
-};
+});
 
-const headers = {
-  'Access-Control-Allow-Origin': '*',
-  'Content-Type': 'application/json'
-};
-const bucketName = 'lenster-media';
-const everEndpoint = 'https://endpoint.4everland.co';
+const port = process.env.PORT ?? 8082;
 
-const params = {
-  DurationSeconds: 900,
-  Policy: `{
+app.listen(port, () => {
+  envVars = {
+    bucketName: process.env.BUCKET_NAME ?? 'lineaster-media',
+    bucketRegion: process.env.BUCKET_REGION ?? 'us-west-2',
+    stsEndpoint: process.env.STS_ENDPOINT ?? 'https://sts.amazonaws.com',
+    filePath: process.env.FILE_PATH ?? '',
+    secretContent: process.env.SECRET_CONTENT
+  };
+
+  params = {
+    RoleArn: undefined,
+    RoleSessionName: undefined,
+    DurationSeconds: 900,
+    Policy: `{
     "Version": "2012-10-17",
     "Statement": [
       {
@@ -30,42 +79,12 @@ const params = {
           "s3:GetObject"
         ],
         "Resource": [
-          "arn:aws:s3:::${bucketName}/*"
+          "arn:aws:s3:::${envVars.bucketName}/*"
         ]
       }
     ]
   }`
-};
+  };
 
-async function handleRequest(request: Request, env: EnvType) {
-  try {
-    const accessKeyId = env.EVER_ACCESS_KEY;
-    const secretAccessKey = env.EVER_ACCESS_SECRET;
-
-    const stsClient = new STSClient({
-      endpoint: everEndpoint,
-      region: 'us-west-2',
-      credentials: { accessKeyId, secretAccessKey }
-    });
-
-    const data = await stsClient.send(
-      new AssumeRoleCommand({
-        ...params,
-        RoleArn: undefined,
-        RoleSessionName: undefined
-      })
-    );
-
-    return new Response(
-      JSON.stringify({
-        success: true,
-        accessKeyId: data.Credentials?.AccessKeyId,
-        secretAccessKey: data.Credentials?.SecretAccessKey,
-        sessionToken: data.Credentials?.SessionToken
-      }),
-      { headers }
-    );
-  } catch {
-    return new Response(JSON.stringify({ success: false, message: 'Something went wrong!' }), { headers });
-  }
-}
+  console.log(`ready - started server on 0.0.0.0:8082, url: http://localhost:${port}`);
+});
