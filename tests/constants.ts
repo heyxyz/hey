fa3c3e11636d1feedfa362b298f4a10242c035c6
Alain Nicolas <alainncls@users.noreply.github.com>
2023-05-31 15:48:27 +0200

    [CI] Activate nightly tests (#54)

--- tests/constants.ts
+++ tests/constants.ts
@@ -1,4 +1,4 @@
 export const isNightly = process.env.NIGHTLY === 'true';
-export const WEB_BASE_URL = isNightly ? 'https://lenster.xyz' : 'http://localhost:4783';
+export const WEB_BASE_URL = isNightly ? 'https://lineaster.dev.linea.build' : 'http://localhost:4783';
 export const PRERENDER_BASE_URL = isNightly ? 'https://prerender.lenster.xyz' : 'http://localhost:4784';
 export const METADATA_BASE_URL = isNightly ? 'https://metadata.lenster.xyz' : 'http://localhost:8083';
