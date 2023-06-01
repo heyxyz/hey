2586b08a7056244f4cba795b29c4da57ea4fb102
bigint <69431456+bigint@users.noreply.github.com>
2023-06-01 12:16:42 +0530

    fix: referrer-policy to render oembed

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -58,7 +58,7 @@ const nextConfig = {
         headers: [
           { key: 'X-Content-Type-Options', value: 'nosniff' },
           { key: 'X-XSS-Protection', value: '1; mode=block' },
-          { key: 'Referrer-Policy', value: 'strict-origin' }
+          { key: 'Referrer-Policy', value: 'no-referrer-when-downgrade' }
         ]
       },
       { source: '/about', headers },
