41c7304dcd1d083474cab0ec5fe21ed6fa6d7fe7
Carlos Beltran <imthatcarlos@gmail.com>
2023-06-01 08:01:45 -0500

    lint

--- packages/madfi/index.ts
+++ packages/madfi/index.ts
@@ -2,8 +2,4 @@ import useFetchSuggestedFollowsForProfile from './useFetchSuggestedFollowsForPro
 import useLogImpression from './useLogImpression';
 import { MADFI_API_KEY } from './utils';
 
-export {
-  useFetchSuggestedFollowsForProfile,
-  useLogImpression,
-  MADFI_API_KEY
-};
+export { MADFI_API_KEY, useFetchSuggestedFollowsForProfile, useLogImpression };
