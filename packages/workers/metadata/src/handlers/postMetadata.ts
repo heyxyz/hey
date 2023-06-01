e53fccaa1fc9d3c4f1202c98109d8d90d101eb57
bigint <69431456+bigint@users.noreply.github.com>
2023-06-01 14:12:23 +0530

    refactor: metadata worker to use itty-router

---
+++ packages/workers/metadata/src/handlers/postMetadata.ts
@@ -0,0 +1,36 @@
+import { createData, EthereumSigner } from '@lenster/bundlr';
+import type { IRequest } from 'itty-router';
+
+import type { Env } from '../types';
+
+export default async (request: IRequest, env: Env) => {
+  try {
+    const payload = await request.json();
+    const signer = new EthereumSigner(env.BUNDLR_PRIVATE_KEY);
+    const tx = createData(JSON.stringify(payload), signer, {
+      tags: [
+        { name: 'content-type', value: 'application/json' },
+        { name: 'App-Name', value: 'Lenster' }
+      ]
+    });
+    await tx.sign(signer);
+    const bundlrRes = await fetch('http://node2.bundlr.network/tx/matic', {
+      method: 'POST',
+      headers: { 'content-type': 'application/octet-stream' },
+      body: tx.getRaw()
+    });
+
+    if (bundlrRes.statusText === 'Created' || bundlrRes.statusText === 'OK') {
+      return new Response(JSON.stringify({ success: true, id: tx.id }));
+    } else {
+      return new Response(
+        JSON.stringify({ success: false, message: 'Bundlr error!', bundlrRes })
+      );
+    }
+  } catch (error) {
+    console.error('Failed to get STS data', error);
+    return new Response(
+      JSON.stringify({ success: false, error: 'Something went wrong!' })
+    );
+  }
+};
