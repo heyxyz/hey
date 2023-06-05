bd28d695eaf73fb82d93b8e28c6226058668f4f5
Sébastien Van Eyck <sebastien.vaneyck@consensys.net>
2023-06-05 16:27:41 +0200

    feat(LENS-108): [UI] Replace favicon and any others lenster logo usage (#66)

--- apps/web/src/components/Shared/Loading.tsx
+++ apps/web/src/components/Shared/Loading.tsx
@@ -6,7 +6,10 @@ const Loading: FC = () => {
   return (
     <div className="grid h-screen place-items-center">
       <MetaTags />
-      <img className="h-28 w-28" height={112} width={112} src="/logo.svg" alt="Logo" />
+      <span className="relative flex h-24 w-24">
+        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-brand-500 opacity-75" />
+        <span className="relative inline-flex rounded-full h-24 w-24 bg-brand-500" />
+      </span>
     </div>
   );
 };
--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -9,22 +9,87 @@ class LensterDocument extends Document {
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 
           {/* Prefetch and Preconnect */}
-          <link rel="preconnect" href="https://user-content.lenster.xyz" />
+          {/* <link rel="preconnect" href="https://user-content.lenster.xyz" />
           <link rel="dns-prefetch" href="https://user-content.lenster.xyz" />
           <link rel="preconnect" href="https://static-assets.lenster.xyz" />
-          <link rel="dns-prefetch" href="https://static-assets.lenster.xyz" />
+          <link rel="dns-prefetch" href="https://static-assets.lenster.xyz" /> */}
 
           {/* Misc */}
-          <meta name="application-name" content="Lenster" />
+          <meta name="application-name" content="Lineaster" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-          <meta name="apple-mobile-web-app-title" content="Lenster" />
+          <meta name="apple-mobile-web-app-title" content="Lineaster" />
 
           {/* Icons */}
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon-dark.png"
+            media="(prefers-color-scheme: light)"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon-light.png"
+            media="(prefers-color-scheme: dark)"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="512x512"
+            href="/android-chrome-512x512-dark.png"
+            media="(prefers-color-scheme: light)"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="512x512"
+            href="/android-chrome-512x512-light.png"
+            media="(prefers-color-scheme: dark)"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="/android-chrome-192x192-dark.png"
+            media="(prefers-color-scheme: light)"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="/android-chrome-192x192-light.png"
+            media="(prefers-color-scheme: dark)"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32-dark.png"
+            media="(prefers-color-scheme: light)"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32-light.png"
+            media="(prefers-color-scheme: dark)"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16-dark.png"
+            media="(prefers-color-scheme: light)"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16-light.png"
+            media="(prefers-color-scheme: dark)"
+          />
         </Head>
         <body>
           <Main />
