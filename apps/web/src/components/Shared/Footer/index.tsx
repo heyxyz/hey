a2aaccc2fc9de61a9817fc6626e729eb0b51af0a
Alain Nicolas <alainncls@users.noreply.github.com>
2023-06-06 17:04:43 +0200

    LENS-110 - Remove the link to Lenster Discord (#71)

--- apps/web/src/components/Shared/Footer/index.tsx
+++ apps/web/src/components/Shared/Footer/index.tsx
@@ -23,9 +23,6 @@ const Footer: FC = () => {
         <a href="https://discord.com/invite/9QwXqsyAps" target="_blank" rel="noreferrer noopener">
           <Trans>ConsenSys Discord</Trans>
         </a>
-        <a href="https://lenster.xyz/discord" target="_blank" rel="noreferrer noopener">
-          <Trans>Lenster Discord</Trans>
-        </a>
         <a href="https://lenster.xyz/donate" target="_blank" rel="noreferrer noopener">
           <Trans>Donate to Lenster</Trans>
         </a>
