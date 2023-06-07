50a612ae0e82ae27356224f411e5ceeaa33b54a5
bigint <69431456+bigint@users.noreply.github.com>
2023-06-07 22:49:14 +0530

    chore: update verified list

--- packages/data/verified.ts
+++ packages/data/verified.ts
@@ -2,6 +2,7 @@ import { aaveMembers } from './aave-members';
 import { mainnetStaffs } from './staffs';
 
 export const mainnetVerified = [
+  '0x3a77', // kipto
   '0x01cc3a', // zonday
   '0x01c8b6', // alchemy
   '0x01a7a3', // bundlr-network
