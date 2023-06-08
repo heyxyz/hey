ab1e92b7f9ca9c10b0bd1d8e5a41ef89920eb768
bigint <69431456+bigint@users.noreply.github.com>
2023-06-08 20:21:32 +0530

    feat: spaces api

--- packages/workers/spaces/src/apollo/cache.ts
+++
@@ -1,9 +0,0 @@
-import { InMemoryCache } from '@apollo/client';
-
-import result from '../../generated';
-
-const cache = new InMemoryCache({
-  possibleTypes: result.possibleTypes
-});
-
-export default cache;
--- packages/workers/spaces/src/apollo/retryLink.ts
+++
@@ -1,9 +0,0 @@
-import { RetryLink } from '@apollo/client/link/retry';
-
-// RetryLink is a link that retries requests based on the status code returned.
-const retryLink = new RetryLink({
-  delay: { initial: 100 },
-  attempts: { max: 2, retryIf: (error) => Boolean(error) }
-});
-
-export default retryLink;
