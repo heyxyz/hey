4635d146b815abc76e24ce72aebcccc3be5d937a
bigint <69431456+bigint@users.noreply.github.com>
2023-06-08 20:02:11 +0530

    feat: init spaces mvp (#3025)
    * feat: init spaces
    
    * style: Format code with prettier
    
    * feat: add mvp design
    
    * fix: lint
    
    * chore: add spaces feature flag

--- apps/web/src/components/Shared/SmallUserProfile.tsx
+++ apps/web/src/components/Shared/SmallUserProfile.tsx
@@ -6,6 +6,7 @@ import isVerified from '@lenster/lib/isVerified';
 import sanitizeDisplayName from '@lenster/lib/sanitizeDisplayName';
 import { Image } from '@lenster/ui';
 import { formatTime, getTwitterFormat } from '@lib/formatTime';
+import clsx from 'clsx';
 import Link from 'next/link';
 import type { FC } from 'react';
 import { memo } from 'react';
@@ -15,19 +16,24 @@ import Slug from './Slug';
 interface UserProfileProps {
   profile: Profile;
   timestamp?: Date;
+  smallAvatar?: boolean;
 }
 
 const SmallUserProfile: FC<UserProfileProps> = ({
   profile,
-  timestamp = ''
+  timestamp = '',
+  smallAvatar = false
 }) => {
   const UserAvatar = () => (
     <Image
       src={getAvatar(profile)}
       loading="lazy"
-      className="h-6 w-6 rounded-full border bg-gray-200 dark:border-gray-700"
-      height={24}
-      width={24}
+      className={clsx(
+        smallAvatar ? 'h-5 w-5' : 'h-6 w-6',
+        'rounded-full border bg-gray-200 dark:border-gray-700'
+      )}
+      height={smallAvatar ? 20 : 24}
+      width={smallAvatar ? 20 : 24}
       alt={formatHandle(profile?.handle)}
     />
   );
