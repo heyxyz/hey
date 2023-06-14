f17d2a67f8ebe761d6c2a1a657edb99ca4a151c3
bigint <69431456+bigint@users.noreply.github.com>
2023-06-14 15:11:11 +0530

    chore: add space settings in composer (#3096)
    * chore: add space settings in composer with ff
    
    * feat: add space states
    
    * feat: update placeholder
    
    * chore: update create post button
    
    * feat: add space to composer
    
    * chore: rename attr to audioSpace
    
    * chore: rename attr to audioSpace

---
+++ apps/web/src/components/Composer/Actions/SpaceSettings/index.tsx
@@ -0,0 +1,37 @@
+import { MicrophoneIcon } from '@heroicons/react/outline';
+import { FeatureFlag } from '@lenster/data';
+import { Tooltip } from '@lenster/ui';
+import { Growthbook } from '@lib/growthbook';
+import { t } from '@lingui/macro';
+import { motion } from 'framer-motion';
+import type { FC } from 'react';
+import { usePublicationStore } from 'src/store/publication';
+
+const SpaceSettings: FC = () => {
+  const showSpaceEditor = usePublicationStore((state) => state.showSpaceEditor);
+  const setShowSpaceEditor = usePublicationStore(
+    (state) => state.setShowSpaceEditor
+  );
+  const { on: isSpacesEnabled } = Growthbook.feature(FeatureFlag.Spaces);
+
+  if (!isSpacesEnabled) {
+    return null;
+  }
+
+  return (
+    <Tooltip placement="top" content={t`Space`}>
+      <motion.button
+        whileTap={{ scale: 0.9 }}
+        type="button"
+        onClick={() => {
+          setShowSpaceEditor(!showSpaceEditor);
+        }}
+        aria-label="Space"
+      >
+        <MicrophoneIcon className="text-brand h-5 w-5" />
+      </motion.button>
+    </Tooltip>
+  );
+};
+
+export default SpaceSettings;
