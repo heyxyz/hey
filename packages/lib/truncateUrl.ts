5c53e124f50a51733627b1dc11391116d2ea2d5f
Olof Andersson <olof.andersson@gmail.com>
2023-06-15 17:45:46 +0200

    rename parameter to url

--- packages/lib/truncateUrl.ts
+++ packages/lib/truncateUrl.ts
@@ -1,16 +1,16 @@
 /**
  * Truncate a string by character length and add ellipsis.
- * @param string string to truncate
+ * @param url string to truncate
  * @param maxLength number of characters to truncate to
  * @returns truncated string
  */
-const truncateUrl = (string: string, maxLength: number): string => {
-  string = string.replace(/^(http|https):\/\//, '');
-  string = string.replace(/^www\./, '');
-  if (string.length > maxLength) {
-    return string.substring(0, maxLength - 1) + '…';
+const truncateUrl = (url: string, maxLength: number): string => {
+  url = url.replace(/^(http|https):\/\//, '');
+  url = url.replace(/^www\./, '');
+  if (url.length > maxLength) {
+    return url.substring(0, maxLength - 1) + '…';
   }
-  return string;
+  return url;
 };
 
 export default truncateUrl;
