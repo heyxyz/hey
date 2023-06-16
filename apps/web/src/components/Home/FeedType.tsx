f4d7746a01d4b34c71aad479f7013800f6fc8599
bigint <69431456+bigint@users.noreply.github.com>
2023-06-16 19:12:38 +0530

    feat: add for you tab (#3129)

--- apps/web/src/components/Home/FeedType.tsx
+++ apps/web/src/components/Home/FeedType.tsx
@@ -1,4 +1,10 @@
-import { SparklesIcon, ViewListIcon } from '@heroicons/react/outline';
+import {
+  LightBulbIcon,
+  SparklesIcon,
+  UserGroupIcon
+} from '@heroicons/react/outline';
+import { FeatureFlag } from '@lenster/data';
+import isFeatureEnabled from '@lenster/lib/isFeatureEnabled';
 import { TabButton } from '@lenster/ui';
 import { Leafwatch } from '@lib/leafwatch';
 import { t } from '@lingui/macro';
@@ -8,39 +14,59 @@ import { MISCELLANEOUS } from 'src/tracking';
 import FeedEventFilters from './FeedEventFilters';
 import SeeThroughLens from './SeeThroughLens';
 
+export enum Type {
+  FOR_YOU = 'FOR_YOU',
+  FOLLOWING = 'FOLLOWING',
+  HIGHLIGHTS = 'HIGHLIGHTS'
+}
+
 interface FeedTypeProps {
-  setFeedType: Dispatch<'TIMELINE' | 'HIGHLIGHTS'>;
-  feedType: 'TIMELINE' | 'HIGHLIGHTS';
+  setFeedType: Dispatch<Type>;
+  feedType: Type;
 }
 
 const FeedType: FC<FeedTypeProps> = ({ setFeedType, feedType }) => {
+  const isForYouEnabled = isFeatureEnabled(FeatureFlag.ForYou);
+
   return (
     <div className="flex flex-wrap items-center justify-between px-1 md:px-0">
       <div className="flex gap-3 overflow-x-auto sm:px-0">
+        {isForYouEnabled && (
+          <TabButton
+            name={t`For you`}
+            icon={<SparklesIcon className="h-4 w-4" />}
+            active={feedType === Type.FOR_YOU}
+            showOnSm={false}
+            onClick={() => {
+              setFeedType(Type.FOR_YOU);
+              Leafwatch.track(MISCELLANEOUS.SWITCH_FOR_YOU_FEED);
+            }}
+          />
+        )}
         <TabButton
-          name={t`Timeline`}
-          icon={<ViewListIcon className="h-4 w-4" />}
-          active={feedType === 'TIMELINE'}
+          name={t`Following`}
+          icon={<UserGroupIcon className="h-4 w-4" />}
+          active={feedType === Type.FOLLOWING}
           showOnSm={false}
           onClick={() => {
-            setFeedType('TIMELINE');
-            Leafwatch.track(MISCELLANEOUS.SWITCH_TIMELINE);
+            setFeedType(Type.FOLLOWING);
+            Leafwatch.track(MISCELLANEOUS.SWITCH_FOLLOWING_FEED);
           }}
         />
         <TabButton
           name={t`Highlights`}
-          icon={<SparklesIcon className="h-4 w-4" />}
-          active={feedType === 'HIGHLIGHTS'}
+          icon={<LightBulbIcon className="h-4 w-4" />}
+          active={feedType === Type.HIGHLIGHTS}
           showOnSm={false}
           onClick={() => {
-            setFeedType('HIGHLIGHTS');
-            Leafwatch.track(MISCELLANEOUS.SWITCH_HIGHLIGHTS);
+            setFeedType(Type.HIGHLIGHTS);
+            Leafwatch.track(MISCELLANEOUS.SWITCH_HIGHLIGHTS_FEED);
           }}
         />
       </div>
       <div className="flex items-center space-x-4">
         <SeeThroughLens />
-        {feedType === 'TIMELINE' && <FeedEventFilters />}
+        {feedType === Type.FOLLOWING && <FeedEventFilters />}
       </div>
     </div>
   );
