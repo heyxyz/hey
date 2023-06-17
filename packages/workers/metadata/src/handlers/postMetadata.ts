a414a5f5f877031a02d009036957f9670fe822c6
bigint <69431456+bigint@users.noreply.github.com>
2023-06-17 18:26:41 +0530

    feat: add tagger(ai) to metadata worker

--- packages/workers/metadata/src/handlers/postMetadata.ts
+++ packages/workers/metadata/src/handlers/postMetadata.ts
@@ -1,11 +1,12 @@
 import { createData, EthereumSigner } from '@lenster/bundlr';
+import type { PublicationMetadataV2Input } from '@lenster/lens';
 import type { IRequest } from 'itty-router';
 
 import type { Env } from '../types';
 
 export default async (request: IRequest, env: Env) => {
   try {
-    const payload = await request.json();
+    const payload: PublicationMetadataV2Input = await request.json();
     const signer = new EthereumSigner(env.BUNDLR_PRIVATE_KEY);
     const tx = createData(JSON.stringify(payload), signer, {
       tags: [
@@ -14,6 +15,21 @@ export default async (request: IRequest, env: Env) => {
       ]
     });
     await tx.sign(signer);
+
+    const taggerResponse = await fetch(
+      'https://api-inference.huggingface.co/models/yo/tagger',
+      {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/octet-stream',
+          Authorization: `Bearer ${env.HUGGINGFACE_API_KEY}`
+        },
+        body: JSON.stringify({ inputs: payload.content })
+      }
+    );
+
+    const taggerResponseJson = await taggerResponse.json();
+
     const bundlrRes = await fetch('http://node2.bundlr.network/tx/matic', {
       method: 'POST',
       headers: { 'content-type': 'application/octet-stream' },
@@ -21,7 +37,9 @@ export default async (request: IRequest, env: Env) => {
     });
 
     if (bundlrRes.statusText === 'Created' || bundlrRes.statusText === 'OK') {
-      return new Response(JSON.stringify({ success: true, id: tx.id }));
+      return new Response(
+        JSON.stringify({ success: true, id: tx.id, taggerResponseJson })
+      );
     } else {
       return new Response(
         JSON.stringify({ success: false, message: 'Bundlr error!', bundlrRes })
