345b0fafcf0e7ff0539c624582c5511fe440db93
Olof Andersson <olof.andersson@gmail.com>
2023-06-17 07:58:50 +0200

    feat: truncate big urls in publication (#3102)

--- apps/web/src/components/Shared/Markup/MarkupLink/index.tsx
+++ apps/web/src/components/Shared/Markup/MarkupLink/index.tsx
@@ -1,3 +1,4 @@
+import truncateUrl from '@lenster/lib/truncateUrl';
 import type { MarkupLinkProps } from 'src/types';
 
 import ExternalLink from './ExternalLink';
@@ -19,7 +20,9 @@ const MarkupLink = ({ href, title = href }: MarkupLinkProps) => {
     return <Hashtag href={href} title={title} />;
   }
 
-  return <ExternalLink href={href} title={title} />;
+  return (
+    <ExternalLink href={href} title={title ? truncateUrl(title, 30) : title} />
+  );
 };
 
 export default MarkupLink;
