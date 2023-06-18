ac878d83c6b1df8d30ea99956fe3ca5b8f18f9b2
bigint <69431456+bigint@users.noreply.github.com>
2023-06-18 11:44:21 +0530

    feat: init tags explorer (#3139)
    * feat: add tag explorer-beta
    
    * feat: add tags in staff panel

---
+++ apps/web/src/components/Home/Tags.tsx
@@ -0,0 +1,80 @@
+import TrendingTagShimmer from '@components/Shared/Shimmer/TrendingTagShimmer';
+import { TrendingUpIcon } from '@heroicons/react/solid';
+import type { TagResult } from '@lenster/lens';
+import { TagSortCriteria, useTrendingQuery } from '@lenster/lens';
+import nFormatter from '@lenster/lib/nFormatter';
+import { Card, ErrorMessage } from '@lenster/ui';
+import { Leafwatch } from '@lib/leafwatch';
+import { Plural, t, Trans } from '@lingui/macro';
+import Link from 'next/link';
+import type { FC } from 'react';
+import { MISCELLANEOUS } from 'src/tracking';
+
+const Title = () => {
+  return (
+    <div className="mb-2 flex items-center gap-2 px-5 sm:px-0">
+      <TrendingUpIcon className="h-4 w-4 text-green-500" />
+      <div>
+        <Trans>Trending</Trans>
+      </div>
+    </div>
+  );
+};
+
+const Tags: FC = () => {
+  const { data, loading, error } = useTrendingQuery({
+    variables: { request: { limit: 7, sort: TagSortCriteria.MostPopular } }
+  });
+
+  if (loading) {
+    return (
+      <>
+        <Title />
+        <Card className="mb-4 space-y-4 p-5">
+          <TrendingTagShimmer />
+          <TrendingTagShimmer />
+          <TrendingTagShimmer />
+          <TrendingTagShimmer />
+          <TrendingTagShimmer />
+          <TrendingTagShimmer />
+        </Card>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Title />
+      <Card as="aside" className="mb-4 space-y-4 p-5">
+        <ErrorMessage title={t`Failed to load trending`} error={error} />
+        {data?.allPublicationsTags?.items?.map((tag: TagResult) =>
+          tag?.tag !== '{}' ? (
+            <div key={tag?.tag}>
+              <Link
+                href={`/search?q=${tag?.tag}&type=pubs`}
+                onClick={() =>
+                  Leafwatch.track(MISCELLANEOUS.OPEN_TRENDING_TAG, {
+                    trending_tag: tag?.tag
+                  })
+                }
+              >
+                <div className="font-bold">{tag?.tag}</div>
+                <div className="lt-text-gray-500 text-[12px]">
+                  {nFormatter(tag?.total)}{' '}
+                  <Plural
+                    value={tag?.total}
+                    zero="Publication"
+                    one="Publication"
+                    other="Publications"
+                  />
+                </div>
+              </Link>
+            </div>
+          ) : null
+        )}
+      </Card>
+    </>
+  );
+};
+
+export default Tags;
