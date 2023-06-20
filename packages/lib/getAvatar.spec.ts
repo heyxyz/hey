01240cf32381311ec2c0c2343430bc9514983a3e
bigint <69431456+bigint@users.noreply.github.com>
2023-06-20 20:14:03 +0530

    feat: add vitest test for lib

---
+++ packages/lib/getAvatar.spec.ts
@@ -0,0 +1,34 @@
+import { IPFS_GATEWAY } from '@lenster/data/constants';
+import { describe, expect, test } from 'vitest';
+
+import getAvatar from './getAvatar';
+
+describe('getAvatar', () => {
+  const ipfsLink = `${IPFS_GATEWAY}bafkreianwlir2groq5l52zdnikon4rtgjcostjosaadbbfekgpzhaprmri`;
+
+  test('should return original avatar url', () => {
+    const profile = { picture: { original: { url: ipfsLink } } };
+    const result = getAvatar(profile);
+    expect(result).toBe(ipfsLink);
+  });
+
+  test.skip('should return original avatar url when hostname is in skip list', () => {
+    const avatarUrl = 'https://avatar.tobi.sh/1.png';
+    const profile = {
+      picture: { uri: avatarUrl, original: { url: avatarUrl } }
+    };
+    const result = getAvatar(profile);
+    expect(result).toBe(avatarUrl);
+  });
+
+  test("should use profile's ownedBy address to build URL when all else fails", () => {
+    const profile = {
+      ownedBy: '0x3a5bd1e37b099ae3386d13947b6a90d97675e5e3',
+      picture: {}
+    };
+    const result = getAvatar(profile);
+    expect(result).toBe(
+      `https://cdn.stamp.fyi/avatar/eth:${profile.ownedBy}?s=300`
+    );
+  });
+});
