d717d16a2cdb3115a1857d8861a55b64e549b9c4
bigint <69431456+bigint@users.noreply.github.com>
2023-06-21 00:40:21 +0530

    feat: add vitest test for lib (#3148)
    * feat: add vitest test for lib
    
    * chore: add remaning test cases in lib

---
+++ packages/lib/getAppName.spec.ts
@@ -0,0 +1,35 @@
+import { describe, expect, test } from 'vitest';
+
+import getAppName from './getAppName';
+
+describe('getAppName', () => {
+  test('should capitalize first character and replace hyphen with a space in a string', () => {
+    const inputString = 'my-app';
+    const expectedOutput = 'My app';
+    expect(getAppName(inputString)).toBe(expectedOutput);
+  });
+
+  test('should handle empty string as input', () => {
+    const inputString = '';
+    const expectedOutput = '';
+    expect(getAppName(inputString)).toBe(expectedOutput);
+  });
+
+  test('should handle single character as input', () => {
+    const inputString = 'a';
+    const expectedOutput = 'A';
+    expect(getAppName(inputString)).toBe(expectedOutput);
+  });
+
+  test('should handle input with no hyphens and only a single character', () => {
+    const inputString = 'word';
+    const expectedOutput = 'Word';
+    expect(getAppName(inputString)).toBe(expectedOutput);
+  });
+
+  test('should handle input with multiple hyphens', () => {
+    const inputString = 'one-two-three-four';
+    const expectedOutput = 'One two three four';
+    expect(getAppName(inputString)).toBe(expectedOutput);
+  });
+});
---
+++ packages/lib/getSignature.spec.ts
@@ -0,0 +1,36 @@
+import { describe, expect, test } from 'vitest';
+
+import getSignature from './getSignature';
+
+describe('getSignature', () => {
+  test('should return an object with domain, types, and value keys', () => {
+    const result = getSignature({
+      domain: {},
+      types: {
+        Post: []
+      },
+      value: {}
+    });
+
+    expect(result).toHaveProperty('domain');
+    expect(result).toHaveProperty('types');
+    expect(result).toHaveProperty('message');
+    expect(result.primaryType).toEqual('Post');
+  });
+
+  test('should remove __typename property from domain, types, and value properties', () => {
+    const input = {
+      domain: { key: 'value', __typename: 'Domain' },
+      types: { key: 'value', __typename: 'Types' },
+      value: { key: 'value', __typename: 'Value' }
+    };
+    const result = getSignature(input);
+
+    expect(result).toEqual({
+      primaryType: 'key',
+      domain: { key: 'value' },
+      types: { key: 'value' },
+      message: { key: 'value' }
+    });
+  });
+});
