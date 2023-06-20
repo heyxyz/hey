6607fe246e9fd2e33c9d601312b7dbb7692d2d77
bigint <69431456+bigint@users.noreply.github.com>
2023-06-20 20:03:51 +0530

    chore: refactor test structure (#3147)
    * feat: move web test cases to its own
    
    * feat: move prerender test cases to its own
    
    * chore: remove nightly tests
    
    * chore: temp remove global tests folder
    
    * fix: lint errors

---
+++ apps/web/playwright.config.ts
@@ -0,0 +1,19 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+import { devices } from '@playwright/test';
+import * as os from 'os';
+
+const config: PlaywrightTestConfig = {
+  testDir: './',
+  timeout: 50 * 1000,
+  expect: { timeout: 50000 },
+  fullyParallel: true,
+  maxFailures: 3,
+  forbidOnly: Boolean(process.env.CI),
+  retries: 3,
+  workers: os.cpus().length === 1 ? 1 : os.cpus().length - 1,
+  reporter: 'list',
+  use: { actionTimeout: 0, trace: 'on-first-retry' },
+  projects: [{ name: 'chromium', use: { ...devices['Desktop Chrome'] } }]
+};
+
+export default config;
