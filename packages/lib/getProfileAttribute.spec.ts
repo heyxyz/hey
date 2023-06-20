aad792a35979acff7381f2f79e2e4799c1af657e
bigint <69431456+bigint@users.noreply.github.com>
2023-06-21 00:40:21 +0530

    feat: add vitest test for lib (#3148)
    * feat: add vitest test for lib
    
    * chore: add remaning test cases in lib

---
+++ packages/lib/getProfileAttribute.spec.ts
@@ -0,0 +1,24 @@
+import type { Attribute } from '@lenster/lens';
+import { describe, expect, test } from 'vitest';
+
+import getProfileAttribute from './getProfileAttribute';
+
+describe('getProfileAttribute', () => {
+  test('should return the attribute value from a trait if key is valid', () => {
+    const attributes: Attribute[] = [
+      { key: 'hasPrideLogo', value: 'true' },
+      { key: 'app', value: 'Lenster' },
+      { key: 'twitter', value: '@mytwitter' },
+      { key: 'location', value: 'New York' },
+      { key: 'website', value: 'https://www.example.com' },
+      { key: 'statusEmoji', value: '👋' },
+      { key: 'statusMessage', value: 'Hello World!' }
+    ];
+    expect(getProfileAttribute(attributes, 'app')).toEqual('Lenster');
+  });
+
+  test('should return an empty string when attributes are undefined', () => {
+    const attributes = undefined;
+    expect(getProfileAttribute(attributes, 'app')).toEqual('');
+  });
+});
---
+++ packages/lib/getTokenImage.spec.ts
@@ -0,0 +1,15 @@
+import { STATIC_IMAGES_URL } from '@lenster/data/constants';
+import { describe, expect, test } from 'vitest';
+
+import getTokenImage from './getTokenImage';
+
+describe('getTokenImage', () => {
+  test('should return the correct image url for a given token symbol', () => {
+    expect(getTokenImage('ETH')).toEqual(`${STATIC_IMAGES_URL}/tokens/eth.svg`);
+    expect(getTokenImage('BTC')).toEqual(`${STATIC_IMAGES_URL}/tokens/btc.svg`);
+  });
+
+  test('should convert the token symbol to lowercase before generating the url', () => {
+    expect(getTokenImage('Eth')).toEqual(`${STATIC_IMAGES_URL}/tokens/eth.svg`);
+  });
+});
