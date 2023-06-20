c0da0136fa9781717cdcc81151b9477c2ff96b01
bigint <69431456+bigint@users.noreply.github.com>
2023-06-20 20:03:51 +0530

    chore: refactor test structure (#3147)
    * feat: move web test cases to its own
    
    * feat: move prerender test cases to its own
    
    * chore: remove nightly tests
    
    * chore: temp remove global tests folder
    
    * fix: lint errors

---
+++ apps/prerender/tests/constants.ts
@@ -0,0 +1,1 @@
+export const PRERENDER_BASE_URL = 'http://localhost:4784';
---
+++ apps/web/tests/home.spec.ts
@@ -0,0 +1,21 @@
+import { APP_NAME } from '@lenster/data/constants';
+import { expect, test } from '@playwright/test';
+import { WEB_BASE_URL } from 'tests/constants';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(WEB_BASE_URL);
+});
+
+test('should have page title', async ({ page }) => {
+  await expect(page).toHaveTitle(APP_NAME);
+});
+
+test('should have hero', async ({ page }) => {
+  await expect(page.getByTestId('home-hero')).toContainText(
+    'Welcome to Lenster 👋'
+  );
+});
+
+test('should have explore feed', async ({ page }) => {
+  await expect(page.getByTestId('explore-feed')).toBeVisible();
+});
