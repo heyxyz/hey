cf44a2811f76bf5411a73529ea7f0cc003f7d86e
bigint <69431456+bigint@users.noreply.github.com>
2023-06-24 11:37:34 +0530

    feat: add postMetadata test cases

---
+++ packages/workers/metadata/src/constants.ts
@@ -0,0 +1,1 @@
+export const TEST_URL = 'http://127.0.0.1:8083';
---
+++ packages/workers/metadata/src/handlers/postMetadata.spec.ts
@@ -0,0 +1,29 @@
+import { describe, expect, test } from 'vitest';
+
+import { TEST_URL } from '../constants';
+
+describe('postMetadata', () => {
+  test('should return welcome message', async () => {
+    const getRequest = await fetch(TEST_URL);
+    const response = await getRequest.text();
+
+    expect(response).toContain('gm, to metadata service 👋');
+  });
+
+  test('should return resolved address', async () => {
+    const postRequest = await fetch(TEST_URL, {
+      method: 'POST',
+      body: JSON.stringify({
+        content: 'gm, running test from lenster codebase'
+      })
+    });
+    const response: any = await postRequest.json();
+
+    expect(response.success).toBeTruthy();
+    expect(response.metadata).toEqual({
+      content: 'gm, running test from lenster codebase',
+      tags: ['science_&_technology', 'learning_&_educational'],
+      locale: 'en'
+    });
+  });
+});
