c6b20f857d87c89d706c7e55e7cd3077fb36abca
bigint <69431456+bigint@users.noreply.github.com>
2023-06-25 13:09:50 +0530

    chore: cleanup

--- apps/mobile/src/components/common/Timeline.tsx
+++ apps/mobile/src/components/common/Timeline.tsx
@@ -1,55 +1,39 @@
 import type { Publication } from '@lenster/lens';
 import {
   CustomFiltersTypes,
-  PublicationMainFocus,
   PublicationSortCriteria,
-  PublicationTypes,
   useExploreFeedQuery
 } from '@lenster/lens';
 import { FlashList } from '@shopify/flash-list';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import tailwind from 'twrnc';
 
-const TimelineCell = ({ item }: { item: Publication }) => {
-  return (
-    <View>
-      <View style={tailwind`py-1`}>
-        <Text style={tailwind`text-white font-bold`}>
-          {item.metadata.content}
-        </Text>
-      </View>
-    </View>
-  );
-};
+import SinglePublication from '../../publication/SinglePublication';
 
 const Timeline = () => {
   const request = {
     sortCriteria: PublicationSortCriteria.CuratedProfiles,
-    limit: 32,
+    limit: 50,
     noRandomize: false,
-    publicationTypes: [PublicationTypes.Post],
-    customFilters: [CustomFiltersTypes.Gardeners],
-    metadata: {
-      mainContentFocus: [PublicationMainFocus.Video]
-    }
+    customFilters: [CustomFiltersTypes.Gardeners]
   };
 
   const { data } = useExploreFeedQuery({
     variables: { request }
   });
 
-  const videos = data?.explorePublications?.items as Publication[];
+  const publications = data?.explorePublications?.items as Publication[];
 
   return (
     <View style={tailwind`mt-2 mx-3`}>
       <FlashList
         ItemSeparatorComponent={() => <View />}
         renderItem={({ item }) => {
-          return <TimelineCell item={item} />;
+          return <SinglePublication publication={item} />;
         }}
         estimatedItemSize={50}
-        data={videos}
+        data={publications}
       />
     </View>
   );
