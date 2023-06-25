67f7ca21aece2f91277191986bb5e61a1871de74
bigint <69431456+bigint@users.noreply.github.com>
2023-06-25 11:06:12 +0530

    feat: add test cases to lens package (#3168)

---
+++ packages/lens/apollo/lib/keyFields.spec.ts
@@ -0,0 +1,23 @@
+import { publicationKeyFields } from '@lenster/lens/apollo/lib';
+import { describe, expect, test } from 'vitest';
+
+describe('keyFields', () => {
+  describe('publicationKeyFields', () => {
+    const publication = {
+      __typename: 'Post',
+      id: '123',
+      createdAt: '2022-01-01'
+    };
+
+    test('should generate the expected publication key fields string', () => {
+      const expectedResult = 'Post:{"id":"123","createdAt":"2022-01-01"}';
+      expect(publicationKeyFields(publication)).toEqual(expectedResult);
+    });
+
+    test('should return a non-empty string', () => {
+      const result = publicationKeyFields(publication);
+      expect(result).toBeDefined();
+      expect(result.length).toBeGreaterThan(0);
+    });
+  });
+});
