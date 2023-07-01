0dfe45605905974f5aa4db746ecc42bc2402a52f
bigint <bigint@lenster.xyz>
2023-07-01 08:41:22 +0530

    chore: update lf

--- packages/workers/leafwatch/src/handlers/ingest.ts
+++ packages/workers/leafwatch/src/handlers/ingest.ts
@@ -52,7 +52,7 @@ export default async (request: IRequest, env: Env) => {
   const user_agent = request.headers.get('user-agent');
 
   try {
-    await fetch(env.CLICKHOUSE_REST_ENDPOINT, {
+    const response = await fetch(env.CLICKHOUSE_REST_ENDPOINT, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: `
@@ -67,20 +67,26 @@ export default async (request: IRequest, env: Env) => {
           user_agent
         ) VALUES (
           '${name}',
-          ${actor ? `'${actor}'` : `NULL`},
-          ${properties ? `'${properties}'` : `NULL`},
-          ${fingerprint ? `'${fingerprint}'` : `NULL`},
-          ${country ? `'${country}'` : `NULL`},
-          ${referrer ? `'${referrer}'` : `NULL`},
-          ${platform ? `'${platform}'` : `NULL`},
-          ${user_agent ? `'${user_agent}'` : `NULL`}
+          ${actor ? `'${actor}'` : null},
+          ${properties ? `'${JSON.stringify(properties)}'` : null},
+          ${fingerprint ? `'${fingerprint}'` : null},
+          ${country ? `'${country}'` : null},
+          ${referrer ? `'${referrer}'` : null},
+          ${platform ? `'${platform}'` : null},
+          ${user_agent ? `'${user_agent}'` : null}
         )
       `
     });
 
+    if (response.status !== 200) {
+      return new Response(
+        JSON.stringify({ success: false, error: 'Status code is not 200!' })
+      );
+    }
+
     return new Response(JSON.stringify({ success: true }));
   } catch (error) {
-    console.error('Failed to create space', error);
+    console.error('Failed to ingest', error);
     return new Response(
       JSON.stringify({ success: false, error: 'Something went wrong!' })
     );
