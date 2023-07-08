cca8e7705ebb7cb8d1afc4f10007535c3c369a40
Kiranism <iamkiran2831@gmail.com>
2023-07-08 15:20:40 +0530

    fixed ENS names are not loaded in DMs when we are in All tab #3031

--- apps/web/src/components/Messages/Preview.tsx
+++ apps/web/src/components/Messages/Preview.tsx
@@ -82,10 +82,8 @@ const Preview: FC<PreviewProps> = ({
             <div className="flex justify-between space-x-1">
               <div className="flex items-center gap-1 overflow-hidden">
                 <div className="text-md truncate">
-                  {profile?.name
-                    ? sanitizeDisplayName(profile?.name) ??
-                      formatHandle(profile.handle)
-                    : ensName ?? formatAddress(conversationKey?.split('/')[0])}
+                  {sanitizeDisplayName(profile?.name) ??
+                    formatHandle(profile?.handle)}
                 </div>
                 {isVerified(profile?.id) && (
                   <BadgeCheckIcon className="text-brand h-4 w-4 min-w-fit" />
