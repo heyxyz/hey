06522aac9f0d63772c482f6543111cefa8ff7ed2
bigint <69431456+bigint@users.noreply.github.com>
2023-07-18 10:30:53 +0530

    feat: add scam badge

---
+++ packages/lib/isScam.ts
@@ -0,0 +1,11 @@
+import { scam } from '@lenster/data/scam';
+
+/**
+ * Checks whether a profile is scam or not.
+ *
+ * @param id The profile id to check.
+ * @returns True if the profile is scam, false otherwise.
+ */
+const isScam = (id: string): boolean => scam.includes(id);
+
+export default isScam;
