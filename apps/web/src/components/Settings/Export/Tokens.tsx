d4330de57c5828a81d3b268349fed9c86c730141
bigint <bigint@lenster.xyz>
2023-07-20 19:50:19 +0530

    feat: add tokens in export settings (#3272)
    feat: add tokens

---
+++ apps/web/src/components/Settings/Export/Tokens.tsx
@@ -0,0 +1,31 @@
+import { Card } from '@lenster/ui';
+import { Trans } from '@lingui/macro';
+import type { FC } from 'react';
+
+const Tokens: FC = () => {
+  const accessToken = localStorage.getItem('accessToken');
+  const refreshToken = localStorage.getItem('refreshToken');
+
+  return (
+    <>
+      <Card className="space-y-2 p-5">
+        <div className="pb-1 text-lg font-bold">
+          <Trans>Your temporary access token</Trans>
+        </div>
+        <div className="break-words rounded-md bg-gray-300 px-1.5 py-0.5 text-sm font-bold dark:bg-gray-600">
+          {accessToken}
+        </div>
+      </Card>
+      <Card className="space-y-2 p-5">
+        <div className="pb-1 text-lg font-bold">
+          <Trans>Your temporary refresh token</Trans>
+        </div>
+        <div className="break-words rounded-md bg-gray-300 px-1.5 py-0.5 text-sm font-bold dark:bg-gray-600">
+          {refreshToken}
+        </div>
+      </Card>
+    </>
+  );
+};
+
+export default Tokens;
