854a83e34a0ea4f51244f18dae01b1f202c0963c
bigint <69431456+bigint@users.noreply.github.com>
2023-07-20 18:38:21 +0530

    feat: add communities/:slug endpoint

--- packages/workers/communities/src/index.ts
+++ packages/workers/communities/src/index.ts
@@ -1,6 +1,7 @@
 import { createCors, error, json, Router } from 'itty-router';
 
 import createCommunity from './handlers/createCommunity';
+import getCommunity from './handlers/getCommunity';
 import type { Env } from './types';
 
 const { preflight, corsify } = createCors({
@@ -13,6 +14,9 @@ const router = Router();
 router.all('*', preflight);
 router.get('/', () => new Response('gm, to communities service 👋'));
 router.post('/create', createCommunity);
+router.get('/communities/:slug', ({ params }, env) =>
+  getCommunity(params.slug, env)
+);
 
 const routerHandleStack = (request: Request, env: Env, ctx: ExecutionContext) =>
   router.handle(request, env, ctx).then(json);
