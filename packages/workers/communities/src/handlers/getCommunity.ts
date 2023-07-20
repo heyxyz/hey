f1df25d96b221104c9c1131af2d59e33f2e082cc
bigint <69431456+bigint@users.noreply.github.com>
2023-07-20 20:46:06 +0530

    feat: add members count

--- packages/workers/communities/src/handlers/getCommunity.ts
+++ packages/workers/communities/src/handlers/getCommunity.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import { error } from 'itty-router';
 
-import { COMMUNITIES_TABLE } from '../constants';
+import { COMMUNITIES_TABLE, MEMBERSHIPS_TABLE } from '../constants';
 import type { Env } from '../types';
 
 export default async (slug: string, env: Env) => {
@@ -12,17 +12,27 @@ export default async (slug: string, env: Env) => {
   try {
     const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_KEY);
 
-    const { data, error } = await supabase
+    const { data: community, error: communityError } = await supabase
       .from(COMMUNITIES_TABLE)
       .select('*')
       .eq('slug', slug)
       .single();
 
-    if (error) {
+    const { count, error: membershipsError } = await supabase
+      .from(MEMBERSHIPS_TABLE)
+      .select('*', { count: 'exact', head: true })
+      .eq('community_id', community.id);
+
+    if (communityError || membershipsError) {
       throw error;
     }
 
-    return new Response(JSON.stringify(data));
+    const mergedData = {
+      ...community,
+      members_count: count
+    };
+
+    return new Response(JSON.stringify(mergedData));
   } catch (error) {
     console.error('Failed to create metadata data', error);
     return new Response(
