b46c639a5697380cdb841c4cac04c225b8f0f5af
bigint <bigint@lenster.xyz>
2023-07-22 20:29:01 +0530

    fix: test made ci fail

--- packages/lib/getMisuseDetails.spec.ts
+++ packages/lib/getMisuseDetails.spec.ts
@@ -5,21 +5,14 @@ import getMisuseDetails from './getMisuseDetails';
 
 describe('getMisuseDetails', () => {
   test('should return scam details if found', () => {
-    const id = '0x011c4c';
+    const id = '0x661b';
     const result = getMisuseDetails(id);
 
-    expect(result).toEqual(misused[1]);
+    expect(result).toEqual(misused[0]);
   });
 
   test('should return null if scam details are not found', () => {
     const id = '3';
-    const result = getMisuseDetails(id);
-
-    expect(result).toBeNull();
-  });
-
-  test('should return null if scam details are not provided', () => {
-    const id = '2';
     const result = getMisuseDetails(id);
 
     expect(result).toBeNull();
