664543cc35e2457935b69394a0ea376da0c4b799
bigint <69431456+bigint@users.noreply.github.com>
2023-07-22 14:55:22 +0530

    feat: add mvp members list

--- apps/web/src/components/Community/Members/List.tsx
+++ apps/web/src/components/Community/Members/List.tsx
@@ -1,12 +1,16 @@
 import Loader from '@components/Shared/Loader';
+import UserProfile from '@components/Shared/UserProfile';
 import { UsersIcon } from '@heroicons/react/outline';
 import { COMMUNITIES_WORKER_URL } from '@lenster/data/constants';
+import { FollowUnfollowSource } from '@lenster/data/tracking';
+import type { Profile } from '@lenster/lens';
+import { useProfilesLazyQuery } from '@lenster/lens';
 import type { Community } from '@lenster/types/communities';
 import { EmptyState, ErrorMessage } from '@lenster/ui';
 import { t, Trans } from '@lingui/macro';
-import { useInfiniteQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import type { FC } from 'react';
+import { type FC, useState } from 'react';
 import { Virtuoso } from 'react-virtuoso';
 
 interface MembersListProps {
@@ -14,29 +18,40 @@ interface MembersListProps {
 }
 
 const MembersList: FC<MembersListProps> = ({ community }) => {
-  const fetchMembers = async (offset: number) => {
+  const [offset, setOffset] = useState(0);
+  const [fetchProfiles] = useProfilesLazyQuery();
+
+  const fetchMembers = async () => {
     try {
       const response = await axios(
         `${COMMUNITIES_WORKER_URL}/members/${community.id}/${offset}`
       );
 
-      return response.data.map(
+      const profileIds = response.data.map(
         (obj: { id: string; profile_id: string }) => obj.profile_id
       );
+
+      const profiles = await fetchProfiles({
+        variables: { request: { profileIds } }
+      });
+
+      return profiles.data?.profiles.items;
     } catch (error) {
       return [];
     }
   };
 
-  const { data, isFetching, error } = useInfiniteQuery({
-    queryFn: ({ pageParam = 0 }) => fetchMembers(pageParam)
-  });
+  const {
+    data: members,
+    isLoading,
+    error
+  } = useQuery(['communityMembers', community.id, offset], () =>
+    fetchMembers().then((res) => res)
+  );
 
-  if (isFetching) {
+  if (isLoading) {
     return <Loader message={t`Loading members`} />;
   }
-
-  const members = data?.pages;
 
   if (members?.length === 0) {
     return (
@@ -65,27 +80,43 @@ const MembersList: FC<MembersListProps> = ({ community }) => {
       <Virtuoso
         className="virtual-profile-list"
         data={members}
-        itemContent={(index, member) => {
+        itemContent={(index, profile) => {
           return (
-            <div
-              className="cursor-pointer p-5 hover:bg-gray-100 dark:hover:bg-gray-900"
-              aria-hidden="true"
-            >
-              {/* <UserProfile
-                profile={following?.profile as Profile}
-                linkToProfile={!onProfileSelected}
-                isFollowing={following?.profile?.isFollowedByMe}
+            <div className="p-5">
+              <UserProfile
+                profile={profile as Profile}
+                isFollowing={profile?.isFollowedByMe}
                 followUnfollowPosition={index + 1}
-                followUnfollowSource={FollowUnfollowSource.FOLLOWING_MODAL}
+                followUnfollowSource={
+                  FollowUnfollowSource.COMMUNITY_MEMBERS_MODAL
+                }
                 showBio
                 showFollow
                 showUserPreview={false}
-              /> */}
-              {JSON.stringify(member)}
+              />
             </div>
           );
         }}
       />
+      {/* {members?.pages.map((page: any) => (
+        <>
+          {page.map((profile: Profile, index: number) => (
+            <div key={profile.id}>
+              <UserProfile
+                profile={profile as Profile}
+                isFollowing={profile?.isFollowedByMe}
+                followUnfollowPosition={index + 1}
+                followUnfollowSource={
+                  FollowUnfollowSource.COMMUNITY_MEMBERS_MODAL
+                }
+                showBio
+                showFollow
+                showUserPreview={false}
+              />
+            </div>
+          ))}
+        </>
+      ))} */}
     </div>
   );
 };
