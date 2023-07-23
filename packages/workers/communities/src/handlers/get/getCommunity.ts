70e6645e15975d61801f92d5c832358d90939e1d
bigint <bigint@lenster.xyz>
2023-07-23 12:03:36 +0530

    feat: add rules to get community (#3343)
    * feat: add rules to get community
    
    * feat: add rules on ui

--- packages/workers/communities/src/handlers/get/getCommunity.ts
+++ packages/workers/communities/src/handlers/get/getCommunity.ts
@@ -16,20 +16,21 @@ export default async (identifier: string, type: 'id' | 'slug', env: Env) => {
       text: `
         SELECT
           c.*,
-          COALESCE(m.members_count, 0)::integer AS members_count
-        FROM
-          communities AS c
+          COALESCE(m.members_count, 0)::integer AS members_count,
+          (
+            SELECT json_agg(json_build_object('id', r.id, 'title', r.title, 'description', r.description))
+            FROM rules AS r
+            WHERE r.community_id = c.id
+          ) AS rules
+        FROM communities AS c
         LEFT JOIN (
           SELECT
             community_id,
             COUNT(profile_id) AS members_count
-          FROM
-            memberships
-          GROUP BY
-            community_id
+          FROM memberships
+          GROUP BY community_id
         ) AS m ON c.id = m.community_id
-        WHERE
-          ${type === 'id' ? 'c.id' : 'c.slug'} = $1;
+        WHERE ${type === 'id' ? 'c.id' : 'c.slug'} = $1;
       `,
       values: [identifier]
     };
