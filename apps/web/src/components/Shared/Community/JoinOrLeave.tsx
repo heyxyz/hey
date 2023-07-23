ba54743e3d4ffc8b2171c51faa22bbdbdf985790
bigint <69431456+bigint@users.noreply.github.com>
2023-07-23 09:34:06 +0530

    feat: add get community by slug and id

--- apps/web/src/components/Shared/Community/JoinOrLeave.tsx
+++ apps/web/src/components/Shared/Community/JoinOrLeave.tsx
@@ -28,7 +28,7 @@ const JoinOrLeave: FC<JoinOrLeaveProps> = ({ community }) => {
   const fetchIsMember = async () => {
     try {
       const response = await axios(
-        `${COMMUNITIES_WORKER_URL}/isMember/${community.id}/${currentProfile?.id}`
+        `${COMMUNITIES_WORKER_URL}/getIsMember/${community.id}/${currentProfile?.id}`
       );
 
       setIsMember(response.data?.isMember);
