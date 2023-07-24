ec627bdda9ef422ec0dc31f1604b9807461567a7
spantheslayer <supriyosarkar282@gmail.com>
2023-07-25 01:48:29 +0530

    chore: refactored the code

--- packages/data/verify.js
+++ packages/data/verify.js
@@ -26,8 +26,8 @@ async function main() {
 
   let cursor = null;
   while (true) {
-    const fetch = await import('node-fetch');
-    const response = await fetch.default(url, {
+    // const fetch = await import('node-fetch');
+    const response = await fetch(url, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -56,120 +56,91 @@ async function main() {
   }
 
   if (allResponses.length) {
-    // Store all the response data in 'resp.json'
     const fs = require('fs');
-    fs.writeFileSync('resp.json', JSON.stringify(allResponses, null, 2));
+    try {
+      const jsonData = JSON.stringify(allResponses, null, 2);
+      const parsedJson = JSON.parse(jsonData);
 
-    // Read the contents of the 'resp.json' file
-    fs.readFile('./resp.json', 'utf8', (err, data) => {
-      if (err) {
-        console.error('Error reading the resp.json file:', err);
-        return;
-      }
+      // Extract the id and name properties from parsedJson
+      const profiles = parsedJson.map((entry) => {
+        const { id } = entry.profile;
+        const { name } = entry.profile;
+        return `'${id}', // ${name}`;
+      });
 
-      try {
-        const jsonData = JSON.parse(data);
+      // Load the verified.ts file
+      fs.readFile('./verified.ts', 'utf8', (err, tsData) => {
+        if (err) {
+          console.error('Error reading the verified.ts file:', err);
+          return;
+        }
 
-        // Extract the id and name properties from resp.json
-        const profiles = jsonData.map((entry) => {
-          const { id, name } = entry.profile;
-          return `'${id}', // ${name}`;
-        });
+        const regex =
+          /export const mainnetVerified = \[[\S\s]*?mainnetLensTeamMembers,[\S\s]*?...mainnetStaffs\n];/;
+        const match = tsData.match(regex);
+        // console.log('match:', match);
 
-        // Load the verified.ts file
-        fs.readFile('./verified.ts', 'utf8', (err, tsData) => {
-          if (err) {
-            console.error('Error reading the verified.ts file:', err);
-            return;
-          }
+        if (match) {
+          // Extract existing profiles from verified.ts
+          const existingProfiles = new Set(
+            match[0]
+              .split('\n')
+              .map((line) => line.trim())
+              .slice(1, -3)
+          );
 
-          const regex =
-            /export const mainnetVerified = \[[\S\s]*?...mainnetLensTeamMembers,[\S\s]*?...mainnetStaffs\n];/;
-          const match = tsData.match(regex);
+          // console.log('existingProfiles:', existingProfiles);
 
-          if (match) {
-            // Extract existing profiles from verified.ts
-            const existingProfiles = new Set(
-              match[0]
-                .split('\n')
-                .map((line) => line.trim())
-                .slice(1, -3)
+          const regex = /'0x([\dA-Fa-f]+)'/g;
+
+          // Existing IDs
+          const existingIDs = Array.from(existingProfiles)
+            .join('\n')
+            .match(regex)
+            .map((idString) => idString.slice(1, -1));
+
+          // Getting unique IDs by comparing existing IDs with new IDs
+          const profileIDs = profiles.map((entry) =>
+            entry.split("',")[0].trim().slice(1)
+          );
+
+          const profileIDsSet = new Set(profileIDs);
+          const existingIDsSet = new Set(existingIDs);
+
+          const uniqueIDs = new Set(
+            [...profileIDsSet].filter((id) => !existingIDsSet.has(id))
+          );
+          const uniqueIDsArray = Array.from(uniqueIDs);
+
+          const updatedContent = tsData.replace(match[0], (match) => {
+            const mainnetVerifiedArray = match.split('\n');
+            mainnetVerifiedArray.splice(
+              -3,
+              0,
+              ...uniqueIDsArray.map((line) => `  '${line}',`)
             );
+            return mainnetVerifiedArray.join('\n');
+          });
 
-            const regex = /'0x([\dA-Fa-f]+)'/g;
-
-            // Existing IDs
-            const existingIDs = Array.from(existingProfiles)
-              .join('\n')
-              .match(regex)
-              .map((idString) => idString.slice(1, -1));
-
-            // Profile IDs
-            const profileIDs = profiles.map((entry) =>
-              entry.split("',")[0].trim().slice(1)
-            );
-
-            const profileIDsSet = new Set(profileIDs);
-            const existingIDsSet = new Set(existingIDs);
-
-            const uniqueIDs = new Set(
-              [...profileIDsSet].filter((id) => !existingIDsSet.has(id))
-            );
-
-            const uniqueNames = [];
-            for (const entry of jsonData) {
-              const { id, name } = entry.profile;
-              if (uniqueIDs.has(id)) {
-                uniqueNames.push(name);
-              }
+          // updating verified.ts file
+          fs.writeFile('./verified.ts', updatedContent, (err) => {
+            if (err) {
+              console.error('Error writing to the verified.ts file:', err);
+              return;
             }
-
-            const uniqueProfilesFormatted = [];
-            const uniqueIDsArray = Array.from(uniqueIDs);
-
-            for (const [i, id] of uniqueIDsArray.entries()) {
-              const name = uniqueNames[i];
-              uniqueProfilesFormatted.push(`'${id}', // ${name}`);
-            }
-
-            const updatedContent = tsData.replace(match[0], (match) => {
-              const mainnetVerifiedArray = match.split('\n');
-              mainnetVerifiedArray.splice(
-                -3,
-                0,
-                ...uniqueProfilesFormatted.map((line) => `  ${line}`)
-              );
-              return mainnetVerifiedArray.join('\n');
-            });
-
-            // updating verified.ts file
-            fs.writeFile('./verified.ts', updatedContent, (err) => {
-              if (err) {
-                console.error('Error writing to the verified.ts file:', err);
-                return;
-              }
-              console.log('verified.ts updated successfully!');
-            });
-          } else {
-            console.error(
-              'mainnetVerified array not found in the verified.ts file.'
-            );
-          }
-        });
-      } catch (error) {
-        console.error('Error parsing resp.json:', error);
-      }
-    });
-
-    fs.unlink('./resp.json', (err) => {
-      if (err) {
-        console.error('Error deleting the resp.json file:', err);
-        return;
-      }
-    });
+            console.log('verified.ts updated successfully!');
+          });
+        } else {
+          console.error(
+            'mainnetVerified array not found in the verified.ts file.'
+          );
+        }
+      });
+    } catch (error) {
+      console.error('Error:', error);
+    }
 
     // console.log(`Total number of IDs: ${allResponses.length}`);
-    // console.log("All response data stored in 'resp.json' file.");
   } else {
     console.log('Error occurred while making the GraphQL requests.');
   }
