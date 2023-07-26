42ccd0ee42f3fc471788847712b7aa6f1ad5efb0
bigint <69431456+bigint@users.noreply.github.com>
2023-07-26 15:03:05 +0530

    chore: use getRpc in web constants

--- apps/web/src/constants.ts
+++ apps/web/src/constants.ts
@@ -1,15 +1,16 @@
 import { IS_MAINNET } from '@lenster/data/constants';
+import getRpc from '@lenster/lib/getRpc';
 import { polygon, polygonMumbai } from 'wagmi/chains';
 
 // Web3
 export const POLYGON_MAINNET = {
   ...polygon,
   name: 'Polygon Mainnet',
-  rpcUrls: { default: 'https://polygon-rpc.com' }
+  rpcUrls: { default: getRpc(137) }
 };
 export const POLYGON_MUMBAI = {
   ...polygonMumbai,
   name: 'Polygon Mumbai',
-  rpcUrls: { default: 'https://rpc-mumbai.maticvigil.com' }
+  rpcUrls: { default: getRpc(80001) }
 };
 export const CHAIN_ID = IS_MAINNET ? POLYGON_MAINNET.id : POLYGON_MUMBAI.id;
