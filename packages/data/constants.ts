78c921b5f42ad863e92ade81ada13728b9e2da42
bigint <69431456+bigint@users.noreply.github.com>
2023-07-26 12:56:14 +0530

    chore: use pinata gateway

--- packages/data/constants.ts
+++ packages/data/constants.ts
@@ -66,7 +66,7 @@ export const POLYGONSCAN_URL = IS_MAINNET
 export const RARIBLE_URL = IS_MAINNET
   ? 'https://rarible.com'
   : 'https://testnet.rarible.com';
-export const IPFS_GATEWAY = 'https://ipfs.lenster.xyz/ipfs/';
+export const IPFS_GATEWAY = 'https://gateway.pinata.cloud/ipfs/';
 export const ARWEAVE_GATEWAY = 'https://arweave.net/';
 export const EVER_API = 'https://endpoint.4everland.co';
 export const DEFAULT_OG = `${STATIC_IMAGES_URL}/og/logo.jpeg`;
