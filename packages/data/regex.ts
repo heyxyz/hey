9f3a82e62b53d4fe24829088cd521807e0606d7f
Olof Andersson <olof.andersson@gmail.com>
2023-07-29 13:06:58 +0200

    fix: change mention regex so that @mention can not end with "." (#3389)
    fix mention regex

--- packages/data/regex.ts
+++ packages/data/regex.ts
@@ -2,7 +2,7 @@ const RESTRICTED_SYMBOLS = '☑️✓✔✅';
 
 export const Regex = {
   url: /(http|https):\/\/([\w+.?])+([\w!#$%&'()*+,./:;=?@\\^~\-]*)?/g,
-  mention: /(@[.A-Za-z\d-_]{1,31})/g,
+  mention: /(@[\w.\-]{1,30}[\w-])/g,
   hashtag: /(#\w*[A-Za-z]\w*)/g,
   ethereumAddress: /^(0x)?[\da-f]{40}$/i,
   handle: /^[\da-z]+$/g,
