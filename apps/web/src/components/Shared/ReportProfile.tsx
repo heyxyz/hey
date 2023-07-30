7b2887eeb6caf2977ed7286ba29b9668b249b744
spantheslayer <supriyosarkar282@gmail.com>
2023-07-30 12:12:47 +0530

    chore: added textarea
    Signed-off-by: spantheslayer <supriyosarkar282@gmail.com>

--- apps/web/src/components/Shared/ReportProfile.tsx
+++ apps/web/src/components/Shared/ReportProfile.tsx
@@ -1,48 +1,62 @@
-import { Button, Radio, TextArea } from '@lenster/ui';
+import { Button, Form, Radio, TextArea, useZodForm } from '@lenster/ui';
 import { t } from '@lingui/macro';
 import { type FC, useState } from 'react';
-import { useAppStore } from 'src/store/app';
+import { object, string } from 'zod';
+
+const reportProfileSchema = object({
+  report: string()
+    .min(10, { message: t`Report should not be empty` })
+    .max(300, { message: t`Report should not exceed 300 characters` })
+});
 
 const Report: FC = () => {
-  const currentProfile = useAppStore((state) => state.currentProfile);
   const [isRadioSelected, setIsRadioSelected] = useState(false);
+
+  const form = useZodForm({
+    schema: reportProfileSchema
+  });
 
   return (
     <div className="flex flex-col space-y-2 p-3">
-      <div className="space-y-2">
-        <Radio
-          title={t`Misleading Account`}
-          value={t`Impersonation or false claims about identity or affiliation`}
-          name="reportReason"
-          onChange={() => setIsRadioSelected(true)}
-        />
-        <Radio
-          title={t`Frequently Posts Unwanted Content`}
-          value={t`Spam; excessive mentions or replies`}
-          name="reportReason"
-          onChange={() => setIsRadioSelected(true)}
-        />
-      </div>
+      <Form
+        form={form}
+        onSubmit={async ({}) => {
+          alert('report');
+        }}
+      >
+        <div className="space-y-2">
+          <Radio
+            title={t`Misleading Account`}
+            value={t`Impersonation or false claims about identity or affiliation`}
+            name="reportReason"
+            onChange={() => setIsRadioSelected(true)}
+          />
+          <Radio
+            title={t`Frequently Posts Unwanted Content`}
+            value={t`Spam; excessive mentions or replies`}
+            name="reportReason"
+            onChange={() => setIsRadioSelected(true)}
+          />
+        </div>
 
-      <div className="flex flex-col p-2">
-        <TextArea
-          label={t`Add details to report`}
-          placeholder={t`Enter a reason or any other details here...`}
-        />
-      </div>
-      <div className="flex justify-center">
-        <Button
-          type="submit"
-          variant="primary"
-          className="w-full"
-          onClick={() => {
-            alert('gm 🚀' + currentProfile?.id);
-          }}
-          disabled={!isRadioSelected}
-        >
-          Submit
-        </Button>
-      </div>
+        <div className="flex flex-col p-2">
+          <TextArea
+            label={t`Add details to report`}
+            placeholder={t`Enter a reason or any other details here...`}
+            {...form.register('report')}
+          />
+        </div>
+        <div className="flex justify-center">
+          <Button
+            type="submit"
+            variant="primary"
+            className="w-full"
+            disabled={!isRadioSelected}
+          >
+            Submit
+          </Button>
+        </div>
+      </Form>
     </div>
   );
 };
