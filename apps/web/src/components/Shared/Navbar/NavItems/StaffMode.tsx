7aa02de2172df94d671ff9c63dd855ecd7e13651
bigint <bigint@lenster.xyz>
2023-08-03 12:29:46 +0530

    feat: add mod store and fix lenster store usage (#3426)

--- apps/web/src/components/Shared/Navbar/NavItems/StaffMode.tsx
+++ apps/web/src/components/Shared/Navbar/NavItems/StaffMode.tsx
@@ -8,7 +8,7 @@ import { Leafwatch } from '@lib/leafwatch';
 import { Trans } from '@lingui/macro';
 import clsx from 'clsx';
 import type { FC } from 'react';
-import { useAppPersistStore } from 'src/store/app';
+import { useModePersistStore } from 'src/store/mode';
 
 interface StaffModeProps {
   className?: string;
@@ -16,7 +16,7 @@ interface StaffModeProps {
 
 const StaffMode: FC<StaffModeProps> = ({ className = '' }) => {
   const { allowed: staffMode } = useStaffMode();
-  const setStaffMode = useAppPersistStore((state) => state.setStaffMode);
+  const setStaffMode = useModePersistStore((state) => state.setStaffMode);
 
   const toggleStaffMode = () => {
     setStaffMode(!staffMode);
