458c87618492c0348126a01ff516617d8c14bdcf
bigint <bigint@lenster.xyz>
2023-08-04 19:54:48 +0530

    feat: add cookiesOption (#3441)
    * feat: add cookiesOption
    
    * style: Format code with prettier
    
    * fix: sad migrate back to LC
    
    ---------
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

--- apps/web/src/lib/getIsAuthTokensAvailable.ts
+++ apps/web/src/lib/getIsAuthTokensAvailable.ts
@@ -1,14 +1,13 @@
-import { Cookie } from '@lenster/data/storage';
-import Cookies from 'js-cookie';
+import { Localstorage } from '@lenster/data/storage';
 
 /**
- * Checks if the access token and refresh token are available in cookies.
+ * Checks if the access token and refresh token are available in localstorage
  *
  * @returns True if the access token and refresh token are available, `false` otherwise
  */
 const getIsAuthTokensAvailable = () => {
-  const accessToken = Cookies.get(Cookie.AccessToken);
-  const refreshToken = Cookies.get(Cookie.RefreshToken);
+  const accessToken = localStorage.getItem(Localstorage.AccessToken);
+  const refreshToken = localStorage.getItem(Localstorage.RefreshToken);
 
   return accessToken !== 'undefined' && refreshToken !== 'undefined';
 };
