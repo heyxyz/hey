0fe1ce147169725585b000768a90be5fe01386bf
bigint <bigint@lenster.xyz>
2023-08-04 20:54:12 +0530

    feat: add more regex test cases (#3442)
    * feat: add more regex test cases
    
    * feat: add more regex test cases

--- packages/data/regex.ts
+++ packages/data/regex.ts
@@ -2,11 +2,10 @@ const RESTRICTED_SYMBOLS = '☑️✓✔✅';
 
 export const Regex = {
   url: /(http|https):\/\/([\w+.?])+([\w!#$%&'()*+,./:;=?@\\^~\-]*)?/g,
-  mention: /(@[\w.\-]{1,30}[\w-])/g,
+  mention: /@[\w.\-]{1,30}[\w-]/g,
   hashtag: /(#\w*[A-Za-z]\w*)/g,
   ethereumAddress: /^(0x)?[\da-f]{40}$/i,
   handle: /^[\da-z]+$/g,
-  allHandles: /([\s+])@(\S+)/g,
   santiizeHandle: /[^\d .A-Za-z]/g,
   profileNameValidator: new RegExp('^[^' + RESTRICTED_SYMBOLS + ']+$'),
   profileNameFilter: new RegExp('[' + RESTRICTED_SYMBOLS + ']', 'gu'),
