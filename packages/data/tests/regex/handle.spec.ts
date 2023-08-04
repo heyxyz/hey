da25d6cfdaec2e52beb3e808c757a9137b3c2179
bigint <69431456+bigint@users.noreply.github.com>
2023-08-04 20:40:36 +0530

    feat: add more regex test cases

--- packages/data/tests/regex/handle.spec.ts
+++ packages/data/tests/regex/handle.spec.ts
@@ -3,37 +3,32 @@ import { describe, expect, test } from 'vitest';
 import { Regex } from '../../regex';
 
 const validate = (text: string) => {
-  Regex.mention.lastIndex = 0;
-  return Regex.mention.test(text);
+  Regex.handle.lastIndex = 0;
+  return Regex.handle.test(text);
 };
 
 describe('handle regex', () => {
-  test('should pass for string starting with @ followed by 1-30 characters', () => {
-    expect(validate('@john_doe-123')).toBe(true);
-    expect(validate('@example.email-service')).toBe(true);
-    expect(validate('@a.b-c')).toBe(true);
-    expect(validate('@username')).toBe(true);
-    expect(validate('@abc123')).toBe(true);
+  test('should pass for valid handles', () => {
+    expect(validate('example')).toBe(true);
+    expect(validate('handle123')).toBe(true);
+    expect(validate('username')).toBe(true);
+    expect(validate('abc123')).toBe(true);
   });
 
-  test.skip('should fail for string with uppercase letters after @', () => {
-    expect(validate('@Abc123')).toBe(false);
+  test('should fail for underscore', () => {
+    expect(validate('john_doe_123')).toBe(false);
   });
 
-  test.skip('should fail for string with spaces after @', () => {
-    expect(validate('@abc 123')).toBe(false);
+  test('should fail for uppercase letters', () => {
+    expect(validate('Abc123')).toBe(false);
+    expect(validate('Handle')).toBe(false);
+    expect(validate('TestUser')).toBe(false);
   });
 
-  test.skip('should fail for string with special characters after @', () => {
-    expect(validate('@abc!def')).toBe(false);
-  });
-
-  test('should fail for string that does not end with a word character or a hyphen', () => {
-    expect(validate('@.....')).toBe(false);
-  });
-
-  test.skip('should fail for string longer than 30 characters after @', () => {
-    expect(validate('@abcdefghij1234567890abcdefghij12')).toBe(false);
+  test('should fail for special characters', () => {
+    expect(validate('handle@user')).toBe(false);
+    expect(validate('user*name')).toBe(false);
+    expect(validate('name#123')).toBe(false);
   });
 
   test('should fail for empty string', () => {
