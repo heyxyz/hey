198ac8b0f2d288ab45e04e4202a506a5f20cc001
bigint <69431456+bigint@users.noreply.github.com>
2023-08-05 16:28:38 +0530

    fix: algo names

--- packages/data/algorithms.ts
+++ packages/data/algorithms.ts
@@ -9,28 +9,28 @@ export const algorithms: {
   by: string;
 }[] = [
   {
-    name: 'K3L Recommended',
+    name: 'Recommended',
     feedType: HomeFeedType.K3L_RECOMMENDED,
     description: 'New and interesting content powered by AI + EigenTrust.',
     image: `${STATIC_IMAGES_URL}/algorithms/k3l.png`,
     by: 'Karma3Labs'
   },
   {
-    name: 'K3L Popular',
+    name: 'Popular',
     feedType: HomeFeedType.K3L_POPULAR,
     description: 'Posts sorted by top ranked profiles engagement.',
     image: `${STATIC_IMAGES_URL}/algorithms/k3l.png`,
     by: 'Karma3Labs'
   },
   {
-    name: 'K3L Recent',
+    name: 'Recent',
     feedType: HomeFeedType.K3L_RECENT,
     description: 'Recent posts sorted by time of posting.',
     image: `${STATIC_IMAGES_URL}/algorithms/k3l.png`,
     by: 'Karma3Labs'
   },
   {
-    name: 'K3L Crowdsourced',
+    name: 'Crowdsourced',
     feedType: HomeFeedType.K3L_CROWDSOURCED,
     description:
       'Posts that garnered much interest interactions weighted by the reputation of the interacting parties.',
