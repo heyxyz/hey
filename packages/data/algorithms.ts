204bbe01c9d66af726ce33b89f41f81ed6d53e25
bigint <69431456+bigint@users.noreply.github.com>
2023-08-05 16:15:31 +0530

    chore: update k3l image

--- packages/data/algorithms.ts
+++ packages/data/algorithms.ts
@@ -1,3 +1,4 @@
+import { STATIC_IMAGES_URL } from './constants';
 import { HomeFeedType } from './enums';
 
 export const algorithms: {
@@ -11,28 +12,28 @@ export const algorithms: {
     name: 'K3L Recommended',
     feedType: HomeFeedType.K3L_RECOMMENDED,
     description: 'Recommended for you based on your interests',
-    image: 'https://picsum.photos/300/300',
+    image: `${STATIC_IMAGES_URL}/algorithms/k3l.png`,
     by: 'Karma3Labs'
   },
   {
     name: 'K3L Popular',
     feedType: HomeFeedType.K3L_POPULAR,
     description: 'Popular in your network',
-    image: 'https://picsum.photos/300/300',
+    image: `${STATIC_IMAGES_URL}/algorithms/k3l.png`,
     by: 'Karma3Labs'
   },
   {
     name: 'K3L Recent',
     feedType: HomeFeedType.K3L_RECENT,
     description: 'Recent in your network',
-    image: 'https://picsum.photos/300/300',
+    image: `${STATIC_IMAGES_URL}/algorithms/k3l.png`,
     by: 'Karma3Labs'
   },
   {
     name: 'K3L Crowdsourced',
     feedType: HomeFeedType.K3L_CROWDSOURCED,
     description: 'Crowdsourced by your network',
-    image: 'https://picsum.photos/300/300',
+    image: `${STATIC_IMAGES_URL}/algorithms/k3l.png`,
     by: 'Karma3Labs'
   }
 ];
