6534aaa6ca392cb6d6a6de0f1d80ccf460aab371
bigint <bigint@lenster.xyz>
2023-08-07 12:36:22 +0530

    feat: add lenster most viewed algorithm

---
+++ packages/workers/algorithms/src/helpers/providers/lenster/algorithms/lensterMostViewed.ts
@@ -0,0 +1,40 @@
+import type { Env } from '../../../../types';
+import clickhouseQuery from '../clickhouseQuery';
+
+const lensterMostViewed = async (
+  limit: string,
+  offset: string,
+  env: Env
+): Promise<any[]> => {
+  try {
+    const query = `
+      SELECT
+          url,
+          COUNT(*) AS view_count
+      FROM
+          events
+      WHERE
+          name = 'Pageview'
+          AND url LIKE 'https://lenster.xyz/posts/%'
+          AND created >= now() - INTERVAL 1 DAY
+      GROUP BY
+          url
+      ORDER BY
+          view_count DESC
+      LIMIT ${limit}
+      OFFSET ${offset};
+    `;
+    const response = await clickhouseQuery(query, env);
+
+    return response.map((row) => {
+      const url = row[0];
+      const id = url.split('/').pop();
+      return id;
+    });
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+
+export default lensterMostViewed;
