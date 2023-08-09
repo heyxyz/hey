23e96ba8410cdc4f42b0bbdf28973e29c2c74900
bhavya2611 <bhavya.y.mehta@gmail.com>
2023-08-09 17:13:07 +0530

    Feat: Emoji Picker Updated

--- apps/web/src/components/Shared/EmojiPicker/index.tsx
+++ apps/web/src/components/Shared/EmojiPicker/index.tsx
@@ -1,28 +1,36 @@
 import { Menu } from '@headlessui/react';
 import { EmojiHappyIcon } from '@heroicons/react/outline';
+import EmojiPicker, { Theme } from 'emoji-picker-react';
+import { useTheme } from 'next-themes';
 import type { FC } from 'react';
 
 import MenuTransition from '../MenuTransition';
-import List from './List';
 
-interface EmojiPickerProps {
+interface CustomEmojiPickerProps {
   emoji?: string | null;
   setEmoji: (emoji: string) => void;
 }
 
-const EmojiPicker: FC<EmojiPickerProps> = ({ emoji, setEmoji }) => {
+const CustomEmojiPicker: FC<CustomEmojiPickerProps> = ({ emoji, setEmoji }) => {
+  const { resolvedTheme } = useTheme();
+
   return (
     <Menu as="div" className="relative">
       <Menu.Button className="rounded-md p-1 hover:bg-gray-300/20">
         {emoji ? <span>{emoji}</span> : <EmojiHappyIcon className="h-5 w-5" />}
       </Menu.Button>
       <MenuTransition>
-        <div className="fixed z-[5] mt-1 w-2/4 rounded-xl border bg-white shadow-sm focus:outline-none dark:border-gray-700 dark:bg-gray-900">
-          <List setEmoji={setEmoji} />
+        <div className="fixed z-[5] mt-1 rounded-xl border shadow-sm focus:outline-none dark:border-gray-700 dark:bg-gray-900">
+          <EmojiPicker
+            onEmojiClick={(emojiData) => setEmoji(emojiData.emoji)}
+            height={320}
+            lazyLoadEmojis={true}
+            theme={resolvedTheme === 'dark' ? Theme['DARK'] : Theme['LIGHT']}
+          />
         </div>
       </MenuTransition>
     </Menu>
   );
 };
 
-export default EmojiPicker;
+export default CustomEmojiPicker;
