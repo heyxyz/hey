179efa83007694cfa05e9fe783b2a6a5970d3bc1
bigint <69431456+bigint@users.noreply.github.com>
2023-08-09 11:45:57 +0530

    feat: add getAlgorithmicFeed lib

---
+++ apps/web/src/lib/getAlgorithmicFeed.ts
@@ -0,0 +1,30 @@
+import { HomeFeedType } from '@lenster/data/enums';
+import getPublicationIds from '@lenster/lib/getPublicationIds';
+
+/**
+ * Get the algorithmic feed for a given feed type
+ * @param feedType The type of feed to get
+ * @returns The algorithmic feed
+ */
+const getAlgorithmicFeed = async (feedType: HomeFeedType) => {
+  switch (feedType) {
+    case HomeFeedType.K3L_RECOMMENDED:
+    case HomeFeedType.K3L_POPULAR:
+    case HomeFeedType.K3L_RECENT:
+    case HomeFeedType.K3L_CROWDSOURCED:
+      return getPublicationIds(
+        'k3l',
+        feedType.replace('K3L_', '').toLowerCase()
+      ).then((data) => data);
+    case HomeFeedType.LENSTER_MOSTVIEWED:
+    case HomeFeedType.LENSTER_MOSTINTERACTED:
+      return getPublicationIds(
+        'lenster',
+        feedType.replace('LENSTER_', '').toLowerCase()
+      ).then((data) => data);
+    default:
+      return [];
+  }
+};
+
+export default getAlgorithmicFeed;
