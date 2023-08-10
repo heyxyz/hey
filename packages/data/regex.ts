19c554c2b10446b2f0d49a0a5f6f830ebf669574
bhavya2611 <bhavya.y.mehta@gmail.com>
2023-08-10 18:24:41 +0530

    regex for url updated

--- packages/data/regex.ts
+++ packages/data/regex.ts
@@ -1,7 +1,7 @@
 const RESTRICTED_SYMBOLS = '☑️✓✔✅';
 
 export const Regex = {
-  url: /[\w#%+.:=@~-]{1,256}\.[\d()a-z]{1,6}\b([\w#%&()+./:=?@~-]*)?/gi,
+  url: /[\w#%+.:=@~-]{1,256}\.[\d()a-z]{2,6}\b([\w#%&()+./:=?@~-]*)?/gi,
   mention: /@[\w.\-]{1,30}[\w-]/g,
   hashtag: /(#\w*[A-Za-z]\w*)/g,
   ethereumAddress: /^(0x)?[\da-f]{40}$/i,
