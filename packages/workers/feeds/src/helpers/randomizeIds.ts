58b2b0e516765ffce7374c6e7603374d4d674cfe
bigint <bigint@lenster.xyz>
2023-08-10 20:02:01 +0530

    chore: update docs

--- packages/workers/feeds/src/helpers/randomizeIds.ts
+++ packages/workers/feeds/src/helpers/randomizeIds.ts
@@ -1,3 +1,8 @@
+/**
+ * Randomize the order of the ids
+ * @param ids The ids to randomize
+ * @returns The randomized ids
+ */
 const randomizeIds = (ids: string[]) => {
   return ids.sort(() => Math.random() - Math.random());
 };
