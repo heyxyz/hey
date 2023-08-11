62731893948b05c2ed6a2f8da78d8fa6d0f287f5
bigint <bigint@lenster.xyz>
2023-08-11 20:45:12 +0530

    feat: add following algorithm (#3508)
    * feat: add following algorithm
    
    * style: Format code with prettier
    
    * fix: prettier warn
    
    ---------
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

--- apps/web/src/lib/getAlgorithmicFeed.ts
+++ apps/web/src/lib/getAlgorithmicFeed.ts
@@ -1,12 +1,17 @@
 import { AlgorithmProvider, HomeFeedType } from '@lenster/data/enums';
+import type { Profile } from '@lenster/lens';
 import getPublicationIds from '@lenster/lib/getPublicationIds';
 
 /**
  * Get the algorithmic feed for a given feed type
  * @param feedType The type of feed to get
+ * @param profile The lens profile to get the feed for
  * @returns The algorithmic feed
  */
-const getAlgorithmicFeed = async (feedType: HomeFeedType) => {
+const getAlgorithmicFeed = async (
+  feedType: HomeFeedType,
+  profile: Profile | null
+) => {
   switch (feedType) {
     case HomeFeedType.K3L_RECOMMENDED:
     case HomeFeedType.K3L_POPULAR:
@@ -15,6 +20,12 @@ const getAlgorithmicFeed = async (feedType: HomeFeedType) => {
       return getPublicationIds(
         AlgorithmProvider.K3L,
         feedType.replace('K3L_', '').toLowerCase()
+      ).then((data) => data);
+    case HomeFeedType.K3L_FOLLOWING:
+      return getPublicationIds(
+        AlgorithmProvider.K3L,
+        feedType.replace('K3L_', '').toLowerCase(),
+        profile?.handle
       ).then((data) => data);
     case HomeFeedType.LENSTER_MOSTVIEWED:
     case HomeFeedType.LENSTER_MOSTINTERACTED:
