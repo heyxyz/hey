666a055f4a0bead7033f60afd147d89e8191e411
bigint <69431456+bigint@users.noreply.github.com>
2023-08-11 12:41:22 +0530

    feat: add sql

--- packages/workers/sns/src/handlers/firehose.ts
+++ packages/workers/sns/src/handlers/firehose.ts
@@ -1,7 +1,21 @@
 import { Errors } from '@lenster/data/errors';
-import { type IRequest } from 'itty-router';
+import sanitizeDStorageUrl from '@lenster/lib/sanitizeDStorageUrl';
+import type { IRequest } from 'itty-router';
 
-export default async (request: IRequest) => {
+import type { Env } from '../types';
+
+type ExtensionRequest = {
+  profileId?: string;
+  serverPubId?: string;
+  contentURI?: string;
+  collectModule?: string;
+  collectModuleReturnData?: string;
+  referenceModule?: string;
+  referenceModuleReturnData?: string;
+  timestamp?: string;
+};
+
+export default async (request: IRequest, env: Env) => {
   const body = await request.json();
   if (!body) {
     return new Response(
@@ -9,8 +23,60 @@ export default async (request: IRequest) => {
     );
   }
 
+  const {
+    profileId,
+    serverPubId,
+    contentURI,
+    collectModule,
+    collectModuleReturnData,
+    referenceModule,
+    referenceModuleReturnData,
+    timestamp
+  } = body as ExtensionRequest;
+
   try {
-    console.log('sending to firehose', JSON.stringify(body));
+    // Extract metadata
+    let metadata;
+    try {
+      const contentURIResponse = await fetch(sanitizeDStorageUrl(contentURI));
+      metadata = await contentURIResponse.json();
+    } catch {}
+
+    const response = await fetch(env.CLICKHOUSE_REST_ENDPOINT, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: `
+        INSERT INTO firehose (
+          profileId,
+          pubId,
+          contentURI,
+          metadata,
+          collectModule,
+          collectModuleReturnData,
+          referenceModule,
+          referenceModuleReturnData,
+          timestamp
+        ) VALUES (
+          ${profileId ? `'${profileId}'` : null},
+          ${serverPubId ? `'${serverPubId}'` : null},
+          ${contentURI ? `'${contentURI}'` : null},
+          ${metadata ? `'${JSON.stringify(metadata)}'` : null},
+          ${collectModule ? `'${collectModule}'` : null},
+          ${collectModuleReturnData ? `'${collectModuleReturnData}'` : null},
+          ${referenceModule ? `'${referenceModule}'` : null},
+          ${
+            referenceModuleReturnData ? `'${referenceModuleReturnData}'` : null
+          },
+          ${timestamp ? `'${timestamp}'` : null}
+        )
+      `
+    });
+
+    if (response.status !== 200) {
+      return new Response(
+        JSON.stringify({ success: false, error: Errors.StatusCodeIsNot200 })
+      );
+    }
 
     return new Response(JSON.stringify({ success: true }));
   } catch (error) {
