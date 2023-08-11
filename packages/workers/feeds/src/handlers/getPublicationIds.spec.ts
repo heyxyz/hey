ee939e085c7c3ec64afce52a5ff62db32054a0b7
bigint <bigint@lenster.xyz>
2023-08-11 11:05:55 +0530

    feat: add test cases for algo worker (#3500)

---
+++ packages/workers/feeds/src/handlers/getPublicationIds.spec.ts
@@ -0,0 +1,43 @@
+import { describe, expect, test } from 'vitest';
+
+import { TEST_URL } from '../constants';
+
+type Response = {
+  ids: string[];
+};
+
+describe('getPublicationIds', () => {
+  describe('k3l provider', () => {
+    test('should return ids for k3l provider and recent strategy', async () => {
+      const getRequest = await fetch(
+        `${TEST_URL}/ids?provider=k3l&strategy=recent`
+      );
+      const response: Response = await getRequest.json();
+      expect(response.ids.length).toBe(50);
+    });
+
+    test('should return ids for k3l provider and recommended strategy', async () => {
+      const getRequest = await fetch(
+        `${TEST_URL}/ids?provider=k3l&strategy=recommended`
+      );
+      const response: Response = await getRequest.json();
+      expect(response.ids.length).toBe(50);
+    });
+
+    test('should return ids for k3l provider and popular strategy', async () => {
+      const getRequest = await fetch(
+        `${TEST_URL}/ids?provider=k3l&strategy=popular`
+      );
+      const response: Response = await getRequest.json();
+      expect(response.ids.length).toBe(50);
+    });
+
+    test('should return ids for k3l provider and crowdsourced strategy', async () => {
+      const getRequest = await fetch(
+        `${TEST_URL}/ids?provider=k3l&strategy=crowdsourced`
+      );
+      const response: Response = await getRequest.json();
+      expect(response.ids.length).toBe(50);
+    });
+  });
+});
