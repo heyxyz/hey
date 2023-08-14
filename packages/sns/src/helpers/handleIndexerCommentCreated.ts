af6b5e965145fe50ce5067aa2e614938e94bd526
bigint <69431456+bigint@users.noreply.github.com>
2023-08-14 23:39:03 +0530

    feat: add handleIndexerMirrorCreated and handleIndexerCommentCreated

--- packages/sns/src/helpers/handleIndexerCommentCreated.ts
+++ packages/sns/src/helpers/handleIndexerCommentCreated.ts
@@ -1,30 +1,31 @@
 import createClickHouseClient from './createClickHouseClient';
 
 interface ReactionData {
-  serverPubId: string;
-  reaction: string;
+  serverPubIdPointer: string;
 }
 
-const handleReaction = async (data: ReactionData) => {
-  const { serverPubId, reaction } = data;
+const handleIndexerCommentCreated = async (data: ReactionData) => {
+  const { serverPubIdPointer } = data;
   const clickhouse = createClickHouseClient();
 
   // Check if the publication exists before updating
   const checkQuery = `
-    SELECT id FROM firehose WHERE id = '${serverPubId}' LIMIT 1;
+    SELECT id FROM firehose WHERE id = '${serverPubIdPointer}' LIMIT 1;
   `;
   const existsResult = await clickhouse.query(checkQuery).toPromise();
 
   if (existsResult.length > 0) {
     const updateQuery = `
       ALTER TABLE firehose
-      UPDATE likesCount = likesCount ${reaction === 'UPVOTE' ? '+' : '-'} 1
-      WHERE id = '${serverPubId}';
+      UPDATE commentsCount = commentsCount + 1
+      WHERE id = '${serverPubIdPointer}';
     `;
     await clickhouse.query(updateQuery).toPromise();
   } else {
-    console.log(`Publication with serverPubId ${serverPubId} not found.`);
+    console.log(
+      `Publication with serverPubIdPointer ${serverPubIdPointer} not found.`
+    );
   }
 };
 
-export default handleReaction;
+export default handleIndexerCommentCreated;
