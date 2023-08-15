38d56ea533b20c706a59847dd8b6e582e0373de1
bigint <69431456+bigint@users.noreply.github.com>
2023-08-15 13:04:37 +0530

    feat: add access worker

---
+++ packages/workers/access/src/index.ts
@@ -0,0 +1,35 @@
+import { createCors, error, json, Router } from 'itty-router';
+
+import access from './handlers/access';
+import type { Env } from './types';
+
+const { preflight, corsify } = createCors({
+  origins: ['*'],
+  methods: ['HEAD', 'GET', 'POST']
+});
+
+const router = Router();
+
+router.all('*', preflight);
+router.get('/', () => new Response('gm, to access service 👋'));
+router.get('/access/:id', ({ params }, env) => access(params.id, env));
+
+const routerHandleStack = (request: Request, env: Env, ctx: ExecutionContext) =>
+  router.handle(request, env, ctx).then(json);
+
+const handleFetch = async (
+  request: Request,
+  env: Env,
+  ctx: ExecutionContext
+) => {
+  try {
+    return await routerHandleStack(request, env, ctx);
+  } catch {
+    return error(500);
+  }
+};
+
+export default {
+  fetch: (request: Request, env: Env, context: ExecutionContext) =>
+    handleFetch(request, env, context).then(corsify)
+};
