0f16731a9e3d7d5af4e18957eec837660ec561de
Bhavya Mehta <bhavya.y.mehta@gmail.com>
2023-08-17 10:02:55 +0530

    feat: nft avatar modal added (#3536)
    * feat: nft avatar modal added
    
    * nft avatar modal loading state changes
    
    * used nft gallery instead of nft feed component
    
    * addressed review comments
    
    * addressed review comments
    
    ---------
    
    Co-authored-by: bigint <bigint@lenster.xyz>

--- apps/web/src/components/Shared/Shimmer/NftShimmer.tsx
+++ apps/web/src/components/Shared/Shimmer/NftShimmer.tsx
@@ -4,7 +4,7 @@ import type { FC } from 'react';
 const NftShimmer: FC = () => {
   return (
     <Card>
-      <div className="shimmer h-52 space-y-4 sm:h-80 md:rounded-t-[10px]" />
+      <div className="shimmer h-52 space-y-4 sm:h-80 md:h-40 md:rounded-t-[10px]" />
       <div className="space-y-2 border-t p-5 dark:border-gray-700">
         <div className="shimmer h-3 w-7/12 rounded-lg" />
         <div className="shimmer h-3 w-1/3 rounded-lg" />
