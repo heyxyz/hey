39375c1837dc6e298d7180d3906ad55483cfc3fc
bhavya2611 <bhavya.y.mehta@gmail.com>
2023-08-17 10:46:24 +0530

    fix: nft shimmer

--- apps/web/src/components/Shared/Shimmer/NftPickerShimmer.tsx
+++ apps/web/src/components/Shared/Shimmer/NftPickerShimmer.tsx
@@ -2,7 +2,9 @@ import NftShimmer from './NftShimmer';
 
 const NftPickerShimmer = () => {
   return (
-    <div className="m-4 grid grid-cols-1 gap-4 sm:grid-cols-3">
+    <div className="m-4 grid w-[100%] grid-cols-1 gap-4 sm:grid-cols-3 md:h-40 md:grid-cols-4">
+      <NftShimmer />
+      <NftShimmer />
       <NftShimmer />
       <NftShimmer />
       <NftShimmer />
