498aeb3606d16fe00e590cf469a56958e7b4df24
bigint <69431456+bigint@users.noreply.github.com>
2023-08-18 10:49:55 +0530

    chore: use state instead of disclosure in navbar

--- apps/web/src/components/Shared/Navbar/index.tsx
+++ apps/web/src/components/Shared/Navbar/index.tsx
@@ -1,6 +1,5 @@
 import MessageIcon from '@components/Messages/MessageIcon';
 import NotificationIcon from '@components/Notification/NotificationIcon';
-import { Disclosure } from '@headlessui/react';
 import { SearchIcon, XIcon } from '@heroicons/react/outline';
 import type { Profile } from '@lenster/lens';
 import formatHandle from '@lenster/lib/formatHandle';
@@ -9,7 +8,7 @@ import { t } from '@lingui/macro';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import type { FC } from 'react';
+import { type FC, useState } from 'react';
 import { useAccessStore } from 'src/store/access';
 import { useAppStore } from 'src/store/app';
 
@@ -22,6 +21,7 @@ const Navbar: FC = () => {
   const router = useRouter();
   const currentProfile = useAppStore((state) => state.currentProfile);
   const staffMode = useAccessStore((state) => state.staffMode);
+  const [showSearch, setShowSearch] = useState(false);
 
   const onProfileSelected = (profile: Profile) => {
     router.push(`/u/${formatHandle(profile?.handle)}`);
@@ -70,84 +70,76 @@ const Navbar: FC = () => {
   };
 
   return (
-    <Disclosure
-      as="header"
-      className="divider sticky top-0 z-10 w-full bg-white dark:bg-black"
-    >
-      {({ open }) => (
-        <>
-          {staffMode && <StaffBar />}
-          <div className="container mx-auto max-w-screen-xl px-5">
-            <div className="relative flex h-14 items-center justify-between sm:h-16">
-              <div className="flex items-center justify-start">
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md text-gray-500 focus:outline-none md:hidden">
-                  {open ? (
-                    <XIcon className="h-6 w-6" />
-                  ) : (
-                    <SearchIcon className="h-6 w-6" />
-                  )}
-                </Disclosure.Button>
-                <Link href="/" className="hidden md:block">
-                  <img
-                    className="h-8 w-8"
-                    height={32}
-                    width={32}
-                    src={
-                      currentProfile && hasPrideLogo(currentProfile)
-                        ? '/pride.svg'
-                        : '/logo.svg'
-                    }
-                    alt="Logo"
-                  />
-                </Link>
-                <div className="hidden sm:ml-6 md:block">
-                  <div className="flex items-center space-x-4">
-                    <div className="hidden md:block">
-                      <Search onProfileSelected={onProfileSelected} />
-                    </div>
-                    <NavItems />
-                  </div>
+    <header className="divider sticky top-0 z-10 w-full bg-white dark:bg-black">
+      {staffMode && <StaffBar />}
+      <div className="container mx-auto max-w-screen-xl px-5">
+        <div className="relative flex h-14 items-center justify-between sm:h-16">
+          <div className="flex items-center justify-start">
+            <button
+              className="inline-flex items-center justify-center rounded-md text-gray-500 focus:outline-none md:hidden"
+              onClick={() => setShowSearch(!showSearch)}
+            >
+              {showSearch ? (
+                <XIcon className="h-6 w-6" />
+              ) : (
+                <SearchIcon className="h-6 w-6" />
+              )}
+            </button>
+            <Link href="/" className="hidden md:block">
+              <img
+                className="h-8 w-8"
+                height={32}
+                width={32}
+                src={
+                  currentProfile && hasPrideLogo(currentProfile)
+                    ? '/pride.svg'
+                    : '/logo.svg'
+                }
+                alt="Logo"
+              />
+            </Link>
+            <div className="hidden sm:ml-6 md:block">
+              <div className="flex items-center space-x-4">
+                <div className="hidden md:block">
+                  <Search onProfileSelected={onProfileSelected} />
                 </div>
-              </div>
-              <Link
-                href="/"
-                className={clsx(
-                  'md:hidden',
-                  !currentProfile?.id && 'ml-[60px]'
-                )}
-              >
-                <img
-                  className="h-7 w-7"
-                  height={32}
-                  width={32}
-                  src={
-                    currentProfile && hasPrideLogo(currentProfile)
-                      ? '/pride.svg'
-                      : '/logo.svg'
-                  }
-                  alt="Logo"
-                />
-              </Link>
-              <div className="flex items-center gap-4">
-                {currentProfile ? (
-                  <>
-                    <MessageIcon />
-                    <NotificationIcon />
-                  </>
-                ) : null}
-                <MenuItems />
+                <NavItems />
               </div>
             </div>
           </div>
-
-          <Disclosure.Panel className="md:hidden">
-            <div className="m-3">
-              <Search hideDropdown onProfileSelected={onProfileSelected} />
-            </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
+          <Link
+            href="/"
+            className={clsx('md:hidden', !currentProfile?.id && 'ml-[60px]')}
+          >
+            <img
+              className="h-7 w-7"
+              height={32}
+              width={32}
+              src={
+                currentProfile && hasPrideLogo(currentProfile)
+                  ? '/pride.svg'
+                  : '/logo.svg'
+              }
+              alt="Logo"
+            />
+          </Link>
+          <div className="flex items-center gap-4">
+            {currentProfile ? (
+              <>
+                <MessageIcon />
+                <NotificationIcon />
+              </>
+            ) : null}
+            <MenuItems />
+          </div>
+        </div>
+      </div>
+      {showSearch ? (
+        <div className="m-3 md:hidden">
+          <Search hideDropdown onProfileSelected={onProfileSelected} />
+        </div>
+      ) : null}
+    </header>
   );
 };
 
