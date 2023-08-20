296ac341c48db7c9977d40483f5ab92a91bb8a7e
bigint <bigint@lenster.xyz>
2023-08-20 22:06:19 +0530

    fix: bookmark cache (#3591)
    * fix: bookmark cache
    
    * fix: bookmark cache

--- apps/web/src/components/Publication/Actions/Menu/Bookmark.tsx
+++ apps/web/src/components/Publication/Actions/Menu/Bookmark.tsx
@@ -38,7 +38,7 @@ const Bookmark: FC<BookmarkProps> = ({ publication }) => {
     publicationId: publication.id
   };
 
-  const updateCache = (cache: ApolloCache<any>, bookmarked: boolean) => {
+  const updateCache = (cache: ApolloCache<any>) => {
     const bookmarkedPublications = isMirror
       ? publication?.mirrorOf
       : publication;
@@ -46,7 +46,7 @@ const Bookmark: FC<BookmarkProps> = ({ publication }) => {
     cache.modify({
       id: publicationKeyFields(bookmarkedPublications),
       fields: {
-        bookmarked: () => bookmarked,
+        bookmarked: (bookmarked) => !bookmarked,
         stats: (stats) => ({
           ...stats,
           totalBookmarks: bookmarked
@@ -77,7 +77,7 @@ const Bookmark: FC<BookmarkProps> = ({ publication }) => {
           bookmarked: true
         });
       },
-      update: (cache) => updateCache(cache, true)
+      update: (cache) => updateCache(cache)
     });
 
   const [removePublicationProfileBookmark] =
@@ -91,7 +91,7 @@ const Bookmark: FC<BookmarkProps> = ({ publication }) => {
           bookmarked: false
         });
       },
-      update: (cache) => updateCache(cache, false)
+      update: (cache) => updateCache(cache)
     });
 
   const togglePublicationProfileBookmark = async () => {
