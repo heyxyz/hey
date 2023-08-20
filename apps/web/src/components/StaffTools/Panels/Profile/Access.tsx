57748f9fdd72968568bae4ebda468840da375d99
bigint <69431456+bigint@users.noreply.github.com>
2023-08-20 18:56:52 +0530

    feat: add updateByAdmin param

--- apps/web/src/components/StaffTools/Panels/Profile/Access.tsx
+++ apps/web/src/components/StaffTools/Panels/Profile/Access.tsx
@@ -76,6 +76,7 @@ const Access: FC<RankProps> = ({ profile }) => {
         ...(type === Type.TUSTED_MEMBER && {
           isTrustedMember: !isTrustedMember
         }),
+        updateByAdmin: true,
         accessToken: localStorage.getItem(Localstorage.AccessToken)
       }),
       {
