0159a0cd076cda009322eb6f1c201557ad5f195d
Olof Andersson <olof.andersson@gmail.com>
2023-08-22 05:41:35 +0200

    fix: url regex, use same regex for getURLs as for url detection (#3599)

--- packages/data/regex.ts
+++ packages/data/regex.ts
@@ -1,7 +1,8 @@
 const RESTRICTED_SYMBOLS = '☑️✓✔✅';
 
 export const Regex = {
-  url: /(http|https):\/\/([\w+.?])+([\w!#$%&'()*+,./:;=?@\\^~\-]*)?/g,
+  // modified version of https://stackoverflow.com/a/6041965/961254 to support unicode international characters
+  url: /\b(http|https):\/\/([\p{L}\p{N}_-]+(?:(?:\.[\p{L}\p{N}_-]+)+))([\p{L}\p{N}_.,@?^=%&:\/~+#-]*[\p{L}\p{N}_@?^=%&\/~+#-])/gu,
   mention: /@[\w.\-]{1,30}[\w-]/g,
   hashtag: /(#\w*[A-Za-z]\w*)/g,
   ethereumAddress: /^(0x)?[\da-f]{40}$/i,
