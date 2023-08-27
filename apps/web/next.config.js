cf2c067f12794420643a56f5e692d5c0833576bc
bigint <bigint@lenster.xyz>
2023-08-27 16:44:51 +0530

    chore: update dependencies 📦 (#3624)
    * chore: update dependencies 📦
    
    * fix: Referrer-Policy

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -60,7 +60,7 @@ const nextConfig = {
         headers: [
           { key: 'X-Content-Type-Options', value: 'nosniff' },
           { key: 'X-XSS-Protection', value: '1; mode=block' },
-          { key: 'Referrer-Policy', value: 'strict' }
+          { key: 'Referrer-Policy', value: 'strict-origin' }
         ]
       },
       { source: '/about', headers },
