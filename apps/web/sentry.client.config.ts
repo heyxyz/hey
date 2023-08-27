91e0440a9c1fcd267dd721f1f61dbb71a74c826e
bigint <69431456+bigint@users.noreply.github.com>
2023-08-27 20:25:51 +0530

    revert: disable autoSessionTracking in sentry

--- apps/web/sentry.client.config.ts
+++ apps/web/sentry.client.config.ts
@@ -3,6 +3,5 @@ import * as Sentry from '@sentry/nextjs';
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   tracesSampleRate: 1,
-  debug: false,
-  autoSessionTracking: false
+  debug: false
 });
