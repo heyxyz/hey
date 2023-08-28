168878ab4699bdccf63969eb6f8229e463231a40
bigint <69431456+bigint@users.noreply.github.com>
2023-08-28 20:35:54 +0530

    feat: add sentry to metadata worker

--- packages/workers/metadata/src/types.ts
+++ packages/workers/metadata/src/types.ts
@@ -1,4 +1,16 @@
+import type { IRequestStrict } from 'itty-router';
+import type { Toucan } from 'toucan-js';
+
 export interface Env {
+  SENTRY_DSN: string;
+  RELEASE: string;
   BUNDLR_PRIVATE_KEY: string;
   HUGGINGFACE_API_KEY: string;
 }
+
+export type WorkerRequest = {
+  req: Request;
+  env: Env;
+  ctx: ExecutionContext;
+  sentry?: Toucan;
+} & IRequestStrict;
