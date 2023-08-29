71a5e1fd104fecdefe1c5d61217dc4c4666405c0
bigint <bigint@lenster.xyz>
2023-08-29 15:48:51 +0530

    feat: add sentry to achievements worker (#3640)
    * feat: add sentry to achievements worker
    
    * feat: add RELEASE var

--- packages/workers/achievements/src/handlers/hasUsedLenster.ts
+++ packages/workers/achievements/src/handlers/hasUsedLenster.ts
@@ -1,16 +1,27 @@
+import '@sentry/tracing';
+
 import { Errors } from '@lenster/data/errors';
 import response from '@lenster/lib/response';
 
-import type { Env } from '../types';
+import type { WorkerRequest } from '../types';
 
-export default async (id: string, env: Env) => {
+export default async (request: WorkerRequest) => {
+  const transaction = request.sentry?.startTransaction({
+    name: '@lenster/achievements/hasUsedLenster'
+  });
+
+  const { id } = request.params;
+
   if (!id) {
     return response({ success: false, error: Errors.NoBody });
   }
 
   try {
+    const clickhouseRequestSpan = transaction?.startChild({
+      name: 'clickhouse-request'
+    });
     const clickhouseResponse = await fetch(
-      `${env.CLICKHOUSE_REST_ENDPOINT}&default_format=JSONCompact`,
+      `${request.env.CLICKHOUSE_REST_ENDPOINT}&default_format=JSONCompact`,
       {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -18,6 +29,7 @@ export default async (id: string, env: Env) => {
         body: `SELECT count(*) FROM events WHERE actor = '${id}';`
       }
     );
+    clickhouseRequestSpan?.finish();
 
     if (clickhouseResponse.status !== 200) {
       return response({ success: false, error: Errors.StatusCodeIsNot200 });
@@ -32,6 +44,9 @@ export default async (id: string, env: Env) => {
       hasUsedLenster: parseInt(json.data[0][0]) > 0
     });
   } catch (error) {
+    request.sentry?.captureException(error);
     throw error;
+  } finally {
+    transaction?.finish();
   }
 };
