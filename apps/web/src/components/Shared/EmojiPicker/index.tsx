eeb8e8a6997a5f8987089b19903c31bb1b7a26a8
bhavya2611 <bhavya.y.mehta@gmail.com>
2023-08-29 19:43:32 +0530

    added emoji picker for the new publication modal and comments

--- apps/web/src/components/Shared/EmojiPicker/index.tsx
+++ apps/web/src/components/Shared/EmojiPicker/index.tsx
@@ -3,7 +3,8 @@ import stopEventPropagation from '@lenster/lib/stopEventPropagation';
 import { Tooltip } from '@lenster/ui';
 import { t } from '@lingui/macro';
 import clsx from 'clsx';
-import type { Dispatch, FC, SetStateAction } from 'react';
+import { type Dispatch, type FC, type SetStateAction, useRef } from 'react';
+import { useOnClickOutside } from 'usehooks-ts';
 
 import List from './List';
 
@@ -22,31 +23,34 @@ const EmojiPicker: FC<EmojiPickerProps> = ({
   setShowEmojiPicker,
   emojiClassName
 }) => {
+  const listRef = useRef(null);
+
+  useOnClickOutside(listRef, () => setShowEmojiPicker(false));
+
   return (
-    <Tooltip placement="top" content={t`Emoji`}>
-      <div className="relative">
-        <div
-          onClick={(e) => {
-            debugger;
-            e.preventDefault();
-            stopEventPropagation(e);
-            setShowEmojiPicker(!showEmojiPicker);
-          }}
-          className="cursor-pointer"
-        >
-          {emoji ? (
-            <span>{emoji}</span>
-          ) : (
+    <div ref={listRef} className="relative">
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          stopEventPropagation(e);
+          setShowEmojiPicker(!showEmojiPicker);
+        }}
+        className="cursor-pointer"
+      >
+        {emoji ? (
+          <span>{emoji}</span>
+        ) : (
+          <Tooltip placement="top" content={t`Emoji`}>
             <EmojiHappyIcon className={clsx('h-5 w-5', emojiClassName)} />
-          )}
+          </Tooltip>
+        )}
+      </button>
+      {showEmojiPicker ? (
+        <div className="absolute z-[5] mt-1 w-2/4 w-[300px] rounded-xl border bg-white shadow-sm focus:outline-none dark:border-gray-700 dark:bg-gray-900">
+          <List setEmoji={setEmoji} />
         </div>
-        {showEmojiPicker ? (
-          <div className="fixed z-[5] mt-1 w-2/4 w-[300px] rounded-xl border bg-white shadow-sm focus:outline-none dark:border-gray-700 dark:bg-gray-900">
-            <List setEmoji={setEmoji} />
-          </div>
-        ) : null}
-      </div>
-    </Tooltip>
+      ) : null}
+    </div>
   );
 };
 
