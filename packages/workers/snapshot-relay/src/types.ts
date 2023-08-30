d27231038021ef0677001fed2689e27d4e20a11b
bigint <69431456+bigint@users.noreply.github.com>
2023-08-30 19:54:48 +0530

    feat: add sentry to snapshot relay worker (#3656)
    * feat: add sentry to snapshot relay worker
    
    * fix: name

--- packages/workers/snapshot-relay/src/types.ts
+++ packages/workers/snapshot-relay/src/types.ts
@@ -1,4 +1,16 @@
+import type { IRequestStrict } from 'itty-router';
+import type { Toucan } from 'toucan-js';
+
 export interface Env {
+  SENTRY_DSN: string;
+  RELEASE: string;
   MAINNET_PROPOSAL_CREATOR_PRIVATE_KEY: string;
   TESTNET_PROPOSAL_CREATOR_PRIVATE_KEY: string;
 }
+
+export type WorkerRequest = {
+  req: Request;
+  env: Env;
+  ctx: ExecutionContext;
+  sentry?: Toucan;
+} & IRequestStrict;
