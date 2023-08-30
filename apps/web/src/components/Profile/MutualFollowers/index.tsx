b39e42baab2bce9561aca0e9d2d72b30249472a3
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-08-31 02:51:43 +0530

    fix mutual spacing

--- apps/web/src/components/Profile/MutualFollowers/index.tsx
+++ apps/web/src/components/Profile/MutualFollowers/index.tsx
@@ -39,7 +39,7 @@ const MutualFollowers: FC<MutualFollowersProps> = ({
       onClick={() => setShowMutualFollowersModal?.(true)}
       aria-hidden="true"
     >
-      <div className="contents -space-x-2">
+      <div className="contents [&>*]:-space-x-2">
         {profiles ? (
           <For each={profiles} as="div">
             {(profile) => (
