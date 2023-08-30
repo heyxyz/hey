fcfcfeab33d1ef21fb147deda2aed0b393dd51f2
bigint <69431456+bigint@users.noreply.github.com>
2023-08-30 18:55:37 +0530

    feat: add sentry to preferences worker

--- packages/workers/preferences/src/types.ts
+++ packages/workers/preferences/src/types.ts
@@ -1,4 +1,16 @@
+import type { IRequestStrict } from 'itty-router';
+import type { Toucan } from 'toucan-js';
+
 export interface Env {
+  SENTRY_DSN: string;
+  RELEASE: string;
   SUPABASE_KEY: string;
   PREFERENCES: KVNamespace;
 }
+
+export type WorkerRequest = {
+  req: Request;
+  env: Env;
+  ctx: ExecutionContext;
+  sentry?: Toucan;
+} & IRequestStrict;
