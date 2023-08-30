5d10d0eae56d7c687c23e0456d2b6c4a463a3827
bigint <69431456+bigint@users.noreply.github.com>
2023-08-30 14:41:51 +0530

    feat: add sentry to oembed worker (#3649)
    * feat: add sentry to oembed worker
    
    * fix: corsify in oembed worker

--- packages/workers/oembed/src/types.ts
+++ packages/workers/oembed/src/types.ts
@@ -1,4 +1,16 @@
+import type { IRequestStrict } from 'itty-router';
+import type { Toucan } from 'toucan-js';
+
 export interface Env {
+  SENTRY_DSN: string;
+  RELEASE: string;
   WORKER_ENV: string;
   IMAGEKIT_URL: string;
 }
+
+export type WorkerRequest = {
+  req: Request;
+  env: Env;
+  ctx: ExecutionContext;
+  sentry?: Toucan;
+} & IRequestStrict;
