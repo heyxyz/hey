fbc26a74fd908a2a53b536be46fbd7339ea2452f
bigint <69431456+bigint@users.noreply.github.com>
2023-08-30 19:54:48 +0530

    feat: add sentry to snapshot relay worker (#3656)
    * feat: add sentry to snapshot relay worker
    
    * fix: name

--- packages/workers/snapshot-relay/src/index.ts
+++ packages/workers/snapshot-relay/src/index.ts
@@ -1,7 +1,11 @@
-import { createCors, error, json, Router } from 'itty-router';
+import { Errors } from '@lenster/data/errors';
+import response from '@lenster/lib/response';
+import { createCors, error, Router, status } from 'itty-router';
+import { Toucan } from 'toucan-js';
 
 import createPoll from './handlers/createPoll';
-import type { Env } from './types';
+import buildRequest from './helpers/buildRequest';
+import type { Env, WorkerRequest } from './types';
 
 const { preflight, corsify } = createCors({
   origins: ['*'],
@@ -10,26 +14,40 @@ const { preflight, corsify } = createCors({
 
 const router = Router();
 
-router.all('*', preflight);
-router.get('/', () => new Response('gm, to snapshot relay service 👋'));
-router.post('/createPoll', createPoll);
-
-const routerHandleStack = (request: Request, env: Env, ctx: ExecutionContext) =>
-  router.handle(request, env, ctx).then(json);
-
-const handleFetch = async (
-  request: Request,
-  env: Env,
-  ctx: ExecutionContext
-) => {
-  try {
-    return await routerHandleStack(request, env, ctx);
-  } catch {
-    return error(500);
-  }
-};
+router
+  .all('*', preflight)
+  .head('*', () => status(200))
+  .get('/', (request: WorkerRequest) =>
+    response({
+      message: 'gm, to snapshot relay service 👋',
+      version: request.env.RELEASE ?? 'unknown'
+    })
+  )
+  .post('/createPoll', createPoll)
+  .all('*', () => error(404));
 
 export default {
-  fetch: (request: Request, env: Env, context: ExecutionContext) =>
-    handleFetch(request, env, context).then(corsify)
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const sentry = new Toucan({
+      request,
+      context: ctx,
+      tracesSampleRate: 1.0,
+      dsn: env.SENTRY_DSN,
+      release: env.RELEASE,
+      requestDataOptions: { allowedIps: true }
+    });
+    const incomingRequest = buildRequest(request, env, ctx, sentry);
+
+    return await router
+      .handle(incomingRequest)
+      .then(corsify)
+      .catch((error_) => {
+        sentry.captureException(error_);
+        return error(500, Errors.InternalServerError);
+      });
+  }
 };
