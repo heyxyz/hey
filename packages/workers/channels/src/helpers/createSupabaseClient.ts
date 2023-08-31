a38d6c48d34a440b932c36814d43ff27e23cb9fc
bigint <69431456+bigint@users.noreply.github.com>
2023-08-31 22:04:40 +0530

    feat: add channels worker (#3674)
    * feat: add channels worker
    
    * chore: update slug
    
    * chore: update slug

---
+++ packages/workers/channels/src/helpers/createSupabaseClient.ts
@@ -0,0 +1,12 @@
+import { SUPABASE_URL } from '@lenster/data/constants';
+import { createClient } from '@supabase/supabase-js';
+
+// @ts-ignore
+import type { Database } from '../database.types';
+import type { Env } from '../types';
+
+const createSupabaseClient = (env: Env) => {
+  return createClient<Database>(SUPABASE_URL, env.SUPABASE_KEY);
+};
+
+export default createSupabaseClient;
