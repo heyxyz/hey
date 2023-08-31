ed98fea02aeabaeac6623c51c89790f0933c4179
bigint <69431456+bigint@users.noreply.github.com>
2023-08-31 17:19:23 +0530

    feat: add traits

--- apps/web/src/components/Nft/Details/index.tsx
+++ apps/web/src/components/Nft/Details/index.tsx
@@ -1,36 +1,42 @@
 import type { OpenSeaNft } from '@lenster/types/opensea-nft';
 import { Card, GridItemFour } from '@lenster/ui';
+import { Trans } from '@lingui/macro';
 import type { FC } from 'react';
-import { useAppStore } from 'src/store/app';
 
 import NftTitle from './Title';
+import NftTraits from './Traits';
 
 interface NftDetailsProps {
   nft: OpenSeaNft;
 }
 
 const NftDetails: FC<NftDetailsProps> = ({ nft }) => {
-  const currentProfile = useAppStore((state) => state.currentProfile);
-
   return (
     <GridItemFour className="space-y-4">
       <NftTitle nft={nft} />
       {nft?.description ? (
         <Card className="max-h-60 overflow-y-auto">
-          <h1 className="divider px-5 py-4 font-bold">Description</h1>
+          <h1 className="divider px-5 py-4 font-bold">
+            <Trans>Description</Trans>
+          </h1>
           <p className="p-5 text-sm opacity-60">{nft.description}</p>
         </Card>
       ) : null}
       <Card className="divide-y dark:divide-gray-700">
         <div className="px-5 py-4">
-          <div className="text-sm opacity-50">Contract address</div>
+          <div className="text-sm opacity-50">
+            <Trans>Contract address</Trans>
+          </div>
           <div className="truncate">{nft.contract}</div>
         </div>
         <div className="px-5 py-4">
-          <div className="text-sm opacity-50">Token ID</div>
+          <div className="text-sm opacity-50">
+            <Trans>Token ID</Trans>
+          </div>
           <div className="truncate">{nft.identifier}</div>
         </div>
       </Card>
+      <NftTraits nft={nft} />
     </GridItemFour>
   );
 };
