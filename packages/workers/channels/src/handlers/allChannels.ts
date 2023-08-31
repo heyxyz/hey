0dcc7c8db2eb924675c7e840c59ebbb1eebc5ea4
bigint <69431456+bigint@users.noreply.github.com>
2023-08-31 22:04:40 +0530

    feat: add channels worker (#3674)
    * feat: add channels worker
    
    * chore: update slug
    
    * chore: update slug

---
+++ packages/workers/channels/src/handlers/allChannels.ts
@@ -0,0 +1,31 @@
+import '@sentry/tracing';
+
+import response from '@lenster/lib/response';
+
+import createSupabaseClient from '../helpers/createSupabaseClient';
+import type { WorkerRequest } from '../types';
+
+export default async (request: WorkerRequest) => {
+  const transaction = request.sentry?.startTransaction({
+    name: '@lenster/channels/allChannels'
+  });
+
+  try {
+    const cache = await request.env.CHANNELS.get('channels-list');
+
+    if (!cache) {
+      const client = createSupabaseClient(request.env);
+      const { data } = await client.from('channels').select('*');
+      await request.env.CHANNELS.put('channels-list', JSON.stringify(data));
+
+      return response({ success: true, result: data });
+    }
+
+    return response({ success: true, fromKV: true, result: JSON.parse(cache) });
+  } catch (error) {
+    request.sentry?.captureException(error);
+    throw error;
+  } finally {
+    transaction?.finish();
+  }
+};
