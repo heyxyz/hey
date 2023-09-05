a8508f18710cfc70cffd46d5110ea9e58be93e79
bigint <69431456+bigint@users.noreply.github.com>
2023-09-05 17:52:46 +0530

    chore: migrate more feature flags

--- packages/data/feature-flags.ts
+++ packages/data/feature-flags.ts
@@ -1,42 +1,15 @@
-import { mainnetLensTeamMembers } from './lens-members';
-
 export enum FeatureFlag {
-  TrendingWidget = 'trending-widget',
   GatedLocales = 'gated-locales',
-  ForYou = 'for-you',
-  WTF2 = 'wtf2',
-  ExploreTags = 'explore-tags',
-  Spaces = 'spaces',
-  NftLogin = 'nft-login'
+  Spaces = 'spaces'
 }
 
 export const featureFlags = [
-  {
-    key: FeatureFlag.TrendingWidget,
-    enabledFor: []
-  },
   {
     key: FeatureFlag.GatedLocales,
     enabledFor: []
   },
   {
-    key: FeatureFlag.ForYou,
-    enabledFor: [...mainnetLensTeamMembers]
-  },
-  {
-    key: FeatureFlag.WTF2,
-    enabledFor: [...mainnetLensTeamMembers]
-  },
-  {
-    key: FeatureFlag.ExploreTags,
-    enabledFor: [...mainnetLensTeamMembers]
-  },
-  {
     key: FeatureFlag.Spaces,
-    enabledFor: ['0x0d']
-  },
-  {
-    key: FeatureFlag.NftLogin,
     enabledFor: ['0x0d']
   }
 ];
