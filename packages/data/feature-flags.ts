3160f80252d9d21f9d8322a7fcf5dc0150d80e9f
bigint <69431456+bigint@users.noreply.github.com>
2023-09-05 17:56:56 +0530

    chore: migrate more feature flags

--- packages/data/feature-flags.ts
+++ packages/data/feature-flags.ts
@@ -1,13 +1,8 @@
 export enum FeatureFlag {
-  GatedLocales = 'gated-locales',
   Spaces = 'spaces'
 }
 
 export const featureFlags = [
-  {
-    key: FeatureFlag.GatedLocales,
-    enabledFor: []
-  },
   {
     key: FeatureFlag.Spaces,
     enabledFor: ['0x0d']
