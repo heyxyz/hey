9175511b37ffa652bd01e6f4c5a0853fe3d57fc0
bigint <69431456+bigint@users.noreply.github.com>
2023-09-11 14:49:55 +0530

    fix: NotLoggedIn comp logo size

--- apps/web/src/components/Shared/NotLoggedIn.tsx
+++ apps/web/src/components/Shared/NotLoggedIn.tsx
@@ -9,20 +9,20 @@ const NotLoggedIn: FC = () => {
     <div className="page-center flex-col">
       <MetaTags title={`Not logged in • ${APP_NAME}`} />
       <img
-        className="h-8 w-8"
-        height={32}
-        width={32}
+        className="h-20 w-20"
+        height={80}
+        width={80}
         src={'/logo.svg'}
         alt="Logo"
       />
       <div className="py-10 text-center">
         <h1 className="mb-4 text-3xl font-bold">
-          <Trans>Not logged in</Trans>
+          <Trans>Not logged in!</Trans>
         </h1>
         <div className="mb-4">
           <Trans>Log in to continue</Trans>
         </div>
-        <LoginButton />
+        <LoginButton isBig />
       </div>
     </div>
   );
