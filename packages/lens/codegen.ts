fafccb79667d06a781a0bee1e86f1f8dfb32113d
bigint <69431456+bigint@users.noreply.github.com>
2023-09-19 11:26:54 +0530

    chore: migrate all

--- packages/lens/codegen.ts
+++ packages/lens/codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 const config: CodegenConfig = {
   overwrite: true,
   schema: 'https://api-v2-mumbai.lens.dev',
-  documents: './documents/fragments/*.graphql',
+  documents: './documents/**/*.graphql',
   generates: {
     'generated.ts': {
       plugins: [
