a34e005e224e61219ef34bdf5f81737ba6210cf6
bigint <bigint@lenster.xyz>
2023-09-23 23:12:08 +0530

    feat: add mint fun endpoint to get members count

--- packages/workers/channels/src/handlers/getChannel.ts
+++ packages/workers/channels/src/handlers/getChannel.ts
@@ -10,11 +10,22 @@ export default async (request: WorkerRequest) => {
     const client = createSupabaseClient(request.env.SUPABASE_KEY);
     const { data } = await client
       .from('channels')
-      .select('*, members:channel_memberships(count)')
+      .select('*')
       .eq('slug', slug)
       .single();
 
-    return response({ success: true, result: data });
+    const membersCountRequest = await fetch(
+      `https://mint.fun/api/mintfun/contract/7777777:${data?.contract?.toLowerCase()}/details`
+    );
+
+    const json: {
+      details: { minterCount: string };
+    } = await membersCountRequest.json();
+
+    return response({
+      success: true,
+      result: { ...data, members: parseInt(json.details.minterCount || '0') }
+    });
   } catch (error) {
     throw error;
   }
