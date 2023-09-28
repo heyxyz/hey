42babe5cab24dd6881ef96e81a3d234b124f0ff9
bigint <bigint@lenster.xyz>
2023-09-28 09:54:12 +0530

    chore: rename hidden urls

--- apps/web/src/components/Common/MetaTags.tsx
+++ apps/web/src/components/Common/MetaTags.tsx
@@ -20,7 +20,7 @@ const MetaTags: FC<MetaTagsProps> = ({
         content="width=device-width, initial-scale=1, maximum-scale=5, viewport-fit=cover"
       />
 
-      <meta property="og:url" content="https://lenster.xyz" />
+      <meta property="og:url" content="https://hey.xyz" />
       <meta property="og:site_name" content={APP_NAME} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
