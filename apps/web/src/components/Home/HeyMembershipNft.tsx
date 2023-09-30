f83c6f450523523a81ca28a6eb051d56b4ad1003
bigint <69431456+bigint@users.noreply.github.com>
2023-09-30 21:08:47 +0530

    chore: update sidebar

--- apps/web/src/components/Home/HeyMembershipNft.tsx
+++ apps/web/src/components/Home/HeyMembershipNft.tsx
@@ -1,19 +1,28 @@
+import Mint from '@components/Publication/OpenActions/Nft/ZoraNft/Mint';
+import { CursorArrowRaysIcon } from '@heroicons/react/24/outline';
 import { PREFERENCES_WORKER_URL } from '@hey/data/constants';
 import { Localstorage } from '@hey/data/storage';
-import { MISCELLANEOUS } from '@hey/data/tracking';
+import { PUBLICATION } from '@hey/data/tracking';
 import type { MembershipNft } from '@hey/types/hey';
-import { Button, Card } from '@hey/ui';
+import { Button, Card, Modal } from '@hey/ui';
 import { Leafwatch } from '@lib/leafwatch';
-import { Trans } from '@lingui/macro';
+import { t, Trans } from '@lingui/macro';
 import axios from 'axios';
 import { type FC, useState } from 'react';
 import toast from 'react-hot-toast';
+import useZoraNft from 'src/hooks/zora/useZoraNft';
 import { useAppStore } from 'src/store/app';
 import { useQuery } from 'wagmi';
 
 const HeyMembershipNft: FC = () => {
   const currentProfile = useAppStore((state) => state.currentProfile);
   const [showMintModal, setShowMintModal] = useState(false);
+
+  const { data: nft, loading } = useZoraNft({
+    chain: 'zora',
+    address: '0x8fcfdad5ebdd1ce815aa769bbd7499091ac056d1',
+    token: ''
+  });
 
   const fetchPreferences = async (): Promise<MembershipNft> => {
     const response = await axios.get(
@@ -59,6 +68,7 @@ const HeyMembershipNft: FC = () => {
           loading: 'Updating...',
           success: () => {
             refetch();
+            setShowMintModal(false);
             return 'Updated!';
           },
           error: 'Error updating.'
@@ -79,19 +89,39 @@ const HeyMembershipNft: FC = () => {
       />
       <div className="p-5">
         <div className="mb-1 font-bold">
-          Hey Buddy! Grab your special Hey NFT Here.
+          <Trans>Hey Buddy! Grab your special Hey NFT Here.</Trans>
         </div>
         <div className="text-brand-400 mb-4">
-          Because you're part of the Hey crew. This NFT's for all our awesome
-          moments together!
+          <Trans>
+            New or OG, this NFT's for our epic times together. Let's keep the
+            vibe alive!
+          </Trans>
         </div>
         <div className="flex flex-col items-center space-y-1.5">
           <Button
             className="w-full"
-            onClick={() => Leafwatch.track(MISCELLANEOUS.OPEN_GITCOIN)}
+            onClick={() => {
+              setShowMintModal(true);
+              Leafwatch.track(PUBLICATION.OPEN_ACTIONS.ZORA_NFT.OPEN_MINT, {
+                from: 'mint_membership_nft'
+              });
+            }}
+            disabled={loading}
           >
             <Trans>Mint now</Trans>
           </Button>
+          <Modal
+            title={t`Mint`}
+            show={showMintModal}
+            icon={<CursorArrowRaysIcon className="text-brand h-5 w-5" />}
+            onClose={() => setShowMintModal(false)}
+          >
+            <Mint
+              nft={nft}
+              zoraLink="https://zora.co/collect/zora:0x8fcfdad5ebdd1ce815aa769bbd7499091ac056d1"
+              onCompleted={updateHeyMemberNftStatus}
+            />
+          </Modal>
           <button
             className="text-sm underline"
             onClick={updateHeyMemberNftStatus}
