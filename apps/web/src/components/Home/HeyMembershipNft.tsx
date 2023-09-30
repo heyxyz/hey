b72d876f42b293fe7d7afcc8b5081720729d789f
bigint <69431456+bigint@users.noreply.github.com>
2023-09-30 18:00:22 +0530

    feat: add membership nft

--- apps/web/src/components/Home/HeyMembershipNft.tsx
+++ apps/web/src/components/Home/HeyMembershipNft.tsx
@@ -1,35 +1,103 @@
-import { APP_NAME, STATIC_IMAGES_URL } from '@hey/data/constants';
+import { PREFERENCES_WORKER_URL } from '@hey/data/constants';
+import { Localstorage } from '@hey/data/storage';
 import { MISCELLANEOUS } from '@hey/data/tracking';
-import { Card } from '@hey/ui';
+import type { MembershipNft } from '@hey/types/hey';
+import { Button, Card } from '@hey/ui';
 import { Leafwatch } from '@lib/leafwatch';
 import { Trans } from '@lingui/macro';
-import Link from 'next/link';
-import type { FC } from 'react';
+import axios from 'axios';
+import { type FC, useState } from 'react';
+import toast from 'react-hot-toast';
+import { useAppStore } from 'src/store/app';
+import { useQuery } from 'wagmi';
 
 const HeyMembershipNft: FC = () => {
+  const currentProfile = useAppStore((state) => state.currentProfile);
+  const [showMintModal, setShowMintModal] = useState(false);
+
+  const fetchPreferences = async (): Promise<MembershipNft> => {
+    const response = await axios.get(
+      `${PREFERENCES_WORKER_URL}/getHeyMemberNftStatus/${currentProfile?.id}`
+    );
+    const { data } = response;
+
+    return data.result;
+  };
+
+  const { data, isLoading, refetch } = useQuery(
+    ['getHeyMemberNftStatus', currentProfile?.id],
+    () => fetchPreferences(),
+    { enabled: Boolean(currentProfile?.id) }
+  );
+
+  if (isLoading) {
+    return null;
+  }
+
+  const dismissedOrMinted = data?.dismissedOrMinted;
+
+  if (dismissedOrMinted) {
+    return null;
+  }
+
+  const updateHeyMemberNftStatus = async () => {
+    try {
+      toast.promise(
+        axios.post(
+          `${PREFERENCES_WORKER_URL}/updateHeyMemberNftStatus`,
+          {
+            id: currentProfile?.id,
+            dismissedOrMinted: true
+          },
+          {
+            headers: {
+              'X-Access-Token': localStorage.getItem(Localstorage.AccessToken)
+            }
+          }
+        ),
+        {
+          loading: 'Updating...',
+          success: () => {
+            refetch();
+            return 'Updated!';
+          },
+          error: 'Error updating.'
+        }
+      );
+    } catch {}
+  };
+
   return (
     <Card
       as="aside"
-      className="mb-4 space-y-4 !border-[#27bdce] !bg-[#27bdce]/10 p-5 text-[#1396a5] dark:bg-[#27bdce]/50"
+      className="text-brand dark:bg-brand-10/50 !border-brand-500 !bg-brand-50 mb-4"
     >
       <img
-        src={`${STATIC_IMAGES_URL}/brands/gitcoin.png`}
+        src="https://ipfs.decentralized-content.com/ipfs/bafybeib6infyovvtawokys4ejjr4r3qk4soy7jqriejp2wbmttedupsy64"
         alt="Gitcoin emoji"
-        className="mx-auto h-14"
+        className="h-48 w-full rounded-t-xl object-cover"
       />
-      <div className="space-y-3 text-center text-sm">
-        <div className="font-bold">
-          Support ${APP_NAME} on Gitcoin Grants Round 18
+      <div className="p-5">
+        <div className="mb-1 font-bold">
+          Hey Buddy! Grab your special Hey NFT Here.
         </div>
-        <div>
-          <Link
-            href="https://hey.xyz/gitcoin"
-            className="font-bold underline"
-            target="_blank"
+        <div className="text-brand-400 mb-4">
+          Because you're part of the Hey crew. This NFT's for all our awesome
+          moments together!
+        </div>
+        <div className="flex flex-col items-center space-y-1.5">
+          <Button
+            className="w-full"
             onClick={() => Leafwatch.track(MISCELLANEOUS.OPEN_GITCOIN)}
           >
-            <Trans>Contribute now</Trans>
-          </Link>
+            <Trans>Mint now</Trans>
+          </Button>
+          <button
+            className="text-sm underline"
+            onClick={updateHeyMemberNftStatus}
+          >
+            Dismiss
+          </button>
         </div>
       </div>
     </Card>
---
+++ packages/workers/preferences/src/handlers/updateHeyMemberNftStatus.ts
@@ -0,0 +1,70 @@
+import { Errors } from '@hey/data/errors';
+import hasOwnedLensProfiles from '@hey/lib/hasOwnedLensProfiles';
+import response from '@hey/lib/response';
+import validateLensAccount from '@hey/lib/validateLensAccount';
+import createSupabaseClient from '@hey/supabase/createSupabaseClient';
+import jwt from '@tsndr/cloudflare-worker-jwt';
+import { boolean, object, string } from 'zod';
+
+import type { WorkerRequest } from '../types';
+
+type ExtensionRequest = {
+  id: string;
+  dismissedOrMinted: boolean;
+};
+
+const validationSchema = object({
+  id: string(),
+  dismissedOrMinted: boolean()
+});
+
+export default async (request: WorkerRequest) => {
+  const body = await request.json();
+  if (!body) {
+    return response({ success: false, error: Errors.NoBody });
+  }
+
+  const accessToken = request.headers.get('X-Access-Token');
+  if (!accessToken) {
+    return response({ success: false, error: Errors.NoAccessToken });
+  }
+
+  const validation = validationSchema.safeParse(body);
+
+  if (!validation.success) {
+    return response({ success: false, error: validation.error.issues });
+  }
+
+  const { id, dismissedOrMinted } = body as ExtensionRequest;
+
+  try {
+    const isAuthenticated = await validateLensAccount(accessToken, true);
+    if (!isAuthenticated) {
+      return response({ success: false, error: Errors.InvalidAccesstoken });
+    }
+
+    const { payload } = jwt.decode(accessToken);
+    const hasOwned = await hasOwnedLensProfiles(payload.id, id, true);
+    if (!hasOwned) {
+      return new Response(
+        JSON.stringify({ success: false, error: Errors.InvalidProfileId })
+      );
+    }
+
+    const client = createSupabaseClient(request.env.SUPABASE_KEY);
+
+    const { data, error } = await client
+      .from('membership-nft')
+      .upsert({ id, dismissedOrMinted })
+      .select()
+      .single();
+
+    if (error) {
+      throw error;
+    }
+
+    return response({ success: true, result: data });
+  } catch (error) {
+    throw error;
+  }
+};
