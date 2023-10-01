95fa52326c2af6f7f8f908daf679b704f7907aa9
bigint <69431456+bigint@users.noreply.github.com>
2023-10-01 12:42:13 +0530

    feat: add cache

--- packages/workers/staff-picks/src/handlers/getStaffPicks.ts
+++ packages/workers/staff-picks/src/handlers/getStaffPicks.ts
@@ -1,19 +1,26 @@
 import response from '@hey/lib/response';
 import createSupabaseClient from '@hey/supabase/createSupabaseClient';
 
+import { STAFFPICKS_KV_KEY } from '../constants';
 import type { WorkerRequest } from '../types';
 
 export default async (request: WorkerRequest) => {
   try {
-    const client = createSupabaseClient(request.env.SUPABASE_KEY);
+    const cache = await request.env.STAFFPICKS.get(STAFFPICKS_KV_KEY);
 
-    const { data } = await client
-      .from('staff-picks')
-      .select('*')
-      .order('score', { ascending: false })
-      .limit(10);
+    if (!cache) {
+      const client = createSupabaseClient(request.env.SUPABASE_KEY);
 
-    return response({ success: true, result: data });
+      const { data } = await client
+        .from('staff-picks')
+        .select('*')
+        .order('score', { ascending: false })
+        .limit(10);
+
+      return response({ success: true, result: data });
+    }
+
+    return response({ success: true, fromKV: true, result: JSON.parse(cache) });
   } catch (error) {
     throw error;
   }
