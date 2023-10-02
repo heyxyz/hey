568e15bd76911b05ae9a2e0edf1b4d777d129e25
bigint <69431456+bigint@users.noreply.github.com>
2023-10-02 17:59:48 +0530

    feat: add user details in leafwatch

--- packages/workers/leafwatch/src/index.ts
+++ packages/workers/leafwatch/src/index.ts
@@ -3,6 +3,7 @@ import response from '@hey/lib/response';
 import { createCors, error, Router, status } from 'itty-router';
 
 import ingest from './handlers/ingest';
+import userDetails from './handlers/userDetails';
 import buildRequest from './helpers/buildRequest';
 import type { Env, WorkerRequest } from './types';
 
@@ -22,6 +23,7 @@ router
       version: request.env.RELEASE ?? 'unknown'
     })
   )
+  .get('/user', userDetails)
   .post('/ingest', ingest)
   .all('*', () => error(404));
 
