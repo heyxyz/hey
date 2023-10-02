d2d838b0761b9d4f1614b7d699721d4094ec9105
bigint <69431456+bigint@users.noreply.github.com>
2023-10-02 14:45:18 +0530

    feat: use urlcat in workers

--- packages/workers/nft/src/handlers/getZoraNft.ts
+++ packages/workers/nft/src/handlers/getZoraNft.ts
@@ -1,5 +1,6 @@
 import getZoraChainIsMainnet from '@hey/lib/nft/getZoraChainIsMainnet';
 import response from '@hey/lib/response';
+import urlcat from 'urlcat';
 
 import type { WorkerRequest } from '../types';
 
@@ -16,14 +17,16 @@ export default async (request: WorkerRequest) => {
   try {
     const network = getZoraChainIsMainnet(chain as string) ? '' : 'testnet.';
     const zoraResponse = await fetch(
-      `https://${network}zora.co/api/personalize/collection/${chain}:${address}/${
-        token || ''
-      }`
+      urlcat(
+        `https://${network}zora.co/api/personalize/collection/:chain::address/:token`,
+        { chain, address, token: token || 0 }
+      )
     );
     const nft: { collection: any } = await zoraResponse.json();
 
     return response({ success: true, nft: nft.collection || null });
   } catch (error) {
+    console.error(error);
     throw error;
   }
 };
