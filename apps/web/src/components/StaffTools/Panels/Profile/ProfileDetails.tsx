ff835b18ba8fcb16fdab7b3bee6b19c9ff33d30c
bigint <69431456+bigint@users.noreply.github.com>
2023-10-02 19:55:53 +0530

    fix: add user details in leafwatch FE

--- apps/web/src/components/StaffTools/Panels/Profile/ProfileDetails.tsx
+++ apps/web/src/components/StaffTools/Panels/Profile/ProfileDetails.tsx
@@ -1,7 +1,13 @@
-import { UserPlusIcon } from '@heroicons/react/24/outline';
+import {
+  ComputerDesktopIcon,
+  CursorArrowRaysIcon,
+  GlobeAltIcon,
+  MapPinIcon
+} from '@heroicons/react/24/outline';
 import { AdjustmentsVerticalIcon } from '@heroicons/react/24/solid';
 import { LEAFWATCH_WORKER_URL } from '@hey/data/constants';
 import type { Profile } from '@hey/lens';
+import humanize from '@hey/lib/humanize';
 import { t, Trans } from '@lingui/macro';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
@@ -9,15 +15,16 @@ import type { FC } from 'react';
 
 import MetaDetails from '../MetaDetails';
 
-interface RankProps {
+interface ProfileDetailsProps {
   profile: Profile;
 }
 
-const Access: FC<RankProps> = ({ profile }) => {
+const ProfileDetails: FC<ProfileDetailsProps> = ({ profile }) => {
   const getProfileDetails = async (): Promise<{
     region: string;
     city: string;
-    events: string;
+    country: string;
+    events: number;
     os: string;
     browser: string;
     version: string;
@@ -49,20 +56,41 @@ const Access: FC<RankProps> = ({ profile }) => {
       <div className="mt-5 flex items-center space-x-2 text-yellow-600">
         <AdjustmentsVerticalIcon className="h-5 w-5" />
         <div className="text-lg font-bold">
-          <Trans>Access</Trans>
+          <Trans>Leafwatch Details</Trans>
         </div>
       </div>
       <div className="mt-3 space-y-2 font-bold">
         <MetaDetails
-          icon={<UserPlusIcon className="lt-text-gray-500 h-4 w-4" />}
-          value={data.events}
+          icon={<CursorArrowRaysIcon className="lt-text-gray-500 h-4 w-4" />}
+          value={humanize(data.events)}
           title={t`Total events`}
         >
-          {data.events}
+          {humanize(data.events)}
+        </MetaDetails>
+        <MetaDetails
+          icon={<MapPinIcon className="lt-text-gray-500 h-4 w-4" />}
+          value={data.city}
+          title={t`Location`}
+        >
+          {data.city}, {data.region}, {data.country}
+        </MetaDetails>
+        <MetaDetails
+          icon={<ComputerDesktopIcon className="lt-text-gray-500 h-4 w-4" />}
+          value={data.os}
+          title={t`OS`}
+        >
+          {data.os}
+        </MetaDetails>
+        <MetaDetails
+          icon={<GlobeAltIcon className="lt-text-gray-500 h-4 w-4" />}
+          value={data.browser}
+          title={t`Browser`}
+        >
+          {data.browser} {data.version}
         </MetaDetails>
       </div>
     </>
   );
 };
 
-export default Access;
+export default ProfileDetails;
