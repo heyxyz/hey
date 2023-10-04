f9892db7cd9b564c96911ced733658ed8c1e1075
Swaroop <maddisaiswaroop@gmail.com>
2023-10-05 00:23:12 +0530

    chore: remove sw header in next.config.js as next addes it automatically

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -64,8 +64,7 @@ const nextConfig = {
         headers: [
           { key: 'X-Content-Type-Options', value: 'nosniff' },
           { key: 'X-XSS-Protection', value: '1; mode=block' },
-          { key: 'Referrer-Policy', value: 'strict-origin' },
-          { key: 'Service-Worker-Allowed', value: '/' }
+          { key: 'Referrer-Policy', value: 'strict-origin' }
         ]
       },
       { source: '/about', headers },
