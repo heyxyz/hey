698762bf230475c8348e17d1fe3ddae64638f414
5war00p <maddisaiswaroop@gmail.com>
2023-10-05 17:19:08 +0530

    fix: remove unnecessary .then

--- apps/web/script/build-sw.js
+++ apps/web/script/build-sw.js
@@ -8,18 +8,16 @@ const outfile = !!process.env.NEXT_PUBLIC_IS_PRODUCTION
   ? '.vercel/output/static/sw.js' // this vary based deployment environment
   : 'public/sw.js';
 
-esbuild
-  .build({
-    target: 'es2020',
-    platform: 'browser',
-    entryPoints: ['./src/sw/index.ts'],
-    outfile,
-    allowOverwrite: true,
-    format: 'esm',
-    bundle: true,
-    define: {
-      'process.env.STATIC_ASSETS': JSON.stringify(STATIC_ASSETS)
-    },
-    minify: true
-  })
-  .then(() => {});
+esbuild.build({
+  target: 'es2020',
+  platform: 'browser',
+  entryPoints: ['./src/sw/index.ts'],
+  outfile,
+  allowOverwrite: true,
+  format: 'esm',
+  bundle: true,
+  define: {
+    'process.env.STATIC_ASSETS': JSON.stringify(STATIC_ASSETS)
+  },
+  minify: true
+});
