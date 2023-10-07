09824df9a19d8466af9ff0029e8fc324d570fbf2
bigint <69431456+bigint@users.noreply.github.com>
2023-10-07 12:28:31 +0530

    feat: lens v2

--- apps/web/src/components/Home/Highlights.tsx
+++ apps/web/src/components/Home/Highlights.tsx
@@ -2,8 +2,8 @@ import QueuedPublication from '@components/Publication/QueuedPublication';
 import SinglePublication from '@components/Publication/SinglePublication';
 import PublicationsShimmer from '@components/Shared/Shimmer/PublicationsShimmer';
 import { LightBulbIcon } from '@heroicons/react/24/outline';
-import type { FeedHighlightsRequest, Publication } from '@hey/lens';
-import { useFeedHighlightsQuery } from '@hey/lens';
+import type { AnyPublication, FeedHighlightsRequest } from '@hey/lens';
+import { LimitType, useFeedHighlightsQuery } from '@hey/lens';
 import { Card, EmptyState, ErrorMessage } from '@hey/ui';
 import { t } from '@lingui/macro';
 import type { FC } from 'react';
@@ -22,15 +22,12 @@ const Highlights: FC = () => {
 
   // Variables
   const request: FeedHighlightsRequest = {
-    profileId: seeThroughProfile?.id ?? currentProfile?.id,
-    limit: 30
+    where: { for: seeThroughProfile?.id ?? currentProfile?.id },
+    limit: LimitType.Fifty
   };
-  const reactionRequest = currentProfile
-    ? { profileId: currentProfile?.id }
-    : null;
 
   const { data, loading, error, fetchMore } = useFeedHighlightsQuery({
-    variables: { request, reactionRequest, profileId: currentProfile?.id }
+    variables: { request }
   });
 
   const publications = data?.feedHighlights?.items;
@@ -44,11 +41,7 @@ const Highlights: FC = () => {
       }
 
       await fetchMore({
-        variables: {
-          request: { ...request, cursor: pageInfo?.next },
-          reactionRequest,
-          profileId: currentProfile?.id
-        }
+        variables: { request: { ...request, cursor: pageInfo?.next } }
       });
     }
   });
@@ -84,7 +77,7 @@ const Highlights: FC = () => {
           key={`${publication?.id}_${index}`}
           isFirst={index === 0}
           isLast={index === publications.length - 1}
-          publication={publication as Publication}
+          publication={publication as AnyPublication}
         />
       ))}
       {hasMore ? <span ref={observe} /> : null}
