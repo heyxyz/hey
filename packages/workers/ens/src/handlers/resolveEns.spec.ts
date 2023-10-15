9350623b3fa9a15efece6f279c3c0390c4112fa5
Yoginth <yoginth@Yoginths-Mac-Studio.local>
2023-10-15 17:41:44 +0530

    fix: test cases

--- packages/workers/ens/src/handlers/resolveEns.spec.ts
+++ packages/workers/ens/src/handlers/resolveEns.spec.ts
@@ -17,7 +17,7 @@ describe('resolveEns', () => {
       method: 'POST',
       body: JSON.stringify({
         addresses: [
-          '0x3A5bd1E37b099aE3386D13947b6a90d97675e5e3',
+          '0x03ba34f6ea1496fa316873cf8350a3f7ead317ef',
           '0x01d79BcEaEaaDfb8fD2F2f53005289CFcF483464',
           '0x82aae03e76290a4aed2c63ed7740ab5ef7c07b66',
           '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045'
