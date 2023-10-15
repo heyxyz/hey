b59f0c4e084daf5db93d6c96acbb9ffe07e9c90c
bigint <69431456+bigint@users.noreply.github.com>
2023-10-15 13:51:00 +0530

    refactor: migrate to lens v2

--- apps/web/src/components/Notification/Type/MirrorNotification.tsx
+++ apps/web/src/components/Notification/Type/MirrorNotification.tsx
@@ -23,36 +23,32 @@ const MirrorNotification: FC<MirrorNotificationProps> = ({ notification }) => {
   const firstProfile = mirrors?.[0]?.profile;
 
   return (
-    <div className="flex items-start justify-between">
-      <div className="flex-1 space-y-2">
-        <div className="flex items-center justify-between">
-          <div className="flex items-center space-x-3">
-            <ArrowsRightLeftIcon className="text-brand-500/70 h-6 w-6" />
-            <div className="flex items-center space-x-1">
-              {mirrors.map((mirror) => (
-                <div key={mirror.mirrorId}>
-                  <NotificationProfileAvatar profile={mirror.profile} />
-                </div>
-              ))}
+    <div className="space-y-2">
+      <div className="flex items-center space-x-3">
+        <ArrowsRightLeftIcon className="text-brand-500/70 h-6 w-6" />
+        <div className="flex items-center space-x-1">
+          {mirrors.map((mirror) => (
+            <div key={mirror.mirrorId}>
+              <NotificationProfileAvatar profile={mirror.profile} />
             </div>
-          </div>
+          ))}
         </div>
-        <div className="ml-9">
-          <AggregatedNotificationTitle
-            firstProfile={firstProfile}
-            text={`and ${mirrors.length} ${plur(
-              'other',
-              mirrors.length
-            )} mirrored your`}
-            type={notification?.publication.__typename}
-          />
-          <Link
-            href={`/posts/${notification?.publication?.id}`}
-            className="lt-text-gray-500 linkify mt-2 line-clamp-2"
-          >
-            <Markup>{filteredContent}</Markup>
-          </Link>
-        </div>
+      </div>
+      <div className="ml-9">
+        <AggregatedNotificationTitle
+          firstProfile={firstProfile}
+          text={`and ${mirrors.length} ${plur(
+            'other',
+            mirrors.length
+          )} mirrored your`}
+          type={notification?.publication.__typename}
+        />
+        <Link
+          href={`/posts/${notification?.publication?.id}`}
+          className="lt-text-gray-500 linkify mt-2 line-clamp-2"
+        >
+          <Markup>{filteredContent}</Markup>
+        </Link>
       </div>
     </div>
   );
