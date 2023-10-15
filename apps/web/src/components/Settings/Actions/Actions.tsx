3100564b070090a2904a32b4aec3c1a23ea9df7b
bigint <69431456+bigint@users.noreply.github.com>
2023-10-15 20:24:42 +0530

    refactor: migrate to lens v2

--- apps/web/src/components/Settings/Actions/Actions.tsx
+++ apps/web/src/components/Settings/Actions/Actions.tsx
@@ -1,5 +1,9 @@
+import { POLYGONSCAN_URL } from '@hey/data/constants';
 import type { ProfileActionHistory } from '@hey/lens';
+import formatAddress from '@hey/lib/formatAddress';
 import { Card } from '@hey/ui';
+import { formatDate } from '@lib/formatTime';
+import Link from 'next/link';
 import type { FC } from 'react';
 
 interface ActionsProps {
@@ -15,12 +19,36 @@ const Actions: FC<ActionsProps> = ({ actions }) => {
     <div className="space-y-4 px-5 pb-5">
       {actions?.map((action) => {
         return (
-          <Card
-            key={action.id}
-            className="flex flex-wrap items-start justify-between p-5"
-            forceRounded
-          >
-            {JSON.stringify(action)}
+          <Card key={action.id} className="space-y-1 p-5" forceRounded>
+            <b>{action.actionType.toLowerCase()}</b>
+            <div className="lt-text-gray-500 text-sm">
+              {action.txHash ? (
+                <span>
+                  <span>Hash: </span>
+                  <Link
+                    className="hover:underline"
+                    href={`${POLYGONSCAN_URL}/tx/${action.txHash}`}
+                  >
+                    {action.txHash.slice(0, 8 + 2)}…
+                    {action.txHash.slice(action.txHash.length - 8)}
+                  </Link>
+                  <span className="mx-2 border-l dark:border-gray-700" />
+                </span>
+              ) : null}
+              {action.who ? (
+                <span>
+                  <span>Acted by: </span>
+                  <Link
+                    className="hover:underline"
+                    href={`${POLYGONSCAN_URL}/address/${action.who}`}
+                  >
+                    {formatAddress(action.who)}
+                  </Link>
+                  <span className="mx-2 border-l dark:border-gray-700" />
+                </span>
+              ) : null}
+              {formatDate(action.actionedOn)}
+            </div>
           </Card>
         );
       })}
--- packages/data/regex.ts
+++ packages/data/regex.ts
@@ -7,7 +7,6 @@ export const Regex = {
   hashtag: /(#\w*[A-Za-z]\w*)/g,
   ethereumAddress: /^(0x)?[\da-f]{40}$/i,
   handle: /^[\da-z]+$/g,
-  santiizeHandle: /[^\d .A-Za-z]/g,
   profileNameValidator: new RegExp('^[^' + RESTRICTED_SYMBOLS + ']+$'),
   profileNameFilter: new RegExp('[' + RESTRICTED_SYMBOLS + ']', 'gu'),
   gm: /\bgm\b/i
