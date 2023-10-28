d8e2641ca15f9927d78f489f096a0eb2ac8a275b
bigint <69431456+bigint@users.noreply.github.com>
2023-10-28 21:25:13 +0530

    chore: update endpoints

--- packages/data/lens-endpoints.ts
+++ packages/data/lens-endpoints.ts
@@ -1,6 +1,6 @@
 enum LensEndpoint {
   Mainnet = 'https://api-v2.lens.dev',
-  Testnet = 'https://api-v2-mumbai.lens.dev'
+  Testnet = 'https://api-v2-mumbai-live.lens.dev'
 }
 
 export default LensEndpoint;
