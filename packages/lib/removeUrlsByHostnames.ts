37932ccdd861fdc8106307b0451c896022d89aab
bigint <69431456+bigint@users.noreply.github.com>
2023-11-02 10:16:07 +0530

    feat: add removeUrlsByHostnames test cases

--- packages/lib/removeUrlsByHostnames.ts
+++ packages/lib/removeUrlsByHostnames.ts
@@ -8,9 +8,12 @@ const removeUrlsByHostnames = (
         `https?:\\/\\/(www\\.)?${hostname.replace('.', '\\.')}[\\S]+`
     )
     .join('|');
-
   const regex = new RegExp(regexPattern, 'g');
-  return content.replace(regex, '');
+
+  return content
+    .replace(regex, '')
+    .replace(/\s{2,}/g, ' ')
+    .trim();
 };
 
 export default removeUrlsByHostnames;
