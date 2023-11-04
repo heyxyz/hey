4ce1a5b59fc0ca036b3e7dad87e3028182ec9005
bigint <69431456+bigint@users.noreply.github.com>
2023-11-05 00:20:52 +0530

    refactor: views utils

---
+++ packages/lib/getPublicationViewCountById.ts
@@ -0,0 +1,16 @@
+import type { PublicationViewCount } from '@hey/types/hey';
+
+/**
+ * Get the number of views of a publication
+ * @param views The views of the publications
+ * @param id The id of the publication
+ * @returns The number of views of the publication
+ */
+const getPublicationViewCountById = (
+  views: PublicationViewCount[],
+  id: string
+) => {
+  return views.find((v) => v.id === id)?.views || 0;
+};
+
+export default getPublicationViewCountById;
