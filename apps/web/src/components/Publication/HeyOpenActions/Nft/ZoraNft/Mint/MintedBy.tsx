ecab4b8731c1bcc33faf4f473a1314948bb81e1a
bigint <69431456+bigint@users.noreply.github.com>
2023-11-05 21:59:23 +0530

    feat: use useDefaultProfileQuery in minted by modal

--- apps/web/src/components/Publication/HeyOpenActions/Nft/ZoraNft/Mint/MintedBy.tsx
+++ apps/web/src/components/Publication/HeyOpenActions/Nft/ZoraNft/Mint/MintedBy.tsx
@@ -1,4 +1,8 @@
+import SmallUserProfileShimmer from '@components/Shared/Shimmer/SmallUserProfileShimmer';
+import SmallUserProfile from '@components/Shared/SmallUserProfile';
 import SmallWalletProfile from '@components/Shared/SmallWalletProfile';
+import type { Profile } from '@hey/lens';
+import { useDefaultProfileQuery } from '@hey/lens';
 import { type FC } from 'react';
 import type { Address } from 'viem';
 
@@ -7,15 +11,28 @@ interface MintedByProps {
 }
 
 const MintedBy: FC<MintedByProps> = ({ address }) => {
+  const { data, loading } = useDefaultProfileQuery({
+    variables: { request: { for: address } },
+    skip: !Boolean(address)
+  });
+
   if (!address) {
     return null;
   }
 
-  // TODO: use default profile
   return (
     <div className="mb-4 flex items-center gap-x-2">
       <span>by</span>
-      <SmallWalletProfile address={address} smallAvatar />
+      {loading ? (
+        <SmallUserProfileShimmer smallAvatar />
+      ) : data?.defaultProfile ? (
+        <SmallUserProfile
+          profile={data.defaultProfile as Profile}
+          smallAvatar
+        />
+      ) : (
+        <SmallWalletProfile address={address} smallAvatar />
+      )}
     </div>
   );
 };
