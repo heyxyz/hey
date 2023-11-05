f1235ee674cd10a4ca704d9a54c5ffe7013bea97
bigint <69431456+bigint@users.noreply.github.com>
2023-11-05 17:08:04 +0530

    fix: use max queue as 1

--- packages/workers/ws/src/helpers/consumeImpression.ts
+++ packages/workers/ws/src/helpers/consumeImpression.ts
@@ -4,7 +4,7 @@ const consumeImpression = async (payload: any, websocket: any, env: Env) => {
   if (payload.viewer_id && payload.publication_id) {
     const { viewer_id, publication_id } = payload;
     await env.IMPRESSIONS_QUEUE.send({ viewer_id, publication_id });
-    websocket.send(JSON.stringify({ error: 'Queued for processing' }));
+    websocket.send(JSON.stringify({ id: crypto.randomUUID() }));
   }
 };
 
