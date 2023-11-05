3e40a00226cfb32a38b514fc44b4b312fdd8276a
bigint <69431456+bigint@users.noreply.github.com>
2023-11-05 10:44:05 +0530

    feat: add views in explore feed

--- packages/lib/getPublicationViewCountById.ts
+++ packages/lib/getPublicationViewCountById.ts
@@ -1,15 +1,22 @@
+import type { AnyPublication } from '@hey/lens';
 import type { PublicationViewCount } from '@hey/types/hey';
+
+import { isMirrorPublication } from './publicationHelpers';
 
 /**
  * Get the number of views of a publication
  * @param views The views of the publications
- * @param id The id of the publication
+ * @param publication The publication
  * @returns The number of views of the publication
  */
 const getPublicationViewCountById = (
   views: PublicationViewCount[],
-  id: string
+  publication: AnyPublication
 ) => {
+  const id = isMirrorPublication(publication)
+    ? publication.mirrorOn.id
+    : publication.id;
+
   return views.find((v) => v.id === id)?.views || 0;
 };
 
