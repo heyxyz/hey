5207c2f108db906f774d7e6ee5bd40f20adb3d6b
bigint <69431456+bigint@users.noreply.github.com>
2023-11-05 15:33:19 +0530

    feat: add virtual list for search feed

--- apps/web/src/components/Search/Publications.tsx
+++ apps/web/src/components/Search/Publications.tsx
@@ -8,14 +8,20 @@ import {
   useSearchPublicationsQuery
 } from '@hey/lens';
 import { Card, EmptyState, ErrorMessage } from '@hey/ui';
-import { type FC } from 'react';
-import { useInView } from 'react-cool-inview';
+import { motion } from 'framer-motion';
+import { type FC, useRef } from 'react';
+import type { StateSnapshot } from 'react-virtuoso';
+import { Virtuoso } from 'react-virtuoso';
+
+let virtuosoState: any = { ranges: [], screenTop: 0 };
 
 interface PublicationsProps {
   query: string;
 }
 
 const Publications: FC<PublicationsProps> = ({ query }) => {
+  const virtuosoRef = useRef<any>();
+
   // Variables
   const request: PublicationSearchRequest = {
     where: { customFilters: [CustomFiltersType.Gardeners] },
@@ -32,17 +38,23 @@ const Publications: FC<PublicationsProps> = ({ query }) => {
   const pageInfo = search?.pageInfo;
   const hasMore = pageInfo?.next;
 
-  const { observe } = useInView({
-    onChange: async ({ inView }) => {
-      if (!inView || !hasMore) {
-        return;
-      }
-
-      await fetchMore({
-        variables: { request: { ...request, cursor: pageInfo?.next } }
-      });
+  const onEndReached = async () => {
+    if (!hasMore) {
+      return;
     }
-  });
+
+    await fetchMore({
+      variables: { request: { ...request, cursor: pageInfo?.next } }
+    });
+  };
+
+  const onScrolling = (scrolling: boolean) => {
+    virtuosoRef?.current?.getState((state: StateSnapshot) => {
+      if (!scrolling) {
+        virtuosoState = { ...state };
+      }
+    });
+  };
 
   if (loading) {
     return <PublicationsShimmer />;
@@ -66,17 +78,37 @@ const Publications: FC<PublicationsProps> = ({ query }) => {
   }
 
   return (
-    <>
-      <Card className="divide-y-[1px] dark:divide-gray-700">
-        {publications?.map((publication, index) => (
-          <SinglePublication
-            key={`${publication?.id}_${index}`}
-            publication={publication}
-          />
-        ))}
-      </Card>
-      {hasMore ? <span ref={observe} /> : null}
-    </>
+    <Card className="divide-y-[1px] dark:divide-gray-700">
+      {publications?.length ? (
+        <Virtuoso
+          useWindowScroll
+          restoreStateFrom={
+            virtuosoState.ranges.length === 0
+              ? virtuosoRef?.current?.getState((state: StateSnapshot) => state)
+              : virtuosoState
+          }
+          ref={virtuosoRef}
+          data={publications}
+          isScrolling={onScrolling}
+          endReached={onEndReached}
+          className="virtual-feed-list"
+          itemContent={(index, publication) => {
+            return (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              >
+                <SinglePublication
+                  key={`${publication?.id}_${index}`}
+                  publication={publication}
+                />
+              </motion.div>
+            );
+          }}
+        />
+      ) : null}
+    </Card>
   );
 };
 
