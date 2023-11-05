499b9881cc61671e7b728655077ea76e1f4f8fb2
bigint <69431456+bigint@users.noreply.github.com>
2023-11-05 13:28:57 +0530

    fix: insert everything in ingestImpression, dont check for recents

--- packages/workers/ws/src/helpers/ingestImpression.ts
+++ packages/workers/ws/src/helpers/ingestImpression.ts
@@ -3,52 +3,27 @@ import type { Env } from '../types';
 const ingestImpression = async (payload: any, websocket: any, env: Env) => {
   if (payload.viewer_id && payload.publication_id) {
     const { viewer_id, publication_id } = payload;
-    const insertedRecently = await fetch(
-      `${env.CLICKHOUSE_REST_ENDPOINT}&default_format=JSONCompact`,
-      {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: `
-        SELECT 
-          count(*)
-        FROM impressions
-        WHERE viewer_id = '${viewer_id}' AND
-        publication_id = '${publication_id}' AND
-        viewed_at > now64(3) - INTERVAL 20 SECOND;
+    const clickhouseResponse = await fetch(env.CLICKHOUSE_REST_ENDPOINT, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: `
+        INSERT INTO impressions (
+          viewer_id,
+          publication_id
+        ) VALUES (
+          '${viewer_id}',
+          '${publication_id}'
+        )
       `
-      }
-    );
-    const json: {
-      data: [string][];
-    } = await insertedRecently.json();
+    });
 
-    if (parseInt(json.data[0][0]) < 1) {
-      const clickhouseResponse = await fetch(env.CLICKHOUSE_REST_ENDPOINT, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: `
-      INSERT INTO impressions (
-        viewer_id,
-        publication_id
-      ) VALUES (
-        '${viewer_id}',
-        '${publication_id}'
-      )
-    `
-      });
-
-      if (clickhouseResponse.status !== 200) {
-        websocket.send(JSON.stringify({ error: 'Insert error' }));
-      } else {
-        websocket.send(
-          JSON.stringify({
-            id: clickhouseResponse.headers.get('x-clickhouse-query-id')
-          })
-        );
-      }
+    if (clickhouseResponse.status !== 200) {
+      websocket.send(JSON.stringify({ error: 'Insert error' }));
     } else {
       websocket.send(
-        JSON.stringify({ message: 'Already seen less than 10 seconds!' })
+        JSON.stringify({
+          id: clickhouseResponse.headers.get('x-clickhouse-query-id')
+        })
       );
     }
   }
