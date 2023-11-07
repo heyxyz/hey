5e3314a3734557b8daf803c947a8bb7da727a72c
bigint <69431456+bigint@users.noreply.github.com>
2023-11-07 11:30:07 +0530

    fix: build error

--- packages/services/impressions/.eslintrc.js
+++ packages/services/impressions/.eslintrc.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: [require.resolve('@hey/config/eslint/base.js')],
   rules: {
     'import/no-anonymous-default-export': 'off'
-  }
+  },
+  ignorePatterns: ['**/dist/**/*']
 };
