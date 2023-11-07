60cb7497724fb506c05aa007d1f9080f2551d75a
bigint <69431456+bigint@users.noreply.github.com>
2023-11-07 19:31:54 +0530

    fix: getUserLocale

--- apps/web/src/lib/getUserLocale.ts
+++ apps/web/src/lib/getUserLocale.ts
@@ -4,9 +4,11 @@
  * @returns the user's locale or 'en-US' as default
  */
 const getUserLocale = () => {
-  return navigator?.languages?.length
+  const locale = navigator?.languages?.length
     ? navigator.languages[0]
     : navigator.language;
+
+  return locale ? locale.split('-')[0] : 'en';
 };
 
 export default getUserLocale;
