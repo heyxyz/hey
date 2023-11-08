5d61326ffbb4faeff6c6e44307f6a4f33192d57e
bigint <69431456+bigint@users.noreply.github.com>
2023-11-08 13:20:21 +0530

    fix: add temp uuid

--- apps/web/public/impressions.js
+++ apps/web/public/impressions.js
@@ -10,7 +10,10 @@ function sendVisiblePublicationsToServer() {
     fetch(impressionsEndpoint, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ ids: publicationsToSend }),
+      body: JSON.stringify({
+        viewer_id: '550e8400-e29b-41d4-a716-446655440000',
+        ids: publicationsToSend
+      }),
       keepalive: true
     })
       .then(() => {})
