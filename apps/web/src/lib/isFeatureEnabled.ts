e941631cba3ee60cc05a5054ce00f4c08671684e
bigint <69431456+bigint@users.noreply.github.com>
2023-11-09 21:55:23 +0530

    docs: add feature flags back

--- apps/web/src/lib/isFeatureEnabled.ts
+++ apps/web/src/lib/isFeatureEnabled.ts
@@ -3,6 +3,11 @@ import { featureFlags } from '@hey/data/feature-flags';
 
 import getCurrentSessionProfileId from './getCurrentSessionProfileId';
 
+/**
+ * Checks if a feature is enabled for the current user
+ * @param key The feature flag key
+ * @returns Whether the feature is enabled
+ */
 const isFeatureEnabled = (key: FeatureFlag) => {
   const currentSessionProfileId = getCurrentSessionProfileId();
 
