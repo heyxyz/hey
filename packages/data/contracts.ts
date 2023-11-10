bf06669acb8052ea5a873e8dda1d9b88c40d3492
bigint <69431456+bigint@users.noreply.github.com>
2023-11-10 22:10:01 +0530

    feat: add support for publicAct

--- packages/data/contracts.ts
+++ packages/data/contracts.ts
@@ -1,10 +1,12 @@
 export enum MainnetContracts {
   LensHubProxy = '0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d',
+  PublicActProxy = '0x53582b1b7BE71622E7386D736b6baf87749B7a2B',
   DefaultToken = '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270'
 }
 
 export enum TestnetContracts {
   LensHubProxy = '0xC1E77eE73403B8a7478884915aA599932A677870',
+  PublicActProxy = '0xab5607f5447d538fc79bb32364ddecd8f76d7ee8',
   DefaultToken = '0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889'
 }
 
