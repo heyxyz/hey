ac05e02dabf7128df1983ea4afa1a40268596fdb
Nikoemme.eth <94803063+nikomatt69@users.noreply.github.com>
2023-11-12 23:33:29 +0100

    Update Player.tsx

--- apps/web/src/components/Shared/Oembed/Player.tsx
+++ apps/web/src/components/Shared/Oembed/Player.tsx
@@ -7,7 +7,7 @@ interface PlayerProps {
 
 const Player: FC<PlayerProps> = ({ og }) => {
   return (
-    <div className="mt-4 w-5/6 text-sm">
+    <div className="mt-4 w-5/6 text-sm oembed-player">
       <div
         className="oembed-player"
         dangerouslySetInnerHTML={{ __html: og.html as string }}
