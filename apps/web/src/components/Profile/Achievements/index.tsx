f426bb650910a6b353ec7d1a4cd14fa359753872
bigint <69431456+bigint@users.noreply.github.com>
2023-11-12 11:00:37 +0530

    feat: enable profile analytics only for pro

--- apps/web/src/components/Profile/Achievements/index.tsx
+++ apps/web/src/components/Profile/Achievements/index.tsx
@@ -1,4 +1,6 @@
+import { FeatureFlag } from '@hey/data/feature-flags';
 import type { Profile } from '@hey/lens';
+import isFeatureEnabled from '@lib/isFeatureEnabled';
 import { type FC } from 'react';
 
 import ProfileAnalytics from './ProfileAnalytics';
@@ -14,7 +16,9 @@ const Achievements: FC<AchievementsProps> = ({ profile }) => {
     <div className="space-y-4">
       <Streaks profile={profile} />
       <StreaksList profile={profile} />
-      <ProfileAnalytics profile={profile} />
+      {isFeatureEnabled(FeatureFlag.Pro) && (
+        <ProfileAnalytics profile={profile} />
+      )}
     </div>
   );
 };
