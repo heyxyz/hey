b7005e73b8dd91368b05b55389bbec292f8fff4f
bigint <69431456+bigint@users.noreply.github.com>
2023-11-14 22:45:52 +0530

    feat: add tests for getRedstonePrice

---
+++ packages/lib/getRedstonePrice.spec.ts
@@ -0,0 +1,10 @@
+import { describe, expect, test } from 'vitest';
+
+import getRedstonePrice from './getRedstonePrice';
+
+describe('getRedstonePrice', () => {
+  test('should return symbol', async () => {
+    const price = await getRedstonePrice('USDC');
+    expect(price).toBeGreaterThan(0);
+  });
+});
