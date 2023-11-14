0bb99dd9a441215271f0a3dac12c7b084a3f0088
bigint <69431456+bigint@users.noreply.github.com>
2023-11-14 20:18:30 +0530

    chore: remove usage of @tsndr/cloudflare-worker-jwt

--- packages/workers/preferences/src/handlers/updatePreferences.ts
+++ packages/workers/preferences/src/handlers/updatePreferences.ts
@@ -1,7 +1,7 @@
 import { Errors } from '@hey/data/errors';
+import parseJwt from '@hey/lib/parseJwt';
 import response from '@hey/lib/response';
 import createSupabaseClient from '@hey/supabase/createSupabaseClient';
-import jwt from '@tsndr/cloudflare-worker-jwt';
 import { boolean, object, string } from 'zod';
 
 import type { WorkerRequest } from '../types';
@@ -34,7 +34,7 @@ export default async (request: WorkerRequest) => {
   const { isPride, highSignalNotificationFilter } = body as ExtensionRequest;
 
   try {
-    const { payload } = jwt.decode(accessToken as string);
+    const payload = parseJwt(accessToken as string);
     const client = createSupabaseClient(request.env.SUPABASE_KEY);
 
     const clearCache = async () => {
