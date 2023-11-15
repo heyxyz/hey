47a2374a6fa4af0196bee992afa7be268261a454
bigint <69431456+bigint@users.noreply.github.com>
2023-11-15 17:19:06 +0530

    feat: enable gitcoin

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -29,7 +29,7 @@ const nextConfig = {
       {
         source: '/gitcoin',
         destination:
-          'https://explorer.gitcoin.co/#/round/10/0x8de918f0163b2021839a8d84954dd7e8e151326d/0x8de918f0163b2021839a8d84954dd7e8e151326d-2',
+          'https://explorer.gitcoin.co/#/round/424/0xd4cc0dd193c7dc1d665ae244ce12d7fab337a008/0xd4cc0dd193c7dc1d665ae244ce12d7fab337a008-4',
         permanent: true
       }
     ];
