b373de5c990f08c29d457c54a6e95b796d2dd3a5
bigint <69431456+bigint@users.noreply.github.com>
2023-11-15 15:30:24 +0530

    chore: ignore score 0 for staff picks

--- packages/workers/staff-picks/src/handlers/getStaffPicks.ts
+++ packages/workers/staff-picks/src/handlers/getStaffPicks.ts
@@ -14,6 +14,7 @@ export default async (request: WorkerRequest) => {
       const { data } = await client
         .from('staff-picks')
         .select('*')
+        .neq('score', 0)
         .order('score', { ascending: false })
         .limit(10);
 
