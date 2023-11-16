2d8bae2a2def2a77e1389ccf1a0dddb66122d157
bigint <69431456+bigint@users.noreply.github.com>
2023-11-17 00:37:39 +0530

    fix: verified cache

--- apps/api/pages/api/feature/getVerified.ts
+++ apps/api/pages/api/feature/getVerified.ts
@@ -1,40 +1,25 @@
 import { kv } from '@vercel/kv';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import allowCors from 'utils/allowCors';
-import { KV_VERIFIED_PROFILES } from 'utils/constants';
+import { CACHE_AGE } from 'utils/constants';
 import createSupabaseClient from 'utils/createSupabaseClient';
 
 const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const cache = await kv.get(KV_VERIFIED_PROFILES);
+    const client = createSupabaseClient();
+    const { data, error } = await client.from('verified').select('*');
 
-    if (!cache) {
-      const client = createSupabaseClient();
-      const { data, error } = await client.from('verified').select('*');
-
-      if (error) {
-        throw error;
-      }
-
-      const ids = data.map((item) => item.id);
-      await kv.set('list', ids);
-
-      return res
-        .status(200)
-        .setHeader(
-          'Cache-Control',
-          'public, s-maxage=1, stale-while-revalidate=59'
-        )
-        .json({ success: true, result: ids });
+    if (error) {
+      throw error;
     }
+
+    const ids = data.map((item) => item.id);
+    await kv.set('list', ids);
 
     return res
       .status(200)
-      .setHeader(
-        'Cache-Control',
-        'public, s-maxage=1, stale-while-revalidate=59'
-      )
-      .json({ success: true, fromKv: true, result: cache });
+      .setHeader('Cache-Control', CACHE_AGE)
+      .json({ success: true, result: ids });
   } catch (error) {
     throw error;
   }
