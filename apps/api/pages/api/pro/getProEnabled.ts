9ed3e6b66110ddc8d47a78cc3b4f5a797c7e811a
bigint <69431456+bigint@users.noreply.github.com>
2023-11-17 00:44:33 +0530

    chore: add cache to CACHE_AGE

--- apps/api/pages/api/pro/getProEnabled.ts
+++ apps/api/pages/api/pro/getProEnabled.ts
@@ -1,6 +1,7 @@
 import { Errors } from '@hey/data/errors';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import allowCors from 'utils/allowCors';
+import { CACHE_AGE } from 'utils/constants';
 import createSupabaseClient from 'utils/createSupabaseClient';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -18,7 +19,10 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       .eq('profile_id', id)
       .single();
 
-    return res.status(200).json({ success: true, enabled: Boolean(data) });
+    return res
+      .status(200)
+      .setHeader('Cache-Control', CACHE_AGE)
+      .json({ success: true, enabled: Boolean(data) });
   } catch (error) {
     throw error;
   }
