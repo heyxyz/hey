263033f229dbb67e0751ffb60ea2835b9a89af40
bigint <69431456+bigint@users.noreply.github.com>
2023-11-16 23:37:37 +0530

    feat: migrate sts + ipfs worker to next api (#4017)

---
+++ apps/api/pages/api/ipfs/pin.ts
@@ -0,0 +1,32 @@
+import { Errors } from '@hey/data/errors';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import allowCors from 'utils/allowCors';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { cid } = req.query;
+
+  if (!cid) {
+    return res.status(400).json({ success: false, error: Errors.NoBody });
+  }
+
+  try {
+    const ipfsResponse = await fetch(
+      `https://cl-api.ipfs-lens.dev/pins/${cid}`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Basic ${process.env.LENS_IPFS_AUTH_KEY}`,
+          'X-App': 'hey.xyz'
+        }
+      }
+    );
+
+    const json: { cid: string } = await ipfsResponse.json();
+
+    return res.status(200).json({ success: true, cid: json.cid });
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default allowCors(handler);
