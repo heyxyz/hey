9ee5e31784915aec344d932d8691b2ceb60de3dc
bigint <69431456+bigint@users.noreply.github.com>
2023-11-17 00:38:07 +0530

    fix: cache

--- apps/api/pages/api/feature/getFeatureFlags.ts
+++ apps/api/pages/api/feature/getFeatureFlags.ts
@@ -1,7 +1,7 @@
 import { Errors } from '@hey/data/errors';
-import { kv } from '@vercel/kv';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import allowCors from 'utils/allowCors';
+import { CACHE_AGE } from 'utils/constants';
 import createSupabaseClient from 'utils/createSupabaseClient';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -12,33 +12,24 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   }
 
   try {
-    const cacheKey = `features:${id}`;
-    const cache = await kv.get(cacheKey);
+    const client = createSupabaseClient();
+    const { data, error } = await client
+      .from('profile-features')
+      .select('profile_id, enabled, features!inner(key, enabled)')
+      .eq('features.enabled', true)
+      .eq('profile_id', id)
+      .eq('enabled', true);
 
-    if (!cache) {
-      const client = createSupabaseClient();
-      const { data, error } = await client
-        .from('profile-features')
-        .select('profile_id, enabled, features!inner(key, enabled)')
-        .eq('features.enabled', true)
-        .eq('profile_id', id)
-        .eq('enabled', true);
-
-      if (error) {
-        throw error;
-      }
-
-      const features = data.map((feature: any) => feature.features?.key);
-      await kv.set(cacheKey, features);
-
-      return res.status(200).json({ success: true, features });
+    if (error) {
+      throw error;
     }
 
-    return res.status(200).json({
-      success: true,
-      fromKV: true,
-      features: cache
-    });
+    const features = data.map((feature: any) => feature.features?.key);
+
+    return res
+      .status(200)
+      .setHeader('Cache-Control', CACHE_AGE)
+      .json({ success: true, features });
   } catch (error) {
     throw error;
   }
