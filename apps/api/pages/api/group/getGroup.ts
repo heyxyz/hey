3218fcfa8716a86cc502a7dc13d9a93923047dab
bigint <69431456+bigint@users.noreply.github.com>
2023-11-17 12:51:16 +0530

    feat: migrate groups worker to next api (#4022)

---
+++ apps/api/pages/api/group/getGroup.ts
@@ -0,0 +1,31 @@
+import { Errors } from '@hey/data/errors';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import allowCors from 'utils/allowCors';
+import { CACHE_AGE } from 'utils/constants';
+import createSupabaseClient from 'utils/createSupabaseClient';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { slug } = req.query;
+
+  if (!slug) {
+    return res.status(400).json({ success: false, error: Errors.NoBody });
+  }
+
+  try {
+    const client = createSupabaseClient();
+    const { data } = await client
+      .from('groups')
+      .select('*')
+      .eq('slug', slug)
+      .single();
+
+    return res
+      .status(200)
+      .setHeader('Cache-Control', CACHE_AGE)
+      .json({ success: true, result: data });
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default allowCors(handler);
