b00972ae7bfe61a7995c3c911fe3eb050364f587
bigint <69431456+bigint@users.noreply.github.com>
2023-11-17 14:09:21 +0530

    feat: migrate oembed worker to next api

--- docs/ports.md
+++ docs/ports.md
@@ -7,4 +7,3 @@ The following ports are exposed by the repository
 | 4783 | Web app         | http://localhost:4783 | https://hey.xyz           |
 | 4784 | Prerender app   | http://localhost:4784 | https://prerender.hey.xyz |
 | 8083 | Metadata worker | http://localhost:8083 | https://metadata.hey.xyz  |
-| 8086 | Oembed worker   | http://localhost:8086 | https://oembed.hey.xyz    |
--- packages/data/constants.ts
+++ packages/data/constants.ts
@@ -62,9 +62,6 @@ export const PLACEHOLDER_IMAGE = `${STATIC_IMAGES_URL}/placeholder.webp`;
 export const METADATA_WORKER_URL = IS_PRODUCTION
   ? 'https://metadata.hey.xyz'
   : 'http://localhost:8083';
-export const OEMBED_WORKER_URL = IS_PRODUCTION
-  ? 'https://oembed.hey.xyz'
-  : 'http://localhost:8086';
 
 // Tokens / Keys
 export const OPENSEA_KEY = '8b95f9e6d52b42fe8c19ddea847c0f5d';
