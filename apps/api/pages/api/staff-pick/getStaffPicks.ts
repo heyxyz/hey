433d8df8819c347413aa550b9975c5f6769da54c
bigint <69431456+bigint@users.noreply.github.com>
2023-11-17 13:47:16 +0530

    feat: migrate staff-picks worker to next api

---
+++ apps/api/pages/api/staff-pick/getStaffPicks.ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import allowCors from 'utils/allowCors';
+import { CACHE_AGE } from 'utils/constants';
+import createSupabaseClient from 'utils/createSupabaseClient';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const client = createSupabaseClient();
+    const { data } = await client
+      .from('staff-picks')
+      .select('*')
+      .neq('score', 0)
+      .order('score', { ascending: false })
+      .limit(10);
+
+    return res
+      .status(200)
+      .setHeader('Cache-Control', CACHE_AGE)
+      .json({ success: true, result: data });
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default allowCors(handler);
