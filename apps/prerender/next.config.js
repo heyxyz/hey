4dbc179222cf99987d59f468b4e4cae79fbb178c
bigint <69431456+bigint@users.noreply.github.com>
2023-11-18 23:38:43 +0530

    chore: disable source maps

--- apps/prerender/next.config.js
+++ apps/prerender/next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  productionBrowserSourceMaps: true,
   reactStrictMode: false,
   trailingSlash: false,
   transpilePackages: ['data'],
