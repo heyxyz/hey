02f5850109ae030c701f734ad1d00c78124935d1
bigint <69431456+bigint@users.noreply.github.com>
2023-11-19 12:32:57 +0530

    feat: add region and version

--- apps/api/pages/api/index.ts
+++ apps/api/pages/api/index.ts
@@ -4,7 +4,9 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 const handler = (_req: NextApiRequest, res: NextApiResponse) => {
   try {
     res.status(200).json({
-      message: 'Hey API ✨'
+      message: 'Hey API ✨',
+      region: process.env.VERCEL_REGION,
+      version: process.env.VERCEL_GIT_COMMIT_SHA
     });
   } catch {
     res.status(500).json({ success: false });
