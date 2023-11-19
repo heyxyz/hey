bf8152e6995bb1bf01e2d78ca572318b5481870f
bigint <69431456+bigint@users.noreply.github.com>
2023-11-19 22:04:19 +0530

    fix: to use redis url

--- apps/api/utils/createRedisClient.ts
+++ apps/api/utils/createRedisClient.ts
@@ -1,12 +1,7 @@
 import { Redis } from 'ioredis';
 
 const createRedisClient = () => {
-  return new Redis({
-    port: 11416,
-    host: 'viaduct.proxy.rlwy.net',
-    username: 'default',
-    password: process.env.REDIS_PASSWORD
-  });
+  return new Redis(process.env.REDIS_URL!);
 };
 
 export default createRedisClient;
