0f74d1ed677289cbeeb1fca07928a2dafdb1c912
bigint <69431456+bigint@users.noreply.github.com>
2023-11-19 12:27:02 +0530

    feat: add stats test cases

--- apps/api/tests/sts/token.spec.ts
+++ apps/api/tests/sts/token.spec.ts
@@ -1,10 +1,11 @@
 import { TEST_ENDPOINT } from '@utils/constants';
 import axios from 'axios';
+import urlcat from 'urlcat';
 import { describe, expect, test } from 'vitest';
 
 describe('sts/token', () => {
   test('should return token', async () => {
-    const { data } = await axios.get(`${TEST_ENDPOINT}/sts/token`);
+    const { data } = await axios.get(urlcat(TEST_ENDPOINT, '/sts/token'));
     expect(data.success).toBeTruthy();
     expect(data.sessionToken).contains('eyJh');
   });
