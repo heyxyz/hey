53a876521aab7139a85345db2f9f2d70cece0e3a
neo773 <62795688+neo773@users.noreply.github.com>
2023-11-22 22:57:35 +0530

    refactor: extract <head> to component

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -1,3 +1,4 @@
+import PageHead from '@components/Common/Head';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 class HeyDocument extends Document {
@@ -5,53 +6,7 @@ class HeyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta charSet="utf-8" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-
-          {/* Prefetch and Preconnect */}
-          <link rel="preconnect" href="https://hey-assets.b-cdn.net" />
-          <link rel="dns-prefetch" href="https://hey-assets.b-cdn.net" />
-
-          {/* Misc */}
-          <meta name="application-name" content="Hey" />
-          <meta name="mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta
-            name="apple-mobile-web-app-status-bar-style"
-            content="default"
-          />
-          <meta name="apple-mobile-web-app-title" content="Hey" />
-
-          {/* Icons */}
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
-
-          {/* PWA config */}
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta name="mobile-web-app-capable" content="yes" />
-          <meta
-            name="apple-mobile-web-app-status-bar-style"
-            content="default"
-          />
-          <meta name="apple-mobile-web-app-title" content="Hey" />
-          <link rel="icon" href="/images/icons/iconmain-512x512.png" />
-          <meta name="theme-color" content="#ffffff" />
+          <PageHead />
         </Head>
         <body>
           <Main />
