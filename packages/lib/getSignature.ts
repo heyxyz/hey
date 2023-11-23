564a45f710a66a1250d50200252c188c19df77f3
bigint <69431456+bigint@users.noreply.github.com>
2023-11-23 18:04:02 +0530

    chore: use omit-deep

--- packages/lib/getSignature.ts
+++ packages/lib/getSignature.ts
@@ -1,18 +1,4 @@
-/**
- * Returns a new object with the specified property removed.
- *
- * @param obj The object to remove properties from.
- * @param prop The name of the property to remove.
- * @returns A new object with the property removed.
- */
-const omit = <T extends Record<string, any>, K extends keyof T>(
-  obj: T,
-  prop: K
-): Omit<T, K> => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { [prop]: _, ...rest } = obj;
-  return rest;
-};
+import omitDeep from 'omit-deep';
 
 interface TypedData {
   domain: Record<string, any>;
@@ -37,10 +23,10 @@ const getSignature = (
   const { domain, types, value } = typedData;
 
   return {
-    primaryType: Object.keys(omit(types, '__typename'))[0],
-    domain: omit(domain, '__typename'),
-    types: omit(types, '__typename'),
-    message: omit(value, '__typename')
+    primaryType: Object.keys(omitDeep(types, ['__typename']))[0],
+    domain: omitDeep(domain, ['__typename']),
+    types: omitDeep(types, ['__typename']),
+    message: omitDeep(value, ['__typename'])
   };
 };
 
