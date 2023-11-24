c2059c7c623be4e958273ce5e9602db3726aabc1
bigint <69431456+bigint@users.noreply.github.com>
2023-11-24 21:51:09 +0530

    fix: cache typo

--- apps/api/utils/constants.ts
+++ apps/api/utils/constants.ts
@@ -8,10 +8,10 @@ export const GARDENER_FEATURE_ID = '0a441129-182a-4a3f-83cf-a13c5ad8282b';
 export const GARDENER_MODE_FEATURE_ID = '9f66a465-e1d7-4123-b329-ddd14fd85510';
 
 // Cache
-// Cache for 1 second, stale for 30 days
-export const SWR_CACHE_AGE_1_SEC_30_DAYS =
+// Cache for 1 minute, stale for 30 days
+export const SWR_CACHE_AGE_1_MIN_30_DAYS =
   'public, s-maxage=1, stale-while-revalidate=2592000';
-// Cache for 10 minute, stale for 30 days
+// Cache for 10 minutes, stale for 30 days
 export const SWR_CACHE_AGE_10_MINS_30_DAYS =
   'public, s-maxage=600, stale-while-revalidate=2592000';
 // Cache for 30 days
