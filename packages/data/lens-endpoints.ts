fc0cf88f8658cc67824ac21711f9f8b70ca5f2a8
bigint <69431456+bigint@users.noreply.github.com>
2023-11-24 13:23:51 +0530

    chore: revert to devnet

--- packages/data/lens-endpoints.ts
+++ packages/data/lens-endpoints.ts
@@ -1,6 +1,6 @@
 enum LensEndpoint {
   Mainnet = 'https://api-v2.lens.dev',
-  Testnet = 'https://api-v2-mumbai-live.lens.dev',
+  Testnet = 'https://api-v2-mumbai.lens.dev',
   Staging = 'https://api-mumbai.lens-v2.crtlkey.com'
 }
 
