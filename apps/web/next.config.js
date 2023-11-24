0df81a8bbe103585d6c138f1963916c03e5be360
bigint <69431456+bigint@users.noreply.github.com>
2023-11-24 14:20:34 +0530

    chore: test deploy on amplify

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,44 +1,9 @@
-const headers = [{ key: 'Cache-Control', value: 'public, max-age=3600' }];
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['data'],
   reactStrictMode: false,
   experimental: { scrollRestoration: true },
-  images: { unoptimized: true },
-  async rewrites() {
-    return [
-      {
-        source: '/sitemaps/:match*',
-        destination: 'https://api.hey.xyz/sitemap/:match*'
-      }
-    ];
-  },
-  async redirects() {
-    return [
-      {
-        source: '/discord',
-        destination: 'https://discord.com/invite/B8eKhSSUwX',
-        permanent: true
-      },
-      {
-        source: '/donate',
-        destination: 'https://giveth.io/project/hey?utm_source=hey',
-        permanent: true
-      },
-      {
-        source: '/u/lens/:username*',
-        destination: '/u/:username*',
-        permanent: true
-      },
-      {
-        source: '/gitcoin',
-        destination:
-          'https://explorer.gitcoin.co/#/round/424/0xd4cc0dd193c7dc1d665ae244ce12d7fab337a008/0xd4cc0dd193c7dc1d665ae244ce12d7fab337a008-4',
-        permanent: true
-      }
-    ];
-  }
+  images: { unoptimized: true }
 };
 
 module.exports = nextConfig;
