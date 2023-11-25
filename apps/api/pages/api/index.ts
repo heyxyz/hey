864aac22ec4a64a0c4733cc6b04858a3d828ad9b
bigint <69431456+bigint@users.noreply.github.com>
2023-11-25 09:13:14 +0530

    chore: remove vercel vars

--- apps/api/pages/api/index.ts
+++ apps/api/pages/api/index.ts
@@ -4,9 +4,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 const handler = (_req: NextApiRequest, res: NextApiResponse) => {
   try {
     res.status(200).json({
-      message: 'Hey API ✨',
-      region: process.env.VERCEL_REGION,
-      version: process.env.VERCEL_GIT_COMMIT_SHA
+      message: 'Hey API ✨'
     });
   } catch {
     res.status(500).json({ success: false });
