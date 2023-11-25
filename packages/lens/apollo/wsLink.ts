81784fef021c15f61f9571c65ffb6058d302d716
bigint <69431456+bigint@users.noreply.github.com>
2023-11-25 20:23:58 +0530

    feat: add lensApiEndpoint on getEnvConfig

--- packages/lens/apollo/wsLink.ts
+++ packages/lens/apollo/wsLink.ts
@@ -1,10 +1,10 @@
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
-import { API_URL } from '@hey/data/constants';
+import { LENS_API_URL } from '@hey/data/constants';
 import { createClient } from 'graphql-ws';
 
 const wsLink = new GraphQLWsLink(
   createClient({
-    url: API_URL.replace('http', 'ws'),
+    url: LENS_API_URL.replace('http', 'ws'),
     keepAlive: 5000
   })
 );
