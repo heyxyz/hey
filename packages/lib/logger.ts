975b60e73b11ae02f7a954ed57a487d60515588a
bigint <69431456+bigint@users.noreply.github.com>
2023-11-25 21:43:17 +0530

    chore: update logger

--- packages/lib/logger.ts
+++ packages/lib/logger.ts
@@ -1,8 +1,19 @@
-import pino from 'pino';
-import pretty from 'pino-pretty';
+class Logger {
+  private formatMessage(level: string, message: string, colorCode: string) {
+    return `\x1b[${colorCode}m${level}\x1b[0m: ${message}`;
+  }
 
-const logger = pino(
-  pretty({ colorize: true, levelFirst: true, translateTime: 'SYS:standard' })
-);
+  info(message: string) {
+    const formattedMessage = this.formatMessage('INFO', message, '1;34');
+    console.log(formattedMessage);
+  }
+
+  error(message: string) {
+    const formattedMessage = this.formatMessage('ERROR', message, '1;31');
+    console.error(formattedMessage);
+  }
+}
+
+const logger = new Logger();
 
 export default logger;
