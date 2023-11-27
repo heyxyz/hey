6cacb62965f8fdf4391a3105bf03daa365bbd5ca
bigint <69431456+bigint@users.noreply.github.com>
2023-11-27 09:48:45 +0530

    fix: add cors

--- apps/apiv2/src/server.ts
+++ apps/apiv2/src/server.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import * as dotenv from 'dotenv';
 import express from 'express';
 import { router } from 'express-file-routing';
@@ -5,7 +6,7 @@ import ViteExpress from 'vite-express';
 
 dotenv.config({ override: true });
 
-const app = express().use(express.json()).use('/', router());
+const app = express().use(cors()).use(express.json()).use('/', router());
 
 ViteExpress.listen(app, 4785, () =>
   console.log('Server is listening on port 4785...')
