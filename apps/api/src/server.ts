73621a3beba7b09ba4d0e43ae164e43c79ae9e29
bigint <69431456+bigint@users.noreply.github.com>
2023-11-27 22:34:10 +0530

    chore: update deps

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,3 +1,4 @@
+import compression from 'compression';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
 import express from 'express';
@@ -6,8 +7,16 @@ import ViteExpress from 'vite-express';
 
 dotenv.config({ override: true });
 
-const app = express().use(cors()).use(express.json()).use('/', router());
+const app = express();
 
-ViteExpress.listen(app, 4785, () =>
-  console.log('Server is listening on port 4785...')
-);
+app.use(express.json({ limit: '1mb' }));
+app.use(cors());
+app.use(compression());
+
+(async () => {
+  app.use('/', await router());
+
+  ViteExpress.listen(app, 4785, () =>
+    console.log('Server is listening on port 4785...')
+  );
+})();
