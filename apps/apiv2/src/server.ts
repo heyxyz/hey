3443186a023ea3fc82c319f8d52961e75a7d9071
bigint <69431456+bigint@users.noreply.github.com>
2023-11-27 09:16:47 +0530

    chore: move to vite

--- apps/apiv2/src/server.ts
+++ apps/apiv2/src/server.ts
@@ -1,13 +1,12 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
 import { router } from 'express-file-routing';
-import http from 'http';
+import ViteExpress from 'vite-express';
 
 dotenv.config({ override: true });
 
 const app = express().use(express.json()).use('/', router());
 
-const httpPort = 4785;
-http.createServer(app).listen(httpPort, () => {
-  console.log(`Server listening on port ${httpPort}`);
-});
+ViteExpress.listen(app, 4785, () =>
+  console.log('Server is listening on port 4785...')
+);
