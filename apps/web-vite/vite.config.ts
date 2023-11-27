1c75c53d133ac9d7ce93a1c080f9f5fe3821d6de
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-11-27 16:44:23 +0530

    run prettier

--- apps/web-vite/vite.config.ts
+++ apps/web-vite/vite.config.ts
@@ -1,7 +1,7 @@
-import path from 'path';
-import * as dotenv from 'dotenv';
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import * as dotenv from 'dotenv';
+import path from 'path';
+import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 dotenv.config();
