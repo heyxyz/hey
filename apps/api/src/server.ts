46fcd5b30c0f54c7ce8188d7defa46db70bd083e
bigint <69431456+bigint@users.noreply.github.com>
2023-11-27 22:46:22 +0530

    chore: update dependencies (#4076)

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,13 +1,31 @@
+import compression from 'compression';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
 import express from 'express';
 import { router } from 'express-file-routing';
+import { rateLimit } from 'express-rate-limit';
 import ViteExpress from 'vite-express';
 
 dotenv.config({ override: true });
 
-const app = express().use(cors()).use(express.json()).use('/', router());
+const app = express();
 
-ViteExpress.listen(app, 4785, () =>
-  console.log('Server is listening on port 4785...')
-);
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  limit: 200, // Limit each IP to 200 requests per `window`
+  standardHeaders: 'draft-7',
+  legacyHeaders: false
+});
+
+app.use(express.json({ limit: '1mb' }));
+app.use(cors());
+app.use(compression());
+app.use(limiter);
+
+(async () => {
+  app.use('/', await router());
+
+  ViteExpress.listen(app, 4785, () =>
+    console.log('Server is listening on port 4785...')
+  );
+})();
