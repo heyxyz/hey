84cad8b6fae7105415a2c9986b03fdc8970e53e2
bigint <69431456+bigint@users.noreply.github.com>
2023-11-28 22:19:59 +0530

    fix: use addressInvited instead of by

--- packages/lens/codegen.ts
+++ packages/lens/codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: 'https://api-v2-mumbai.lens.dev',
+  schema: 'https://api-v2-mumbai-live.lens.dev',
   customFetch: 'node-fetch',
   documents: './documents/**/*.graphql',
   generates: {
