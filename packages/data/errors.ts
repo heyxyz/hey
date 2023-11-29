db53ccfbeb3dca399fb3f491685adc4914e97a47
bigint <69431456+bigint@users.noreply.github.com>
2023-11-29 16:18:55 +0530

    feat: add allowOwnerAndStaffOnly middleware

--- packages/data/errors.ts
+++ packages/data/errors.ts
@@ -3,6 +3,7 @@ export enum Errors {
   SignWallet = 'Please sign in your wallet.',
   Limit500 = 'Limit must be less than 500!',
   InvalidAccesstoken = 'Invalid access token!',
+  NotAllowed = 'Not allowed!',
   NotStaff = 'You are not staff!',
   NotGarnder = 'You are not gardener!',
   NoBody = 'No body provided!',
