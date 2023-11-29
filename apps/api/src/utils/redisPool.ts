158d189755337227fdefbb3432cafb78bccda2e6
bigint <69431456+bigint@users.noreply.github.com>
2023-11-29 12:41:50 +0530

    fix: set redis family (#4081)
    * feat: use redis pool
    
    * chore: add redispool
    
    * fix: set family

--- apps/api/src/utils/redisPool.ts
+++ apps/api/src/utils/redisPool.ts
@@ -2,9 +2,11 @@ import { IORedisPool, IORedisPoolOptions } from 'ts-ioredis-pool';
 
 const ioRedisPoolOpts = IORedisPoolOptions.fromUrl(
   process.env.REDIS_URL as string
-).withPoolOptions({
-  min: 2,
-  max: 20
-});
+)
+  .withIORedisOptions({ family: 0 })
+  .withPoolOptions({
+    min: 2,
+    max: 20
+  });
 
 export default new IORedisPool(ioRedisPoolOpts);
