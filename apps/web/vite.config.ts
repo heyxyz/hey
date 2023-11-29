6f5448d7aa84a987a05b8c9dcef8e134e0f9c45e
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-11-30 00:35:43 +0530

    ...

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -6,9 +6,9 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 dotenv.config();
 
-const randomChunkNames = new Array(10)
-  .fill(0)
-  .map((i) => (Math.random() + 1).toString(36).substring(7));
+// const randomChunkNames = new Array(10)
+//   .fill(0)
+//   .map((i) => (Math.random() + 1).toString(36).substring(7));
 
 export default defineConfig(() => {
   return {
@@ -26,32 +26,32 @@ export default defineConfig(() => {
       open: true,
       port: 4783,
       host: '0.0.0.0'
-    },
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks: (id) => {
-            if (id.includes('date-fns')) {
-              return randomChunkNames[0];
-            }
-            if (id.includes('@aws-sdk')) {
-              return randomChunkNames[1];
-            }
-            if (id.includes('livepeer') || id.includes('hls')) {
-              return randomChunkNames[2];
-            }
-            if (id.includes('bn.js')) {
-              return randomChunkNames[3];
-            }
-            if (id.includes('zod')) {
-              return randomChunkNames[4];
-            }
-            if (id.includes('plyr')) {
-              return randomChunkNames[5];
-            }
-          }
-        }
-      }
     }
+    // build: {
+    //   rollupOptions: {
+    //     output: {
+    //       manualChunks: (id) => {
+    // if (id.includes('date-fns')) {
+    //   return randomChunkNames[0];
+    // }
+    // if (id.includes('@aws-sdk')) {
+    //   return randomChunkNames[1];
+    // }
+    // if (id.includes('livepeer') || id.includes('hls')) {
+    //   return randomChunkNames[2];
+    // }
+    // if (id.includes('bn.js')) {
+    //   return randomChunkNames[3];
+    // }
+    // if (id.includes('zod')) {
+    //   return randomChunkNames[4];
+    // }
+    // if (id.includes('plyr')) {
+    //   return randomChunkNames[5];
+    // }
+    //     }
+    //   }
+    // }
+    // }
   };
 });
