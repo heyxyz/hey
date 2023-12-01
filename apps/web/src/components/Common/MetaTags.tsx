e58c55506f8b566be955ab023c25f69f68f86ec9
bigint <69431456+bigint@users.noreply.github.com>
2023-12-01 19:30:55 +0530

    feat: update meta tags

--- apps/web/src/components/Common/MetaTags.tsx
+++ apps/web/src/components/Common/MetaTags.tsx
@@ -44,6 +44,24 @@ const MetaTags: FC<MetaTagsProps> = ({
         href="/opensearch.xml"
         title={APP_NAME}
       />
+
+      {/* Prefetch and Preconnect */}
+      <link rel="preconnect" href="https://hey-assets.b-cdn.net" />
+      <link rel="dns-prefetch" href="https://hey-assets.b-cdn.net" />
+
+      {/* Icons */}
+      <link rel="apple-touch-icon" sizes="180x180" href="/ati.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/16x16.png" />
+
+      {/* PWA config */}
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="application-name" content="Hey" />
+      <meta name="apple-mobile-web-app-title" content="Hey" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
   );
 };
