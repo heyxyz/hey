99a3a965ec40f9fda95d82e9ac81743d8eb74569
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-12-03 04:00:55 +0530

    platform

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -5,7 +5,9 @@ export function middleware(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
   const userAgent = requestHeaders.get('user-agent');
   if (
-    userAgent?.match(/twitterbot|linkedinbot|whatsapp|slackbot|telegrambot/i)
+    userAgent?.match(
+      /twitterbot|linkedinbot|whatsapp|slackbot|telegrambot|discordbot|facebookbot/i
+    )
   ) {
     // return NextResponse.rewrite(new URL(url.pathname, 'https://my-og-service.railway.app'));
     return NextResponse.rewrite(new URL('/', 'https://rishi.app'));
