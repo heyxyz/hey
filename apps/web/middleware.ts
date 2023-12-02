49a4ef8c82837a1df77d01bcbfcf91922f11c86d
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-12-03 02:53:44 +0530

    middleware

---
+++ apps/web/middleware.ts
@@ -0,0 +1,17 @@
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  // const url = new URL(request.url);
+  const userAgent = request.headers.get('user-agent');
+  if (
+    userAgent?.match(/twitterbot|linkedinbot|whatsapp|slackbot|telegrambot/i)
+  ) {
+    // Use url.pathname here
+    return NextResponse.rewrite(new URL('/', 'https://rishi.app'));
+  }
+}
+
+export const config = {
+  matcher: ['/posts/:path', '/u/:path', '/u/lens/:path']
+};
