ef16f3e0cfd5eeb803788f00d86d0fb94b8dcc11
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-12-03 03:20:51 +0530

    ogimage

---
+++ apps/web/middleware.ts
@@ -0,0 +1,17 @@
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const userAgent = request.headers.get('user-agent');
+  if (
+    userAgent?.match(/twitterbot|linkedinbot|whatsapp|slackbot|telegrambot/i)
+  ) {
+    // return NextResponse.rewrite(new URL(url.pathname, 'https://my-og-service.railway.app'));
+    return NextResponse.rewrite(new URL('/', 'https://rishi.app'));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/posts/:path', '/u/:path', '/u/lens/:path']
+};
