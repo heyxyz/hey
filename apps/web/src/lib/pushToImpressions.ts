746311e738f2899870497c359e973cace5526cab
bigint <69431456+bigint@users.noreply.github.com>
2023-12-04 21:46:29 +0530

    Add docs to pushToImpressions function

--- apps/web/src/lib/pushToImpressions.ts
+++ apps/web/src/lib/pushToImpressions.ts
@@ -3,6 +3,11 @@ import { hydrateLeafwatchAnonymousId } from 'src/store/persisted/useLeafwatchSto
 
 import getCurrentSession from './getCurrentSession';
 
+/**
+ * Push publication to impressions queue
+ * @param id Publication ID
+ * @returns void
+ */
 const pushToImpressions = (id: string): void => {
   const anonymousId = hydrateLeafwatchAnonymousId();
   const { id: sessionProfileId } = getCurrentSession();
