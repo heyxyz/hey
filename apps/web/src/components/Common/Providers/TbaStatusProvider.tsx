de5985857aff93776225bbb9bd4cec9b9f20d42f
bigint <69431456+bigint@users.noreply.github.com>
2023-12-05 22:55:40 +0530

    Refactor TbaStatusProvider and add TbaBadge component

--- apps/web/src/components/Common/Providers/TbaStatusProvider.tsx
+++ apps/web/src/components/Common/Providers/TbaStatusProvider.tsx
@@ -1,8 +1,7 @@
 import type { FC } from 'react';
 
-import { HEY_API_URL } from '@hey/data/constants';
+import getTbaStatus from '@hey/lib/api/getTbaStatus';
 import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
 import useProfileStore from 'src/store/persisted/useProfileStore';
 import { useTbaStatusStore } from 'src/store/persisted/useTbaStatusStore';
 
@@ -10,25 +9,12 @@ const TbaStatusProvider: FC = () => {
   const currentProfile = useProfileStore((state) => state.currentProfile);
   const setIsTba = useTbaStatusStore((state) => state.setIsTba);
 
-  // Fetch TBA status
-  const fetchTbaStatus = async () => {
-    try {
-      if (Boolean(currentProfile?.ownedBy.address)) {
-        const response = await axios.get(`${HEY_API_URL}/tba/deployed`, {
-          params: { address: currentProfile?.ownedBy.address }
-        });
-        const { data } = response;
-        setIsTba(data.deployed);
-      }
-      return true;
-    } catch {
-      return false;
-    }
-  };
-
   useQuery({
-    queryFn: fetchTbaStatus,
-    queryKey: ['fetchTbaStatus', currentProfile?.ownedBy.address]
+    queryFn: () =>
+      getTbaStatus(currentProfile?.ownedBy.address, (deployed) =>
+        setIsTba(deployed)
+      ),
+    queryKey: ['getTbaStatus', currentProfile?.ownedBy.address]
   });
 
   return null;
