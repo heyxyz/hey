36a1c0b26cff77717484d1f3095758f2710b18ea
bigint <69431456+bigint@users.noreply.github.com>
2023-12-06 22:52:13 +0530

    feat: add support page (#4123)
    * feat: add support page
    
    * fix: values

--- apps/web/src/components/Shared/Navbar/NavItems/Support.tsx
+++ apps/web/src/components/Shared/Navbar/NavItems/Support.tsx
@@ -6,35 +6,29 @@ import {
 } from '@heroicons/react/24/outline';
 import cn from '@hey/ui/cn';
 import Link from 'next/link';
-import urlcat from 'urlcat';
 
-interface ReportBugProps {
+interface SupportProps {
   className?: string;
   onClick?: () => void;
 }
 
-const ReportBug: FC<ReportBugProps> = ({ className = '', onClick }) => {
+const Support: FC<SupportProps> = ({ className = '', onClick }) => {
   return (
     <Link
       className={cn(
         'flex w-full items-center justify-between px-2 py-1.5 text-sm text-gray-700 dark:text-gray-200',
         className
       )}
-      href={urlcat('https://github.com/heyxyz/hey/issues/new', {
-        assignees: 'bigint',
-        labels: 'needs review',
-        template: 'bug_report.yml'
-      })}
+      href="/support"
       onClick={onClick}
-      target="_blank"
     >
       <div className="flex items-center space-x-1.5">
         <HandRaisedIcon className="h-4 w-4" />
-        <div>Report a bug</div>
+        <div>Support</div>
       </div>
       <ArrowTopRightOnSquareIcon className="h-4 w-4 md:hidden" />
     </Link>
   );
 };
 
-export default ReportBug;
+export default Support;
