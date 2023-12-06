0fe200492594a19f8f388439c318a91a179958c6
bigint <69431456+bigint@users.noreply.github.com>
2023-12-07 00:34:25 +0530

    Add Select component to List and SuperFollow components (#4126)

--- packages/ui/src/Select.tsx
+++ packages/ui/src/Select.tsx
@@ -2,29 +2,39 @@ import type { ComponentProps } from 'react';
 
 import { forwardRef, useId } from 'react';
 
+import cn from '../cn';
+
 interface SelectProps extends ComponentProps<'select'> {
+  className?: string;
   label?: string;
-  options: {
+  options?: {
+    disabled?: boolean;
     label: string;
+    selected?: boolean;
     value: number | string;
   }[];
 }
 
 export const Select = forwardRef<HTMLSelectElement, SelectProps>(
-  function TextArea({ label, options, ...rest }, ref) {
+  function Select({ className = '', label, options, ...rest }, ref) {
     const id = useId();
 
     return (
       <label htmlFor={id}>
         {label ? <div className="label">{label}</div> : null}
         <select
-          className="focus:border-brand-500 focus:ring-brand-400 w-full rounded-xl border border-gray-300 bg-white outline-none dark:border-gray-700 dark:bg-gray-800"
+          className={cn(
+            'focus:border-brand-500 focus:ring-brand-400 w-full rounded-xl border border-gray-300 bg-white outline-none dark:border-gray-700 dark:bg-gray-800',
+            className
+          )}
           id={id}
           ref={ref}
           {...rest}
         >
-          {options.map(({ label, value }) => (
-            <option value={value}>{label}</option>
+          {options?.map(({ disabled, label, selected, value }) => (
+            <option disabled={disabled} selected={selected} value={value}>
+              {label}
+            </option>
           ))}
         </select>
       </label>
