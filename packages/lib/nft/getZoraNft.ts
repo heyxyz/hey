8a5f8f5957e3e6f39fb2594b32d464addd9a9038
bigint <69431456+bigint@users.noreply.github.com>
2023-12-08 12:16:56 +0530

    fix: zora nft rendering (#4145)

--- packages/lib/nft/getZoraNft.ts
+++ packages/lib/nft/getZoraNft.ts
@@ -5,7 +5,7 @@ import { REWARDS_ADDRESS } from '@hey/data/constants';
 import getZoraChainIsMainnet from './getZoraChainIsMainnet';
 
 export const regex =
-  /https:\/\/(?:testnet\.)?zora\.co\/collect\/(eth|oeth|base|zora|gor|ogor|basegor|zgor):(0x[\dA-Fa-f]{40})((?:\/(\d+))?|$)/;
+  /https:\/\/(?:testnet\.)?zora\.co\/collect\/(eth|oeth|base|zora|gor|ogor|basegor|zgor):(0x[\dA-Fa-f]{40})\/?([\w-]+)?/;
 
 /**
  * Get Zora NFT metadata from a URL
@@ -17,7 +17,7 @@ const getZoraNFT = (url: string): BasicNftMetadata | null => {
   if (regex.test(url) && matches && matches.length >= 3) {
     const chain = matches[1];
     const address = matches[2];
-    const token = matches[4];
+    const token = matches[3];
 
     const network = getZoraChainIsMainnet(chain) ? '' : 'testnet.';
     const mintLink = `https://${network}zora.co/collect/${chain}:${address}/${
