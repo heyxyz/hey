b01a6096d9e956429c6946218f6a6417703a5b01
bigint <69431456+bigint@users.noreply.github.com>
2023-12-11 21:50:43 +0530

    feat: add support link to error message component (#4180)

--- packages/ui/src/ErrorMessage.tsx
+++ packages/ui/src/ErrorMessage.tsx
@@ -1,5 +1,10 @@
 import type { FC } from 'react';
 
+import { LifebuoyIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+
+import cn from '../cn';
+
 interface ErrorMessageProps {
   className?: string;
   error?: Error;
@@ -17,16 +22,25 @@ export const ErrorMessage: FC<ErrorMessageProps> = ({
 
   return (
     <div
-      className={`space-y-1 rounded-xl border-2 border-red-500/50 bg-red-50 p-4 dark:bg-red-900/10 ${className}`}
+      className={cn(
+        'space-y-1 rounded-xl border-2 border-red-500/50 bg-red-50 p-4 text-sm dark:bg-red-900/10',
+        className
+      )}
     >
-      {title ? (
-        <h3 className="text-sm font-medium text-red-800 dark:text-red-200">
-          {title}
-        </h3>
-      ) : null}
-      <div className="text-sm text-red-700 dark:text-red-200">
-        {error?.message}
+      <div className="flex flex-wrap items-center gap-2">
+        {title ? (
+          <h3 className="font-medium text-red-800 dark:text-red-200">
+            {title}
+          </h3>
+        ) : null}
+        <div className="flex items-center space-x-1 text-xs text-red-800">
+          <LifebuoyIcon className="h-4 w-4" />
+          <Link className="font-bold underline" href="/support">
+            Contact Support
+          </Link>
+        </div>
       </div>
+      <div className="text-red-700 dark:text-red-200">{error?.message}</div>
     </div>
   );
 };
