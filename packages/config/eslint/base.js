2615f8a12a2ae915874110567f168a89150e96d7
bigint <69431456+bigint@users.noreply.github.com>
2023-12-12 11:57:39 +0530

    fix: search result (#4186)

--- packages/config/eslint/base.js
+++ packages/config/eslint/base.js
@@ -13,12 +13,12 @@ module.exports = {
   rules: {
     curly: 'error',
     'prefer-destructuring': ['error', { VariableDeclarator: { object: true } }],
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/consistent-type-imports': 'error',
     'no-use-before-define': 'error',
     'no-unexpected-multiline': 'error',
-    'unicorn/better-regex': 'error'
+    'unicorn/better-regex': 'error',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/consistent-type-imports': 'error'
   },
   ignorePatterns: ['generated.ts', 'node_modules']
 };
