4742d4a7d5bf1f553d4c10c9449015a785ab9a10
bigint <69431456+bigint@users.noreply.github.com>
2023-12-12 11:09:46 +0530

    fix: add more rules

--- packages/config/eslint/base.js
+++ packages/config/eslint/base.js
@@ -14,17 +14,12 @@ module.exports = {
     curly: 'error',
     'import/no-duplicates': ['error', { considerQueryString: true }],
     'prefer-destructuring': ['error', { VariableDeclarator: { object: true } }],
-    '@typescript-eslint/ban-ts-comment': 'off', // turn warn
+    '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/consistent-type-imports': 'error',
     'no-use-before-define': 'error',
     'no-unexpected-multiline': 'error',
-    'unicorn/better-regex': 'error',
-    'unicorn/catch-error-name': 'error',
-    'unicorn/no-array-for-each': 'error',
-    'unicorn/no-for-loop': 'error',
-    'unicorn/no-useless-undefined': 'error',
-    'unicorn/prefer-array-find': 'error'
+    'unicorn/better-regex': 'error'
   },
   ignorePatterns: ['generated.ts', 'node_modules']
 };
