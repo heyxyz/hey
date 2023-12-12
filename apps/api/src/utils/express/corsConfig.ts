dd21a2aacdc5b6c8bf9d045407a8aae17b7880f5
Ameer <ameer.mantiqh@gmail.com>
2023-12-12 20:19:56 +0530

    cors validation for specific endpoints

--- apps/api/src/utils/express/corsConfig.ts
+++ apps/api/src/utils/express/corsConfig.ts
@@ -1,16 +1,31 @@
 import type cors from 'cors';
 
+class CorsError extends Error {
+  statusCode: number;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
 const corsConfig: cors.CorsOptions = {
   origin: (origin, callback) => {
     if (
       !origin ||
-      origin.includes('hey.xyz') ||
-      origin.includes('heyxyz.vercel.app') ||
-      origin.startsWith('http://localhost')
+      /^https:\/\/(\w+\.)?hey\.xyz$/.test(origin) || // https://hey.xyz and https://*.hey.xyz
+      /^https:\/\/\w+-heyxyz\.vercel\.app$/.test(origin) || // https://*-heyxyz.vercel.app
+      origin === 'http://localhost:4783'
     ) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      const errorMessage = 'Not allowed by CORS. Check the allowed origins: https://hey.xyz, https://*.hey.xyz, https://*-heyxyz.vercel.app, http://localhost:4783';
+
+      // Using the custom CorsError class
+      const errorResponse = new CorsError(errorMessage, 403);
+
+      callback(errorResponse);
+
     }
   }
 };
