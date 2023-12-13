9385ae2b25438c8c675d4f7401cf28e823d75179
bigint <69431456+bigint@users.noreply.github.com>
2023-12-13 23:01:37 +0530

    feat: add paid actions feed

--- packages/data/tracking.ts
+++ packages/data/tracking.ts
@@ -101,7 +101,8 @@ export const HOME = {
   },
   SELECT_USER_FEED: 'Select user feed',
   SWITCH_FOLLOWING_FEED: 'Switch to following feed',
-  SWITCH_HIGHLIGHTS_FEED: 'Switch to highlights feed'
+  SWITCH_HIGHLIGHTS_FEED: 'Switch to highlights feed',
+  SWITCH_PAID_ACTIONS_FEED: 'Switch to paid actions feed'
 };
 
 export const EXPLORE = {
