621f28cb01455a49df64ce8ab83bc04ce91fa4f9
bigint <69431456+bigint@users.noreply.github.com>
2023-12-14 00:01:32 +0530

    feat: add PaidActionsShimmer

---
+++ apps/web/src/components/Shared/Shimmer/PaidActionsShimmer.tsx
@@ -0,0 +1,41 @@
+import type { FC } from 'react';
+
+import { Card } from '@hey/ui';
+
+import PublicationShimmer from './PublicationShimmer';
+import SmallUserProfileShimmer from './SmallUserProfileShimmer';
+import UserProfileShimmer from './UserProfileShimmer';
+
+const PaidActionsShimmer: FC = () => {
+  return (
+    <div className="space-y-5">
+      <Card>
+        <div className="flex items-center space-x-2 p-5">
+          <div className="shimmer h-3 w-3/12 rounded-full" />
+          <SmallUserProfileShimmer smallAvatar />
+        </div>
+        <div className="divider" />
+        <PublicationShimmer />
+      </Card>
+      <Card>
+        <div className="flex items-center space-x-2 p-5">
+          <div className="shimmer h-3 w-3/12 rounded-full" />
+          <SmallUserProfileShimmer smallAvatar />
+        </div>
+        <div className="divider" />
+        <div className="p-5">
+          <UserProfileShimmer isBig showFollow />
+        </div>
+      </Card>
+      <Card>
+        <div className="flex items-center space-x-2 p-5">
+          <div className="shimmer h-3 w-3/12 rounded-full" />
+          <SmallUserProfileShimmer smallAvatar />
+        </div>
+        <PublicationShimmer />
+      </Card>
+    </div>
+  );
+};
+
+export default PaidActionsShimmer;
