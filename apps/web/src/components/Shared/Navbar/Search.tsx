b1e5598ff6d0c3c1d1219a78b30f1d33f1ea507d
bigint <69431456+bigint@users.noreply.github.com>
2023-12-13 13:54:32 +0530

    fix: lint errors

--- apps/web/src/components/Shared/Navbar/Search.tsx
+++ apps/web/src/components/Shared/Navbar/Search.tsx
@@ -63,6 +63,7 @@ const Search: FC<SearchProps> = ({
 
       searchUsers({ variables: { request } });
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [debouncedSearchText]);
 
   const searchResult = searchUsersData?.searchProfiles;
