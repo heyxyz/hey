0b4d8c2da6748bb272f4a0467d7842aa4dfd05ee
bigint <69431456+bigint@users.noreply.github.com>
2023-12-13 21:16:31 +0530

    feat: show license on audio and video posts (#4222)
    * feat: show license on video posts
    
    * feat: add audio license
    
    * feat: allow to all lens member

---
+++ apps/web/src/components/Publication/Metadata.tsx
@@ -0,0 +1,47 @@
+import type { AnyPublication } from '@hey/lens';
+import type { FC } from 'react';
+
+import { ScaleIcon } from '@heroicons/react/24/outline';
+import { FeatureFlag } from '@hey/data/feature-flags';
+import getPublicationData from '@hey/lib/getPublicationData';
+import { isMirrorPublication } from '@hey/lib/publicationHelpers';
+import { Card } from '@hey/ui';
+import getLicense from '@lib/getLicence';
+import isFeatureEnabled from '@lib/isFeatureEnabled';
+import { memo } from 'react';
+
+interface MetadataProps {
+  publication: AnyPublication;
+}
+
+const Metadata: FC<MetadataProps> = ({ publication }) => {
+  const targetPublication = isMirrorPublication(publication)
+    ? publication.mirrorOn
+    : publication;
+
+  const filteredAsset = getPublicationData(targetPublication.metadata)?.asset;
+
+  if (!isFeatureEnabled(FeatureFlag.LensMember)) {
+    return null;
+  }
+
+  if (!filteredAsset?.license) {
+    return null;
+  }
+
+  return (
+    <Card
+      className="ld-text-gray-500 mt-3 space-y-2 px-3 py-2 text-sm"
+      forceRounded
+    >
+      <div className="flex items-center space-x-2">
+        <ScaleIcon className="text-brand-500 h-4 w-4 min-w-max" />
+        <div>
+          Licenced under <b>{getLicense(filteredAsset.license)}</b>
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default memo(Metadata);
