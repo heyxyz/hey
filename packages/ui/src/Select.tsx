2b0e79bf33137b25a682ab08c9bedbbc8ea5119c
RutamBhagat <rutambhagat@gmail.com>
2023-12-18 23:45:29 +0530

    Enhancement: convert icons from svg tags to @heroicons/react (#4246)

--- packages/ui/src/Select.tsx
+++ packages/ui/src/Select.tsx
@@ -1,6 +1,7 @@
 import type { ChangeEvent, ComponentProps, ReactNode } from 'react';
 
 import { Listbox, Transition } from '@headlessui/react';
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/24/outline';
 import { forwardRef, Fragment, useState } from 'react';
 
 import cn from '../cn';
@@ -62,18 +63,10 @@ export const Select = forwardRef<HTMLSelectElement, SelectProps>(
                   {selected?.title || selected?.label}
                 </span>
                 <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
-                  <svg
+                  <ChevronUpDownIcon
                     aria-hidden="true"
                     className="h-5 w-5 text-gray-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      clip-rule="evenodd"
-                      d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z"
-                      fill-rule="evenodd"
-                    />
-                  </svg>
+                  />
                 </span>
               </Listbox.Button>
 
@@ -129,18 +122,10 @@ export const Select = forwardRef<HTMLSelectElement, SelectProps>(
                                 active ? 'text-white' : 'text-[#Fa4669]'
                               )}
                             >
-                              <svg
+                              <CheckIcon
                                 aria-hidden="true"
                                 className="h-5 w-5"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                              >
-                                <path
-                                  clip-rule="evenodd"
-                                  d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-                                  fill-rule="evenodd"
-                                />
-                              </svg>
+                              />
                             </span>
                           ) : null}
                         </>
