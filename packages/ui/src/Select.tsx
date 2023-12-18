4e48b443242d8bb6ce47dc80514850e786949908
RutamBhagat <rutambhagat@gmail.com>
2023-12-19 00:20:43 +0530

    Enhancement: Change Listbox.options shadow and radius (#4246)

--- packages/ui/src/Select.tsx
+++ packages/ui/src/Select.tsx
@@ -55,7 +55,7 @@ export const Select = forwardRef<HTMLSelectElement, SelectProps>(
             <div className="relative mt-2">
               <Listbox.Button
                 className={cn(
-                  'relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-[#Fa4669] sm:text-sm sm:leading-6',
+                  'relative w-full cursor-default rounded-xl bg-white py-2 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-[#Fa4669] sm:text-sm sm:leading-6',
                   className
                 )}
               >
@@ -77,7 +77,7 @@ export const Select = forwardRef<HTMLSelectElement, SelectProps>(
                 leaveTo="opacity-0"
                 show={open}
               >
-                <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-xl bg-white py-1 text-base shadow-sm ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                   {options?.map((option) => (
                     <Listbox.Option
                       className={({ active }) =>
