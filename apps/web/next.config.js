e497278793369d135d08dd37f348dd008daa2d87
bigint <69431456+bigint@users.noreply.github.com>
2023-12-18 17:56:01 +0530

    fix: next lint config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,70 +2,70 @@ const headers = [{ key: 'Cache-Control', value: 'public, max-age=3600' }];
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['data'],
-  reactStrictMode: false,
   experimental: { scrollRestoration: true },
-  async rewrites() {
-    return [
-      {
-        source: '/sitemaps/:match*',
-        destination: 'https://api.hey.xyz/sitemap/:match*'
-      }
-    ];
-  },
-  async redirects() {
-    return [
-      {
-        source: '/discord',
-        destination: 'https://discord.com/invite/B8eKhSSUwX',
-        permanent: true
-      },
-      {
-        source: '/donate',
-        destination: 'https://giveth.io/project/hey?utm_source=hey',
-        permanent: true
-      },
-      {
-        source: '/u/lens/:username*',
-        destination: '/u/:username*',
-        permanent: true
-      },
-      {
-        source: '/gitcoin',
-        destination:
-          'https://explorer.gitcoin.co/#/round/424/0xd4cc0dd193c7dc1d665ae244ce12d7fab337a008/0xd4cc0dd193c7dc1d665ae244ce12d7fab337a008-4',
-        permanent: true
-      },
-      // Forms
-      {
-        source: '/-/token-request',
-        destination:
-          'https://hey.height.app/?taskForm=Token-Allowlist-Request-mwarXOg6ks0A',
-        permanent: true
-      },
-      {
-        source: '/-/verification-request',
-        destination:
-          'https://hey.height.app/?taskForm=Verification-Request-fBxpj55hUMmf',
-        permanent: true
-      }
-    ];
-  },
   async headers() {
     return [
       {
-        source: '/(.*)',
         headers: [
           { key: 'X-Content-Type-Options', value: 'nosniff' },
           { key: 'X-XSS-Protection', value: '1; mode=block' },
           { key: 'Referrer-Policy', value: 'strict-origin' }
-        ]
+        ],
+        source: '/(.*)'
       },
-      { source: '/about', headers },
-      { source: '/privacy', headers },
-      { source: '/thanks', headers }
+      { headers, source: '/about' },
+      { headers, source: '/privacy' },
+      { headers, source: '/thanks' }
     ];
-  }
+  },
+  reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        destination: 'https://discord.com/invite/B8eKhSSUwX',
+        permanent: true,
+        source: '/discord'
+      },
+      {
+        destination: 'https://giveth.io/project/hey?utm_source=hey',
+        permanent: true,
+        source: '/donate'
+      },
+      {
+        destination: '/u/:username*',
+        permanent: true,
+        source: '/u/lens/:username*'
+      },
+      {
+        destination:
+          'https://explorer.gitcoin.co/#/round/424/0xd4cc0dd193c7dc1d665ae244ce12d7fab337a008/0xd4cc0dd193c7dc1d665ae244ce12d7fab337a008-4',
+        permanent: true,
+        source: '/gitcoin'
+      },
+      // Forms
+      {
+        destination:
+          'https://hey.height.app/?taskForm=Token-Allowlist-Request-mwarXOg6ks0A',
+        permanent: true,
+        source: '/-/token-request'
+      },
+      {
+        destination:
+          'https://hey.height.app/?taskForm=Verification-Request-fBxpj55hUMmf',
+        permanent: true,
+        source: '/-/verification-request'
+      }
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        destination: 'https://api.hey.xyz/sitemap/:match*',
+        source: '/sitemaps/:match*'
+      }
+    ];
+  },
+  transpilePackages: ['data']
 };
 
 module.exports = nextConfig;
