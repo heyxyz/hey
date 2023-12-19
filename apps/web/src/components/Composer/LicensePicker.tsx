6a8d7ce36e4b1c2c5892f0fc814b4025f2d2a6f8
RutamBhagat <rutambhagat@gmail.com>
2023-12-19 15:34:09 +0530

    Bug: Update LicensePicker.tsx move options to useState  (#4246)

--- apps/web/src/components/Composer/LicensePicker.tsx
+++ apps/web/src/components/Composer/LicensePicker.tsx
@@ -1,14 +1,23 @@
 import type { MetadataLicenseType } from '@lens-protocol/metadata';
-import type { FC } from 'react';
 
 import { FeatureFlag } from '@hey/data/feature-flags';
 import { PublicationMetadataLicenseType } from '@hey/lens';
 import { Select } from '@hey/ui';
 import getAssetLicense from '@lib/getAssetLicense';
 import isFeatureEnabled from '@lib/isFeatureEnabled';
+import { type FC, useState } from 'react';
 import { usePublicationStore } from 'src/store/non-persisted/usePublicationStore';
 
 const LicensePicker: FC = () => {
+  const [options] = useState(
+    Object.values(PublicationMetadataLicenseType)
+      .filter((license) => getAssetLicense(license))
+      .map((license) => ({
+        label: getAssetLicense(license) as string,
+        selected: true,
+        value: license
+      })) as any
+  );
   const setLicense = usePublicationStore((state) => state.setLicense);
 
   if (!isFeatureEnabled(FeatureFlag.LensMember)) {
@@ -20,15 +29,7 @@ const LicensePicker: FC = () => {
       <Select
         label="Choose a license"
         onChange={(e) => setLicense(e.target.value as MetadataLicenseType)}
-        options={
-          Object.values(PublicationMetadataLicenseType)
-            .filter((license) => getAssetLicense(license))
-            .map((license) => ({
-              label: getAssetLicense(license) as string,
-              selected: true,
-              value: license
-            })) as any
-        }
+        options={options}
       />
     </div>
   );
