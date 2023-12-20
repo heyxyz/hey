f256a7240cbd5fe9cb6022866870a081ab5e9ca5
Vimal Saraswat <saraswatvimal1@gmail.com>
2023-12-20 10:35:40 +0530

    style: update lock icon color

--- apps/web/src/components/Publication/EncryptedPublication.tsx
+++ apps/web/src/components/Publication/EncryptedPublication.tsx
@@ -13,7 +13,7 @@ const EncryptedPublication: FC<EncryptedPublicationProps> = ({
   return (
     <Card className="!bg-gray-100 dark:!bg-gray-800">
       <div className="flex items-center space-x-1 px-4 py-3 text-sm">
-        <LockClosedIcon className="h-4 w-4 text-gray-500" />
+        <LockClosedIcon className="h-4 w-4 text-green-500" />
         <span>{type} has been encrypted</span>
       </div>
     </Card>
