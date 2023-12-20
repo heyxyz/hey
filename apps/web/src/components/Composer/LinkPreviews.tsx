60dd49af26ed4bda0bbfefbc26e10ed0cff1ffa3
bigint <69431456+bigint@users.noreply.github.com>
2023-12-20 19:57:33 +0530

    feat: add link oembed previews in composer (#4295)

--- apps/web/src/components/Composer/LinkPreviews.tsx
+++ apps/web/src/components/Composer/LinkPreviews.tsx
@@ -1,6 +1,7 @@
 import type { FC } from 'react';
 
 import Nft from '@components/Publication/HeyOpenActions/Nft';
+import Oembed from '@components/Shared/Oembed';
 import getURLs from '@hey/lib/getURLs';
 import getNft from '@hey/lib/nft/getNft';
 import { usePublicationStore } from 'src/store/non-persisted/usePublicationStore';
@@ -11,17 +12,22 @@ const LinkPreviews: FC = () => {
   );
 
   const urls = getURLs(publicationContent);
-  const nft = getNft(urls);
 
-  if (!urls.length || !nft) {
+  if (!urls.length) {
     return null;
   }
 
-  return (
-    <div className="m-5">
-      <Nft mintLink={nft.mintLink} />
-    </div>
-  );
+  const nft = getNft(urls);
+
+  if (nft) {
+    return (
+      <div className="m-5">
+        {nft ? <Nft mintLink={nft.mintLink} /> : <Oembed url={urls[0]} />}
+      </div>
+    );
+  }
+
+  return <Oembed className="m-5" url={urls[0]} />;
 };
 
 export default LinkPreviews;
