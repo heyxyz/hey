52fd59c41a1fec22f55510d620e93212d2f07381
bigint <69431456+bigint@users.noreply.github.com>
2023-12-22 14:27:03 +0530

    fix: reduce cover size of default cover to display more patterns (#4320)

--- apps/web/src/components/Profile/Cover.tsx
+++ apps/web/src/components/Profile/Cover.tsx
@@ -9,19 +9,21 @@ interface CoverProps {
 }
 
 const Cover: FC<CoverProps> = ({ cover }) => {
+  const hasCover = !cover.includes(STATIC_IMAGES_URL);
+
   return (
     <div
       className="h-52 sm:h-80"
       style={{
         backgroundColor: BRAND_COLOR,
         backgroundImage: `url(${
-          cover
+          hasCover
             ? imageKit(sanitizeDStorageUrl(cover), COVER)
             : `${STATIC_IMAGES_URL}/patterns/2.svg`
         })`,
         backgroundPosition: 'center center',
-        backgroundRepeat: cover ? 'no-repeat' : 'repeat',
-        backgroundSize: cover ? 'cover' : '30%'
+        backgroundRepeat: hasCover ? 'no-repeat' : 'repeat',
+        backgroundSize: hasCover ? 'cover' : '30%'
       }}
     />
   );
