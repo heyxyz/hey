e40bd84223005505059ddc52e21a079ef8509d94
bigint <69431456+bigint@users.noreply.github.com>
2023-12-22 14:16:29 +0530

    feat: make token gated pub view live

--- apps/web/src/components/Publication/EncryptedPublication/index.tsx
+++ apps/web/src/components/Publication/EncryptedPublication/index.tsx
@@ -1,6 +1,7 @@
 import type { MirrorablePublication } from '@hey/lens';
 import type { FC } from 'react';
 
+import Beta from '@components/Shared/Badges/Beta';
 import { LockClosedIcon } from '@heroicons/react/24/solid';
 import { Card } from '@hey/ui';
 
@@ -16,11 +17,14 @@ const EncryptedPublication: FC<EncryptedPublicationProps> = ({
   const type = publication.__typename;
 
   return (
-    <Card className="!bg-gray-100 dark:!bg-gray-800">
+    <Card className="!bg-gray-100 dark:!bg-gray-800" forceRounded>
       <div className="space-y-4 px-4 py-3 text-sm">
-        <div className="flex items-center space-x-1">
-          <LockClosedIcon className="size-4 text-green-500" />
-          <span>{type} has been encrypted</span>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center space-x-1">
+            <LockClosedIcon className="size-4 text-green-500" />
+            <span>{type} has been encrypted</span>
+          </div>
+          <Beta />
         </div>
         <Criteria publication={publication} />
       </div>
