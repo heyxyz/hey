ab7d4bc80f2b9a54b2408c4508da520fd8233991
bigint <69431456+bigint@users.noreply.github.com>
2023-12-23 16:20:19 +0530

    feat: add radix switch for toggle (#4325)

--- packages/ui/src/Toggle.tsx
+++ packages/ui/src/Toggle.tsx
@@ -1,6 +1,6 @@
 import type { FC } from 'react';
 
-import { Switch } from '@headlessui/react';
+import * as Switch from '@radix-ui/react-switch';
 
 import cn from '../cn';
 
@@ -12,7 +12,7 @@ interface ToggleProps {
 
 export const Toggle: FC<ToggleProps> = ({ disabled = false, on, setOn }) => {
   return (
-    <Switch
+    <Switch.Root
       checked={on}
       className={cn(
         on ? 'bg-brand-500' : 'bg-gray-200 dark:bg-gray-700',
@@ -20,9 +20,7 @@ export const Toggle: FC<ToggleProps> = ({ disabled = false, on, setOn }) => {
         'outline-brand-500 inline-flex h-[22px] w-[42.5px] min-w-[42.5px] rounded-full border-2 border-transparent outline-offset-4 transition-colors duration-200 ease-in-out'
       )}
       disabled={disabled}
-      onChange={() => {
-        setOn(!on);
-      }}
+      onCheckedChange={() => setOn(!on)}
     >
       <span
         className={cn(
@@ -30,6 +28,6 @@ export const Toggle: FC<ToggleProps> = ({ disabled = false, on, setOn }) => {
           'pointer-events-none inline-block size-[18px] rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out'
         )}
       />
-    </Switch>
+    </Switch.Root>
   );
 };
