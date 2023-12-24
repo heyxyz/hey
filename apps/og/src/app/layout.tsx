e3ce05a80a4f56ebc211bf43e102da3f34a7930a
bigint <69431456+bigint@users.noreply.github.com>
2023-12-24 17:52:54 +0530

    feat: add logger

--- apps/og/src/app/layout.tsx
+++ apps/og/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import type { ReactNode } from 'react';
 
 import { APP_NAME, DESCRIPTION } from '@hey/data/constants';
 
@@ -7,11 +8,7 @@ export const metadata: Metadata = {
   title: APP_NAME
 };
 
-export default function RootLayout({
-  children
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
