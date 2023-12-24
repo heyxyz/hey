bfda72f458a20df1deaf0236d55028c7dfc1d1bc
bigint <69431456+bigint@users.noreply.github.com>
2023-12-24 20:59:00 +0530

    feat: add sidebar menu component (#4341)
    * feat: add sidebar menu component
    
    * feat: wrap sidebar tabs and menu in single

--- apps/web/src/components/Staff/Sidebar.tsx
+++ apps/web/src/components/Staff/Sidebar.tsx
@@ -8,33 +8,33 @@ import {
   UserIcon
 } from '@heroicons/react/24/outline';
 
+const settingsSidebarItems = [
+  {
+    icon: <ClipboardIcon className="size-4" />,
+    title: 'Overview',
+    url: '/staff'
+  },
+  {
+    icon: <UserIcon className="size-4" />,
+    title: 'Users',
+    url: '/staff/users'
+  },
+  {
+    icon: <CurrencyDollarIcon className="size-4" />,
+    title: 'Tokens',
+    url: '/staff/tokens'
+  },
+  {
+    icon: <AdjustmentsHorizontalIcon className="size-4" />,
+    title: 'Feature flags',
+    url: '/staff/feature-flags'
+  }
+];
+
 const StaffSidebar: FC = () => {
   return (
     <div className="mb-4 px-3 sm:px-0">
-      <Sidebar
-        items={[
-          {
-            icon: <ClipboardIcon className="size-4" />,
-            title: 'Overview',
-            url: '/staff'
-          },
-          {
-            icon: <UserIcon className="size-4" />,
-            title: 'Users',
-            url: '/staff/users'
-          },
-          {
-            icon: <CurrencyDollarIcon className="size-4" />,
-            title: 'Tokens',
-            url: '/staff/tokens'
-          },
-          {
-            icon: <AdjustmentsHorizontalIcon className="size-4" />,
-            title: 'Feature flags',
-            url: '/staff/feature-flags'
-          }
-        ]}
-      />
+      <Sidebar items={settingsSidebarItems} />
     </div>
   );
 };
