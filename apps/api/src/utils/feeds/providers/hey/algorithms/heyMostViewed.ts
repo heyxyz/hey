820ce015eebd5dc9a000c1be4501465c015988d0
bigint <69431456+bigint@users.noreply.github.com>
2023-12-27 00:32:57 +0530

    feat: update heyMostViewed algo (#4364)

--- apps/api/src/utils/feeds/providers/hey/algorithms/heyMostViewed.ts
+++ apps/api/src/utils/feeds/providers/hey/algorithms/heyMostViewed.ts
@@ -1,9 +1,7 @@
 import { Errors } from '@hey/data/errors';
-import { PAGEVIEW } from '@hey/data/tracking';
 import logger from '@hey/lib/logger';
 import createClickhouseClient from '@utils/createClickhouseClient';
 import randomizeIds from '@utils/feeds/randomizeIds';
-import removeParamsFromString from '@utils/feeds/removeParamsFromString';
 
 const heyMostViewed = async (limit: number, offset: number): Promise<any[]> => {
   if (limit > 500) {
@@ -15,33 +13,21 @@ const heyMostViewed = async (limit: number, offset: number): Promise<any[]> => {
     const rows = await client.query({
       format: 'JSONEachRow',
       query: `
-        SELECT
-          url,
+        SELECT 
+          publication_id AS id,
           COUNT(*) AS view_count
-        FROM
-          events
-        WHERE
-          name = '${PAGEVIEW}'
-          AND url LIKE 'https://hey.xyz/posts/%'
-          AND created >= now() - INTERVAL 1 DAY
-        GROUP BY
-          url
-        ORDER BY
-          view_count DESC
+        FROM impressions
+        WHERE viewed_at > now() - INTERVAL 1 DAY
+        GROUP BY id
+        ORDER BY view_count DESC
         LIMIT ${limit}
         OFFSET ${offset};
       `
     });
 
-    const result =
-      await rows.json<Array<{ url: string; view_count: number }>>();
+    const result = await rows.json<Array<{ id: string }>>();
 
-    const ids = result.map((row) => {
-      const { url } = row;
-      const id = url.split('/').pop() || '';
-
-      return removeParamsFromString(id);
-    });
+    const ids = result.map((r) => r.id);
     logger.info(`[Hey] Most viewed: ${ids.length} ids`);
 
     return randomizeIds(ids);
