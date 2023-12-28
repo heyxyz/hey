45762e9f998552b5839f16a7a7169f61e0aee0fd
bigint <69431456+bigint@users.noreply.github.com>
2023-12-28 22:28:14 +0530

    chore: update codegen

--- apps/api/src/utils/prisma.ts
+++ apps/api/src/utils/prisma.ts
@@ -1,7 +1,19 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient({
-  datasources: { db: { url: process.env.DATABASE_URL } }
-});
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
