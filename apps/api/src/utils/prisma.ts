079e00423bd34391ebda47cda50afd68ce260783
bigint <69431456+bigint@users.noreply.github.com>
2023-12-28 22:21:28 +0530

    chore: update codegen

--- apps/api/src/utils/prisma.ts
+++ apps/api/src/utils/prisma.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: { db: { url: process.env.DATABASE_URL } }
+});
 
 export default prisma;
