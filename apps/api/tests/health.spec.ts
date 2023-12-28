07c6acc506b5b7cf7771c25287f293336c216e28
bigint <69431456+bigint@users.noreply.github.com>
2023-12-28 22:01:57 +0530

    chore: add console

--- apps/api/tests/health.spec.ts
+++ apps/api/tests/health.spec.ts
@@ -1,11 +1,11 @@
 import { TEST_URL } from '@utils/constants';
-import axios from 'axios';
 import { describe, test } from 'vitest';
 
 describe('health', () => {
   test('should return pong', async () => {
-    const response = await axios.get(`${TEST_URL}/health`);
-    console.log(response.data);
+    const response = await fetch(`${TEST_URL}/health`);
+    const json = await response.json();
+    console.log(json);
     // expect(response.data.ping).toEqual('pong');
   });
 });
