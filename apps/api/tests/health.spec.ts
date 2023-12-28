f1f763ebbe1c8eee44ad658e426af9613ecfd689
bigint <69431456+bigint@users.noreply.github.com>
2023-12-28 20:16:25 +0530

    feat: add test coverage to: health

--- apps/api/tests/health.spec.ts
+++ apps/api/tests/health.spec.ts
@@ -2,7 +2,7 @@ import { TEST_URL } from '@utils/constants';
 import axios from 'axios';
 import { describe, expect, test } from 'vitest';
 
-describe('ens/index', () => {
+describe('health', () => {
   test('should return pong', async () => {
     const response = await axios.get(`${TEST_URL}/health`);
     expect(response.data.ping).toEqual('pong');
