233785e4df00a14e44078062607eea4842ad6166
bigint <69431456+bigint@users.noreply.github.com>
2023-12-30 00:03:02 +0530

    fix: verified test case

--- apps/api/tests/internal/verified/updateVerified.spec.ts
+++ apps/api/tests/internal/verified/updateVerified.spec.ts
@@ -1,4 +1,3 @@
-import { TEST_LENS_ID } from '@hey/data/constants';
 import getAuthWorkerHeadersForTest from '@hey/lib/getAuthWorkerHeadersForTest';
 import { TEST_URL } from '@utils/constants';
 import axios from 'axios';
@@ -8,7 +7,7 @@ describe('internal/verified/updateVerified', () => {
   test('should update verified status', async () => {
     const response = await axios.post(
       `${TEST_URL}/internal/verified/updateVerified`,
-      { enabled: true, id: TEST_LENS_ID },
+      { enabled: true, id: Math.random().toString() },
       { headers: await getAuthWorkerHeadersForTest() }
     );
 
