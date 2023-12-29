267b1e5fe3d68b1d8bda39a0b197b0187cedac5e
bigint <69431456+bigint@users.noreply.github.com>
2023-12-29 12:25:28 +0530

    feat: add test cases for stats/profileDetails

--- apps/api/tests/sts/token.spec.ts
+++ apps/api/tests/sts/token.spec.ts
@@ -5,6 +5,7 @@ import { describe, expect, test } from 'vitest';
 describe('sts/token', () => {
   test('should return sts token', async () => {
     const response = await axios.get(`${TEST_URL}/sts/token`);
+
     expect(response.data).toHaveProperty('accessKeyId');
     expect(response.data).toHaveProperty('secretAccessKey');
     expect(response.data).toHaveProperty('sessionToken');
