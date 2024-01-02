d83462b885e3ee060edcfafb91eadcf06c168caf
bigint <69431456+bigint@users.noreply.github.com>
2024-01-02 18:49:53 +0530

    feat: add profile restriction

--- apps/web/src/components/Shared/GlobalBanners.tsx
+++
@@ -1,9 +0,0 @@
-import type { FC } from 'react';
-
-import ProtectProfile from './ProtectProfile';
-
-const GlobalBanners: FC = () => {
-  return <ProtectProfile />;
-};
-
-export default GlobalBanners;
