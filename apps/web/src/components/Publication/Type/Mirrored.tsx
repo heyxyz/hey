13d0c37e35bf9ccf5f1be98a8e5ade8e5bc7a17b
bigint <69431456+bigint@users.noreply.github.com>
2024-01-04 19:53:41 +0530

    chore: reduce props load on Mirrored

--- apps/web/src/components/Publication/Type/Mirrored.tsx
+++ apps/web/src/components/Publication/Type/Mirrored.tsx
@@ -1,18 +1,18 @@
-import type { Mirror } from '@hey/lens';
+import type { Profile } from '@hey/lens';
 import type { FC } from 'react';
 
 import Profiles from '@components/Shared/Profiles';
 import { ArrowsRightLeftIcon } from '@heroicons/react/24/outline';
 
 interface MirroredProps {
-  publication: Mirror;
+  profile: Profile;
 }
 
-const Mirrored: FC<MirroredProps> = ({ publication }) => {
+const Mirrored: FC<MirroredProps> = ({ profile }) => {
   return (
     <div className="ld-text-gray-500 flex items-center space-x-1 pb-4 text-[13px]">
       <ArrowsRightLeftIcon className="size-4" />
-      <Profiles context="mirrored" profiles={[publication.by]} />
+      <Profiles context="mirrored" profiles={[profile]} />
     </div>
   );
 };
