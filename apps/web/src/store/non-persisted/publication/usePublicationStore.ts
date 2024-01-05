17469f0491d5b345806a9a0c69d707441b12cf12
bigint <69431456+bigint@users.noreply.github.com>
2024-01-05 20:37:33 +0530

    feat: add usePublicationLiveStore

--- apps/web/src/store/non-persisted/publication/usePublicationStore.ts
+++ apps/web/src/store/non-persisted/publication/usePublicationStore.ts
@@ -3,38 +3,18 @@ import type { AnyPublication } from '@hey/lens';
 import { create } from 'zustand';
 
 interface PublicationState {
-  liveVideoConfig: {
-    id: string;
-    playbackId: string;
-    streamKey: string;
-  };
   publicationContent: string;
   quotedPublication: AnyPublication | null;
-  resetLiveVideoConfig: () => void;
-  setLiveVideoConfig: (liveVideoConfig: {
-    id: string;
-    playbackId: string;
-    streamKey: string;
-  }) => void;
   setPublicationContent: (publicationContent: string) => void;
   setQuotedPublication: (quotedPublication: AnyPublication | null) => void;
-  setShowLiveVideoEditor: (showLiveVideoEditor: boolean) => void;
-  showLiveVideoEditor: boolean;
 }
 
 export const usePublicationStore = create<PublicationState>((set) => ({
   isUploading: false,
-  liveVideoConfig: { id: '', playbackId: '', streamKey: '' },
   publicationContent: '',
   quotedPublication: null,
-  resetLiveVideoConfig: () =>
-    set(() => ({ liveVideoConfig: { id: '', playbackId: '', streamKey: '' } })),
-  setLiveVideoConfig: (liveVideoConfig) => set(() => ({ liveVideoConfig })),
   setPublicationContent: (publicationContent) =>
     set(() => ({ publicationContent })),
   setQuotedPublication: (quotedPublication) =>
-    set(() => ({ quotedPublication })),
-  setShowLiveVideoEditor: (showLiveVideoEditor) =>
-    set(() => ({ showLiveVideoEditor })),
-  showLiveVideoEditor: false
+    set(() => ({ quotedPublication }))
 }));
