9f0b3b2f6fdb8a04df74478c6ebdd85fcc3605b4
bigint <bigint@lenster.xyz>
2024-01-06 20:58:00 +0530

    fix: api test cases

--- apps/api/tests/preferences/updateNftStatus.spec.ts
+++ apps/api/tests/preferences/updateNftStatus.spec.ts
@@ -3,10 +3,10 @@ import { TEST_URL } from '@utils/constants';
 import axios from 'axios';
 import { describe, expect, test } from 'vitest';
 
-describe('preferences/updateHeyMemberNftStatus', () => {
+describe('preferences/updateNftStatus', () => {
   test('should update profile membership nft status', async () => {
     const response = await axios.post(
-      `${TEST_URL}/preferences/updateHeyMemberNftStatus`,
+      `${TEST_URL}/preferences/updateNftStatus`,
       undefined,
       { headers: await getAuthWorkerHeadersForTest() }
     );
