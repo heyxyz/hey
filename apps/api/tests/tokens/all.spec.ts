0c2585d5da14138afa8a5f881932593681c07150
bigint <bigint@lenster.xyz>
2024-01-06 18:54:28 +0530

    refactor: tokens api endpoints

--- apps/api/tests/tokens/all.spec.ts
+++ apps/api/tests/tokens/all.spec.ts
@@ -2,9 +2,9 @@ import { TEST_URL } from '@utils/constants';
 import axios from 'axios';
 import { describe, expect, test } from 'vitest';
 
-describe('token/all', () => {
+describe('tokens/all', () => {
   test('should return all tokens', async () => {
-    const response = await axios.get(`${TEST_URL}/token/all`);
+    const response = await axios.get(`${TEST_URL}/tokens/all`);
 
     expect(response.data.tokens).toBeInstanceOf(Array);
     expect(response.data.tokens[0].name).toEqual('Wrapped Matic');
