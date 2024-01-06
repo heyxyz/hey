6675ca7e8801386d758c1bf83d5a79231213d1ab
bigint <bigint@lenster.xyz>
2024-01-06 19:04:03 +0530

    refactor: updateGardenerMode api endpoints

--- apps/web/src/components/Shared/Navbar/NavItems/GardenerMode.tsx
+++ apps/web/src/components/Shared/Navbar/NavItems/GardenerMode.tsx
@@ -27,7 +27,7 @@ const GardenerMode: FC<ModModeProps> = ({ className = '' }) => {
   const toggleModMode = () => {
     toast.promise(
       axios.post(
-        `${HEY_API_URL}/internal/features/updateGardenerMode`,
+        `${HEY_API_URL}/internal/features/gardener-mode`,
         { enabled: !gardenerMode },
         { headers: getAuthWorkerHeaders() }
       ),
