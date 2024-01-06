f8eead604d8a0a3bb2204a3a8c0699cddcfda554
bigint <bigint@lenster.xyz>
2024-01-06 18:51:05 +0530

    refactor: polls api endpoints

--- apps/api/tests/poll/get.spec.ts
+++ apps/api/tests/poll/get.spec.ts
@@ -3,11 +3,11 @@ import { TEST_URL } from '@utils/constants';
 import axios from 'axios';
 import { describe, expect, test } from 'vitest';
 
-describe('poll/get', async () => {
+describe('polls/get', async () => {
   const id = '76018d99-0dcb-4b54-8fc1-7782b63f3055';
 
   test('should return a poll without authentification', async () => {
-    const response = await axios.get(`${TEST_URL}/poll/get`, {
+    const response = await axios.get(`${TEST_URL}/polls/get`, {
       params: { id }
     });
 
@@ -17,7 +17,7 @@ describe('poll/get', async () => {
   });
 
   test('should return a poll with authentification', async () => {
-    const response = await axios.get(`${TEST_URL}/poll/get`, {
+    const response = await axios.get(`${TEST_URL}/polls/get`, {
       headers: await getAuthWorkerHeadersForTest(),
       params: { id }
     });
