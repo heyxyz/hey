d86ca89ca2ab790219acb0e4eb59fcd5f7eb3ceb
bigint <bigint@lenster.xyz>
2024-01-06 20:49:43 +0530

    refactor: updateNftStatus api endpoints

--- apps/api/tests/preferences/nftStatus.spec.ts
+++ apps/api/tests/preferences/nftStatus.spec.ts
@@ -3,9 +3,9 @@ import { TEST_URL } from '@utils/constants';
 import axios from 'axios';
 import { describe, expect, test } from 'vitest';
 
-describe('preferences/nft-status', () => {
+describe('preferences/nftStatus', () => {
   test('should return profile membership nft status', async () => {
-    const response = await axios.get(`${TEST_URL}/preferences/nft-status`, {
+    const response = await axios.get(`${TEST_URL}/preferences/nftStatus`, {
       params: { id: TEST_LENS_ID }
     });
 
