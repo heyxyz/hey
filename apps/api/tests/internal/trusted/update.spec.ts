e41eda2d7fb8007180a12d9ddedf32926aa08275
bigint <69431456+bigint@users.noreply.github.com>
2024-01-07 22:41:29 +0530

    feat: rename trusted-profiles to trusted in api route

--- apps/api/tests/internal/trusted/update.spec.ts
+++ apps/api/tests/internal/trusted/update.spec.ts
@@ -3,10 +3,10 @@ import { TEST_URL } from '@utils/constants';
 import axios from 'axios';
 import { describe, expect, test } from 'vitest';
 
-describe('internal/trusted-profiles/update', () => {
+describe('internal/trusted/update', () => {
   test('should update trusted profile status', async () => {
     const response = await axios.post(
-      `${TEST_URL}/internal/trusted-profiles/update`,
+      `${TEST_URL}/internal/trusted/update`,
       { enabled: true, id: Math.random().toString() },
       { headers: await getAuthWorkerHeadersForTest() }
     );
