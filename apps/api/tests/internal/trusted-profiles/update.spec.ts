16da2f35babbd04800706209c4f176d9078459c7
bigint <69431456+bigint@users.noreply.github.com>
2024-01-07 22:20:25 +0530

    feat: add trusted profile status

---
+++ apps/api/tests/internal/trusted-profiles/update.spec.ts
@@ -0,0 +1,16 @@
+import getAuthWorkerHeadersForTest from '@hey/lib/getAuthWorkerHeadersForTest';
+import { TEST_URL } from '@utils/constants';
+import axios from 'axios';
+import { describe, expect, test } from 'vitest';
+
+describe('internal/trusted-profiles/update', () => {
+  test('should update trusted profile status', async () => {
+    const response = await axios.post(
+      `${TEST_URL}/internal/trusted-profiles/update`,
+      { enabled: true, id: Math.random().toString() },
+      { headers: await getAuthWorkerHeadersForTest() }
+    );
+
+    expect(response.data.enabled).toBeTruthy();
+  });
+});
