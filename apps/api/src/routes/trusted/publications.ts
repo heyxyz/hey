c48e3df58a61b0e15e40d8c0badabd7cdbd0ed50
bigint <69431456+bigint@users.noreply.github.com>
2024-01-08 19:57:16 +0530

    feat: add heyTrustedReports pub ids api

---
+++ apps/api/src/routes/trusted/publications.ts
@@ -0,0 +1,21 @@
+import type { Handler } from 'express';
+
+import catchedError from '@utils/catchedError';
+import { SWR_CACHE_AGE_1_MIN_30_DAYS } from '@utils/constants';
+import heyTrustedReports from '@utils/feeds/providers/hey/algorithms/heyTrustedReports';
+
+export const get: Handler = async (req, res) => {
+  const limit = (parseInt(req.query?.limit as string) || 50) as number;
+  const offset = (parseInt(req.query?.offset as string) || 0) as number;
+
+  try {
+    const ids = await heyTrustedReports(limit, offset);
+
+    return res
+      .status(200)
+      .setHeader('Cache-Control', SWR_CACHE_AGE_1_MIN_30_DAYS)
+      .json({ ids, success: true });
+  } catch (error) {
+    return catchedError(res, error);
+  }
+};
