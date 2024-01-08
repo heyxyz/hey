af807a7f37d8dc0c0124f5a609c9f963238551ba
bigint <69431456+bigint@users.noreply.github.com>
2024-01-08 20:31:40 +0530

    feat: update mod feed design

---
+++ apps/web/src/components/Mod/Actions.tsx
@@ -0,0 +1,49 @@
+import type { FC } from 'react';
+
+import GardenerActions from '@components/Publication/Actions/GardenerActions';
+import TrustedProfilesActions from '@components/Publication/Actions/TrustedProfilesActions';
+import { useFeatureFlagsStore } from 'src/store/persisted/useFeatureFlagsStore';
+
+interface ActionsProps {
+  hideTrustedReport?: boolean;
+  publicationId: string;
+}
+
+const Actions: FC<ActionsProps> = ({
+  hideTrustedReport = false,
+  publicationId
+}) => {
+  const trusted = useFeatureFlagsStore((state) => state.trusted);
+  const gardenerMode = useFeatureFlagsStore((state) => state.gardenerMode);
+
+  return (
+    <div>
+      {gardenerMode && (
+        <>
+          <div className="divider" />
+          <div className="m-5">
+            <b>Gardener actions</b>
+            <GardenerActions
+              className="mt-3 max-w-md"
+              publicationId={publicationId}
+            />
+          </div>
+        </>
+      )}
+      {trusted && !hideTrustedReport && (
+        <>
+          <div className="divider" />
+          <div className="m-5">
+            <b>Moderate Post</b>
+            <TrustedProfilesActions
+              className="mt-3 max-w-md"
+              publicationId={publicationId}
+            />
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Actions;
