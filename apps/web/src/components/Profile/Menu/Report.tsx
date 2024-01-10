06536d058f4225f8a2abaef023a04029efbaf988
Simran Deep Thakral <simrandeep332000@gmail.com>
2024-01-10 22:15:32 +0530

    Making prettier Report.tsx

--- apps/web/src/components/Profile/Menu/Report.tsx
+++ apps/web/src/components/Profile/Menu/Report.tsx
@@ -11,19 +11,19 @@ interface ReportProfileProps {
 
 const Report: FC<ReportProfileProps> = ({ profile }) => {
   const setShowReportProfileModal = useGlobalModalStateStore(
-      (state) => state.setShowReportProfileModal
+    (state) => state.setShowReportProfileModal
   );
 
   return (
-      <DropdownMenuItem
-          className={
-            'm-2 flex cursor-pointer items-center space-x-2 rounded-lg px-2 py-1.5 text-sm data-[highlighted]:bg-gray-100 dark:data-[highlighted]:bg-gray-800'
-          }
-          onClick={() => setShowReportProfileModal(true, profile)}
-      >
-        <FlagIcon className="size-4" />
-        <div>Report profile</div>
-      </DropdownMenuItem>
+    <DropdownMenuItem
+      className={
+        'm-2 flex cursor-pointer items-center space-x-2 rounded-lg px-2 py-1.5 text-sm focus:outline-none data-[highlighted]:bg-gray-100 dark:data-[highlighted]:bg-gray-800'
+      }
+      onClick={() => setShowReportProfileModal(true, profile)}
+    >
+      <FlagIcon className="size-4" />
+      <div>Report profile</div>
+    </DropdownMenuItem>
   );
 };
 
