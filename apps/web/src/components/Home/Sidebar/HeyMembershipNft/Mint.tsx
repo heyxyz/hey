36a14c6b4018b217c98af3f974a79b079f0ac67c
bigint <69431456+bigint@users.noreply.github.com>
2024-01-11 09:20:53 +0530

    fix: use onCollectSuccess instead of dupe

--- apps/web/src/components/Home/Sidebar/HeyMembershipNft/Mint.tsx
+++ apps/web/src/components/Home/Sidebar/HeyMembershipNft/Mint.tsx
@@ -4,23 +4,14 @@ import type { FC } from 'react';
 import CollectAction from '@components/Publication/LensOpenActions/CollectModule/CollectAction';
 import Loader from '@components/Shared/Loader';
 import { Errors } from '@hey/data';
-import { HEY_API_URL } from '@hey/data/constants';
 import { usePublicationQuery } from '@hey/lens';
 import { ErrorMessage } from '@hey/ui';
-import getAuthApiHeaders from '@lib/getAuthApiHeaders';
-import axios from 'axios';
-import toast from 'react-hot-toast';
-import { usePreferencesStore } from 'src/store/non-persisted/usePreferencesStore';
 
 interface MintProps {
-  setShowMintModal: (show: boolean) => void;
+  onCollectSuccess: () => void;
 }
 
-const Mint: FC<MintProps> = ({ setShowMintModal }) => {
-  const setHasDismissedOrMintedMembershipNft = usePreferencesStore(
-    (state) => state.setHasDismissedOrMintedMembershipNft
-  );
-
+const Mint: FC<MintProps> = ({ onCollectSuccess }) => {
   const { data, error, loading } = usePublicationQuery({
     variables: { request: { forId: '0x06-0x05' } }
   });
@@ -51,25 +42,6 @@ const Mint: FC<MintProps> = ({ setShowMintModal }) => {
   const publication = data?.publication as Post;
   const openAction = publication.openActionModules[0];
 
-  const updateHeyMemberNftStatus = async () => {
-    try {
-      toast.promise(
-        axios.post(`${HEY_API_URL}/preferences/updateNftStatus`, undefined, {
-          headers: getAuthApiHeaders()
-        }),
-        {
-          error: 'Error updating.',
-          loading: 'Updating...',
-          success: () => {
-            setHasDismissedOrMintedMembershipNft(true);
-            setShowMintModal(false);
-            return 'Updated!';
-          }
-        }
-      );
-    } catch {}
-  };
-
   return (
     <div className="p-5">
       <img
@@ -80,7 +52,7 @@ const Mint: FC<MintProps> = ({ setShowMintModal }) => {
         className="w-full justify-center"
         countOpenActions={0}
         forceShowCollect
-        onCollectSuccess={updateHeyMemberNftStatus}
+        onCollectSuccess={onCollectSuccess}
         openAction={openAction}
         publication={publication}
       />
