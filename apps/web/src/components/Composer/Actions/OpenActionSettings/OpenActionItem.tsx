a4880737100dd7d372215fe8c1c0d5fa3768ce55
bigint <69431456+bigint@users.noreply.github.com>
2024-01-15 12:37:08 +0530

    feat: mvp save tip

--- apps/web/src/components/Composer/Actions/OpenActionSettings/OpenActionItem.tsx
+++ apps/web/src/components/Composer/Actions/OpenActionSettings/OpenActionItem.tsx
@@ -1,29 +1,44 @@
+import type { OpenAction } from '@hey/data/enums';
 import type { FC, ReactNode } from 'react';
 
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import { CheckCircleIcon } from '@heroicons/react/24/solid';
 import { Card } from '@hey/ui';
+import {
+  ScreenType,
+  useOpenActionStore
+} from 'src/store/non-persisted/publication/useOpenActionStore';
 
 interface OpenActionItemProps {
   description: string;
   icon: ReactNode;
-  onClick: () => void;
   selected: boolean;
   title: string;
+  type: OpenAction;
 }
 
 const OpenActionItem: FC<OpenActionItemProps> = ({
   description,
   icon,
-  onClick,
   selected,
-  title
+  title,
+  type
 }) => {
+  const setScreen = useOpenActionStore((state) => state.setScreen);
+  const setSelectedOpenAction = useOpenActionStore(
+    (state) => state.setSelectedOpenAction
+  );
+
+  const onOpenActionSelected = (name: OpenAction) => {
+    setScreen(ScreenType.Config);
+    setSelectedOpenAction(name);
+  };
+
   return (
     <Card
       className="flex cursor-pointer items-center justify-between p-5"
       forceRounded
-      onClick={onClick}
+      onClick={() => onOpenActionSelected(type)}
     >
       <div className="flex items-center space-x-3">
         <div className="text-brand-500">{icon}</div>
