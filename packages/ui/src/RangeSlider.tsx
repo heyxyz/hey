86410dec7c3a0a5bbfdb2213cf1241a3dfa04b9a
bigint <69431456+bigint@users.noreply.github.com>
2024-01-15 23:05:39 +0530

    feat: update tip modal

--- packages/ui/src/RangeSlider.tsx
+++ packages/ui/src/RangeSlider.tsx
@@ -6,10 +6,14 @@ import cn from '../cn';
 interface RangeSliderProps extends SliderPrimitive.SliderProps {
   className?: string;
   displayValue?: string;
+  showValueInThumb?: boolean;
 }
 
 export const RangeSlider = forwardRef<HTMLInputElement, RangeSliderProps>(
-  function RangeSlider({ className = '', displayValue, ...rest }, ref) {
+  function RangeSlider(
+    { className = '', displayValue, showValueInThumb = false, ...rest },
+    ref
+  ) {
     return (
       <SliderPrimitive.Root
         className={cn(
@@ -21,14 +25,19 @@ export const RangeSlider = forwardRef<HTMLInputElement, RangeSliderProps>(
         step={1}
         {...rest}
       >
-        <SliderPrimitive.Track className="dark:bg-brand-800 bg-brand-200 relative h-1.5 grow rounded-full">
-          <SliderPrimitive.Range className="bg-brand-600 absolute h-full rounded-full" />
+        <SliderPrimitive.Track className="relative h-1 grow rounded-full bg-gray-200 dark:bg-gray-800">
+          <SliderPrimitive.Range className="absolute h-full rounded-full bg-gray-600" />
         </SliderPrimitive.Track>
         <SliderPrimitive.Thumb
           aria-label="Slider"
-          className="bg-brand-500 block rounded-lg px-2 py-1 text-xs font-bold text-white focus:outline-none active:scale-110"
+          className={cn(
+            showValueInThumb
+              ? 'rounded-lg px-2 py-1 text-xs font-bold text-white'
+              : 'size-5 rounded-full',
+            'block bg-gray-900 focus:outline-none active:scale-110'
+          )}
         >
-          {displayValue || rest.value}
+          {showValueInThumb ? displayValue || rest.value : null}
         </SliderPrimitive.Thumb>
       </SliderPrimitive.Root>
     );
