392223cfa898140795d1852fa05ca5965ff84c5c
bigint <69431456+bigint@users.noreply.github.com>
2024-01-19 21:44:23 +0530

    chore: update

--- apps/web/src/components/Publication/FullPublication.tsx
+++ apps/web/src/components/Publication/FullPublication.tsx
@@ -10,6 +10,7 @@ import { useEffectOnce } from 'usehooks-ts';
 import PublicationActions from './Actions';
 import FeaturedGroup from './FeaturedGroup';
 import HiddenPublication from './HiddenPublication';
+import PublicationAvatar from './PublicationAvatar';
 import PublicationBody from './PublicationBody';
 import PublicationHeader from './PublicationHeader';
 import PublicationStats from './PublicationStats';
@@ -33,9 +34,10 @@ const FullPublication: FC<FullPublicationProps> = ({ publication }) => {
   return (
     <article className="p-5">
       <PublicationType publication={publication} showType />
-      <div>
-        <PublicationHeader publication={targetPublication} />
-        <div className="ml-[60px]">
+      <div className="flex items-start space-x-3">
+        <PublicationAvatar profile={publication.by} />
+        <div>
+          <PublicationHeader publication={targetPublication} />
           {targetPublication.isHidden ? (
             <HiddenPublication type={targetPublication.__typename} />
           ) : (
