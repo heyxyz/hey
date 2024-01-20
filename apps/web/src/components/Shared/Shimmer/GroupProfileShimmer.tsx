8c6921f06ddfad4c4cc659a15532154363eb02a0
bigint <69431456+bigint@users.noreply.github.com>
2024-01-20 18:41:24 +0530

    fix: shimmer size

--- apps/web/src/components/Shared/Shimmer/GroupProfileShimmer.tsx
+++ apps/web/src/components/Shared/Shimmer/GroupProfileShimmer.tsx
@@ -4,7 +4,7 @@ const GroupProfileShimmer: FC = () => {
   return (
     <div className="py-1">
       <div className="flex items-center space-x-3">
-        <div className="shimmer size-10 rounded-lg" />
+        <div className="shimmer size-11 rounded-lg" />
         <div className="space-y-3">
           <div className="shimmer h-3 w-28 rounded-lg" />
           <div className="shimmer h-3 w-20 rounded-lg" />
