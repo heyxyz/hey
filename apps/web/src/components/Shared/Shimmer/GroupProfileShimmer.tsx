c25f1fd310bae02e75aa69dc40658343cd661f8b
bigint <69431456+bigint@users.noreply.github.com>
2024-01-21 14:38:20 +0530

    fix: GroupProfileShimmer

--- apps/web/src/components/Shared/Shimmer/GroupProfileShimmer.tsx
+++ apps/web/src/components/Shared/Shimmer/GroupProfileShimmer.tsx
@@ -2,13 +2,11 @@ import type { FC } from 'react';
 
 const GroupProfileShimmer: FC = () => {
   return (
-    <div className="py-1">
-      <div className="flex items-center space-x-3">
-        <div className="shimmer size-11 rounded-lg" />
-        <div className="space-y-3">
-          <div className="shimmer h-3 w-28 rounded-lg" />
-          <div className="shimmer h-3 w-20 rounded-lg" />
-        </div>
+    <div className="flex items-center space-x-3">
+      <div className="shimmer size-11 rounded-lg" />
+      <div className="space-y-4 py-1">
+        <div className="shimmer h-3 w-28 rounded-lg" />
+        <div className="shimmer h-3 w-20 rounded-lg" />
       </div>
     </div>
   );
