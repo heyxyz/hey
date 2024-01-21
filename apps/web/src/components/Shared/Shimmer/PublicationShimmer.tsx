7e8f4b5a4c9f66a9143404ac2adc53f770de5a8b
bigint <69431456+bigint@users.noreply.github.com>
2024-01-21 14:42:52 +0530

    fix: PublicationShimmer

--- apps/web/src/components/Shared/Shimmer/PublicationShimmer.tsx
+++ apps/web/src/components/Shared/Shimmer/PublicationShimmer.tsx
@@ -2,13 +2,13 @@ import type { FC } from 'react';
 
 const PublicationShimmer: FC = () => {
   return (
-    <div className="flex items-start space-x-3 p-5">
+    <div className="flex items-start space-x-3 px-5 py-4">
       <div>
         <div className="shimmer size-11 rounded-full" />
       </div>
       <div className="w-full space-y-4">
         <div className="item flex justify-between">
-          <div className="item flex space-x-3">
+          <div className="item flex space-x-3 pt-1">
             <div className="shimmer h-3 w-28 rounded-lg" />
             <div className="shimmer h-3 w-20 rounded-lg" />
           </div>
@@ -18,7 +18,7 @@ const PublicationShimmer: FC = () => {
           <div className="shimmer h-3 w-7/12 rounded-lg" />
           <div className="shimmer h-3 w-1/3 rounded-lg" />
         </div>
-        <div className="flex gap-7 pt-1">
+        <div className="flex gap-8 pt-1">
           <div className="shimmer size-5 rounded-lg" />
           <div className="shimmer size-5 rounded-lg" />
           <div className="shimmer size-5 rounded-lg" />
