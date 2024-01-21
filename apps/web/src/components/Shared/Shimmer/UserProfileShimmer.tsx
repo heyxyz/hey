4885b369f57715cdbcce13a3912bac8ae73581d5
bigint <69431456+bigint@users.noreply.github.com>
2024-01-21 14:33:02 +0530

    fix: wtf shimmer

--- apps/web/src/components/Shared/Shimmer/UserProfileShimmer.tsx
+++ apps/web/src/components/Shared/Shimmer/UserProfileShimmer.tsx
@@ -12,12 +12,12 @@ const UserProfileShimmer: FC<UserProfileShimmerProps> = ({
   showFollow = false
 }) => {
   return (
-    <div className="flex items-center justify-between py-1">
+    <div className="flex items-center justify-between">
       <div className="flex items-center space-x-3">
         <div
           className={cn(isBig ? 'size-14' : 'size-11', 'shimmer rounded-full')}
         />
-        <div className="space-y-3">
+        <div className="space-y-4 py-1">
           <div className="shimmer h-3 w-28 rounded-lg" />
           <div className="shimmer h-3 w-20 rounded-lg" />
           {isBig ? <div className="shimmer h-3 w-48 rounded-lg" /> : null}
