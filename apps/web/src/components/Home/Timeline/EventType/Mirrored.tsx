06f66cdea1b70569e017400f54bb1f5c562de29e
bigint <69431456+bigint@users.noreply.github.com>
2024-01-24 18:12:22 +0530

    chore: update dependencies 📦

--- apps/web/src/components/Home/Timeline/EventType/Mirrored.tsx
+++ apps/web/src/components/Home/Timeline/EventType/Mirrored.tsx
@@ -19,7 +19,7 @@ const Mirrored: FC<MirroredProps> = ({ mirrors }) => {
   };
 
   return (
-    <div className="ld-text-gray-500 flex items-center space-x-1 pb-4 text-[13px]">
+    <div className="ld-text-gray-500 mb-3 flex items-center space-x-1 text-[13px]">
       <ArrowsRightLeftIcon className="size-4" />
       <Profiles context="mirrored" profiles={getMirroredProfiles()} />
     </div>
