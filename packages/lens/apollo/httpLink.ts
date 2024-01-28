e1d09e2e21644ff84473b9f09b283e3379f9bcdc
bigint <69431456+bigint@users.noreply.github.com>
2024-01-28 18:21:39 +0530

    feat: add request id to lens api header

--- packages/lens/apollo/httpLink.ts
+++ packages/lens/apollo/httpLink.ts
@@ -1,9 +1,11 @@
 import { HttpLink } from '@apollo/client';
 import { LENS_API_URL } from '@hey/data/constants';
+import { v4 as uuid } from 'uuid';
 
 const httpLink = new HttpLink({
   fetch,
   fetchOptions: 'no-cors',
+  headers: { 'x-request-id': uuid() },
   uri: LENS_API_URL
 });
 
