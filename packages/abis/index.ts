44cdbad021a522334dc0135d8321e41fdc491f7d
bigint <69431456+bigint@users.noreply.github.com>
2024-01-29 10:25:49 +0530

    feat: use new nft spec instead of hardcoding everything

--- packages/abis/index.ts
+++ packages/abis/index.ts
@@ -1,7 +1,3 @@
-export * from './BasePaint';
-export * from './FollowNft';
 export * from './LensHub';
 export * from './PublicAct';
 export * from './TokenHandleRegistry';
-export * from './ZoraCreator1155Impl';
-export * from './ZoraERC721Drop';
