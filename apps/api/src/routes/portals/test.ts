4e4a462bf76b0d24c4ade2da6d00a8689af08259
bigint <69431456+bigint@users.noreply.github.com>
2024-01-29 14:04:15 +0530

    chore: update url

--- apps/api/src/routes/portals/test.ts
+++ apps/api/src/routes/portals/test.ts
@@ -6,21 +6,35 @@ export const get: Handler = async (req, res) => {
   const { id } = req.query;
 
   let idd = Number(id as string) || 0;
-  // br if 0 brr if 1 brrr if 2 brrrr if 3
   const title = `br${'r'.repeat(idd)}`;
 
+  console.log(req);
+  console.log('gmgm');
+
   try {
-    // return html
     return res.send(`
       <html>
         <head>
           <meta charset="utf-8" />
+          <meta property="og:title" content="1337" />
+          <meta property="og:description" content="1337" />
+          <meta property="og:type" content="article" />
+          <meta property="og:title" content="#1" />
+          <meta property="og:image" content="https://fcframes.vercel.app/br${idd + 1}.gif" />
+
           <meta property="hey:portal" content="vLatest" />
           <meta property="hey:portal:image" content="https://fcframes.vercel.app/br${idd + 1}.gif" />
           <meta property="hey:portal:button:1" content="⏪" />
           <meta property="hey:portal:button:2" content="${title}" />
           <meta property="hey:portal:button:3" content="⏩︎" />
           <meta property="hey:portal:post_url" content="https://api.hey.xyz/portals/test?id=${idd + 1}" />
+
+          <meta property="fc:frame" content="vNext" />
+          <meta property="fc:frame:image" content="https://fcframes.vercel.app/br${idd + 1}.gif" />
+          <meta property="fc:frame:button:1" content="⏪" />
+          <meta property="fc:frame:button:2" content="br" />
+          <meta property="fc:frame:button:3" content="⏩︎" />
+          <meta property="fc:frame:post_url" content="https://fcframes.vercel.app/api/brians?id=${idd + 1}" />
         </head>
         <body>OK</body>
       </html>
