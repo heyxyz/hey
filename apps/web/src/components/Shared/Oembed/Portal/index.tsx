67bdc99449b87b78f965d63cc1024bf7299eadde
bigint <69431456+bigint@users.noreply.github.com>
2024-01-29 13:24:06 +0530

    feat: mvp button

--- apps/web/src/components/Shared/Oembed/Portal/index.tsx
+++ apps/web/src/components/Shared/Oembed/Portal/index.tsx
@@ -1,8 +1,11 @@
 import type { FC } from 'react';
 
+import { PUBLICATION } from '@hey/data/tracking';
 import stopEventPropagation from '@hey/lib/stopEventPropagation';
 import { Portal } from '@hey/types/misc';
-import { Card } from '@hey/ui';
+import { Button, Card } from '@hey/ui';
+import { Leafwatch } from '@lib/leafwatch';
+import toast from 'react-hot-toast';
 
 interface PortalProps {
   portal: Portal;
@@ -10,15 +13,48 @@ interface PortalProps {
 }
 
 const Portal: FC<PortalProps> = ({ portal, publicationId }) => {
+  const { buttons, image } = portal;
+
   return (
     <Card className="mt-3" forceRounded onClick={stopEventPropagation}>
       <img
-        alt={portal.image}
+        alt={image}
         className="h-[400px] max-h-[400px] w-full rounded-t-xl object-cover"
-        src={portal.image}
+        src={image}
       />
-      <div className="flex items-center justify-between border-t px-3 py-2 dark:border-gray-700">
-        gm
+      <div
+        className="grid gap-4 p-5 dark:border-gray-700"
+        style={{
+          gridTemplateColumns: buttons.length === 1 ? '1fr' : '1fr 1fr',
+          gridTemplateRows: buttons.length === 3 ? 'auto auto' : 'auto'
+        }}
+      >
+        {buttons.map(({ action, button, type }, index) => (
+          <Button
+            className={`${
+              buttons.length === 3 && index === 2 ? 'col-span-2' : 'col-span-1'
+            }`}
+            key={index}
+            onClick={() => {
+              Leafwatch.track(PUBLICATION.CLICK_PORTAL_BUTTON, {
+                publication_id: publicationId,
+                type
+              });
+
+              if (type === 'redirect') {
+                window.open(action, '_blank');
+              } else if (type === 'submit') {
+                toast.success('Not implemented yet, check back later!');
+              }
+            }}
+            outline
+            size="lg"
+            type={type === 'submit' ? 'submit' : 'button'}
+            variant="secondary"
+          >
+            {button}
+          </Button>
+        ))}
       </div>
     </Card>
   );
