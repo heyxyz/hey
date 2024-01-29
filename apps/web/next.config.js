5e9c8e0baf36e5a31d717d79bf0f6e0fee74bbf2
bigint <69431456+bigint@users.noreply.github.com>
2024-01-29 20:52:06 +0530

    feat: add portals docs link

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -21,43 +21,41 @@ const nextConfig = {
     return [
       {
         destination: 'https://discord.com/invite/B8eKhSSUwX',
-        permanent: true,
         source: '/discord'
       },
       {
         destination: 'https://giveth.io/project/hey?utm_source=hey',
-        permanent: true,
         source: '/donate'
       },
       {
         destination:
           'https://explorer.gitcoin.co/#/round/424/0xd4cc0dd193c7dc1d665ae244ce12d7fab337a008/0xd4cc0dd193c7dc1d665ae244ce12d7fab337a008-4',
-        permanent: true,
         source: '/gitcoin'
       },
       // Forms
       {
         destination:
           'https://hey.height.app/?taskForm=Token-Allowlist-Request-mwarXOg6ks0A',
-        permanent: true,
         source: '/-/token-request'
       },
       {
         destination:
           'https://hey.height.app/?taskForm=Verification-Request-fBxpj55hUMmf',
-        permanent: true,
         source: '/-/verification-request'
       },
       {
         destination: 'https://tally.so/r/3No6NQ',
-        permanent: true,
         source: '/-/trusted'
       },
       {
         destination:
           'https://reflect.site/g/yoginth/-hey-changelog/c6eae172c9cd43cebfc38b5afc64e456',
-        permanent: true,
         source: '/-/changelog'
+      },
+      {
+        destination:
+          'https://reflect.site/g/yoginth/hey-portal-open-graph-spec/cd7225f128274da382f1f516e7e63f15',
+        source: '/-/portals'
       }
     ];
   },
