f47146c0c81effa8b86abc53636822e296d8a0ac
bigint <69431456+bigint@users.noreply.github.com>
2024-01-30 12:59:05 +0530

    feat: support farcaster frames as well

--- apps/api/src/lib/oembed/meta/getPortal.ts
+++ apps/api/src/lib/oembed/meta/getPortal.ts
@@ -2,30 +2,33 @@ import type { ButtonType, Portal } from '@hey/types/misc';
 import type { Document } from 'linkedom';
 
 const getPortal = (document: Document): null | Portal => {
-  const version = document.querySelector('meta[property="hey:portal"]');
+  const version =
+    document.querySelector('meta[property="hey:portal"]') ||
+    document.querySelector('meta[property="fc:frame"]');
   const image =
     document.querySelector('meta[property="hey:portal:image"]') ||
-    document.querySelector('meta[property="og:image"]');
-  const postUrl = document.querySelector(
-    'meta[property="hey:portal:post_url"]'
-  );
+    document.querySelector('meta[property="og:image"]') ||
+    document.querySelector('meta[property="fc:frame:image"]');
+  const postUrl =
+    document.querySelector('meta[property="hey:portal:post_url"]') ||
+    document.querySelector('meta[property="fc:frame:post_url"]');
 
   let buttons: Portal['buttons'] = [];
   for (let i = 1; i < 5; i++) {
-    const button = document.querySelector(
-      `meta[property="hey:portal:button:${i}"]`
-    );
+    const button =
+      document.querySelector(`meta[property="hey:portal:button:${i}"]`) ||
+      document.querySelector(`meta[property="fc:frame:button:${i}"]`);
     const type = document.querySelector(
       `meta[property="hey:portal:button:${i}:type"]`
     );
 
-    if (!button || !type) {
+    if (!button) {
       break;
     }
 
     buttons.push({
       button: button?.getAttribute('content') as string,
-      type: type?.getAttribute('content') as ButtonType
+      type: (type?.getAttribute('content') || 'submit') as ButtonType
     });
   }
 
