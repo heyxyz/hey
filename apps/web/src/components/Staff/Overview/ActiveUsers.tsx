4366a12f6c4fd93647fdfe0fc0f6edf46d47b839
bigint <69431456+bigint@users.noreply.github.com>
2024-01-30 22:07:43 +0530

    feat: add active users table

--- apps/web/src/components/Staff/Overview/ActiveUsers.tsx
+++ apps/web/src/components/Staff/Overview/ActiveUsers.tsx
@@ -13,20 +13,18 @@ const ActiveUsers: FC<ActiveUsersProps> = ({ activeUsers }) => {
       <div className="p-5 text-lg font-bold">Daily Active Users</div>
       <div className="divider" />
       <div className="p-5">
-        <table className="border-default w-full border-x border-t">
+        <table className="w-full border-x border-t">
           <thead>
-            <tr className="border-default border-b">
-              <th className="border-default border-r px-2 text-left">Date</th>
+            <tr className="border-b">
+              <th className="border-r px-2 text-left">Date</th>
               <th className="border-r px-2 text-right">Active users</th>
               <th className="px-2 text-right">Total events</th>
             </tr>
           </thead>
           <tbody>
             {activeUsers.map((activeUser, index) => (
-              <tr className="border-default border-b" key={index}>
-                <td className="border-default border-r px-2">
-                  {activeUser.date}
-                </td>
+              <tr className="border-b" key={index}>
+                <td className="border-r px-2">{activeUser.date}</td>
                 <td className="border-r px-2 text-right">{activeUser.dau}</td>
                 <td className="px-2 text-right">{activeUser.events}</td>
               </tr>
