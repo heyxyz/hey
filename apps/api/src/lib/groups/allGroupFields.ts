6af0b218ddefb5bb8f2bda379091bb5caf0c0dd6
bigint <69431456+bigint@users.noreply.github.com>
2024-02-02 23:35:23 +0530

    feat: update all and featured list

---
+++ apps/api/src/lib/groups/allGroupFields.ts
@@ -0,0 +1,18 @@
+const allGroupFields = {
+  avatar: true,
+  createdAt: true,
+  creatorId: true,
+  description: true,
+  discord: true,
+  featured: true,
+  id: true,
+  instagram: true,
+  lens: true,
+  members: true,
+  name: true,
+  slug: true,
+  tags: true,
+  x: true
+};
+
+export default allGroupFields;
