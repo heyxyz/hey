01dad1530b9ec02555c4abee9b6bc5b2370be9e6
aschmidt20 <andrew@aave.com>
2024-02-08 01:12:05 -0600

    feat: add Decent NFT minting verified module

--- packages/data/verified-openaction-modules.ts
+++ packages/data/verified-openaction-modules.ts
@@ -1,6 +1,9 @@
 import { IS_MAINNET } from './constants';
 
 export const VerifiedOpenActionModules = {
+  DecentNFT: IS_MAINNET
+    ? '0x14860D0495CAF16914Db10117D4111AC4Da2F0a5'
+    : '0xe310b5Ed0B3c19B1F0852Ce985a4C38BAE738FDb',
   Tip: IS_MAINNET
     ? '0x22cb67432C101a9b6fE0F9ab542c8ADD5DD48153'
     : '0x6111e258a6d00d805DcF1249900895c7aA0cD186'
