c45aefa73868555f9095eb312d3b5bb80bf7e7ce
Bigint <69431456+bigint@users.noreply.github.com>
2024-02-08 19:23:57 +0530

    chore: remove tooltip

--- apps/web/src/components/Shared/Auth/Signup/Minting.tsx
+++ apps/web/src/components/Shared/Auth/Signup/Minting.tsx
@@ -1,5 +1,5 @@
 import { Spinner } from '@hey/ui';
-import { type FC } from 'react';
+import { type FC, useState } from 'react';
 import { useUpdateEffect } from 'usehooks-ts';
 import { useTransactionReceipt } from 'wagmi';
 
@@ -8,19 +8,21 @@ import { useSignupStore } from '.';
 const Minting: FC = () => {
   const setScreen = useSignupStore((state) => state.setScreen);
   const transactionHash = useSignupStore((state) => state.transactionHash);
+  const [receipt, setReceipt] = useState<any>();
 
-  const { status } = useTransactionReceipt({
+  const { data, status } = useTransactionReceipt({
     hash: transactionHash as `0x${string}`
   });
 
   useUpdateEffect(() => {
     if (status === 'success') {
-      setScreen('success');
+      setReceipt(data);
     }
   }, [status]);
 
   return (
     <div className="m-8 flex flex-col items-center justify-center">
+      {JSON.stringify(receipt)}
       <div className="text-xl font-bold">We are minting your profile!</div>
       <div className="ld-text-gray-500 mt-3 text-center font-semibold">
         This will take a few seconds to a few minutes. Please be patient.
