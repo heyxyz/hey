a93f5f2806ec781ebce6bb66d8746550bcc60158
aschmidt20 <andrew@aave.com>
2024-02-08 01:15:31 -0600

    feat: allowedUnknownOpenActionModule

--- packages/lib/allowedUnknownOpenActionModules.ts
+++ packages/lib/allowedUnknownOpenActionModules.ts
@@ -2,7 +2,7 @@ import { VerifiedOpenActionModules } from '@hey/data/verified-openaction-modules
 
 const allowedUnknownOpenActionModules = [
   VerifiedOpenActionModules.Tip,
-  VerifiedOpenActionModules.Decent
+  VerifiedOpenActionModules.DecentNFT
 ];
 
 export default allowedUnknownOpenActionModules;
