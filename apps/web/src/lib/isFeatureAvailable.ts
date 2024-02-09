6649bdb0a907b82788c7dec12f4a66570a9c60cc
Bigint <69431456+bigint@users.noreply.github.com>
2024-02-09 11:11:54 +0530

    fix: remove default true

--- apps/web/src/lib/isFeatureAvailable.ts
+++ apps/web/src/lib/isFeatureAvailable.ts
@@ -1,6 +1,5 @@
 import type { FeatureFlag } from '@hey/data/feature-flags';
 
-import { IS_PREVIEW, IS_PRODUCTION } from '@hey/data/constants';
 import { hydrateFeatureFlags } from 'src/store/persisted/useFeatureFlagsStore';
 
 import getCurrentSession from './getCurrentSession';
@@ -11,10 +10,6 @@ import getCurrentSession from './getCurrentSession';
  * @returns Whether the feature is enabled
  */
 const isFeatureAvailable = (key: FeatureFlag | string) => {
-  if (!IS_PRODUCTION || IS_PREVIEW) {
-    return true;
-  }
-
   const { id: sessionProfileId } = getCurrentSession();
   const featureFlags = hydrateFeatureFlags();
 
