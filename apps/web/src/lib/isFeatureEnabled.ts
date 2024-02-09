dae425abead200b767a5fd09a8da07d5547dff99
Bigint <69431456+bigint@users.noreply.github.com>
2024-02-09 11:54:24 +0530

    rip: kill switches

--- apps/web/src/lib/isFeatureEnabled.ts
+++ apps/web/src/lib/isFeatureEnabled.ts
@@ -1,20 +1,12 @@
-import type { KillSwitch } from '@hey/data/feature-flags';
-
-import { hydrateKillSwitches } from 'src/store/persisted/useKillSwitchesStore';
+import { enabledKillSwitches, type KillSwitch } from '@hey/data/feature-flags';
 
 /**
  * Checks if a feature is enabled globally
  * @param key The kill switch key
  * @returns Whether the feature is enabled
  */
-const isFeatureEnabled = (key: KillSwitch | string) => {
-  const killSwitches = hydrateKillSwitches();
-
-  if (!killSwitches) {
-    return false;
-  }
-
-  return killSwitches.includes(key);
+const isFeatureEnabled = (key: KillSwitch) => {
+  return enabledKillSwitches.includes(key);
 };
 
 export default isFeatureEnabled;
