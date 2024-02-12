7a648449d8ca88847f44781101420b8e0cbf89aa
bigint <69431456+bigint@users.noreply.github.com>
2024-02-12 18:51:01 +0530

    fix: update cache

--- apps/api/src/routes/staff-picks/index.ts
+++ apps/api/src/routes/staff-picks/index.ts
@@ -2,7 +2,7 @@ import type { Handler } from 'express';
 
 import logger from '@hey/lib/logger';
 import catchedError from 'src/lib/catchedError';
-import { SWR_CACHE_AGE_10_MINS_30_DAYS } from 'src/lib/constants';
+import { SWR_CACHE_AGE_5_MINS_30_DAYS } from 'src/lib/constants';
 import prisma from 'src/lib/prisma';
 
 export const get: Handler = async (_, res) => {
@@ -11,13 +11,13 @@ export const get: Handler = async (_, res) => {
       take: 30
     });
     const random = data.sort(() => Math.random() - Math.random());
-    const picks = random.slice(0, 7);
+    const picks = random.slice(0, 5);
 
     logger.info('Staff picks fetched');
 
     return res
       .status(200)
-      .setHeader('Cache-Control', SWR_CACHE_AGE_10_MINS_30_DAYS)
+      .setHeader('Cache-Control', SWR_CACHE_AGE_5_MINS_30_DAYS)
       .json({ result: picks, success: true });
   } catch (error) {
     return catchedError(res, error);
