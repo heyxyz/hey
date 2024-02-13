a0c54be88bc9c9f9311979cfc6fd15b5cf11c2bb
bigint <69431456+bigint@users.noreply.github.com>
2024-02-13 18:11:26 +0530

    chore: add polygon

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -11,9 +11,13 @@ const config: HardhatUserConfig = {
     apiKey: { polygonMumbai: process.env.POLYGON_ETHERSCAN_API_KEY! }
   },
   networks: {
-    polygonMumbai: {
+    polygon: {
       accounts: [process.env.PRIVATE_KEY!],
       url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
+    },
+    polygonMumbai: {
+      accounts: [process.env.PRIVATE_KEY!],
+      url: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
     }
   },
   solidity: '0.8.23',
