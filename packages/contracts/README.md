202caaa29e6dec99f4768c9937d9ec5fa7525714
bigint <69431456+bigint@users.noreply.github.com>
2024-02-15 00:48:53 +0530

    chore: update contracts and added method

--- packages/contracts/README.md
+++ packages/contracts/README.md
@@ -9,7 +9,7 @@ npx hardhat compile
 
 # Deploy to Mumbai
 npx hardhat run script/deploy.ts --network polygonMumbai
-npx hardhat verify 0x58e3E478e6581ca443650d2969354701CC6C53b8 --network polygonMumbai
+npx hardhat verify 0x6476d0E3eDd88BEc0d60b3D04079e1B124B13922 --network polygonMumbai
 
 # Deploy to Polygon
 npx hardhat run script/deploy.ts --network polygon
@@ -18,6 +18,6 @@ npx hardhat verify 0x11E6452982601F47bB2898c2761514CA73C26b48 --network polygon
 
 ## Contracts
 
-| Contract        | Mainnet                                                                                                                         | Mumbai                                                                                                                          |
-| --------------- | ------------------------------------------------------------------------------------------------------------------------------- | ------------------------------------------------------------------------------------------------------------------------------- |
-| `HeyLensSignup` | [0x58e3E478e6581ca443650d2969354701CC6C53b8](https://mumbai.polygonscan.com/address/0x58e3E478e6581ca443650d2969354701CC6C53b8) | [0x11E6452982601F47bB2898c2761514CA73C26b48](https://mumbai.polygonscan.com/address/0x11E6452982601F47bB2898c2761514CA73C26b48) |
+| Contract        | Mainnet                                                                                                                         | Mumbai                                                                                                                   |
+| --------------- | ------------------------------------------------------------------------------------------------------------------------------- | ------------------------------------------------------------------------------------------------------------------------ |
+| `HeyLensSignup` | [0x6476d0E3eDd88BEc0d60b3D04079e1B124B13922](https://mumbai.polygonscan.com/address/0x6476d0E3eDd88BEc0d60b3D04079e1B124B13922) | [0x11E6452982601F47bB2898c2761514CA73C26b48](https://polygonscan.com/address/0x11E6452982601F47bB2898c2761514CA73C26b48) |
--- packages/contracts/contracts/HeyLensSignup.sol
+++ packages/contracts/contracts/HeyLensSignup.sol
@@ -24,6 +24,12 @@ contract HeyLensSignup is Initializable, OwnableUpgradeable {
   uint256 public signupPrice;
   uint256 public profilesCreated;
   mapping(uint256 => bool) public profileCreated;
+  mapping(address => bool) public allowedAddresses;
+
+  modifier onlyAllowed() {
+    require(allowedAddresses[msg.sender], 'HeyLensSignup: Not allowed');
+    _;
+  }
 
   event ProfileCreated(uint256 profileId, uint256 handleId);
 
@@ -45,6 +51,24 @@ contract HeyLensSignup is Initializable, OwnableUpgradeable {
     profilesCreated = 0;
   }
 
+  function addAllowedAddresses(
+    address[] calldata newAddresses
+  ) external onlyOwner {
+    for (uint256 i = 0; i < newAddresses.length; i++) {
+      allowedAddresses[newAddresses[i]] = true;
+    }
+  }
+
+  function removeAllowedAddress(address addressToRemove) external onlyOwner {
+    allowedAddresses[addressToRemove] = false;
+  }
+
+  function isAllowedAddress(
+    address addressToCheck
+  ) external view returns (bool) {
+    return allowedAddresses[addressToCheck];
+  }
+
   function setSignupPrice(uint256 _signupPrice) external onlyOwner {
     signupPrice = _signupPrice;
   }
@@ -53,6 +77,29 @@ contract HeyLensSignup is Initializable, OwnableUpgradeable {
     address creatorAddress
   ) external onlyOwner {
     lensPermissionlessCreator = ILensPermissionlessCreator(creatorAddress);
+  }
+
+  function createProfileWithHandle(
+    CreateProfileParams calldata createProfileParams,
+    string calldata handle,
+    address[] calldata delegatedExecutors
+  ) external onlyAllowed returns (uint256 profileId, uint256 handleId) {
+    // Call the lensPermissionlessCreator to create a profile
+    (profileId, handleId) = lensPermissionlessCreator
+      .createProfileWithHandleUsingCredits(
+        createProfileParams,
+        handle,
+        delegatedExecutors
+      );
+
+    // Mark the profile as created
+    profileCreated[profileId] = true;
+    profilesCreated++;
+
+    // Emit the event to signal that a profile has been successfully created
+    emit ProfileCreated(profileId, handleId);
+
+    return (profileId, handleId);
   }
 
   function createProfileWithHandleUsingCredits(
