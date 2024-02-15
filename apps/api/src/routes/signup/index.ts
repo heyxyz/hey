1be68b030b549bc88e2ff2b3f7721b0728fa05d8
bigint <69431456+bigint@users.noreply.github.com>
2024-02-15 17:32:03 +0530

    feat: add paddle support

--- apps/api/src/routes/signup/index.ts
+++ apps/api/src/routes/signup/index.ts
@@ -3,7 +3,9 @@ import type { Handler } from 'express';
 import { HeyLensSignup } from '@hey/abis';
 import { HEY_LENS_SIGNUP, ZERO_ADDRESS } from '@hey/data/constants';
 import logger from '@hey/lib/logger';
+import axios from 'axios';
 import catchedError from 'src/lib/catchedError';
+import { PADDLE_API_ENDPOINT } from 'src/lib/constants';
 import { invalidBody, noBody, notAllowed } from 'src/lib/responses';
 import { createWalletClient, http } from 'viem';
 import { privateKeyToAccount } from 'viem/accounts';
@@ -11,15 +13,25 @@ import { polygonMumbai } from 'viem/chains';
 import { object, string } from 'zod';
 
 type ExtensionRequest = {
-  address: string;
-  delegatedExecutor: string;
-  handle: string;
+  data: {
+    custom_data: {
+      address: string;
+      delegatedExecutor: string;
+      handle: string;
+    };
+    id: string;
+  };
 };
 
 const validationSchema = object({
-  address: string(),
-  delegatedExecutor: string(),
-  handle: string()
+  data: object({
+    custom_data: object({
+      address: string(),
+      delegatedExecutor: string(),
+      handle: string()
+    }),
+    id: string()
+  })
 });
 
 export const post: Handler = async (req, res) => {
@@ -38,12 +50,28 @@ export const post: Handler = async (req, res) => {
   const validation = validationSchema.safeParse(body);
 
   if (!validation.success) {
+    console.error(validation.error);
     return invalidBody(res);
   }
 
-  const { address, delegatedExecutor, handle } = body as ExtensionRequest;
+  const { data } = body as ExtensionRequest;
+  const { custom_data, id } = data;
+  const { address, delegatedExecutor, handle } = custom_data;
 
   try {
+    // Check Paddle is the Transaction is valid
+    const response = await axios.get(
+      `${PADDLE_API_ENDPOINT}/transactions/${id}`,
+      { headers: { Authorization: `Bearer ${process.env.PADDLE_API_KEY}` } }
+    );
+
+    if (response.data.data.status !== 'completed') {
+      return res.status(400).json({
+        message: 'Transaction not completed',
+        success: false
+      });
+    }
+
     const account = privateKeyToAccount(
       process.env.RELAYER_PRIVATE_KEY as `0x${string}`
     );
