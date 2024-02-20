78561aa7f5ab7f250891a184a3ea4131bc58f1f8
bigint <69431456+bigint@users.noreply.github.com>
2024-02-20 13:57:59 +0530

    feat: add prisma clean and seed

---
+++ apps/api/src/db/seeds/seedProfileFeatures.ts
@@ -0,0 +1,30 @@
+import {
+  GARDENER_FEATURE_ID,
+  GARDENER_MODE_FEATURE_ID,
+  STAFF_FEATURE_ID,
+  STAFF_MODE_FEATURE_ID,
+  VERIFIED_FEATURE_ID
+} from '../../lib/constants';
+import { prisma } from '../seed';
+
+const seedProfileFeatures = async (): Promise<number> => {
+  const profileIds = ['0x0d', '0x06'];
+
+  const data = profileIds.map((profileId) => {
+    return [
+      { featureId: VERIFIED_FEATURE_ID, profileId },
+      { featureId: STAFF_FEATURE_ID, profileId },
+      { featureId: STAFF_MODE_FEATURE_ID, profileId },
+      { featureId: GARDENER_FEATURE_ID, profileId },
+      { featureId: GARDENER_MODE_FEATURE_ID, profileId }
+    ];
+  });
+
+  const profileFeatures = await prisma.profileFeature.createMany({
+    data: data.flat()
+  });
+
+  return profileFeatures.count;
+};
+
+export default seedProfileFeatures;
