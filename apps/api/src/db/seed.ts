49bc03a39187878e5e1f31e54221fc5ed1804b2e
bigint <69431456+bigint@users.noreply.github.com>
2024-02-21 11:47:56 +0530

    feat: add test cases for staff-picks/index

--- apps/api/src/db/seed.ts
+++ apps/api/src/db/seed.ts
@@ -6,6 +6,7 @@ import seedMembershipNfts from './seeds/seedMembershipNfts';
 import seedPolls from './seeds/seedPolls';
 import seedPreferences from './seeds/seedPreferences';
 import seedProfileFeatures from './seeds/seedProfileFeatures';
+import seedStaffPicks from './seeds/seedStaffPicks';
 
 export const prisma = new PrismaClient();
 
@@ -27,6 +28,9 @@ async function main() {
 
   const membershipNft = await seedMembershipNfts();
   console.log(`Seeded ${membershipNft} membership nft`);
+
+  const staffPicks = await seedStaffPicks();
+  console.log(`Seeded ${staffPicks} staff picks`);
 }
 
 main()
