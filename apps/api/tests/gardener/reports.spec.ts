a0e130afa51ce378728d01a5a5e920fb9a54f01e
bigint <69431456+bigint@users.noreply.github.com>
2024-02-21 23:18:15 +0530

    chore: remove has reported from the api

--- apps/api/tests/gardener/reports.spec.ts
+++ apps/api/tests/gardener/reports.spec.ts
@@ -13,6 +13,5 @@ describe('gardener/reports', () => {
     expect(response.data.result.id).toEqual('0x8c9c-0x12-DA-b1a993ad');
     expect(response.data.result.actor).toEqual('0x0274');
     expect(response.data.result.both).toBeGreaterThan(0);
-    expect(response.data.result.hasReported).toBe(true);
   });
 });
