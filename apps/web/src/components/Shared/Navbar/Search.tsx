16a3dca7df04e73da955a11276e5417c3a00ac02
bigint <69431456+bigint@users.noreply.github.com>
2024-02-21 16:34:24 +0530

    feat: add leafwatch event for search

--- apps/web/src/components/Shared/Navbar/Search.tsx
+++ apps/web/src/components/Shared/Navbar/Search.tsx
@@ -2,7 +2,7 @@ import type { Profile, ProfileSearchRequest } from '@hey/lens';
 import type { ChangeEvent, FC, MutableRefObject } from 'react';
 
 import { MagnifyingGlassIcon, XMarkIcon } from '@heroicons/react/24/outline';
-import { ProfileLinkSource } from '@hey/data/tracking';
+import { MISCELLANEOUS, ProfileLinkSource } from '@hey/data/tracking';
 import {
   CustomFiltersType,
   LimitType,
@@ -10,6 +10,7 @@ import {
 } from '@hey/lens';
 import { Card, Input, Spinner } from '@hey/ui';
 import cn from '@hey/ui/cn';
+import { Leafwatch } from '@lib/leafwatch';
 import { useClickAway, useDebounce } from '@uidotdev/usehooks';
 import { motion } from 'framer-motion';
 import { useRouter } from 'next/router';
@@ -45,6 +46,7 @@ const Search: FC<SearchProps> = ({
 
   const handleKeyDown = (evt: ChangeEvent<HTMLFormElement>) => {
     evt.preventDefault();
+    Leafwatch.track(MISCELLANEOUS.SEARCH, { query: searchText });
     if (pathname === '/search') {
       push(`/search?q=${encodeURIComponent(searchText)}&type=${query.type}`);
     } else {
