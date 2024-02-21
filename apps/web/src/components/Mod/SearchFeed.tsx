d2e66458060e655547ece16e856f562d2c387049
bigint <69431456+bigint@users.noreply.github.com>
2024-02-21 15:15:15 +0530

    feat: add SearchFeed in mod page

---
+++ apps/web/src/components/Mod/SearchFeed.tsx
@@ -0,0 +1,121 @@
+import type { AnyPublication, PublicationSearchRequest } from '@hey/lens';
+import type { FC } from 'react';
+
+import GardenerActions from '@components/Publication/Actions/GardenerActions';
+import SinglePublication from '@components/Publication/SinglePublication';
+import PublicationsShimmer from '@components/Shared/Shimmer/PublicationsShimmer';
+import { RectangleStackIcon } from '@heroicons/react/24/outline';
+import {
+  CustomFiltersType,
+  LimitType,
+  useSearchPublicationsQuery
+} from '@hey/lens';
+import { Button, Card, EmptyState, ErrorMessage, Input } from '@hey/ui';
+import { useState } from 'react';
+import { useInView } from 'react-cool-inview';
+
+const SearchFeed: FC = () => {
+  const [query, setQuery] = useState('');
+  const [value, setValue] = useState('');
+
+  // Variables
+  const request: PublicationSearchRequest = {
+    limit: LimitType.TwentyFive,
+    query,
+    where: { customFilters: [CustomFiltersType.Gardeners] }
+  };
+
+  const { data, error, fetchMore, loading } = useSearchPublicationsQuery({
+    variables: { request }
+  });
+
+  const publications = data?.searchPublications?.items;
+  const pageInfo = data?.searchPublications?.pageInfo;
+  const hasMore = pageInfo?.next;
+
+  const { observe } = useInView({
+    onChange: async ({ inView }) => {
+      if (!inView || !hasMore) {
+        return;
+      }
+
+      await fetchMore({
+        variables: { request: { ...request, cursor: pageInfo?.next } }
+      });
+    }
+  });
+
+  const Search = () => {
+    return (
+      <form
+        className="flex items-center space-x-2"
+        onSubmit={() => setQuery(value)}
+      >
+        <Input
+          autoFocus
+          onChange={(event) => {
+            setValue(event.target.value);
+          }}
+          placeholder="Search Publications"
+          type="text"
+          value={value}
+        />
+        <Button size="lg" type="submit">
+          Search
+        </Button>
+      </form>
+    );
+  };
+
+  if (loading) {
+    return (
+      <div className="space-y-5">
+        <Search />
+        <PublicationsShimmer />
+      </div>
+    );
+  }
+
+  if (publications?.length === 0) {
+    return (
+      <div className="space-y-5">
+        <Search />
+        <EmptyState
+          icon={<RectangleStackIcon className="text-brand-500 size-8" />}
+          message="No posts yet!"
+        />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <ErrorMessage error={error} title="Failed to load search feed" />;
+  }
+
+  return (
+    <div className="space-y-5">
+      <Search />
+      {publications?.map((publication, index) => (
+        <Card key={`${publication.id}_${index}`}>
+          <SinglePublication
+            isFirst
+            isLast={false}
+            publication={publication as AnyPublication}
+            showActions={false}
+            showThread={false}
+          />
+          <div>
+            <div className="divider" />
+            <div className="m-5 space-y-2">
+              <b>Gardener actions</b>
+              <GardenerActions publicationId={publication.id} />
+            </div>
+          </div>
+        </Card>
+      ))}
+      {hasMore ? <span ref={observe} /> : null}
+    </div>
+  );
+};
+
+export default SearchFeed;
