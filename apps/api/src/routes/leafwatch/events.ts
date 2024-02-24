bc1311b1e952d243c09d4585b20da6fc94e0d12d
bigint <69431456+bigint@users.noreply.github.com>
2024-02-24 20:59:08 +0530

    chore: update event

--- apps/api/src/routes/leafwatch/events.ts
+++ apps/api/src/routes/leafwatch/events.ts
@@ -6,9 +6,12 @@ import requestIp from 'request-ip';
 import catchedError from 'src/lib/catchedError';
 import createClickhouseClient from 'src/lib/createClickhouseClient';
 import checkEventExistence from 'src/lib/leafwatch/checkEventExistence';
+import { notionText, notionTitle } from 'src/lib/notion/notionBlocks';
+import pushToNotionDatabase from 'src/lib/notion/pushToNotionDatabase';
 import { invalidBody, noBody } from 'src/lib/responses';
 import UAParser from 'ua-parser-js';
 import urlcat from 'urlcat';
+import { v4 as uuid } from 'uuid';
 import { any, object, string } from 'zod';
 
 type ExtensionRequest = {
@@ -109,6 +112,23 @@ export const post: Handler = async (req, res) => {
           utm_term: utmTerm || null
         }
       ]
+    });
+
+    // Ingest to Notion for auditing with some retention policy
+    pushToNotionDatabase('88afbf03f9204a949e80360fa8ebfd0f', {
+      Actor: notionText(actor || 'N/A'),
+      Browser: notionText(ua.browser.name || 'N/A'),
+      'Browser Version': notionText(ua.browser.version || 'N/A'),
+      City: notionText(ipData?.city || 'N/A'),
+      Country: notionText(ipData?.country || 'N/A'),
+      Event: notionText(name),
+      ID: notionTitle(uuid()),
+      IP: notionText(ip || 'N/A'),
+      OS: notionText(ua.os.name || 'N/A'),
+      Properties: notionText(JSON.stringify(properties) || 'N/A'),
+      Referrer: notionText(referrer || 'N/A'),
+      Region: notionText(ipData?.regionName || 'N/A'),
+      Url: notionText(url || 'N/A')
     });
 
     logger.info('Ingested event to Leafwatch');
