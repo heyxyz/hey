00eeb43f01b626ed6a1ff753ddebbddcde8fefb6
bigint <69431456+bigint@users.noreply.github.com>
2024-02-24 21:36:53 +0530

    chore: remove lens checks on OG

--- apps/api/src/lib/oembed/meta/getIsLarge.ts
+++ apps/api/src/lib/oembed/meta/getIsLarge.ts
@@ -1,19 +1,11 @@
 import type { Document } from 'linkedom';
 
 const getIsLarge = (document: Document): boolean | null => {
-  const lens =
-    document.querySelector('meta[name="lens:card"]') ||
-    document.querySelector('meta[property="lens:card"]');
   const twitter =
     document.querySelector('meta[name="twitter:card"]') ||
     document.querySelector('meta[property="twitter:card"]');
 
   const largeTypes = ['summary_large_image', 'player'];
-
-  if (lens) {
-    const card = lens.getAttribute('content') || '';
-    return largeTypes.includes(card);
-  }
 
   if (twitter) {
     const card = twitter.getAttribute('content') || '';
--- apps/api/src/lib/oembed/meta/getSite.ts
+++ apps/api/src/lib/oembed/meta/getSite.ts
@@ -1,19 +1,12 @@
 import type { Document } from 'linkedom';
 
 const getSite = (document: Document): null | string => {
-  const lens =
-    document.querySelector('meta[name="lens:site"]') ||
-    document.querySelector('meta[property="lens:site"]');
   const og =
     document.querySelector('meta[name="og:site_name"]') ||
     document.querySelector('meta[property="og:site_name"]');
   const twitter =
     document.querySelector('meta[name="twitter:site"]') ||
     document.querySelector('meta[property="twitter:site"]');
-
-  if (lens) {
-    return lens.getAttribute('content');
-  }
 
   if (og) {
     return og.getAttribute('content');
