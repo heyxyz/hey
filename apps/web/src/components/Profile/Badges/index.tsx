4e9b150bdf8fb54747771c8590b36dacf7918786
bigint <69431456+bigint@users.noreply.github.com>
2024-02-26 20:42:03 +0530

    feat: add hey profile badge on ui

--- apps/web/src/components/Profile/Badges/index.tsx
+++ apps/web/src/components/Profile/Badges/index.tsx
@@ -1,21 +1,42 @@
 import type { ProfileOnchainIdentity } from '@hey/lens';
 import type { FC } from 'react';
 
+import { HEY_API_URL } from '@hey/data/constants';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
 import Ens from './Ens';
+import HeyProfile from './HeyProfile';
 import ProofOfHumanity from './ProofOfHumanity';
 import Sybil from './Sybil';
 import Worldcoin from './Worldcoin';
 
 interface BadgesProps {
+  id: string;
   onchainIdentity: ProfileOnchainIdentity;
 }
 
-const Badges: FC<BadgesProps> = ({ onchainIdentity }) => {
+const Badges: FC<BadgesProps> = ({ id, onchainIdentity }) => {
+  const getIsHeyProfile = async (): Promise<boolean> => {
+    const response = await axios.get(`${HEY_API_URL}/badges/isHeyProfile`, {
+      params: { id }
+    });
+    const { data } = response;
+
+    return data?.isHeyProfile || false;
+  };
+
+  const { data: isHeyProfile } = useQuery({
+    queryFn: getIsHeyProfile,
+    queryKey: ['getIsHeyProfile', id]
+  });
+
   const hasOnChainIdentity =
     onchainIdentity?.proofOfHumanity ||
     onchainIdentity?.sybilDotOrg?.verified ||
     onchainIdentity?.ens?.name ||
-    onchainIdentity?.worldcoin?.isHuman;
+    onchainIdentity?.worldcoin?.isHuman ||
+    isHeyProfile;
 
   if (!hasOnChainIdentity) {
     return null;
@@ -29,6 +50,7 @@ const Badges: FC<BadgesProps> = ({ onchainIdentity }) => {
         <Ens onchainIdentity={onchainIdentity} />
         <Sybil onchainIdentity={onchainIdentity} />
         <Worldcoin onchainIdentity={onchainIdentity} />
+        {isHeyProfile && <HeyProfile />}
       </div>
     </>
   );
