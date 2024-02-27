6a9822e4bcbebe555687e09ec3716c9a6569085f
bigint <69431456+bigint@users.noreply.github.com>
2024-02-27 15:56:52 +0530

    feat: merge staff to features list

--- apps/api/src/routes/staff-picks/index.ts
+++ apps/api/src/routes/staff-picks/index.ts
@@ -2,14 +2,19 @@ import type { Handler } from 'express';
 
 import logger from '@hey/lib/logger';
 import catchedError from 'src/lib/catchedError';
-import { SWR_CACHE_AGE_10_SECS_30_DAYS } from 'src/lib/constants';
+import {
+  STAFF_PICK_FEATURE_ID,
+  SWR_CACHE_AGE_10_SECS_30_DAYS
+} from 'src/lib/constants';
 import prisma from 'src/lib/prisma';
 
 export const get: Handler = async (_, res) => {
   try {
-    const data = await prisma.staffPick.findMany({
-      take: 30
+    const data = await prisma.profileFeature.findMany({
+      select: { profileId: true },
+      where: { featureId: STAFF_PICK_FEATURE_ID }
     });
+
     const random = data.sort(() => Math.random() - Math.random());
     const picks = random.slice(0, 5);
 
