66a6335335daed88303995fe4103b18428bc9f09
bigint <69431456+bigint@users.noreply.github.com>
2024-02-27 21:02:45 +0530

    fix: query param after signup

--- apps/web/src/components/Shared/GlobalModalsFromUrl.tsx
+++ apps/web/src/components/Shared/GlobalModalsFromUrl.tsx
@@ -8,7 +8,7 @@ import useProfileStore from 'src/store/persisted/useProfileStore';
 import { useSignupStore } from './Auth/Signup';
 
 const GlobalModalsFromUrl: FC = () => {
-  const { isReady, query } = useRouter();
+  const { isReady, push, query } = useRouter();
 
   const currentProfile = useProfileStore((state) => state.currentProfile);
   const setShowAuthModal = useGlobalModalStateStore(
@@ -18,10 +18,13 @@ const GlobalModalsFromUrl: FC = () => {
 
   // Trigger Signup modal
   useEffect(() => {
-    if (isReady && query?.signup && !currentProfile) {
+    if (isReady && query?.signup && !currentProfile?.id) {
       setScreen('choose');
       setShowAuthModal(true, 'signup');
       Leafwatch.track(AUTH.OPEN_SIGNUP);
+
+      // remove query param
+      push({ pathname: '/' }, undefined, { shallow: true });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [query]);
