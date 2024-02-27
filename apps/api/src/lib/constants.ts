8642a708101d981490095cb2de19497a2071cb06
bigint <69431456+bigint@users.noreply.github.com>
2024-02-27 15:56:52 +0530

    feat: merge staff to features list

--- apps/api/src/lib/constants.ts
+++ apps/api/src/lib/constants.ts
@@ -9,6 +9,7 @@ export const STAFF_MODE_FEATURE_ID = '0e588583-b347-4752-9e1e-0ad4128348e8';
 export const GARDENER_FEATURE_ID = '0a441129-182a-4a3f-83cf-a13c5ad8282b';
 export const GARDENER_MODE_FEATURE_ID = '9f66a465-e1d7-4123-b329-ddd14fd85510';
 export const VERIFIED_FEATURE_ID = 'a0d6d247-50ef-419f-a045-54fa96054922';
+export const STAFF_PICK_FEATURE_ID = '73d2f48d-0291-4a36-adc2-9737057ad2b7';
 
 // Cache
 // Cache for 1 second, stale for 30 days
