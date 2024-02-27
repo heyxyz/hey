801f01d358bba62065d27be26808a11579df327e
bigint <69431456+bigint@users.noreply.github.com>
2024-02-27 15:57:23 +0530

    feat: merge staff to features list

--- apps/api/src/db/seeds/seedProfileFeatures.ts
+++ apps/api/src/db/seeds/seedProfileFeatures.ts
@@ -1,18 +1,20 @@
 import {
   GARDENER_FEATURE_ID,
   STAFF_FEATURE_ID,
+  STAFF_PICK_FEATURE_ID,
   VERIFIED_FEATURE_ID
 } from '../../lib/constants';
 import { prisma } from '../seed';
 
 const seedProfileFeatures = async (): Promise<number> => {
-  const profileIds = ['0x0d', '0x06', '0x0383'];
+  const profileIds = ['0x0d', '0x06', '0x0383', '0x01', '0x02', '0x03'];
 
   const data = profileIds.map((profileId) => {
     return [
       { featureId: VERIFIED_FEATURE_ID, profileId },
       { featureId: STAFF_FEATURE_ID, profileId },
-      { featureId: GARDENER_FEATURE_ID, profileId }
+      { featureId: GARDENER_FEATURE_ID, profileId },
+      { featureId: STAFF_PICK_FEATURE_ID, profileId }
     ];
   });
 
