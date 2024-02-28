d24434a3b5f8c3aca4ace5447d7a4c23ee199953
bigint <69431456+bigint@users.noreply.github.com>
2024-02-28 16:09:21 +0530

    fix: add CACHE_AGE_INDEFINITE if true (#4694)

--- apps/api/src/routes/badges/hasHeyNft.ts
+++ apps/api/src/routes/badges/hasHeyNft.ts
@@ -4,7 +4,11 @@ import type { Address } from 'viem';
 import { IS_MAINNET } from '@hey/data/constants';
 import logger from '@hey/lib/logger';
 import catchedError from 'src/lib/catchedError';
-import { RPC_URL, SWR_CACHE_AGE_10_MINS_30_DAYS } from 'src/lib/constants';
+import {
+  CACHE_AGE_INDEFINITE,
+  RPC_URL,
+  SWR_CACHE_AGE_10_MINS_30_DAYS
+} from 'src/lib/constants';
 import { noBody } from 'src/lib/responses';
 import { createPublicClient, http } from 'viem';
 import { polygon } from 'viem/chains';
@@ -22,7 +26,7 @@ export const get: Handler = async (req, res) => {
       transport: http(RPC_URL)
     });
 
-    const hasHeyNft = await client.readContract({
+    const heyNft = await client.readContract({
       abi: [
         {
           inputs: [{ internalType: 'address', name: 'owner', type: 'address' }],
@@ -38,13 +42,17 @@ export const get: Handler = async (req, res) => {
       args: [address as Address],
       functionName: 'balanceOf'
     });
+    const hasHeyNft = Number(heyNft) > 0;
 
     logger.info(`Hey NFT badge fetched for ${address}`);
 
     return res
       .status(200)
-      .setHeader('Cache-Control', SWR_CACHE_AGE_10_MINS_30_DAYS)
-      .json({ hasHeyNft: Number(hasHeyNft) > 0, success: true });
+      .setHeader(
+        'Cache-Control',
+        hasHeyNft ? CACHE_AGE_INDEFINITE : SWR_CACHE_AGE_10_MINS_30_DAYS
+      )
+      .json({ hasHeyNft, success: true });
   } catch (error) {
     return catchedError(res, error);
   }
