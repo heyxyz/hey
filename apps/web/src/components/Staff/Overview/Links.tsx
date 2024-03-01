0cfd0b6e2d2738ea33804a5c1e17735fff4cc790
bigint <69431456+bigint@users.noreply.github.com>
2024-03-01 16:31:22 +0530

    feat: use black as primary

--- apps/web/src/components/Staff/Overview/Links.tsx
+++ apps/web/src/components/Staff/Overview/Links.tsx
@@ -20,7 +20,7 @@ const Links: FC = () => {
     <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-2 md:gap-y-8">
       <div>
         <p className="mb-3 font-bold">Monitoring 📈</p>
-        <ul className="linkify mb-4 mt-0 list-disc space-y-1 pl-5">
+        <ul className="mb-4 mt-0 list-disc space-y-1 pl-5">
           <ListItem
             link="https://hey.metabaseapp.com/public/dashboard/3ee79f5b-07d7-43d7-9237-58c6442a1ad8#refresh=2"
             title="Leafwatch Overview"
@@ -37,7 +37,7 @@ const Links: FC = () => {
       </div>
       <div>
         <p className="mb-4 font-bold">Other helpful links 🌱</p>
-        <ul className="linkify mb-3 mt-0 list-disc space-y-1 pl-5">
+        <ul className="mb-3 mt-0 list-disc space-y-1 pl-5">
           <ListItem
             link="https://app.lemonsqueezy.com/dashboard"
             title="Lemon Squeezy"
@@ -58,7 +58,7 @@ const Links: FC = () => {
       </div>
       <div>
         <p className="mb-4 font-bold">Forms 📜</p>
-        <ul className="linkify mb-3 mt-0 list-disc space-y-1 pl-5">
+        <ul className="mb-3 mt-0 list-disc space-y-1 pl-5">
           <ListItem
             link="https://hey.xyz/-/token-request"
             title="Token Allowlist Request"
