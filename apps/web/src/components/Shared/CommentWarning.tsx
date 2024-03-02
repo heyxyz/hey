45b380071d11583fe946d1948781e894d3901195
bigint <69431456+bigint@users.noreply.github.com>
2024-03-02 19:04:21 +0530

    chore: update to use ChatBubbleLeftIcon

--- apps/web/src/components/Shared/CommentWarning.tsx
+++ apps/web/src/components/Shared/CommentWarning.tsx
@@ -1,12 +1,12 @@
 import type { FC } from 'react';
 
-import { ChatBubbleLeftRightIcon } from '@heroicons/react/24/outline';
+import { ChatBubbleLeftIcon } from '@heroicons/react/24/outline';
 import { Card } from '@hey/ui';
 
 const CommentWarning: FC = () => {
   return (
     <Card className="flex items-center space-x-1.5 border-blue-300 !bg-blue-100 p-5 text-sm font-bold text-gray-500">
-      <ChatBubbleLeftRightIcon className="size-4 text-blue-500" />
+      <ChatBubbleLeftIcon className="size-4 text-blue-500" />
       <span>You can't reply to this post</span>
     </Card>
   );
