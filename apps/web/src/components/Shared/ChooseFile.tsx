cc2ae0d7fdd093858b8d30adb8e61b669b474ca5
bigint <69431456+bigint@users.noreply.github.com>
2024-03-03 22:05:04 +0530

    fix: remove brand usage

--- apps/web/src/components/Shared/ChooseFile.tsx
+++ apps/web/src/components/Shared/ChooseFile.tsx
@@ -8,11 +8,9 @@ const ChooseFile: FC<ChooseFileProps> = ({ onChange }) => {
   return (
     <input
       accept=".png, .jpg, .jpeg, .gif"
-      className="focus:border-brand-400 cursor-pointer rounded-xl border border-gray-300 bg-white pr-1 text-sm text-gray-700 shadow-sm focus:outline-none dark:border-gray-700 dark:bg-gray-800 dark:text-white"
+      className="cursor-pointer rounded-xl border border-gray-300 bg-white pr-1 text-sm text-gray-700 shadow-sm outline-offset-4 dark:border-gray-700 dark:bg-gray-800 dark:text-white"
       onChange={onChange}
       onClick={(event) => {
-        // reset the value so that an onChange event is generated also when the user selects the same filename again
-        // this allows the user to make a new attempt with the same image
         (event.target as HTMLInputElement).value = '';
       }}
       type="file"
