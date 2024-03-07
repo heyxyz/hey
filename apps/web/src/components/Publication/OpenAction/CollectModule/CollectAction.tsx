1456d5c0982c42ab24ee777900595c91418795b8
bigint <69431456+bigint@users.noreply.github.com>
2024-03-07 22:33:48 +0530

    chore: remove refs

--- apps/web/src/components/Publication/OpenAction/CollectModule/CollectAction.tsx
+++ apps/web/src/components/Publication/OpenAction/CollectModule/CollectAction.tsx
@@ -331,8 +331,7 @@ const CollectAction: FC<CollectActionProps> = ({
       setIsLoading(true);
       if (isLegacyCollectModule) {
         const legcayCollectRequest: LegacyCollectRequest = {
-          on: targetPublication?.id,
-          referrer: [publication.by.id]
+          on: targetPublication?.id
         };
 
         if (canUseManager) {
@@ -349,8 +348,7 @@ const CollectAction: FC<CollectActionProps> = ({
 
       const actOnRequest: ActOnOpenActionLensManagerRequest = {
         actOn: { [getOpenActionActOnKey(openAction.type)]: true },
-        for: targetPublication?.id,
-        referrers: [publication.by.id]
+        for: targetPublication?.id
       };
 
       if (canUseManager) {
