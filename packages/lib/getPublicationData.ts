08c31f72db2b48b1cb1a62ee5b56232dc323a15a
bigint <69431456+bigint@users.noreply.github.com>
2024-03-08 22:10:44 +0530

    feat: add title only for tape

--- packages/lib/getPublicationData.ts
+++ packages/lib/getPublicationData.ts
@@ -1,6 +1,7 @@
 import type { PublicationMetadata } from '@hey/lens';
 import type { MetadataAsset } from '@hey/types/misc';
 
+import ALLOWED_APP_FOR_TITLE from '@hey/data/allowed-app-for-title';
 import { PLACEHOLDER_IMAGE } from '@hey/data/constants';
 
 import getAttachmentsData from './getAttachmentsData';
@@ -15,17 +16,28 @@ const getPublicationData = (
   }[];
   content?: string;
 } | null => {
+  console.log(metadata.appId);
+  const showTitle = ALLOWED_APP_FOR_TITLE.includes(metadata.appId);
+  const willHaveTitle =
+    metadata.__typename === 'ArticleMetadataV3' ||
+    metadata.__typename === 'ImageMetadataV3' ||
+    metadata.__typename === 'AudioMetadataV3' ||
+    metadata.__typename === 'VideoMetadataV3' ||
+    metadata.__typename === 'LiveStreamMetadataV3';
+  const canShowTitle = showTitle && willHaveTitle;
+  const content = canShowTitle
+    ? `${metadata.title}\n\n${metadata.content}`
+    : metadata.content;
+
   switch (metadata.__typename) {
     case 'ArticleMetadataV3':
       return {
         attachments: getAttachmentsData(metadata.attachments),
-        content: metadata.content
+        content
       };
     case 'TextOnlyMetadataV3':
     case 'LinkMetadataV3':
-      return {
-        content: metadata.content
-      };
+      return { content };
     case 'ImageMetadataV3':
       return {
         asset: {
@@ -33,7 +45,7 @@ const getPublicationData = (
           uri: metadata.asset.image.optimized?.uri
         },
         attachments: getAttachmentsData(metadata.attachments),
-        content: metadata.content
+        content
       };
     case 'AudioMetadataV3': {
       const audioAttachments = getAttachmentsData(metadata.attachments)[0];
@@ -50,7 +62,7 @@ const getPublicationData = (
           type: 'Audio',
           uri: metadata.asset.audio.optimized?.uri || audioAttachments?.uri
         },
-        content: metadata.content
+        content
       };
     }
     case 'VideoMetadataV3': {
@@ -66,7 +78,7 @@ const getPublicationData = (
           type: 'Video',
           uri: metadata.asset.video.optimized?.uri || videoAttachments?.uri
         },
-        content: metadata.content
+        content
       };
     }
     case 'MintMetadataV3':
@@ -77,7 +89,7 @@ const getPublicationData = (
     case 'LiveStreamMetadataV3':
       return {
         attachments: getAttachmentsData(metadata.attachments),
-        content: metadata.content
+        content
       };
     default:
       return null;
