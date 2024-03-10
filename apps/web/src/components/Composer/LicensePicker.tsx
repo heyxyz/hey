9a1c232f9a0bee744a09fa6f90bb202c1ef8ee32
bigint <69431456+bigint@users.noreply.github.com>
2024-03-10 20:56:42 +0530

    chore: add back ff for LicensePicker

--- apps/web/src/components/Composer/LicensePicker.tsx
+++ apps/web/src/components/Composer/LicensePicker.tsx
@@ -2,16 +2,23 @@ import type { MetadataLicenseType } from '@lens-protocol/metadata';
 import type { FC } from 'react';
 
 import { ExclamationCircleIcon } from '@heroicons/react/24/outline';
+import { FeatureFlag } from '@hey/data/feature-flags';
 import { PublicationMetadataLicenseType } from '@hey/lens';
 import { Select } from '@hey/ui';
 import getAssetLicense from '@lib/getAssetLicense';
+import isFeatureAvailable from '@lib/isFeatureAvailable';
 import { usePublicationLicenseStore } from 'src/store/non-persisted/publication/usePublicationLicenseStore';
 
 const LicensePicker: FC = () => {
   const { setLicense } = usePublicationLicenseStore();
 
+  if (!isFeatureAvailable(FeatureFlag.Staff)) {
+    return null;
+  }
+
   return (
-    <div className="m-5">
+    <div className="my-5">
+      <div className="divider mb-3" />
       <div className="mb-2 flex items-center justify-between">
         <b>License</b>
         <div className="ld-text-gray-500 text-sm">What's this?</div>
