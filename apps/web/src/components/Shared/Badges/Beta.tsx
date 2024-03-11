db289ca138924870dcd630e12a349021ae29583c
bigint <69431456+bigint@users.noreply.github.com>
2024-03-11 12:57:13 +0530

    Update apps/web/src/components/Shared/Badges/Beta.tsx

--- apps/web/src/components/Shared/Badges/Beta.tsx
+++ apps/web/src/components/Shared/Badges/Beta.tsx
@@ -5,7 +5,7 @@ import { Badge } from '@hey/ui';
 
 const Beta: FC = () => {
   return (
-    <Badge className="flex items-center space-x-1 py-1">
+    <Badge className="flex items-center space-x-1">
       <StarIcon className="size-3" />
       <span>Beta</span>
     </Badge>
