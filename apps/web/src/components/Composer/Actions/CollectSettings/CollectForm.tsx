1630411406034d94318840ef651b5f66aa8f8995
bigint <69431456+bigint@users.noreply.github.com>
2024-03-12 19:20:50 +0530

    feat: license picker GA

--- apps/web/src/components/Composer/Actions/CollectSettings/CollectForm.tsx
+++ apps/web/src/components/Composer/Actions/CollectSettings/CollectForm.tsx
@@ -3,11 +3,9 @@ import type { Dispatch, FC, SetStateAction } from 'react';
 
 import LicensePicker from '@components/Composer/LicensePicker';
 import ToggleWithHelper from '@components/Shared/ToggleWithHelper';
-import { FeatureFlag } from '@hey/data/feature-flags';
 import { CollectOpenActionModuleType } from '@hey/lens';
 import getAllTokens from '@hey/lib/api/getAllTokens';
 import { Button, ErrorMessage, Spinner } from '@hey/ui';
-import isFeatureAvailable from '@lib/isFeatureAvailable';
 import { useQuery } from '@tanstack/react-query';
 import { useCollectModuleStore } from 'src/store/non-persisted/publication/useCollectModuleStore';
 import { isAddress } from 'viem';
@@ -114,14 +112,10 @@ const CollectForm: FC<CollectFormProps> = ({ setShowModal }) => {
             <TimeLimitConfig setCollectType={setCollectType} />
             <FollowersConfig setCollectType={setCollectType} />
           </div>
-          {isFeatureAvailable(FeatureFlag.Staff) && (
-            <>
-              <div className="divider" />
-              <div className="m-5">
-                <LicensePicker />
-              </div>
-            </>
-          )}
+          <div className="divider" />
+          <div className="m-5">
+            <LicensePicker />
+          </div>
           <div className="divider" />
         </>
       ) : null}
