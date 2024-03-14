157d7479188b06f7b6af350af9ade943d8f699ef
bigint <69431456+bigint@users.noreply.github.com>
2024-03-14 21:39:45 +0530

    fix: typo

--- apps/web/src/components/Settings/Manager/ProfileManager/Management/Unmanaged.tsx
+++ apps/web/src/components/Settings/Manager/ProfileManager/Management/Unmanaged.tsx
@@ -5,7 +5,7 @@ import List from './List';
 const Unmanaged: FC = () => {
   return (
     <div className="pt-2">
-      <div>Profiles under your oversight and management.</div>
+      <div>Profiles under your oversight but not under your management.</div>
       <div className="divider my-5" />
       <List />
     </div>
