311f9180579eaeb65d96a208157bb9b9d25ab1ea
bigint <69431456+bigint@users.noreply.github.com>
2024-03-17 03:42:09 +0530

    chore: update license link

--- apps/web/src/lib/getAssetLicense.ts
+++ apps/web/src/lib/getAssetLicense.ts
@@ -7,7 +7,6 @@ const getAssetLicense = (
 ): {
   helper: string;
   label: string;
-  link: string;
 } | null => {
   if (!licenseId) {
     return null;
@@ -18,22 +17,19 @@ const getAssetLicense = (
       return {
         helper:
           'Anyone can use, modify and distribute the work without any restrictions or need for attribution. CC0',
-        label: 'CC0 - no restrictions',
-        link: 'https://creativecommons.org/public-domain/cc0'
+        label: 'CC0 - no restrictions'
       };
     case MetadataLicenseType.TBNL_C_D_NPL_Legal:
       return {
         helper:
           'You allow the collector to use the content for any purpose, except creating or sharing any derivative works, such as remixes.',
-        label: 'Commercial rights for the collector',
-        link: 'https://eips.ethereum.org/assets/eip-5218/ic3license/ic3license.pdf'
+        label: 'Commercial rights for the collector'
       };
     case MetadataLicenseType.TBNL_NC_D_NPL_Legal:
       return {
         helper:
           'You allow the collector to use the content for any personal, non-commercial purpose, except creating or sharing any derivative works, such as remixes.',
-        label: 'Personal rights for the collector',
-        link: 'https://eips.ethereum.org/assets/eip-5218/ic3license/ic3license.pdf'
+        label: 'Personal rights for the collector'
       };
     default:
       return null;
