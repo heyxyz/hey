590cdc28c1f1c8ed4cc83d3ff5c24683a6116a50
bigint <69431456+bigint@users.noreply.github.com>
2024-03-17 20:33:54 +0530

    fix: enabled flag for sentry client

--- apps/web/sentry.client.config.ts
+++ apps/web/sentry.client.config.ts
@@ -3,5 +3,6 @@ import * as Sentry from '@sentry/nextjs';
 Sentry.init({
   debug: false,
   dsn: 'https://e59a95ba0b70b237301fbc5fcb8e71db@o180224.ingest.us.sentry.io/4506721358512128',
+  enabled: process.env.NODE_ENV === 'production',
   tracesSampleRate: 1
 });
