442bf0a9a7b306cf75ba4bcb33f4fe5ea79b7c1e
bigint <69431456+bigint@users.noreply.github.com>
2024-03-17 11:42:29 +0530

    refactor: ARWEAVE_GATEWAY constant usage

--- packages/data/constants.ts
+++ packages/data/constants.ts
@@ -57,7 +57,7 @@ export const POLYGONSCAN_URL = IS_MAINNET
   ? 'https://polygonscan.com'
   : 'https://mumbai.polygonscan.com';
 export const IPFS_GATEWAY = 'https://gw.ipfs-lens.dev/ipfs';
-export const ARWEAVE_GATEWAY = 'https://gateway.irys.xyz/';
+export const ARWEAVE_GATEWAY = 'https://gateway.irys.xyz';
 export const EVER_API = 'https://endpoint.4everland.co';
 export const DEFAULT_OG = `${STATIC_IMAGES_URL}/og/cover.png`;
 export const PLACEHOLDER_IMAGE = `${STATIC_IMAGES_URL}/placeholder.webp`;
