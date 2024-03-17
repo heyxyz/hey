bccabf9a4614e6202b4bf7e10262b10890fa4a06
bigint <69431456+bigint@users.noreply.github.com>
2024-03-17 22:33:16 +0530

    chore: remove console

--- apps/web/src/hooks/usePushToImpressions.tsx
+++ apps/web/src/hooks/usePushToImpressions.tsx
@@ -3,7 +3,6 @@ import { useEffect } from 'react';
 
 const usePushToImpressions = (id: string): void => {
   useEffect(() => {
-    console.log('usePushToImpressions', id);
     pushToImpressions(id);
   }, [id]);
 };
