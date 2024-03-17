01d74f6aa5a3084e1398415debb0dcd26f5bea81
bigint <69431456+bigint@users.noreply.github.com>
2024-03-17 11:31:40 +0530

    feat: add cohort and added lens team to it

--- apps/api/src/db/seeds/seedFeatureFlags.ts
+++ apps/api/src/db/seeds/seedFeatureFlags.ts
@@ -1,3 +1,5 @@
+import { FeatureFlag } from '@hey/data/feature-flags';
+
 import {
   GARDENER_FEATURE_ID,
   GARDENER_MODE_FEATURE_ID,
@@ -11,21 +13,50 @@ import { prisma } from '../seed';
 const seedFeatureFlags = async (): Promise<number> => {
   const featureFlags = await prisma.feature.createMany({
     data: [
-      { id: GARDENER_FEATURE_ID, key: 'gardener', type: 'PERMISSION' },
-      { id: STAFF_MODE_FEATURE_ID, key: 'staff-mode', type: 'MODE' },
-      { id: GARDENER_MODE_FEATURE_ID, key: 'gardener-mode', type: 'MODE' },
-      { id: VERIFIED_FEATURE_ID, key: 'verified', type: 'PERMISSION' },
-      { id: STAFF_FEATURE_ID, key: 'staff', type: 'PERMISSION' },
-      { id: STAFF_PICK_FEATURE_ID, key: 'staff-pick', type: 'MODE' },
+      {
+        id: GARDENER_FEATURE_ID,
+        key: FeatureFlag.Gardener,
+        type: 'PERMISSION'
+      },
+      {
+        id: STAFF_MODE_FEATURE_ID,
+        key: FeatureFlag.StaffMode,
+        type: 'MODE'
+      },
+      {
+        id: GARDENER_MODE_FEATURE_ID,
+        key: FeatureFlag.GardenerMode,
+        type: 'MODE'
+      },
+      {
+        id: VERIFIED_FEATURE_ID,
+        key: FeatureFlag.Verified,
+        type: 'PERMISSION'
+      },
+      {
+        id: STAFF_FEATURE_ID,
+        key: FeatureFlag.Staff,
+        type: 'PERMISSION'
+      },
+      {
+        id: STAFF_PICK_FEATURE_ID,
+        key: FeatureFlag.StaffPick,
+        type: 'MODE'
+      },
       {
         id: 'd3f3e067-5624-4119-83d6-968b6d1621af',
-        key: 'flagged',
+        key: FeatureFlag.Flagged,
         type: 'PERMISSION'
       },
       {
         id: '8ed8b26a-279d-4111-9d39-a40164b273a0',
-        key: 'suspended',
+        key: FeatureFlag.Suspended,
         type: 'PERMISSION'
+      },
+      {
+        id: '8ed8b26a-279d-4111-9d39-a40164b273a0',
+        key: FeatureFlag.LensTeam,
+        type: 'COHORT'
       }
     ]
   });
