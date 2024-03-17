94bf5b19f0e98ea4332153679cd00d504dc80d4b
bigint <69431456+bigint@users.noreply.github.com>
2024-03-18 00:20:21 +0530

    chore: handle error on sentry

--- apps/web/sentry.client.config.ts
+++ apps/web/sentry.client.config.ts
@@ -4,5 +4,9 @@ Sentry.init({
   debug: false,
   dsn: 'https://e59a95ba0b70b237301fbc5fcb8e71db@o180224.ingest.us.sentry.io/4506721358512128',
   enabled: process.env.NODE_ENV === 'production',
+  ignoreErrors: [
+    'ResizeObserver loop completed with undelivered notifications.',
+    'ResizeObserver loop limit exceeded'
+  ],
   tracesSampleRate: 1
 });
