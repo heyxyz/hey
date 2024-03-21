d9cda7fceb0002ede327e286f1344c2311a534c3
bigint <69431456+bigint@users.noreply.github.com>
2024-03-21 19:36:44 +0530

    chore: update sitemap profiles

---
+++ apps/api/src/routes/sitemap/profileMaps.ts
@@ -0,0 +1,57 @@
+import type { Handler } from 'express';
+
+import logger from '@hey/lib/logger';
+import { XMLBuilder } from 'fast-xml-parser';
+import catchedError from 'src/lib/catchedError';
+import { SWR_CACHE_AGE_1_SEC_30_DAYS } from 'src/lib/constants';
+
+const profiles = [
+  '1jI5D--iDuPrWhfNdPm3AbXR_3eCSKtY60sYbkQrqEA4',
+  '1lPsDZM1BDnJgW7uklpmzS-OECZg8Xq7Ax-883zpRRtI',
+  '1MsVenKF0yaDbw0ppER1h1f_uhSvK4c7JiPeXKA2hoAY',
+  '1ORpHMMJN13oarscm0Pe8BlPynVWScO7n_x63TfPi7ZY',
+  '1Cl8kSYP4Tn1oI7aOANXn_vN9eSMhL8RWJHHyUWhN-q4'
+];
+
+export const config = {
+  api: { responseLimit: '8mb' }
+};
+
+interface Url {
+  loc: string;
+}
+
+const buildSitemapXml = (url: Url[]): string => {
+  const builder = new XMLBuilder({
+    format: true,
+    ignoreAttributes: false,
+    processEntities: true,
+    suppressEmptyNode: true
+  });
+
+  return builder.build({
+    sitemapindex: {
+      '@_xmlns': 'https://www.sitemaps.org/schemas/sitemap/0.9',
+      sitemap: url
+    }
+  });
+};
+
+export const get: Handler = (_, res) => {
+  try {
+    const entries: Url[] = profiles.map((id) => ({
+      loc: `https://hey.xyz/sitemaps/profiles?id=${id}`
+    }));
+
+    const xml = buildSitemapXml(entries);
+    logger.info('Sitemap fetched from Google Sheets');
+
+    return res
+      .status(200)
+      .setHeader('Content-Type', 'text/xml')
+      .setHeader('Cache-Control', SWR_CACHE_AGE_1_SEC_30_DAYS)
+      .send(xml);
+  } catch (error) {
+    return catchedError(res, error);
+  }
+};
