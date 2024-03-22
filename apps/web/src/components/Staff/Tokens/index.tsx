9fb3efa9c590574fcff0429cf40624ddb3599872
bigint <69431456+bigint@users.noreply.github.com>
2024-03-22 18:26:23 +0530

    feat: card header usage

--- apps/web/src/components/Staff/Tokens/index.tsx
+++ apps/web/src/components/Staff/Tokens/index.tsx
@@ -31,7 +31,7 @@ const Tokens: NextPage = () => {
       <GridItemFour>
         <StaffSidebar />
       </GridItemFour>
-      <GridItemEight className="space-y-5">
+      <GridItemEight>
         <List />
       </GridItemEight>
     </GridLayout>
