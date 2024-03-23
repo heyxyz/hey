864a8b741a39951762a5bd11e1221c98073dfa59
bigint <69431456+bigint@users.noreply.github.com>
2024-03-23 12:09:10 +0530

    chore: refactor follow unfollow button

--- apps/web/src/components/Shared/Profile/FollowUnfollowButton.tsx
+++ apps/web/src/components/Shared/Profile/FollowUnfollowButton.tsx
@@ -9,25 +9,31 @@ import SuperFollow from './SuperFollow';
 import Unfollow from './Unfollow';
 
 interface FollowUnfollowButtonProps {
+  hideFollowButton?: boolean;
+  hideUnfollowButton?: boolean;
   profile: Profile;
   small?: boolean;
 }
 
 const FollowUnfollowButton: FC<FollowUnfollowButtonProps> = ({
+  hideFollowButton = false,
+  hideUnfollowButton = false,
   profile,
   small = false
 }) => {
   return (
     <div onClick={stopEventPropagation}>
-      {profile.operations.isFollowedByMe.value ? null : profile?.followModule
-          ?.type === FollowModuleType.FeeFollowModule ? (
-        <SuperFollow profile={profile} small={small} />
-      ) : (
-        <Follow profile={profile} small={small} />
-      )}
-      {profile.operations.isFollowedByMe.value ? (
-        <Unfollow profile={profile} small={small} />
-      ) : null}
+      {!hideFollowButton &&
+        (profile.operations.isFollowedByMe.value ? null : profile?.followModule
+            ?.type === FollowModuleType.FeeFollowModule ? (
+          <SuperFollow profile={profile} small={small} />
+        ) : (
+          <Follow profile={profile} small={small} />
+        ))}
+      {!hideUnfollowButton &&
+        (profile.operations.isFollowedByMe.value ? (
+          <Unfollow profile={profile} small={small} />
+        ) : null)}
     </div>
   );
 };
