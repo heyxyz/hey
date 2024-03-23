3928a30453d6ccd5f257e9ebe4bd3ad7492ad582
bigint <69431456+bigint@users.noreply.github.com>
2024-03-23 13:53:40 +0530

    fix: follow / unfollow button

--- apps/web/src/components/Settings/Blocked/List.tsx
+++ apps/web/src/components/Settings/Blocked/List.tsx
@@ -64,7 +64,11 @@ const List: FC = () => {
         itemContent={(_, profile) => {
           return (
             <div className="flex items-center justify-between p-5">
-              <UserProfile profile={profile as Profile} />
+              <UserProfile
+                hideFollowButton
+                hideUnfollowButton
+                profile={profile as Profile}
+              />
               <Button
                 onClick={() =>
                   setShowBlockOrUnblockAlert(true, profile as Profile)
