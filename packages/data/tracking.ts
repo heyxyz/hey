75cb46a445576a737dbfcb557659cc4d13a58f37
bigint <69431456+bigint@users.noreply.github.com>
2024-03-23 14:56:52 +0530

    chore: remove portals

--- packages/data/tracking.ts
+++ packages/data/tracking.ts
@@ -42,7 +42,6 @@ export const PUBLICATION = {
   CLICK_HASHTAG: 'Click publication hashtag',
   CLICK_MENTION: 'Click publication mention',
   CLICK_OEMBED: 'Click publication oembed',
-  CLICK_PORTAL_BUTTON: 'Click portal button',
   COLLECT_MODULE: {
     COLLECT: 'Collect publication',
     OPEN_COLLECT: 'Open collect modal',
