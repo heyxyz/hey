6a86c073db71aa06282b679de9631f6f75281026
bigint <69431456+bigint@users.noreply.github.com>
2024-03-24 19:42:29 +0530

    feat: add timestamp

---
+++ packages/lib/datetime/getTimeFromNow.ts
@@ -0,0 +1,16 @@
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(relativeTime);
+
+/**
+ * Formats a date as a string representing the time from now.
+ *
+ * @param date The date to format.
+ * @returns A string representing the time from now.
+ */
+const getTimeFromNow = (date: Date) => {
+  return dayjs(date).fromNow();
+};
+
+export default getTimeFromNow;
