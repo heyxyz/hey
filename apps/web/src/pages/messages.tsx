952efe910cc266bb8af7f142df81d3725ff82b90
bigint <69431456+bigint@users.noreply.github.com>
2024-03-24 19:45:41 +0530

    feat: add dms button

--- apps/web/src/pages/messages.tsx
+++ apps/web/src/pages/messages.tsx
@@ -1,16 +1,9 @@
 import Messages from '@components/Messages';
-import isFeatureAvailable from '@lib/isFeatureAvailable';
 import { reactionContentTypeConfig, XMTPProvider } from '@xmtp/react-sdk';
-
-import Custom404 from './404';
 
 const contentTypeConfigs = [reactionContentTypeConfig];
 
 const XMTPMessages = () => {
-  if (!isFeatureAvailable('messages')) {
-    return <Custom404 />;
-  }
-
   return (
     <XMTPProvider contentTypeConfigs={contentTypeConfigs}>
       <Messages />
