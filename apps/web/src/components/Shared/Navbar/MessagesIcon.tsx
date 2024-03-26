19b832d7312cfee59d67c581f7788d05bf4f5dd2
bigint <69431456+bigint@users.noreply.github.com>
2024-03-26 23:55:50 +0530

    chore: enable DMs to public

--- apps/web/src/components/Shared/Navbar/MessagesIcon.tsx
+++ apps/web/src/components/Shared/Navbar/MessagesIcon.tsx
@@ -2,14 +2,9 @@ import type { FC } from 'react';
 
 import { EnvelopeIcon } from '@heroicons/react/24/outline';
 import { Tooltip } from '@hey/ui';
-import isFeatureAvailable from '@lib/isFeatureAvailable';
 import Link from 'next/link';
 
 const MessagesIcon: FC = () => {
-  if (!isFeatureAvailable('messages')) {
-    return null;
-  }
-
   return (
     <Tooltip content="Messages" placement="bottom">
       <Link
