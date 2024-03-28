39f02455b29a2c6cf37b870f479ee3383e36985d
bigint <69431456+bigint@users.noreply.github.com>
2024-03-28 14:53:18 +0530

    feat: add usePreventScrollOnNumberInput hook

---
+++ apps/web/src/hooks/usePreventScrollOnNumberInput.tsx
@@ -0,0 +1,23 @@
+import type { RefObject } from 'react';
+
+import { useEffect } from 'react';
+
+const usePreventScrollOnNumberInput = (ref: RefObject<HTMLInputElement>) => {
+  useEffect(() => {
+    const input = ref.current;
+    if (input) {
+      const preventScroll = (event: WheelEvent) => {
+        event.preventDefault();
+        event.stopPropagation();
+      };
+
+      input.addEventListener('wheel', preventScroll, { passive: false });
+
+      return () => {
+        input.removeEventListener('wheel', preventScroll);
+      };
+    }
+  }, [ref]);
+};
+
+export default usePreventScrollOnNumberInput;
