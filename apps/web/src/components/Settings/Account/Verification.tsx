be05e600678ecad3cc07e1bb3b9eadc1070602b3
bigint <69431456+bigint@users.noreply.github.com>
2024-04-02 20:09:39 +0530

    fix: add profile verification reqs

--- apps/web/src/components/Settings/Account/Verification.tsx
+++ apps/web/src/components/Settings/Account/Verification.tsx
@@ -1,7 +1,12 @@
 import type { FC } from 'react';
 
-import { CheckBadgeIcon } from '@heroicons/react/24/solid';
-import { Card } from '@hey/ui';
+import {
+  CheckBadgeIcon,
+  CheckCircleIcon,
+  XCircleIcon
+} from '@heroicons/react/24/solid';
+import getNumberOfDaysFromDate from '@hey/lib/datetime/getNumberOfDaysFromDate';
+import { Button, Card } from '@hey/ui';
 import { Crisp } from 'crisp-sdk-web';
 import { useProfileStore } from 'src/store/persisted/useProfileStore';
 import { hydrateVerifiedMembers } from 'src/store/persisted/useVerifiedMembersStore';
@@ -9,6 +14,22 @@ import { hydrateVerifiedMembers } from 'src/store/persisted/useVerifiedMembersSt
 const Verification: FC = () => {
   const { currentProfile } = useProfileStore();
   const { verifiedMembers } = hydrateVerifiedMembers();
+
+  if (!currentProfile) {
+    return null;
+  }
+
+  const hasMetFollowersRequirement =
+    (currentProfile.stats.followers || 0) >= 3000;
+  const hasMetPublicationsRequirement =
+    (currentProfile.stats.publications || 0) >= 50;
+  const hasMetTimeRequirement =
+    30 < -getNumberOfDaysFromDate(currentProfile?.createdAt);
+
+  const hasAllRequirements =
+    hasMetFollowersRequirement &&
+    hasMetPublicationsRequirement &&
+    hasMetTimeRequirement;
 
   return (
     <Card className="space-y-2 p-5">
@@ -19,15 +40,53 @@ const Verification: FC = () => {
           <CheckBadgeIcon className="text-brand-500 size-5" />
         </div>
       ) : (
-        <div className="linkify-button">
-          <span>No. </span>
-          <button
+        <div className="space-y-2">
+          <div className="flex items-center space-x-1.5">
+            {hasMetFollowersRequirement ? (
+              <CheckCircleIcon className="size-5 text-green-500" />
+            ) : (
+              <XCircleIcon className="size-5 text-red-500" />
+            )}
+            <div>
+              You have {hasMetFollowersRequirement ? 'met' : 'not met'} the 3000
+              followers requirement.
+            </div>
+          </div>
+          <div className="flex items-center space-x-1.5">
+            {hasMetPublicationsRequirement ? (
+              <CheckCircleIcon className="size-5 text-green-500" />
+            ) : (
+              <XCircleIcon className="size-5 text-red-500" />
+            )}
+            <div>
+              You have {hasMetPublicationsRequirement ? 'met' : 'not met'} the
+              50 publications requirement.
+            </div>
+          </div>
+          <div className="flex items-center space-x-1.5">
+            {hasMetTimeRequirement ? (
+              <CheckCircleIcon className="size-5 text-green-500" />
+            ) : (
+              <XCircleIcon className="size-5 text-red-500" />
+            )}
+            <div>
+              You have {hasMetTimeRequirement ? 'met' : 'not met'} the 30 days
+              requirement.
+            </div>
+          </div>
+          <Button
+            className="!mt-4"
+            disabled={!hasAllRequirements}
             onClick={() => {
               Crisp.chat.show();
               Crisp.message.show('picker', {
                 choices: [
                   { label: 'Creator', selected: false, value: 'creator' },
-                  { label: 'Journalist', selected: false, value: 'journalist' },
+                  {
+                    label: 'Journalist',
+                    selected: false,
+                    value: 'journalist'
+                  },
                   {
                     label: 'Public Figure',
                     selected: false,
@@ -41,7 +100,7 @@ const Verification: FC = () => {
             }}
           >
             Request for profile verification
-          </button>
+          </Button>
         </div>
       )}
     </Card>
