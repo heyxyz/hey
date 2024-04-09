bb7b29fa2726bf15c142be14d0db8a0b0ae49825
bigint <69431456+bigint@users.noreply.github.com>
2024-04-09 23:21:49 +0530

    feat: add update email

---
+++ apps/api/src/routes/email/update.ts
@@ -0,0 +1,67 @@
+import type { Handler } from 'express';
+
+import logger from '@hey/lib/logger';
+import parseJwt from '@hey/lib/parseJwt';
+import catchedError from 'src/lib/catchedError';
+import validateLensAccount from 'src/lib/middlewares/validateLensAccount';
+import prisma from 'src/lib/prisma';
+import { invalidBody, noBody, notAllowed } from 'src/lib/responses';
+import { v4 as uuid } from 'uuid';
+import { object, string } from 'zod';
+
+type ExtensionRequest = {
+  email: string;
+};
+
+const validationSchema = object({
+  email: string().email()
+});
+
+export const post: Handler = async (req, res) => {
+  const { body } = req;
+
+  if (!body) {
+    return noBody(res);
+  }
+
+  const accessToken = req.headers['x-access-token'] as string;
+  const validation = validationSchema.safeParse(body);
+
+  if (!validation.success) {
+    return invalidBody(res);
+  }
+
+  if (!(await validateLensAccount(req))) {
+    return notAllowed(res);
+  }
+
+  const { email } = body as ExtensionRequest;
+
+  try {
+    const payload = parseJwt(accessToken);
+
+    const data = await prisma.email.findUnique({
+      where: { id: payload.id }
+    });
+
+    if (data?.email === email) {
+      return res.status(200).json({ success: false });
+    }
+
+    const result = await prisma.email.update({
+      data: {
+        email,
+        tokenExpiresAt: new Date(Date.now() + 24 * 60 * 60 * 1000), // 24 hours
+        verificationToken: uuid()
+      },
+      select: { email: true, id: true, verified: true },
+      where: { id: payload.id }
+    });
+
+    logger.info(`Email updated to ${email} for ${payload.id}`);
+
+    return res.status(200).json({ result, success: true });
+  } catch (error) {
+    return catchedError(res, error);
+  }
+};
