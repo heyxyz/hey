bef8d0f3981b018f397d1e10ff31ab342405542b
bigint <69431456+bigint@users.noreply.github.com>
2024-04-10 00:21:36 +0530

    fix: update to us-west-2

--- apps/api/src/lib/sendEmail.ts
+++ apps/api/src/lib/sendEmail.ts
@@ -6,7 +6,7 @@ const sesClient = new SESClient({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
   },
-  region: 'eu-west-2'
+  region: 'us-west-2'
 });
 
 const sendEmail = async ({
