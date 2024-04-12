ce3e27c11a69d0b3498a6128db13d332b2f2b7c7
bigint <69431456+bigint@users.noreply.github.com>
2024-04-13 00:18:35 +0530

    chore: remove tip open action

--- apps/web/src/components/Composer/Actions/OpenActionSettings/OpenActionsConfig.tsx
+++ apps/web/src/components/Composer/Actions/OpenActionSettings/OpenActionsConfig.tsx
@@ -4,17 +4,11 @@ import { OpenAction } from '@hey/data/enums';
 import { useOpenActionStore } from 'src/store/non-persisted/publication/useOpenActionStore';
 
 import SwapConfig from './Config/Swap';
-import TipConfig from './Config/Tip';
 
 const OpenActionsConfig: FC = () => {
   const { selectedOpenAction } = useOpenActionStore();
 
-  return (
-    <div>
-      {selectedOpenAction === OpenAction.Tip && <TipConfig />}
-      {selectedOpenAction === OpenAction.Swap && <SwapConfig />}
-    </div>
-  );
+  return <div>{selectedOpenAction === OpenAction.Swap && <SwapConfig />}</div>;
 };
 
 export default OpenActionsConfig;
