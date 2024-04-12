95924cd5d9e29de47b9a1e6883b2c18b44fc0da3
bigint <69431456+bigint@users.noreply.github.com>
2024-04-13 00:14:39 +0530

    feat: add confetti

--- apps/web/src/components/Publication/Actions/Tip/index.tsx
+++ apps/web/src/components/Publication/Actions/Tip/index.tsx
@@ -1,5 +1,4 @@
 import type { AnyPublication } from '@hey/lens';
-import type { FC } from 'react';
 
 import MenuTransition from '@components/Shared/MenuTransition';
 import { Popover } from '@headlessui/react';
@@ -12,6 +11,8 @@ import { Tooltip } from '@hey/ui';
 import cn from '@hey/ui/cn';
 import hasOptimisticallyMirrored from '@lib/optimistic/hasOptimisticallyMirrored';
 import { motion } from 'framer-motion';
+import party from 'party-js';
+import { type FC, useRef } from 'react';
 
 import Action from './Action';
 
@@ -30,8 +31,17 @@ const Tip: FC<TipProps> = ({ publication, showCount }) => {
     hasOptimisticallyMirrored(targetPublication.id);
   const shares =
     targetPublication.stats.mirrors + targetPublication.stats.quotes;
+  const confettiDom = useRef<HTMLDivElement>(null);
 
   const iconClassName = 'w-[15px] sm:w-[18px]';
+
+  const triggerConfetti = () => {
+    party.resolvableShapes['moneybag'] = `<span>💰</span>`;
+    party.sparkles(confettiDom.current as any, {
+      lifetime: 2,
+      shapes: ['moneybag']
+    });
+  };
 
   return (
     <div className="flex items-center space-x-1">
@@ -48,6 +58,7 @@ const Tip: FC<TipProps> = ({ publication, showCount }) => {
           onClick={stopEventPropagation}
           whileTap={{ scale: 0.9 }}
         >
+          <div ref={confettiDom} />
           <Tooltip
             content={shares > 0 ? `${humanize(shares)} Tips` : 'Tip'}
             placement="top"
@@ -61,7 +72,15 @@ const Tip: FC<TipProps> = ({ publication, showCount }) => {
             className="absolute z-[5] mt-1 w-max rounded-xl border bg-white p-5 shadow-sm focus:outline-none dark:border-gray-700 dark:bg-gray-900"
             static
           >
-            <Action publication={targetPublication} />
+            {({ close }) => (
+              <Action
+                publication={targetPublication}
+                triggerConfetti={() => {
+                  triggerConfetti();
+                  close();
+                }}
+              />
+            )}
           </Popover.Panel>
         </MenuTransition>
       </Popover>
