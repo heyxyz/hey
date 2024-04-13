c3353461483a18e169cea3d1073a79870b9d3c85
bigint <69431456+bigint@users.noreply.github.com>
2024-04-13 16:53:15 +0530

    feat: add more fallback rpc

--- apps/web/src/components/Common/Providers/Web3Provider.tsx
+++ apps/web/src/components/Common/Providers/Web3Provider.tsx
@@ -1,7 +1,7 @@
 import type { FC, ReactNode } from 'react';
 
 import { APP_NAME, WALLETCONNECT_PROJECT_ID } from '@hey/data/constants';
-import { createConfig, http, WagmiProvider } from 'wagmi';
+import { createConfig, fallback, http, WagmiProvider } from 'wagmi';
 import { polygon, polygonAmoy } from 'wagmi/chains';
 import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors';
 
@@ -15,8 +15,17 @@ const wagmiConfig = createConfig({
   chains: [polygon, polygonAmoy],
   connectors,
   transports: {
-    [polygon.id]: http(),
-    [polygonAmoy.id]: http()
+    [polygon.id]: fallback([
+      http('https://1rpc.io/matic'),
+      http('https://polygon-bor-rpc.publicnode.com'),
+      http('https://rpc.ankr.com/polygon'),
+      http('https://lb.nodies.app/v1/975f16c52f5f4732b20b6692137eec17')
+    ]),
+    [polygonAmoy.id]: fallback([
+      http('https://rpc-amoy.polygon.technology'),
+      http('https://polygon-amoy-bor-rpc.publicnode.com'),
+      http('https://lb.nodies.app/v1/6a17e9caa9f1424c87be69746244a8c8')
+    ])
   }
 });
 
