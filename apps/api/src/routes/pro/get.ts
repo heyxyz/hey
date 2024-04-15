3ae301a0430210b579544c9c8bae5d2b720e07eb
bigint <69431456+bigint@users.noreply.github.com>
2024-04-15 22:49:52 +0530

    feat: add HEY_PRO contract

--- apps/api/src/routes/pro/get.ts
+++ apps/api/src/routes/pro/get.ts
@@ -1,50 +1,37 @@
 import type { Handler } from 'express';
 
-import logger from '@hey/lib/logger';
-import axios from 'axios';
+import { HeyPro } from '@hey/abis';
+import { HEY_PRO, IS_MAINNET } from '@hey/data/constants';
 import catchedError from 'src/lib/catchedError';
-import { SWR_CACHE_AGE_10_MINS_30_DAYS } from 'src/lib/constants';
+import getRpc from 'src/lib/getRpc';
 import { noBody } from 'src/lib/responses';
+import { createPublicClient } from 'viem';
+import { polygon, polygonAmoy } from 'viem/chains';
 
 // TODO: add tests
 export const get: Handler = async (req, res) => {
-  const { address } = req.query;
+  const { id } = req.query;
 
-  if (!address) {
+  if (!id) {
     return noBody(res);
   }
 
   try {
-    const id = `${address.toString().toLowerCase()}-0xf618330f51fa54ce5951d627ee150c0fdadeba43-0x3ad736904e9e65189c3000c7dd2c8ac8bb7cd4e3-0.0`;
+    const client = createPublicClient({
+      chain: IS_MAINNET ? polygon : polygonAmoy,
+      transport: getRpc({ mainnet: IS_MAINNET })
+    });
 
-    const response = await axios.post(
-      `https://polygon-mainnet.subgraph.x.superfluid.dev`,
-      {
-        operationName: 'streams',
-        query: `{
-            streams(where: { id: "${id}" },
-            skip: 0,
-            take: 1
-          ) {
-            createdAtBlockNumber
-            createdAtTimestamp
-            currentFlowRate
-            deposit
-          }
-        }
-        `,
-        variables: { id }
-      }
-    );
+    const data: any = await client.readContract({
+      abi: HeyPro,
+      address: HEY_PRO,
+      args: [id],
+      functionName: 'proExpiresAt'
+    });
 
-    logger.info(`Fetched pro status from Superfluid for ${address}`);
+    const jsonData = JSON.parse(data);
 
-    const result = response.data?.data?.streams[0] || null;
-
-    return res
-      .status(200)
-      .setHeader('Cache-Control', SWR_CACHE_AGE_10_MINS_30_DAYS)
-      .json({ result, success: true });
+    return res.status(200).json({ result: jsonData, success: true });
   } catch (error) {
     return catchedError(res, error);
   }
