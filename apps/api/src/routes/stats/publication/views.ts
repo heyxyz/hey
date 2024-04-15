ca79f3d70e867d5d64a1a176329583a5a1532bc5
bigint <69431456+bigint@users.noreply.github.com>
2024-04-15 22:14:43 +0530

    fix: add ff

--- apps/api/src/routes/stats/publication/views.ts
+++ apps/api/src/routes/stats/publication/views.ts
@@ -2,7 +2,7 @@ import type { Handler } from 'express';
 
 import logger from '@hey/lib/logger';
 import catchedError from 'src/lib/catchedError';
-import { SWR_CACHE_AGE_1_SEC_30_DAYS } from 'src/lib/constants';
+import { SWR_CACHE_AGE_10_MINS_30_DAYS } from 'src/lib/constants';
 import createClickhouseClient from 'src/lib/createClickhouseClient';
 import { invalidBody, noBody } from 'src/lib/responses';
 import { array, object, string } from 'zod';
@@ -52,7 +52,7 @@ export const post: Handler = async (req, res) => {
 
     return res
       .status(200)
-      .setHeader('Cache-Control', SWR_CACHE_AGE_1_SEC_30_DAYS)
+      .setHeader('Cache-Control', SWR_CACHE_AGE_10_MINS_30_DAYS)
       .json({ success: true, views: viewCounts });
   } catch (error) {
     return catchedError(res, error);
