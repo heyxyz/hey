216141ac81be6d5e53c22325412a54aa4effc59f
bigint <69431456+bigint@users.noreply.github.com>
2024-04-15 12:45:01 +0530

    feat: add more test cases

--- apps/api/tests/preferences/update.spec.ts
+++ apps/api/tests/preferences/update.spec.ts
@@ -4,6 +4,8 @@ import { TEST_URL } from 'src/lib/constants';
 import { describe, expect, test } from 'vitest';
 
 describe('preferences/update', () => {
+  const payload = { highSignalNotificationFilter: true, isPride: true };
+
   test('should update profile preferences', async () => {
     const response = await axios.post(
       `${TEST_URL}/preferences/update`,
@@ -13,5 +15,15 @@ describe('preferences/update', () => {
 
     expect(response.data.result.highSignalNotificationFilter).toBeTruthy();
     expect(response.data.result.isPride).toBeTruthy();
+  });
+
+  test('should fail if not authenticated', async () => {
+    try {
+      const response = await axios.post(
+        `${TEST_URL}/preferences/update`,
+        payload
+      );
+      expect(response.status).toEqual(401);
+    } catch {}
   });
 });
