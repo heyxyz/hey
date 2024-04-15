f112e125719a149f71af8e698160aefeb2bf5d85
bigint <69431456+bigint@users.noreply.github.com>
2024-04-15 12:35:03 +0530

    feat: add more test cases

--- apps/api/tests/internal/features/delete.spec.ts
+++ apps/api/tests/internal/features/delete.spec.ts
@@ -4,19 +4,50 @@ import { TEST_URL } from 'src/lib/constants';
 import { describe, expect, test } from 'vitest';
 
 describe('internal/features/delete', () => {
-  test('should delete a feature', async () => {
-    const newFeatureResponse = await axios.post(
+  const createNewFeature = async () => {
+    const response = await axios.post(
       `${TEST_URL}/internal/features/create`,
       { key: Math.random().toString() },
       { headers: await getAuthApiHeadersForTest() }
     );
 
+    return response.data.feature.id;
+  };
+
+  test('should delete a feature', async () => {
+    const newFeature = await createNewFeature();
+
     const response = await axios.post(
       `${TEST_URL}/internal/features/delete`,
-      { id: newFeatureResponse.data.feature.id },
+      { id: newFeature },
       { headers: await getAuthApiHeadersForTest() }
     );
 
     expect(response.data.success).toBeTruthy();
+  });
+
+  test('should fail if not staff', async () => {
+    try {
+      const newFeature = await createNewFeature();
+
+      const response = await axios.post(
+        `${TEST_URL}/internal/features/delete`,
+        { id: newFeature },
+        { headers: await getAuthApiHeadersForTest({ staff: false }) }
+      );
+      expect(response.status).toEqual(401);
+    } catch {}
+  });
+
+  test('should fail if not authenticated', async () => {
+    try {
+      const newFeature = await createNewFeature();
+
+      const response = await axios.post(
+        `${TEST_URL}/internal/features/delete`,
+        { id: newFeature }
+      );
+      expect(response.status).toEqual(401);
+    } catch {}
   });
 });
