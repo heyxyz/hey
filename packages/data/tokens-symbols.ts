64c775105e52249448117009f0841feffa19ad89
bigint <69431456+bigint@users.noreply.github.com>
2024-04-16 21:29:24 +0530

    feat: add profile revenue

--- packages/data/tokens-symbols.ts
+++
@@ -1,9 +0,0 @@
-export const TOKEN_SYMBOLS: Record<string, string> = {
-  DAI: 'DAI',
-  USDC: 'USDC',
-  USDT: 'USDT',
-  WETH: 'ETH',
-  WMATIC: 'MATIC'
-};
-
-export const USD_ENABLED_TOKEN_SYMBOLS = Object.keys(TOKEN_SYMBOLS);
