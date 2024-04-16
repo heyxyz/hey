eb19f598b1ed0f041d0c31a84b04a63cdeef284b
bigint <69431456+bigint@users.noreply.github.com>
2024-04-16 09:01:09 +0530

    chore: update addresses

--- packages/contracts/script/HeyLensSignup/deploy.ts
+++ packages/contracts/script/HeyLensSignup/deploy.ts
@@ -1,7 +1,7 @@
 const hre = require('hardhat');
 
 async function deployProxy() {
-  const owner = '0x03Ba34f6Ea1496fa316873CF8350A3f7eaD317EF';
+  const owner = '0x47F7E79785812482715c96eF9337A945Af9Af470';
   // Amoy: 0x36440da1D98FF46637f0b98AAA082bc77977B49B
   // Mainnet: 0x0b5e6100243f793e480DE6088dE6bA70aA9f3872
   const lensPermissionlessCreator =
