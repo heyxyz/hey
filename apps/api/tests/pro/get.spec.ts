e666ce8bebddd8d2f876c7b3e1c33e4b9bf7a9b2
bigint <69431456+bigint@users.noreply.github.com>
2024-04-17 17:32:59 +0530

    feat: add pro test case

--- apps/api/tests/pro/get.spec.ts
+++ apps/api/tests/pro/get.spec.ts
@@ -3,13 +3,23 @@ import { TEST_URL } from 'src/lib/constants';
 import { describe, expect, test } from 'vitest';
 
 describe('pro/get', () => {
-  test('should return pro details', async () => {
+  test('should return pro details if subscribed', async () => {
     const response = await axios.get(`${TEST_URL}/pro/get`, {
       params: { id: '0x05' }
     });
 
     expect(response.data.result.isPro).toBeTruthy();
     expect(response.data.result.expiresAt).toEqual('2027-05-16T11:56:00.000Z');
+    expect(response.data.success).toBeTruthy();
+  });
+
+  test('should not return pro details if not subscribed', async () => {
+    const response = await axios.get(`${TEST_URL}/pro/get`, {
+      params: { id: '0x00' }
+    });
+
+    expect(response.data.result.isPro).toBeFalsy();
+    expect(response.data.result.expiresAt).toBeNull();
     expect(response.data.success).toBeTruthy();
   });
 });
