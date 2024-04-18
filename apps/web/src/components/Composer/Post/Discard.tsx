656affb0da525cc1efbcf8e4e04ec900ca922304
bigint <69431456+bigint@users.noreply.github.com>
2024-04-18 22:11:35 +0530

    feat: add create draft endpoint

--- apps/web/src/components/Composer/Post/Discard.tsx
+++ apps/web/src/components/Composer/Post/Discard.tsx
@@ -1,6 +1,14 @@
 import type { FC } from 'react';
 
+import { HEY_API_URL } from '@hey/data/constants';
 import { Alert } from '@hey/ui';
+import getAuthApiHeaders from '@lib/getAuthApiHeaders';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import { useCollectModuleStore } from 'src/store/non-persisted/publication/useCollectModuleStore';
+import { useOpenActionStore } from 'src/store/non-persisted/publication/useOpenActionStore';
+import { usePublicationAttachmentStore } from 'src/store/non-persisted/publication/usePublicationAttachmentStore';
+import { usePublicationStore } from 'src/store/non-persisted/publication/usePublicationStore';
 import { useGlobalModalStateStore } from 'src/store/non-persisted/useGlobalModalStateStore';
 
 interface DiscardProps {
@@ -9,14 +17,43 @@ interface DiscardProps {
 
 const Discard: FC<DiscardProps> = ({ onDiscard }) => {
   const { setShowDiscardModal, showDiscardModal } = useGlobalModalStateStore();
+  const { publicationContent } = usePublicationStore();
+  const { attachments, isUploading } = usePublicationAttachmentStore(
+    (state) => state
+  );
+  const { collectModule } = useCollectModuleStore((state) => state);
+  const { openAction } = useOpenActionStore();
+
+  const updateDraft = async () => {
+    try {
+      const draft = {
+        attachments: attachments.length ? JSON.stringify(attachments) : null,
+        collectModule: collectModule.type
+          ? JSON.stringify(collectModule)
+          : null,
+        content: publicationContent,
+        openActionModules: openAction ? JSON.stringify(openAction) : null
+      };
+
+      await axios.post(`${HEY_API_URL}/drafts/update`, draft, {
+        headers: getAuthApiHeaders()
+      });
+      onDiscard();
+      setShowDiscardModal(false);
+    } catch {
+      return toast.error('Error saving draft');
+    }
+  };
 
   return (
     <Alert
+      cancelText="Save as draft"
       confirmText="Discard"
-      description="This can’t be undone and you’ll lose your draft."
+      description="You can save this to send later from your drafts."
       isDestructive
-      onClose={() => setShowDiscardModal(false)}
-      onConfirm={onDiscard}
+      isPerformingAction={isUploading}
+      onClose={updateDraft}
+      onConfirm={() => setShowDiscardModal(false)}
       show={showDiscardModal}
       title="Discard Post"
     />
