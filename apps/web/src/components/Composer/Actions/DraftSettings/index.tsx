f9fa4685b97c571df7046644484c8c0cdebbcb55
bigint <69431456+bigint@users.noreply.github.com>
2024-04-18 22:42:16 +0530

    feat: refactor draft

---
+++ apps/web/src/components/Composer/Actions/DraftSettings/index.tsx
@@ -0,0 +1,37 @@
+import { ArchiveBoxArrowDownIcon } from '@heroicons/react/24/outline';
+import { Modal, Tooltip } from '@hey/ui';
+import { motion } from 'framer-motion';
+import { type FC, useState } from 'react';
+
+import List from './List';
+
+const DraftSettings: FC = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      <Tooltip content="Drafts" placement="top">
+        <motion.button
+          aria-label="Drafts"
+          className="rounded-full outline-offset-8"
+          onClick={() => setShowModal(true)}
+          type="button"
+          whileTap={{ scale: 0.9 }}
+        >
+          <ArchiveBoxArrowDownIcon className="size-5" />
+        </motion.button>
+      </Tooltip>
+      <Modal
+        icon={<ArchiveBoxArrowDownIcon className="size-5" />}
+        onClose={() => setShowModal(false)}
+        show={showModal}
+        size="md"
+        title="Drafts"
+      >
+        <List setShowModal={setShowModal} />
+      </Modal>
+    </>
+  );
+};
+
+export default DraftSettings;
