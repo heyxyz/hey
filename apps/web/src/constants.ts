a8845b437375788d282be1a308368f4ceed0b762
aschmidt20 <andrew@aave.com>
2024-04-18 16:21:21 -0500

    feat: referral profileid constant

--- apps/web/src/constants.ts
+++ apps/web/src/constants.ts
@@ -4,3 +4,5 @@ import { polygon, polygonMumbai } from 'wagmi/chains';
 export const CHAIN = IS_MAINNET ? polygon : polygonMumbai;
 
 export const PERMIT_2_ADDRESS = '0x000000000022D473030F116dDEE9F6B43aC78BA3'; // same on Polygon and Mumbai
+
+export const HEY_REFERRAL_PROFILE_ID = '12';
