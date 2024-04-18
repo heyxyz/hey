fd3595bf0684f910874bf82a9a1d29d30c5f1126
bigint <69431456+bigint@users.noreply.github.com>
2024-04-18 22:11:35 +0530

    feat: add create draft endpoint

---
+++ apps/api/src/routes/drafts/update.ts
@@ -0,0 +1,78 @@
+import type { Handler } from 'express';
+
+import logger from '@hey/lib/logger';
+import parseJwt from '@hey/lib/parseJwt';
+import catchedError from 'src/lib/catchedError';
+import validateLensAccount from 'src/lib/middlewares/validateLensAccount';
+import prisma from 'src/lib/prisma';
+import { invalidBody, noBody, notAllowed } from 'src/lib/responses';
+import { object, string } from 'zod';
+
+type ExtensionRequest = {
+  attachments: null | string;
+  collectModule: null | string;
+  content: string;
+  id: null | string;
+  openActionModules: null | string;
+};
+
+const validationSchema = object({
+  attachments: string().nullable(),
+  collectModule: string().nullable(),
+  content: string().min(1).max(100000),
+  openActionModules: string().nullable()
+});
+
+export const post: Handler = async (req, res) => {
+  const { body } = req;
+
+  if (!body) {
+    return noBody(res);
+  }
+
+  const accessToken = req.headers['x-access-token'] as string;
+  const validation = validationSchema.safeParse(body);
+
+  if (!validation.success) {
+    return invalidBody(res);
+  }
+
+  if (!(await validateLensAccount(req))) {
+    return notAllowed(res);
+  }
+
+  const { attachments, collectModule, content, id, openActionModules } =
+    body as ExtensionRequest;
+
+  try {
+    const payload = parseJwt(accessToken);
+
+    const baseData = {
+      ...(attachments && { attachments }),
+      ...(collectModule && { collectModule }),
+      content,
+      ...(openActionModules && { openActionModules })
+    };
+
+    if (id) {
+      const result = await prisma.draftPublication.update({
+        data: baseData,
+        where: { id: id as string }
+      });
+
+      logger.info(`Draft updated for ${payload.id} - ${result.id}`);
+
+      return res.status(200).json({ result, success: true });
+    } else {
+      const result = await prisma.draftPublication.create({
+        data: { profileId: payload.id, ...baseData }
+      });
+
+      logger.info(`Draft created for ${payload.id} - ${result.id}`);
+
+      return res.status(200).json({ result, success: true });
+    }
+  } catch (error) {
+    return catchedError(res, error);
+  }
+};
