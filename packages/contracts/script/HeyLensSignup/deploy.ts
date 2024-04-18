129822d3989b512de607a415b55a6f418b449d52
bigint <69431456+bigint@users.noreply.github.com>
2024-04-18 09:28:56 +0530

    chore: update safe address

--- packages/contracts/script/HeyLensSignup/deploy.ts
+++ packages/contracts/script/HeyLensSignup/deploy.ts
@@ -1,7 +1,7 @@
 const hre = require('hardhat');
 
 async function deployProxy() {
-  const owner = '0x47F7E79785812482715c96eF9337A945Af9Af470';
+  const owner = '0x698386C93513d6D0C58f296633A7A3e529bd4026';
   // Amoy: 0x36440da1D98FF46637f0b98AAA082bc77977B49B
   // Mainnet: 0x0b5e6100243f793e480DE6088dE6bA70aA9f3872
   const lensPermissionlessCreator =
