72971d7cb56ec73c992ba935aa2fec4bc44217df
bigint <69431456+bigint@users.noreply.github.com>
2024-04-21 01:12:39 +0530

    feat: new sitemaps

--- apps/api/src/routes/sitemap/profiles/index.ts
+++ apps/api/src/routes/sitemap/profiles/index.ts
@@ -18,7 +18,7 @@ export const get: Handler = async (req, res) => {
     const totalBatches = Math.ceil(totalHandles / SITEMAP_BATCH_SIZE);
 
     const entries = Array.from({ length: totalBatches }, (_, index) => ({
-      loc: `https://hey.xyz/sitemap/profiles/${index + 1}.xml`
+      loc: `https://api.hey.xyz/sitemap/profiles/${index + 1}`
     }));
     const xml = buildSitemapXml(entries);
 
