31a848131ece53a6e8d7e96386dacc07aba7ef36
bigint <69431456+bigint@users.noreply.github.com>
2024-04-20 08:55:13 +0530

    feat: remove maxTipAmount

---
+++ apps/api/src/db/migrations/20240420032344_remove_max_tip_amount/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `maxTipAmount` on the `AllowedToken` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "AllowedToken" DROP COLUMN "maxTipAmount";
