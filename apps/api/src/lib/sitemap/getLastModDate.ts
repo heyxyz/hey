6920165b794a3a9c78f4a72635d0fa7f8ac059c8
bigint <69431456+bigint@users.noreply.github.com>
2024-04-21 21:53:56 +0530

    chore: update deps

--- apps/api/src/lib/sitemap/getLastModDate.ts
+++
@@ -1,8 +0,0 @@
-const getLastModDate = (date: Date): string =>
-  date.toLocaleDateString('en-CA', {
-    day: '2-digit',
-    month: '2-digit',
-    year: 'numeric'
-  });
-
-export default getLastModDate;
