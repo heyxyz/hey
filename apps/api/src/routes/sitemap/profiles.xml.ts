2a2100e44d90a373ae4a88930f1cb43610a3ea77
bigint <69431456+bigint@users.noreply.github.com>
2024-04-21 07:44:42 +0530

    feat: new sitemaps

---
+++ apps/api/src/routes/sitemap/profiles.xml.ts
@@ -0,0 +1,33 @@
+import type { Handler } from 'express';
+
+import logger from '@hey/lib/logger';
+import catchedError from 'src/lib/catchedError';
+import { SITEMAP_BATCH_SIZE } from 'src/lib/constants';
+import lensPrisma from 'src/lib/lensPrisma';
+import { buildSitemapXml } from 'src/lib/sitemap/buildSitemap';
+
+export const get: Handler = async (req, res) => {
+  const user_agent = req.headers['user-agent'];
+
+  try {
+    const response = await lensPrisma.$queryRaw<{ count: number }[]>`
+      SELECT COUNT(*) as count FROM namespace.handle;
+    `;
+
+    const totalHandles = Number(response[0]?.count) || 0;
+    const totalBatches = Math.ceil(totalHandles / SITEMAP_BATCH_SIZE);
+
+    const entries = Array.from({ length: totalBatches }, (_, index) => ({
+      loc: `https://api.hey.xyz/sitemap/profiles/${index + 1}.xml`
+    }));
+    const xml = buildSitemapXml(entries);
+
+    logger.info(
+      `Lens: Fetched all profiles sitemap having ${totalBatches} batches from user-agent: ${user_agent}`
+    );
+
+    return res.status(200).setHeader('Content-Type', 'text/xml').send(xml);
+  } catch (error) {
+    return catchedError(res, error);
+  }
+};
---
+++ apps/api/src/routes/sitemap/sitemap.xml.ts
@@ -0,0 +1,27 @@
+import type { Handler } from 'express';
+
+import logger from '@hey/lib/logger';
+import catchedError from 'src/lib/catchedError';
+import { buildSitemapXml } from 'src/lib/sitemap/buildSitemap';
+
+export const get: Handler = (req, res) => {
+  const user_agent = req.headers['user-agent'];
+
+  try {
+    const sitemaps = [
+      'https://api.hey.xyz/sitemap/profiles.xml',
+      'https://api.hey.xyz/sitemap/publications.xml'
+    ];
+
+    const entries = sitemaps.map((sitemap) => ({
+      loc: sitemap
+    }));
+    const xml = buildSitemapXml(entries);
+
+    logger.info(`Lens: Fetched all sitemaps from user-agent: ${user_agent}`);
+
+    return res.status(200).setHeader('Content-Type', 'text/xml').send(xml);
+  } catch (error) {
+    return catchedError(res, error);
+  }
+};
