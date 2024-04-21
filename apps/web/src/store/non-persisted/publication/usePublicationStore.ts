a38417a1b2a9df51ebc2bd01f44262545480ab2e
bigint <69431456+bigint@users.noreply.github.com>
2024-04-21 11:24:03 +0530

    feat: add able to update drafts

--- apps/web/src/store/non-persisted/publication/usePublicationStore.ts
+++ apps/web/src/store/non-persisted/publication/usePublicationStore.ts
@@ -4,15 +4,19 @@ import { createTrackedSelector } from 'react-tracked';
 import { create } from 'zustand';
 
 interface State {
+  draftId: null | string;
   publicationContent: string;
   quotedPublication: AnyPublication | null;
+  setDraftId: (draftId: null | string) => void;
   setPublicationContent: (publicationContent: string) => void;
   setQuotedPublication: (quotedPublication: AnyPublication | null) => void;
 }
 
 const store = create<State>((set) => ({
+  draftId: null,
   publicationContent: '',
   quotedPublication: null,
+  setDraftId: (draftId) => set(() => ({ draftId })),
   setPublicationContent: (publicationContent) =>
     set(() => ({ publicationContent })),
   setQuotedPublication: (quotedPublication) =>
