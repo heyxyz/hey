5aed9ea4b50fa08b37bd9d31047cb43209e0e634
bigint <69431456+bigint@users.noreply.github.com>
2024-04-22 23:09:48 +0530

    feat: add ui with new contract

--- packages/contracts/contracts/HeyTipping/HeyTipping.sol
+++ packages/contracts/contracts/HeyTipping/HeyTipping.sol
@@ -14,6 +14,7 @@ contract HeyTipping is
   ReentrancyGuardUpgradeable,
   PausableUpgradeable
 {
+  uint256 public feesBps;
   mapping(bytes32 => mapping(bytes32 => bool)) public hasTipped;
 
   event TipSent(
@@ -29,10 +30,11 @@ contract HeyTipping is
   error InsufficientAllowance(string message);
 
   // Initializer instead of constructor for upgradeable contracts
-  function initialize(address owner) public initializer {
+  function initialize(address owner, uint256 _feesBps) public initializer {
     __Ownable_init(owner);
     __ReentrancyGuard_init();
     __Pausable_init();
+    feesBps = _feesBps;
   }
 
   function pause() public onlyOwner {
@@ -41,6 +43,10 @@ contract HeyTipping is
 
   function unpause() public onlyOwner {
     _unpause();
+  }
+
+  function setFees(uint256 _feesBps) external onlyOwner {
+    feesBps = _feesBps;
   }
 
   function checkIfTipped(
@@ -67,19 +73,25 @@ contract HeyTipping is
     uint256 amount,
     string calldata profileId,
     string calldata publicationId
-  ) public whenNotPaused onlyOwner {
+  ) public whenNotPaused nonReentrant {
     bytes32 profileHash = keccak256(abi.encodePacked(profileId));
     bytes32 publicationHash = keccak256(abi.encodePacked(publicationId));
     IERC20 token = IERC20(tokenAddress);
 
-    // Check if the contract has enough allowance
     uint256 allowed = token.allowance(msg.sender, address(this));
     if (allowed < amount) {
       revert InsufficientAllowance('Not enough allowance');
     }
 
-    bool success = token.transferFrom(msg.sender, recipient, amount);
-    if (!success) {
+    // Calculate fee and net amount
+    uint256 fee = (amount * feesBps) / 10000;
+    uint256 netAmount = amount - fee;
+
+    // Transfer fee to the owner and net amount to the recipient in one go
+    if (
+      !token.transferFrom(msg.sender, owner(), fee) ||
+      !token.transferFrom(msg.sender, recipient, netAmount)
+    ) {
       revert TipFailed('Transfer failed');
     }
 
@@ -88,7 +100,7 @@ contract HeyTipping is
       tokenAddress,
       msg.sender,
       recipient,
-      amount,
+      netAmount,
       profileId,
       publicationId
     );
--- packages/contracts/script/HeyTipping/deploy.ts
+++ packages/contracts/script/HeyTipping/deploy.ts
@@ -2,9 +2,13 @@ const hre = require('hardhat');
 
 async function deployProxy() {
   const owner = '0x03Ba34f6Ea1496fa316873CF8350A3f7eaD317EF';
+  const feesBps = '500'; // 5%
 
   const HeyTipping = await hre.ethers.getContractFactory('HeyTipping');
-  const deployProxy = await hre.upgrades.deployProxy(HeyTipping, [owner]);
+  const deployProxy = await hre.upgrades.deployProxy(HeyTipping, [
+    owner,
+    feesBps
+  ]);
   await deployProxy.waitForDeployment();
 
   console.log(`HeyTipping deployed to ${await deployProxy.getAddress()}`);
