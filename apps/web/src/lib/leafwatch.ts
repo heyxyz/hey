ecf0f292a686892e8123a498ceca5bc86009fde7
bigint <69431456+bigint@users.noreply.github.com>
2024-04-23 12:51:30 +0530

    feat: add score endpoint

--- apps/web/src/lib/leafwatch.ts
+++ apps/web/src/lib/leafwatch.ts
@@ -2,7 +2,6 @@ import { HEY_API_URL } from '@hey/data/constants';
 import { Localstorage } from '@hey/data/storage';
 
 import getAuthApiHeaders from './getAuthApiHeaders';
-import getCurrentSession from './getCurrentSession';
 
 let worker: Worker;
 
@@ -14,28 +13,20 @@ if (typeof Worker !== 'undefined') {
  * Leafwatch analytics
  */
 export const Leafwatch = {
-  track: (
-    name: string,
-    properties?: Record<string, unknown>,
-    scoreAddress?: string
-  ) => {
-    const { evmAddress: wallet, id: sessionProfileId } = getCurrentSession();
+  track: (name: string, properties?: Record<string, unknown>) => {
     const { referrer } = document;
     const referrerDomain = referrer ? new URL(referrer).hostname : null;
     const fingerprint = localStorage.getItem(Localstorage.FingerprintStore);
 
     worker.postMessage({
       accessToken: getAuthApiHeaders()['X-Access-Token'] || undefined,
-      actor: sessionProfileId || undefined,
       fingerprint: fingerprint || undefined,
       name,
       network: getAuthApiHeaders()['X-Lens-Network'] || undefined,
       platform: 'web',
       properties,
       referrer: referrerDomain,
-      scoreAddress,
-      url: window.location.href,
-      wallet: wallet || undefined
+      url: window.location.href
     });
 
     worker.onmessage = (event: MessageEvent) => {
