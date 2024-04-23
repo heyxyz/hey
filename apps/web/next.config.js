c5f60e24221edc38dd342a463acdbc32c79a36bd
bigint <69431456+bigint@users.noreply.github.com>
2024-04-23 20:00:45 +0530

    feat: add gitcoin

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -32,7 +32,7 @@ const nextConfig = {
       },
       {
         destination:
-          'https://explorer.gitcoin.co/#/round/424/0xd4cc0dd193c7dc1d665ae244ce12d7fab337a008/0xd4cc0dd193c7dc1d665ae244ce12d7fab337a008-4',
+          'https://explorer.gitcoin.co/#/round/42161/25/1?utm_source=hey.xyz',
         permanent: true,
         source: '/gitcoin'
       },
