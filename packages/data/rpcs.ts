9e126396e05e3767243560c927148e51b65fef34
bigint <69431456+bigint@users.noreply.github.com>
2024-04-23 09:02:54 +0530

    feat: add rpcs test cases

--- packages/data/rpcs.ts
+++ packages/data/rpcs.ts
@@ -2,12 +2,12 @@ export const POLYGON_RPCS = [
   'https://lb.nodies.app/v1/c4af832850924699b25128e185bde36e',
   'https://polygon-pokt.nodies.app',
   'https://rpc.ankr.com/polygon',
-  'https://polygon-bor-rpc.publicnode.com',
-  'https://1rpc.io/matic'
+  'https://1rpc.io/matic',
+  'https://polygon-bor-rpc.publicnode.com'
 ];
 
 export const POLYGON_AMOY_RPCS = [
-  'https://lb.nodies.app/v1/1c9670faa1fb4c59ac4b0185ec9f19b7',
   'https://rpc-amoy.polygon.technology',
+  'https://lb.nodies.app/v1/1c9670faa1fb4c59ac4b0185ec9f19b7',
   'https://polygon-amoy-bor-rpc.publicnode.com'
 ];
