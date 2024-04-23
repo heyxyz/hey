5eda9121ccef2730baf21ff9cdce45220d62f68c
bigint <69431456+bigint@users.noreply.github.com>
2024-04-23 08:55:21 +0530

    chore: update rpcs

--- packages/data/rpcs.ts
+++ packages/data/rpcs.ts
@@ -1,12 +1,13 @@
 export const POLYGON_RPCS = [
+  'https://lb.nodies.app/v1/c4af832850924699b25128e185bde36e',
+  'https://polygon-pokt.nodies.app',
   'https://rpc.ankr.com/polygon',
-  'https://lb.nodies.app/v1/975f16c52f5f4732b20b6692137eec17',
   'https://polygon-bor-rpc.publicnode.com',
   'https://1rpc.io/matic'
 ];
 
 export const POLYGON_AMOY_RPCS = [
+  'https://lb.nodies.app/v1/1c9670faa1fb4c59ac4b0185ec9f19b7',
   'https://rpc-amoy.polygon.technology',
-  'https://polygon-amoy-bor-rpc.publicnode.com',
-  'https://lb.nodies.app/v1/6a17e9caa9f1424c87be69746244a8c8'
+  'https://polygon-amoy-bor-rpc.publicnode.com'
 ];
