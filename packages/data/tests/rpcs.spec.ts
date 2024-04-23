3f6bd8ca2eb88c0751d544ba893652324d29f1ef
bigint <69431456+bigint@users.noreply.github.com>
2024-04-23 09:03:23 +0530

    feat: add rpcs test cases

---
+++ packages/data/tests/rpcs.spec.ts
@@ -0,0 +1,19 @@
+import { describe, expect, test } from 'vitest';
+
+import { POLYGON_AMOY_RPCS, POLYGON_RPCS } from '../rpcs';
+
+describe('rpc liveliness', () => {
+  for (const rpc of POLYGON_RPCS) {
+    test(`should return 200 for POLYGON_RPCS - ${rpc}`, async () => {
+      const response = await fetch(rpc);
+      expect(response.status).toEqual(200);
+    });
+  }
+
+  for (const rpc of POLYGON_AMOY_RPCS) {
+    test(`should return 200 for POLYGON_AMOY_RPCS - ${rpc}`, async () => {
+      const response = await fetch(rpc);
+      expect(response.status).toEqual(200);
+    });
+  }
+});
