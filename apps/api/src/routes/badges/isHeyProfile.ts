2d82c587c82f2709fa7b3f5b0e18121c58d2a83e
bigint <69431456+bigint@users.noreply.github.com>
2024-04-24 08:47:12 +0530

    chore: move isHeyProfile badge check to use Lens PG Proxy

--- apps/api/src/routes/badges/isHeyProfile.ts
+++ apps/api/src/routes/badges/isHeyProfile.ts
@@ -4,7 +4,6 @@ import type { Address } from 'viem';
 import { HEY_LENS_SIGNUP } from '@hey/data/constants';
 import logger from '@hey/lib/logger';
 import catchedError from 'src/lib/catchedError';
-import { CACHE_AGE_INDEFINITE } from 'src/lib/constants';
 import lensPrisma from 'src/lib/lensPrisma';
 import { noBody } from 'src/lib/responses';
 import { getAddress } from 'viem';
@@ -19,7 +18,7 @@ export const get: Handler = async (req, res) => {
 
   try {
     const formattedAddress = address
-      ? getAddress(address as Address).toLowerCase()
+      ? getAddress(address as Address)
       : undefined;
     const data = await lensPrisma.$queryRaw<{ result: boolean }[]>`
       SELECT EXISTS (
@@ -27,7 +26,7 @@ export const get: Handler = async (req, res) => {
         FROM profile.record p
         JOIN app.onboarding_profile o ON p.profile_id = o.profile_id
         WHERE 
-          (p.profile_id = ${id} OR p.owned_by = ${address})
+          (p.profile_id = ${id} OR p.owned_by = ${formattedAddress})
           AND o.onboarded_by_address = ${HEY_LENS_SIGNUP}
       ) AS result;  
     `;
@@ -36,13 +35,15 @@ export const get: Handler = async (req, res) => {
 
     logger.info(`Hey profile badge fetched for ${id || formattedAddress}`);
 
-    return res
-      .status(200)
-      .setHeader(
-        'Cache-Control',
-        isHeyProfile ? CACHE_AGE_INDEFINITE : 'no-cache'
-      )
-      .json({ isHeyProfile, success: true });
+    return (
+      res
+        .status(200)
+        // .setHeader(
+        //   'Cache-Control',
+        //   isHeyProfile ? CACHE_AGE_INDEFINITE : 'no-cache'
+        // )
+        .json({ isHeyProfile, success: true })
+    );
   } catch (error) {
     return catchedError(res, error);
   }
