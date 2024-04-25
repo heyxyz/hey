17705de98dae1377cfc7de59eb3c433893b2051f
bigint <69431456+bigint@users.noreply.github.com>
2024-04-25 20:49:03 +0530

    chore: remove mod followers

--- apps/web/src/components/Mod/FeedType.tsx
+++ apps/web/src/components/Mod/FeedType.tsx
@@ -3,7 +3,6 @@ import type { Dispatch, FC, SetStateAction } from 'react';
 import {
   ClockIcon,
   MagnifyingGlassIcon,
-  UserPlusIcon,
   UsersIcon
 } from '@heroicons/react/24/outline';
 import { ModFeedType } from '@hey/data/enums';
@@ -34,12 +33,6 @@ const FeedType: FC<FeedTypeProps> = ({ feedType, setFeedType }) => {
         icon={<UsersIcon className="size-4" />}
         name="Profiles"
         onClick={() => setFeedType(ModFeedType.PROFILES)}
-      />
-      <TabButton
-        active={feedType === ModFeedType.FOLLOWERINGS}
-        icon={<UserPlusIcon className="size-4" />}
-        name="Followerings"
-        onClick={() => setFeedType(ModFeedType.FOLLOWERINGS)}
       />
     </div>
   );
