c4e108f13349b237e31b548326aa7e05bf5c09b6
bigint <69431456+bigint@users.noreply.github.com>
2024-04-25 09:11:13 +0530

    feat: add optimistic tip

--- apps/web/src/store/non-persisted/useTipsStore.ts
+++ apps/web/src/store/non-persisted/useTipsStore.ts
@@ -6,11 +6,29 @@ import { createTrackedSelector } from 'react-tracked';
 import { create } from 'zustand';
 
 interface State {
+  addTip: (id: string) => void;
   fetchAndStoreTips: (ids: string[]) => void;
   publicationTips: PublicationTip[];
 }
 
-const store = create<State>((set) => ({
+const store = create<State>((set, get) => ({
+  addTip: (id) => {
+    const existingTip = get().publicationTips.find((tip) => tip.id === id);
+    if (existingTip) {
+      set((state) => ({
+        publicationTips: state.publicationTips.map((tip) =>
+          tip.id === id ? { ...tip, count: tip.count + 1, tipped: true } : tip
+        )
+      }));
+    } else {
+      set((state) => ({
+        publicationTips: [
+          ...state.publicationTips,
+          { count: 1, id, tipped: true }
+        ]
+      }));
+    }
+  },
   fetchAndStoreTips: async (ids) => {
     if (!ids.length) {
       return;
