c15d7e3c5a8b654fcef4b4f585ae03f32133a3cf
bigint <69431456+bigint@users.noreply.github.com>
2024-04-25 22:27:02 +0530

    feat: add mvp reports list ui

--- packages/lens/apollo/cache/createModLatestReportsFieldPolicy.ts
+++ packages/lens/apollo/cache/createModLatestReportsFieldPolicy.ts
@@ -3,7 +3,7 @@ import type { FieldPolicy } from '@apollo/client';
 import { cursorBasedPagination } from '../lib';
 
 const createModLatestReportsFieldPolicy = (): FieldPolicy => {
-  return cursorBasedPagination(['request', ['limit']]);
+  return cursorBasedPagination(['request', ['limit', 'forPublication']]);
 };
 
 export default createModLatestReportsFieldPolicy;
