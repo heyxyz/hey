1674272dc47eaf2b601c188a73ad2e61d0631191
ocavue <ocavue@users.noreply.github.com>
2024-04-28 18:01:49 +1000

    Add tag matching

--- apps/web/src/components/Composer/TextEditor/extension.ts
+++ apps/web/src/components/Composer/TextEditor/extension.ts
@@ -1,8 +1,10 @@
+import { Regex } from '@hey/data/regex';
 import {
   defineBaseCommands,
   defineBaseKeymap,
   defineDoc,
   defineHistory,
+  defineMarkSpec,
   defineParagraph,
   defineText,
   union
@@ -11,11 +13,42 @@ import { defineBold } from 'prosekit/extensions/bold';
 import { defineCode } from 'prosekit/extensions/code';
 import { defineHeading } from 'prosekit/extensions/heading';
 import { defineItalic } from 'prosekit/extensions/italic';
-import { defineLink } from 'prosekit/extensions/link';
+import { defineLinkMarkRule, defineLinkSpec } from 'prosekit/extensions/link';
+import { defineMarkRule } from 'prosekit/extensions/mark-rule';
 import { defineMention } from 'prosekit/extensions/mention';
 import { definePlaceholder } from 'prosekit/extensions/placeholder';
 import { defineUnderline } from 'prosekit/extensions/underline';
 import { defineVirtualSelection } from 'prosekit/extensions/virtual-selection';
+
+function defineHashtag() {
+  return union([
+    defineMarkSpec({
+      name: 'hashtag',
+      toDOM: () => ['span', { class: 'text-brand-500' }, 0]
+    }),
+    defineMarkRule({
+      regex: Regex.hashtag,
+      type: 'hashtag'
+    })
+  ]);
+}
+
+function defineCashtag() {
+  return union([
+    defineMarkSpec({
+      name: 'cashtag',
+      toDOM: () => ['span', { class: 'text-brand-500' }, 0]
+    }),
+    defineMarkRule({
+      regex: Regex.cashtag,
+      type: 'cashtag'
+    })
+  ]);
+}
+
+function defineLink() {
+  return union([defineLinkMarkRule(), defineLinkSpec()]);
+}
 
 export function defineTextEditorExtension() {
   return union([
@@ -30,6 +63,8 @@ export function defineTextEditorExtension() {
     defineBold(),
     defineUnderline(),
     defineCode(),
+    defineHashtag(),
+    defineCashtag(),
     defineLink(),
     defineVirtualSelection(),
     defineMention(),
