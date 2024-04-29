efee40135187b7159c56c1893358b0502d3a65b0
ocavue <ocavue@gmail.com>
2024-04-29 22:14:20 +1000

    Remove `fetch` and add `useQuery`

--- apps/web/src/components/Composer/TextEditor/useEmojiQuery.ts
+++ apps/web/src/components/Composer/TextEditor/useEmojiQuery.ts
@@ -2,27 +2,25 @@ import type { Emoji } from '@hey/types/misc';
 
 import { STATIC_ASSETS_URL } from '@hey/data/constants';
 import { useEffect, useMemo, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
 const MAX_EMOJI_SUGGESTION_COUNT = 5;
 
-export const useEmojiQuery = (load: boolean, query: string): Emoji[] => {
-  const [emojis, setEmojis] = useState<Emoji[]>([]);
-  const [loaded, setLoaded] = useState(false);
-
-  const fetchEmojis = async () => {
-    const res = await fetch(`${STATIC_ASSETS_URL}/emoji.json`);
-    const data = await res.json();
-    setEmojis(data);
-  };
-
-  useEffect(() => {
-    if (load && !loaded) {
-      fetchEmojis();
-      setLoaded(true);
-    }
-  }, [load, loaded]);
+export const useEmojiQuery = (query: string): Emoji[] => {
+  const { data: emojis } = useQuery<Emoji[]>({
+    queryFn: async () => {
+      const response = await axios.get(`${STATIC_ASSETS_URL}/emoji.json`);
+      return response.data;
+    },
+    queryKey: ['getEmojis']
+  });
 
   return useMemo(() => {
+    if (!emojis || query.length <= 2) {
+      return [];
+    }
+
     return emojis
       .filter((emoji) => {
         return (
