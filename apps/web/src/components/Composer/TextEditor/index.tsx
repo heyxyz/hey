5d511e03dffb8fa1388864ad35c4e840300c78ab
ocavue <ocavue@users.noreply.github.com>
2024-05-03 00:31:04 +1000

    Support draft

--- apps/web/src/components/Composer/TextEditor/index.tsx
+++ apps/web/src/components/Composer/TextEditor/index.tsx
@@ -1,3 +1,3 @@
 import TextEditor from './TextEditor';
 export default TextEditor;
-export type { TextEditorHandle } from './useEditorHandle';
+export type { EditorRef, TextEditorHandle } from './useEditorHandle';
--- apps/web/src/components/Composer/TextEditor/useEditorHandle.ts
+++ apps/web/src/components/Composer/TextEditor/useEditorHandle.ts
@@ -1,12 +1,26 @@
-import type { Editor } from 'prosekit/core';
-
+import { type Editor, nodeFromHTML } from 'prosekit/core';
 import { useImperativeHandle } from 'react';
 
 import type { TextEditorExtension } from './extension';
 
+import { htmlFromMarkdown } from './markdown';
+
+/**
+ * Some methods for operating the editor from outside the editor component.
+ */
 export interface TextEditorHandle {
-  insertText: (emoji: string) => void;
+  /**
+   * Insert text at the current text cursor position.
+   */
+  insertText: (text: string) => void;
+
+  /**
+   * Replace the current document with the given markdown.
+   */
+  setMarkdown: (markdown: string) => void;
 }
+
+export type EditorRef = React.RefObject<TextEditorHandle>;
 
 export const useEditorHandle = (
   editor: Editor<TextEditorExtension>,
@@ -16,9 +30,24 @@ export const useEditorHandle = (
     ref,
     (): TextEditorHandle => ({
       insertText: (text: string): void => {
-        if (editor.mounted) {
-          editor.commands.insertText({ text });
+        if (!editor.mounted) {
+          return;
         }
+
+        editor.commands.insertText({ text });
+      },
+      setMarkdown: (markdown: string): void => {
+        if (!editor.mounted) {
+          return;
+        }
+
+        const html = htmlFromMarkdown(markdown);
+        const { view } = editor;
+        const { state } = view;
+        const doc = nodeFromHTML(html, { schema: state.schema });
+        view.dispatch(
+          state.tr.replaceWith(0, state.doc.content.size, doc.content)
+        );
       }
     }),
     [editor]
