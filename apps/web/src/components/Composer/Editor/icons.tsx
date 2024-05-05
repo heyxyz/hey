6bea63f66f6e391842e284bc7606fc0f6b405930
bigint <69431456+bigint@users.noreply.github.com>
2024-05-05 19:13:24 +0530

    chore: remove code, strike and underline for now

--- apps/web/src/components/Composer/Editor/icons.tsx
+++ apps/web/src/components/Composer/Editor/icons.tsx
@@ -24,30 +24,6 @@ export const BoldIcon = (props: SVGProps<SVGSVGElement>) => {
   );
 };
 
-export const CodeIcon = (props: SVGProps<SVGSVGElement>) => {
-  // Lucide Icon
-  // License: ISC
-  // https://lucide.dev/license
-  return (
-    <svg
-      height="1em"
-      viewBox="0 0 24 24"
-      width="1em"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
-      <path
-        d="m16 18l6-6l-6-6M8 6l-6 6l6 6"
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-      />
-    </svg>
-  );
-};
-
 export const ItalicIcon = (props: SVGProps<SVGSVGElement>) => {
   // Lucide Icon
   // License: ISC
@@ -62,54 +38,6 @@ export const ItalicIcon = (props: SVGProps<SVGSVGElement>) => {
     >
       <path
         d="M19 4h-9m4 16H5M15 4L9 20"
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-      />
-    </svg>
-  );
-};
-
-export const StrikethroughIcon = (props: SVGProps<SVGSVGElement>) => {
-  // Lucide Icon
-  // License: ISC
-  // https://lucide.dev/license
-  return (
-    <svg
-      height="1em"
-      viewBox="0 0 24 24"
-      width="1em"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
-      <path
-        d="M16 4H9a3 3 0 0 0-2.83 4M14 12a4 4 0 0 1 0 8H6m-2-8h16"
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-      />
-    </svg>
-  );
-};
-
-export const UnderlineIcon = (props: SVGProps<SVGSVGElement>) => {
-  // Lucide Icon
-  // License: ISC
-  // https://lucide.dev/license
-  return (
-    <svg
-      height="1em"
-      viewBox="0 0 24 24"
-      width="1em"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
-      <path
-        d="M6 4v6a6 6 0 0 0 12 0V4M4 20h16"
         fill="none"
         stroke="currentColor"
         strokeLinecap="round"
