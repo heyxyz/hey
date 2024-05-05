9c63b4c5be6650c3be1a77bb06066c7a37bdeb0e
bigint <69431456+bigint@users.noreply.github.com>
2024-05-05 19:53:52 +0530

    chore: refactor editor

---
+++ apps/web/src/hooks/prosekit/useEmojiQuery.ts
@@ -0,0 +1,35 @@
+import type { Emoji } from '@hey/types/misc';
+
+import { STATIC_ASSETS_URL } from '@hey/data/constants';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { useMemo } from 'react';
+
+const MAX_EMOJI_SUGGESTION_COUNT = 5;
+
+const useEmojiQuery = (query: string): Emoji[] => {
+  const { data: emojis } = useQuery<Emoji[]>({
+    queryFn: async () => {
+      const response = await axios.get(`${STATIC_ASSETS_URL}/emoji.json`);
+      return response.data;
+    },
+    queryKey: ['getEmojis']
+  });
+
+  return useMemo(() => {
+    if (!emojis) {
+      return [];
+    }
+
+    return emojis
+      .filter((emoji) => {
+        return (
+          emoji.aliases.some((alias) => alias.includes(query)) ||
+          emoji.tags.some((tag) => tag.includes(query))
+        );
+      })
+      .slice(0, MAX_EMOJI_SUGGESTION_COUNT);
+  }, [query, emojis]);
+};
+
+export default useEmojiQuery;
