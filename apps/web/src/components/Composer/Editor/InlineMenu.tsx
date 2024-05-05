335f4e10ece0b8e72d5d3273b20780e857d1e07b
bigint <69431456+bigint@users.noreply.github.com>
2024-05-05 18:59:26 +0530

    chore: refactor types

--- apps/web/src/components/Composer/Editor/InlineMenu.tsx
+++ apps/web/src/components/Composer/Editor/InlineMenu.tsx
@@ -18,50 +18,46 @@ const InlineMenu: FC = () => {
   const editor = useEditor<EditorExtension>({ update: true });
 
   return (
-    <InlinePopover className="relative z-10 box-border flex min-w-[120px] space-x-1 overflow-auto whitespace-nowrap rounded-md border border-gray-200 bg-white p-1 shadow-lg dark:border-gray-800 dark:bg-neutral-900">
+    <InlinePopover className="z-10 box-border flex min-w-[120px] space-x-1 rounded-md border border-gray-200 bg-white shadow-sm dark:border-gray-800 dark:bg-neutral-900">
       <Toggle
         disabled={!editor.commands.toggleBold.canApply()}
         onClick={() => editor.commands.toggleBold()}
         pressed={editor.marks.bold.isActive()}
         tooltip="Bold"
       >
-        <BoldIcon className="h-5 w-5" />
+        <BoldIcon className="size-4" />
       </Toggle>
-
       <Toggle
         disabled={!editor.commands.toggleItalic.canApply()}
         onClick={() => editor.commands.toggleItalic()}
         pressed={editor.marks.italic.isActive()}
         tooltip="Italic"
       >
-        <ItalicIcon className="h-5 w-5" />
+        <ItalicIcon className="size-4" />
       </Toggle>
-
       <Toggle
         disabled={!editor.commands.toggleUnderline.canApply()}
         onClick={() => editor.commands.toggleUnderline()}
         pressed={editor.marks.underline.isActive()}
         tooltip="Underline"
       >
-        <UnderlineIcon className="h-5 w-5" />
+        <UnderlineIcon className="size-4" />
       </Toggle>
-
       <Toggle
         disabled={!editor.commands.toggleCode.canApply()}
         onClick={() => editor.commands.toggleCode()}
         pressed={editor.marks.code.isActive()}
         tooltip="Code"
       >
-        <CodeIcon className="h-5 w-5" />
+        <CodeIcon className="size-4" />
       </Toggle>
-
       <Toggle
         disabled={!editor.commands.toggleStrike.canApply()}
         onClick={() => editor.commands.toggleStrike()}
         pressed={editor.marks.strike.isActive()}
         tooltip="Strikethrough"
       >
-        <StrikethroughIcon className="h-5 w-5" />
+        <StrikethroughIcon className="size-4" />
       </Toggle>
     </InlinePopover>
   );
