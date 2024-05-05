93d2923847009de87bfe55ee651b90b03a953554
bigint <69431456+bigint@users.noreply.github.com>
2024-05-05 19:09:47 +0530

    chore: refactor editor

---
+++ apps/web/src/helpers/markdown.ts
@@ -0,0 +1,29 @@
+import rehypeParse from 'rehype-parse';
+import rehypeRemark from 'rehype-remark';
+import remarkGfm from 'remark-gfm';
+import remarkHtml from 'remark-html';
+import remarkParse from 'remark-parse';
+import remarkStringify from 'remark-stringify';
+import { unified } from 'unified';
+
+import { remarkLinkProtocol } from '../components/Composer/Editor/remarkLinkProtocol';
+
+export const markdownFromHTML = (html: string): string => {
+  return unified()
+    .use(rehypeParse)
+    .use(rehypeRemark)
+    .use(remarkGfm)
+    .use(remarkLinkProtocol)
+    .use(remarkStringify)
+    .processSync(html)
+    .toString();
+};
+
+export const htmlFromMarkdown = (markdown: string): string => {
+  return unified()
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkHtml)
+    .processSync(markdown)
+    .toString();
+};
