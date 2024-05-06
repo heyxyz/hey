d6c5b63a561c069e25adc3a71d3f65f5cd563e07
bigint <69431456+bigint@users.noreply.github.com>
2024-05-06 09:45:03 +0530

    chore: add suspend inside gardener itself

--- apps/web/src/components/Publication/Actions/HigherActions/index.tsx
+++ apps/web/src/components/Publication/Actions/HigherActions/index.tsx
@@ -1,22 +1,16 @@
 import type { MirrorablePublication } from '@hey/lens';
 import type { FC } from 'react';
 
-import { useFeatureFlagsStore } from 'src/store/persisted/useFeatureFlagsStore';
-
 import GardenerActions from './GardenerActions';
-import StaffActions from './StaffActions';
 
 interface HigherActionsProps {
   publication: MirrorablePublication;
 }
 
 const HigherActions: FC<HigherActionsProps> = ({ publication }) => {
-  const { staffMode } = useFeatureFlagsStore();
-
   return (
     <div className="m-5">
       <GardenerActions publication={publication} />
-      {staffMode && <StaffActions publication={publication} />}
     </div>
   );
 };
