9d9a9c80f7cf4f60ce75a29f6571c2323c6974cd
bigint <69431456+bigint@users.noreply.github.com>
2024-05-06 18:41:56 +0530

    fix: type imports

--- apps/web/src/components/Composer/Actions/OpenActionSettings/Config/Swap/PoolConfig.tsx
+++ apps/web/src/components/Composer/Actions/OpenActionSettings/Config/Swap/PoolConfig.tsx
@@ -1,11 +1,12 @@
 import type { RewardPool } from '@hey/types/misc';
 import type { FC } from 'react';
+import type { Address } from 'viem';
 
 import { GiftIcon } from '@heroicons/react/24/outline';
 import getSwapRewardPool from '@hey/helpers/getSwapRewardPool';
 import humanize from '@hey/helpers/humanize';
 import { useEffect, useState } from 'react';
-import { type Address, formatUnits, isAddress } from 'viem';
+import { formatUnits, isAddress } from 'viem';
 
 import { useSwapActionStore } from '.';
 import RewardConfig from './RewardConfig';
