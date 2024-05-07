e147eb4149090564b694eedd24a2c8e67be6afe2
ocavue <ocavue@gmail.com>
2024-05-07 19:31:33 +1000

    fix: don't escape markdown underscores

--- apps/web/src/helpers/prosekit/markdown.ts
+++ apps/web/src/helpers/prosekit/markdown.ts
@@ -6,14 +6,22 @@ import remarkParse from 'remark-parse';
 import remarkStringify from 'remark-stringify';
 import { unified } from 'unified';
 
+// By default, remark-stringify escapes underscores (i.e. "_" => "\_"). We want
+// to disable this behavior so that we can have underscores in mention handles.
+const unescapeUnderscore = (str: string) => {
+  return str.replace(/(^|[^\\])\\_/g, '$1_');
+};
+
 export const markdownFromHTML = (html: string): string => {
-  return unified()
+  const markdown = unified()
     .use(rehypeParse)
     .use(rehypeRemark)
     .use(remarkLinkProtocol)
     .use(remarkStringify)
     .processSync(html)
     .toString();
+
+  return unescapeUnderscore(markdown);
 };
 
 export const htmlFromMarkdown = (markdown: string): string => {
