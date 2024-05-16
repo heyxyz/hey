3bbddf7a86ad995a5938b96fd3c80ad8995ad6b4
bigint <69431456+bigint@users.noreply.github.com>
2024-05-16 20:00:35 +0530

    feat: add test cases for badges/isHeyProfile

--- apps/api/src/routes/badges/isHeyProfile.ts
+++ apps/api/src/routes/badges/isHeyProfile.ts
@@ -9,7 +9,6 @@ import { CACHE_AGE_INDEFINITE } from 'src/helpers/constants';
 import { noBody } from 'src/helpers/responses';
 import { getAddress } from 'viem';
 
-// TODO: add tests
 export const get: Handler = async (req, res) => {
   const { address, id } = req.query;
 
@@ -34,6 +33,8 @@ export const get: Handler = async (req, res) => {
       `,
       [id, formattedAddress, HEY_LENS_SIGNUP]
     );
+
+    console.log(data);
 
     const isHeyProfile = data[0]?.result;
 
