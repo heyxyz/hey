572395124cb1c6b79b5acac884af48b19c8a9057
bigint <69431456+bigint@users.noreply.github.com>
2024-05-16 23:02:14 +0530

    fix: decent nft ui

--- apps/web/src/components/Shared/Shimmer/DecentOpenActionShimmer.tsx
+++ apps/web/src/components/Shared/Shimmer/DecentOpenActionShimmer.tsx
@@ -2,21 +2,14 @@ import type { FC } from 'react';
 
 const DecentOpenActionShimmer: FC = () => {
   return (
-    <div className="flex items-start space-x-3 border-t px-5 py-4 dark:border-gray-700">
-      <div>
-        <div className="shimmer size-6 rounded-full" />
-      </div>
-      <div className="w-full space-y-2">
-        <div className="item flex flex-col items-start justify-between sm:flex-row sm:items-center">
-          <div>
-            <div className="item flex space-x-3 pt-1">
-              <div className="shimmer h-3 w-28 rounded-lg" />
-              <div className="shimmer hidden h-3 w-20 rounded-lg sm:block" />
-            </div>
-            <div className="shimmer mt-2 h-3 w-12 rounded-lg" />
-          </div>
-          <div className="shimmer mt-2 h-10 w-full rounded-full bg-red-500 sm:mt-0 sm:w-24" />
+    <div className="flex items-center space-x-2 border-t px-4 py-2 dark:border-gray-700">
+      <div className="shimmer size-5 rounded-full" />
+      <div className="flex w-full items-center justify-between">
+        <div className="item flex space-x-3">
+          <div className="shimmer h-3 w-24 rounded-lg" />
+          <div className="shimmer h-3 w-16 rounded-lg" />
         </div>
+        <div className="shimmer h-[30px] w-[85px] rounded-full" />
       </div>
     </div>
   );
