004e0027344b0428c4b58750876397e242e2fb36
bigint <69431456+bigint@users.noreply.github.com>
2024-05-16 22:05:15 +0530

    chore: add referrer

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -32,7 +32,7 @@ const nextConfig = {
       },
       {
         destination:
-          'https://zora.co/collect/zora:0xf2086c0eaa8b34b0eef73920d0b1b53f4146e2e4/1',
+          'https://zora.co/collect/zora:0xf2086c0eaa8b34b0eef73920d0b1b53f4146e2e4/1?referrer=0x03Ba34f6Ea1496fa316873CF8350A3f7eaD317EF',
         permanent: true,
         source: '/zorb'
       },
