e151a07fb2b6d5ed4ce96d4a39b79c8abe7df8e9
bigint <69431456+bigint@users.noreply.github.com>
2024-05-17 11:48:20 +0530

    fix: decent nft code

---
+++ apps/web/src/components/Publication/OpenAction/UnknownModule/Decent/QuantityConfig.tsx
@@ -0,0 +1,40 @@
+import type { FC } from 'react';
+
+import { MinusIcon, PlusIcon } from '@heroicons/react/24/outline';
+import stopEventPropagation from '@hey/helpers/stopEventPropagation';
+
+import { useNftOpenActionStore } from './FeedEmbed';
+
+const QuantityConfig: FC = () => {
+  const { selectedQuantity, setSelectedQuantity } = useNftOpenActionStore();
+
+  return (
+    <div className="flex items-center justify-between border-y border-zinc-200 px-5 py-4">
+      <p className="ld-text-gray-500">Quantity</p>
+      <div className="flex items-center space-x-4">
+        <button
+          className="flex size-6 items-center justify-center rounded-full bg-gray-200 disabled:opacity-50"
+          disabled={selectedQuantity === 1}
+          onClick={(e) => {
+            stopEventPropagation(e);
+            setSelectedQuantity(selectedQuantity - 1);
+          }}
+        >
+          <MinusIcon className="size-3 stroke-black text-gray-600" />
+        </button>
+        <span className="size-6 text-center">{selectedQuantity}</span>
+        <button
+          className="flex size-6 items-center justify-center rounded-full bg-gray-200 disabled:opacity-40"
+          onClick={(e) => {
+            stopEventPropagation(e);
+            setSelectedQuantity(selectedQuantity + 1);
+          }}
+        >
+          <PlusIcon className="size-3 stroke-black text-gray-600" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default QuantityConfig;
