cc7781840a73508999d1506f6217e190f85b3995
bigint <69431456+bigint@users.noreply.github.com>
2024-05-24 10:41:34 +0530

    chore: remove feed type icon in mod

--- apps/web/src/components/Mod/FeedType.tsx
+++ apps/web/src/components/Mod/FeedType.tsx
@@ -1,11 +1,5 @@
 import type { Dispatch, FC, SetStateAction } from 'react';
 
-import {
-  ClockIcon,
-  FlagIcon,
-  MagnifyingGlassIcon,
-  UsersIcon
-} from '@heroicons/react/24/outline';
 import { ModFeedType } from '@hey/data/enums';
 import { TabButton } from '@hey/ui';
 
@@ -19,25 +13,21 @@ const FeedType: FC<FeedTypeProps> = ({ feedType, setFeedType }) => {
     <div className="flex gap-3 overflow-x-auto px-5 sm:px-0">
       <TabButton
         active={feedType === ModFeedType.LATEST}
-        icon={<ClockIcon className="size-4" />}
         name="Latest Publications"
         onClick={() => setFeedType(ModFeedType.LATEST)}
       />
       <TabButton
         active={feedType === ModFeedType.REPORTS}
-        icon={<FlagIcon className="size-4" />}
         name="Latest Reports"
         onClick={() => setFeedType(ModFeedType.REPORTS)}
       />
       <TabButton
         active={feedType === ModFeedType.SEARCH}
-        icon={<MagnifyingGlassIcon className="size-4" />}
         name="Search Publications"
         onClick={() => setFeedType(ModFeedType.SEARCH)}
       />
       <TabButton
         active={feedType === ModFeedType.PROFILES}
-        icon={<UsersIcon className="size-4" />}
         name="Search Profiles"
         onClick={() => setFeedType(ModFeedType.PROFILES)}
       />
