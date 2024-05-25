a810ec82cc6e120306bd9ebf56ae1103a4675cc0
bigint <69431456+bigint@users.noreply.github.com>
2024-05-25 11:09:58 +0530

    Revert "feat: add mvp cron (#4937)"
    This reverts commit 256fe9a1c98eaadd75cc34aa921e973d56d92033.

--- apps/api/scripts/dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Development script to run server and cron job concurrently
-echo "Starting development environment..."
-
-concurrently --prefix "{name}" \
-  "nodemon -w src -x tsx src/server.ts" \
-  "nodemon -w src -x tsx src/cron/index.ts" 
--- apps/api/src/cron/index.ts
+++
@@ -1,5 +0,0 @@
-import cron from 'node-cron';
-
-cron.schedule('*/2 * * * * *', () => {
-  console.log('Running a task every 2 seconds');
-});
