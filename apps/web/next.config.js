91dbd43ffc81fecf5d0e338c6a3cb6a3a4799475
bigint <69431456+bigint@users.noreply.github.com>
2024-05-25 10:40:51 +0530

    chore: add sentry profiling

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -12,6 +12,7 @@ const nextConfig = {
     return [
       {
         headers: [
+          { key: 'Document-Policy', value: 'js-profiling' },
           { key: 'X-Content-Type-Options', value: 'nosniff' },
           { key: 'X-XSS-Protection', value: '1; mode=block' },
           { key: 'Referrer-Policy', value: 'strict-origin' }
