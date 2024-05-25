edd27c2cd0a8cb0a29c9d85208c474d3e43be048
bigint <69431456+bigint@users.noreply.github.com>
2024-05-25 12:10:25 +0530

    feat: migrate to new cron

--- apps/api/src/cron/index.ts
+++ apps/api/src/cron/index.ts
@@ -1,5 +1,20 @@
+import dotenv from 'dotenv';
 import cron from 'node-cron';
 
-cron.schedule('*/2 * * * * *', () => {
-  console.log('Running a task every 2 seconds');
+import deletePublications from './deletePublications';
+import replicateGardeners from './replicateGardeners';
+import replicatePublications from './replicatePublications';
+
+dotenv.config({ override: true });
+
+cron.schedule('*/5 * * * *', async () => {
+  await replicateGardeners();
+});
+
+cron.schedule('*/10 * * * * *', async () => {
+  await replicatePublications();
+});
+
+cron.schedule('*/10 * * * * *', async () => {
+  await deletePublications();
 });
