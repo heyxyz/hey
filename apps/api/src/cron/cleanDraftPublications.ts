90e8884c2fe133678c6fb15b28b615d809d608f2
bigint <69431456+bigint@users.noreply.github.com>
2024-05-25 12:23:55 +0530

    feat: migrate to new cron

---
+++ apps/api/src/cron/cleanDraftPublications.ts
@@ -0,0 +1,15 @@
+import logger from '@hey/helpers/logger';
+import prisma from 'src/helpers/prisma';
+
+const cleanDraftPublications = async () => {
+  const { count } = await prisma.draftPublication.deleteMany({
+    where: {
+      updatedAt: { lt: new Date(Date.now() - 100 * 24 * 60 * 60 * 1000) }
+    }
+  });
+  logger.info(
+    `Cron: Cleaned up ${count} draft publications that are older than 100 days`
+  );
+};
+
+export default cleanDraftPublications;
