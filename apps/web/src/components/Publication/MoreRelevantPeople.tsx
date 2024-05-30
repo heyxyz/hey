5fce17ab34019a6c3e609ad09bc3f3789e0e3f4d
bigint <69431456+bigint@users.noreply.github.com>
2024-05-30 10:40:28 +0530

    feat: limit mentioned profiles on publication limit to 5 and add more button (#4952)

---
+++ apps/web/src/components/Publication/MoreRelevantPeople.tsx
@@ -0,0 +1,42 @@
+import type { Profile } from '@hey/lens';
+import type { FC } from 'react';
+
+import UserProfile from '@components/Shared/UserProfile';
+import { ProfileLinkSource } from '@hey/data/tracking';
+import { Virtuoso } from 'react-virtuoso';
+import { useProfileStore } from 'src/store/persisted/useProfileStore';
+
+interface MoreRelevantPeopleProps {
+  profiles: Profile[];
+}
+
+const MoreRelevantPeople: FC<MoreRelevantPeopleProps> = ({ profiles }) => {
+  const { currentProfile } = useProfileStore();
+
+  return (
+    <div className="max-h-[80vh] overflow-y-auto">
+      <Virtuoso
+        className="virtual-profile-list"
+        computeItemKey={(index, profile) => `${profile.id}-${index}`}
+        // remove the first 5 profiles from the list because they are already shown in the sidebar
+        data={profiles.slice(5)}
+        itemContent={(_, profile) => {
+          return (
+            <div className="p-5">
+              <UserProfile
+                hideFollowButton={currentProfile?.id === profile.id}
+                hideUnfollowButton={currentProfile?.id === profile.id}
+                profile={profile as Profile}
+                showBio
+                showUserPreview={false}
+                source={ProfileLinkSource.WhoToFollow}
+              />
+            </div>
+          );
+        }}
+      />
+    </div>
+  );
+};
+
+export default MoreRelevantPeople;
--- apps/web/src/components/Publication/RelevantPeople.tsx
+++ apps/web/src/components/Publication/RelevantPeople.tsx
@@ -1,12 +1,15 @@
 import type { Profile, ProfileMentioned } from '@hey/lens';
-import type { FC } from 'react';
 
 import UserProfileShimmer from '@components/Shared/Shimmer/UserProfileShimmer';
 import UserProfile from '@components/Shared/UserProfile';
+import { UsersIcon } from '@heroicons/react/24/outline';
 import { ProfileLinkSource } from '@hey/data/tracking';
 import { useProfilesQuery } from '@hey/lens';
-import { Card, ErrorMessage } from '@hey/ui';
+import { Card, ErrorMessage, Modal } from '@hey/ui';
+import { type FC, useState } from 'react';
 import { useProfileStore } from 'src/store/persisted/useProfileStore';
+
+import MoreRelevantPeople from './MoreRelevantPeople';
 
 interface RelevantPeopleProps {
   profilesMentioned: ProfileMentioned[];
@@ -14,6 +17,8 @@ interface RelevantPeopleProps {
 
 const RelevantPeople: FC<RelevantPeopleProps> = ({ profilesMentioned }) => {
   const { currentProfile } = useProfileStore();
+  const [showMore, setShowMore] = useState(false);
+
   const profileIds = profilesMentioned.map(
     (profile) => profile.snapshotHandleMentioned.linkedTo?.nftTokenId
   );
@@ -35,6 +40,9 @@ const RelevantPeople: FC<RelevantPeopleProps> = ({ profilesMentioned }) => {
         <UserProfileShimmer showFollowUnfollowButton />
         <UserProfileShimmer showFollowUnfollowButton />
         <UserProfileShimmer showFollowUnfollowButton />
+        <div className="pb-1 pt-2">
+          <div className="shimmer h-3 w-5/12 rounded-full" />
+        </div>
       </Card>
     );
   }
@@ -43,21 +51,42 @@ const RelevantPeople: FC<RelevantPeopleProps> = ({ profilesMentioned }) => {
     return null;
   }
 
+  const firstProfiles = data?.profiles?.items?.slice(0, 5);
+
   return (
-    <Card as="aside" className="space-y-4 p-5">
-      <ErrorMessage error={error} title="Failed to load relevant people" />
-      {data?.profiles?.items?.map((profile) => (
-        <div className="truncate" key={profile?.id}>
-          <UserProfile
-            hideFollowButton={currentProfile?.id === profile.id}
-            hideUnfollowButton={currentProfile?.id === profile.id}
-            profile={profile as Profile}
-            showUserPreview={false}
-            source={ProfileLinkSource.RelevantPeople}
-          />
-        </div>
-      ))}
-    </Card>
+    <>
+      <Card as="aside" className="space-y-4 p-5">
+        <ErrorMessage error={error} title="Failed to load relevant people" />
+        {firstProfiles?.map((profile) => (
+          <div className="truncate" key={profile?.id}>
+            <UserProfile
+              hideFollowButton={currentProfile?.id === profile.id}
+              hideUnfollowButton={currentProfile?.id === profile.id}
+              profile={profile as Profile}
+              showUserPreview={false}
+              source={ProfileLinkSource.RelevantPeople}
+            />
+          </div>
+        ))}
+        {(data?.profiles?.items?.length || 0) > 5 && (
+          <button
+            className="ld-text-gray-500 font-bold"
+            onClick={() => setShowMore(true)}
+            type="button"
+          >
+            Show more
+          </button>
+        )}
+      </Card>
+      <Modal
+        icon={<UsersIcon className="size-5" />}
+        onClose={() => setShowMore(false)}
+        show={showMore}
+        title="Relevant people"
+      >
+        <MoreRelevantPeople profiles={data?.profiles?.items as Profile[]} />
+      </Modal>
+    </>
   );
 };
 
