aef73a3a4003ef9e5a875c80b0743f590c9313f0
bigint <69431456+bigint@users.noreply.github.com>
2024-06-02 00:09:26 +0530

    feat: connections mvp (#4957)
    * feat: add connection routes
    
    * feat: add save github
    
    * feat: add discord storage
    
    * feat: add get connections endpoint

---
+++ apps/api/src/helpers/middlewares/validateLensAccessToken.ts
@@ -0,0 +1,49 @@
+import LensEndpoint from '@hey/data/lens-endpoints';
+import axios from 'axios';
+
+/**
+ * Middleware to validate Lens access token for connections
+ * @param accessToken Incoming access token
+ * @param network Incoming network
+ * @returns Response
+ */
+const validateLensAccessToken = async (
+  accessToken: string,
+  network: string
+): Promise<200 | 400 | 401 | 500> => {
+  const allowedNetworks = ['mainnet', 'testnet'];
+
+  if (!accessToken || !network || !allowedNetworks.includes(network)) {
+    return 400;
+  }
+
+  const isMainnet = network === 'mainnet';
+  try {
+    const { data } = await axios.post(
+      isMainnet ? LensEndpoint.Mainnet : LensEndpoint.Testnet,
+      {
+        query: `
+          query Verify {
+            verify(request: { accessToken: "${accessToken}" })
+          }
+        `
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'User-agent': 'Hey.xyz'
+        }
+      }
+    );
+
+    if (data.data.verify) {
+      return 200;
+    }
+
+    return 401;
+  } catch {
+    return 500;
+  }
+};
+
+export default validateLensAccessToken;
