082d4f88acf54a9ceb36a42de377426361b909f5
bigint <69431456+bigint@users.noreply.github.com>
2024-06-01 21:34:07 +0530

    feat: add signup graph in staff tool (#4955)

--- apps/web/src/components/Staff/Overview/ImpressionsToday.tsx
+++ apps/web/src/components/Staff/Overview/ImpressionsToday.tsx
@@ -49,7 +49,7 @@ const ImpressionsToday: FC<ImpressionsTodayProps> = ({ impressionsToday }) => {
                 backgroundColor:
                   resolvedTheme === 'dark'
                     ? colors['zinc']['900']
-                    : colors['zinc']['200'],
+                    : colors['zinc']['400'],
                 borderColor:
                   resolvedTheme === 'dark' ? colors['white'] : colors['black'],
                 data: impressionsToday.map((impression) => impression.count),
