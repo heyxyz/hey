d0d0cec8b2802acb02bc5b3b4e654e5c861485eb
bigint <69431456+bigint@users.noreply.github.com>
2024-06-01 23:43:57 +0530

    feat: add save github

---
+++ apps/api/src/db/migrations/20240601174448_remove_email/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `email` on the `GitHubConnection` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "GitHubConnection" DROP COLUMN "email";
