422fc66d471de2a221a5df302b47bcab38238617
bigint <69431456+bigint@users.noreply.github.com>
2024-06-04 18:14:21 +0530

    feat: migrate hasHeyNft to use lens PG

--- apps/api/src/routes/badges/hasHeyNft.ts
+++ apps/api/src/routes/badges/hasHeyNft.ts
@@ -1,48 +1,46 @@
 import type { Handler } from 'express';
 import type { Address } from 'viem';
 
-import { HeyMembershipNft } from '@hey/abis';
-import { HEY_MEMBERSHIP_NFT } from '@hey/data/constants';
+import { HEY_MEMBERSHIP_NFT_PUBLICATION_ID } from '@hey/data/constants';
 import logger from '@hey/helpers/logger';
+import lensPg from 'src/db/lensPg';
 import catchedError from 'src/helpers/catchedError';
-import {
-  CACHE_AGE_INDEFINITE,
-  SWR_CACHE_AGE_10_MINS_30_DAYS
-} from 'src/helpers/constants';
-import getRpc from 'src/helpers/getRpc';
+import { CACHE_AGE_INDEFINITE } from 'src/helpers/constants';
 import { noBody } from 'src/helpers/responses';
-import { createPublicClient } from 'viem';
-import { polygon } from 'viem/chains';
+import { getAddress } from 'viem';
 
 export const get: Handler = async (req, res) => {
-  const { address } = req.query;
+  const { address, id } = req.query;
 
-  if (!address) {
+  if (!id && !address) {
     return noBody(res);
   }
 
   try {
-    const client = createPublicClient({
-      chain: polygon,
-      transport: getRpc({ mainnet: true })
-    });
+    const formattedAddress = address
+      ? getAddress(address as Address)
+      : undefined;
+    const data = await lensPg.query(
+      `
+        SELECT EXISTS (
+          SELECT 1
+          FROM profile.record p
+          JOIN publication.open_action_module_acted_record o ON p.profile_id = o.acted_profile_id
+          WHERE
+            (p.profile_id = $1 OR p.owned_by = $2)
+            AND o.publication_id = $3
+        ) AS result;
+      `,
+      [id, formattedAddress, HEY_MEMBERSHIP_NFT_PUBLICATION_ID]
+    );
 
-    const heyNft = await client.readContract({
-      abi: HeyMembershipNft,
-      address: HEY_MEMBERSHIP_NFT,
-      args: [address as Address],
-      functionName: 'balanceOf'
-    });
-    const hasHeyNft = Number(heyNft) > 0;
+    const hasHeyNft = data[0]?.result;
 
-    logger.info(`Hey NFT badge fetched for ${address}`);
+    logger.info(`Hey NFT badge fetched for ${id || formattedAddress}`);
 
     return res
       .status(200)
-      .setHeader(
-        'Cache-Control',
-        hasHeyNft ? CACHE_AGE_INDEFINITE : SWR_CACHE_AGE_10_MINS_30_DAYS
-      )
+      .setHeader('Cache-Control', hasHeyNft ? CACHE_AGE_INDEFINITE : 'no-cache')
       .json({ hasHeyNft, success: true });
   } catch (error) {
     return catchedError(res, error);
