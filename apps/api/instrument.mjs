cbe8c6829e25cd15434acf2adf76a14c31387a42
bigint <69431456+bigint@users.noreply.github.com>
2024-06-06 12:12:51 +0530

    chore: rip sentry

--- apps/api/instrument.mjs
+++
@@ -1,6 +0,0 @@
-import * as Sentry from '@sentry/node';
-
-Sentry.init({
-  dsn: 'https://ea87088dce4e7b2b02221a2edc5c9c56@o180224.ingest.us.sentry.io/4506721358512128',
-  tracesSampleRate: 1.0
-});
--- apps/api/src/helpers/catchedError.ts
+++ apps/api/src/helpers/catchedError.ts
@@ -2,11 +2,9 @@ import type { Response } from 'express';
 
 import { Errors } from '@hey/data/errors';
 import logger from '@hey/helpers/logger';
-import * as Sentry from '@sentry/node';
 
 const catchedError = (res: Response, error: any) => {
   logger.error(error);
-  Sentry.captureException(error);
 
   return res
     .status(500)
