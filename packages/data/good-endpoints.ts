ab6aee840d22662fb3dd2fcfd420f50105776115
Ezra <ezra.huang2024@ecssen.ca>
2024-06-10 09:48:57 -0600

    fix backend api endpoint for deployment

--- packages/data/good-endpoints.ts
+++ packages/data/good-endpoints.ts
@@ -1,7 +1,7 @@
 enum GoodEndpoint {
   Mainnet = 'https://api.bcharity.net',
   Staging = 'https://api-staging.bcharity.net',
-  Testnet = 'https://api-testnet.bcharity.net'
+  Testnet = 'https://good-api-nine.vercel.app'
 }
 
 export default GoodEndpoint;
