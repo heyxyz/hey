c22c429b360778ea2ad9cee0987a630d0306604b
John2T <johntu537@gmail.com>
2024-06-11 15:07:42 -0700

    Minor alignment fixes for icons and text

--- apps/web/src/components/Shared/Navbar/MoreNavItems.tsx
+++ apps/web/src/components/Shared/Navbar/MoreNavItems.tsx
@@ -15,19 +15,19 @@ const MoreNavItems: FC = () => {
     <Menu as="div">
       {({ open }) => (
         <>
-          <MenuButton
-      className={cn(
-        'w-full cursor-pointer rounded-md px-2 py-1 text-left text-sm font-bold tracking-wide md:px-3 flex items-center space-x-2',
-        {
-          'bg-gray-200 text-black dark:bg-gray-800 dark:text-white': open,
-          'text-gray-700 hover:bg-gray-200 hover:text-black dark:text-gray-300 dark:hover:bg-gray-800 dark:hover:text-white':
-            !open
-        },
-      )}
-    >
-      <EllipsisHorizontalCircleIcon className="size-6" />
-      <span>More</span> 
-    </MenuButton>
+           <MenuButton
+            className={cn(
+              'w-full cursor-pointer rounded-md px-2 py-1 text-left text-sm font-bold tracking-wide md:px-3 flex items-center space-x-2',
+              {
+                'bg-gray-200 text-black dark:bg-gray-800 dark:text-white': open,
+                'text-gray-700 hover:bg-gray-200 hover:text-black dark:text-gray-300 dark:hover:bg-gray-800 dark:hover:text-white':
+                  !open
+              },
+            )}
+          >
+            <EllipsisHorizontalCircleIcon className="size-6 ml-[-3px]" /> {/* Adjusted margin-left */}
+            <span>More</span>
+          </MenuButton>
           <MenuTransition>
             <MenuItems
               className="absolute mt-2 rounded-xl border bg-white shadow-sm focus:outline-none dark:border-gray-700 dark:bg-gray-900"
