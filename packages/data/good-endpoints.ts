ca76db5e2fa98344792b5de28a680e27832607a1
Ezra <ezra.huang2024@ecssen.ca>
2024-06-11 14:03:46 -0600

    update api endpoint

--- packages/data/good-endpoints.ts
+++ packages/data/good-endpoints.ts
@@ -1,7 +1,7 @@
 enum GoodEndpoint {
   Mainnet = 'https://api.bcharity.net',
   Staging = 'https://api-staging.bcharity.net',
-  Testnet = 'http://api.bcharity.net'
+  Testnet = 'https://api-testnet.bcharity.net'
 }
 
 export default GoodEndpoint;
