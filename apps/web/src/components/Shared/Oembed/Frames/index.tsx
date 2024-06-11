b42566d8451308a09e43985478f1bd6b979dd462
bigint <69431456+bigint@users.noreply.github.com>
2024-06-11 11:09:26 +0530

    fix: rm identityToken passing on frames post

--- apps/web/src/components/Shared/Oembed/Frames/index.tsx
+++ apps/web/src/components/Shared/Oembed/Frames/index.tsx
@@ -11,7 +11,6 @@ import cn from '@hey/ui/cn';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
-import { hydrateAuthTokens } from 'src/store/persisted/useAuthStore';
 import { useProfileStore } from 'src/store/persisted/useProfileStore';
 
 interface FrameProps {
@@ -20,7 +19,6 @@ interface FrameProps {
 }
 
 const Frame: FC<FrameProps> = ({ frame, publicationId }) => {
-  const { identityToken } = hydrateAuthTokens();
   const { currentProfile } = useProfileStore();
   const [frameData, setFrameData] = useState<IFrame | null>(null);
   const [isLoading, setIsLoading] = useState(false);
@@ -49,7 +47,6 @@ const Frame: FC<FrameProps> = ({ frame, publicationId }) => {
         `${HEY_API_URL}/frames/post`,
         {
           buttonIndex: index + 1,
-          identityToken,
           postUrl: buttons[index].target || buttons[index].postUrl || postUrl,
           pubId: publicationId
         },
