93942e5553ebe0b214f603ed8035a2d894fc6574
bigint <69431456+bigint@users.noreply.github.com>
2024-06-13 09:36:45 +0530

    chore: add sw update

--- apps/web/src/components/Common/Providers/ServiceWorkerProvider.tsx
+++ apps/web/src/components/Common/Providers/ServiceWorkerProvider.tsx
@@ -5,6 +5,14 @@ import { useEffect } from 'react';
 const ServiceWorkerProvider: FC = () => {
   useEffect(() => {
     if ('serviceWorker' in navigator) {
+      // Trigger an update
+      navigator.serviceWorker.getRegistrations().then(function (registrations) {
+        for (let registration of registrations) {
+          registration.update();
+        }
+      });
+
+      // Register the service worker
       (navigator.serviceWorker as ServiceWorkerContainer)
         .register('/sw.js', { scope: '/' })
         .catch(console.error);
