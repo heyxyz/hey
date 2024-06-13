5212e3b8c85c68ece81b76c80fea4a0d63523896
bigint <69431456+bigint@users.noreply.github.com>
2024-06-13 15:07:47 +0530

    feat: add invoice app

---
+++ apps/invoice/src/app/layout.tsx
@@ -0,0 +1,13 @@
+import type { ReactNode } from 'react';
+
+import { heyFont } from 'src/fonts';
+
+import '../globals.css';
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html className={heyFont.className} lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
