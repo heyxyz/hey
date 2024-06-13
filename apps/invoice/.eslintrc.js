f70ca2c93fe1277509ea06fd8299afdc719c63d0
bigint <69431456+bigint@users.noreply.github.com>
2024-06-13 15:07:47 +0530

    feat: add invoice app

---
+++ apps/invoice/.eslintrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  extends: [require.resolve('@hey/config/eslint/react.js')],
+  ignorePatterns: ['sw.js']
+};
---
+++ apps/invoice/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { APP_NAME } from '@hey/data/constants';
+
+export default function Home() {
+  return (
+    <main>
+      <div>{APP_NAME} Invoice</div>
+    </main>
+  );
+}
