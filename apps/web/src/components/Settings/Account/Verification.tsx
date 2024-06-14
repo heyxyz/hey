ef970ed49814c68cf9a68251702e13cd87d486a8
bigint <69431456+bigint@users.noreply.github.com>
2024-06-14 23:30:28 +0530

    chore: remove crisp support

--- apps/web/src/components/Settings/Account/Verification.tsx
+++ apps/web/src/components/Settings/Account/Verification.tsx
@@ -1,10 +1,12 @@
 import type { FC } from 'react';
 
+import { Leafwatch } from '@helpers/leafwatch';
 import { MinusCircleIcon } from '@heroicons/react/24/outline';
 import { CheckBadgeIcon, CheckCircleIcon } from '@heroicons/react/24/solid';
+import { SETTINGS } from '@hey/data/tracking';
 import getNumberOfDaysFromDate from '@hey/helpers/datetime/getNumberOfDaysFromDate';
 import { Button, Card } from '@hey/ui';
-import { Crisp } from 'crisp-sdk-web';
+import toast from 'react-hot-toast';
 import { useProfileStore } from 'src/store/persisted/useProfileStore';
 import { hydrateVerifiedMembers } from 'src/store/persisted/useVerifiedMembersStore';
 
@@ -75,25 +77,9 @@ const Verification: FC = () => {
             className="!mt-4"
             disabled={!hasAllRequirements}
             onClick={() => {
-              Crisp.chat.show();
-              Crisp.message.show('picker', {
-                choices: [
-                  { label: 'Creator', selected: false, value: 'creator' },
-                  {
-                    label: 'Journalist',
-                    selected: false,
-                    value: 'journalist'
-                  },
-                  {
-                    label: 'Public Figure',
-                    selected: false,
-                    value: 'public-figure'
-                  },
-                  { label: 'Other', selected: false, value: 'other' }
-                ],
-                id: 'verification-request',
-                text: 'Hi, why do you think you should be verified? And tell us more about yourself.'
-              });
+              // TODO: Migrate to Hey API
+              Leafwatch.track(SETTINGS.ACCOUNT.REQUEST_VERIFICATION);
+              toast.success('Verification request sent to staff!');
             }}
           >
             Request for profile verification
