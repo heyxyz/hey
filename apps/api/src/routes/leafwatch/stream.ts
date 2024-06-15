c45449b7e1920f2c7d36b28916accca79a928790
bigint <69431456+bigint@users.noreply.github.com>
2024-06-15 20:40:17 +0530

    feat: rm auth to stream

--- apps/api/src/routes/leafwatch/stream.ts
+++ apps/api/src/routes/leafwatch/stream.ts
@@ -1,17 +1,10 @@
 import type { Handler } from 'express';
 
 import catchedError from 'src/helpers/catchedError';
-import validateIsStaff from 'src/helpers/middlewares/validateIsStaff';
-import { notAllowed } from 'src/helpers/responses';
 
 let sseClients = require('../../helpers/leafwatch/sseClients');
 
-export const get: Handler = async (req, res) => {
-  const validateIsStaffStatus = await validateIsStaff(req);
-  if (validateIsStaffStatus !== 200) {
-    return notAllowed(res, validateIsStaffStatus);
-  }
-
+export const get: Handler = (req, res) => {
   try {
     res.setHeader('Content-Type', 'text/event-stream');
     res.setHeader('Cache-Control', 'no-cache');
