ce91cdcb3922421b85e115ec25d9fb5b5988c8b8
Ezra <ezra.huang2024@ecssen.ca>
2024-06-17 16:09:24 -0600

    update publication stats api endpoint to no longer use lensPg

--- apps/api/src/routes/lens/internal/stats/publication.ts
+++ apps/api/src/routes/lens/internal/stats/publication.ts
@@ -1,11 +1,164 @@
 import type { Handler } from 'express';
 
-import { APP_NAME } from '@good/data/constants';
+import { APP_NAME, IS_MAINNET } from '@good/data/constants';
 import logger from '@good/helpers/logger';
-import lensPg from 'src/db/lensPg';
 import catchedError from 'src/helpers/catchedError';
 import validateIsStaff from 'src/helpers/middlewares/validateIsStaff';
 import { notAllowed } from 'src/helpers/responses';
+import axios from 'axios';
+import LensEndpoint from '@good/data/lens-endpoints';
+import { GOOD_USER_AGENT } from 'src/helpers/constants';
+
+const FETCH_STATS_QUERY = `
+query Publications($request: PublicationsRequest!, $countOpenActionsRequest2: PublicationStatsCountOpenActionArgs) {
+  publications(request: $request) {
+    items {
+      ... on Post {
+        __typename
+        stats {
+          reactions
+          bookmarks
+          collects: countOpenActions(request: $countOpenActionsRequest2)
+          actions: countOpenActions
+        }
+      }
+      ... on Comment {
+        __typename
+        stats {
+          reactions
+          bookmarks
+          collects: countOpenActions(request: $countOpenActionsRequest2)
+          actions: countOpenActions
+        }
+      }
+      ... on Mirror {
+        __typename
+      }
+      ... on Quote {
+        __typename
+        stats {
+          bookmarks
+          reactions
+          collects: countOpenActions(request: $countOpenActionsRequest2)
+          actions: countOpenActions
+        }
+      }
+    }
+    pageInfo {
+      next
+      prev
+    }
+  }
+}
+`;
+
+async function fetchStatsBatch(cursor: string|undefined) {
+  const getStatsQuery = {
+    query: FETCH_STATS_QUERY,
+    variables: {
+      request: {
+        where: {
+          metadata: {
+            publishedOn: APP_NAME
+          }
+        },
+        cursor: cursor ?? null
+      },
+      countOpenActionsRequest2: {
+        anyOf: [
+          {
+            category: "COLLECT"
+          }
+        ]
+      },
+    }
+  };
+
+  const { data } = await axios.post(
+    IS_MAINNET ? LensEndpoint.Mainnet : LensEndpoint.Testnet,
+    getStatsQuery,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        'User-agent': GOOD_USER_AGENT
+      }
+    }
+  );
+
+  const stats = {
+    publications: 0,
+    mirrors: 0,
+    comments: 0,
+    quotes: 0,
+    reactions: 0,
+    collects: 0,
+    actions: 0,
+    bookmarks: 0
+  };
+
+  data.data.publications.items.forEach((p: any) => {
+    if (p.__typename === 'Post') stats.publications++;
+    else if (p.__typename === 'Mirror') stats.mirrors++;
+    else if (p.__typename === 'Comment') stats.comments++;
+    else if (p.__typename === 'Quote') stats.quotes++;
+
+    stats.reactions += p.stats.reactions;
+    stats.bookmarks += p.stats.bookmarks;
+    stats.collects += p.stats.collects;
+    stats.actions += p.stats.actions;
+  });
+
+  const nextCursor: string | undefined = data.data.publications.pageInfo?.next;
+  const prevCursor: string | undefined = data.data.publications.pageInfo?.prev;
+  return { stats, nextCursor, prevCursor };
+}
+
+async function getAggregateStats() {
+  const totals: Record<string, number> = {
+    publications: 0,
+    mirrors: 0,
+    comments: 0,
+    quotes: 0,
+    reactions: 0,
+    collects: 0,
+    actions: 0,
+    bookmarks: 0
+  };
+
+  let { stats, nextCursor, prevCursor } = await fetchStatsBatch(undefined);
+  for (const [k, v] of Object.entries(stats)) {
+    console.log(totals[k], v)
+    totals[k] += v;
+  }
+  while (nextCursor) {
+    try {
+      let res = await fetchStatsBatch(nextCursor);
+      nextCursor = res.nextCursor;
+      for (const [k, v] of Object.entries(res.stats)) {
+        console.log(totals[k], v)
+        totals[k] += v;
+      }
+    } catch (e) {
+      console.log(e);
+      break;
+    }
+  }
+
+  while (prevCursor) {
+    try {
+      let res = await fetchStatsBatch(prevCursor);
+      prevCursor = res.prevCursor;
+      for (const [k, v] of Object.entries(res.stats)) {
+        totals[k] += v;
+      }
+    } catch (e) {
+      console.log(e);
+      break;
+    }
+  }
+
+  return totals;
+}
 
 // TODO: add tests
 export const get: Handler = async (req, res) => {
@@ -15,25 +168,11 @@ export const get: Handler = async (req, res) => {
   }
 
   try {
-    const result = await lensPg.query(`
-      SELECT 
-        app,
-        COUNT(publication_id) AS publications,
-        SUM(total_amount_of_mirrors) AS mirrors,
-        SUM(total_amount_of_comments) AS comments,
-        SUM(total_amount_of_quotes) AS quotes,
-        SUM(total_reactions) AS reactions,
-        SUM(total_amount_of_collects) AS collects,
-        SUM(total_amount_of_acted) AS actions,
-        SUM(total_bookmarks) AS bookmarks
-      FROM app_stats.publication
-      WHERE app = '${APP_NAME.toLowerCase()}'
-      GROUP BY app;
-    `);
+    const stats = await getAggregateStats()
 
     logger.info('Lens: Fetched publication stats');
 
-    return res.status(200).json({ result: result[0], success: true });
+    return res.status(200).json({ result: stats, success: true });
   } catch (error) {
     catchedError(res, error);
   }
