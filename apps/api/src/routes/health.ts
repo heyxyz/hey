9bd5a91b39c75a36e0ce70af7fbb30c595926b43
bigint <69431456+bigint@users.noreply.github.com>
2024-06-20 18:38:31 +0530

    feat: add RAILWAY_DEPLOYMENT_ID

--- apps/api/src/routes/health.ts
+++ apps/api/src/routes/health.ts
@@ -64,6 +64,9 @@ export const get: Handler = async (_, res) => {
 
     // Format response times in milliseconds and return
     return res.status(200).json({
+      meta: {
+        deployment: process.env.RAILWAY_DEPLOYMENT_ID || 'unknown'
+      },
       ping: 'pong',
       responseTimes: {
         clickhouse: `${Number(clickhouseTime / BigInt(1000000))}ms`,
