18f892583c4e3be75ed6a8b263b2a04ee28b418c
bigint <bigint@hey.xyz>
2024-06-22 11:08:59 +0530

    chore: update date

--- apps/api/src/script/save-invoices.ts
+++ apps/api/src/script/save-invoices.ts
@@ -8,30 +8,44 @@ import lensPg from '../db/lensPg';
 const baseUrl = 'https://invoice.hey.xyz/signup/';
 const rate = 83;
 const outputDir = path.join(__dirname, 'pdfs');
-const startDate = new Date('2024-02-20');
-const endDate = new Date('2024-02-29');
+const startDate = '2024-04-01 00:16:54';
+const endDate = '2024-04-30 23:30:09';
+const batchSize = 5;
 
 const getProfiles = async () => {
   const profiles = await lensPg.query(
     `
-      SELECT profile_id FROM app.onboarding_profile
+      SELECT profile_id, block_timestamp FROM app.onboarding_profile
       WHERE onboarded_by_address = $1
       AND block_timestamp BETWEEN $2 AND $3;
     `,
     ['0x4b8845ACb8148dE64D1D99Cf27A3890a91F55E53', startDate, endDate]
   );
 
-  return profiles.map((profile) => profile.profile_id);
+  return profiles;
 };
 
-const generatePdf = async (url: string, outputPath: string): Promise<void> => {
+const generatePdf = async (
+  url: string,
+  outputPath: string,
+  blockTimestamp: Date
+) => {
   try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: 'networkidle0' });
     await page.pdf({ format: 'A4', path: outputPath });
     await browser.close();
-    logger.info(`PDF generated at ${outputPath}`);
+
+    const year = blockTimestamp.getFullYear();
+    const month = String(blockTimestamp.getMonth() + 1).padStart(2, '0');
+    const day = String(blockTimestamp.getDate()).padStart(2, '0');
+    const hours = String(blockTimestamp.getHours()).padStart(2, '0');
+    const minutes = String(blockTimestamp.getMinutes()).padStart(2, '0');
+    const seconds = String(blockTimestamp.getSeconds()).padStart(2, '0');
+    const formattedTimestamp = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+    logger.info(`PDF generated at ${outputPath} at ${formattedTimestamp}`);
   } catch (error) {
     logger.error('Error generating PDF:', error as Error);
   }
@@ -42,12 +56,20 @@ if (!fs.existsSync(outputDir)) {
 }
 
 const generatePdfsForUsers = async () => {
-  const ids = await getProfiles();
+  const profiles = await getProfiles();
 
-  for (const id of ids) {
-    const url = `${baseUrl}${id}?rate=${rate}`;
-    const outputPath = path.join(outputDir, `Invoice #${parseInt(id)}.pdf`);
-    await generatePdf(url, outputPath);
+  for (let i = 0; i < profiles.length; i += batchSize) {
+    const batch = profiles.slice(i, i + batchSize);
+    const pdfPromises = batch.map(async (profile) => {
+      const url = `${baseUrl}${profile.profile_id}?rate=${rate}`;
+      const outputPath = path.join(
+        outputDir,
+        `Invoice #${parseInt(profile.profile_id)}.pdf`
+      );
+      await generatePdf(url, outputPath, profile.block_timestamp);
+    });
+
+    await Promise.all(pdfPromises);
   }
 };
 
