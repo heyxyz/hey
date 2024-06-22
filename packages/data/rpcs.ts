d08d5b5ed82635743b951938a54c8f7220ddcc0a
bigint <bigint@hey.xyz>
2024-06-22 21:21:54 +0530

    feat: use alchemy rpc

--- packages/data/rpcs.ts
+++ packages/data/rpcs.ts
@@ -1,7 +1,5 @@
-export const POYGON_WRITE_RPC =
-  'https://polygon-mainnet.g.alchemy.com/v2/N_HuqeYE3mr_enxw-BGFI2rOm1U7bhGy';
-
 export const POLYGON_RPCS = [
+  'https://polygon-mainnet.g.alchemy.com/v2/N_HuqeYE3mr_enxw-BGFI2rOm1U7bhGy',
   'https://lb.nodies.app/v1/c4af832850924699b25128e185bde36e',
   'https://polygon-pokt.nodies.app',
   'https://rpc.ankr.com/polygon',
