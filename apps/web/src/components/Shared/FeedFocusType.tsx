d9c50417db8e7096d4a81c6f21ec19c8c0a8d371
bigint <bigint@hey.xyz>
2024-06-25 20:10:38 +0530

    refactor: FeedFocusType

--- apps/web/src/components/Shared/FeedFocusType.tsx
+++ apps/web/src/components/Shared/FeedFocusType.tsx
@@ -6,9 +6,33 @@ import { PublicationMetadataMainFocusType } from '@hey/lens';
 import cn from '@hey/ui/cn';
 
 interface FeedLinkProps {
+  focus?: PublicationMetadataMainFocusType;
   name: string;
+  setFocus: Dispatch<
+    SetStateAction<PublicationMetadataMainFocusType | undefined>
+  >;
   type?: PublicationMetadataMainFocusType;
 }
+
+const FeedLink: FC<FeedLinkProps> = ({ focus, name, setFocus, type }) => (
+  <button
+    aria-label={name}
+    className={cn(
+      focus === type ? 'bg-black text-white' : 'bg-gray-100 dark:bg-gray-800',
+      'rounded-full px-3 py-1.5 text-xs sm:px-4',
+      'border border-gray-300 dark:border-gray-500'
+    )}
+    onClick={() => {
+      setFocus(type);
+      Leafwatch.track(EXPLORE.SWITCH_EXPLORE_FEED_FOCUS, {
+        explore_feed_focus: (type || 'all_posts').toLowerCase()
+      });
+    }}
+    type="button"
+  >
+    {name}
+  </button>
+);
 
 interface FeedFocusTypeProps {
   focus?: PublicationMetadataMainFocusType;
@@ -17,36 +41,34 @@ interface FeedFocusTypeProps {
   >;
 }
 
-const FeedFocusType: FC<FeedFocusTypeProps> = ({ focus, setFocus }) => {
-  const FeedLink: FC<FeedLinkProps> = ({ name, type }) => (
-    <button
-      aria-label={name}
-      className={cn(
-        focus === type ? 'bg-black text-white' : 'bg-gray-100 dark:bg-gray-800',
-        'rounded-full px-3 py-1.5 text-xs sm:px-4',
-        'border border-gray-300 dark:border-gray-500'
-      )}
-      onClick={() => {
-        setFocus(type as PublicationMetadataMainFocusType);
-        Leafwatch.track(EXPLORE.SWITCH_EXPLORE_FEED_FOCUS, {
-          explore_feed_focus: (type || 'all_posts').toLowerCase()
-        });
-      }}
-      type="button"
-    >
-      {name}
-    </button>
-  );
-
-  return (
-    <div className="mx-5 my-5 flex flex-wrap gap-3 sm:mx-0">
-      <FeedLink name="All posts" />
-      <FeedLink name="Text" type={PublicationMetadataMainFocusType.TextOnly} />
-      <FeedLink name="Video" type={PublicationMetadataMainFocusType.Video} />
-      <FeedLink name="Audio" type={PublicationMetadataMainFocusType.Audio} />
-      <FeedLink name="Images" type={PublicationMetadataMainFocusType.Image} />
-    </div>
-  );
-};
+const FeedFocusType: FC<FeedFocusTypeProps> = ({ focus, setFocus }) => (
+  <div className="mx-5 my-5 flex flex-wrap gap-3 sm:mx-0">
+    <FeedLink focus={focus} name="All posts" setFocus={setFocus} />
+    <FeedLink
+      focus={focus}
+      name="Text"
+      setFocus={setFocus}
+      type={PublicationMetadataMainFocusType.TextOnly}
+    />
+    <FeedLink
+      focus={focus}
+      name="Video"
+      setFocus={setFocus}
+      type={PublicationMetadataMainFocusType.Video}
+    />
+    <FeedLink
+      focus={focus}
+      name="Audio"
+      setFocus={setFocus}
+      type={PublicationMetadataMainFocusType.Audio}
+    />
+    <FeedLink
+      focus={focus}
+      name="Images"
+      setFocus={setFocus}
+      type={PublicationMetadataMainFocusType.Image}
+    />
+  </div>
+);
 
 export default FeedFocusType;
