423c3cf64ecef4b299c517bd7687dac670a4676a
bigint <bigint@hey.xyz>
2024-06-25 19:42:47 +0530

    refactor: PageMetatags

--- apps/web/src/components/Shared/PageMetatags.tsx
+++ apps/web/src/components/Shared/PageMetatags.tsx
@@ -8,54 +8,40 @@ import MetaTags from '../Common/MetaTags';
 const PageMetatags: FC = () => {
   const { pathname } = useRouter();
 
-  const getOg = () => {
-    switch (pathname) {
-      case '/signup': {
-        return {
-          description: `Signup on ${APP_NAME} to create, share and discover content.`,
-          title: `Signup • ${APP_NAME}`
-        };
-      }
-      case '/explore': {
-        return {
-          description: `Explore top commented, collected and latest publications in the ${APP_NAME}.`,
-          title: `Explore • ${APP_NAME}`
-        };
-      }
-      case '/privacy': {
-        return {
-          description: `Privacy Policy of ${APP_NAME}.`,
-          title: `Privacy Policy • ${APP_NAME}`
-        };
-      }
-      case '/rules': {
-        return {
-          description: `Rules of ${APP_NAME}.`,
-          title: `Rules • ${APP_NAME}`
-        };
-      }
-      case '/terms': {
-        return {
-          description: `Terms & Conditions of ${APP_NAME}.`,
-          title: `Terms & Conditions • ${APP_NAME}`
-        };
-      }
-      case '/thanks': {
-        return {
-          description: `Thanks to all the people and projects that have supported ${APP_NAME}.`,
-          title: `Thanks • ${APP_NAME}`
-        };
-      }
-      default: {
-        return {
-          description: DESCRIPTION,
-          title: APP_NAME
-        };
-      }
+  const ogData: Record<string, { description: string; title: string }> = {
+    '/explore': {
+      description: `Explore top commented, collected and latest publications in the ${APP_NAME}.`,
+      title: `Explore • ${APP_NAME}`
+    },
+    '/privacy': {
+      description: `Privacy Policy of ${APP_NAME}.`,
+      title: `Privacy Policy • ${APP_NAME}`
+    },
+    '/rules': {
+      description: `Rules of ${APP_NAME}.`,
+      title: `Rules • ${APP_NAME}`
+    },
+    '/signup': {
+      description: `Signup on ${APP_NAME} to create, share and discover content.`,
+      title: `Signup • ${APP_NAME}`
+    },
+    '/terms': {
+      description: `Terms & Conditions of ${APP_NAME}.`,
+      title: `Terms & Conditions • ${APP_NAME}`
+    },
+    '/thanks': {
+      description: `Thanks to all the people and projects that have supported ${APP_NAME}.`,
+      title: `Thanks • ${APP_NAME}`
+    },
+    default: {
+      description: DESCRIPTION,
+      title: APP_NAME
     }
   };
 
-  return <MetaTags description={getOg().description} title={getOg().title} />;
+  const { description, title } = ogData[pathname] || ogData.default;
+
+  return <MetaTags description={description} title={title} />;
 };
 
 export default PageMetatags;
