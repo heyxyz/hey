acc6b274e8d404dd37e5d81c15a15c5cd10c8f06
bigint <bigint@hey.xyz>
2024-06-25 23:54:43 +0530

    feat: add new pro eoa to eoa

--- apps/web/src/hooks/useUploadAttachments.tsx
+++ apps/web/src/hooks/useUploadAttachments.tsx
@@ -6,9 +6,11 @@ import { useCallback } from 'react';
 import { toast } from 'react-hot-toast';
 import { usePublicationAttachmentStore } from 'src/store/non-persisted/publication/usePublicationAttachmentStore';
 import { usePublicationVideoStore } from 'src/store/non-persisted/publication/usePublicationVideoStore';
+import { useProStore } from 'src/store/non-persisted/useProStore';
 import { v4 as uuid } from 'uuid';
 
 const useUploadAttachments = () => {
+  const { isPro } = useProStore();
   const { setUploadedPercentage } = usePublicationVideoStore();
   const {
     addAttachments,
@@ -17,31 +19,41 @@ const useUploadAttachments = () => {
     updateAttachments
   } = usePublicationAttachmentStore((state) => state);
 
-  const validateFileSize = (file: any) => {
-    const isImage = file.type.includes('image');
-    const isVideo = file.type.includes('video');
-    const isAudio = file.type.includes('audio');
-
-    if (isImage && file.size > 50000000) {
-      toast.error('Image size should be less than 50MB');
-      return false;
-    }
-
-    if (isVideo && file.size > 500000000) {
-      toast.error('Video size should be less than 500MB');
-      return false;
-    }
-
-    if (isAudio && file.size > 200000000) {
-      toast.error('Audio size should be less than 200MB');
-      return false;
-    }
-
-    return true;
-  };
-
   const handleUploadAttachments = useCallback(
     async (attachments: any): Promise<NewAttachment[]> => {
+      const validateFileSize = (file: any) => {
+        const isImage = file.type.includes('image');
+        const isVideo = file.type.includes('video');
+        const isAudio = file.type.includes('audio');
+
+        const IMAGE_UPLOAD_LIMIT = 5000000;
+        const VIDEO_UPLOAD_LIMIT = isPro ? 200000000 : 50000000;
+        const AUDIO_UPLOAD_LIMIT = isPro ? 60000000 : 20000000;
+
+        if (isImage && file.size > IMAGE_UPLOAD_LIMIT) {
+          toast.error(
+            `Image size should be less than ${IMAGE_UPLOAD_LIMIT / 1000000}MB`
+          );
+          return false;
+        }
+
+        if (isVideo && file.size > VIDEO_UPLOAD_LIMIT) {
+          toast.error(
+            `Video size should be less than ${VIDEO_UPLOAD_LIMIT / 1000000}MB`
+          );
+          return false;
+        }
+
+        if (isAudio && file.size > AUDIO_UPLOAD_LIMIT) {
+          toast.error(
+            `Audio size should be less than ${AUDIO_UPLOAD_LIMIT / 1000000}MB`
+          );
+          return false;
+        }
+
+        return true;
+      };
+
       setIsUploading(true);
 
       const files = Array.from(attachments);
@@ -117,7 +129,8 @@ const useUploadAttachments = () => {
       removeAttachments,
       updateAttachments,
       setIsUploading,
-      setUploadedPercentage
+      setUploadedPercentage,
+      isPro
     ]
   );
 
