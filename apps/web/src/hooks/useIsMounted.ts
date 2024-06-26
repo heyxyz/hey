c8f06efbc5f75f23554054d809fe972322e6b092
bigint <bigint@hey.xyz>
2024-06-26 20:59:31 +0530

    feat: add useIsMounted

---
+++ apps/web/src/hooks/useIsMounted.ts
@@ -0,0 +1,17 @@
+import { useCallback, useEffect, useRef } from 'react';
+
+const useIsMounted = () => {
+  const isMounted = useRef(false);
+
+  useEffect(() => {
+    isMounted.current = true;
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  return useCallback(() => isMounted.current, []);
+};
+
+export default useIsMounted;
