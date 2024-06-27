77d7842fbd3f49ec94d92fad4a48c221cb01aaef
bigint <bigint@hey.xyz>
2024-06-27 18:14:39 +0530

    feat: add purgeWebCache

--- apps/api/src/helpers/catchedError.ts
+++ apps/api/src/helpers/catchedError.ts
@@ -6,9 +6,11 @@ import logger from '@hey/helpers/logger';
 const catchedError = (res: Response, error: any) => {
   logger.error(error);
 
-  return res
-    .status(500)
-    .json({ error: Errors.SomethingWentWrong, message: error, success: false });
+  return res.status(500).json({
+    error: Errors.SomethingWentWrong,
+    message: error.message,
+    success: false
+  });
 };
 
 export default catchedError;
