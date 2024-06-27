d0229c7dcb6bb0988442b34269af0f9c3f62e352
bigint <bigint@hey.xyz>
2024-06-27 14:04:01 +0530

    feat: update invoice

--- apps/api/src/data/invoice-rates.ts
+++ apps/api/src/data/invoice-rates.ts
@@ -2,7 +2,8 @@ const invoiceRates = [
   { date: new Date('2024-02-01'), rate: 664 },
   { date: new Date('2024-03-01'), rate: 664 },
   { date: new Date('2024-04-01'), rate: 648 },
-  { date: new Date('2024-05-01'), rate: 640 }
+  { date: new Date('2024-05-01'), rate: 640 },
+  { date: new Date('2024-06-01'), rate: 50 }
 ];
 
 export default invoiceRates;
