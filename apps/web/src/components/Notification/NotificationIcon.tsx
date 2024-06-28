1a39a1ca509474ed035d279d2271e9009daa526d
bigint <bigint@hey.xyz>
2024-06-28 23:42:54 +0530

    cleanup: remove browser push notifications

--- apps/web/src/components/Notification/NotificationIcon.tsx
+++ apps/web/src/components/Notification/NotificationIcon.tsx
@@ -1,33 +1,19 @@
 import type { FC } from 'react';
 
 import { BellIcon } from '@heroicons/react/24/outline';
+import { Tooltip } from '@hey/ui';
 import Link from 'next/link';
-import { useNotificationStore } from 'src/store/persisted/useNotificationStore';
 
 const NotificationIcon: FC = () => {
-  const {
-    lastOpenedNotificationId,
-    latestNotificationId,
-    setLastOpenedNotificationId
-  } = useNotificationStore();
-
-  const handleClick = () => {
-    if (latestNotificationId) {
-      setLastOpenedNotificationId(latestNotificationId);
-    }
-  };
-
   return (
-    <Link
-      className="hidden items-start justify-center rounded-md px-2 py-1 hover:bg-gray-300/20 md:flex"
-      href="/notifications"
-      onClick={handleClick}
-    >
-      <BellIcon className="size-5 sm:size-6" />
-      {lastOpenedNotificationId !== latestNotificationId && (
-        <span className="size-2 rounded-full bg-red-500" />
-      )}
-    </Link>
+    <Tooltip content="Notifications" placement="bottom">
+      <Link
+        className="hidden rounded-md px-2 py-1 hover:bg-gray-300/20 md:flex"
+        href="/notifications"
+      >
+        <BellIcon className="size-5 sm:size-6" />
+      </Link>
+    </Tooltip>
   );
 };
 
