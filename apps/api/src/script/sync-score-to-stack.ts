1f9f450e3cdc785c1eaf148e51e0f85ee611f331
bigint <bigint@hey.xyz>
2024-06-28 13:01:43 +0530

    chore: add VERCEL_DEPLOYMENT_ID

--- apps/api/src/script/sync-score-to-stack.ts
+++ apps/api/src/script/sync-score-to-stack.ts
@@ -4,9 +4,9 @@ import axios from 'axios';
 import lensPg from '../db/lensPg';
 
 const baseUrl = 'https://api.hey.xyz/score/get';
-const batchSize = 7;
+const batchSize = 6;
 const limit = 300000;
-const offset = 154000;
+const offset = 154454;
 
 const getProfiles = async () => {
   const profiles = await lensPg.query(
