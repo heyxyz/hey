82dcc8c271bbe565643501f58663c1cf200ca384
bigint <bigint@hey.xyz>
2024-06-28 23:04:09 +0530

    cleanup: remove legacy collects support

--- packages/helpers/allowedOpenActionModules.ts
+++ packages/helpers/allowedOpenActionModules.ts
@@ -2,14 +2,7 @@ import { OpenActionModuleType } from '@hey/lens';
 
 const allowedOpenActionModules = [
   OpenActionModuleType.SimpleCollectOpenActionModule,
-  OpenActionModuleType.MultirecipientFeeCollectOpenActionModule,
-  OpenActionModuleType.LegacySimpleCollectModule,
-  OpenActionModuleType.LegacyMultirecipientFeeCollectModule,
-  OpenActionModuleType.LegacyFreeCollectModule,
-  OpenActionModuleType.LegacyFeeCollectModule,
-  OpenActionModuleType.LegacyLimitedFeeCollectModule,
-  OpenActionModuleType.LegacyTimedFeeCollectModule,
-  OpenActionModuleType.LegacyLimitedTimedFeeCollectModule
+  OpenActionModuleType.MultirecipientFeeCollectOpenActionModule
 ];
 
 export default allowedOpenActionModules;
