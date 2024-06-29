0f340e69ec81e89e04ce44968ec3118ea1085679
bigint <bigint@hey.xyz>
2024-06-29 23:36:09 +0530

    refactor: ChooseFile

--- apps/web/src/components/Shared/ChooseFile.tsx
+++ apps/web/src/components/Shared/ChooseFile.tsx
@@ -1,20 +1,32 @@
 import type { ChangeEventHandler, FC } from 'react';
 
+import { PaperClipIcon } from '@heroicons/react/24/outline';
+
 interface ChooseFileProps {
   onChange: ChangeEventHandler<HTMLInputElement>;
 }
 
 const ChooseFile: FC<ChooseFileProps> = ({ onChange }) => {
   return (
-    <input
-      accept=".png, .jpg, .jpeg, .gif"
-      className="cursor-pointer rounded-xl border border-gray-300 bg-white pr-1 text-sm text-gray-700 shadow-sm outline-offset-4 dark:border-gray-700 dark:bg-gray-800 dark:text-white"
-      onChange={onChange}
-      onClick={(event) => {
-        (event.target as HTMLInputElement).value = '';
-      }}
-      type="file"
-    />
+    <div className="flex items-center space-x-2">
+      <label
+        className="flex cursor-pointer items-center space-x-2 rounded-xl border border-gray-300 bg-white px-3 py-1 text-gray-700 shadow-sm outline-offset-4 dark:border-gray-700 dark:bg-gray-800 dark:text-white"
+        htmlFor="file-upload"
+      >
+        <PaperClipIcon className="size-4" />
+        <span>Choose File</span>
+      </label>
+      <input
+        accept=".png, .jpg, .jpeg, .gif"
+        className="hidden"
+        id="file-upload"
+        onChange={onChange}
+        onClick={(event) => {
+          (event.target as HTMLInputElement).value = '';
+        }}
+        type="file"
+      />
+    </div>
   );
 };
 
