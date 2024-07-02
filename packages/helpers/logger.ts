c87dc1e516bde2d6b333d463f36d07880342f243
bigint <bigint@hey.xyz>
2024-07-02 19:57:57 +0530

    chore: rm datadog implementation

--- packages/helpers/logger.ts
+++ packages/helpers/logger.ts
@@ -1,4 +1,3 @@
-import DatadogWinston from 'datadog-winston';
 import dotenv from 'dotenv';
 import winston from 'winston';
 
@@ -20,13 +19,6 @@ class Logger {
       level: 'info',
       transports: [new winston.transports.Console()]
     });
-
-    const datadogApiKey = process.env.DATADOG_API_KEY;
-    if (datadogApiKey) {
-      this.logger.add(
-        new DatadogWinston({ apiKey: datadogApiKey, intakeRegion: 'eu' })
-      );
-    }
   }
 
   error(message: string, error?: Error) {
