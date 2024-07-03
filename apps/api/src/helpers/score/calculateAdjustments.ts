9c141146d3d709ff7df5e258c146fac9bcc03e1d
bigint <bigint@hey.xyz>
2024-07-03 21:54:54 +0530

    chore: remove return

--- apps/api/src/helpers/score/calculateAdjustments.ts
+++ apps/api/src/helpers/score/calculateAdjustments.ts
@@ -6,8 +6,6 @@ import getAndStoreHeyZorbNftScore from './getAndStoreHeyZorbNftScore';
 const calculateAdjustments = (id: string, address: Address) => {
   getAndStoreHeyZorbNftScore(id, address);
   getAndStoreHeyNativeMintNftScore(id, address);
-
-  return true;
 };
 
 export default calculateAdjustments;
