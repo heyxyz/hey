a96574350ba14cc1960b83189e3c23d793f01e81
bigint <bigint@hey.xyz>
2024-07-04 14:38:20 +0530

    feat: add club feed

---
+++ apps/web/src/components/Club/Feed.tsx
@@ -0,0 +1,123 @@
+import type { AnyPublication, PublicationsRequest } from '@hey/lens';
+import type { FC } from 'react';
+import type { StateSnapshot, VirtuosoHandle } from 'react-virtuoso';
+
+import SinglePublication from '@components/Publication/SinglePublication';
+import PublicationsShimmer from '@components/Shared/Shimmer/PublicationsShimmer';
+import { RectangleStackIcon } from '@heroicons/react/24/outline';
+import { LimitType, usePublicationsQuery } from '@hey/lens';
+import { Card, EmptyState, ErrorMessage } from '@hey/ui';
+import { useEffect, useRef } from 'react';
+import { Virtuoso } from 'react-virtuoso';
+import { useImpressionsStore } from 'src/store/non-persisted/useImpressionsStore';
+import { useTipsStore } from 'src/store/non-persisted/useTipsStore';
+
+let virtuosoState: any = { ranges: [], screenTop: 0 };
+
+interface FeedProps {
+  handle: string;
+}
+
+const Feed: FC<FeedProps> = ({ handle }) => {
+  const { fetchAndStoreViews } = useImpressionsStore();
+  const { fetchAndStoreTips } = useTipsStore();
+  const virtuoso = useRef<VirtuosoHandle>(null);
+
+  useEffect(() => {
+    virtuosoState = { ranges: [], screenTop: 0 };
+  }, [handle]);
+
+  const request: PublicationsRequest = {
+    limit: LimitType.TwentyFive,
+    where: { metadata: { tags: { oneOf: [`orbcommunities${handle}`] } } }
+  };
+
+  const { data, error, fetchMore, loading } = usePublicationsQuery({
+    onCompleted: async ({ publications }) => {
+      const ids =
+        publications?.items?.map((p) => {
+          return p.__typename === 'Mirror' ? p.mirrorOn?.id : p.id;
+        }) || [];
+      await fetchAndStoreViews(ids);
+      await fetchAndStoreTips(ids);
+    },
+    skip: !handle,
+    variables: { request }
+  });
+
+  const publications = data?.publications?.items;
+  const pageInfo = data?.publications?.pageInfo;
+  const hasMore = pageInfo?.next;
+
+  const onScrolling = (scrolling: boolean) => {
+    if (!scrolling) {
+      virtuoso?.current?.getState((state: StateSnapshot) => {
+        virtuosoState = { ...state };
+      });
+    }
+  };
+
+  const onEndReached = async () => {
+    if (hasMore) {
+      const { data } = await fetchMore({
+        variables: { request: { ...request, cursor: pageInfo?.next } }
+      });
+      const ids =
+        data?.publications?.items?.map((p) => {
+          return p.__typename === 'Mirror' ? p.mirrorOn?.id : p.id;
+        }) || [];
+      await fetchAndStoreViews(ids);
+      await fetchAndStoreTips(ids);
+    }
+  };
+
+  if (loading) {
+    return <PublicationsShimmer />;
+  }
+
+  if (publications?.length === 0) {
+    return (
+      <EmptyState
+        icon={<RectangleStackIcon className="size-8" />}
+        message={
+          <div>
+            <span className="mr-1 font-bold">/{handle}</span>
+            <span>has no posts yet!</span>
+          </div>
+        }
+      />
+    );
+  }
+
+  if (error) {
+    return <ErrorMessage error={error} title="Failed to load club feed" />;
+  }
+
+  return (
+    <Card>
+      <Virtuoso
+        className="virtual-divider-list-window"
+        computeItemKey={(index, publication) => `${publication.id}-${index}`}
+        data={publications}
+        endReached={onEndReached}
+        isScrolling={onScrolling}
+        itemContent={(index, publication) => (
+          <SinglePublication
+            isFirst={index === 0}
+            isLast={index === (publications?.length || 0) - 1}
+            publication={publication as AnyPublication}
+          />
+        )}
+        ref={virtuoso}
+        restoreStateFrom={
+          virtuosoState.ranges.length === 0
+            ? virtuosoState?.current?.getState((state: StateSnapshot) => state)
+            : virtuosoState
+        }
+        useWindowScroll
+      />
+    </Card>
+  );
+};
+
+export default Feed;
