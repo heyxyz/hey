953b669b5d889386721d0f1fecc49fc93de9bf91
bigint <bigint@hey.xyz>
2024-07-05 12:21:49 +0530

    fix: htmlFor

--- apps/web/src/components/Shared/ChooseFile.tsx
+++ apps/web/src/components/Shared/ChooseFile.tsx
@@ -1,17 +1,20 @@
 import type { ChangeEventHandler, FC } from 'react';
 
 import { PaperClipIcon } from '@heroicons/react/24/outline';
+import { useId } from 'react';
 
 interface ChooseFileProps {
   onChange: ChangeEventHandler<HTMLInputElement>;
 }
 
 const ChooseFile: FC<ChooseFileProps> = ({ onChange }) => {
+  const id = useId();
+
   return (
     <div className="flex items-center space-x-2">
       <label
         className="flex cursor-pointer items-center space-x-2 rounded-xl border border-gray-300 bg-white px-3 py-1 text-gray-700 shadow-sm outline-offset-4 dark:border-gray-700 dark:bg-gray-800 dark:text-white"
-        htmlFor="file-upload"
+        htmlFor={id}
       >
         <PaperClipIcon className="size-4" />
         <span>Choose File</span>
@@ -19,7 +22,7 @@ const ChooseFile: FC<ChooseFileProps> = ({ onChange }) => {
       <input
         accept=".png, .jpg, .jpeg, .gif"
         className="hidden"
-        id="file-upload"
+        id={id}
         onChange={onChange}
         onClick={(event) => {
           (event.target as HTMLInputElement).value = '';
