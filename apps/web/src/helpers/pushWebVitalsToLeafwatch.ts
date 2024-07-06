cc810dc29b5293eaff72b27cc42b9cdbb59180c1
bigint <bigint@hey.xyz>
2024-07-06 20:42:25 +0530

    feat: add web vitals

---
+++ apps/web/src/helpers/pushWebVitalsToLeafwatch.ts
@@ -0,0 +1,22 @@
+import { HEY_API_URL } from '@hey/data/constants';
+import axios from 'axios';
+
+const pushWebVitalsToLeafwatch = async (metric: {
+  id: string;
+  name: string;
+  value: number;
+}): Promise<boolean> => {
+  try {
+    const response = await axios.post(`${HEY_API_URL}/leafwatch/vitals`, {
+      delta: metric.value,
+      id: metric.id,
+      name: metric.name
+    });
+
+    return response.data.success;
+  } catch {
+    return false;
+  }
+};
+
+export default pushWebVitalsToLeafwatch;
