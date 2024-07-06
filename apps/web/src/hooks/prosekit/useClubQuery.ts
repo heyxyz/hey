97704be9ddf58e9ec09b74a3d5464fda049da053
bigint <bigint@hey.xyz>
2024-07-06 10:48:10 +0530

    feat: add club picker

--- apps/web/src/hooks/prosekit/useClubQuery.ts
+++ apps/web/src/hooks/prosekit/useClubQuery.ts
@@ -4,10 +4,12 @@ import getAuthApiHeaders from '@helpers/getAuthApiHeaders';
 import { HEY_API_URL } from '@hey/data/constants';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { useProfileStore } from 'src/store/persisted/useProfileStore';
 
 const SUGGESTION_LIST_LENGTH_LIMIT = 5;
 
 export type ClubProfile = {
+  displayHandle: string;
   handle: string;
   id: string;
   name: string;
@@ -15,6 +17,7 @@ export type ClubProfile = {
 };
 
 const useClubQuery = (query: string): ClubProfile[] => {
+  const { currentProfile } = useProfileStore();
   const [results, setResults] = useState<ClubProfile[]>([]);
 
   useEffect(() => {
@@ -26,7 +29,7 @@ const useClubQuery = (query: string): ClubProfile[] => {
     axios
       .post(
         `${HEY_API_URL}/clubs/get`,
-        { club_handle: query, limit: 10 },
+        { limit: 10, profile_id: currentProfile?.id, query_id: query },
         { headers: getAuthApiHeaders() }
       )
       .then(({ data }) => {
@@ -35,6 +38,7 @@ const useClubQuery = (query: string): ClubProfile[] => {
         const clubs = clubSearchResult?.items as Club[];
         const clubsResults = (clubs ?? []).map(
           (club): ClubProfile => ({
+            displayHandle: `/${club.handle}`,
             handle: club.handle,
             id: club.id,
             name: club.name,
