2883a96a8633181449618d33ada2cb12ed0de617
bigint <bigint@hey.xyz>
2024-07-06 20:55:42 +0530

    chore: revert vitals

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -2,14 +2,10 @@ import type { AppProps } from 'next/app';
 
 import Providers from '@components/Common/Providers';
 import { heyFont } from '@helpers/fonts';
-import pushWebVitalsToLeafwatch from '@helpers/pushWebVitalsToLeafwatch';
-import { useReportWebVitals } from 'next/web-vitals';
 
 import '../styles.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  useReportWebVitals((metric) => pushWebVitalsToLeafwatch(metric));
-
   return (
     <Providers>
       <style global jsx>{`
