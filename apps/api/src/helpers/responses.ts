f8a700a81da6dee6271ae8addf29782325357b57
bigint <bigint@hey.xyz>
2024-07-09 21:01:23 +0530

    chore: remove notAllowed

--- apps/api/src/helpers/responses.ts
+++ apps/api/src/helpers/responses.ts
@@ -2,25 +2,6 @@ import type { Response } from 'express';
 
 import { Errors } from '@hey/data';
 
-const getMessageByStatus = (status: number) => {
-  switch (status) {
-    case 400:
-      return Errors.InvalidBody;
-    case 401:
-      return Errors.Unauthorized;
-    case 500:
-      return Errors.SomethingWentWrong;
-    default:
-      return Errors.NotAllowed;
-  }
-};
-
-export const notAllowed = (response: Response, status?: number) => {
-  return response
-    .status(status || 401)
-    .json({ error: getMessageByStatus(status || 401), success: false });
-};
-
 export const invalidBody = (response: Response) => {
   return response
     .status(400)
