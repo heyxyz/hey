3cac8c367b8f8648a1284a006de2a5fa7bcc47b7
bigint <bigint@hey.xyz>
2024-07-09 22:39:50 +0530

    feat: add ratelimiter middleware

---
+++ apps/api/src/helpers/middlewares/rateLimiter.ts
@@ -0,0 +1,38 @@
+import type { NextFunction, Request, Response } from 'express';
+
+import getIp from '@hey/helpers/getIp';
+import sha256 from '@hey/helpers/sha256';
+import rateLimit from 'express-rate-limit';
+
+import catchedError from '../catchedError';
+
+const hashedIp = (req: Request): string => sha256(getIp(req)).slice(0, 25);
+
+const createRateLimiter = (window: number, max: number) => {
+  return rateLimit({
+    handler: (req, res) =>
+      catchedError(res, new Error(`Too many requests - ${req.path}`), 429),
+    keyGenerator: (req) => {
+      console.log('Yoginth', hashedIp(req), getIp(req));
+      return hashedIp(req);
+    },
+    legacyHeaders: false,
+    max, // Maximum number of requests allowed within the window
+    standardHeaders: true,
+    windowMs: window * 60 * 1000 // Time window in milliseconds
+  });
+};
+
+export const rateLimiter = ({
+  requests,
+  within
+}: {
+  requests: number;
+  within: number;
+}) => {
+  const rateLimiter = createRateLimiter(requests, within);
+
+  return (req: Request, res: Response, next: NextFunction) => {
+    rateLimiter(req, res, next);
+  };
+};
