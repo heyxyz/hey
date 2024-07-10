f16cf8409983c3cb85e575f11b02bf8e9d123f67
bigint <bigint@hey.xyz>
2024-07-10 22:13:59 +0530

    feat: add staff picks cache

--- apps/api/src/routes/staff-picks/index.ts
+++ apps/api/src/routes/staff-picks/index.ts
@@ -7,28 +7,45 @@ import {
   CACHE_AGE_30_MINS,
   STAFF_PICK_FEATURE_ID
 } from 'src/helpers/constants';
+import {
+  generateMediumExpiry,
+  getRedis,
+  setRedis
+} from 'src/helpers/redisClient';
+
+const getRandomPicks = (data: any[]) => {
+  const random = data.sort(() => Math.random() - Math.random());
+  return random.slice(0, 150);
+};
 
 export const get: Handler = async (_, res) => {
   try {
+    const cacheKey = `staff-picks`;
+    const cachedData = await getRedis(cacheKey);
+
+    if (cachedData) {
+      logger.info('(cached) Staff picks fetched');
+      return res.status(200).json({
+        result: getRandomPicks(JSON.parse(cachedData)),
+        success: true
+      });
+    }
+
     const data = await heyPg.query(
       `
-        SELECT "profileId"
-        FROM "ProfileFeature"
-        WHERE enabled = TRUE
-        AND "featureId" = $1;
+        SELECT "profileId" FROM "ProfileFeature"
+        WHERE enabled = TRUE AND "featureId" = $1;
       `,
       [STAFF_PICK_FEATURE_ID]
     );
 
-    const random = data.sort(() => Math.random() - Math.random());
-    const picks = random.slice(0, 150);
-
+    await setRedis(cacheKey, data, generateMediumExpiry());
     logger.info('Staff picks fetched');
 
     return res
       .status(200)
       .setHeader('Cache-Control', CACHE_AGE_30_MINS)
-      .json({ result: picks, success: true });
+      .json({ result: getRandomPicks(data), success: true });
   } catch (error) {
     return catchedError(res, error);
   }
