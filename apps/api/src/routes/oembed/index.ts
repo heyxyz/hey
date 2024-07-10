7740f9611716d2b2485642626d342e251cf5a378
bigint <bigint@hey.xyz>
2024-07-10 22:08:21 +0530

    feat: cache oembed

--- apps/api/src/routes/oembed/index.ts
+++ apps/api/src/routes/oembed/index.ts
@@ -1,9 +1,13 @@
 import type { Handler } from 'express';
 
+import hoursToSeconds from '@hey/helpers/hoursToSeconds';
 import logger from '@hey/helpers/logger';
+import randomNumber from '@hey/helpers/randomNumber';
+import sha256 from '@hey/helpers/sha256';
 import catchedError from 'src/helpers/catchedError';
 import { CACHE_AGE_1_DAY } from 'src/helpers/constants';
 import getMetadata from 'src/helpers/oembed/getMetadata';
+import { getRedis, setRedis } from 'src/helpers/redisClient';
 import { noBody } from 'src/helpers/responses';
 
 export const get: Handler = async (req, res) => {
@@ -14,6 +18,16 @@ export const get: Handler = async (req, res) => {
   }
 
   try {
+    const cacheKey = `oembed:${sha256(url as string).slice(0, 10)}`;
+    const cachedData = await getRedis(cacheKey);
+
+    if (cachedData) {
+      logger.info(`(cached) Oembed generated for ${url}`);
+      return res
+        .status(200)
+        .json({ result: JSON.parse(cachedData), success: true });
+    }
+
     const oembed = await getMetadata(url as string);
 
     if (!oembed) {
@@ -21,6 +35,14 @@ export const get: Handler = async (req, res) => {
     }
 
     const skipCache = oembed.frame !== null;
+
+    if (!skipCache) {
+      await setRedis(
+        cacheKey,
+        oembed,
+        randomNumber(hoursToSeconds(72), hoursToSeconds(96))
+      );
+    }
 
     logger.info(`Oembed generated for ${url}`);
 
