232de9edb57d01deaabdd2e0974a1222ce129eef
bigint <bigint@hey.xyz>
2024-07-10 21:56:52 +0530

    chore: add logger

--- apps/api/src/routes/polls/get.ts
+++ apps/api/src/routes/polls/get.ts
@@ -19,6 +19,7 @@ export const get: Handler = async (req, res) => {
     const cachedData = await getRedis(cacheKey);
 
     if (cachedData) {
+      logger.info(`(cached) Poll fetched`);
       return res
         .status(200)
         .json({ result: JSON.parse(cachedData), success: true });
