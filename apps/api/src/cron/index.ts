ebbf354b80a621c83a4e91a9e4956112d0924c7d
bigint <bigint@hey.xyz>
2024-07-10 14:42:18 +0530

    chore: migrate cached profile score to use redis

--- apps/api/src/cron/index.ts
+++ apps/api/src/cron/index.ts
@@ -5,7 +5,6 @@ import cron from 'node-cron';
 import cleanClickhouse from './cleanClickhouse';
 import cleanDraftPublications from './cleanDraftPublications';
 import cleanEmailTokens from './cleanEmailTokens';
-import cleanEmptyScores from './cleanEmptyScores';
 import cleanPreferences from './cleanPreferences';
 import heartbeat from './heartbeat';
 
@@ -36,11 +35,6 @@ const main = () => {
 
   cron.schedule('*/5 * * * *', async () => {
     await cleanPreferences();
-    return;
-  });
-
-  cron.schedule('*/5 * * * *', async () => {
-    await cleanEmptyScores();
     return;
   });
 };
---
+++ apps/api/src/db/migrations/20240710091131_remove_cached_score/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the `CachedProfileScore` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "CachedProfileScore";
