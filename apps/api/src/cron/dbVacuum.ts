22e4c561851cf1ea4858a27ad5655713a6cd2fbe
bigint <bigint@hey.xyz>
2024-07-10 15:02:46 +0530

    feat: add dbVacuum

---
+++ apps/api/src/cron/dbVacuum.ts
@@ -0,0 +1,14 @@
+import logger from '@hey/helpers/logger';
+
+import heyPg from '../../src/db/heyPg';
+
+const dbVacuum = async () => {
+  if (process.env.NEXT_PUBLIC_LENS_NETWORK !== 'mainnet') {
+    return;
+  }
+
+  await heyPg.query('VACUUM FULL');
+  logger.info('Cron: dbVacuum - Vacuumed database');
+};
+
+export default dbVacuum;
