268c21209201e44b75ac1833f647708793293965
bigint <bigint@hey.xyz>
2024-07-11 11:01:15 +0530

    fix: server reboot

--- apps/api/src/helpers/score/getAndStoreHeyZorbNftScore.ts
+++ apps/api/src/helpers/score/getAndStoreHeyZorbNftScore.ts
@@ -41,7 +41,10 @@ const getAndStoreHeyZorbNftScore = async (id: string, address: Address) => {
 
     return true;
   } catch {
-    throw new Error('Failed to get and store Hey Zorb NFT score');
+    logger.error(
+      `[Worker] Failed to get and store Hey Zorb NFT score for ${id} - ${address}`
+    );
+    return false;
   }
 };
 
