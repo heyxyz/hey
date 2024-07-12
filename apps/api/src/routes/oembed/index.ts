627ae5ad714ac8b2d125861ad8d52cd8c920e203
bigint <bigint@hey.xyz>
2024-07-12 23:02:52 +0530

    chore: rm cache for avatar

--- apps/api/src/routes/oembed/index.ts
+++ apps/api/src/routes/oembed/index.ts
@@ -1,8 +1,6 @@
 import type { Handler } from 'express';
 
-import daysToSeconds from '@hey/helpers/daysToSeconds';
 import logger from '@hey/helpers/logger';
-import randomNumber from '@hey/helpers/randomNumber';
 import sha256 from '@hey/helpers/sha256';
 import catchedError from 'src/helpers/catchedError';
 import { CACHE_AGE_1_DAY } from 'src/helpers/constants';
@@ -37,11 +35,7 @@ export const get: Handler = async (req, res) => {
     const skipCache = oembed.frame !== null;
 
     if (!skipCache) {
-      await setRedis(
-        cacheKey,
-        oembed,
-        randomNumber(daysToSeconds(4), daysToSeconds(8))
-      );
+      await setRedis(cacheKey, oembed);
     }
 
     logger.info(`Oembed generated for ${url}`);
