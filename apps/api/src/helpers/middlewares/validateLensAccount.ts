3e0819a54db29c01403e09b79647d4f465e7353d
bigint <bigint@hey.xyz>
2024-07-13 15:47:33 +0530

    fix: validateLensAccount to use lensPg

--- apps/api/src/helpers/middlewares/validateLensAccount.ts
+++ apps/api/src/helpers/middlewares/validateLensAccount.ts
@@ -1,11 +1,11 @@
 import type { NextFunction, Request, Response } from 'express';
 
 import { Errors } from '@hey/data';
-import LensEndpoint from '@hey/data/lens-endpoints';
-import axios from 'axios';
+import parseJwt from '@hey/helpers/parseJwt';
+import lensPg from 'src/db/lensPg';
 
 import catchedError from '../catchedError';
-import { HEY_USER_AGENT } from '../constants';
+import { getRedis, setRedis } from '../redisClient';
 
 /**
  * Middleware to validate Lens account
@@ -18,33 +18,32 @@ const validateLensAccount = async (
   next: NextFunction
 ) => {
   const identityToken = req.headers['x-identity-token'] as string;
-  const network = req.headers['x-lens-network'] as string;
-  const allowedNetworks = ['mainnet', 'testnet'];
-
-  if (!identityToken || !network || !allowedNetworks.includes(network)) {
+  if (!identityToken) {
     return catchedError(res, new Error(Errors.Unauthorized), 401);
   }
 
-  const isMainnet = network === 'mainnet';
   try {
-    const { data } = await axios.post(
-      isMainnet ? LensEndpoint.Mainnet : LensEndpoint.Testnet,
-      {
-        query: `
-          query Verify {
-            verify(request: { identityToken: "${identityToken}" })
-          }
-        `
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'User-agent': HEY_USER_AGENT
-        }
-      }
+    const payload = parseJwt(identityToken);
+    const cacheKey = `auth:${payload.id}`;
+    const cachedData = await getRedis(cacheKey);
+
+    if (cachedData) {
+      return next();
+    }
+
+    const data = await lensPg.query(
+      `
+        SELECT EXISTS (
+          SELECT 1 FROM authentication.record
+          WHERE profile_id = $1
+          AND authorization_id = $2
+        ) AS result;
+      `,
+      [payload.id, payload.authorizationId]
     );
 
-    if (data.data.verify) {
+    if (data[0]?.result) {
+      await setRedis(cacheKey, true);
       return next();
     }
 
