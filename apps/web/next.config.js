ef9a43f775bb916a7cd7ac1cc97e09ffa2e988fc
bigint <bigint@hey.xyz>
2024-07-14 22:27:21 +0530

    fix: web docker deploy

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -85,12 +85,12 @@ module.exports = {
   rewrites() {
     return [
       {
-        destination: `${process.env.NEXT_PUBLIC_OG_URL}/u/:match*`,
+        destination: 'https://og.hey.xyz/u/:match*',
         has: [{ key: 'user-agent', type: 'header', value: allowedBots }],
         source: '/u/:match*'
       },
       {
-        destination: `${process.env.NEXT_PUBLIC_OG_URL}/posts/:match*`,
+        destination: 'https://og.hey.xyz/posts/:match*',
         has: [{ key: 'user-agent', type: 'header', value: allowedBots }],
         source: '/posts/:match*'
       }
