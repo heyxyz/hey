1574f9f8f50c0bbf61845b3b62c887eb9e52dc05
bigint <bigint@hey.xyz>
2024-07-15 13:43:31 +0530

    chore: testing ch by removing getPublicationsViews

--- packages/helpers/getPublicationsViews.ts
+++ packages/helpers/getPublicationsViews.ts
@@ -1,23 +1,17 @@
-import { HEY_API_URL, IS_MAINNET } from '@hey/data/constants';
-import axios from 'axios';
+import { IS_MAINNET } from '@hey/data/constants';
 
 /**
  * Get the number of views of a publication
  * @param ids The ids of the publications
  * @returns The number of views of the publication
  */
-const getPublicationsViews = async (ids: string[]) => {
+const getPublicationsViews = (ids: string[]) => {
   if (!IS_MAINNET) {
     return [];
   }
 
   try {
-    const response = await axios.post(
-      `${HEY_API_URL}/stats/publication/views`,
-      { ids }
-    );
-
-    return response.data?.views;
+    return [];
   } catch {
     return [];
   }
