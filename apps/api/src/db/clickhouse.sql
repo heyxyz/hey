f9b5a516966edb92c4a37459bc83a07479bd0958
bigint <bigint@hey.xyz>
2024-07-15 12:44:13 +0530

    chore: optimize clickhouse table

--- apps/api/src/db/clickhouse.sql
+++ apps/api/src/db/clickhouse.sql
@@ -2,26 +2,16 @@
 CREATE TABLE events (
   id UUID DEFAULT generateUUIDv4(),
   actor Nullable(String),
-  wallet Nullable(String),
   fingerprint Nullable(String),
   name String,
   properties Nullable(String),
   referrer Nullable(String),
   url Nullable(String),
-  platform String,
   browser Nullable(String),
-  browser_version Nullable(String),
   os Nullable(String),
   ip Nullable(String),
   city Nullable(String),
-  region Nullable(String),
   country LowCardinality(String),
-  version LowCardinality(String),
-  utm_source Nullable(String),
-  utm_medium Nullable(String),
-  utm_campaign Nullable(String),
-  utm_term Nullable(String),
-  utm_content Nullable(String),
   created DateTime DEFAULT now()
 ) ENGINE = MergeTree
 ORDER BY created;
@@ -33,7 +23,6 @@ CREATE TABLE impressions (
   publication_id String,
   ip Nullable(String),
   city Nullable(String),
-  region Nullable(String),
   country LowCardinality(String),
   viewed_at DateTime64(3, 'UTC') DEFAULT now64(3)
 ) ENGINE = MergeTree()
