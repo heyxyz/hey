220dcb77a5c4c7a4c0aefa4b75ff2420b288d90f
bigint <bigint@hey.xyz>
2024-07-15 21:07:51 +0530

    chore: remove vercel

--- apps/og/next.config.js
+++ apps/og/next.config.js
@@ -1,21 +1,12 @@
-const {
-  NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA,
-  VERCEL_DEPLOYMENT_ID,
-  VERCEL_GITHUB_COMMIT_SHA
-} = process.env;
-const COMMIT_SHA =
-  NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA || VERCEL_GITHUB_COMMIT_SHA || 'local';
-const DEPLOYMENT_ID = VERCEL_DEPLOYMENT_ID || 'unknown';
+const { CF_PAGES_COMMIT_SHA } = process.env;
+const COMMIT_SHA = CF_PAGES_COMMIT_SHA || 'local';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   headers() {
     return [
       {
-        headers: [
-          { key: 'X-Hey-Version', value: COMMIT_SHA },
-          { key: 'X-Hey-Deployment', value: DEPLOYMENT_ID }
-        ],
+        headers: [{ key: 'X-Hey-Version', value: COMMIT_SHA }],
         source: '/(.*)'
       }
     ];
