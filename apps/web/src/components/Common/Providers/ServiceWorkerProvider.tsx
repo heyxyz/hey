75ce57e9b357444cff63894fc0a4a14cd47bd752
bigint <bigint@hey.xyz>
2024-07-17 11:15:22 +0530

    chore: refactor new clickhouse impressions

--- apps/web/src/components/Common/Providers/ServiceWorkerProvider.tsx
+++ apps/web/src/components/Common/Providers/ServiceWorkerProvider.tsx
@@ -5,40 +5,9 @@ import { useEffect } from 'react';
 const ServiceWorkerProvider: FC = () => {
   useEffect(() => {
     if ('serviceWorker' in navigator) {
-      const registerServiceWorker = async () => {
-        try {
-          const registration = await navigator.serviceWorker.register(
-            '/sw.js',
-            { scope: '/' }
-          );
-          console.log('ServiceWorker registered successfully!');
-
-          registration.onupdatefound = () => {
-            const installingWorker = registration.installing;
-            if (installingWorker) {
-              installingWorker.onstatechange = () => {
-                if (
-                  installingWorker.state === 'installed' &&
-                  navigator.serviceWorker.controller
-                ) {
-                  console.log(
-                    'New ServiceWorker available. Refresh the page to update.'
-                  );
-                }
-              };
-            }
-          };
-
-          await navigator.serviceWorker.ready;
-          if (navigator.serviceWorker.controller) {
-            console.log('Service Worker is ready to receive messages.');
-          }
-        } catch (error) {
-          console.error('ServiceWorker registration failed:', error);
-        }
-      };
-
-      registerServiceWorker();
+      (navigator.serviceWorker as ServiceWorkerContainer)
+        .register('/sw.js', { scope: '/' })
+        .catch(console.error);
     }
   }, []);
 
