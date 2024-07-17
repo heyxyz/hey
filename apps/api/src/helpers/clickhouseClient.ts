12e65b3e4b88e0dc7f65fa57f1a247007d92c922
bigint <bigint@hey.xyz>
2024-07-17 17:58:48 +0530

    chore: add keep_alive

--- apps/api/src/helpers/clickhouseClient.ts
+++ apps/api/src/helpers/clickhouseClient.ts
@@ -4,10 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config({ override: true });
 
 const clickhouseClient = createClient({
-  compression: {
-    request: true,
-    response: true
-  },
+  compression: { request: true, response: true },
+  keep_alive: { enabled: true },
   password: process.env.CLICKHOUSE_PASSWORD,
   url: 'http://clickhouse.railway.internal:8123',
   username: 'clickhouse'
