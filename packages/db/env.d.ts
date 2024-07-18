865dc20d80f9b29be1fd77cb9719b222226c64d6
bigint <bigint@hey.xyz>
2024-07-18 22:35:14 +0530

    fix: env

--- packages/db/env.d.ts
+++ packages/db/env.d.ts
@@ -1,6 +1,8 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     CLICKHOUSE_PASSWORD: string;
+    DATABASE_URL: string;
+    LENS_DATABASE_PASSWORD: string;
     REDIS_URL: string;
   }
 }
