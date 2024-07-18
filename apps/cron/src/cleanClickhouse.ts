6866027cc46354236bec1842fa4741d39f16e206
bigint <bigint@hey.xyz>
2024-07-18 23:44:28 +0530

    fix: envs for cron

--- apps/cron/src/cleanClickhouse.ts
+++ apps/cron/src/cleanClickhouse.ts
@@ -2,10 +2,6 @@ import clickhouseClient from '@hey/db/clickhouseClient';
 import logger from '@hey/helpers/logger';
 
 const cleanClickhouse = async () => {
-  if (process.env.NEXT_PUBLIC_LENS_NETWORK !== 'mainnet') {
-    return;
-  }
-
   try {
     await clickhouseClient.command({
       query: "ALTER TABLE events DELETE WHERE url NOT LIKE '%hey.xyz%';"
--- apps/cron/src/cleanDraftPublications.ts
+++ apps/cron/src/cleanDraftPublications.ts
@@ -1,10 +1,6 @@
 import logger from '@hey/helpers/logger';
 
 const cleanDraftPublications = () => {
-  if (process.env.NEXT_PUBLIC_LENS_NETWORK !== 'mainnet') {
-    return;
-  }
-
   try {
     // const { count } = await prisma.draftPublication.deleteMany({
     //   where: {
