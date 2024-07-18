590700510842a74995db597ab63f739e198a2f69
bigint <bigint@hey.xyz>
2024-07-18 22:15:33 +0530

    refactor: migrate clickhouseClient to db

--- apps/api/src/routes/meta.ts
+++ apps/api/src/routes/meta.ts
@@ -1,10 +1,10 @@
 import type { Request, Response } from 'express';
 
+import clickhouseClient from '@hey/db/clickhouseClient';
 import axios from 'axios';
 import heyPg from 'src/db/heyPg';
 import lensPg from 'src/db/lensPg';
 import catchedError from 'src/helpers/catchedError';
-import { clickhouseClient } from 'src/helpers/clickhouseClient';
 import { SCORE_WORKER_URL } from 'src/helpers/constants';
 import { rateLimiter } from 'src/helpers/middlewares/rateLimiter';
 import { getRedis } from 'src/helpers/redisClient';
