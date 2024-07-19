851e07443850437fe90d9fdf8bebd7f22560c84d
bigint <bigint@hey.xyz>
2024-07-19 12:43:17 +0530

    fix: cron to use heypg

--- apps/cron/src/cleanEmailTokens.ts
+++ apps/cron/src/cleanEmailTokens.ts
@@ -1,14 +1,15 @@
+import heyPg from '@hey/db/heyPg';
 import logger from '@hey/helpers/logger';
 
 const cleanEmailTokens = () => {
   try {
-    // const { count } = await prisma.email.updateMany({
-    //   data: { tokenExpiresAt: null, verificationToken: null, verified: false },
-    //   where: { tokenExpiresAt: { lt: new Date() } }
-    // });
+    heyPg.query(
+      'UPDATE "Email" SET "tokenExpiresAt" = NULL, "verificationToken" = NULL, "verified" = false WHERE "tokenExpiresAt" < $1',
+      [new Date()]
+    );
 
     logger.info(
-      `[Cron] cleanEmailTokens - Cleaned up ${1} email tokens that are expired`
+      `[Cron] cleanEmailTokens - Cleaned up email tokens that are expired`
     );
   } catch (error) {
     logger.error(
