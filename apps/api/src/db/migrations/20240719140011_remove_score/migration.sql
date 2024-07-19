23291a8ebbc08f5ced6aa176769e33ffe3d9678e
bigint <bigint@hey.xyz>
2024-07-19 19:30:28 +0530

    fix: db migrations

---
+++ apps/api/src/db/migrations/20240719140011_remove_score/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the `AdjustedProfileScore` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "AdjustedProfileScore";
