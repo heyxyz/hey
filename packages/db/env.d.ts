0e49ce57cdd2981c25ec279dfd037fb1a3c6692d
bigint <bigint@hey.xyz>
2024-07-20 21:10:28 +0530

    feat: use CLICKHOUSE_URL

--- packages/db/env.d.ts
+++ packages/db/env.d.ts
@@ -1,6 +1,7 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     CLICKHOUSE_PASSWORD: string;
+    CLICKHOUSE_URL: string;
     DATABASE_URL: string;
     LENS_DATABASE_PASSWORD: string;
     REDIS_URL: string;
