4fb0160de387638a4378402960bd335533b9e1da
bigint <bigint@hey.xyz>
2024-08-02 19:57:53 +0530

    feat: add profiles design

--- apps/web/src/components/Design/index.tsx
+++ apps/web/src/components/Design/index.tsx
@@ -4,12 +4,14 @@ import { GridLayout } from '@hey/ui';
 import { GridItemTwelve } from '@hey/ui/src/GridLayout';
 
 import Buttons from './Buttons';
+import Profiles from './Profiles';
 
 const Design: NextPage = () => {
   return (
     <GridLayout>
       <GridItemTwelve className="space-y-5">
         <Buttons />
+        <Profiles />
       </GridItemTwelve>
     </GridLayout>
   );
