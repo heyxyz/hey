8835ffde7db6aa5e8009dda59e0e369f1a4490fb
bigint <bigint@hey.xyz>
2024-08-04 17:10:23 +0530

    feat: mvp big collect button

--- apps/web/src/components/Publication/OpenAction/index.tsx
+++ apps/web/src/components/Publication/OpenAction/index.tsx
@@ -1,77 +1,44 @@
 import type { MirrorablePublication } from '@hey/lens';
 import type { FC } from 'react';
 
-import { Leafwatch } from '@helpers/leafwatch';
 import { RectangleStackIcon } from '@heroicons/react/24/outline';
-import { PUBLICATION } from '@hey/data/tracking';
-import allowedOpenActionModules from '@hey/helpers/allowedOpenActionModules';
 import humanize from '@hey/helpers/humanize';
 import nFormatter from '@hey/helpers/nFormatter';
-import { Modal, Tooltip } from '@hey/ui';
+import { Tooltip } from '@hey/ui';
 import { motion } from 'framer-motion';
 import plur from 'plur';
-import { useState } from 'react';
-
-import CollectModule from './CollectModule';
 
 interface OpenActionProps {
   publication: MirrorablePublication;
 }
 
 const OpenAction: FC<OpenActionProps> = ({ publication }) => {
-  const [showOpenActionModal, setShowOpenActionModal] = useState(false);
-  const openActions = publication.openActionModules.filter((module) =>
-    allowedOpenActionModules.includes(module.type)
-  );
-
   const { countOpenActions } = publication.stats;
 
   return (
-    <>
-      <div className="ld-text-gray-500 flex items-center space-x-1">
-        <motion.button
-          aria-label="Action"
-          className="rounded-full p-1.5 outline-offset-2 hover:bg-gray-300/20"
-          onClick={() => {
-            setShowOpenActionModal(true);
-            Leafwatch.track(PUBLICATION.COLLECT_MODULE.OPEN_COLLECT, {
-              publication_id: publication.id
-            });
-          }}
-          whileTap={{ scale: 0.9 }}
-        >
-          <Tooltip
-            content={`${humanize(countOpenActions)} ${plur(
-              'Action',
-              countOpenActions
-            )}`}
-            placement="top"
-            withDelay
-          >
-            <RectangleStackIcon className="w-[15px] sm:w-[18px]" />
-          </Tooltip>
-        </motion.button>
-        {countOpenActions > 0 ? (
-          <span className="text-[11px] sm:text-xs">
-            {nFormatter(countOpenActions)}
-          </span>
-        ) : null}
-      </div>
-      <Modal
-        icon={<RectangleStackIcon className="size-5" />}
-        onClose={() => setShowOpenActionModal(false)}
-        show={showOpenActionModal}
-        title="Open Actions"
+    <div className="ld-text-gray-500 flex items-center space-x-1">
+      <motion.button
+        aria-label="Action"
+        className="cursor-default rounded-full p-1.5 outline-offset-2 hover:bg-gray-300/20"
+        whileTap={{ scale: 0.9 }}
       >
-        {openActions?.map((action) => (
-          <CollectModule
-            key={action.type}
-            openAction={action}
-            publication={publication}
-          />
-        ))}
-      </Modal>
-    </>
+        <Tooltip
+          content={`${humanize(countOpenActions)} ${plur(
+            'Action',
+            countOpenActions
+          )}`}
+          placement="top"
+          withDelay
+        >
+          <RectangleStackIcon className="w-[15px] sm:w-[18px]" />
+        </Tooltip>
+      </motion.button>
+      {countOpenActions > 0 ? (
+        <span className="text-[11px] sm:text-xs">
+          {nFormatter(countOpenActions)}
+        </span>
+      ) : null}
+    </div>
   );
 };
 
