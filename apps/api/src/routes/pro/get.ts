25994e8c2f24e5557768e1956922c89f10c26411
bigint <bigint@hey.xyz>
2024-08-04 11:58:51 +0530

    feat: add more rl

--- apps/api/src/routes/pro/get.ts
+++ apps/api/src/routes/pro/get.ts
@@ -1,52 +1,56 @@
-import type { Handler } from 'express';
+import type { Request, Response } from 'express';
 
 import { generateMediumExpiry, getRedis, setRedis } from '@hey/db/redisClient';
 import logger from '@hey/helpers/logger';
 import catchedError from 'src/helpers/catchedError';
 import { CACHE_AGE_30_MINS } from 'src/helpers/constants';
+import { rateLimiter } from 'src/helpers/middlewares/rateLimiter';
 import prisma from 'src/helpers/prisma';
 import { noBody } from 'src/helpers/responses';
 
-export const get: Handler = async (req, res) => {
-  const { id } = req.query;
+export const get = [
+  rateLimiter({ requests: 250, within: 1 }),
+  async (req: Request, res: Response) => {
+    const { id } = req.query;
 
-  if (!id) {
-    return noBody(res);
-  }
+    if (!id) {
+      return noBody(res);
+    }
 
-  try {
-    const cacheKey = `pro:${id}`;
-    const cachedData = await getRedis(cacheKey);
+    try {
+      const cacheKey = `pro:${id}`;
+      const cachedData = await getRedis(cacheKey);
 
-    if (cachedData) {
-      logger.info(`(cached) Fetched pro status for ${id}`);
+      if (cachedData) {
+        logger.info(`(cached) Fetched pro status for ${id}`);
+
+        return res
+          .status(200)
+          .setHeader('Cache-Control', CACHE_AGE_30_MINS)
+          .json({ result: JSON.parse(cachedData), success: true });
+      }
+
+      const pro = await prisma.pro.findUnique({
+        where: { id: id as string }
+      });
+
+      if (!pro) {
+        return res
+          .status(200)
+          .json({ result: { expiresAt: null, isPro: false } });
+      }
+
+      const result = { expiresAt: pro.expiresAt, isPro: true };
+
+      await setRedis(cacheKey, result, generateMediumExpiry());
+      logger.info(`Fetched pro status for ${id}`);
 
       return res
         .status(200)
         .setHeader('Cache-Control', CACHE_AGE_30_MINS)
-        .json({ result: JSON.parse(cachedData), success: true });
+        .json({ result, success: true });
+    } catch (error) {
+      return catchedError(res, error);
     }
-
-    const pro = await prisma.pro.findUnique({
-      where: { id: id as string }
-    });
-
-    if (!pro) {
-      return res
-        .status(200)
-        .json({ result: { expiresAt: null, isPro: false } });
-    }
-
-    const result = { expiresAt: pro.expiresAt, isPro: true };
-
-    await setRedis(cacheKey, result, generateMediumExpiry());
-    logger.info(`Fetched pro status for ${id}`);
-
-    return res
-      .status(200)
-      .setHeader('Cache-Control', CACHE_AGE_30_MINS)
-      .json({ result, success: true });
-  } catch (error) {
-    return catchedError(res, error);
   }
-};
+];
