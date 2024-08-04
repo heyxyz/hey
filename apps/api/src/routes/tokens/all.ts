0798070d115623020d3dc95bedf5021c713c7dee
bigint <bigint@hey.xyz>
2024-08-04 11:59:50 +0530

    feat: add more rl

--- apps/api/src/routes/tokens/all.ts
+++ apps/api/src/routes/tokens/all.ts
@@ -1,38 +1,42 @@
-import type { Handler } from 'express';
+import type { Request, Response } from 'express';
 
 import heyPg from '@hey/db/heyPg';
 import { getRedis, setRedis } from '@hey/db/redisClient';
 import logger from '@hey/helpers/logger';
 import catchedError from 'src/helpers/catchedError';
 import { CACHE_AGE_1_DAY } from 'src/helpers/constants';
+import { rateLimiter } from 'src/helpers/middlewares/rateLimiter';
 
-export const get: Handler = async (_, res) => {
-  try {
-    const cacheKey = 'allowed-tokens';
-    const cachedData = await getRedis(cacheKey);
+export const get = [
+  rateLimiter({ requests: 250, within: 1 }),
+  async (_: Request, res: Response) => {
+    try {
+      const cacheKey = 'allowed-tokens';
+      const cachedData = await getRedis(cacheKey);
 
-    if (cachedData) {
-      logger.info('(cached) All tokens fetched');
-      return res
-        .status(200)
-        .setHeader('Cache-Control', CACHE_AGE_1_DAY)
-        .json({ success: true, tokens: JSON.parse(cachedData) });
-    }
+      if (cachedData) {
+        logger.info('(cached) All tokens fetched');
+        return res
+          .status(200)
+          .setHeader('Cache-Control', CACHE_AGE_1_DAY)
+          .json({ success: true, tokens: JSON.parse(cachedData) });
+      }
 
-    const data = await heyPg.query(`
+      const data = await heyPg.query(`
       SELECT *
       FROM "AllowedToken"
       ORDER BY priority DESC;
     `);
 
-    await setRedis(cacheKey, data);
-    logger.info('All tokens fetched');
+      await setRedis(cacheKey, data);
+      logger.info('All tokens fetched');
 
-    return res
-      .status(200)
-      .setHeader('Cache-Control', CACHE_AGE_1_DAY)
-      .json({ success: true, tokens: data });
-  } catch (error) {
-    return catchedError(res, error);
+      return res
+        .status(200)
+        .setHeader('Cache-Control', CACHE_AGE_1_DAY)
+        .json({ success: true, tokens: data });
+    } catch (error) {
+      return catchedError(res, error);
+    }
   }
-};
+];
