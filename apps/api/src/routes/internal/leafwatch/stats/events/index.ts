5c0697c86087f4546ef82ea32cae5b1fa495518a
bigint <bigint@hey.xyz>
2024-08-10 22:49:17 +0530

    chore: update schema

--- apps/api/src/routes/internal/leafwatch/stats/events/index.ts
+++ apps/api/src/routes/internal/leafwatch/stats/events/index.ts
@@ -11,35 +11,39 @@ export const get = [
   validateIsStaff,
   async (_: Request, res: Response) => {
     try {
-      const [lastHour, today, yesterday, thisWeek, thisMonth, allTime] =
-        await leafwatch.$transaction([
-          leafwatch.event.count({
-            where: { created: { gte: new Date(Date.now() - 3600000) } }
-          }),
-          leafwatch.event.count({ where: { created: { equals: new Date() } } }),
-          leafwatch.event.count({
-            where: { created: { gte: new Date(Date.now() - 86400000) } }
-          }),
-          leafwatch.event.count({
-            where: { created: { gte: new Date(Date.now() - 604800000) } }
-          }),
-          leafwatch.event.count({
-            where: { created: { gte: new Date(Date.now() - 2592000000) } }
-          }),
-          leafwatch.event.count()
-        ]);
+      const stats: [
+        {
+          allTime: bigint;
+          lastHour: bigint;
+          thisMonth: bigint;
+          thisWeek: bigint;
+          today: bigint;
+          yesterday: bigint;
+        }
+      ] = await leafwatch.$queryRaw`
+        SELECT
+          COUNT(*) AS "allTime",
+          COUNT(CASE WHEN created >= NOW() - INTERVAL '1 hour' THEN 1 END) AS "lastHour",
+          COUNT(CASE WHEN DATE(created) = CURRENT_DATE THEN 1 END) AS "today",
+          COUNT(CASE WHEN created >= DATE_TRUNC('day', NOW()) - INTERVAL '1 day' AND created < DATE_TRUNC('day', NOW()) THEN 1 END) AS "yesterday",
+          COUNT(CASE WHEN created >= NOW() - INTERVAL '7 day' THEN 1 END) AS "thisWeek",
+          COUNT(CASE WHEN created >= NOW() - INTERVAL '30 day' THEN 1 END) AS "thisMonth"
+        FROM "Event";
+      `;
+
+      // Convert BigInt values to Number
+      const convertedStats = {
+        allTime: Number(stats[0].allTime),
+        lastHour: Number(stats[0].lastHour),
+        thisMonth: Number(stats[0].thisMonth),
+        thisWeek: Number(stats[0].thisWeek),
+        today: Number(stats[0].today),
+        yesterday: Number(stats[0].yesterday)
+      };
 
       logger.info('Fetched Leafwatch events stats');
 
-      return res.status(200).json({
-        allTime,
-        lastHour,
-        success: true,
-        thisMonth,
-        thisWeek,
-        today,
-        yesterday
-      });
+      return res.status(200).json(convertedStats);
     } catch (error) {
       return catchedError(res, error);
     }
