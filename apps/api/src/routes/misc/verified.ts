74d6f7bd3e1e4b3380082724ef578f5768820a6e
bigint <bigint@hey.xyz>
2024-08-10 11:08:13 +0530

    feat: migrate to prisma

--- apps/api/src/routes/misc/verified.ts
+++ apps/api/src/routes/misc/verified.ts
@@ -1,11 +1,11 @@
 import type { Request, Response } from 'express';
 
-import heyPg from '@hey/db/heyPg';
 import { getRedis, setRedis } from '@hey/db/redisClient';
 import logger from '@hey/helpers/logger';
 import catchedError from 'src/helpers/catchedError';
 import { CACHE_AGE_30_MINS, VERIFIED_FEATURE_ID } from 'src/helpers/constants';
 import { rateLimiter } from 'src/helpers/middlewares/rateLimiter';
+import prisma from 'src/helpers/prisma';
 
 export const get = [
   rateLimiter({ requests: 250, within: 1 }),
@@ -22,15 +22,10 @@ export const get = [
           .json({ result: JSON.parse(cachedData), success: true });
       }
 
-      const data = await heyPg.query(
-        `
-        SELECT "profileId"
-        FROM "ProfileFeature"
-        WHERE enabled = TRUE
-        AND "featureId" = $1;
-      `,
-        [VERIFIED_FEATURE_ID]
-      );
+      const data = await prisma.profileFeature.findMany({
+        select: { profileId: true },
+        where: { enabled: true, featureId: VERIFIED_FEATURE_ID }
+      });
 
       const ids = data.map(({ profileId }) => profileId);
       await setRedis(cacheKey, ids);
