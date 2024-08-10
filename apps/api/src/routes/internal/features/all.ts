968861f36624b0b6664aa02464f291503aa39c52
bigint <bigint@hey.xyz>
2024-08-10 10:13:12 +0530

    feat: migrate internal ff to prisma

--- apps/api/src/routes/internal/features/all.ts
+++ apps/api/src/routes/internal/features/all.ts
@@ -1,23 +1,20 @@
 import type { Request, Response } from 'express';
 
-import heyPg from '@hey/db/heyPg';
 import logger from '@hey/helpers/logger';
 import catchedError from 'src/helpers/catchedError';
 import validateIsStaff from 'src/helpers/middlewares/validateIsStaff';
 import validateLensAccount from 'src/helpers/middlewares/validateLensAccount';
+import prisma from 'src/helpers/prisma';
 
 export const get = [
   validateLensAccount,
   validateIsStaff,
   async (_: Request, res: Response) => {
     try {
-      const data = await heyPg.query(`
-      SELECT F.*, COUNT(PF."profileId") AS assigned
-      FROM "Feature" F
-      LEFT JOIN "ProfileFeature" PF ON F."id" = PF."featureId"
-      GROUP BY F."id"
-      ORDER BY F.priority ASC;
-    `);
+      const data = await prisma.feature.findMany({
+        include: { _count: { select: { profiles: true } } },
+        orderBy: { priority: 'asc' }
+      });
 
       logger.info('All features fetched');
 
