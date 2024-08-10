24c27d38a54e1ad741c0d3ad0b5c24727c389f63
bigint <bigint@hey.xyz>
2024-08-10 22:39:52 +0530

    chore: update schema

---
+++ packages/db/prisma/leafwatch/migrations/20240810170936_add_more_index/migration.sql
@@ -0,0 +1,8 @@
+-- CreateIndex
+CREATE INDEX "Event_name_idx" ON "Event"("name");
+
+-- CreateIndex
+CREATE INDEX "Event_actor_idx" ON "Event"("actor");
+
+-- CreateIndex
+CREATE INDEX "Impression_publication_idx" ON "Impression"("publication");
