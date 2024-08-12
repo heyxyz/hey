42225dd1e4b8c168b51b8040887dac3e42765ce6
bigint <bigint@hey.xyz>
2024-08-12 13:39:55 +0530

    feat: add backupImpressionsToS3

--- apps/cron/src/index.ts
+++ apps/cron/src/index.ts
@@ -15,52 +15,52 @@ import heartbeat from './heartbeat';
 const startCronJobs = () => {
   logger.info('Cron jobs are started...');
 
-  // cron.schedule('*/30 * * * * *', async () => {
-  //   await heartbeat();
-  //   return;
-  // });
+  cron.schedule('*/30 * * * * *', async () => {
+    await heartbeat();
+    return;
+  });
 
-  // cron.schedule('*/5 * * * *', async () => {
-  //   await cleanClickhouse();
-  //   return;
-  // });
+  cron.schedule('*/5 * * * *', async () => {
+    await cleanClickhouse();
+    return;
+  });
 
-  // cron.schedule('*/5 * * * *', async () => {
-  //   await cleanDraftPublications();
-  //   return;
-  // });
+  cron.schedule('*/5 * * * *', async () => {
+    await cleanDraftPublications();
+    return;
+  });
 
-  // cron.schedule('*/5 * * * *', async () => {
-  //   await cleanEmailTokens();
-  //   return;
-  // });
+  cron.schedule('*/5 * * * *', async () => {
+    await cleanEmailTokens();
+    return;
+  });
 
-  // cron.schedule('*/5 * * * *', async () => {
-  //   await cleanPreferences();
-  //   return;
-  // });
+  cron.schedule('*/5 * * * *', async () => {
+    await cleanPreferences();
+    return;
+  });
 
-  // cron.schedule('0 */6 * * *', async () => {
-  //   await dbVacuum();
-  //   return;
-  // });
+  cron.schedule('0 */6 * * *', async () => {
+    await dbVacuum();
+    return;
+  });
 
-  // cron.schedule('*/10 * * * *', async () => {
-  //   await batchProcessEvents();
-  //   return;
-  // });
+  cron.schedule('*/10 * * * *', async () => {
+    await batchProcessEvents();
+    return;
+  });
 
-  // cron.schedule('*/1 * * * *', async () => {
-  //   await batchProcessImpressions();
-  //   return;
-  // });
+  cron.schedule('*/1 * * * *', async () => {
+    await batchProcessImpressions();
+    return;
+  });
 
-  // cron.schedule('*/5 * * * *', async () => {
-  //   await backupEventsToS3();
-  //   return;
-  // });
+  cron.schedule('*/5 * * * *', async () => {
+    await backupEventsToS3();
+    return;
+  });
 
-  cron.schedule('*/5 * * * * *', async () => {
+  cron.schedule('*/5 * * * *', async () => {
     await backupImpressionsToS3();
     return;
   });
