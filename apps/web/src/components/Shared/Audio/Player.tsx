9ee2ce1d51a93ac2d6f054c7b15d1aa65c3d6869
dog egg <1250844516@qq.com>
2024-08-18 00:36:39 +0800

    fix doucment not find

--- apps/web/src/components/Shared/Audio/Player.tsx
+++ apps/web/src/components/Shared/Audio/Player.tsx
@@ -1,10 +1,11 @@
 import type { APITypes } from 'plyr-react';
 import type { FC, Ref } from 'react';
 
-import Plyr from 'plyr-react';
+import dynamic from 'next/dynamic';
 import 'plyr-react/plyr.css';
 import { memo } from 'react';
 
+const Plyr = dynamic(() => import('plyr-react'), { ssr: false });
 interface PlayerProps {
   playerRef: Ref<APITypes>;
   src: string;
