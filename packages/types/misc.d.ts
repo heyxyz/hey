47995d971068bff6abce11ac1e7e09c502f93371
MOΞ <moe.salih@gmail.com>
2024-08-18 15:01:22 -0400

    fix: add imageAspectRatio to type

--- packages/types/misc.d.ts
+++ packages/types/misc.d.ts
@@ -63,6 +63,7 @@ export interface Frame {
   }[];
   frameUrl: string;
   image: string;
+  imageAspectRatio?: string;
   inputText: null | string;
   lensFramesVersion: null | string;
   location?: string;
