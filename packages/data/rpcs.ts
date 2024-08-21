5a5b3a2f6b3994907878abbce4df197672da058f
bigint <bigint@hey.xyz>
2024-08-21 10:14:16 +0530

    chore: remove outdated POLYGON_AMOY_RPCS endpoint 🌐

--- packages/data/rpcs.ts
+++ packages/data/rpcs.ts
@@ -6,6 +6,5 @@ export const POLYGON_RPCS = [
 
 export const POLYGON_AMOY_RPCS = [
   'https://rpc-amoy.polygon.technology',
-  'https://lb.nodies.app/v1/1c9670faa1fb4c59ac4b0185ec9f19b7',
   'https://polygon-amoy-bor-rpc.publicnode.com'
 ];
