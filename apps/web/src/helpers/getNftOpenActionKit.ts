f1b74e4e237c730bd5561a7175369a47d3888ee9
bigint <bigint@hey.xyz>
2024-09-01 13:29:22 +0530

    chore: remove unused API keys from NftOpenActionKit initialization code

--- apps/web/src/helpers/getNftOpenActionKit.ts
+++ apps/web/src/helpers/getNftOpenActionKit.ts
@@ -2,9 +2,7 @@ import { NftOpenActionKit } from 'nft-openaction-kit';
 
 const getNftOpenActionKit = () => {
   return new NftOpenActionKit({
-    decentApiKey: process.env.NEXT_PUBLIC_DECENT_API_KEY || '',
-    openSeaApiKey: process.env.NEXT_PUBLIC_OPENSEA_API_KEY || '',
-    raribleApiKey: process.env.NEXT_PUBLIC_RARIBLE_API_KEY || ''
+    decentApiKey: process.env.NEXT_PUBLIC_DECENT_API_KEY || ''
   });
 };
 
