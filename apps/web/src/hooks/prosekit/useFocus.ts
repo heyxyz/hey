7a97ac9059e402f19ea57f5dfecfef930f6fee23
bigint <bigint@hey.xyz>
2024-09-02 20:01:01 +0530

    chore: add useFocus hook to Editor for improved focus handling on load ✨

---
+++ apps/web/src/hooks/prosekit/useFocus.ts
@@ -0,0 +1,12 @@
+import type { EditorExtension } from '@helpers/prosekit/extension';
+import type { Editor } from 'prosekit/core';
+
+import { useEffect } from 'react';
+
+const useFocus = (editor: Editor<EditorExtension>) => {
+  useEffect(() => {
+    editor.focus();
+  }, [editor]);
+};
+
+export default useFocus;
