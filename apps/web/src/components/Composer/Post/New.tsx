e257a0aeab568e9551a3e79a611a33b7b8c3095e
bigint <bigint@hey.xyz>
2024-09-02 19:30:31 +0530

    chore: simplify NewPost component layout by removing unnecessary button and adjusting onClick behavior ✨

--- apps/web/src/components/Composer/Post/New.tsx
+++ apps/web/src/components/Composer/Post/New.tsx
@@ -1,9 +1,7 @@
 import type { FC } from 'react';
 
-import { PencilSquareIcon } from '@heroicons/react/24/outline';
 import getAvatar from '@hey/helpers/getAvatar';
 import getLennyURL from '@hey/helpers/getLennyURL';
-import getProfile from '@hey/helpers/getProfile';
 import { Card, Image } from '@hey/ui';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
@@ -16,7 +14,7 @@ interface NewPostProps {
 }
 
 const NewPost: FC<NewPostProps> = ({ tags }) => {
-  const { isReady, push, query } = useRouter();
+  const { isReady, query } = useRouter();
   const { currentProfile } = useProfileStore();
   const { setShowNewPostModal } = useGlobalModalStateStore();
   const { setPublicationContent, setTags } = usePublicationStore();
@@ -51,27 +49,19 @@ const NewPost: FC<NewPostProps> = ({ tags }) => {
   }, []);
 
   return (
-    <Card className="space-y-3 p-5">
+    <Card className="cursor-pointer space-y-3 p-5" onClick={() => openModal()}>
       <div className="flex items-center space-x-3">
         <Image
           alt={currentProfile?.id}
           className="size-11 cursor-pointer rounded-full border bg-gray-200 dark:border-gray-700"
           height={44}
-          onClick={() => push(getProfile(currentProfile).link)}
           onError={({ currentTarget }) => {
             currentTarget.src = getLennyURL(currentProfile?.id);
           }}
           src={getAvatar(currentProfile)}
           width={44}
         />
-        <button
-          className="flex w-full items-center space-x-2 rounded-xl border bg-gray-100 px-4 py-2 dark:border-gray-700 dark:bg-gray-900"
-          onClick={() => openModal()}
-          type="button"
-        >
-          <PencilSquareIcon className="size-5" />
-          <span>What's new?!</span>
-        </button>
+        <span className="ld-text-gray-500">What's new?!</span>
       </div>
     </Card>
   );
