2cbefc5dcdc5bab2a09482117712652d9e2975e6
Yoginth <69431456+bigint@users.noreply.github.com>
2024-09-05 18:19:20 +0530

    chore: update @irys/upload and @irys/upload-ethereum versions to 0.0.2-a1 for improved functionality and stability

--- apps/api/src/routes/metadata/index.ts
+++ apps/api/src/routes/metadata/index.ts
@@ -29,7 +29,6 @@ export const post = [
         account.signMessage({ message })
       );
 
-      // @ts-ignore
       const receipt = await irys.upload(JSON.stringify(signed), {
         tags: [
           { name: 'content-type', value: 'application/json' },
