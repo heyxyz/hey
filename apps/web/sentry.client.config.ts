d630f1464aeb4e7c7dd152a30695c3b0f971c10f
Yoginth <hey@yoginth.com>
2024-09-05 19:27:03 +0530

    feat: add error type to Sentry ignore list for better error handling during onboarding with Talisman extension

--- apps/web/sentry.client.config.ts
+++ apps/web/sentry.client.config.ts
@@ -5,6 +5,7 @@ Sentry.init({
   dsn: 'https://glet_85e8511597c1ec9fd11b49addb52d697@observe.gitlab.com:443/errortracking/api/v1/projects/61401782',
   enabled: process.env.NODE_ENV === 'production',
   ignoreErrors: [
+    'TypeError: Cannot redefine property: ethereum',
     'Error: Talisman extension has not been configured yet. Please continue with onboarding.'
   ]
 });
