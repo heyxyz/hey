5b922739e8e34878dbe288e66f6d94f06c8eb094
Yoginth <69431456+bigint@users.noreply.github.com>
2024-09-05 19:25:26 +0530

    feat: enhance Sentry configuration by enabling production mode and ignoring specific onboarding errors for improved error tracking

--- apps/web/sentry.client.config.ts
+++ apps/web/sentry.client.config.ts
@@ -2,5 +2,9 @@ import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
   debug: false,
-  dsn: 'https://glet_85e8511597c1ec9fd11b49addb52d697@observe.gitlab.com:443/errortracking/api/v1/projects/61401782'
+  dsn: 'https://glet_85e8511597c1ec9fd11b49addb52d697@observe.gitlab.com:443/errortracking/api/v1/projects/61401782',
+  enabled: process.env.NODE_ENV === 'production',
+  ignoreErrors: [
+    'Error: Talisman extension has not been configured yet. Please continue with onboarding.'
+  ]
 });
