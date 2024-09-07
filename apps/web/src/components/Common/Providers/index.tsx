aacedd1ccfa9df97cc310b7b81052fce98542d0e
Bigint <69431456+bigint@users.noreply.github.com>
2024-09-07 09:25:40 +0530

    chore: add SentryProvider to enhance error tracking capability in providers list

--- apps/web/src/components/Common/Providers/index.tsx
+++ apps/web/src/components/Common/Providers/index.tsx
@@ -20,6 +20,7 @@ import LensSubscriptionsProvider from './LensSubscriptionsProvider';
 import OptimisticTransactionsProvider from './OptimisticTransactionsProvider';
 import PreferencesProvider from './PreferencesProvider';
 import ProProvider from './ProProvider';
+import SentryProvider from './SentryProvider';
 import ServiceWorkerProvider from './ServiceWorkerProvider';
 import Web3Provider from './Web3Provider';
 
@@ -40,6 +41,7 @@ const Providers: FC<ProvidersProps> = ({ children }) => {
     <ErrorBoundary>
       <ServiceWorkerProvider />
       <LeafwatchProvider />
+      <SentryProvider />
       <Web3Provider>
         <ApolloProvider client={lensApolloClient}>
           <FeatureFlagProvider>
