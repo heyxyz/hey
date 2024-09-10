f91b6b36859865e9f75e249ccf57de4c2b83f091
Yoginth <69431456+bigint@users.noreply.github.com>
2024-09-10 20:27:06 +0530

    chore: truncate ClickHouse system logs and clean non hey.xyz events

--- apps/cron/src/cleanClickhouse.ts
+++ apps/cron/src/cleanClickhouse.ts
@@ -1,10 +1,34 @@
 import clickhouseClient from '@hey/db/clickhouseClient';
 import logger from '@hey/helpers/logger';
 
+// SELECT
+//   table,
+//   formatReadableSize(sum(bytes_on_disk)) AS total_size
+// FROM system.parts
+// GROUP BY database, table
+// ORDER BY total_size DESC;
+
 const cleanClickhouse = async () => {
   try {
     await clickhouseClient.command({
       query: "ALTER TABLE events DELETE WHERE url NOT LIKE '%hey.xyz%';"
+    });
+
+    // Truncate all Clickhouse system logs
+    await clickhouseClient.command({
+      query: 'TRUNCATE TABLE system.processors_profile_log;'
+    });
+    await clickhouseClient.command({
+      query: 'TRUNCATE TABLE system.asynchronous_metric_log;'
+    });
+    await clickhouseClient.command({
+      query: 'TRUNCATE TABLE system.query_log;'
+    });
+    await clickhouseClient.command({
+      query: 'TRUNCATE TABLE system.metric_log;'
+    });
+    await clickhouseClient.command({
+      query: 'TRUNCATE TABLE system.trace_log;'
     });
 
     logger.info(
