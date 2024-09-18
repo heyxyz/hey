84327cc733295a4fa79fbbad64a1e35637e93905
Bigint <69431456+bigint@users.noreply.github.com>
2024-09-18 13:55:32 +0530

    fix: update button styles for primary and danger variants in dark mode

--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -31,56 +31,61 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
     ref
   ) {
     const nonOutlineTextStyles = !outline && {
-      "text-white dark:text-black":
+      "text-white hover:text-white active:text-gray-400":
+        variant === "primary" || variant === "danger",
+      "dark:text-gray-950 dark:hover:text-gray-900 dark:active:text-gray-600":
         variant === "primary" || variant === "danger"
     };
 
     const nonOutlineBgStyles = !outline && {
-      "bg-black hover:bg-gray-800 active:bg-gray-700": variant === "primary",
+      "bg-gray-950 hover:bg-gray-700 active:bg-gray-700": variant === "primary",
       "bg-red-500 hover:bg-red-800 active:bg-red-700": variant === "danger",
+      "dark:bg-white dark:hover:bg-gray-200 dark:active:bg-gray-200":
+        variant === "primary",
       "dark:bg-red-500 dark:hover:bg-red-800 dark:active:bg-red-700":
-        variant === "danger",
-      "dark:bg-white dark:hover:bg-gray-100 dark:active:bg-gray-200":
-        variant === "primary"
+        variant === "danger"
     };
 
     const nonOutlineBorderStyles = !outline && {
-      "border border-black hover:border-gray-800 active:border-gray-700":
+      "border border-gray-950 hover:border-gray-800 active:border-gray-700":
         variant === "primary",
       "border border-red-500 hover:border-red-800 active:border-red-700":
         variant === "danger",
-      "dark:border-red-500 dark:hover:border-red-800 dark:active:border-red-700":
-        variant === "danger",
       "dark:border-white dark:hover:border-gray-100 dark:active:border-gray-200":
-        variant === "primary"
+        variant === "primary",
+      "dark:border-red-500 dark:hover:border-red-800 dark:active:border-red-700":
+        variant === "danger"
     };
 
     const nonOutlineDisabledStyles = !outline &&
       disabled && {
-        "dark:text-gray-600 dark:hover:bg-gray-800 dark:bg-gray-800 dark:hover:border-gray-800 dark:active:bg-gray-800 dark:active:border-gray-800 dark:border-gray-800":
+        "dark:text-gray-600 hover:dark:text-gray-600 active:dark:text-gray-600 dark:hover:bg-gray-800 dark:bg-gray-800 dark:hover:border-gray-800 dark:active:bg-gray-800 dark:active:border-gray-800 dark:border-gray-800":
           variant === "primary",
         "dark:text-red-900 dark:hover:bg-red-500 dark:hover:border-red-500 dark:active:bg-red-500 dark:active:border-red-500 dark:border-red-500":
           variant === "danger",
-        "text-gray-500 hover:bg-gray-200 hover:border-gray-200 active:bg-gray-200 bg-gray-200 active:border-gray-200 border-gray-200":
+        "text-gray-500 hover:text-gray-500 active:text-gray-500 hover:bg-gray-200 hover:border-gray-200 active:bg-gray-200 bg-gray-200 active:border-gray-200 border-gray-200":
           variant === "primary",
         "text-red-200 hover:bg-red-500 hover:border-red-500 active:bg-red-500 active:border-red-500 border-red-500":
           variant === "danger"
       };
 
     const outlineTextStyles = outline && {
-      "text-black dark:text-white": variant === "primary",
+      "text-gray-950 active:text-gray-300": variant === "primary",
+      "dark:text-white dark:active:text-gray-700": variant === "primary",
       "text-red-500 hover:text-red-400": variant === "danger"
+      // TODO Dark danger
     };
 
     const outlineBorderStyles = outline && {
-      "border border-gray-300 dark:border-white hover:border-gray-500":
-        variant === "primary",
+      "border border-gray-300 hover:border-gray-950": variant === "primary",
+      "dark:border-gray-700 dark:hover:border-gray-100": variant === "primary",
       "border border-red-600 hover:border-red-400": variant === "danger"
+      // TODO Dark danger
     };
 
     const outlineDisabledStyles = outline &&
       disabled && {
-        "text-gray-600 hover:border-gray-300": variant === "primary",
+        "text-gray-300 hover:border-gray-300": variant === "primary",
         "text-red-400 hover:text-red-400 hover:border-red-400 border-red-400":
           variant === "danger"
       };
