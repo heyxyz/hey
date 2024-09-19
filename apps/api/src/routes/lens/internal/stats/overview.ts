efcb63f9c87152d00e2a9a8102bf2229a370350e
Bigint <69431456+bigint@users.noreply.github.com>
2024-09-19 19:16:04 +0530

    feat: add lens stats overview FE

--- apps/api/src/routes/lens/internal/stats/overview.ts
+++ apps/api/src/routes/lens/internal/stats/overview.ts
@@ -12,65 +12,65 @@ export const get = [
     try {
       const result = await lensPg.multi(
         `
-          SELECT reltuples AS authentications_count
+          SELECT reltuples::BIGINT AS authentications_count
           FROM pg_class
           WHERE relname = 'authentication_record' AND relnamespace = (SELECT oid FROM pg_namespace WHERE nspname = 'tracking');
 
-          SELECT reltuples AS relay_usage_count
+          SELECT reltuples::BIGINT AS relay_usage_count
           FROM pg_class
           WHERE relname = 'usage' AND relnamespace = (SELECT oid FROM pg_namespace WHERE nspname = 'relay');
 
-          SELECT reltuples AS publications_count
+          SELECT reltuples::BIGINT AS publications_count
           FROM pg_class
           WHERE relname = 'record' AND relnamespace = (SELECT oid FROM pg_namespace WHERE nspname = 'publication');
 
-          SELECT reltuples AS profiles_count
+          SELECT reltuples::BIGINT AS profiles_count
           FROM pg_class
           WHERE relname = 'record' AND relnamespace = (SELECT oid FROM pg_namespace WHERE nspname = 'profile');
 
-          SELECT reltuples AS bookmarked_publications_count
+          SELECT reltuples::BIGINT AS bookmarked_publications_count
           FROM pg_class
           WHERE relname = 'bookmarked_publication' AND relnamespace = (SELECT oid FROM pg_namespace WHERE nspname = 'personalisation');
 
-          SELECT reltuples AS not_interested_publications_count
+          SELECT reltuples::BIGINT AS not_interested_publications_count
           FROM pg_class
           WHERE relname = 'not_interested_publication' AND relnamespace = (SELECT oid FROM pg_namespace WHERE nspname = 'personalisation');
 
-          SELECT reltuples AS wtf_recommendation_dismissed_count
+          SELECT reltuples::BIGINT AS wtf_recommendation_dismissed_count
           FROM pg_class
           WHERE relname = 'wtf_recommendation_dismissed' AND relnamespace = (SELECT oid FROM pg_namespace WHERE nspname = 'personalisation');
 
-          SELECT reltuples AS notifications_count
+          SELECT reltuples::BIGINT AS notifications_count
           FROM pg_class
           WHERE relname = 'record' AND relnamespace = (SELECT oid FROM pg_namespace WHERE nspname = 'notification');
 
-          SELECT total_count as momoka_count FROM momoka.stats;
+          SELECT total_count::BIGINT as momoka_count FROM momoka.stats;
 
-          SELECT reltuples AS media_snapshots_count
+          SELECT reltuples::BIGINT AS media_snapshots_count
           FROM pg_class
           WHERE relname = 'snapshot_mapping' AND relnamespace = (SELECT oid FROM pg_namespace WHERE nspname = 'media');
 
-          SELECT reltuples AS quality_profiles_count
+          SELECT reltuples::BIGINT AS quality_profiles_count
           FROM pg_class
           WHERE relname = 'quality_profiles' AND relnamespace = (SELECT oid FROM pg_namespace WHERE nspname = 'machine_learning');
 
-          SELECT reltuples AS indexed_transactions_count
+          SELECT reltuples::BIGINT AS indexed_transactions_count
           FROM pg_class
           WHERE relname = 'indexed_transaction' AND relnamespace = (SELECT oid FROM pg_namespace WHERE nspname = 'indexer');
 
-          SELECT reltuples AS hashtags_count
+          SELECT reltuples::BIGINT AS hashtags_count
           FROM pg_class
           WHERE relname = 'hashtag' AND relnamespace = (SELECT oid FROM pg_namespace WHERE nspname = 'global_stats');
 
-          SELECT reltuples AS mentions_count
+          SELECT reltuples::BIGINT AS mentions_count
           FROM pg_class
           WHERE relname = 'mention' AND relnamespace = (SELECT oid FROM pg_namespace WHERE nspname = 'global_stats');
 
-          SELECT reltuples AS ens_count
+          SELECT reltuples::BIGINT AS ens_count
           FROM pg_class
           WHERE relname = 'address_reverse_record' AND relnamespace = (SELECT oid FROM pg_namespace WHERE nspname = 'ens');
 
-          SELECT reltuples AS gardener_reports_count
+          SELECT reltuples::BIGINT AS gardener_reports_count
           FROM pg_class
           WHERE relname = 'gardener_profile_record' AND relnamespace = (SELECT oid FROM pg_namespace WHERE nspname = 'custom_filters');
         `
