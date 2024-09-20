b25308ec595aae5994714bd0c4079851972ce768
Yoginth <hey@yoginth.com>
2024-09-20 20:37:45 +0530

    feat: modify Docker deployment script to use --privileged flag for dockerd to enhance compatibility in CI setup

--- .pierre/ci/dockerhub.ts
+++ .pierre/ci/dockerhub.ts
@@ -4,8 +4,10 @@ export const label = "Deploy to Docker Hub";
 
 const deployApi = async () => {
   const image = "heyxyz/api:latest";
-  await run("curl -fsSL https://get.docker.com | sh");
-  await run("dockerd &");
+  await run("curl -fsSL https://get.docker.com/rootless | sh");
+  await run("export PATH=$HOME/bin:$PATH");
+  await run("export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock");
+  await run("dockerd --privileged &");
   await run("sleep 10");
   await run("docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_TOKEN");
   await run(`docker build -t ${image} -f ./apps/api/Dockerfile .`);
