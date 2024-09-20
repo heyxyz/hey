cfccb59fdafad47f2abb143e016b7aab200d78bf
Bigint <69431456+bigint@users.noreply.github.com>
2024-09-20 18:14:29 +0530

    feat: update Docker deployment script to use entrypoint and sleep instead of usermod and systemctl commands in CI

--- .pierre/ci/dockerhub.ts
+++ .pierre/ci/dockerhub.ts
@@ -5,8 +5,8 @@ export const label = "Deploy to Docker Hub";
 const deployApi = async () => {
   const image = "heyxyz/api:latest";
   await run("curl -fsSL https://get.docker.com | sh");
-  await run("usermod -aG docker $USER");
-  await run("systemctl start docker");
+  await run("dockerd-entrypoint.sh &");
+  await run("sleep 5");
   await run("docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_TOKEN");
   await run(`docker build -t ${image} -f ./apps/api/Dockerfile .`);
   await run(`docker push ${image}`);
