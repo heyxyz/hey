247dc2b94d602801ae5e1b936818b418b4cacd3a
Yoginth <hey@yoginth.com>
2024-09-20 21:31:34 +0530

    feat: add docker build in pierre

--- .pierre/ci/dockerhub.ts
+++ .pierre/ci/dockerhub.ts
@@ -1,13 +1,11 @@
-import { run } from "pierre";
-
 export const label = "Deploy to Docker Hub";
 
 const deployApi = async () => {
-  const image = "heyxyz/api:latest";
-  await run("curl -fsSL https://get.docker.com | sh");
-  await run("docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_TOKEN");
-  await run(`docker build -t ${image} -f ./apps/api/Dockerfile .`);
-  await run(`docker push ${image}`);
+  // const image = "heyxyz/api:latest";
+  // await run("curl -fsSL https://get.docker.com | sh");
+  // await run("docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_TOKEN");
+  // await run(`docker build -t ${image} -f ./apps/api/Dockerfile .`);
+  // await run(`docker push ${image}`);
 };
 
 export default [deployApi];
