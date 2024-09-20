4a0fe67b15fb35f6da1730a4edb5e2d3d5dd4890
Bigint <69431456+bigint@users.noreply.github.com>
2024-09-20 21:03:37 +0530

    feat: add docker build in pierre

--- .pierre/ci/dockerhub.ts
+++ .pierre/ci/dockerhub.ts
@@ -5,8 +5,6 @@ export const label = "Deploy to Docker Hub";
 const deployApi = async () => {
   const image = "heyxyz/api:latest";
   await run("curl -fsSL https://get.docker.com | sh");
-  await run("dockerd --privileged &");
-  await run("sleep 10");
   await run("docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_TOKEN");
   await run(`docker build -t ${image} -f ./apps/api/Dockerfile .`);
   await run(`docker push ${image}`);
