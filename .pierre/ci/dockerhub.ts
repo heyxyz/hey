24c2a4b36ecdd7e71b2cbead87c1f846823c0d72
Yoginth <hey@yoginth.com>
2024-09-20 17:39:19 +0530

    feat: add Docker installation command to deploy API script in Pierre CI

--- .pierre/ci/dockerhub.ts
+++ .pierre/ci/dockerhub.ts
@@ -4,6 +4,7 @@ export const label = "Deploy to Docker Hub";
 
 const deployApi = async () => {
   const image = "heyxyz/api:latest";
+  await run("curl -fsSL https://get.docker.com | sh");
   await run("docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_TOKEN");
   await run(`docker build -t ${image} -f ./apps/api/Dockerfile .`);
   await run(`docker push ${image}`);
