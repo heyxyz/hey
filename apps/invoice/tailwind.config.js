324e5e328a305f838544dab4abba719333f5183f
Yoginth <hey@yoginth.com>
2024-09-21 10:51:25 +0530

    chore: remove invoice app

--- apps/invoice/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-const base = require("@hey/ui/tailwind-preset");
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  ...base,
-  content: ["./src/**/*.{ts,tsx}", "../../packages/ui/src/*.{ts,tsx}"],
-  plugins: [require("@tailwindcss/aspect-ratio"), require("@tailwindcss/forms")]
-};
