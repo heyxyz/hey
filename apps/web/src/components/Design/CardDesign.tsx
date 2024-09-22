5f505f0031e760f73b650be1b6f0df75fb31cc0a
Bigint <69431456+bigint@users.noreply.github.com>
2024-09-22 10:53:52 +0000

    🔧 feat: add design system for Card (#ENG-36)
    Summary: Added a design system for `Card` component.
    
    Highlights:
    
    • Created `CardDesign.tsx` to showcase various `Card` usages.
    • Imported and integrated `CardDesign` into the main design index.
    • Demonstrated different `Card` configurations, including padding and rounded corners.
    
    Read more: https://pierre.co/hey/hey/ENG-36

---
+++ apps/web/src/components/Design/CardDesign.tsx
@@ -0,0 +1,23 @@
+import { Card, CardHeader } from "@hey/ui";
+import type { FC } from "react";
+
+const CardDesign: FC = () => {
+  return (
+    <Card>
+      <CardHeader title="Error Message" />
+      <div className="m-5 flex flex-col items-start gap-5">
+        <Card>Card without padding</Card>
+        <Card className="p-5">Card body</Card>
+        <Card className="p-5" forceRounded>
+          Card force rounded (on mobile)
+        </Card>
+        <Card>
+          <CardHeader title="Card Header" />
+          <div className="p-5">Card body</div>
+        </Card>
+      </div>
+    </Card>
+  );
+};
+
+export default CardDesign;
