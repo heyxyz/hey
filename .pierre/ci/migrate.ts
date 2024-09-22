33b717981251de1172db4d8c826ffd44c84141e0
Yoginth <hey@yoginth.com>
2024-09-22 20:55:26 +0530

    fix: skip non main branch to not run migration

--- .pierre/ci/migrate.ts
+++ .pierre/ci/migrate.ts
@@ -2,6 +2,10 @@ import { run } from "pierre";
 
 export const label = "Run DB Migration";
 
-export default async () => {
+export default async ({ branch }) => {
+  if (branch.name !== "main") {
+    await run('echo "Skipping DB Migration on non-main branches 🚫"');
+  }
+
   await run("cd packages/db && pnpm prisma:migrate");
 };
