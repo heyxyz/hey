453cce72cfb88f58403452cf6af8a04d772c7dbd
Yoginth <69431456+bigint@users.noreply.github.com>
2024-09-23 07:42:49 +0000

    🔥 Remove GIT_COMMIT_SHA from staff bar (#rm-commit-sha)
    Read more: https://pierre.co/hey/hey/rm-commit-sha

--- apps/og/next.config.js
+++ apps/og/next.config.js
@@ -1,16 +1,5 @@
-const { CF_PAGES_COMMIT_SHA } = process.env;
-const COMMIT_SHA = CF_PAGES_COMMIT_SHA || "local";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  headers() {
-    return [
-      {
-        headers: [{ key: "X-Hey-Version", value: COMMIT_SHA }],
-        source: "/(.*)"
-      }
-    ];
-  },
   poweredByHeader: false,
   reactStrictMode: true
 };
