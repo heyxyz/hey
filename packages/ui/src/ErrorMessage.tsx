11ad57576e3bcebc27375c003b15e160c3c3c4c2
Yoginth <hey@yoginth.com>
2024-09-23 19:25:30 +0530

    Update error message to use red color instead of black

--- packages/ui/src/ErrorMessage.tsx
+++ packages/ui/src/ErrorMessage.tsx
@@ -19,14 +19,14 @@ export const ErrorMessage: FC<ErrorMessageProps> = ({
   return (
     <div
       className={cn(
-        "space-y-1 rounded-xl border-2 border-gray-500/50 bg-gray-50 p-4 text-sm dark:bg-gray-900/10",
+        "space-y-1 rounded-xl border-2 border-red-500/50 bg-red-50 p-4 text-red-800 text-sm dark:bg-red-900/10 dark:text-red-200",
         className
       )}
     >
       <div className="flex flex-wrap items-center gap-2">
         {title ? <h3 className="font-medium">{title}</h3> : null}
       </div>
-      <div className="ld-text-gray-500 break-words">{error?.message}</div>
+      <div className="break-words">{error?.message}</div>
     </div>
   );
 };
