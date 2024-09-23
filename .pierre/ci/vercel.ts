5d3ecd1d00151a3b17f0dff033b287008b98c32d
Yoginth <hey@yoginth.com>
2024-09-23 12:30:20 +0530

    Add Vercel deployment from Pierre guage

--- .pierre/ci/vercel.ts
+++ .pierre/ci/vercel.ts
@@ -55,7 +55,7 @@ const Job =
       };
 
       annotate({ color: "fg", ...baseData });
-      new Gauge(baseData.label, { color: "green", ...baseData });
+      new Gauge(baseData.label, { color: "green", value: 1, ...baseData });
 
       try {
         const previewUrlNoScheme = previewURL.replace(/^https?:\/\//, "");
