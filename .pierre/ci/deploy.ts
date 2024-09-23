2fec64368e0179bd3343abd58467d1583e551a59
Yoginth <hey@yoginth.com>
2024-09-23 21:59:29 +0530

    Update Vercel Pierre CI to look much better

--- .pierre/ci/deploy.ts
+++ .pierre/ci/deploy.ts
@@ -3,9 +3,6 @@ import { Job } from "./utils/vercel";
 export const label = "Deploy Web";
 
 export default Job({
-  VERCEL_ORG_ID: "team_Zcr7s5jVtNqVKO8Q4w7Kcm9T",
   VERCEL_PROJECT_ID: "prj_qmnJU7f5coeKD7x2VF33EP46jTHq",
-  VERCEL_SCOPE: "heyxyz",
-  VERCEL_PROJECT_NAME: "web",
-  NAME: "Web"
+  PROJECT_NAME: "Web"
 });
--- .pierre/ci/utils/vercel.ts
+++ .pierre/ci/utils/vercel.ts
@@ -3,11 +3,8 @@ import { Gauge, Icons, type JobContext, annotate, run } from "pierre";
 const vercel = "./node_modules/.bin/vercel";
 
 export interface VercelContext {
-  VERCEL_ORG_ID: string;
   VERCEL_PROJECT_ID: string;
-  VERCEL_SCOPE: string;
-  VERCEL_PROJECT_NAME: string;
-  NAME: string;
+  PROJECT_NAME: string;
 }
 
 interface VercelDeployment {
@@ -17,16 +14,12 @@ interface VercelDeployment {
 }
 
 export const Job =
-  ({
-    VERCEL_ORG_ID,
-    VERCEL_PROJECT_ID,
-    VERCEL_SCOPE,
-    VERCEL_PROJECT_NAME,
-    NAME
-  }: VercelContext) =>
+  ({ VERCEL_PROJECT_ID, PROJECT_NAME }: VercelContext) =>
   async (ctx: JobContext) => {
     const isProd = ctx.branch.name === "main";
     const VERCEL_ACCESS_TOKEN = process.env.VERCEL_ACCESS_TOKEN;
+    const VERCEL_ORG_ID = "team_Zcr7s5jVtNqVKO8Q4w7Kcm9T";
+    const VERCEL_SCOPE = "heyxyz";
 
     await run("rm -rf .pnpm-store");
     await run('echo "$PWD"');
@@ -38,7 +31,7 @@ export const Job =
         ctx.branch.id
       } -e PIERRE_ENVIRONMENT=${isProd ? "production" : "preview"}`,
       {
-        label: `Creating ${NAME} Deployment`,
+        label: `Creating ${PROJECT_NAME} Deployment`,
         env: { VERCEL_ORG_ID, VERCEL_PROJECT_ID }
       }
     );
@@ -54,10 +47,14 @@ export const Job =
         description: previewURL
       };
 
-      annotate({ color: "fg", label: ` ${NAME} App Preview`, ...baseData });
-      new Gauge(NAME, {
+      annotate({
+        color: "fg",
+        label: ` ${PROJECT_NAME} App Preview`,
+        ...baseData
+      });
+      new Gauge(PROJECT_NAME, {
         color: "blue",
-        label: NAME,
+        label: PROJECT_NAME,
         value: 1,
         ...baseData
       });
@@ -65,7 +62,7 @@ export const Job =
       try {
         const previewUrlNoScheme = previewURL.replace(/^https?:\/\//, "");
         const response = await fetch(
-          `https://api.vercel.com/v6/deployments?slug=${VERCEL_SCOPE}&app=${VERCEL_PROJECT_NAME}`,
+          `https://api.vercel.com/v6/deployments?slug=${VERCEL_SCOPE}&app=${PROJECT_NAME.toLowerCase()}`,
           { headers: { Authorization: `Bearer ${VERCEL_ACCESS_TOKEN}` } }
         );
 
@@ -87,7 +84,7 @@ export const Job =
           annotate({
             icon: Icons.Code,
             color: "fg",
-            label: ` ${NAME} Deployment Build Details`,
+            label: ` ${PROJECT_NAME} Deployment Build Details`,
             description: activeDeploy.inspectorUrl,
             href: activeDeploy.inspectorUrl
           });
@@ -107,12 +104,12 @@ export const Job =
     const { exitCode } = await run(
       `${vercel} inspect ${previewURL} --scope ${VERCEL_SCOPE} --logs --wait --timeout=15m --no-color --token $VERCEL_ACCESS_TOKEN`,
       {
-        label: ` ${NAME} Deployment Build`,
+        label: ` ${PROJECT_NAME} Deployment Build`,
         env: { VERCEL_ORG_ID, VERCEL_PROJECT_ID }
       }
     );
 
     if (exitCode !== 0) {
-      throw new Error(` ${NAME} build failed`);
+      throw new Error(` ${PROJECT_NAME} build failed`);
     }
   };
