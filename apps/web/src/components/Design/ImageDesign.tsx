4ae377943f163bed8960fea8b7c145eb831f8315
Yoginth <hey@yoginth.com>
2024-09-23 19:09:53 +0530

    Add design system for Image

---
+++ apps/web/src/components/Design/ImageDesign.tsx
@@ -0,0 +1,32 @@
+import { Card, CardHeader, H5, Image } from "@hey/ui";
+import type { FC } from "react";
+
+const ImageDesign: FC = () => {
+  return (
+    <Card>
+      <CardHeader title="Card" />
+      <div className="m-5 flex flex-col items-start gap-5">
+        <div>
+          <H5 className="mb-2">Simple Image</H5>
+          <Image
+            src="https://github.com/bigint.png"
+            alt="Bigint"
+            height={100}
+            width={100}
+          />
+        </div>
+        <div>
+          <H5 className="mb-2">Invalid Image</H5>
+          <Image
+            src="https://github.com/bigint.pdng"
+            alt="Bigint"
+            height={100}
+            width={100}
+          />
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default ImageDesign;
