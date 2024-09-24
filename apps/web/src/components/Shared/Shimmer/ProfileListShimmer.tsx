956378c5d5dddac1ec6a63ac60b7185e72dceaa9
Yoginth <hey@yoginth.com>
2024-09-24 10:55:09 +0530

    Migrate followers to use modal instead of new page

--- apps/web/src/components/Shared/Shimmer/ProfileListShimmer.tsx
+++ apps/web/src/components/Shared/Shimmer/ProfileListShimmer.tsx
@@ -1,21 +1,15 @@
 import UserProfileShimmer from "@components/Shared/Shimmer/UserProfileShimmer";
-import { ArrowLeftIcon } from "@heroicons/react/24/outline";
-import { Card } from "@hey/ui";
 import type { FC } from "react";
 
 const ProfileListShimmer: FC = () => {
   return (
-    <Card className="divide-y dark:divide-gray-700">
-      <div className="flex items-center space-x-3 px-5 py-6">
-        <ArrowLeftIcon className="size-5" />
-        <div className="shimmer h-4 w-1/5 rounded-full" />
-      </div>
+    <div className="divide-y dark:divide-gray-700">
       <UserProfileShimmer className="p-5" showFollowUnfollowButton />
       <UserProfileShimmer className="p-5" showFollowUnfollowButton />
       <UserProfileShimmer className="p-5" showFollowUnfollowButton />
       <UserProfileShimmer className="p-5" showFollowUnfollowButton />
       <UserProfileShimmer className="p-5" showFollowUnfollowButton />
-    </Card>
+    </div>
   );
 };
 
