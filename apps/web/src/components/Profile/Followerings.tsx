aea0ba9a1b59c7abaa0d7b32da5df0cbdbb50693
Bigint <69431456+bigint@users.noreply.github.com>
2024-09-24 05:26:32 +0000

    🚚 Migrate followers to use modal instead of new page (#ENG-59)
    Summary: Migrated followers to use a modal instead of a new page.
    
    Highlights:
    
    • Replaced `Link` with a button to open the `Following` modal in `Followerings.tsx`.
    • Removed `Following` component usage from `Profile/index.tsx`.
    • Added new `Following` component in `Shared/Modal/Following.tsx`.
    
    Read more: https://pierre.co/hey/hey/ENG-59

--- apps/web/src/components/Profile/Followerings.tsx
+++ apps/web/src/components/Profile/Followerings.tsx
@@ -1,28 +1,32 @@
+import Following from "@components/Shared/Modal/Following";
 import getProfile from "@hey/helpers/getProfile";
 import humanize from "@hey/helpers/humanize";
 import type { Profile } from "@hey/lens";
-import { H4 } from "@hey/ui";
+import { H4, Modal } from "@hey/ui";
 import Link from "next/link";
 import plur from "plur";
-import type { FC } from "react";
+import { type FC, useState } from "react";
 
 interface FolloweringsProps {
   profile: Profile;
 }
 
 const Followerings: FC<FolloweringsProps> = ({ profile }) => {
+  const [showFollowingModal, setShowFollowingModal] = useState(false);
+
   const profileLink = getProfile(profile).link;
   const { followers, following } = profile.stats;
 
   return (
     <div className="flex gap-8">
-      <Link
+      <button
+        onClick={() => setShowFollowingModal(true)}
         className="text-left outline-offset-4"
-        href={`${profileLink}/following`}
+        type="button"
       >
         <H4>{humanize(following)}</H4>
         <div className="ld-text-gray-500">Following</div>
-      </Link>
+      </button>
       <Link
         className="text-left outline-offset-4"
         href={`${profileLink}/followers`}
@@ -30,6 +34,13 @@ const Followerings: FC<FolloweringsProps> = ({ profile }) => {
         <H4>{humanize(followers)}</H4>
         <div className="ld-text-gray-500">{plur("Follower", followers)}</div>
       </Link>
+      <Modal
+        onClose={() => setShowFollowingModal(false)}
+        show={showFollowingModal}
+        title="Following"
+      >
+        <Following handle={getProfile(profile).slug} profileId={profile.id} />
+      </Modal>
     </div>
   );
 };
