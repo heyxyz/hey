027bf2e70b369d8bde3e85f0d8c8d9f25503ec65
Bigint <69431456+bigint@users.noreply.github.com>
2024-09-24 13:12:30 +0530

    Don't migrate on non main branch

--- .pierre/ci/migrate.ts
+++ .pierre/ci/migrate.ts
@@ -4,7 +4,7 @@ export const label = "Migrate DB";
 
 export default async ({ branch }) => {
   if (branch.name !== "main") {
-    await run('echo "Skipping DB Migration on non-main branches 🚫"');
+    return await run('echo "Skipping DB Migration on non-main branches 🚫"');
   }
 
   await run("cd packages/db && pnpm prisma:migrate");
