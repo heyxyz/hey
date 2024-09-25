90246aaf87c42b758e34652af0d0694925c7ad61
Yoginth <69431456+bigint@users.noreply.github.com>
2024-09-25 15:03:44 +0530

    Add basic maps plot

--- apps/web/src/components/Publication/Checkin.tsx
+++ apps/web/src/components/Publication/Checkin.tsx
@@ -12,18 +12,11 @@ const Checkin: FC<CheckinProps> = ({ publication }) => {
   }
 
   const { metadata } = publication;
-  const latitude = metadata.geographic?.latitude;
-  const longitude = metadata.geographic?.longitude;
   const locality = metadata.address?.locality;
   const country = metadata.address?.country;
   const postalCode = metadata.address?.postalCode;
   const location = metadata.location;
   const address = `${locality}, ${country}${postalCode ? `, ${postalCode}` : ""}`;
-  const position = `${latitude},${longitude}`;
-
-  if (!latitude || !longitude) {
-    return null;
-  }
 
   return (
     <Card className="mt-4">
@@ -34,7 +27,7 @@ const Checkin: FC<CheckinProps> = ({ publication }) => {
         className="rounded-t-none"
         loading="lazy"
         title={location}
-        src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBrxocLyV2K3XO99m5c74tedidN3vRU3Yc&q=${position}`}
+        src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBrxocLyV2K3XO99m5c74tedidN3vRU3Yc&q=${location}${address}`}
       />
     </Card>
   );
