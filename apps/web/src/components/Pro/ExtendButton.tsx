aa2724356570c2130f25d0e4b4b41a658fb9be30
Bigint <69431456+bigint@users.noreply.github.com>
2024-09-25 13:07:08 +0530

    Add pro upgrade modal

--- apps/web/src/components/Pro/ExtendButton.tsx
+++ apps/web/src/components/Pro/ExtendButton.tsx
@@ -1,7 +1,10 @@
 import errorToast from "@helpers/errorToast";
 import { MONTHLY_PRO_PRICE, PRO_EOA_ADDRESS } from "@hey/data/constants";
 import { Errors } from "@hey/data/errors";
-import { Button } from "@hey/ui";
+import addMonthsToDate from "@hey/helpers/datetime/addMonthsToDate";
+import formatDate from "@hey/helpers/datetime/formatDate";
+import { Button, Modal, RangeSlider } from "@hey/ui";
+import plur from "plur";
 import type { FC } from "react";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
@@ -17,8 +20,10 @@ interface ExtendButtonProps {
 
 const ExtendButton: FC<ExtendButtonProps> = ({ size = "lg" }) => {
   const { currentProfile } = useProfileStore();
-  const { isPro } = useProStore();
+  const { isPro, proExpiresAt } = useProStore();
 
+  const [showUpgradeModal, setShowUpgradeModal] = useState(false);
+  const [months, setMonths] = useState(5);
   const [isLoading, setIsLoading] = useState(false);
   const [transactionHash, setTransactionHash] = useState<`0x${string}` | null>(
     null
@@ -43,7 +48,7 @@ const ExtendButton: FC<ExtendButtonProps> = ({ size = "lg" }) => {
     }
   }, [isSuccess]);
 
-  const upgrade = async () => {
+  const handleUpgrade = async () => {
     if (!currentProfile) {
       return toast.error(Errors.SignWallet);
     }
@@ -51,12 +56,12 @@ const ExtendButton: FC<ExtendButtonProps> = ({ size = "lg" }) => {
     try {
       setIsLoading(true);
       await handleWrongNetwork();
-
       await sendTransactionAsync({
         data: currentProfile.id,
         to: PRO_EOA_ADDRESS,
-        value: parseEther(MONTHLY_PRO_PRICE.toString())
+        value: parseEther((MONTHLY_PRO_PRICE * months).toString())
       });
+      setShowUpgradeModal(false);
     } catch (error) {
       errorToast(error);
     } finally {
@@ -65,18 +70,63 @@ const ExtendButton: FC<ExtendButtonProps> = ({ size = "lg" }) => {
   };
 
   return (
-    <Button
-      className="mt-3 w-full"
-      disabled={isLoading || transactionLoading}
-      onClick={upgrade}
-      size={size}
-    >
-      {transactionLoading
-        ? "Transaction pending..."
-        : isPro
-          ? "Extend a Month"
-          : "Upgrade to Pro"}
-    </Button>
+    <>
+      <Button
+        className="mt-3 w-full"
+        onClick={() => setShowUpgradeModal(true)}
+        size={size}
+        disabled={isLoading || transactionLoading}
+      >
+        {isPro ? "Extend Pro" : "Upgrade to Pro"}
+      </Button>
+      <Modal
+        onClose={() => setShowUpgradeModal(false)}
+        show={showUpgradeModal}
+        title={isPro ? "Extend" : "Upgrade"}
+      >
+        <div className="m-5 space-y-5">
+          <div className="space-y-2">
+            <div>
+              {isPro ? "Extend your" : "Upgrade to"} Pro subscription for{" "}
+              <b>
+                {months} {plur("month", months)}
+              </b>
+            </div>
+            <div className="ld-text-gray-500 text-sm">
+              This is a non-refundable subscription. You will be charged
+              immediately for the duration of the subscription.
+            </div>
+            {proExpiresAt && (
+              <div className="text-sm">
+                New expiration date:{" "}
+                <b>
+                  {formatDate(addMonthsToDate(proExpiresAt, months) as any)}
+                </b>
+              </div>
+            )}
+          </div>
+          <RangeSlider
+            displayValue={months.toString()}
+            defaultValue={[months]}
+            showValueInThumb
+            max={50}
+            min={1}
+            onValueChange={(value) => setMonths(value[0])}
+          />
+          <Button
+            className="w-full"
+            onClick={handleUpgrade}
+            disabled={isLoading || transactionLoading}
+          >
+            {transactionLoading
+              ? "Transaction pending..."
+              : isPro
+                ? "Extend Pro"
+                : "Upgrade to Pro"}
+          </Button>
+        </div>
+      </Modal>
+    </>
   );
 };
 
