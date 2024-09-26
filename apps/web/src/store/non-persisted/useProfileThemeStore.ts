8b4b7b5593cefcb4adbf5e6f829b0d0e95240299
Yoginth <69431456+bigint@users.noreply.github.com>
2024-09-26 12:40:36 +0530

    Add custom profile design font family for pro users

---
+++ apps/web/src/store/non-persisted/useProfileThemeStore.ts
@@ -0,0 +1,18 @@
+import { createTrackedSelector } from "react-tracked";
+import { create } from "zustand";
+
+interface Theme {
+  fontStyle: string;
+}
+
+interface State {
+  theme: Theme | null;
+  setTheme: (theme: Theme | null) => void;
+}
+
+const store = create<State>((set) => ({
+  theme: null,
+  setTheme: (theme) => set(() => ({ theme }))
+}));
+
+export const useProfileThemeStore = createTrackedSelector(store);
