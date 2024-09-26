82091c297bf222f5c5b074ef1343eba4e22bac38
Yoginth <69431456+bigint@users.noreply.github.com>
2024-09-26 20:24:07 +0530

    Rename to profile theme

--- packages/db/prisma/db/migrations/20240926145252_add_profile_theme/migration.sql
+++ packages/db/prisma/db/migrations/20240926145252_add_profile_theme/migration.sql
@@ -1,9 +1,9 @@
 -- CreateTable
-CREATE TABLE "ProfilePersonalization" (
+CREATE TABLE "ProfileTheme" (
     "id" TEXT NOT NULL,
     "overviewFontStyle" TEXT,
     "publicationFontStyle" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "ProfilePersonalization_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "ProfileTheme_pkey" PRIMARY KEY ("id")
 );
