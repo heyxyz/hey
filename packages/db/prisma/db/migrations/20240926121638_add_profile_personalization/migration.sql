eee06b3d6a6792cc2ebf337b3c9ece4621fe2eb3
Yoginth <hey@yoginth.com>
2024-09-26 17:52:45 +0530

    Update profile theme in publication and profile details

--- packages/db/prisma/db/migrations/20240926121638_add_profile_personalization/migration.sql
+++ packages/db/prisma/db/migrations/20240926121638_add_profile_personalization/migration.sql
@@ -1,8 +1,8 @@
 -- CreateTable
 CREATE TABLE "ProfilePersonalization" (
     "id" TEXT NOT NULL,
-    "overviewFontStyle" TEXT NOT NULL,
-    "publicationFontStyle" TEXT NOT NULL,
+    "overviewFontStyle" TEXT,
+    "publicationFontStyle" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "ProfilePersonalization_pkey" PRIMARY KEY ("id")
