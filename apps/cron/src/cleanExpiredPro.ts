1dc5f3f268e03aff1e7be10cd26f53c5e6eb7d7d
Yoginth <hey@yoginth.com>
2024-09-27 18:54:23 +0530

    Clean Redis cache while running cleanExpiredPro cron

--- apps/cron/src/cleanExpiredPro.ts
+++ apps/cron/src/cleanExpiredPro.ts
@@ -1,11 +1,24 @@
 import prisma from "@hey/db/prisma/db/client";
+import { delRedisBulk } from "@hey/db/redisClient";
 import logger from "@hey/helpers/logger";
 
 const cleanExpiredPro = async () => {
   try {
-    await prisma.pro.deleteMany({
-      where: { expiresAt: { lt: new Date() } }
+    const data = await prisma.pro.findMany({
+      where: { expiresAt: { lt: new Date() } },
+      select: { id: true }
     });
+
+    if (data.length === 0) {
+      logger.info("[Cron] cleanExpiredPro - No expired pro found");
+      return;
+    }
+
+    const ids = data.map((item) => item.id);
+    await prisma.pro.deleteMany({ where: { id: { in: ids } } });
+
+    const redisKeys = data.map((item) => `profile:${item.id}`);
+    await delRedisBulk(redisKeys);
 
     logger.info("[Cron] cleanExpiredPro - Cleaned up expired pro");
   } catch (error) {
