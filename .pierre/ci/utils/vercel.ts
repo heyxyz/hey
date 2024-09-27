597bde3d8b984d4658a96e60172541bc14c1e24b
Yoginth <hey@yoginth.com>
2024-09-27 16:30:40 +0530

    Add back GIT_COMMIT_SHA on staff bar and network requests

--- .pierre/ci/utils/vercel.ts
+++ .pierre/ci/utils/vercel.ts
@@ -27,7 +27,9 @@ export const Job =
     const { stdout } = await run(
       `${vercel} deploy --scope ${VERCEL_SCOPE} ${isProd ? "" : "--no-wait"} --yes ${
         isProd ? "--prod" : ""
-      } --no-color --token $VERCEL_ACCESS_TOKEN`,
+      } --no-color --token $VERCEL_ACCESS_TOKEN -e GIT_COMMIT_SHA=${
+        ctx.branch.latestSha
+      }`,
       { label: `Creating ${PROJECT_NAME} Deployment`, env }
     );
 
