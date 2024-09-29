e651ab999f949eaf2f868af929f849aa62492718
Yoginth <hey@yoginth.com>
2024-09-29 13:23:33 +0530

    Add design system for NumberedStat

--- packages/ui/src/NumberedStat.tsx
+++ packages/ui/src/NumberedStat.tsx
@@ -4,24 +4,15 @@ import { Card } from "./Card";
 import { H4 } from "./Typography";
 
 interface NumberedStatsProps {
-  action?: ReactNode;
   count: string;
   name: ReactNode;
   suffix?: string;
 }
 
-const NumberedStat: FC<NumberedStatsProps> = ({
-  action,
-  count,
-  name,
-  suffix
-}) => {
+const NumberedStat: FC<NumberedStatsProps> = ({ count, name, suffix }) => {
   return (
     <Card className="p-5" forceRounded>
-      <div className="flex items-center space-x-5">
-        <div>{name}</div>
-        {action && <div>{action}</div>}
-      </div>
+      <div>{name}</div>
       <H4 className="tracking-wide">
         {humanize(Number(count))} <span className="text-sm">{suffix}</span>
       </H4>
