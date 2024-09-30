8d8ee0420e7a0630b16f1395696ba27e53f99ae3
Bigint <69431456+bigint@users.noreply.github.com>
2024-09-30 13:43:31 +0530

    Add more test cases to getMentions helper

--- packages/helpers/getMentions.spec.ts
+++ packages/helpers/getMentions.spec.ts
@@ -1,9 +1,15 @@
 import { describe, expect, test } from "vitest";
 import getMentions from "./getMentions";
 
-describe("getAttachmentsData", () => {
+describe("getMentions", () => {
   test("should return an empty array for text without mentions", () => {
     const text = "This is a test message";
+    const result = getMentions(text);
+    expect(result).toEqual([]);
+  });
+
+  test("should return an empty array for an empty string", () => {
+    const text = "";
     const result = getMentions(text);
     expect(result).toEqual([]);
   });
@@ -25,6 +31,75 @@ describe("getAttachmentsData", () => {
 
   test("should return an array with multiple mentions", () => {
     const text = "Hello @lens/john, @hey/jane, and @tape/doe!";
+    const result = getMentions(text);
+    expect(result).toEqual([
+      {
+        profile: {},
+        snapshotHandleMentioned: {
+          fullHandle: "lens/john",
+          localName: "john"
+        },
+        stillOwnsHandle: true
+      },
+      {
+        profile: {},
+        snapshotHandleMentioned: {
+          fullHandle: "hey/jane",
+          localName: "jane"
+        },
+        stillOwnsHandle: true
+      },
+      {
+        profile: {},
+        snapshotHandleMentioned: {
+          fullHandle: "tape/doe",
+          localName: "doe"
+        },
+        stillOwnsHandle: true
+      }
+    ]);
+  });
+
+  test("should handle mentions with special characters", () => {
+    const text = "Hey @lens/john_doe!";
+    const result = getMentions(text);
+    expect(result).toEqual([
+      {
+        profile: {},
+        snapshotHandleMentioned: {
+          fullHandle: "lens/john_doe",
+          localName: "john_doe"
+        },
+        stillOwnsHandle: true
+      }
+    ]);
+  });
+
+  test("should handle multiple mentions in the same sentence", () => {
+    const text = "@lens/jane @hey/john_doe are you coming to the party?";
+    const result = getMentions(text);
+    expect(result).toEqual([
+      {
+        profile: {},
+        snapshotHandleMentioned: {
+          fullHandle: "lens/jane",
+          localName: "jane"
+        },
+        stillOwnsHandle: true
+      },
+      {
+        profile: {},
+        snapshotHandleMentioned: {
+          fullHandle: "hey/john_doe",
+          localName: "john_doe"
+        },
+        stillOwnsHandle: true
+      }
+    ]);
+  });
+
+  test("should return an array with multiple mentions with mixed cases", () => {
+    const text = "Hello @Lens/John, @Hey/Jane, and @Tape/Doe!";
     const result = getMentions(text);
     expect(result).toEqual([
       {
