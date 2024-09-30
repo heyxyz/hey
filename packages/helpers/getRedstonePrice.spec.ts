e78f498832b996c554045004e088c3a56d3daf55
Bigint <69431456+bigint@users.noreply.github.com>
2024-09-30 14:14:17 +0530

    Remove unused getRedstonePrice helper

--- packages/helpers/getRedstonePrice.spec.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, test } from "vitest";
-import getRedstonePrice from "./getRedstonePrice";
-
-describe("getRedstonePrice", () => {
-  test("should return symbol", async () => {
-    const price = await getRedstonePrice("USDC");
-    expect(price).toBeGreaterThan(0);
-  });
-});
