9b2aba6fa7192e127920e1252b5e2b9b450ba509
Yoginth <hey@yoginth.com>
2024-09-30 13:45:44 +0530

    Add more test cases to getMisuseDetails helper

--- packages/helpers/getMisuseDetails.spec.ts
+++ packages/helpers/getMisuseDetails.spec.ts
@@ -6,14 +6,18 @@ describe("getMisuseDetails", () => {
   test("should return scam details if found", () => {
     const id = "0x661b";
     const result = getMisuseDetails(id);
-
     expect(result).toEqual(misused[0]);
   });
 
-  test("should return null if scam details are not found", () => {
-    const id = "3";
+  test("should return null if ID does not match any record", () => {
+    const id = "non-existing-id";
     const result = getMisuseDetails(id);
+    expect(result).toBeNull();
+  });
 
+  test("should return null for an empty string ID", () => {
+    const id = "";
+    const result = getMisuseDetails(id);
     expect(result).toBeNull();
   });
 });
