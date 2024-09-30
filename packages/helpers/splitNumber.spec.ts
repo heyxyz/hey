b147ddb10dae0783e5db11e420a8f50f52a1659a
Bigint <69431456+bigint@users.noreply.github.com>
2024-09-30 20:43:25 +0530

    Add more test cases to splitNumber helper

--- packages/helpers/splitNumber.spec.ts
+++ packages/helpers/splitNumber.spec.ts
@@ -2,15 +2,16 @@ import { describe, expect, test } from "vitest";
 import splitNumber from "./splitNumber";
 
 describe("splitNumber", () => {
-  test("should split number equally", () => {
+  test("should split number equally when divisible", () => {
     expect(splitNumber(10, 2)).toEqual([5, 5]);
   });
 
-  test("should split numbers evenly and get remainder", () => {
+  test("should split number and distribute remainder correctly", () => {
     expect(splitNumber(7, 3)).toEqual([3, 2, 2]);
+    expect(splitNumber(11, 3)).toEqual([4, 4, 3]);
   });
 
-  test("should default to one part if parts is not specified", () => {
+  test("should return one part if parts is not specified", () => {
     expect(splitNumber(8)).toEqual([8]);
   });
 
@@ -18,15 +19,23 @@ describe("splitNumber", () => {
     expect(splitNumber()).toEqual([1]);
   });
 
-  test("should have at least one item in all parts", () => {
+  test("should distribute remainder correctly even when parts are more than num", () => {
     expect(splitNumber(4, 5)).toEqual([1, 1, 1, 1, 0]);
   });
 
-  test("should return array should always have the length of parts even when there is no exact split", () => {
+  test("should return array with correct length even when split is not exact", () => {
     expect(splitNumber(11, 3)).toEqual([4, 4, 3]);
   });
 
-  test("should return array should be sorted in descending order according to input parameters ", () => {
+  test("should return array in descending order when split and remainder is distributed", () => {
     expect(splitNumber(14, 4)).toEqual([4, 4, 3, 3]);
+  });
+
+  test("should return all zeroes if number is 0", () => {
+    expect(splitNumber(0, 5)).toEqual([0, 0, 0, 0, 0]);
+  });
+
+  test("should handle case where number is smaller than parts", () => {
+    expect(splitNumber(2, 5)).toEqual([1, 1, 0, 0, 0]);
   });
 });
