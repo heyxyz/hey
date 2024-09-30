2e5ac8dc620d24e32a6ac4e27fcf86c77f793594
Yoginth <hey@yoginth.com>
2024-09-30 20:27:00 +0530

    Add JSDoc for resolveEns

--- apps/web/src/hooks/useEnsName.ts
+++ apps/web/src/hooks/useEnsName.ts
@@ -1,4 +1,4 @@
-import { resolveEns } from "@hey/helpers/resolveEns";
+import resolveEns from "@hey/helpers/resolveEns";
 import { useQuery } from "@tanstack/react-query";
 
 interface UseEnsNameProps {
--- packages/helpers/resolveEns.ts
+++ packages/helpers/resolveEns.ts
@@ -1,7 +1,12 @@
 import { HEY_API_URL } from "@hey/data/constants";
 import axios from "axios";
 
-export const resolveEns = async (addresses: string[]) => {
+/**
+ * Resolves ENS names for the specified Ethereum addresses.
+ * @param addresses An array of Ethereum addresses.
+ * @returns An array of ENS names.
+ */
+const resolveEns = async (addresses: string[]) => {
   try {
     const response = await axios.post(`${HEY_API_URL}/ens`, {
       addresses: addresses.map((address) => address.split("/")[0])
@@ -11,3 +16,5 @@ export const resolveEns = async (addresses: string[]) => {
     return [];
   }
 };
+
+export default resolveEns;
