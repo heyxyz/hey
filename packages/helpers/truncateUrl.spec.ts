cb846238f4f7cdd464465a828c9832f7d1f2d772
Yoginth <hey@yoginth.com>
2024-09-30 20:49:39 +0530

    Add more test cases to truncateUrl helper

--- packages/helpers/truncateUrl.spec.ts
+++ packages/helpers/truncateUrl.spec.ts
@@ -23,4 +23,34 @@ describe("truncateUrl", () => {
     const mainnetUrl = "https://hey.xyz/long/pathname/test";
     expect(truncateUrl(mainnetUrl, 20)).toEqual("hey.xyz/long/pathname/test");
   });
+
+  test("should handle urls without www and https", () => {
+    const url = "http://example.com/foo/bar";
+    expect(truncateUrl(url, 30)).toEqual("example.com/foo/bar");
+  });
+
+  test("should handle urls with subdomains", () => {
+    const url = "https://sub.example.com/foo";
+    expect(truncateUrl(url, 20)).toEqual("sub.example.com/foo");
+  });
+
+  test("should handle very short urls", () => {
+    const url = "https://a.com";
+    expect(truncateUrl(url, 10)).toEqual("a.com");
+  });
+
+  test("should handle exactly max length urls", () => {
+    const url = "https://example.com/foo";
+    expect(truncateUrl(url, 17)).toEqual("example.com/foo");
+  });
+
+  test("should return the stripped url when it ends with *.hey.xyz", () => {
+    const heyUrl = "https://subdomain.hey.xyz/test/path";
+    expect(truncateUrl(heyUrl, 10)).toEqual("subdomain.hey.xyz/test/path");
+  });
+
+  test("should handle urls with query parameters", () => {
+    const url = "https://example.com/foo?param=value&other=foo";
+    expect(truncateUrl(url, 25)).toEqual("example.com/foo?param=va…");
+  });
 });
--- packages/helpers/truncateUrl.ts
+++ packages/helpers/truncateUrl.ts
@@ -9,12 +9,15 @@ const truncateUrl = (url: string, maxLength: number): string => {
   const strippedUrl = url
     .replace(/^(http|https):\/\//, "")
     .replace(/^www\./, "");
+
   if (new URL(url).hostname.endsWith("hey.xyz")) {
     return strippedUrl;
   }
+
   if (strippedUrl.length > maxLength) {
     return `${strippedUrl.substring(0, maxLength - 1)}…`;
   }
+
   return strippedUrl;
 };
 
