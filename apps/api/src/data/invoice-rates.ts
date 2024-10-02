a8e7f770cd8485310ea2fbea69238cd82b658cbb
Yoginth <hey@yoginth.com>
2024-10-02 12:35:01 +0530

    Remove unused invoice related stuffs

--- apps/api/src/data/invoice-rates.ts
+++
@@ -1,9 +0,0 @@
-const invoiceRates = [
-  { date: new Date("2024-02-01"), rate: 664 },
-  { date: new Date("2024-03-01"), rate: 664 },
-  { date: new Date("2024-04-01"), rate: 648 },
-  { date: new Date("2024-05-01"), rate: 640 },
-  { date: new Date("2024-06-01"), rate: 560 }
-];
-
-export default invoiceRates;
