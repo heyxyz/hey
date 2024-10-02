a09b2fcd505cc24a58cb6686bc102d04c42fcb7e
Yoginth <hey@yoginth.com>
2024-10-02 21:51:32 +0530

    Remove super follow revenue from profile stats

--- apps/web/src/components/Profile/Stats/index.tsx
+++ apps/web/src/components/Profile/Stats/index.tsx
@@ -1,6 +1,5 @@
 import type { FC } from "react";
 import ProfileStats from "./ProfileStats";
-import SuperFollowRevenue from "./SuperFollowRevenue";
 
 interface StatsProps {
   profileId: string;
@@ -10,7 +9,6 @@ const Stats: FC<StatsProps> = ({ profileId }) => {
   return (
     <div className="space-y-5">
       <ProfileStats profileId={profileId} />
-      <SuperFollowRevenue profileId={profileId} />
     </div>
   );
 };
