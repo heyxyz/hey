3a7512daee2878b08c1d1188109e8dc830683d62
Yoginth <hey@yoginth.com>
2024-10-04 14:24:22 +0530

    Add start-server-and-test for running e2e tests

--- .pierre/ci/test.ts
+++ .pierre/ci/test.ts
@@ -5,34 +5,15 @@ export const label = "Test";
 const apiTest = async () => {
   const DATABASE_URL = process.env.TEST_DATABASE_URL as string;
 
+  await run("cd packages/db && pnpm prisma:clean", {
+    label: "Cleaning Test DB",
+    env: { DATABASE_URL }
+  });
+
   await run("cd packages/db && pnpm prisma:seed", {
     label: "Seeding Test DB",
     env: { DATABASE_URL }
   });
-
-  await run("cd apps/api && pnpm dev &", {
-    label: "Running API in background",
-    env: { DATABASE_URL }
-  });
-
-  let attempts = 10;
-  while (attempts > 0) {
-    try {
-      await run("curl http://localhost:4784/health", {
-        label: "Checking API health"
-      });
-      console.log("API is healthy!");
-      break;
-    } catch {
-      attempts--;
-      console.log(`API not ready yet. Retrying... (${10 - attempts}/10)`);
-      await new Promise((resolve) => setTimeout(resolve, 3000)); // Wait for 3 seconds before retrying
-    }
-  }
-
-  if (attempts === 0) {
-    throw new Error("API failed to start in the given time.");
-  }
 
   await run("cd apps/api && pnpm test:dev", {
     label: "Running API tests",
