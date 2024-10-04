02c6e2700f3d16de58a5153e9cd203b12fd70222
Yoginth <hey@yoginth.com>
2024-10-04 11:41:00 +0530

    Add parallel tests in Pierre CI

--- .pierre/ci/test.ts
+++ .pierre/ci/test.ts
@@ -2,8 +2,34 @@ import { run } from "pierre";
 
 export const label = "Test";
 
-export default async () => {
-  await run("pnpm test:dev", {
-    label: "Running tests"
+const apiTest = async () => {
+  await run("cd apps/api && pnpm test:dev", {
+    label: "Running API tests"
   });
 };
+
+const ogTest = async () => {
+  await run("cd packages/og && pnpm test:dev", {
+    label: "Running OG tests"
+  });
+};
+
+const webTest = async () => {
+  await run("cd apps/web && pnpm test:dev", {
+    label: "Running web tests"
+  });
+};
+
+const dataTest = async () => {
+  await run("cd packages/data && pnpm test:dev", {
+    label: "Running data tests"
+  });
+};
+
+const helpersTest = async () => {
+  await run("cd packages/helpers && pnpm test:dev", {
+    label: "Running helpers tests"
+  });
+};
+
+export default [apiTest, ogTest, webTest, dataTest, helpersTest];
