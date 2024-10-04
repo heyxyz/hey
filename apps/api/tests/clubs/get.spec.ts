e5ffd1ed0dfc0e8b21eb39957fcef0f3c9f4fbaf
Yoginth <69431456+bigint@users.noreply.github.com>
2024-10-04 15:55:14 +0530

    Add POST /clubs/get test scripts on API

---
+++ apps/api/tests/clubs/get.spec.ts
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { TEST_URL } from "src/helpers/constants";
+import { describe, expect, test } from "vitest";
+
+describe("POST /clubs/get", () => {
+  test("should return 400 if no body is provided", async () => {
+    try {
+      await axios.post(`${TEST_URL}/clubs/get`, {});
+    } catch (error: any) {
+      expect(error.response.status).toBe(400);
+    }
+  });
+
+  test("should return 400 for invalid body", async () => {
+    try {
+      await axios.post(`${TEST_URL}/clubs/get`, {
+        club_handle: "test_club",
+        limit: 100
+      });
+    } catch (error: any) {
+      expect(error.response.status).toBe(400);
+    }
+  });
+
+  test("should return 200 and fetch clubs for valid body", async () => {
+    const { data, status } = await axios.post(`${TEST_URL}/clubs/get`, {
+      club_handle: "bonsai",
+      profile_id: "0x0d"
+    });
+
+    expect(status).toBe(200);
+    expect(data.data.items[0]).toMatchObject({
+      id: "65e6dec26d85271723b6357c",
+      handle: "bonsai",
+      role: "member",
+      totalMembers: expect.any(Number)
+    });
+  });
+
+  test("should return 500 on server error", async () => {
+    try {
+      await axios.post(`${TEST_URL}/clubs/get`, {
+        club_handle: "non_existent_club",
+        limit: 10
+      });
+    } catch (error: any) {
+      expect(error.response.status).toBe(500);
+    }
+  });
+});
