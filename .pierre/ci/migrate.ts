dfcfa91335ff09a1b7ff286bf4047912fbeccaf1
Yoginth <hey@yoginth.com>
2024-10-04 12:14:07 +0530

    Add parallel DB migration

--- .pierre/ci/migrate.ts
+++ .pierre/ci/migrate.ts
@@ -2,7 +2,7 @@ import { run } from "pierre";
 
 export const label = "Migrate DB";
 
-export default async ({ branch }) => {
+const migrateProductionDb = async ({ branch }) => {
   if (branch.name !== "main") {
     await run('echo "Skipping DB Migration on non-main branches 🚫"', {
       label: "Skipping DB Migration"
@@ -12,10 +12,20 @@ export default async ({ branch }) => {
       label: "Migrating Production DB",
       env: { DATABASE_URL: process.env.PRODUCTION_DATABASE_URL as string }
     });
+  }
+};
 
+const migrateTestDb = async ({ branch }) => {
+  if (branch.name !== "main") {
+    await run('echo "Skipping DB Migration on non-main branches 🚫"', {
+      label: "Skipping DB Migration"
+    });
+  } else {
     await run("cd packages/db && pnpm prisma:migrate", {
       label: "Migrating Test DB",
       env: { DATABASE_URL: process.env.TEST_DATABASE_URL as string }
     });
   }
 };
+
+export default [migrateProductionDb, migrateTestDb];
