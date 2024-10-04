7487a8f76e3acd7243630d51be79c79217ea9263
Bigint <69431456+bigint@users.noreply.github.com>
2024-10-04 21:52:01 +0530

    Add seedProfilePermission util

---
+++ packages/db/prisma/db/seed/seedPermissions.ts
@@ -0,0 +1,22 @@
+import { Permission, PermissionId } from "@hey/data/permissions";
+import prisma from "../client";
+
+const seedPermissions = async (): Promise<number> => {
+  // Delete all permissions
+  await prisma.permission.deleteMany();
+
+  // Seed permissions
+  const permissions = await prisma.permission.createMany({
+    data: [
+      { id: PermissionId.CommentSuspended, key: Permission.CommentSuspended },
+      { id: PermissionId.Suspended, key: Permission.Suspended },
+      { id: PermissionId.Verified, key: Permission.Verified },
+      { id: PermissionId.StaffPick, key: Permission.StaffPick },
+      { id: PermissionId.Beta, key: Permission.Beta }
+    ]
+  });
+
+  return permissions.count;
+};
+
+export default seedPermissions;
