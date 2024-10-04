28a576941e36eb0fd84521aadccdcbd4cb0d108b
Yoginth <hey@yoginth.com>
2024-10-04 17:51:19 +0530

    Fix getFavicon breaking the entire site

--- packages/helpers/getFavicon.ts
+++ packages/helpers/getFavicon.ts
@@ -3,14 +3,18 @@ import urlcat from "urlcat";
 /**
  * Get favicon from google
  * @param url The url to get the favicon from.
- * @returns The favicon url.
+ * @returns The favicon url or null if the URL is invalid.
  */
 const getFavicon = (url: string) => {
-  const { hostname } = new URL(url);
+  try {
+    const { hostname } = new URL(url);
 
-  return urlcat("https://external-content.duckduckgo.com/ip3/:domain.ico", {
-    domain: hostname || "unknowndomain"
-  });
+    return urlcat("https://external-content.duckduckgo.com/ip3/:domain.ico", {
+      domain: hostname || "unknowndomain"
+    });
+  } catch {
+    return "https://external-content.duckduckgo.com/ip3/unknowndomain.ico";
+  }
 };
 
 export default getFavicon;
