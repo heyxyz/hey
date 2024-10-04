95e9bfc35bb23a84e47b507be4fb98560d6a9e85
Yoginth <hey@yoginth.com>
2024-10-04 06:45:40 +0000

    🔧 Add parallel tests in Pierre CI (#yoginth/eng-139-add-parallel-tests-in-pierre-ci)
    Summary: Refactored test execution in Pierre CI and added environment variable logging.
    
    Highlights:
    
    • Added `console.log` for `process.env.TEST_DATABASE_URL` in `migrate.ts`.
    • Split test execution into separate functions for API, OG, Web, Data, and Helpers in `test.ts`.
    • Exported an array of test functions in `test.ts` for parallel execution.
    
    Read more: https://pierre.co/hey/hey/yoginth/eng-139-add-parallel-tests-in-pierre-ci

--- .pierre/ci/migrate.ts
+++ .pierre/ci/migrate.ts
@@ -2,7 +2,7 @@ import { run } from "pierre";
 
 export const label = "Migrate DB";
 
-export default async ({ branch }) => {
+const migrateProductionDb = async ({ branch }) => {
   if (branch.name !== "main") {
     await run('echo "Skipping DB Migration on non-main branches 🚫"', {
       label: "Skipping DB Migration"
@@ -10,12 +10,22 @@ export default async ({ branch }) => {
   } else {
     await run("cd packages/db && pnpm prisma:migrate", {
       label: "Migrating Production DB",
-      env: { DATABASE_URL: process.env.DATABASE_URL as string }
+      env: { DATABASE_URL: process.env.PRODUCTION_DATABASE_URL as string }
     });
+  }
+};
 
+const migrateTestDb = async ({ branch }) => {
+  if (branch.name !== "main") {
+    await run('echo "Skipping DB Migration on non-main branches 🚫"', {
+      label: "Skipping DB Migration"
+    });
+  } else {
     await run("cd packages/db && pnpm prisma:migrate", {
       label: "Migrating Test DB",
       env: { DATABASE_URL: process.env.TEST_DATABASE_URL as string }
     });
   }
 };
+
+export default [migrateProductionDb, migrateTestDb];
--- .pierre/ci/test.ts
+++ .pierre/ci/test.ts
@@ -2,8 +2,35 @@ import { run } from "pierre";
 
 export const label = "Test";
 
-export default async () => {
-  await run("pnpm test:dev", {
-    label: "Running tests"
+const apiTest = async () => {
+  await run("cd apps/api && pnpm test:dev", {
+    label: "Running API tests",
+    env: { DATABASE_URL: process.env.PRODUCTION_DATABASE_URL as string }
   });
 };
+
+const ogTest = async () => {
+  await run("cd apps/og && pnpm test:dev", {
+    label: "Running OG tests"
+  });
+};
+
+const webTest = async () => {
+  await run("cd apps/web && pnpm test:dev", {
+    label: "Running Web tests"
+  });
+};
+
+const dataTest = async () => {
+  await run("cd packages/data && pnpm test:dev", {
+    label: "Running Data tests"
+  });
+};
+
+const helpersTest = async () => {
+  await run("cd packages/helpers && pnpm test:dev", {
+    label: "Running Helpers tests"
+  });
+};
+
+export default [apiTest, ogTest, webTest, dataTest, helpersTest];
