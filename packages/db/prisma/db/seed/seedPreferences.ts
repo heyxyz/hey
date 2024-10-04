3ce40a37e03d4003bea700786fc0b4afb59028d4
Bigint <69431456+bigint@users.noreply.github.com>
2024-10-04 14:09:34 +0530

    Drop DBs before seeding

--- packages/db/prisma/db/seed/seedPreferences.ts
+++ packages/db/prisma/db/seed/seedPreferences.ts
@@ -1,6 +1,10 @@
 import prisma from "../client";
 
 const seedPreferences = async (): Promise<number> => {
+  // Delete all preferences
+  await prisma.preference.deleteMany();
+
+  // Seed preferences
   const preferences = await prisma.preference.createMany({
     data: [{ id: "0x0d" }]
   });
