ba443554b24446dd1e44877d238b40770fd1bfc4
Yoginth <hey@yoginth.com>
2024-10-05 14:43:13 +0530

    Fix failing tests because of theme and status reset

--- apps/api/tests/profile/get.spec.ts
+++ apps/api/tests/profile/get.spec.ts
@@ -3,16 +3,12 @@ import {
   TEST_PRO_LENS_ID,
   TEST_SUSPENDED_LENS_ID
 } from "@hey/data/constants";
+import { delRedis } from "@hey/db/redisClient";
 import axios from "axios";
 import { TEST_URL } from "tests/helpers/constants";
-import { afterAll, describe, expect, test } from "vitest";
-import setTestId from "../helpers/setTestId";
+import { describe, expect, test } from "vitest";
 
 describe("GET /profile/get", () => {
-  afterAll(async () => {
-    await setTestId("GET /profile/get");
-  });
-
   test("should return 400 if no id is provided", async () => {
     try {
       await axios.get(`${TEST_URL}/profile/get`);
@@ -34,9 +30,12 @@ describe("GET /profile/get", () => {
   });
 
   test("should return 200 with pro, status, and theme if pro", async () => {
+    await delRedis(`profile:${TEST_PRO_LENS_ID}`);
     const { data, status } = await axios.get(`${TEST_URL}/profile/get`, {
       params: { id: TEST_PRO_LENS_ID }
     });
+
+    console.log(data);
 
     expect(status).toBe(200);
     expect(data.result.pro.isPro).toBeTruthy();
