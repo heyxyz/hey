a9bc7a86db233960d5b0a2fbb37f4ebc710072af
Yoginth <69431456+bigint@users.noreply.github.com>
2024-10-05 07:23:37 +0000

    ✅ Add API E2E test cases: v4 (#yoginth/eng-140-add-api-e2e-test-cases)
    Summary: Added test scripts for `POST /profile/theme/update` and updated authentication handling.
    
    Highlights:
    
    • Introduced `TEST_PRO_AUTH_TOKEN` and `TEST_SUSPENDED_AUTH_TOKEN` in `.env.example` and `env.d.ts`.
    • Enhanced `getTestAuthHeaders` to support "pro" and "suspended" user types.
    • Added `update.spec.ts` for testing profile theme updates with various user roles.
    
    Read more: https://pierre.co/hey/hey/yoginth/eng-140-add-api-e2e-test-cases

--- apps/api/tests/helpers/getTestAuthHeaders.ts
+++ apps/api/tests/helpers/getTestAuthHeaders.ts
@@ -1,10 +1,27 @@
 const TEST_AUTH_TOKEN = process.env.TEST_AUTH_TOKEN;
+const TEST_PRO_AUTH_TOKEN = process.env.TEST_PRO_AUTH_TOKEN;
+const TEST_SUSPENDED_AUTH_TOKEN = process.env.TEST_SUSPENDED_AUTH_TOKEN;
 
-const getTestAuthHeaders = () => {
-  return {
-    "X-Access-Token": TEST_AUTH_TOKEN,
-    "X-Identity-Token": TEST_AUTH_TOKEN
-  };
+const getTestAuthHeaders = (
+  type: "default" | "pro" | "suspended" = "default"
+) => {
+  switch (type) {
+    case "pro":
+      return {
+        "X-Access-Token": TEST_PRO_AUTH_TOKEN,
+        "X-Identity-Token": TEST_PRO_AUTH_TOKEN
+      };
+    case "suspended":
+      return {
+        "X-Access-Token": TEST_SUSPENDED_AUTH_TOKEN,
+        "X-Identity-Token": TEST_SUSPENDED_AUTH_TOKEN
+      };
+    default:
+      return {
+        "X-Access-Token": TEST_AUTH_TOKEN,
+        "X-Identity-Token": TEST_AUTH_TOKEN
+      };
+  }
 };
 
 export default getTestAuthHeaders;
