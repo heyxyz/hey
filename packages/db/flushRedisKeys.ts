ad11cf9bf75266b76ea05d72ff3019b9cf62bd04
Bigint <69431456+bigint@users.noreply.github.com>
2024-10-05 11:17:12 +0530

    Use generateForeverExpiry for ping after flushing

--- packages/db/flushRedisKeys.ts
+++ packages/db/flushRedisKeys.ts
@@ -1,6 +1,6 @@
-import { exit } from "node:process";
 import logger from "@hey/helpers/logger";
-import redisClient, { setRedis } from "./redisClient";
+import { exit } from "node:process";
+import redisClient, { generateForeverExpiry, setRedis } from "./redisClient";
 
 const flushRedisKeys = async () => {
   if (!redisClient) {
@@ -16,8 +16,7 @@ const flushRedisKeys = async () => {
 
     await redisClient.flushAll();
     logger.info("All Redis keys have been deleted");
-
-    await setRedis("ping", "ping");
+    await setRedis("ping", "ping", generateForeverExpiry());
     logger.info("[Redis] Ping key set");
   } catch (error) {
     logger.error("Error deleting Redis keys", error);
