a0716c33fb575de334f2781d8fbd0592681e7efb
Sagar <sagar@hey.xyz>
2024-10-05 09:33:43 +0000

    🚀 Ship Hey Pro to public (#yoginth/eng-141-ship-hey-pro-to-public)
    Summary: The branch removes feature flag checks for the Pro feature and updates UI elements related to the Pro page.
    
    Highlights:
    
    • Removed `proEnabled` feature flag checks from `MobileDrawerMenu` and `SignedProfile`.
    • Updated aspect ratio and copy in `Pro/index.tsx`.
    • Deleted `Pro` from `FeatureFlag` enum in `feature-flags.ts`.
    
    Details:
    
    “Exciting News! Introducing @lens/hey **Pro**
    
    Unlock these premium features:
    
    ✨ Extended Analytics
    
    ✨ Choose your app icon
    
    ✨ Set Profile Status
    
    ✨ Personalized profile page
    
    ✨ Higher video & audio upload limits
    
    ✨ Pro Badge on your profile
    
    ✨ Early access to new features
    
    ✨ Priority support
    
    **Upgrade now for $5 / month** at <https://hey.xyz/pro>”
    
    Read more: https://pierre.co/hey/hey/yoginth/eng-141-ship-hey-pro-to-public

--- apps/web/src/components/Pro/index.tsx
+++ apps/web/src/components/Pro/index.tsx
@@ -32,7 +32,7 @@ const Pro: NextPage = () => {
     <div className="px-6 py-20">
       <div className="-z-10 absolute inset-x-0 blur-3xl">
         <div
-          className="mx-auto aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-brand-300 to-purple-300 opacity-30"
+          className="mx-auto aspect-[1000/250] bg-gradient-to-tr from-brand-300 to-purple-300 opacity-30"
           style={{
             clipPath:
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
@@ -42,8 +42,7 @@ const Pro: NextPage = () => {
       <div className="text-center font-bold">
         <H3 className="text-brand-500">Upgrade to Pro</H3>
         <H4 className="mt-4">
-          Enjoy an enhanced experience of {APP_NAME}, exclusive creator tools,
-          and more.
+          Enjoy an enhanced experience of {APP_NAME} Pro.
         </H4>
       </div>
       <H5 className="ld-text-gray-500 mx-auto mt-4 max-w-2xl text-center font-normal leading-7">
