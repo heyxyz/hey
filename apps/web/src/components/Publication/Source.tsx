2f2bb5a965db99ae92a4af5d08f2472a895f3ffb
Yoginth <69431456+bigint@users.noreply.github.com>
2024-10-06 06:21:37 +0000

    🗑️ Remove known apps icons except Hey (#yoginth/eng-147-remove-known-apps-icons-except-hey)
    Summary: Refactored `Source.tsx` to simplify app icon logic and remove unused imports.
    
    Highlights:
    
    • Removed `apps`, `getAppName`, and `Tooltip` imports, simplifying dependencies.
    • Replaced `apps.includes` check with `publishedOn
    
    Read more: https://pierre.co/hey/hey/yoginth/eng-147-remove-known-apps-icons-except-hey

--- apps/web/src/components/Publication/Source.tsx
+++ apps/web/src/components/Publication/Source.tsx
@@ -1,8 +1,5 @@
-import { apps } from "@hey/data/apps";
-import { STATIC_IMAGES_URL } from "@hey/data/constants";
+import { APP_NAME, STATIC_IMAGES_URL } from "@hey/data/constants";
 import { FeatureFlag } from "@hey/data/feature-flags";
-import getAppName from "@hey/helpers/getAppName";
-import { Tooltip } from "@hey/ui";
 import { useFlag } from "@unleash/proxy-client-react";
 import type { FC } from "react";
 
@@ -12,25 +9,21 @@ interface SourceProps {
 
 const Source: FC<SourceProps> = ({ publishedOn }) => {
   const enabled = useFlag(FeatureFlag.Staff);
-  const show = publishedOn && apps.includes(publishedOn);
+  const show = publishedOn === APP_NAME.toLowerCase();
 
   if (!enabled || !show) {
     return null;
   }
 
-  const appName = getAppName(publishedOn);
-
   return (
     <span className="ld-text-gray-500 flex items-center">
       <span className="mx-1">·</span>
-      <Tooltip content={appName} placement="top">
-        <img
-          alt={appName}
-          className="mt-0.5 h-3.5 rounded-sm"
-          height={14}
-          src={`${STATIC_IMAGES_URL}/source/${publishedOn}.png`}
-        />
-      </Tooltip>
+      <img
+        alt="Logo"
+        className="mt-0.5 h-3.5 rounded-sm"
+        height={14}
+        src={`${STATIC_IMAGES_URL}/app-icon/0.png`}
+      />
     </span>
   );
 };
