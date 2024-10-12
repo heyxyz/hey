e3076b854a2162c2f207b6f68d17d232219f1a65
Yoginth <69431456+bigint@users.noreply.github.com>
2024-10-12 18:58:57 +0530

    Add POST /clubs/leave test cases on API

---
+++ apps/api/tests/clubs/leave.spec.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { TEST_URL } from "tests/helpers/constants";
+import { describe, expect, test } from "vitest";
+
+describe("POST /clubs/leave", () => {
+  test("should return 401 if the identity token is missing", async () => {
+    try {
+      await axios.post(
+        `${TEST_URL}/clubs/leave`,
+        { id: "validClubId" },
+        { headers: {} } // No identity token provided
+      );
+    } catch (error: any) {
+      expect(error.response.status).toBe(401);
+    }
+  });
+});
