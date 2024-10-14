0a30b9941b9826295c3fb86205ab58a5b56ca8e5
Yoginth <hey@yoginth.com>
2024-10-14 03:39:47 +0000

    🔧 Move to self-hosted Clickhouse server (#yoginth/eng-198)
    Summary: Updated Clickhouse server URL to a self-hosted instance.
    
    Highlights:
    
    • Changed `CLICKHOUSE_URL` in `.env.example` files for `api` and `cron` apps.
    • Removed `username` field from `clickhouseClient` configuration in `clickhouseClient.ts`.
    
    Read more: https://pierre.co/hey/hey/yoginth/eng-198

--- packages/db/clickhouseClient.ts
+++ packages/db/clickhouseClient.ts
@@ -7,8 +7,7 @@ const clickhouseClient = createClient({
   compression: { request: true, response: true },
   keep_alive: { enabled: true },
   password: process.env.CLICKHOUSE_PASSWORD,
-  url: process.env.CLICKHOUSE_URL,
-  username: "clickhouse"
+  url: process.env.CLICKHOUSE_URL
 });
 
 export default clickhouseClient;
