4c7a934e19d5c921d605c948a7494224e6227ed1
Bigint <69431456+bigint@users.noreply.github.com>
2024-10-16 16:02:04 +0530

    Add SuspendWarning banner on FE

--- apps/web/src/components/Shared/GlobalBanners/index.tsx
+++ apps/web/src/components/Shared/GlobalBanners/index.tsx
@@ -1,11 +1,13 @@
 import type { FC } from "react";
 import ProtectProfile from "./ProtectProfile";
+import SuspendWarning from "./SuspendWarning";
 import Suspended from "./Suspended";
 
 const GlobalBanners: FC = () => {
   return (
     <>
       <Suspended />
+      <SuspendWarning />
       <ProtectProfile />
     </>
   );
