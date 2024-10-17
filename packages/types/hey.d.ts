28db1ce21120601b908a96663de99a4631c35343
Yoginth <hey@yoginth.com>
2024-10-17 12:53:43 +0530

    Use clickhouse for caching AI moderation

--- packages/types/hey.d.ts
+++ packages/types/hey.d.ts
@@ -85,3 +85,34 @@ export type ProfileDetails = {
   status: { emoji: string; message: string } | null;
   theme: ProfileTheme | null;
 };
+
+export type Moderation = {
+  id: string;
+  flagged: boolean;
+  harassment: boolean;
+  harassment_threatening: boolean;
+  sexual: boolean;
+  hate: boolean;
+  hate_threatening: boolean;
+  illicit: boolean;
+  illicit_violent: boolean;
+  self_harm_intent: boolean;
+  self_harm_instructions: boolean;
+  self_harm: boolean;
+  sexual_minors: boolean;
+  violence: boolean;
+  violence_graphic: boolean;
+  harassment_score: number;
+  harassment_threatening_score: number;
+  sexual_score: number;
+  hate_score: number;
+  hate_threatening_score: number;
+  illicit_score: number;
+  illicit_violent_score: number;
+  self_harm_intent_score: number;
+  self_harm_instructions_score: number;
+  self_harm_score: number;
+  sexual_minors_score: number;
+  violence_score: number;
+  violence_graphic_score: number;
+};
