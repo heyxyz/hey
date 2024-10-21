94d93258f011e86067f5ee5d7a8bf1e3c9a8252a
Bigint <69431456+bigint@users.noreply.github.com>
2024-10-21 19:47:02 +0530

    Remove dark mode to ProfileTheme

---
+++ packages/db/prisma/db/migrations/20241021141655_remove_dark_mode/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `darkMode` on the `ProfileTheme` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ProfileTheme" DROP COLUMN "darkMode";
