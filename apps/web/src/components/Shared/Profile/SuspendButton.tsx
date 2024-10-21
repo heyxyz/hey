0a3562d0f1a2d0ff928382370b7be940f96594d9
Bigint <69431456+bigint@users.noreply.github.com>
2024-10-21 13:35:32 +0530

    Add report to suspend button

--- apps/web/src/components/Shared/Profile/SuspendButton.tsx
+++ apps/web/src/components/Shared/Profile/SuspendButton.tsx
@@ -1,10 +1,14 @@
-import { getAuthApiHeaders } from "@helpers/getAuthApiHeaders";
+import {
+  getAuthApiHeaders,
+  getAuthApiHeadersWithAccessToken
+} from "@helpers/getAuthApiHeaders";
 import { HEY_API_URL } from "@hey/data/constants";
 import { FeatureFlag } from "@hey/data/feature-flags";
 import { Permission, PermissionId } from "@hey/data/permissions";
 import getInternalProfile from "@hey/helpers/api/getInternalProfile";
+import { PublicationReportingSpamSubreason } from "@hey/lens";
 import { Button } from "@hey/ui";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useFlag } from "@unleash/proxy-client-react";
 import axios from "axios";
 import type { FC } from "react";
@@ -16,6 +20,7 @@ interface SuspendButtonProps {
 
 const SuspendButton: FC<SuspendButtonProps> = ({ id }) => {
   const isStaff = useFlag(FeatureFlag.Staff);
+  const queryClient = useQueryClient();
 
   const { data: profile } = useQuery({
     queryFn: () => getInternalProfile(id, getAuthApiHeaders()),
@@ -29,15 +34,34 @@ const SuspendButton: FC<SuspendButtonProps> = ({ id }) => {
 
   const suspendProfile = () => {
     toast.promise(
-      axios.post(
-        `${HEY_API_URL}/internal/permissions/assign`,
-        { enabled: true, id: PermissionId.Suspended, profile_id: id },
-        { headers: getAuthApiHeaders() }
-      ),
+      Promise.all([
+        axios.post(
+          `${HEY_API_URL}/internal/permissions/assign`,
+          { enabled: true, id: PermissionId.Suspended, profile_id: id },
+          { headers: getAuthApiHeaders() }
+        ),
+        axios.post(
+          `${HEY_API_URL}/internal/gardener/report`,
+          {
+            id: `${id}-0x01`,
+            subreasons: [
+              PublicationReportingSpamSubreason.FakeEngagement,
+              PublicationReportingSpamSubreason.LowSignal,
+              PublicationReportingSpamSubreason.Misleading
+            ]
+          },
+          { headers: getAuthApiHeadersWithAccessToken() }
+        )
+      ]),
       {
         error: "Error suspending profile",
         loading: "Suspending profile...",
-        success: "Profile suspended"
+        success: () => {
+          queryClient.invalidateQueries({
+            queryKey: ["getInternalProfile", id]
+          });
+          return "Profile suspended";
+        }
       }
     );
   };
