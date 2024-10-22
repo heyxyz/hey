4231b7b37a6d147a594b336b4a036068e6205078
Yoginth <69431456+bigint@users.noreply.github.com>
2024-10-22 03:37:12 +0000

    🔧 Migrate to Next.js 15 (#update-dependencies-20241022081416)
    Summary: Updated dependencies and refactored React components to use arrow functions.
    
    Highlights:
    
    • Upgraded `@types/node` to `^22.7.8` across multiple `package.json` files.
    • Updated `next` to `^15.0.0` in `apps/og`, `apps/web`, and `packages/ui`.
    • Refactored `RootLayout`, `Home`, and `Page` components to arrow functions.
    
    Read more: https://pierre.co/hey/hey/update-dependencies-20241022081416

--- apps/og/src/app/layout.tsx
+++ apps/og/src/app/layout.tsx
@@ -4,10 +4,12 @@ import defaultMetadata from "src/defaultMetadata";
 
 export const metadata: Metadata = defaultMetadata;
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
   );
-}
+};
+
+export default RootLayout;
