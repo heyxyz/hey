ca7d973ee987114841a93448e232e57f92a3d113
Yoginth <hey@yoginth.com>
2024-10-23 08:39:31 +0000

    🌱 Add DB seeds for Hey Lists (#yoginth/eng-223)
    Summary: Added database seeding for lists in the Hey application.
    
    Highlights:
    
    • Introduced `seedLists` function to seed list data into the database.
    • Updated `index.ts` to include `seedLists` in the main seeding process.
    • Created `seedLists.ts` to define list data and seeding logic.
    
    Read more: https://pierre.co/hey/hey/yoginth/eng-223

---
+++ packages/db/prisma/db/seed/seedLists.ts
@@ -0,0 +1,27 @@
+import { LENS_MEDIA_SNAPSHOT_URL } from "@hey/data/constants";
+import prisma from "../client";
+
+const seedLists = async (): Promise<number> => {
+  // Delete all lists
+  await prisma.list.deleteMany();
+
+  // Seed lists
+  const lists = await prisma.list.createMany({
+    data: [
+      {
+        name: "Hey Team",
+        description: "The team behind Hey",
+        avatar: `${LENS_MEDIA_SNAPSHOT_URL}/5cffa71e6bb1c1a9bf829a10fae9b52d41dee8b7883dc7ba08271aabfdeaf488.png`
+      },
+      {
+        name: "Lens Team",
+        description: "The team behind Lens",
+        avatar: `${LENS_MEDIA_SNAPSHOT_URL}/bd59a48037d84e558706a277a44dff8ac277629ab12804b1b19be9df254e036c.png`
+      }
+    ]
+  });
+
+  return lists.count;
+};
+
+export default seedLists;
