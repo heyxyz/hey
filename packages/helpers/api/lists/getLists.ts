79fe5623aa8d5046c565a4811cac63d658da63a8
Yoginth <69431456+bigint@users.noreply.github.com>
2024-10-23 22:27:44 +0530

    Fix getLists by adding viewingId

--- packages/helpers/api/lists/getLists.ts
+++ packages/helpers/api/lists/getLists.ts
@@ -2,10 +2,10 @@ import { HEY_API_URL } from "@hey/data/constants";
 import type { List } from "@hey/types/hey";
 import axios from "axios";
 
-const getLists = async (id: string): Promise<List[]> => {
+const getLists = async (id: string, viewingId?: string): Promise<List[]> => {
   try {
     const response = await axios.get(`${HEY_API_URL}/lists/all`, {
-      params: { id }
+      params: { id, viewingId }
     });
 
     return response.data?.result;
