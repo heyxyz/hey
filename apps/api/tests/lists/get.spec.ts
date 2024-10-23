f3cf64862fbc9977a7c17cc336ec74035b157fe2
Yoginth <69431456+bigint@users.noreply.github.com>
2024-10-23 14:47:40 +0530

    Add lists/get endpoint tests for Hey Lists

---
+++ apps/api/tests/lists/get.spec.ts
@@ -0,0 +1,38 @@
+import { faker } from "@faker-js/faker";
+import prisma from "@hey/db/prisma/db/client";
+import axios from "axios";
+import { TEST_URL } from "tests/helpers/constants";
+import getTestAuthHeaders from "tests/helpers/getTestAuthHeaders";
+import { describe, expect, test } from "vitest";
+
+describe("GET /lists/get", () => {
+  test("should return 400 if no id is provided", async () => {
+    try {
+      await axios.get(`${TEST_URL}/lists/get`, {
+        headers: getTestAuthHeaders()
+      });
+    } catch (error: any) {
+      expect(error.response.status).toBe(400);
+    }
+  });
+
+  test("should return 200 with a list", async () => {
+    const newList = await prisma.list.create({
+      data: {
+        name: faker.commerce.productName(),
+        description: faker.lorem.sentence(),
+        avatar: faker.image.url()
+      }
+    });
+
+    const { data, status } = await axios.get(`${TEST_URL}/lists/get`, {
+      params: { id: newList.id }
+    });
+
+    expect(status).toBe(200);
+    expect(data.result).toBeDefined();
+    expect(data.result.name).toBe(newList.name);
+    expect(data.result.description).toBe(newList.description);
+    expect(data.result.avatar).toBe(newList.avatar);
+  });
+});
