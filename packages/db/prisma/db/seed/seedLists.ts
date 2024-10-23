e94354805cf91d94b0f18112fbcf15004953c6d0
Yoginth <hey@yoginth.com>
2024-10-23 14:53:33 +0530

    Update list seeding with LENS_TEAM_ID

--- packages/db/prisma/db/seed/seedLists.ts
+++ packages/db/prisma/db/seed/seedLists.ts
@@ -2,6 +2,7 @@ import { LENS_MEDIA_SNAPSHOT_URL } from "@hey/data/constants";
 import prisma from "../client";
 
 export const HEY_TEAM_ID = "0c34a529-8db6-40b8-9b35-7f474f7d509a";
+export const LENS_TEAM_ID = "bd59a480-37d8-4e55-8706-a277a44dff8a";
 
 const seedLists = async (): Promise<number> => {
   // Delete all lists
@@ -17,6 +18,7 @@ const seedLists = async (): Promise<number> => {
         avatar: `${LENS_MEDIA_SNAPSHOT_URL}/5cffa71e6bb1c1a9bf829a10fae9b52d41dee8b7883dc7ba08271aabfdeaf488.png`
       },
       {
+        id: LENS_TEAM_ID,
         name: "Lens Team",
         description: "The team behind Lens",
         avatar: `${LENS_MEDIA_SNAPSHOT_URL}/bd59a48037d84e558706a277a44dff8ac277629ab12804b1b19be9df254e036c.png`
--- packages/db/prisma/db/seed/seedListsProfiles.ts
+++ packages/db/prisma/db/seed/seedListsProfiles.ts
@@ -1,5 +1,6 @@
+import { TEST_LENS_ID } from "@hey/data/constants";
 import prisma from "../client";
-import { HEY_TEAM_ID } from "./seedLists";
+import { HEY_TEAM_ID, LENS_TEAM_ID } from "./seedLists";
 
 const seedListsProfiles = async (): Promise<number> => {
   // Delete all lists profiles
@@ -10,7 +11,9 @@ const seedListsProfiles = async (): Promise<number> => {
     data: [
       { listId: HEY_TEAM_ID, profileId: "0x0d" },
       { listId: HEY_TEAM_ID, profileId: "0x2d" },
-      { listId: HEY_TEAM_ID, profileId: "0x3d" }
+      { listId: HEY_TEAM_ID, profileId: "0x3d" },
+      { listId: HEY_TEAM_ID, profileId: TEST_LENS_ID },
+      { listId: LENS_TEAM_ID, profileId: TEST_LENS_ID }
     ]
   });
 
