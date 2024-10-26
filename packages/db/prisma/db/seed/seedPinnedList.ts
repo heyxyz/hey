5aaf9b9a247e28a8dd1433932967acebd20fdc02
Yoginth <hey@yoginth.com>
2024-10-26 13:43:36 +0530

    Add GET /lists/pinned test cases

--- packages/db/prisma/db/seed/seedPinnedList.ts
+++ packages/db/prisma/db/seed/seedPinnedList.ts
@@ -1,3 +1,4 @@
+import { TEST_LENS_ID } from "@hey/data/constants";
 import prisma from "../client";
 import { HEY_TEAM_ID, LENS_TEAM_ID } from "./seedLists";
 
@@ -9,7 +10,9 @@ const seedPinnedList = async (): Promise<number> => {
   const pinnedList = await prisma.pinnedList.createMany({
     data: [
       { listId: HEY_TEAM_ID, profileId: "0x0d" },
-      { listId: LENS_TEAM_ID, profileId: "0x0d" }
+      { listId: LENS_TEAM_ID, profileId: "0x0d" },
+      { listId: HEY_TEAM_ID, profileId: TEST_LENS_ID },
+      { listId: LENS_TEAM_ID, profileId: TEST_LENS_ID }
     ]
   });
 
