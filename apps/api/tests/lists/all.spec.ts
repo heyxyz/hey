53dbf199c9eea13f9c260b5482df06e16e3cec70
Yoginth <hey@yoginth.com>
2024-10-26 13:39:54 +0530

    Add more tests to GET /lists/all

--- apps/api/tests/lists/all.spec.ts
+++ apps/api/tests/lists/all.spec.ts
@@ -25,4 +25,40 @@ describe("GET /lists/all", () => {
     expect(data.result.length).toBeGreaterThan(0);
     expect(data.result[0].count).toStrictEqual(expect.any(Number));
   });
+
+  test("should return 200 with isAdded as true if the user is added to the list", async () => {
+    const { data, status } = await axios.get(`${TEST_URL}/lists/all`, {
+      params: { ownerId: "0x0d", viewingId: TEST_LENS_ID }
+    });
+
+    expect(status).toBe(200);
+    expect(data.result[0].isAdded).toBe(true);
+  });
+
+  test("should return 200 with isAdded as false if the user is not added to the list", async () => {
+    const { data, status } = await axios.get(`${TEST_URL}/lists/all`, {
+      params: { ownerId: TEST_LENS_ID, viewingId: "0x0d" }
+    });
+
+    expect(status).toBe(200);
+    expect(data.result[0].isAdded).toBe(false);
+  });
+
+  test("should return 200 with pinned as true if the list is pinned", async () => {
+    const { data, status } = await axios.get(`${TEST_URL}/lists/all`, {
+      params: { ownerId: "0x0d" }
+    });
+
+    expect(status).toBe(200);
+    expect(data.result[0].pinned).toBe(true);
+  });
+
+  test("should return 200 with pinned as false if the list is not pinned", async () => {
+    const { data, status } = await axios.get(`${TEST_URL}/lists/all`, {
+      params: { ownerId: TEST_LENS_ID }
+    });
+
+    expect(status).toBe(200);
+    expect(data.result[0].pinned).toBe(false);
+  });
 });
