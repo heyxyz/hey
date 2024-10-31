abc3e3e9526592d85e9ab66e086b59127df9367c
bigint <69431456+bigint@users.noreply.github.com>
2024-10-31 17:27:31 +0530

    Refactor EncryptedPublication and PublicationBody components

--- apps/web/src/components/Publication/EncryptedPublication.tsx
+++ apps/web/src/components/Publication/EncryptedPublication.tsx
@@ -5,8 +5,8 @@ import type { FC } from "react";
 
 const EncryptedPublication: FC = () => {
   return (
-    <Card className="!bg-gray-100 dark:!bg-gray-800">
-      <div className="flex items-center space-x-1 px-4 py-3 text-sm">
+    <Card className="!bg-gray-100 dark:!bg-gray-800 mt-2 px-4 py-3">
+      <div className="flex items-center space-x-1 text-sm">
         <LockClosedIcon className="size-4 text-gray-500" />
         <span>Encrypted publication not supported on {APP_NAME}</span>
       </div>
