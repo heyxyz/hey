380bfdca3246280f2c59c51600f19ac61bda0d62
bigint <69431456+bigint@users.noreply.github.com>
2024-11-11 10:06:23 +0530

    Init new mobile app

---
+++ apps/mobile/components/ExternalLink.tsx
@@ -0,0 +1,24 @@
+import { type Href, Link } from "expo-router";
+import { openBrowserAsync } from "expo-web-browser";
+import type { ComponentProps } from "react";
+import { Platform } from "react-native";
+
+type Props = Omit<ComponentProps<typeof Link>, "href"> & { href: string };
+
+export function ExternalLink({ href, ...rest }: Props) {
+  return (
+    <Link
+      target="_blank"
+      {...rest}
+      href={href as Href<string | object>}
+      onPress={async (event) => {
+        if (Platform.OS !== "web") {
+          // Prevent the default behavior of linking to the default browser on native.
+          event.preventDefault();
+          // Open the link in an in-app browser.
+          await openBrowserAsync(href);
+        }
+      }}
+    />
+  );
+}
