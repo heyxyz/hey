c7375c91ed1f8b6ccc395f8cc159ef5fa4409cb6
Bigint <69431456+bigint@users.noreply.github.com>
2024-11-19 03:39:21 +0000

    📰 Rename publication to post for v3 preparation (#v3-pub-to-posts)
    Summary: Renamed "publication" to "post" and added a new API endpoint for fetching posts.
    
    Highlights:
    
    • Introduced `get` function in `posts.ts` to fetch posts with pagination.
    • Updated tests in `posts.spec.ts` to validate new API endpoint.
    • Changed variable names and labels from "publication" to "post" across components.
    
    Read more: https://pierre.co/hey/hey/v3-pub-to-posts

--- apps/web/src/helpers/pushToImpressions.ts
+++ apps/web/src/helpers/pushToImpressions.ts
@@ -2,16 +2,16 @@ import { IS_MAINNET } from "@hey/data/constants";
 import getCurrentSession from "./getCurrentSession";
 
 /**
- * Push publication to impressions queue
- * @param id Publication ID
+ * Push post to impressions queue
+ * @param id Post ID
  * @returns void
  */
 const pushToImpressions = (id: string): void => {
   const { id: sessionProfileId } = getCurrentSession();
 
   // Don't push impressions for the current user
-  const publicationProfileId = id.split("-")[0];
-  if (publicationProfileId === sessionProfileId) {
+  const postProfileId = id.split("-")[0];
+  if (postProfileId === sessionProfileId) {
     return;
   }
 
