d4da75ae27a104be28545e6a9ac6656bda17d00c
Yoginth <hey@yoginth.com>
2024-11-26 18:21:06 +0530

    Init lens v3

--- apps/web/src/components/Common/Providers/Web3Provider.tsx
+++ apps/web/src/components/Common/Providers/Web3Provider.tsx
@@ -1,8 +1,9 @@
+import { lensTestnet } from "@helpers/chains";
 import { APP_NAME, WALLETCONNECT_PROJECT_ID } from "@hey/data/constants";
-import { POLYGON_AMOY_RPCS, POLYGON_RPCS } from "@hey/data/rpcs";
+import { LENS_TESTNET_RPCS, POLYGON_RPCS } from "@hey/data/rpcs";
 import type { FC, ReactNode } from "react";
 import { http, WagmiProvider, createConfig, fallback } from "wagmi";
-import { polygon, polygonAmoy } from "wagmi/chains";
+import { polygon } from "wagmi/chains";
 import { coinbaseWallet, injected, walletConnect } from "wagmi/connectors";
 
 const connectors = [
@@ -12,11 +13,11 @@ const connectors = [
 ];
 
 const wagmiConfig = createConfig({
-  chains: [polygon, polygonAmoy],
+  chains: [polygon, lensTestnet],
   connectors,
   transports: {
     [polygon.id]: fallback(POLYGON_RPCS.map((rpc) => http(rpc))),
-    [polygonAmoy.id]: fallback(POLYGON_AMOY_RPCS.map((rpc) => http(rpc)))
+    [lensTestnet.id]: fallback(LENS_TESTNET_RPCS.map((rpc) => http(rpc)))
   }
 });
 
