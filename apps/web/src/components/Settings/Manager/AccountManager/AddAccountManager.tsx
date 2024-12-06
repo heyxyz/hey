d1c9a809c5e9d69a7c5775e56f3d0d591e4406b7
Yoginth <hey@yoginth.com>
2024-12-06 20:18:36 +0530

    Migrate to Lens v3

--- apps/web/src/components/Settings/Manager/AccountManager/AddAccountManager.tsx
+++ apps/web/src/components/Settings/Manager/AccountManager/AddAccountManager.tsx
@@ -2,6 +2,9 @@ import SearchAccounts from "@components/Shared/SearchAccounts";
 import errorToast from "@helpers/errorToast";
 import { ADDRESS_PLACEHOLDER } from "@hey/data/constants";
 import { Errors } from "@hey/data/errors";
+import selfFundedTransactionData from "@hey/helpers/selfFundedTransactionData";
+import sponsoredTransactionData from "@hey/helpers/sponsoredTransactionData";
+import { useAddAccountManagerMutation } from "@hey/indexer";
 import { Button } from "@hey/ui";
 import type { Dispatch, FC, SetStateAction } from "react";
 import { useState } from "react";
@@ -9,6 +12,8 @@ import toast from "react-hot-toast";
 import { useAccountStatus } from "src/store/non-persisted/useAccountStatus";
 import { useAccountStore } from "src/store/persisted/useAccountStore";
 import { isAddress } from "viem";
+import { sendEip712Transaction, sendTransaction } from "viem/zksync";
+import { useWalletClient } from "wagmi";
 
 interface AddAccountManagerProps {
   setShowAddManagerModal: Dispatch<SetStateAction<boolean>>;
@@ -21,11 +26,48 @@ const AddAccountManager: FC<AddAccountManagerProps> = ({
   const { isSuspended } = useAccountStatus();
   const [manager, setManager] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const { data: walletClient } = useWalletClient();
+
+  const onCompleted = (hash: string) => {
+    setIsLoading(false);
+    setShowAddManagerModal(false);
+    toast.success(hash);
+    toast.success("Account manager added");
+  };
 
   const onError = (error: any) => {
     setIsLoading(false);
     errorToast(error);
   };
+
+  const [addAccountManager] = useAddAccountManagerMutation({
+    onCompleted: async ({ addAccountManager }) => {
+      if (walletClient) {
+        try {
+          if (addAccountManager.__typename === "SponsoredTransactionRequest") {
+            const hash = await sendEip712Transaction(walletClient, {
+              account: walletClient.account,
+              ...sponsoredTransactionData(addAccountManager.raw)
+            });
+
+            return onCompleted(hash);
+          }
+
+          if (addAccountManager.__typename === "SelfFundedTransactionRequest") {
+            const hash = await sendTransaction(walletClient, {
+              account: walletClient.account,
+              ...selfFundedTransactionData(addAccountManager.raw)
+            });
+
+            return onCompleted(hash);
+          }
+        } catch (error) {
+          return onError(error);
+        }
+      }
+    },
+    onError
+  });
 
   const handleAddManager = async () => {
     if (!currentAccount) {
@@ -36,20 +78,21 @@ const AddAccountManager: FC<AddAccountManagerProps> = ({
       return toast.error(Errors.Suspended);
     }
 
-    try {
-      setIsLoading(true);
-      return await createChangeProfileManagersTypedData({
-        variables: {
-          request: {
-            changeManagers: [
-              { action: ChangeProfileManagerActionType.Add, address: manager }
-            ]
+    setIsLoading(true);
+
+    return await addAccountManager({
+      variables: {
+        request: {
+          address: manager,
+          permissions: {
+            canExecuteTransactions: true,
+            canSetMetadataUri: true,
+            canTransferNative: true,
+            canTransferTokens: true
           }
         }
-      });
-    } catch (error) {
-      onError(error);
-    }
+      }
+    });
   };
 
   return (
@@ -58,7 +101,7 @@ const AddAccountManager: FC<AddAccountManagerProps> = ({
         error={manager.length > 0 && !isAddress(manager)}
         hideDropdown={isAddress(manager)}
         onChange={(event) => setManager(event.target.value)}
-        onAccountSelected={(account) => setManager(account.owner)}
+        onAccountSelected={(account) => setManager(account.address)}
         placeholder={`${ADDRESS_PLACEHOLDER} or wagmi`}
         value={manager}
       />
