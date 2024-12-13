40dc60cad727a034ef7042b089c323ead3ad9859
Bigint <69431456+bigint@users.noreply.github.com>
2024-12-13 20:39:33 +0530

    Migrate to Lens v3

--- packages/helpers/resolveEns.ts
+++ packages/helpers/resolveEns.ts
@@ -2,8 +2,8 @@ import { HEY_API_URL } from "@hey/data/constants";
 import axios from "axios";
 
 /**
- * Resolves ENS names for the specified Ethereum addresses.
- * @param addresses An array of Ethereum addresses.
+ * Resolves ENS names for the specified EVM addresses.
+ * @param addresses An array of EVM addresses.
  * @returns An array of ENS names.
  */
 const resolveEns = async (addresses: string[]) => {
