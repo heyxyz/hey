64c61eff465ccea73fdcca19730ee209b906f31a
Bigint <69431456+bigint@users.noreply.github.com>
2025-02-16 22:09:24 +0530

    Update verification endpoint

--- apps/api/src/routes/lens/verification.ts
+++ apps/api/src/routes/lens/verification.ts
@@ -1,4 +1,6 @@
-import { HEY_APP } from "@hey/data/constants";
+import { APP_NAME, HEY_APP } from "@hey/data/constants";
+import { PermissionId } from "@hey/data/permissions";
+import prisma from "@hey/db/prisma/db/client";
 import type { Request, Response } from "express";
 import catchedError from "src/helpers/catchedError";
 import { heyWalletClient } from "src/helpers/heyWalletClient";
@@ -27,13 +29,31 @@ export const post = [
       return noBody(res);
     }
 
+    const source = HEY_APP;
+    const { nonce, deadline, account } = body;
+
     try {
-      const signature = await heyWalletClient.signTypedData({
-        domain,
-        types,
-        primaryType: "SourceStamp",
-        message: { source: HEY_APP, nonce: body.nonce, deadline: body.deadline }
-      });
+      const [signature, accountPermission] = await Promise.all([
+        heyWalletClient.signTypedData({
+          domain,
+          types,
+          primaryType: "SourceStamp",
+          message: { source, nonce, deadline }
+        }),
+        prisma.accountPermission.findFirst({
+          where: {
+            permissionId: PermissionId.Suspended,
+            accountAddress: account as string
+          }
+        })
+      ]);
+
+      if (accountPermission?.enabled) {
+        return res.status(200).json({
+          allowed: false,
+          reason: `Account is suspended on ${APP_NAME}`
+        });
+      }
 
       return res.status(200).json({ allowed: true, signature });
     } catch (error) {
