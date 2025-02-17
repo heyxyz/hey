b6dc7f5a702e409fd539e5541a14f0d1666d6ab5
Bigint <69431456+bigint@users.noreply.github.com>
2025-02-17 18:34:25 +0530

    Add GoogleAnalytics

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -2,11 +2,13 @@ import Providers from "@components/Common/Providers";
 import { heyFont } from "@helpers/fonts";
 import { AxiomWebVitals } from "next-axiom";
 import type { AppProps } from "next/app";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 import "../styles.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Providers>
+      <GoogleAnalytics strategy="afterInteractive" trackPageViews />
       <style global jsx>{`
         body {
           font-family: ${heyFont.style.fontFamily};
