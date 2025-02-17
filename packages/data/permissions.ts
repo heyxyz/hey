86b4a12007da22180634147e3daba39fe0c93c1d
Bigint <69431456+bigint@users.noreply.github.com>
2025-02-17 12:48:05 +0530

    Remove beta permission

--- packages/data/permissions.ts
+++ packages/data/permissions.ts
@@ -1,13 +1,11 @@
 export enum Permission {
   StaffPick = "staff-pick",
   Suspended = "suspended",
-  Verified = "verified",
-  Beta = "beta"
+  Verified = "verified"
 }
 
 export enum PermissionId {
   StaffPick = "73d2f48d-0291-4a36-adc2-9737057ad2b7",
   Suspended = "8ed8b26a-279d-4111-9d39-a40164b273a0",
-  Verified = "a0d6d247-50ef-419f-a045-54fa96054922",
-  Beta = "9ff8d280-db7b-4075-be0c-980f8293b77e"
+  Verified = "a0d6d247-50ef-419f-a045-54fa96054922"
 }
