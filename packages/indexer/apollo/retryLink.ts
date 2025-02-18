ab47ba399e79bf286f4526d30da6e4a14d841ebc
Bigint <69431456+bigint@users.noreply.github.com>
2025-02-18 19:54:10 +0530

    Increase retry attempts and enhance delay configuration in RetryLink

--- packages/indexer/apollo/retryLink.ts
+++ packages/indexer/apollo/retryLink.ts
@@ -2,8 +2,8 @@ import { RetryLink } from "@apollo/client/link/retry";
 
 // RetryLink is a link that retries requests based on the status code returned.
 const retryLink = new RetryLink({
-  attempts: { max: 2, retryIf: (error) => Boolean(error) },
-  delay: { initial: 100 }
+  attempts: { max: 3, retryIf: (error) => Boolean(error) },
+  delay: { initial: 200, max: Number.POSITIVE_INFINITY, jitter: true }
 });
 
 export default retryLink;
