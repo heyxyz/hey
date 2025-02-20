94daec04ccbc704b63b9473435c7a91160b1e915
Bigint <69431456+bigint@users.noreply.github.com>
2025-02-20 22:53:25 +0530

    Implement funding options in FundAccount component with transaction handling

--- apps/web/src/components/Shared/Fund/FundAccount.tsx
+++ apps/web/src/components/Shared/Fund/FundAccount.tsx
@@ -1,13 +1,26 @@
+import errorToast from "@helpers/errorToast";
 import { DEFAULT_COLLECT_TOKEN, STATIC_IMAGES_URL } from "@hey/data/constants";
-import { Card, Image } from "@hey/ui";
-import type { FC } from "react";
+import { Button, Card, Image, Input, Spinner } from "@hey/ui";
+import {
+  type ChangeEvent,
+  type FC,
+  type RefObject,
+  useRef,
+  useState
+} from "react";
+import toast from "react-hot-toast";
+import usePreventScrollOnNumberInput from "src/hooks/usePreventScrollOnNumberInput";
 import { useAccountStore } from "src/store/persisted/useAccountStore";
-import { type Address, formatUnits } from "viem";
-import { useAccount, useBalance } from "wagmi";
+import { type Address, formatUnits, parseEther } from "viem";
+import { useAccount, useBalance, useSendTransaction } from "wagmi";
 import Loader from "../Loader";
 
 const FundAccount: FC = () => {
   const { currentAccount } = useAccountStore();
+  const [amount, setAmount] = useState(2);
+  const [other, setOther] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+  usePreventScrollOnNumberInput(inputRef as RefObject<HTMLInputElement>);
   const { address } = useAccount();
 
   const { data: accountBalanceData, isLoading: accountBalanceLoading } =
@@ -24,6 +37,8 @@ const FundAccount: FC = () => {
       query: { refetchInterval: 2000 }
     });
 
+  const { sendTransactionAsync, isPending } = useSendTransaction();
+
   if (accountBalanceLoading || walletBalanceLoading) {
     return <Loader message="Loading balance..." className="my-10" />;
   }
@@ -35,6 +50,29 @@ const FundAccount: FC = () => {
   const walletBalance = walletBalanceData
     ? Number.parseFloat(formatUnits(walletBalanceData.value, 18)).toFixed(2)
     : 0;
+
+  const onOtherAmount = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value as unknown as number;
+    setAmount(value);
+  };
+
+  const handleSetAmount = (amount: number) => {
+    setAmount(amount);
+    setOther(false);
+  };
+
+  const handleFund = async () => {
+    try {
+      const hash = await sendTransactionAsync({
+        to: currentAccount?.address,
+        value: parseEther(amount.toString())
+      });
+
+      return toast.success(`Transaction sent: ${hash}`);
+    } catch (error) {
+      errorToast(error);
+    }
+  };
 
   return (
     <div className="m-5">
@@ -54,7 +92,69 @@ const FundAccount: FC = () => {
           <b>Purchase</b>
         </div>
         <div className="divider" />
-        <div className="p-5">gm {walletBalance}</div>
+        <div className="space-y-5 p-5">
+          <div className="flex space-x-4">
+            <Button
+              className="w-full"
+              onClick={() => handleSetAmount(2)}
+              outline={amount !== 2}
+            >
+              $2
+            </Button>
+            <Button
+              className="w-full"
+              onClick={() => handleSetAmount(5)}
+              outline={amount !== 5}
+            >
+              $5
+            </Button>
+            <Button
+              className="w-full"
+              onClick={() => handleSetAmount(10)}
+              outline={amount !== 10}
+            >
+              $10
+            </Button>
+            <Button
+              className="w-full"
+              onClick={() => {
+                handleSetAmount(other ? 2 : 20);
+                setOther(!other);
+              }}
+              outline={!other}
+            >
+              Other
+            </Button>
+          </div>
+          {other ? (
+            <div>
+              <Input
+                className="no-spinner"
+                max={1000}
+                onChange={onOtherAmount}
+                placeholder="300"
+                ref={inputRef}
+                type="number"
+                value={amount}
+              />
+            </div>
+          ) : null}
+          {isPending ? (
+            <Button
+              className="flex w-full justify-center"
+              disabled
+              icon={<Spinner className="my-1" size="xs" />}
+            />
+          ) : Number(walletBalance) < amount ? (
+            <Button disabled className="w-full">
+              <b>Insufficient balance</b>
+            </Button>
+          ) : (
+            <Button disabled={!amount} className="w-full" onClick={handleFund}>
+              <b>Fund {amount} GHO</b>
+            </Button>
+          )}
+        </div>
       </Card>
     </div>
   );
