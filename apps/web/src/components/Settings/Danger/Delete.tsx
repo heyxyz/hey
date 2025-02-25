8c30e51e063f2465cb3e3155bb3fb8b7e085f75e
Bigint <69431456+bigint@users.noreply.github.com>
2025-02-25 14:21:36 +0530

    refactor: Rename isLoading to loading in states

--- apps/web/src/components/Settings/Danger/Delete.tsx
+++ apps/web/src/components/Settings/Danger/Delete.tsx
@@ -26,7 +26,7 @@ import { useAccountStore } from "src/store/persisted/useAccountStore";
 const DeleteSettings: FC = () => {
   const { currentAccount } = useAccountStore();
   const [showWarningModal, setShowWarningModal] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
   const handleWrongNetwork = useHandleWrongNetwork();
 
   const handleDelete = async () => {
@@ -34,9 +34,9 @@ const DeleteSettings: FC = () => {
       return toast.error(Errors.SignWallet);
     }
 
-    setIsLoading(true);
+    setLoading(true);
     await handleWrongNetwork();
-    setIsLoading(false);
+    setLoading(false);
     return toast.success("Feature not implemented yet");
   };
 
@@ -83,9 +83,9 @@ const DeleteSettings: FC = () => {
           </p>
         </div>
         <Button
-          disabled={isLoading}
+          disabled={loading}
           icon={
-            isLoading ? (
+            loading ? (
               <Spinner size="xs" variant="danger" />
             ) : (
               <TrashIcon className="size-5" />
@@ -94,7 +94,7 @@ const DeleteSettings: FC = () => {
           onClick={() => setShowWarningModal(true)}
           variant="danger"
         >
-          {isLoading ? "Deleting..." : "Delete your account"}
+          {loading ? "Deleting..." : "Delete your account"}
         </Button>
       </div>
       <Modal
