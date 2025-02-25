5e4f1e671c9ded553724bfc48cc67f39b2526d0a
Bigint <69431456+bigint@users.noreply.github.com>
2025-02-25 23:23:20 +0530

    feat: Add sponsorship contract link in staff overview

--- apps/web/src/components/Staff/Overview/Sponsorship.tsx
+++ apps/web/src/components/Staff/Overview/Sponsorship.tsx
@@ -1,7 +1,8 @@
 import Loader from "@components/Shared/Loader";
-import { HEY_SPONSOR } from "@hey/data/constants";
+import { BLOCK_EXPLORER_URL, HEY_SPONSOR } from "@hey/data/constants";
 import { useSponsorshipQuery } from "@hey/indexer";
 import { Card, CardHeader, ErrorMessage, NumberedStat } from "@hey/ui";
+import Link from "next/link";
 import type { FC } from "react";
 
 const Sponsorship: FC = () => {
@@ -20,6 +21,14 @@ const Sponsorship: FC = () => {
           <ErrorMessage error={error} title="Failed to load sponsorship" />
         ) : (
           <div className="space-y-5">
+            <div className="linkify font-bold">
+              <Link
+                href={`${BLOCK_EXPLORER_URL}/address/${data?.sponsorship?.address}`}
+                target="_blank"
+              >
+                Open Sponsorship Contract in Explorer
+              </Link>
+            </div>
             <NumberedStat
               count={data?.sponsorship?.balance}
               name="Balance"
