8ad5a5659151c30b03ae42669fb825da659dc97f
Bigint <69431456+bigint@users.noreply.github.com>
2025-02-26 10:54:00 +0530

    chore: Update Lens RPC endpoints for testnet and mainnet

--- packages/data/rpcs.ts
+++ packages/data/rpcs.ts
@@ -1,1 +1,2 @@
-export const LENS_TESTNET_RPCS = ["https://rpc.testnet.lens.dev"];
+export const LENS_TESTNET_RPCS = ["https://rpc.yoginth.com"];
+export const LENS_MAINNET_RPCS = ["https://rpc.hey.xyz"];
