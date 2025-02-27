f3bbdc4811780851fe59a36e1489400b2d492ec9
Bigint <69431456+bigint@users.noreply.github.com>
2025-02-27 14:48:38 +0530

    refactor: Simplify account verification flow and add verification form modal

--- apps/web/src/components/Settings/Account/Verification.tsx
+++ apps/web/src/components/Settings/Account/Verification.tsx
@@ -1,95 +1,44 @@
-import { MinusCircleIcon } from "@heroicons/react/24/outline";
-import { CheckBadgeIcon, CheckCircleIcon } from "@heroicons/react/24/solid";
-import getNumberOfDaysFromDate from "@hey/helpers/datetime/getNumberOfDaysFromDate";
-import { useAccountStatsQuery } from "@hey/indexer";
-import { Button, Card, H5 } from "@hey/ui";
-import type { FC } from "react";
-import toast from "react-hot-toast";
+import isVerified from "@helpers/isVerified";
+import { CheckBadgeIcon } from "@heroicons/react/24/solid";
+import { Button, Card, H5, Modal } from "@hey/ui";
+import { type FC, useState } from "react";
 import { useAccountStore } from "src/store/persisted/useAccountStore";
-import { hydrateVerifiedMembers } from "src/store/persisted/useVerifiedMembersStore";
 
 const Verification: FC = () => {
   const { currentAccount } = useAccountStore();
-  const { verifiedMembers } = hydrateVerifiedMembers();
-
-  const { data, loading } = useAccountStatsQuery({
-    variables: { request: { account: currentAccount?.address } }
-  });
-
-  if (!currentAccount) {
-    return null;
-  }
-
-  if (loading || !data) {
-    return null;
-  }
-
-  const hasMetFollowersRequirement =
-    (data?.accountStats?.graphFollowStats?.followers || 0) >= 3000;
-  const hasMetPublicationsRequirement =
-    (data?.accountStats?.feedStats.posts || 0) >= 50;
-  const hasMetTimeRequirement =
-    30 < -getNumberOfDaysFromDate(currentAccount?.createdAt);
-
-  const hasAllRequirements =
-    hasMetFollowersRequirement &&
-    hasMetPublicationsRequirement &&
-    hasMetTimeRequirement;
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <Card className="space-y-2 p-5">
       <H5>Verified</H5>
-      {verifiedMembers.includes(currentAccount?.address) ? (
+      {isVerified(currentAccount?.address) ? (
         <div className="flex items-center space-x-1.5">
           <span>Believe it. Yes, you're really verified.</span>
           <CheckBadgeIcon className="size-5 text-brand-500" />
         </div>
       ) : (
-        <div className="space-y-2">
-          <div className="flex items-center space-x-1.5">
-            {hasMetFollowersRequirement ? (
-              <CheckCircleIcon className="size-5" />
-            ) : (
-              <MinusCircleIcon className="size-5" />
-            )}
-            <div>
-              You have {hasMetFollowersRequirement ? "met" : "not met"} the{" "}
-              <b>3000 followers</b> requirement.
-            </div>
+        <div className="space-y-4">
+          <div>
+            We're here to assist you in getting verified. Please complete the
+            form below, and we will respond promptly. Please note that we might
+            take some time.
           </div>
-          <div className="flex items-center space-x-1.5">
-            {hasMetPublicationsRequirement ? (
-              <CheckCircleIcon className="size-5" />
-            ) : (
-              <MinusCircleIcon className="size-5" />
-            )}
-            <div>
-              You have {hasMetPublicationsRequirement ? "met" : "not met"} the{" "}
-              <b>50 publications</b> requirement.
-            </div>
-          </div>
-          <div className="flex items-center space-x-1.5">
-            {hasMetTimeRequirement ? (
-              <CheckCircleIcon className="size-5" />
-            ) : (
-              <MinusCircleIcon className="size-5" />
-            )}
-            <div>
-              You have {hasMetTimeRequirement ? "met" : "not met"} the{" "}
-              <b>30 days</b> requirement.
-            </div>
-          </div>
-          <Button
-            className="!mt-4"
-            disabled={!hasAllRequirements}
-            onClick={() => {
-              toast.success("Verification request sent to staff!");
-            }}
-          >
-            Request for account verification
-          </Button>
+          <Button onClick={() => setShowModal(true)}>Fill out the form</Button>
         </div>
       )}
+      <Modal
+        show={showModal}
+        onClose={() => setShowModal(false)}
+        title="Verification Form"
+        size="lg"
+      >
+        <iframe
+          title="Verification Form"
+          src="https://yoginth.notion.site/ebd/1a0191ff51ed801faf66e1eabf0bf691"
+          width="100%"
+          height="600"
+        />
+      </Modal>
     </Card>
   );
 };
