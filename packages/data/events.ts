406fa511fc4dd6156f78fc86d612b056075b6036
Bigint <69431456+bigint@users.noreply.github.com>
2025-02-28 13:00:48 +0530

    feat: Add simple analytics events

---
+++ packages/data/events.ts
@@ -0,0 +1,19 @@
+export const Events = {
+  Account: {
+    Signup: "signup",
+    Login: "login",
+    Follow: "follow",
+    Unfollow: "unfollow"
+  },
+  Post: {
+    Post: "post",
+    Comment: "comment",
+    Like: "like",
+    Repost: "repost",
+    Collect: "collect"
+  },
+  Group: {
+    Join: "join",
+    Leave: "leave"
+  }
+};
