9c4aff1e7d77c5800e4c7c1dd1830fa34a9f4bd4
Bigint <69431456+bigint@users.noreply.github.com>
2025-03-01 13:11:16 +0530

    chore: Update Lens RPC endpoints with Alchemy provider

--- packages/data/rpcs.ts
+++ packages/data/rpcs.ts
@@ -1,2 +1,8 @@
-export const LENS_TESTNET_RPCS = ["https://rpc.yoginth.com"];
-export const LENS_MAINNET_RPCS = ["https://rpc.hey.xyz"];
+export const LENS_TESTNET_RPCS = [
+  "https://lens-sepolia.g.alchemy.com/v2/Xx-4a1SyWtS9U4h0cEuRmvgYtGeVOlv7",
+  "https://rpc.yoginth.com"
+];
+export const LENS_MAINNET_RPCS = [
+  "https://lens-mainnet.g.alchemy.com/v2/Xx-4a1SyWtS9U4h0cEuRmvgYtGeVOlv7",
+  "https://rpc.hey.xyz"
+];
