d02410740f2a20b0f8d9bba37e291a0bcfc22ddc
Bigint <69431456+bigint@users.noreply.github.com>
2025-03-02 15:42:44 +0530

    refactor: Enhance HeyPro contract with AccessControl and NFT management

--- packages/contracts/contracts/HeyPro.sol
+++ packages/contracts/contracts/HeyPro.sol
@@ -3,53 +3,66 @@ pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract HeyPro is ERC721Enumerable, Ownable {
+contract HeyPro is ERC721Enumerable, Ownable, AccessControl {
     struct NFTData {
         uint256 expiry;
         string name;
     }
 
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+
     mapping(uint256 => NFTData) public nftDetails;
 
-    constructor() ERC721("HeyPro", "PRO") Ownable(msg.sender) {}
+    constructor() ERC721("HeyPro", "PRO") Ownable(msg.sender) {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(ADMIN_ROLE, msg.sender);
+    }
 
     function mint() external {
         uint256 tokenId = totalSupply() + 1;
         uint256 currentTime = block.timestamp;
         uint256 expiry = currentTime + 30 days;
 
-        // NFT name derived from epoch timestamp (10 chars)
         string memory nftName = _uintToString(currentTime);
 
         _safeMint(msg.sender, tokenId);
         nftDetails[tokenId] = NFTData(expiry, nftName);
     }
 
-    function extendSubscription() external {
-        uint256 lastTokenId = balanceOf(msg.sender);
-        require(lastTokenId > 0, "You don't own any NFTs");
+    function burnExpiredNFTs() external onlyRole(ADMIN_ROLE) {
+        uint256 total = totalSupply();
+        uint256 burnedCount = 0;
 
-        uint256 tokenId = tokenOfOwnerByIndex(msg.sender, lastTokenId - 1);
-        require(
-            nftDetails[tokenId].expiry > block.timestamp,
-            "Subscription expired"
-        );
+        for (uint256 i = total; i > 0; i--) {
+            if (_tokenExists(i) && nftDetails[i].expiry <= block.timestamp) {
+                _burn(i);
+                delete nftDetails[i];
+                burnedCount++;
+            }
+        }
 
-        // Extend by 30 days
-        nftDetails[tokenId].expiry += 30 days;
+        require(burnedCount > 0, "No expired NFTs to burn");
     }
 
-    function getNFTDetails(
-        uint256 tokenId
-    ) external view returns (string memory, uint256) {
-        require(_isValidToken(tokenId), "Token does not exist");
-        return (nftDetails[tokenId].name, nftDetails[tokenId].expiry);
+    function addAdmin(address admin) external onlyOwner {
+        grantRole(ADMIN_ROLE, admin);
     }
 
-    function _uintToString(
-        uint256 value
-    ) internal pure returns (string memory) {
+    function removeAdmin(address admin) external onlyOwner {
+        revokeRole(ADMIN_ROLE, admin);
+    }
+
+    function _tokenExists(uint256 tokenId) internal view returns (bool) {
+        try this.ownerOf(tokenId) returns (address) {
+            return true;
+        } catch {
+            return false;
+        }
+    }
+
+    function _uintToString(uint256 value) internal pure returns (string memory) {
         bytes memory buffer = new bytes(10);
         for (uint256 i = 9; i >= 0; i--) {
             buffer[i] = bytes1(uint8(48 + (value % 10)));
@@ -59,7 +72,7 @@ contract HeyPro is ERC721Enumerable, Ownable {
         return string(buffer);
     }
 
-    function _isValidToken(uint256 tokenId) internal view returns (bool) {
-        return tokenId > 0 && tokenId <= totalSupply();
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721Enumerable, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
